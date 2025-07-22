0f33ce9b6e51a540bed239ce777bbd0366ae4d68
陈卓 <chenzhuo63@hisilicon.com>
2025-07-22 01:47:52 +0000

    !457 add ut for PreGraph Merge pull request !457 from 陈卓/cz_master_test

--- tests/ut/interface/passes/src/test_pre_graph.cpp
+++ tests/ut/interface/passes/src/test_pre_graph.cpp
@@ -13,6 +13,9 @@
  * \brief Unit test for PreGraph pass.
  */
 
+#include <fstream>
+#include <vector>
+#include <string>
 #include "gtest/gtest.h"
 #include "operation/tilefwk_op.h"
 #include "interface/function/function.h"
@@ -22,11 +25,12 @@
 #include "interface/configs/config_manager.h"
 #include "passes/tile_graph_pass/pre_graph.h"
 #include "ut_json/ut_json_tool.h"
-#include <fstream>
-#include <vector>
-#include <string>
+#include "computational_graph_builder.h"
 
 using namespace npu::tile_fwk;
+
+namespace npu {
+namespace tile_fwk {
 
 void PrintGraphInfoPreGraph(Function* func, int& memoryMapSize, std::set<int>& tensorMagicWithColorSet) {
     std::cout << "func->Operations().size() = "  << func->Operations().size() << std::endl;
@@ -86,6 +90,176 @@ public:
         config::SetPlatformConfig("ENABLE_COST_MODEL", false);
     }
     void TearDown() override {}
+
+    // input[B, N, S] --> Copy_In --> input_ub[1, 1, S] --> Transpose_Datamove --> outputGm, [N, B, S](i, j, 0) --> Assemble --> output[N, B, S]
+    void TileExpandTransposeDatamove(ComputationalGraphBuilder &G, const int B, const int N, const int S, bool isInner = false) {
+        std::vector<int> tileShape{1, 1, S};
+        // 所有transpose_datamove输出partial结果都要指向同一个raw tensor
+        G.AddTensor(DataType::DT_FP32, {N, B, S}, "temp_out");
+        auto tempOut = G.GetTensor("temp_out");
+        tempOut->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+        auto incast = G.GetTensor("input");
+        for (int i = 0; i < B; i++) {
+            for (int j = 0; j < N; j++) {
+                std::vector<int> offset = {i, j, 0};
+                std::vector<int> offsetNew = {j, i, 0};
+                int subgraphId = i * N + j;
+
+                std::string input_ub = "input_ub_" + std::to_string(subgraphId);
+                G.AddTensor(DataType::DT_FP32, tileShape, input_ub);
+                auto tensorUb = G.GetTensor(input_ub);
+                tensorUb->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+                G.AddOp(Opcode::OP_COPY_IN, {"input"}, {input_ub}, "Ub_Copy_In_" + std::to_string(subgraphId));
+                auto copyInOp = G.GetOp("Ub_Copy_In_" + std::to_string(subgraphId));
+                auto attrCopyIn = std::make_shared<CopyOpAttribute>(
+                    OpImmediate::Specified(offset), MemoryType::MEM_UB,
+                    OpImmediate::Specified(incast->GetShape()), OpImmediate::Specified(incast->tensor->GetRawShape()));
+                copyInOp->SetOpAttribute(attrCopyIn);
+                copyInOp->UpdateSubgraphID(subgraphId);
+
+                std::string outputPartial = "output_ddr_" + std::to_string(subgraphId);
+                G.AddTensor(DataType::DT_FP32, tileShape, outputPartial);
+                auto outputGm = G.GetTensor(outputPartial);
+                outputGm->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+                outputGm->tensor = tempOut->tensor;
+                outputGm->UpdateOffset(offsetNew);
+                G.AddOp(Opcode::OP_TRANSPOSE_DATAMOVE, {input_ub}, {outputPartial}, "Transpose_Datamove_" + std::to_string(subgraphId));
+                auto transposeOp = G.GetOp("Transpose_Datamove_" + std::to_string(subgraphId));
+                transposeOp->UpdateSubgraphID(subgraphId);
+
+                G.AddOp(Opcode::OP_ASSEMBLE, {outputPartial}, {"output"}, "Assemble_" + std::to_string(subgraphId));
+                auto attrAssemble = std::make_shared<AssembleOpAttribute>(MemoryType::MEM_DEVICE_DDR, offsetNew);
+                auto assembleOp = G.GetOp("Assemble_" + std::to_string(subgraphId));
+                assembleOp->SetOpAttribute(attrAssemble);
+                assembleOp->UpdateSubgraphID(subgraphId);
+
+                if (isInner) {
+                    auto outInnerTemp = G.GetTensor("outInnerTemp");
+                    G.AddOp(Opcode::OP_ASSEMBLE, {outputPartial}, {"outInnerTemp"}, "Assemble_Inner_" + std::to_string(subgraphId));
+                    auto attrAssembleInner = std::make_shared<AssembleOpAttribute>(MemoryType::MEM_DEVICE_DDR, offsetNew);
+                    auto assembleOpInner = G.GetOp("Assemble_Inner_" + std::to_string(subgraphId));
+                    assembleOpInner->SetOpAttribute(attrAssembleInner);
+                    assembleOpInner->UpdateSubgraphID(subgraphId);
+                }
+            }
+        }
+    }
+
+    // outInnerTemp[N, B, S] --> Copy_In --> input_ub [1, B, S] --> Exp --> output_ub --> Copy_Out --> output2[N, B, S]
+    void TileExpandExp(ComputationalGraphBuilder &G, const int B, const int N, const int S) {
+        auto outInnerTemp = G.GetTensor("outInnerTemp");
+        auto output2 = G.GetTensor("output2");
+        std::vector<int> tileShape{1, B, S};
+        for (int i = 0; i < N; i++) {
+            std::vector<int> offset = {i, 0, 0};
+            int subgraphId = B * N + i;
+
+            std::string input_ub = "input_ub_" + std::to_string(subgraphId);
+            G.AddTensor(DataType::DT_FP32, tileShape, input_ub);
+            auto tensorUb = G.GetTensor(input_ub);
+            tensorUb->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+            G.AddOp(Opcode::OP_COPY_IN, {"outInnerTemp"}, {input_ub}, "Ub_Copy_In_" + std::to_string(subgraphId));
+            auto copyInOp = G.GetOp("Ub_Copy_In_" + std::to_string(subgraphId));
+            auto attrCopyIn = std::make_shared<CopyOpAttribute>(
+                OpImmediate::Specified(offset), MemoryType::MEM_UB,
+                OpImmediate::Specified(outInnerTemp->GetShape()), OpImmediate::Specified(outInnerTemp->tensor->GetRawShape()));
+            copyInOp->SetOpAttribute(attrCopyIn);
+            copyInOp->UpdateSubgraphID(subgraphId);
+
+            std::string outputExpUb = "output_exp_" + std::to_string(subgraphId);
+            G.AddTensor(DataType::DT_FP32, tileShape, outputExpUb);
+            auto outputGm = G.GetTensor(outputExpUb);
+            outputGm->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+            G.AddOp(Opcode::OP_EXP, {input_ub}, {outputExpUb}, "Exp_" + std::to_string(subgraphId));
+            auto expOp = G.GetOp("Exp_" + std::to_string(subgraphId));
+            expOp->UpdateSubgraphID(subgraphId);
+
+            G.AddOp(Opcode::OP_COPY_OUT, {outputExpUb}, {"output2"}, "Copy_Out_" + std::to_string(subgraphId));
+            auto copyOutOp = G.GetOp("Copy_Out_" + std::to_string(subgraphId));
+            auto attrCopyOut = std::make_shared<CopyOpAttribute>(
+                OpImmediate::Specified(offset), MemoryType::MEM_UB,
+                OpImmediate::Specified(output2->GetShape()), OpImmediate::Specified(output2->tensor->GetRawShape()));
+            copyOutOp->SetOpAttribute(attrCopyOut);
+            copyOutOp->UpdateSubgraphID(subgraphId);
+        }
+    }
+
+    /*
+    [32, 32] --> View --> [16, 16] --> Add --> [16, 16] --> Assemble --> addOutUb[32, 32] -->
+                                                    \--> Copy_Out --> out1
+    */
+    void TileExpandAdd(ComputationalGraphBuilder &G, const int N, const int T) {
+        std::vector<int> tileShape{T, T};
+        auto a = G.GetTensor("a");
+        auto b = G.GetTensor("b");
+        auto out1 = G.GetTensor("out1");
+        auto addOutUb = G.GetTensor("addOutUb");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                std::vector<int> offset = {i * T, j * T};
+                int idx = i * N + j;
+
+                std::string localA = "a_" + std::to_string(idx);
+                G.AddTensor(DataType::DT_FP32, tileShape, localA);
+                auto tensorA = G.GetTensor(localA);
+                tensorA->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+                G.AddOp(Opcode::OP_COPY_IN, {"a"}, {localA}, "Copy_In_A_" + std::to_string(idx));
+                auto copyInA = G.GetOp("Copy_In_A_" + std::to_string(idx));
+                auto attrCopyInA = std::make_shared<CopyOpAttribute>(
+                    OpImmediate::Specified(offset), MemoryType::MEM_UB,
+                    OpImmediate::Specified(a->GetShape()), OpImmediate::Specified(a->tensor->GetRawShape()));
+                copyInA->SetOpAttribute(attrCopyInA);
+                copyInA->UpdateSubgraphID(0);
+
+                std::string localB = "b_" + std::to_string(idx);
+                G.AddTensor(DataType::DT_FP32, tileShape, localB);
+                auto tensorB = G.GetTensor(localB);
+                tensorB->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+                G.AddOp(Opcode::OP_COPY_IN, {"b"}, {localB}, "Copy_In_B_" + std::to_string(idx));
+                auto copyInB = G.GetOp("Copy_In_B_" + std::to_string(idx));
+                auto attrCopyInB = std::make_shared<CopyOpAttribute>(
+                    OpImmediate::Specified(offset), MemoryType::MEM_UB,
+                    OpImmediate::Specified(b->GetShape()), OpImmediate::Specified(b->tensor->GetRawShape()));
+                copyInB->SetOpAttribute(attrCopyInB);
+                copyInB->UpdateSubgraphID(0);
+
+                std::string localAddOut = "add_out_" + std::to_string(idx);
+                G.AddTensor(DataType::DT_FP32, tileShape, localAddOut);
+                G.AddOp(Opcode::OP_ADD, {localA, localB}, {localAddOut}, "Add_" + std::to_string(idx));
+                auto tensorAddOut = G.GetTensor(localAddOut);
+                tensorAddOut->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+                tensorAddOut->tensor = addOutUb->tensor;
+                tensorAddOut->UpdateOffset(offset);
+                auto addOp = G.GetOp("Add_" + std::to_string(idx));
+                addOp->UpdateSubgraphID(0);
+
+                G.AddOp(Opcode::OP_ASSEMBLE, {localAddOut}, {"addOutUb"}, "Assemble_" + std::to_string(idx));
+                auto attrAssemble = std::make_shared<AssembleOpAttribute>(MemoryType::MEM_UB, offset);
+                auto assembleOp = G.GetOp("Assemble_" + std::to_string(idx));
+                assembleOp->SetOpAttribute(attrAssemble);
+                assembleOp->UpdateSubgraphID(0);
+
+                G.AddOp(Opcode::OP_COPY_OUT, {localAddOut}, {"out1"}, "Copy_Out_" + std::to_string(idx));
+                auto copyOutOp = G.GetOp("Copy_Out_" + std::to_string(idx));
+                auto attrCopyOut = std::make_shared<CopyOpAttribute>(
+                    OpImmediate::Specified(offset), MemoryType::MEM_UB,
+                    OpImmediate::Specified(out1->GetShape()), OpImmediate::Specified(out1->tensor->GetRawShape()));
+                copyOutOp->SetOpAttribute(attrCopyOut);
+                copyOutOp->UpdateSubgraphID(0);
+            }
+        }
+    }
+
+    int CountAssemble(Function &function) {
+        int result = 0;
+        for (auto &op : function.Operations()) {
+            std::cout << op.GetOpcodeStr() << " " << op.GetOpMagic() << std::endl;
+            if (op.GetOpcode() == Opcode::OP_ASSEMBLE) {
+                result++;
+            }
+        }
+        return result;
+    }
 };
 
 TEST_F(PreGraphTest, TestVCPartition) {
@@ -391,3 +565,196 @@ TEST_F(PreGraphTest, TestROWMAX_SINGLE) {
     EXPECT_EQ(updated_operations.size(), opSize) << "After the Pass, there should be 29 operations";
     EXPECT_EQ(tensorMagicWithColorSet.size() > 0, true) << "There should be many tensor magic with color";
 }
+
+TEST_F(PreGraphTest, TestTransposeDatamove) {
+    int B = 3;
+    int N = 2;
+    int S = 128;
+    int NUM_1 = 1;
+
+    std::vector<int> shape0{B, N, S};
+    std::vector<int> shape1{N, B, S};
+    std::vector<int> tiledShape{NUM_1, NUM_1, S};
+
+    ComputationalGraphBuilder G;
+    G.AddTensor(DataType::DT_FP32, shape0, "input");
+    G.AddTensor(DataType::DT_FP32, shape1, "output");
+    // [16, 16] --> View --> [8, 8] --> Sub --> [8, 8] --> Assemble --> [16, 16]
+    TileExpandTransposeDatamove(G, B, N, S);
+
+    auto input = G.GetTensor("input");
+    input->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    input->nodetype = NodeType::INCAST;
+    auto output = G.GetTensor("output");
+    output->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    output->nodetype = NodeType::OUTCAST;
+
+    G.SetInCast({"input"});
+    G.SetOutCast({"output"});
+    Function *function = G.GetFunction();
+    const int SUBGRAPH_NUM = 6;
+    function->SetTotalSubGraphCount(SUBGRAPH_NUM);
+    // 确认构图完毕
+    constexpr int opNumBefore = 18;
+    constexpr int assembleNumBefore = 6;
+    auto assembleNumCountBefore = CountAssemble(*function);
+    EXPECT_EQ(function->Operations().size(), opNumBefore) << opNumBefore << " operations before pass";
+    EXPECT_EQ(assembleNumCountBefore, assembleNumBefore) << assembleNumBefore << " OP_ASSEMBLE before pass";
+    std::cout << "Build Graph Done." << std::endl;
+    /*
+    dump graph before Pass
+    function->DumpJsonFile(jsonFilePath);
+    */
+    // 单独执行pass
+    npu::tile_fwk::PreGraphPass preGraph;
+    preGraph.PreCheck(*function);
+    preGraph.RunOnFunction(*function);
+    preGraph.PostCheck(*function);
+    std::cout << "Run Pass Done." << std::endl;
+    /*
+    dump graph after Pass
+    function->DumpJsonFile(jsonFilePath);
+    */
+    constexpr int opNumAfter = 12;
+    constexpr int assembleNumafter = 0;
+    auto assembleNumCountAfter = CountAssemble(*function);
+    EXPECT_EQ(function->Operations().size(), opNumAfter) << opNumAfter << " operations after pass";
+    EXPECT_EQ(assembleNumCountAfter, assembleNumafter) << assembleNumafter << " OP_ASSEMBLE before pass";
+    EXPECT_EQ(opNumBefore - opNumAfter, assembleNumCountBefore - assembleNumCountAfter) << " only OP_ASSEMBLE should be removed";
+}
+
+TEST_F(PreGraphTest, TestTransposeDatamoveExp) {
+    int B = 3;
+    int N = 2;
+    int S = 128;
+
+    std::vector<int> shape0{B, N, S};
+    std::vector<int> shape1{N, B, S};
+
+    ComputationalGraphBuilder G;
+    G.AddTensor(DataType::DT_FP32, shape0, "input");
+    G.AddTensor(DataType::DT_FP32, shape1, "output");
+    G.AddTensor(DataType::DT_FP32, shape1, "output2");
+    G.AddTensor(DataType::DT_FP32, shape1, "outInnerTemp");
+    // [16, 16] --> View --> [8, 8] --> Sub --> [8, 8] --> Assemble --> [16, 16]
+    TileExpandTransposeDatamove(G, B, N, S, true);
+    TileExpandExp(G, B, N, S);
+
+    auto input = G.GetTensor("input");
+    input->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    input->nodetype = NodeType::INCAST;
+    auto output = G.GetTensor("output");
+    output->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    output->nodetype = NodeType::OUTCAST;
+    auto output2 = G.GetTensor("output2");
+    output2->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    output2->nodetype = NodeType::OUTCAST;
+
+    auto outInnerTemp = G.GetTensor("outInnerTemp");
+    outInnerTemp->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+
+    G.SetInCast({"input"});
+    G.SetOutCast({"output"});
+    Function *function = G.GetFunction();
+    const int SUBGRAPH_NUM = 8;
+    function->SetTotalSubGraphCount(SUBGRAPH_NUM);
+    // 确认构图完毕
+    constexpr int opNumBefore = 30;
+    constexpr int assembleNumBefore = 12;
+    auto assembleNumCountBefore = CountAssemble(*function);
+    EXPECT_EQ(function->Operations().size(), opNumBefore) << opNumBefore << " operations before pass";
+    EXPECT_EQ(assembleNumCountBefore, assembleNumBefore) << assembleNumBefore << " OP_ASSEMBLE before pass";
+    std::cout << "Build Graph Done." << std::endl;
+    /*
+    dump graph before Pass
+    function->DumpJsonFile(jsonFilePath);
+    */
+    // 单独执行pass
+    npu::tile_fwk::PreGraphPass preGraph;
+    preGraph.PreCheck(*function);
+    preGraph.RunOnFunction(*function);
+    preGraph.PostCheck(*function);
+    std::cout << "Run Pass Done." << std::endl;
+    /*
+    dump graph after Pass
+    function->DumpJsonFile(jsonFilePath);
+    */
+    constexpr int opNumAfter = 18;
+    constexpr int assembleNumafter = 0;
+    auto assembleNumCountAfter = CountAssemble(*function);
+    EXPECT_EQ(function->Operations().size(), opNumAfter) << opNumAfter << " operations after pass";
+    EXPECT_EQ(assembleNumCountAfter, assembleNumafter) << assembleNumafter << " OP_ASSEMBLE before pass";
+    EXPECT_EQ(opNumBefore - opNumAfter, assembleNumCountBefore - assembleNumCountAfter) << " only OP_ASSEMBLE should be removed";
+}
+
+
+TEST_F(PreGraphTest, TestAddExp) {
+    int N = 2;
+    int T = 16;
+    std::vector<int> shape0{N * T, N * T};
+    // std::vector<int> shape1{T, T};
+    ComputationalGraphBuilder G;
+    G.AddTensor(DataType::DT_FP32, shape0, "a");
+    G.AddTensor(DataType::DT_FP32, shape0, "b");
+    G.AddTensor(DataType::DT_FP32, shape0, "out1");
+    G.AddTensor(DataType::DT_FP32, shape0, "out2");
+    G.AddTensor(DataType::DT_FP32, shape0, "addOutUb");
+    G.AddTensor(DataType::DT_FP32, shape0, "expOutUb");
+    
+    auto a = G.GetTensor("a");
+    a->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    auto b = G.GetTensor("b");
+    b->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    auto out1 = G.GetTensor("out1");
+    out1->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    auto out2 = G.GetTensor("out2");
+    out2->SetMemoryTypeBoth(MemoryType::MEM_DEVICE_DDR, true);
+    auto addOutUb = G.GetTensor("addOutUb");
+    addOutUb->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+    auto expOutUb = G.GetTensor("expOutUb");
+    expOutUb->SetMemoryTypeBoth(MemoryType::MEM_UB, true);
+    /*
+    [32, 32] --> View --> [16, 16] --> Add --> [16, 16] --> Assemble --> addOutUb[32, 32] --> Exp --> expOutUb[32, 32] --> Copy_Out --> out2
+                                                    \--> Copy_Out --> out1
+    */
+    TileExpandAdd(G, N, T);
+
+    G.AddOp(Opcode::OP_EXP, {"addOutUb"}, {"expOutUb"}, "Exp_Op");
+    auto expOp = G.GetOp("Exp_Op");
+    expOp->UpdateSubgraphID(0);
+
+    G.AddOp(Opcode::OP_COPY_OUT, {"expOutUb"}, {"out2"}, "Copy_Out_Exp");
+    auto copyOutOp = G.GetOp("Copy_Out_Exp");
+    auto attrCopyOut = std::make_shared<CopyOpAttribute>(
+        OpImmediate::Specified(std::vector<int> {0, 0}), MemoryType::MEM_UB,
+        OpImmediate::Specified(out2->GetShape()), OpImmediate::Specified(out2->tensor->GetRawShape()));
+    copyOutOp->SetOpAttribute(attrCopyOut);
+    copyOutOp->UpdateSubgraphID(0);
+    
+    G.SetInCast({"a", "b"});
+    G.SetOutCast({"out1", "out2"});
+    Function *function = G.GetFunction();
+    const int SUBGRAPH_NUM = 1;
+    function->SetTotalSubGraphCount(SUBGRAPH_NUM);
+    // 确认构图完毕
+    constexpr int opNumBefore = 22;
+    EXPECT_EQ(function->Operations().size(), opNumBefore) << opNumBefore << " operations before pass";
+    std::cout << "Build Graph Done." << std::endl;
+    /*
+    dump graph before Pass
+    function->DumpJsonFile(jsonFilePath);
+    */
+    // 单独执行pass
+    npu::tile_fwk::PreGraphPass preGraph;
+    preGraph.PreCheck(*function);
+    preGraph.RunOnFunction(*function);
+    preGraph.PostCheck(*function);
+    std::cout << "Run Pass Done." << std::endl;
+    /*
+    dump graph after Pass
+    function->DumpJsonFile(jsonFilePath);
+    */
+    EXPECT_EQ(function->Operations().size(), opNumBefore) << opNumBefore << " operations after pass";
+}
+} // namespace tile_fwk
+} // namespace npu
