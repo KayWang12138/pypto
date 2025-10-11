e5b4ed9ad1a1de5c2c078569dd23bc493275fec1
LordOfMysteries <duhongxiang@huawei.com>
2025-10-11 15:06:14 +0800

    fix compile
    Co-authored-by: LordOfMysteries<duhongxiang@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !330 merge fix into master
    
    fix compile
    
    Created-by: LordOfMysteries
    Commit-by: LordOfMysteries
    Merged-by: ascend-robot
    Description: 修改链接二进制脚本生成路径
    
    See merge request: cann/pypto-dev!330

--- src/machine/kernel/aicore_compiler.cpp
+++ src/machine/kernel/aicore_compiler.cpp
@@ -132,7 +132,8 @@ std::string GenSubFuncCall(std::map<uint64_t, Function *> &leafDict, CoreType co
   return head_file;
 }
 
-static int LinkObject(const std::string &src_objs, std::string &objPath, bool relocate, const std::string &key) {
+static int LinkObject(const std::string &src_objs, std::string &objPath, const std::string &ccePath, bool relocate,
+                      const std::string &key) {
   size_t cmdSize = src_objs.size() + objPath.size() + CMD_SIZE_1K;
   std::string ccecCmd;
   ccecCmd.resize(cmdSize);
@@ -145,14 +146,12 @@ static int LinkObject(const std::string &src_objs, std::string &objPath, bool re
     return ret;
   }
   ALOG_DEBUG_F("Link ccec command:[%s].", ccecCmd.c_str());
-  const std::string linkScript = "link_" + key + "_" + std::to_string(getpid()) + ".sh";
-  const std::string chCmd = "chmod +x " + linkScript;
-  const std::string ldCmd = "./" + linkScript;
+  const std::string linkScript = ccePath + "link_" + key + "_" + std::to_string(getpid()) + ".sh";
   std::ofstream script(linkScript.c_str());
   script << "#!/bin/bash\n";
   script << ccecCmd.c_str();
   script.close();
-  (void)std::system(chCmd.c_str());
+  const std::string ldCmd = "bash " + linkScript;
   ret = std::system(ldCmd.c_str());
   if (ret != 0) {
     ALOG_ERROR_F("Link kernel failed.");
@@ -179,7 +178,7 @@ int CompileAICoreKernel(std::map<uint64_t, Function *> &leafDict, dynamic::Encod
       auto ret = CompileCoreMachine(mid_aic_obj, true, tilingKey, headFile);
       ASSERT(ret == 0);
       src_aic_obj << mid_aic_obj;
-      ret = LinkObject(src_aic_obj.str(), aic_obj, true, "aic");
+      ret = LinkObject(src_aic_obj.str(), aic_obj, ccePath, true, "aic");
       ASSERT(ret == 0);
       return;
   };
@@ -192,7 +191,7 @@ int CompileAICoreKernel(std::map<uint64_t, Function *> &leafDict, dynamic::Encod
       auto ret = CompileCoreMachine(mid_aiv_obj, false, tilingKey, headFile);
       ASSERT(ret == 0);
       src_aiv_obj << mid_aiv_obj;
-      ret = LinkObject(src_aiv_obj.str(), aiv_obj, true, "aiv");
+      ret = LinkObject(src_aiv_obj.str(), aiv_obj, ccePath, true, "aiv");
       ASSERT(ret == 0);
       return;
   };
@@ -203,7 +202,7 @@ int CompileAICoreKernel(std::map<uint64_t, Function *> &leafDict, dynamic::Encod
   src_obj << " " << aic_obj << " " << aiv_obj;
   kernelPath = ccePath + "dy_kernel_" + std::to_string(tilingKey) + ".o";
   ALOG_DEBUG_F("Compile dynamic kernel to %s.", kernelPath.c_str());
-  auto ret = LinkObject(src_obj.str(), kernelPath, false, "mix");
+  auto ret = LinkObject(src_obj.str(), kernelPath, ccePath, false, "mix");
   return ret;
 }
 
