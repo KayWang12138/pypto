da335a509d2f616d0dfdc56b5d44f405ef8e5aa2
li_qingyan <liqingyan10@h-partners.com>
2025-12-02 09:22:49 +0800

    fix L0B
    Co-authored-by: li_qingyan<liqingyan10@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1456 merge fixl0bbug into master
    
    fix L0B
    
    Created-by: li_qingyan
    Commit-by: li_qingyan
    Merged-by: turing_project1
    Description: 描述：
    修复L0B大小写问题
    关联的ISSUE：
    不涉及
    测试
    门槛用例16/16精度通过
    文档更新
    不涉及
    类型标签
    bug修复
    
    See merge request: cann/pypto-dev!1456

--- framework/src/interface/tileop/utils/common_type.h
+++ framework/src/interface/tileop/utils/common_type.h
@@ -16,7 +16,7 @@
 #ifndef TILEOP_UTILS_COMMON_TYPE_H
 #define TILEOP_UTILS_COMMON_TYPE_H
 
-enum class Hardware : uint8_t { GM = 0, UB, L1, L0A, l0B, L0C, BIAS, FIXBUF, MAX };
+enum class Hardware : uint8_t { GM = 0, UB, L1, L0A, L0B, L0C, BIAS, FIXBUF, MAX };
 
 enum class UnaryOp : uint8_t { ABS = 0, EXP, NEG, REC, RSQRT, SQRT };
 
