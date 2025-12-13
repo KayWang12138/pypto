8e13ff159404e5bcb61863a4eef14aa78f4d9099
shi-dingkun <shidingkun1@huawei.com>
2025-12-13 09:42:22 +0800

    modifiy license
    Co-authored-by: shi-dingkun<shidingkun1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1915 merge license_l1 into master
    
    modifiy license
    
    Created-by: shi-dingkun
    Commit-by: shi-dingkun
    Merged-by: DCGDDD
    Description: modifiy license
    
    See merge request: cann/pypto-dev!1915

--- framework/tests/cmake/scripts/golden/op/batchmatmul.py
+++ framework/tests/cmake/scripts/golden/op/batchmatmul.py
@@ -7,7 +7,7 @@
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
-# ======================================================================================================================
+# -----------------------------------------------------------------------------------------------------------
 """ BatchMatmul相关用例 Golden 生成逻辑.
 
 本脚本有 2 种执行模式:
