e2a10ef2307d32fd53c6bfea6994918f03bd845d
liuyang454 <liuyang454@huawei.com>
2025-12-13 17:41:57 +0800

    feat(operation): Adapt TSCATTER pto instr

--- framework/src/interface/interpreter/calc_api.h
+++ framework/src/interface/interpreter/calc_api.h
@@ -102,7 +102,9 @@ struct CalcOps {
     void (*Copy)(LogicalTensorDataPtr, LogicalTensorDataPtr, bool);
     void (*ScatterUpdate)(LogicalTensorDataPtr, LogicalTensorDataPtr, LogicalTensorDataPtr, int, std::string, int);
     void (*LogicalView)(LogicalTensorDataPtr, LogicalTensorDataPtr, Offset);
-    void (*Scatter)(LogicalTensorDataPtr, LogicalTensorDataPtr, LogicalTensorDataPtr, const Element &, int, int);
+    void (*ScatterElement)(LogicalTensorDataPtr, LogicalTensorDataPtr, LogicalTensorDataPtr, const Element &, int, int);
+    void (*Scatter)(LogicalTensorDataPtr, LogicalTensorDataPtr, LogicalTensorDataPtr, LogicalTensorDataPtr,
+        int, int);
     void (*FormatND2NZ)(LogicalTensorDataPtr, LogicalTensorDataPtr);
     void (*FormatNZ2ND)(LogicalTensorDataPtr, LogicalTensorDataPtr);
     void (*MatMul)(LogicalTensorDataPtr, LogicalTensorDataPtr, LogicalTensorDataPtr, LogicalTensorDataPtr, MatMulParam &);
