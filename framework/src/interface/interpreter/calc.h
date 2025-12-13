8532de384464760527b2bd9445a137d40eee8224
liuyang454 <liuyang454@huawei.com>
2025-12-13 17:41:57 +0800

    feat(operation): Adapt TSCATTER pto instr

--- framework/src/interface/interpreter/calc.h
+++ framework/src/interface/interpreter/calc.h
@@ -202,8 +202,12 @@ inline void ScatterUpdate(LogicalTensorDataPtr out, LogicalTensorDataPtr self, L
 inline void LogicalView(LogicalTensorDataPtr out, LogicalTensorDataPtr self, Offset offset) {
     GetCalcOps()->LogicalView(out, self, offset);
 }
-inline void Scatter(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr index, const Element &src,
-    int axis, int reduce) {
+inline void ScatterElement(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr index,
+    const Element &src, int axis, int reduce) {
+    GetCalcOps()->ScatterElement(out, self, index, src, axis, reduce);
+}
+inline void Scatter(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr index,
+    LogicalTensorDataPtr src, int axis, int reduce) {
     GetCalcOps()->Scatter(out, self, index, src, axis, reduce);
 }
 inline void BitSort(LogicalTensorDataPtr out, LogicalTensorDataPtr self, int64_t axis, bool descending) {
