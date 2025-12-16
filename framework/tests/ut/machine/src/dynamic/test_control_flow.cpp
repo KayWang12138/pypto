2a098eaad0aff7b296f6959d71b068def6fd6319
quadrate <anyangle@huawei.com>
2025-12-16 09:06:19 +0800

    modify license
    Co-authored-by: Anyangle<anyangle@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2042 merge master into master
    
    modify license
    
    Created-by: quadrate
    Commit-by: Anyangle
    Merged-by: DCGDDD
    Description: modify license
    
    See merge request: cann/pypto-dev!2042

--- framework/tests/ut/machine/src/dynamic/test_control_flow.cpp
+++ framework/tests/ut/machine/src/dynamic/test_control_flow.cpp
@@ -8,6 +8,11 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
+/*!
+ * \file test_control_flow.cpp
+ * \brief
+ */
+
 #include "interface/utils/string_utils.h"
 
 #include "test_machine_common.h"
