c356623d996f6a0f8bf8b8020193786f0639bedf
muyuying <muyuying1@huawei.com>
2025-12-23 09:18:02 +0800

    1.Add A5 arch support
    Co-authored-by: muyuying<muyuying1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2242 merge A5 into master
    
    1.Add A5 arch support
    
    Created-by: muyuying
    Commit-by: muyuying
    Merged-by: cann-robot
    Description: 1.Add A5 arch support
    
    See merge request: cann/pypto-dev!2242

--- framework/src/codegen/codegen_factory.h
+++ framework/src/codegen/codegen_factory.h
@@ -32,7 +32,7 @@ class CodeGenFactory {
 public:
     static std::shared_ptr<CodeGenCCE> GetCodeGenCCE(const CodeGenCtx &ctx) {
         auto platform = Platform::Instance().GetSoc().GetNPUArch();
-        if (platform == NPUArch::DAV_2201) {
+        if (platform == NPUArch::DAV_2201 || platform == NPUArch::DAV_3510) {
             return std::make_shared<CodeGenCloudNPU>(ctx);
         }
         ASSERT(false) << "can not support this platform";
