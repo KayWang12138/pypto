20201f697d7024ce66689d6e9b35ea008558c087
lihong130 <lihong130@huawei.com>
2025-12-24 09:33:21 +0800

    Pass依赖校验

--- framework/src/passes/pass_mgr/pass_manager.cpp
+++ framework/src/passes/pass_mgr/pass_manager.cpp
@@ -100,52 +100,52 @@ void RegPass() {
 void PassManager::RegDefaultStrategy() {
     RegisterStrategy(
         "PVC2_OOO", {
-            {   "RemoveRedundantReshape",   "RemoveRedundantReshape"},
-            {                 "AutoCast",                 "AutoCast"},
-            {      "InferMemoryConflict",      "InferMemoryConflict"},
-            {       "RemoveUndrivenView",       "RemoveUndrivenView"},
-            {           "ExpandFunction",           "ExpandFunction"},
-            {        "MergeViewAssemble",        "MergeViewAssemble"},
-            {             "SplitReshape",             "SplitReshape"},
-            {           "SplitRawTensor",           "SplitRawTensor"},
-            {   "SplitLargeFanoutTensor",   "SplitLargeFanoutTensor"},
-            {              "DuplicateOp",              "DuplicateOp"},
-            {         "AssignMemoryType",         "AssignMemoryType"},
-            {  "InferDiscontinuousInput",  "InferDiscontinuousInput"},
-            {        "RemoveRedundantOp",        "RemoveRedundantOp"},
-            {                   "SplitK",                   "SplitK"},
-            {           "GraphPartition",           "GraphPartition"},
-            {          "ReduceCopyMerge",          "ReduceCopyMerge"},
-            {             "NBufferMerge",             "NBufferMerge"},
-            {       "L1CopyInReuseMerge",       "L1CopyInReuseMerge"},
-            {     "IntraSubgraphAdapter",     "IntraSubgraphAdapter"},
-            {           "GenerateMoveOp",           "GenerateMoveOp"},
-            { "CommonOperationEliminate", "CommonOperationEliminate"},
-            {              "AxisCombine",              "AxisCombine"},
-            {           "PadLocalBuffer",           "PadLocalBuffer"},
-            {   "RemoveUnalignedReshape",   "RemoveUnalignedReshape"},
-            {          "ReplaceTensor",              "ReplaceTensor"},
-            {          "PreGraphProcess",          "PreGraphProcess"},
-            {            "InferDynShape",            "InferDynShape"},
-            {       "SubgraphToFunction",       "SubgraphToFunction"},
-            {          "InferParamIndex",          "InferParamIndex"},
-            {        "SrcDstBufferMerge",        "SrcDstBufferMerge"},
-            {                 "AddAlloc",                 "AddAlloc"},
-            {              "OoOSchedule",              "OoOSchedule"},
-            {        "GlobalMemoryReuse",        "GlobalMemoryReuse"},
-            {              "RemoveAlloc",              "RemoveAlloc"},
-            {           "CopyOutResolve",           "CopyOutResolve"},
-            {               "InsertSync",               "InsertSync"},
-            {         "MixSubgraphSplit",         "MixSubgraphSplit"},
-            {           "CodegenPreproc",           "CodegenPreproc"},
+            {  "RemoveRedundantReshape",   PassName::REMOVE_REDUNDANT_RESHAPE},
+            {                "AutoCast",                  PassName::AUTO_CAST},
+            {     "InferMemoryConflict",      PassName::INFER_MEMORY_CONFLICT},
+            {      "RemoveUndrivenView",       PassName::REMOVE_UNDRIVEN_VIEW},
+            {          "ExpandFunction",            PassName::EXPAND_FUNCTION},
+            {       "MergeViewAssemble",        PassName::MERGE_VIEW_ASSEMBLE},
+            {            "SplitReshape",              PassName::SPLIT_RESHAPE},
+            {          "SplitRawTensor",           PassName::SPLIT_RAW_TENSOR},
+            {  "SplitLargeFanoutTensor",  PassName::SPLIT_LARGE_FANOUT_TENSOR},
+            {             "DuplicateOp",               PassName::DUPLICATE_OP},
+            {        "AssignMemoryType",         PassName::ASSIGN_MEMORY_TYPE},
+            { "InferDiscontinuousInput",  PassName::INFER_DISCONTINUOUS_INPUT},
+            {       "RemoveRedundantOp",        PassName::REMOVE_REDUNDANT_OP},
+            {                  "SplitK",                    PassName::SPLIT_K},
+            {          "GraphPartition",            PassName::GRAPH_PARTITION},
+            {         "ReduceCopyMerge",          PassName::REDUCE_COPY_MERGE},
+            {            "NBufferMerge",             PassName::N_BUFFER_MERGE},
+            {      "L1CopyInReuseMerge",     PassName::L1_COPY_IN_REUSE_MERGE},
+            {    "IntraSubgraphAdapter",     PassName::INTRA_SUBGRAPH_ADAPTER},
+            {          "GenerateMoveOp",           PassName::GENERATE_MOVE_OP},
+            {"CommonOperationEliminate", PassName::COMMON_OPERATION_ELIMINATE},
+            {             "AxisCombine",               PassName::AXIS_COMBINE},
+            {          "PadLocalBuffer",           PassName::PAD_LOCAL_BUFFER},
+            {  "RemoveUnalignedReshape",   PassName::REMOVE_UNALIGNED_RESHAPE},
+            {           "ReplaceTensor",             PassName::REPLACE_TENSOR},
+            {         "PreGraphProcess",          PassName::PRE_GRAPH_PROCESS},
+            {           "InferDynShape",            PassName::INFER_DYN_SHAPE},
+            {      "SubgraphToFunction",       PassName::SUBGRAPH_TO_FUNCTION},
+            {         "InferParamIndex",          PassName::INFER_PARAM_INDEX},
+            {       "SrcDstBufferMerge",       PassName::SRC_DST_BUFFER_MERGE},
+            {                "AddAlloc",                  PassName::ADD_ALLOC},
+            {             "OoOSchedule",               PassName::OOO_SCHEDULE},
+            {       "GlobalMemoryReuse",        PassName::GLOBAL_MEMORY_REUSE},
+            {             "RemoveAlloc",               PassName::REMOVE_ALLOC},
+            {          "CopyOutResolve",           PassName::COPY_OUT_RESOLVE},
+            {              "InsertSync",                PassName::INSERT_SYNC},
+            {        "MixSubgraphSplit",         PassName::MIX_SUBGRAPH_SPLIT},
+            {          "CodegenPreproc",            PassName::CODEGEN_PREPROC},
     });
     RegisterStrategy(
         "FunctionUnroll", {
-            {               "LoopUnroll",               "LoopUnroll"}
+            {              "LoopUnroll",                PassName::LOOP_UNROLL}
     });
     RegisterStrategy(
         "ExecuteGraph", {
-            {          "DynAttrToStatic",          "DynAttrToStatic"},
+            {         "DynAttrToStatic",         PassName::DYN_ATTR_TO_STATIC},
     });
 }
 
@@ -157,8 +157,8 @@ PassManager::PassManager() {
 
 void PassManager::RegisterStrategy(const std::string &strategy, const std::vector<PassEntry> &passEntries) {
     // check pass dependency
-    std::vector<std::string> passes;
-    for (const auto &passEntry : passEntries){
+    std::vector<PassName> passes;
+    for (const auto &passEntry : passEntries) {
         passes.emplace_back(passEntry.passName);
     }
     PassDependency::Instance().CheckStrategyDependency(strategy, passes);
@@ -218,9 +218,9 @@ Status PassManager::RunPass(Program &program, Function &function, const std::str
     for (size_t i = startIdx; i < strategyPasses.size(); i++) {
         const auto &identifier = strategyPasses[i].identifier;
         const auto &passName = strategyPasses[i].passName;
-        auto pass = PassRegistry::GetInstance().CreatePass(passName);
+        auto pass = PassRegistry::GetInstance().CreatePass(PassNameStr(passName));
         if (pass == nullptr) {
-            ALOG_ERROR_F("Pass [%s] does not exist.", passName.c_str());
+            ALOG_ERROR_F("Pass [%s] does not exist.", PassNameStr(passName));
             return FAILED;
         }
         std::string originLogOutPath = config::LogFile();
@@ -236,7 +236,8 @@ Status PassManager::RunPass(Program &program, Function &function, const std::str
             passDfxCfg.dumpGraph = true;
         }
         pass->SetPassConfigs(passDfxCfg);
-        ALOG_INFO_F("[PassManager] Apply pass <%s> on function: %s.", identifier.c_str(), function.GetMagicName().c_str());
+        ALOG_INFO_F(
+            "[PassManager] Apply pass <%s> on function: %s.", identifier.c_str(), function.GetMagicName().c_str());
         auto start = std::chrono::high_resolution_clock::now();
         if (pass->Run(function, strategy, identifier, i) != SUCCESS) {
             ALOG_ERROR_F("Run pass <%s> failed.", identifier.c_str());
@@ -245,8 +246,8 @@ Status PassManager::RunPass(Program &program, Function &function, const std::str
         if (passDfxCfg.dumpPassTimeCost) {
             auto end = std::chrono::high_resolution_clock::now();
             auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-            ALOG_INFO_F("Runtime of pass %s for program %s function %s is %ld us.", identifier.c_str(), program.Name().c_str(),
-                function.GetMagicName().c_str(), duration.count());
+            ALOG_INFO_F("Runtime of pass %s for program %s function %s is %ld us.", identifier.c_str(),
+                program.Name().c_str(), function.GetMagicName().c_str(), duration.count());
         }
         if (config::GetVerifyOption<bool>(KEY_ENABLE_PASS_VERIFY)) {
             Program::GetInstance().VerifyPass(&function, i, identifier);
