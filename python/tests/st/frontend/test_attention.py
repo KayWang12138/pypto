a1a8f74d489242ee5b4af8c86c173832db15758a
Siyuan Feng <syfeng@sii.edu.cn>
2025-12-24 20:57:13 +0800

    update copyright and remove legacy tesys

--- python/tests/st/frontend/test_attention.py
+++ python/tests/st/frontend/test_attention.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright (c) 2025 CANN community contributors.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+
 # pylint: disable=missing-docstring
 import os
 import pypto
@@ -35,7 +46,7 @@ def test_attention(
     qk_scale = np.reciprocal(np.sqrt(DQK))
     s = pypto.softmax(qk * qk_scale, -1)
     out = pypto.matmul(s, v, out_dtype=pypto.DT_FP32)
-    
+
     attention = out
     return attention
 
@@ -55,7 +66,7 @@ def test_attention_run():
     v = np.random.uniform(-1, 1, (s2, dv)).astype(np.float16)
 
     attention_golden = (softmax(q @ k.T / np.sqrt(dqk)) @ v)
-    
+
     q = torch.from_numpy(q).to(device=f"npu:{device_id}")
     k = torch.from_numpy(k).to(device=f"npu:{device_id}")
     v = torch.from_numpy(v).to(device=f"npu:{device_id}")
--- python/tests/st/frontend/test_flash_attention.py
+++ python/tests/st/frontend/test_flash_attention.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright (c) 2025 CANN community contributors.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+
 # pylint: disable=missing-docstring
 import os
 import pypto
@@ -101,7 +112,7 @@ def test_flash_attention_run():
     s2 = S2
     dqk = DQK
     dv = DV
-    
+
     q = np.random.uniform(-1, 1, (s1, dqk)).astype(np.float16)
     k = np.random.uniform(-1, 1, (s2, dqk)).astype(np.float16)
     v = np.random.uniform(-1, 1, (s2, dv)).astype(np.float16)
