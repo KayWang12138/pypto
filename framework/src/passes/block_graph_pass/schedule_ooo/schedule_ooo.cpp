1191a0f504acae3cb7e1756ae633314014d9955d
zhaoshenjie <zhaoshenjie@huawei.com>
2025-12-25 09:30:34 +0800

    解冲突4

--- framework/src/passes/block_graph_pass/schedule_ooo/schedule_ooo.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/schedule_ooo.cpp
@@ -39,7 +39,7 @@ Status OoOSchedule::RunOnFunction(Function &function) {
         if (IsAicpuProgram(opList)) {
             continue;
         }
-        OoOScheduler oooSchedule(*program.second, ConfigManager::Instance().GetOperationConfig("COMBINE_AXIS", false));
+        OoOScheduler oooSchedule(*program.second);
         oooSchedule.oooCheck.doHealthCheck = passDfxconfigs_.healthCheck;
         APASS_LOG_INFO_F(Elements::Operation, "Subgraph[%d] OOOSchedule start.", program.first);
         if (oooSchedule.Schedule(opList) != SUCCESS) { 
