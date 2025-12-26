476e8f728525903faebeb569e85ff2c2e10573bc
gaoxingwang <camuel.gao@huawei.com>
2025-12-26 16:25:32 +0800

    eager device sync delete bug

--- framework/src/machine/device/dynamic/aicore_manager.h
+++ framework/src/machine/device/dynamic/aicore_manager.h
@@ -601,21 +601,18 @@ private:
         int mngCoreNum = aicNum + aivNum;
         AicoreStatus coreStatus[MAX_AICORE_NUM] = {AicoreStatus::CORE_TASK_WAIT_FINISH};
         bool isLastDevTask = reinterpret_cast<DynDeviceTask *>(curDevTask_)->IsLastTask();
+        bool aicAllStop = false;
+        bool aivAllStop = false;
         if (!isLastDevTask) {
             PreFetchNextDevTask();
         } else {
             preFetchNextDevTaskCtrl_ = nullptr;
             preFetchSuccess_ = false;
-        }
-
-        PreSendStopToIdleCore(isLastDevTask, coreStatus, finishStopNum);
-
-        bool aicAllStop = false;
-        bool aivAllStop = false;
-        if (isLastDevTask) {
             aicAllStop = (coreRunReadyCnt_[static_cast<int>(CoreType::AIC)] == static_cast<uint32_t>(aicNum));
             aivAllStop = (coreRunReadyCnt_[static_cast<int>(CoreType::AIV)] == static_cast<uint32_t>(aivNum));
         }
+
+        PreSendStopToIdleCore(isLastDevTask, coreStatus, finishStopNum);
 
         uint64_t start_cycles = GetCycles();
         while (finishStopNum < mngCoreNum) {
