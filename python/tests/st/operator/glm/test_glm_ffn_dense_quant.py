b940ef78cb49a72461bce9f7b224f4e843f8255b
songyangyang18 <songyangyang18@huawei.com>
2025-12-27 10:34:24 +0800

    merge cfgcache options to cfgcache_size in set_runtime_options

--- python/tests/st/operator/glm/test_glm_ffn_dense_quant.py
+++ python/tests/st/operator/glm/test_glm_ffn_dense_quant.py
@@ -226,9 +226,7 @@ def expert_infer_base(hidden_states, w13_params, w2, ffn_res, tiling_params, off
     host_options={"only_codegen": True},
     codegen_options={"codegen_expression_fusion": True},
     runtime_options={"device_sched_mode": 1,
-                     "cfgcache_device_task_num": 100,
-                     "cfgcache_root_task_num": 1000,
-                     "cfgcache_leaf_task_num": 10000}
+                     "stitch_cfgcache_size": 3203200}
 )
 def dense_moe_main(hidden_states, w13, w13_scale, w2, ffn_res):
     """
