ed10ea6729ab3f560eacc333344a6f47671600bc
songyangyang18 <songyangyang18@huawei.com>
2025-12-27 10:34:24 +0800

    merge cfgcache options to cfgcache_size in set_runtime_options

--- python/pypto/config.py
+++ python/pypto/config.py
@@ -218,10 +218,8 @@ def set_runtime_options(*,
                         stitch_function_outcast_memory: Optional[int] = None,
                         stitch_function_num_initial: Optional[int] = None,
                         stitch_function_num_step: Optional[int] = None,
-                        cfgcache_device_task_num: Optional[int] = None,
-                        cfgcache_root_task_num: Optional[int] = None,
-                        cfgcache_leaf_task_num: Optional[int] = None,
                         stitch_function_size: int = None,
+                        stitch_cfgcache_size: Optional[int] = None,
                         run_mode: Optional[int] = None
                         ) -> None:
     """
@@ -253,6 +251,9 @@ def set_runtime_options(*,
     stitch_function_size: int
         The maximum Callop computation amount per loop for stitch tasks,
         controlled in the ctrlflow AICPU during machine runtime.
+
+    stitch_cfgcache_size: int
+        The size of the control flow cache.
     """
     _pto_options.set_options("runtime", locals())
 
