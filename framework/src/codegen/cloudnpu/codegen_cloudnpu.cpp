ccad9f21b4927f93f2231b39b311e3dcf0df32e6
songyangyang18 <songyangyang18@huawei.com>
2025-12-30 20:49:21 +0800

    feat(machine): Add only_compile option and compile stage option

--- framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
@@ -407,7 +407,12 @@ int CheckInjectStr(const char cmdStr[], size_t strLen) {
 }
 
 void CodeGenCloudNPU::DoCompileCCE(const CompileInfo &compileInfo, const std::string &compileOptions) const {
-    if (!compileInfo.IsNeedCompileCCE()) {
+    if (!compileInfo.IsNeedCompileCCE())
+    {
+        return;
+    }
+    if (config::GetRuntimeOption<int64_t>(CFG_RUN_MODE) == COMPILE_STAGE4) {
+        ALOG_INFO("Compilation stage terminates after codegen instruction.");
         return;
     }
     auto [ret, ccecCmd] = CompileCCE(compileInfo, compileOptions);
