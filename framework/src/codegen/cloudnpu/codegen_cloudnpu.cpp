acb118ca364bec7554222fcd5591e925fe0a2552
songyangyang18 <songyangyang18@huawei.com>
2025-12-30 20:49:21 +0800

    feat(machine): Add only_compile option and compile stage option

--- framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
@@ -271,7 +271,15 @@ void CodeGenCloudNPU::GenCode(
 #ifdef BUILD_WITH_CANN
             if (config::GetRuntimeOption<int64_t>(CFG_RUN_MODE) != CFG_RUN_MODE_SIM) {
                 DumpCCE(compileInfo.GetCCEAbsPath(), leafKernelFunc.str());
+                if (config::GetHostOption<int64_t>(COMPILE_STAGE) == COMPILE_STAGE_CODEGEN_INSTRUCTION) {
+                    ALOG_INFO("Compilation stage terminates after codegen instruction.");
+                    return;
+                }
                 DoCompileCCE(compileInfo, "");
+                if (config::GetHostOption<int64_t>(COMPILE_STAGE) == COMPILE_STAGE_CODEGEN_BINARY) {
+                    ALOG_INFO("Compilation stage terminates after codegen binary.");
+                    return;
+                }
             }
 #endif
             UpdateSubFunc(subFuncPair, compileInfo);
