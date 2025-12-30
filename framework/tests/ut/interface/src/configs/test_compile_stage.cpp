1ec0fa7c338435bcc43ee4fe11ba176c852989c5
songyangyang18 <songyangyang18@huawei.com>
2025-12-30 20:49:21 +0800

    feat(machine): Add only_compile option and compile stage option

---
+++ framework/tests/ut/interface/src/configs/test_compile_stage.cpp
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_config_runmode.cpp
+ * \brief
+ */
+#include <climits>
+#include "gtest/gtest.h"
+#include "tilefwk/tilefwk.h"
+#include "interface/inner/tilefwk.h"
+#include "interface/configs/config_manager.h"
+#include "interface/configs/config_manager_ng.h"
+
+using namespace npu::tile_fwk;
+
+class TestConfigRunmode : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp() override {
+        Program::GetInstance().Reset();
+    }
+    void TearDown() override {
+        Program::GetInstance().Reset();
+        config::Reset();
+    }
+};
+
+TEST_F(TestConfigRunmode, RunModeCompileStage1) {
+    const std::vector<int64_t> shape = {4, 4};
+    TileShape::Current().SetVecTile(shape);
+    Tensor inputA(DT_FP32, shape, "A");
+    Tensor inputB(DT_FP32, shape, "B");
+    Tensor output(DT_FP32, shape, "C");
+    config::SetRuntimeOption(CFG_RUN_MODE, COMPILE_STAGE1);
+    FUNCTION("ADD", {inputA, inputB, output}) {
+        output = Add(inputA, inputB);
+    }
+}
+
+TEST_F(TestConfigRunmode, RunModeCompileStage2) {
+    const std::vector<int64_t> shape = {4, 4};
+    TileShape::Current().SetVecTile(shape);
+    Tensor inputA(DT_FP32, shape, "A");
+    Tensor inputB(DT_FP32, shape, "B");
+    Tensor output(DT_FP32, shape, "C");
+    config::SetRuntimeOption(CFG_RUN_MODE, COMPILE_STAGE2);
+    FUNCTION("ADD", {inputA, inputB, output}) {
+        output = Add(inputA, inputB);
+    }
+}
+
+TEST_F(TestConfigRunmode, RunModeCompileStage3) {
+    const std::vector<int64_t> shape = {4, 4};
+    TileShape::Current().SetVecTile(shape);
+    Tensor inputA(DT_FP32, shape, "A");
+    Tensor inputB(DT_FP32, shape, "B");
+    Tensor output(DT_FP32, shape, "C");
+    config::SetRuntimeOption(CFG_RUN_MODE, COMPILE_STAGE3);
+    FUNCTION("ADD", {inputA, inputB, output}) {
+        output = Add(inputA, inputB);
+    }
+}
+
+TEST_F(TestConfigRunmode, RunModeCompileStage4) {
+    const std::vector<int64_t> shape = {4, 4};
+    TileShape::Current().SetVecTile(shape);
+    Tensor inputA(DT_FP32, shape, "A");
+    Tensor inputB(DT_FP32, shape, "B");
+    Tensor output(DT_FP32, shape, "C");
+    config::SetRuntimeOption(CFG_RUN_MODE, COMPILE_STAGE4);
+    FUNCTION("ADD", {inputA, inputB, output}) {
+        output = Add(inputA, inputB);
+    }
+}
+
+TEST_F(TestConfigRunmode, RunModeCompileStage5) {
+    const std::vector<int64_t> shape = {4, 4};
+    TileShape::Current().SetVecTile(shape);
+    Tensor inputA(DT_FP32, shape, "A");
+    Tensor inputB(DT_FP32, shape, "B");
+    Tensor output(DT_FP32, shape, "C");
+    config::SetRuntimeOption(CFG_RUN_MODE, COMPILE_STAGE5);
+    FUNCTION("ADD", {inputA, inputB, output}) {
+        output = Add(inputA, inputB);
+    }
+}
