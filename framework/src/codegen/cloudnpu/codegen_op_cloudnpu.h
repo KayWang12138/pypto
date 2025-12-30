2f52cdd9ba8c564cc1ee124c5e2fe228dee88f87
wuchenhao123 <wuchenhao6@huawei.com>
2025-12-30 15:20:09 +0800

    hcclcontext整改

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -123,7 +123,8 @@ public:
 
     std::string GenDistOp() const;
     std::string GetTemplateDType() const;
-    std::string GenTemplateParams() const;
+    std::string GetTemplateDType() const;
+    std::string GenTemplateSoCVersion() const;
     void GenExtraTemplateParamsForMoeCombine(std::ostringstream &oss, int32_t operandIndex) const;
     std::string GenOffsets(int32_t operandIndex, int32_t dim) const;
     std::string GenRawShapes(int32_t operandIndex, int32_t dim) const;
