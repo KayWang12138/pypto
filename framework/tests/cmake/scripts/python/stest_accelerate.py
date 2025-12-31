b9353a27c513eecf5d6f5913ff2a654038fbae2d
root <>
2025-12-31 10:44:44 +0800

    Revert "feat(build): Optimize the parallel execution implementation of STest"
    This reverts commit 9659f49d6f28d0fc28e3158eca258422a1726317.

--- framework/tests/cmake/scripts/python/stest_accelerate.py
+++ framework/tests/cmake/scripts/python/stest_accelerate.py
@@ -44,16 +44,16 @@ class STestAccelerate(GTestAccelerate):
         if args.device is not None:
             device_list = [int(d) for d in list(set(args.device)) if d is not None and str(d) != ""]
         for _id in device_list:
-            p = GTestAccelerate.ExecParam(cntr_id=_id, envs_func=STestAccelerate.set_device_id_envs)
+            p = GTestAccelerate.ExecParam(envs_func=STestAccelerate.set_device_id_envs, custom=_id)
             params.append(p)
-        ctrl = STestAccelerate(args=args, params=params, cntr_name="Device")
+        ctrl = STestAccelerate(args=args, params=params)
         ctrl.process()
         return ctrl.post()
 
     @staticmethod
     def set_device_id_envs(p: Any) -> Optional[Dict[str, str]]:
         self: GTestAccelerate.ExecParam = p
-        return {"TILE_FWK_DEVICE_ID": f"{self.cntr_id}"}
+        return {"TILE_FWK_DEVICE_ID": f"{self.custom}"}
 
 
 if __name__ == "__main__":
--- python/tests/st/test_debug_mode.py
+++ python/tests/st/test_debug_mode.py
@@ -60,7 +60,9 @@ def get_out_put_path():
     return None
 
 
-def device_run(is_run_add, device_id):
+def device_run(is_run_add):
+    device_id = os.environ.get('TILE_FWK_DEVICE_ID', 0)
+    torch.npu.set_device(int(device_id))
     tiling = 32
     n, m = tiling * 1, tiling * 1
 
@@ -94,10 +96,11 @@ def device_run(is_run_add, device_id):
 
     output_path = get_out_put_path()
     assert output_path
+    merged_swimlane, error = safe_json_load(os.path.join(output_path, 'merged_swimlane.json'))
+    assert not error
+    assert "traceEvents" in merged_swimlane
 
 
 def test_debug_mode():
-    device_id = os.environ.get('TILE_FWK_DEVICE_ID', 0)
-    torch.npu.set_device(int(device_id))
-    device_run(True, device_id)
-    device_run(False, device_id)
+    device_run(True)
+    device_run(False)
