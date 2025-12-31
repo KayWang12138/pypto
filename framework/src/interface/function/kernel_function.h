6987254d84c2f02cd6bd22aa0646f063b63d612e
luohuan40 <luohuan40@huawei.com>
2025-12-31 11:09:11 +0800

    Separate the properties and methods belonging to KernelFunction within the Function class.

--- framework/src/interface/function/kernel_function.h
+++ framework/src/interface/function/kernel_function.h
@@ -65,7 +65,32 @@ public:
         return std::make_pair(true, code);
     }
 
+    void CreateLeafInAndOutCast(const LogicalTensorPtr &inOrOut, LogicalTensors &inOrOutList) const override;
+    GetTensorDataIODescDict GetTensorDataForLeafGraph() override;
+    
+    void AppendIncast(LogicalTensorPtr tensor, int opmagic, int k) override {
+        incastPosition.emplace_back(opmagic, k);
+        inCasts_.emplace_back(tensor);
+    }
+
+    void AppendOutcast(LogicalTensorPtr tensor, int opmagic, int k) override {
+        outcastPosition.emplace_back(opmagic, k);
+        outCasts_.emplace_back(tensor);
+    }
+
+    DynParamInfo &GetMutableDynParam(std::string dim) override {
+        return dynParamTable_[dim];
+    }
+
+    void InsertDynParam(std::string dim, DynParamInfo &info) override {
+        dynParamTable_.emplace(dim, info);
+    }
+
+    const std::map<std::string, DynParamInfo> &GetDynParamTable() const override {
+        return dynParamTable_;
+    }
 private:
+    std::map<std::string, DynParamInfo> dynParamTable_;
     SubfuncParam parameter_; // Parameter information for heterogeneous subgraph
     int programId_; // Heterogeneous subgraph ID
     std::shared_ptr<LeafFuncAttribute> leafFuncAttr_; // Leaf function attributes
