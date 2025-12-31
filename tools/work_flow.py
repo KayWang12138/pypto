511681fa1c0ad531576c003730226c12ebf34a30
wan_jianfen <wanjianfen@hisilicon.com>
2025-12-31 10:58:26 +0800

    fix work flow

--- tools/work_flow.py
+++ tools/work_flow.py
@@ -31,15 +31,7 @@ def ini(path, prof, pe):
     "ai_core_profiling":"off","aicpuTrace":"on"}'
     """
     # 定义字典
-    level = None
-    real_pe = None
-    if prof == 1:
-        level = "l1"
-    elif prof == 2:
-        level = "l2"
-        real_pe = pe
-    else:
-        assert False, f'Current {prof} is invalid, only support[1, 2]'
+    level = "l2"
 
     env = {
         "PROFILER_SAMPLECONFIG": "{"
@@ -72,7 +64,7 @@ def find_file_and_get_parent_dirs(target_file, search_dir='.'):
 
 
 def work_flow_plot(path, level, pe):
-    path_pro = find_file_and_get_parent_dirs("aicpu.data.0.slice_0", path)
+    path_pro = find_file_and_get_parent_dirs("aicpu.data.*", path)
     cmd_rm = f'rm -rf {str(path)}/PROF*'
     if len(path_pro) < 1:
         subprocess.run(cmd_rm, shell=True, capture_output=False, check=True, text=True, encoding='utf-8')
