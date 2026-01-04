a73795fac0b5a65bac4f32326f28450608c9acd4
luoxinhui666 <luoxinhui2@huawei.com>
2026-01-04 15:35:14 +0800

    dispatch calcOccerrenceVector

--- framework/src/interface/operation/distributed/distributed_expand.h
+++ framework/src/interface/operation/distributed/distributed_expand.h
@@ -48,13 +48,6 @@ inline void CreateTileOp(const TileShape& tileShape,
     }
 }
 
-Tensor SendToRoutingExpert(const Tensor &shmemData, const Tensor &tokenTensor, const Tensor &tokenExpertTable,
-    const char *group, const MoeConfig &moeConfig);
-void SendToSharedExpert(const Tensor &shmemData, const Tensor &tokenTensor, 
-    const Tensor &syncTensor, const char *group);
-Tensor CopyToLocalExpert(const Tensor &tokenTensor, const Tensor &syncTensor);
-Tensor DispatchSetFlag(Tensor &shmemFlag, const Tensor &tokenExpertTable, 
-    const Tensor &syncTensor, const char *group, const MoeConfig &moeConfig);
 void TiledSendToRoutingExpert(Function &function, const TileShape &tileShape,
     const std::vector<std::shared_ptr<LogicalTensor>> &iOperand,
     const std::vector<std::shared_ptr<LogicalTensor>> &oOperand, const Operation &op);
