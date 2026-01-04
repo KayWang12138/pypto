c21dd2180d1d0db6ed0d4da09c9b18626808230b
yinjie27 <yinjie27@huawei.com>
2026-01-04 09:01:01 +0800

    (feat)ir: Add op template

---
+++ framework/include/ir/operation.h
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file operation_def.h
+ * \brief
+ */
+
+#pragma once
+
+#include "operation_def.h"
+
+namespace pto {
+
+#define DEF_OP DEF_CLASS_OP
+#include "operation.def"
+#undef DEF_OP
+
+} // namespace pto
