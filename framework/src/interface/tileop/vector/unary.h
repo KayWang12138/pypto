6a58b661f09c3f365342c9c975d48c197172a849
echoes_00444757 <luwei44@huawei.com>
2026-01-04 09:35:34 +0800

    feat(interface): Support ptobrcinline

--- framework/src/interface/tileop/vector/unary.h
+++ framework/src/interface/tileop/vector/unary.h
@@ -33,6 +33,10 @@ TILEOP void UnaryComputeImpl(T0 dst, T1 src) {
         pto::TSQRT(dst, src);
         return;
     }
+    if constexpr (op == UnaryOp::BRCB) {
+        pto::TROWEXPAND(dst, src);
+        return;
+    }
 }
 
 template <UnaryOp op, typename T0, typename T1>
@@ -77,5 +81,10 @@ TILEOP void TRsqrt(T0 dst, T1 src) {
 template <typename T0, typename T1>
 TILEOP void TSqrt(T0 dst, T1 src) {
     UnaryCompute<UnaryOp::SQRT>(dst, src);
+}
+
+template <typename T0, typename T1>
+TILEOP void Tbrcb(T0 dst, T1 src) {
+    UnaryCompute<UnaryOp::BRCB>(dst, src);
 }
 #endif
