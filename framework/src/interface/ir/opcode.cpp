2a68e3500fc99747dc8cff5dda905d2a34621cdb
yinjie27 <yinjie27@huawei.com>
2026-01-04 09:01:01 +0800

    (feat)ir: Add op template

---
+++ framework/src/interface/ir/opcode.cpp
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file opcode.cpp
+ * \brief
+ */
+
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include "ir/opcode.h"
+
+namespace pto {
+
+static std::unordered_map<Opcode, std::string> opcodeNameDict = {
+
+#define DEF_OP(name, inherit, opcode, ...) DEF_##opcode
+#define DEF_OPCODE(...) MAP(DEF_OPCODE_NAME, __VA_ARGS__)
+#define DEF_OPCODE_NAME(n) {n, #n},
+
+#include "operation.def"
+#include "tile_graph.def"
+
+#undef DEF_OPCODE_NAME
+#undef DEF_OPCODE
+#undef DEF_OP
+
+};
+
+std::string GetOpcodeName(Opcode opcode) {
+    if (opcodeNameDict.count(opcode)) {
+        return opcodeNameDict[opcode];
+    } else {
+        return "";
+    }
+}
+
+} // namespace pto
