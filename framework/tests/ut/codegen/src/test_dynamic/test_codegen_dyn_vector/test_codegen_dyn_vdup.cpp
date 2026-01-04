8313a63b1c7c3ebec9c609cb387f31670e71c7fe
Youhehzen <youhezhen@huawei.com>
2026-01-04 14:44:41 +0800

    Introduce KernelFunction
    Separate the properties and methods belonging to KernelFunction within the Function class.
    
    change name from Kernelfunction to BlockFunction; replace dynParamTable_ with InsertDynParam()
    
    this commit will be merged
    
    rename kernel_function to block_function, redefine Function::GetDynParamTable()
    
    set GraphType for subFunction
    
    Set SubFunc Graph
    
    For all UT test cases using BlockFunction, create a ComputationalGraphBuilder using ComputationalGraphBuilder subGraph(blockFunc.get());
    
    Fix ut with leaf(Function) -> leaf(BlockFunction)
    
    修改test_remove_alloc测试样例

--- framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_vector/test_codegen_dyn_vdup.cpp
+++ framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_vector/test_codegen_dyn_vdup.cpp
@@ -74,9 +74,9 @@ TEST_F(TestCodegenDynVdup, TestDynVdupUnaligned) {
             }
         }
         DynParamInfo fakeParam = {3, 0, 0, DynParamInfoType::VALID_SHAPE, 0, SymbolicScalar(), false, ""};
-        subFunc.second->dynParamTable_.emplace("sym_23_dim_0", fakeParam);
-        subFunc.second->dynParamTable_.emplace("sym_23_dim_1", fakeParam);
-        subFunc.second->dynParamTable_.emplace("sym_23_dim_2", fakeParam);
+        subFunc.second->InsertDynParam("sym_23_dim_0", fakeParam);
+        subFunc.second->InsertDynParam("sym_23_dim_1", fakeParam);
+        subFunc.second->InsertDynParam("sym_23_dim_2", fakeParam);
     }
 
     npu::tile_fwk::CodeGenCtx ctx;
