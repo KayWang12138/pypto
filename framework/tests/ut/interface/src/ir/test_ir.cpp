4e914d1e8c3f61bef04638388df62d0348e7b4f6
yinjie27 <yinjie27@huawei.com>
2026-01-04 09:01:01 +0800

    (feat)ir: Add op template

---
+++ framework/tests/ut/interface/src/ir/test_ir.cpp
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_ir.cpp
+ * \brief
+ */
+
+#include "gtest/gtest.h"
+#include "ir/utils.h"
+
+using namespace npu::tile_fwk;
+
+class IRTest : public testing::Test {
+public:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+static_assert(MAP_SIZE(a, b, c) == 3, "Invalid MAP_SIZE 3");
+static_assert(MAP_SIZE(a, b, c, d, e, f, g, h, a, b, c, d, e, f, g, h,
+                       a, b, c, d, e, f, g, h, a, b, c, d, e, f, g, h) == 32, "Invalid MAP_SIZE 32");
+
+TEST_F(IRTest, TestUtils) {
+    EXPECT_EQ(std::vector<int>({2, 3, 4}), std::vector<int>({
+#define ADD_1(n) (n) + 1,
+        MAP(ADD_1, 1, 2, 3)
+    }));
+}
+
+TEST_F(IRTest, TestOpcode) {
+    EXPECT_EQ("OP_SCALAR_NEG", GetOpcode(Opcode::OP_SCALAR_NEG));
+    EXPECT_EQ("", GetOpcode(Opcode::OP_INVALID));
+}
