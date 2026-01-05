9260307b509e3af941ff3630d52009e3274a1f31
luoxinhui666 <luoxinhui2@huawei.com>
2026-01-05 17:07:27 +0800

    ss

--- framework/tests/st/distributed/ops/src/test_allgather_attn_post_reducescatter.cpp
+++ framework/tests/st/distributed/ops/src/test_allgather_attn_post_reducescatter.cpp
@@ -57,10 +57,9 @@ void TestAllGatherAttentionPostReducescatter(OpTestParam &testParam)
         Tensor agOut(dtype, {b * n * s, kvLoraRank}, "agOut");
         LOOP("ALLGATHER", FunctionType::DYNAMIC_LOOP, unusedDynRankId, LoopRange(1)) {
             (void) unusedDynRankId;
-            TileShape::Current().SetDistTile({64, b * n * s / testParam.rankSize / 64, 0}, {kvLoraRank, 1, 0}, 
-                {1, testParam.rankSize, 0});
-            Tensor fakeBarrierDummy(DT_INT32, {1, 1}, "fakeBarrierDummy");
-            ShmemAllGather(agIn, fakeBarrierDummy, testParam.group, agOut);
+            TileShape::Current().SetVecTile({64, kvLoraRank});
+            Tensor predToken(DT_INT32, {1, 1}, "predToken");
+            AllGather(predToken, agIn, testParam.group, static_cast<uint32_t>(testParam.rankSize), agOut);
         }
         Tensor attnOut(dtype, {b * s, h}, "attnOut");
         LOOP("ATTNPOST", FunctionType::DYNAMIC_LOOP, batchId, LoopRange(1)) {
@@ -87,9 +86,10 @@ void TestAllGatherAttentionPostReducescatter(OpTestParam &testParam)
         }
         LOOP("REDUCESCATTER", FunctionType::DYNAMIC_LOOP, unusedIndex, LoopRange(1)) {
             (void) unusedIndex;
-            TileShape::Current().SetDistTile({16, b * s / testParam.rankSize / 16, 0}, {h, 1, 0}, 
-                {1, testParam.rankSize, 0});
-            Distributed::ShmemReduceScatter(attnOut, testParam.group, DistReduceType::DIST_REDUCE_ADD, out);
+            Tensor predToken(DT_INT32, {1, 1}, "predToken");
+            TileShape::Current().SetVecTile({16, h});
+            Distributed::ReduceScatter(predToken, attnOut, testParam.group, static_cast<uint32_t>(testParam.rankSize),
+                DistReduceType::DIST_REDUCE_ADD, out);
         }
     }
     auto dynAttr = Program::GetInstance().GetLastFunction()->GetDyndevAttribute();
