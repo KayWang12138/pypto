a7fdc3f0c63b704de420425effe258c0e91ba5e9
xujiayi1999 <xujiayi28@huawei.com>
2026-01-05 21:34:21 +0800

    Merge branch 'master' into develope1
    Co-authored-by: xujiayi1999<xujiayi28@huawei.com>

--- framework/src/passes/pass_check/remove_redundant_reshape_checker.cpp
+++ framework/src/passes/pass_check/remove_redundant_reshape_checker.cpp
@@ -38,6 +38,18 @@ Status RemoveRedundantReshapeChecker::DoPreCheck(Function &function) {
     }
     return SUCCESS;
 }
+
+Status RemoveRedundantReshapeChecker::DoPostCheck(Function &function) {
+    APASS_LOG_INFO_F(Elements::Operation, "PostCheck for RemoveRedundantShape.");
+    for (const auto &op : function.Operations().DuplicatedOpList()) {
+        if (ProcessPostCheck(op)) {
+            APASS_LOG_ERROR_F(Elements::Operation, "Postcheck RemoveRedundantShape failed. %s", GetFormatBacktrace(*op).c_str());
+            return FAILED;
+        }
+    }
+    return SUCCESS;
+}
+
 Status RemoveRedundantReshapeChecker::ProcessPreCheck(const Operation *op) {
     if (op->GetOpcode() == Opcode::OP_RESHAPE) {
         auto in = op->iOperand.front();
@@ -62,5 +74,40 @@ Status RemoveRedundantReshapeChecker::PreCheckReshape(const LogicalTensorPtr &in
     }
     return SUCCESS;
 }
+
+Status RemoveRedundantReshapeChecker::ProcessPostCheck(const Operation *op) {
+    if (op->GetOpcode() == Opcode::OP_RESHAPE) {
+        auto in = op->iOperand.front();
+        if (PostCheckReshape(in) != SUCCESS) {
+            APASS_LOG_ERROR_F(Elements::Operation, "Postcheck of reshape op[%d] failed. %s", 
+            op->GetOpMagic(), GetFormatBacktrace(*op).c_str());
+            return FAILED;
+        }
+    }
+    return SUCCESS;
+}
+
+bool CheckForConsecutiveReshape(const Operation *childOp){
+    for (auto &consumer : childOp->GetOOperands()[0]->GetConsumers()){
+        if (consumer->GetOpcode() == Opcode::OP_RESHAPE){
+            return true;
+        }
+    }
+    return false;   
+}
+
+// Postcheck for reshape
+Status RemoveRedundantReshapeChecker::PostCheckReshape(const LogicalTensorPtr &in) {
+    for (auto &childOp : in->GetConsumers()) {
+        if (childOp->GetOpcode() == Opcode::OP_RESHAPE) {
+            if (CheckForConsecutiveReshape(childOp)) {
+                APASS_LOG_ERROR_F(Elements::Operation, "PostCheckReshape failed: Found consecutive reshape ops.");
+                return FAILED;
+            }
+        }
+    }
+    return SUCCESS;
+}
+
 } // namespace tile_fwk
 } // namespace npu
--- framework/tests/ut/passes/src/test_removeredundantreshape.cpp
+++ framework/tests/ut/passes/src/test_removeredundantreshape.cpp
@@ -84,8 +84,8 @@ TEST_F(TestRemoveRedundantReshapePass, RemoveRedundantReshapeUTest1) {
     currFunctionPtr->outCasts_.push_back(outCast);
 
     RemoveRedundantReshape removeredundantpass;
-    auto status = removeredundantpass.RunOnFunction(*currFunctionPtr);
-    EXPECT_EQ(status, SUCCESS);
+    EXPECT_EQ(removeredundantpass.RunOnFunction(*currFunctionPtr), SUCCESS);
+    EXPECT_EQ(removeredundantpass.PostCheck(*currFunctionPtr), SUCCESS);
 
     const auto &operations = currFunctionPtr->Operations();
     uint32_t reshape_num = kNumZero;
@@ -174,6 +174,7 @@ TEST_F(TestRemoveRedundantReshapePass, RemoveRedundantReshapeUTest3) {
     RemoveRedundantReshape removeredundantpass;
     EXPECT_NE(removeredundantpass.PreCheck(*currFunctionPtr), SUCCESS);
     EXPECT_EQ(removeredundantpass.RunOnFunction(*currFunctionPtr), SUCCESS);
+    EXPECT_EQ(removeredundantpass.PostCheck(*currFunctionPtr), SUCCESS);
 
     uint32_t reshape_num = kNumZero;
     for (auto &op : currFunctionPtr->Operations()) {
@@ -233,6 +234,30 @@ TEST_F(TestRemoveRedundantReshapePass, RemoveRedundantReshapeUTest4) {
     EXPECT_EQ(reshape1.GetInputOperand(kSizeZero), inCast);
     EXPECT_EQ(reshape2.GetInputOperand(kSizeZero), inCast);
     EXPECT_EQ(reshape_num, kNumTwo);
+}
+
+TEST_F(TestRemoveRedundantReshapePass, RemoveRedundantReshapeUTest5) {
+    auto currFunctionPtr = std::make_shared<Function>(Program::GetInstance(), "TestRemoveRedundantReshape", "TestRemoveRedundantReshape", nullptr);
+    EXPECT_TRUE(currFunctionPtr != nullptr);
+
+    // Prepare the graph
+    std::vector<int64_t> shape1 = {kNumEight, kNumExpFour};
+    std::vector<int64_t> shape2 = {kNumExpFour, kNumEight};
+    std::vector<int64_t> shape3 = {kNumExpFive, kNumFour};
+    auto inCast = std::make_shared<LogicalTensor>(*currFunctionPtr, DT_FP32, shape1);
+    auto ubTensor1 = std::make_shared<LogicalTensor>(*currFunctionPtr, DT_FP32, shape2);
+    auto ubTensor2 = std::make_shared<LogicalTensor>(*currFunctionPtr, DT_FP32, shape3);
+    auto outCast = std::make_shared<LogicalTensor>(*currFunctionPtr, DT_FP32, shape3);
+
+    auto &reshape1 = currFunctionPtr->AddOperation(Opcode::OP_RESHAPE, {inCast}, {ubTensor1});
+    auto &reshape2 = currFunctionPtr->AddOperation(Opcode::OP_RESHAPE, {ubTensor1}, {ubTensor2});
+    auto &sqrt = currFunctionPtr->AddOperation(Opcode::OP_SQRT, {ubTensor2}, {outCast});
+
+    currFunctionPtr->inCasts_.push_back(inCast);
+    currFunctionPtr->outCasts_.push_back(outCast);
+
+    RemoveRedundantReshape removeredundantpass;
+    EXPECT_NE(removeredundantpass.PostCheck(*currFunctionPtr), SUCCESS);
 }
 
 /*
