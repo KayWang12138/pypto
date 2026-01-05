d5c383d8fd540b33e109bc117bdbe6b27d01dfed
yang-zhipeng19 <yangzhipeng19@huawei.com>
2026-01-05 15:57:56 +0800

    fix(machine): Set cpu num and freq from platform

--- framework/src/machine/runtime/device_runner.cpp
+++ framework/src/machine/runtime/device_runner.cpp
@@ -64,6 +64,8 @@ constexpr uint32_t HIGHT_BIT = 16;
 
 constexpr uint32_t SUB_CORE = 3;
 constexpr uint32_t AIV_PER_AICORE = 2;
+constexpr uint32_t FREQ_DAV_2201 = 50;
+constexpr uint32_t FREQ_DAV_3510 = 1000;
 
 extern "C" __attribute__((weak)) int AdxDataDumpServerUnInit();
 namespace npu::tile_fwk {
@@ -185,7 +187,6 @@ int DeviceRunner::InitDeviceArgs(DeviceArgs &args) {
     };
     
     hostProf_.RegHostProf();
-    aicpuNum_ = aicpuNum_ < static_cast<int>(Platform::Instance().GetSoc().GetAICPUNum() - 1) ? aicpuNum_ : static_cast<int>(Platform::Instance().GetSoc().GetAICPUNum() - 1);
     GetHostProfTypeSwtich();
 
     memset_s(&args, sizeof(args), 0, sizeof(args));
@@ -193,6 +194,10 @@ int DeviceRunner::InitDeviceArgs(DeviceArgs &args) {
     std::vector<int64_t> regsPmu;
 
     args.archInfo = static_cast<ArchInfo>(Platform::Instance().GetSoc().GetNPUArch());
+    aicpuNum_ = args.archInfo == ArchInfo::DAV_2201 ? npu::tile_fwk::dynamic::DEVICE_MAX_AICPU_NUM - 1 :
+        npu::tile_fwk::dynamic::DEVICE_MAX_AICPU_NUM;
+    int cpuNum = static_cast<int>(Platform::Instance().GetSoc().GetAICPUNum() - 1);
+    aicpuNum_ = aicpuNum_ < cpuNum ? aicpuNum_ : cpuNum;
     auto it = addressMappingTable_.find(args.archInfo);
     if (it != addressMappingTable_.end()){
         if (it->second(regs, regsPmu) != 0) {
@@ -459,14 +464,15 @@ void DeviceRunner::DumpAiCoreExecutionTimeData() {
     std::string topo_txt_path = config::LogTopFolder() + "/dyn_topo.txt";
     std::string program_json_path = config::LogTopFolder() + "/program.json";
     std::string draw_swim_lane_py_path = GetCurrentSharedLibPath() + "/scripts/draw_swim_lane.py";
-    config::SetRunDataOption(KEY_SWIM_GRAPH_PATH, config::GetAbsoluteTopFolder() + "/merged_swimlane.json");        
+    config::SetRunDataOption(KEY_SWIM_GRAPH_PATH, config::GetAbsoluteTopFolder() + "/merged_swimlane.json");
+    uint64_t freq = (args_.archInfo == ArchInfo::DAV_2201) ? FREQ_DAV_2201 : FREQ_DAV_3510;
 
     if (FileExist(program_json_path) && FileExist(topo_txt_path)) {
         ALOG_INFO("The files program.json and dyn_topo.txt exist. Start merging the swimlane.");
         std::string command = "python3 "+ draw_swim_lane_py_path + " \""
                                 + jsonFilePath + "\" \""
                                 + topo_txt_path + "\" \""
-                                + program_json_path + "\" --label_type=1 --time_convert_denominator=50";
+                                + program_json_path + "\" --label_type=1 --time_convert_denominator=" + std::to_string(freq);
         if (system(command.c_str()) != 0) {
            ALOG_WARN("Failed to execute draw_swim_lane.py. Stop merging the swimlane.");
         }
