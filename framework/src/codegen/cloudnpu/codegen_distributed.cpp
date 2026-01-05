3766e117f351c613b6555d4e39fc3d72badc63ca
luoxinhui666 <luoxinhui2@huawei.com>
2026-01-05 17:07:27 +0800

    feat(distributed): Align predToken tiling with input tiles using index-based mapping

--- framework/src/codegen/cloudnpu/codegen_distributed.cpp
+++ framework/src/codegen/cloudnpu/codegen_distributed.cpp
@@ -152,9 +152,15 @@ std::string CodeGenOpCloudNPU::GenTemplateParamsForSet() const
     int64_t bufferEleNum = distOpAttr.setBufferShape[0];
     int32_t rowDimIndex = 2;
     int32_t colDimIndex = 3;
-    oss << "<" << GetTemplateDType() << ", " << originShape[shmemTensorIndex][1] << ", "
-        << originShape[shmemTensorIndex][rowDimIndex] << ", " << originShape[shmemTensorIndex][colDimIndex]
-        << ", " << bufferEleNum << ">";
+    if (distOpAttr.memType == 0) {
+        oss << "<" << GetTemplateDType() << ", " << originShape[shmemTensorIndex][1] << ", "
+            << originShape[shmemTensorIndex][rowDimIndex] << ", " << originShape[shmemTensorIndex][colDimIndex]
+            << ", " << bufferEleNum << ">";
+    } else {
+        oss << "<" << GetTemplateDType() << ", " << originShape[shmemTensorIndex][1] << ", "
+            << Distributed::SHMEM_SIGNAL_STRIDE << ", " << Distributed::MAX_TILE_NUM << ", " << bufferEleNum
+            << ">";
+    }
     return oss.str();
 }
 
@@ -329,9 +335,14 @@ std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapesForFfnCombineInfo() const
 std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapesForShmemSet() const
 {
     std::ostringstream oss;
+    DistOpAttr distOpAttr = npu::tile_fwk::AnyCast<DistOpAttr>(opAttrs.at(OpAttributeKey::distOpAttr));
     int32_t shmemTensorIndex = 3;
-    int32_t shmemTensorDim = 4;
-    oss << ", " << GenOffsets(shmemTensorIndex, shmemTensorDim);
+    int32_t shmemTensorDim = distOpAttr.memType == 0 ? 4 : 5;
+    if (distOpAttr.memType == 0) {
+        oss << ", " << GenOffsets(shmemTensorIndex, shmemTensorDim);
+    } else {
+        oss << ", " << GenOffsetsAndRawShapes(shmemTensorIndex, shmemTensorDim) << ", " << GenShapes(shmemTensorIndex, shmemTensorDim);
+    }
     return oss.str();
 }
 
