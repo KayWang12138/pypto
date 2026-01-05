ce314082c8cf20a8ef92f798544cccbb560674fa
zhaoshenjie <zhaoshenjie@huawei.com>
2026-01-05 16:34:47 +0800

    修复刷新MemId时错误刷新非spill链路的问题

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
@@ -87,7 +87,7 @@ struct IssueEntry {
         LogicalTensorPtr tensor) const;
     void UpdateTensorInputForView(Operation& op, std::shared_ptr<IssueEntry> &spillSrcIssue,
         LogicalTensorPtr tensor) const;
-    Operation* FindFirstView(Operation *op, LogicalTensorPtr tensor) const;
+    Operation* FindFirstView(Operation *op) const;
     std::string GetOpInfo();
 };
 
