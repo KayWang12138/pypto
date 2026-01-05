a63a465849724b69554c0c8577aa965131f4f311
luoxinhui666 <luoxinhui2@huawei.com>
2026-01-05 17:07:27 +0800

    feat(distributed): Align predToken tiling with input tiles using index-based mapping

--- framework/src/interface/operation/distributed/distributed_common.h
+++ framework/src/interface/operation/distributed/distributed_common.h
@@ -49,6 +49,7 @@ constexpr int32_t AIV_NUM = 4;
 constexpr int32_t RECEIVE_CNT_OUT_ROW = 1024;
 constexpr int32_t RECEIVE_CNT_OUT_COL = 512;
 constexpr int32_t SHMEM_SIGNAL_STRIDE = 8;
+constexpr int32_t DEFAULT_SIGNAL_NUM = 512;
 constexpr int32_t MAX_TILE_NUM = 1024;
 enum class TileIndex : size_t {
     HEAD_SHAPE,
