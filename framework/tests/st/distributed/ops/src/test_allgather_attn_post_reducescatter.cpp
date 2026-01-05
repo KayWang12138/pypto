ea4913a31745e2365c7f62cb803437519d275e94
luozhonglin <luozhonglin1@huawei.com>
2026-01-05 14:45:49 +0800

    fix(ditributed): Allgather attention用例前端修改

--- framework/tests/st/distributed/ops/src/test_allgather_attn_post_reducescatter.cpp
+++ framework/tests/st/distributed/ops/src/test_allgather_attn_post_reducescatter.cpp
@@ -24,34 +24,63 @@
 namespace npu::tile_fwk {
 namespace Distributed {
 
-void TestAllGatherAttentionPostReducescatter(OpTestParam &testParam)
-{
-     constexpr size_t paramsSize = 7;
-    auto [b, s, n, kvLoraRank, vHeadDim, h, typeNum] = GetParams<paramsSize>(GetGoldenDir() + "/params.bin");
-    DataType dtype = GetDataTypeNum(typeNum);
+struct TestParams {
+    size_t b;
+    size_t s;
+    size_t n;
+    size_t kvLoraRank;
+    size_t vHeadDim;
+    size_t h;
+    size_t typeNum;
+    DataType dtype;
+    
+    TestParams(OpTestParam& testParam) {
+        constexpr size_t paramsSize = 7;
+        auto [b_val, s_val, n_val, kvLoraRank_val, vHeadDim_val, h_val, typeNum_val] = 
+            GetParams<paramsSize>(GetGoldenDir() + "/params.bin");
+        
+        b = b_val;
+        s = s_val;
+        n = n_val;
+        kvLoraRank = kvLoraRank_val;
+        vHeadDim = vHeadDim_val;
+        h = h_val;
+        typeNum = typeNum_val;
+        dtype = GetDataTypeNum(typeNum);
+    }
+};
 
-    Shape agInShape = {b * n * s / testParam.rankSize, kvLoraRank};
-    Shape wLoraShape = {n, kvLoraRank, vHeadDim};
-    Shape wOutShape = {n * vHeadDim, h};
+std::tuple<Tensor, Tensor, Tensor, Tensor> InitializeTestData(OpTestParam &testParam, const TestParams& params) { 
+    Shape agInShape = {params.b * params.n * params.s / testParam.rankSize, params.kvLoraRank};
+    Shape wLoraShape = {params.n, params.kvLoraRank, params.vHeadDim};
+    Shape wOutShape = {params.n * params.vHeadDim, params.h};
+    Shape outShape = {params.b * params.s / testParam.rankSize, params.h};
 
-    Tensor agIn(dtype, agInShape, "agIn");
-    Tensor wLora(dtype, wLoraShape, "wLora");
-    Tensor wOut(dtype, wOutShape, "wOut");
+    Tensor agIn(params.dtype, agInShape, "agIn");
+    Tensor wLora(params.dtype, wLoraShape, "wLora");
+    Tensor wOut(params.dtype, wOutShape, "wOut");
+    Tensor out(params.dtype, outShape, "out");
 
-    std::vector<bfloat16> agInPtr =
+    std::vector<bfloat16> agInPtr = 
         ReadToVector<bfloat16>(GetGoldenDir() + "/ag_in_rank_" + std::to_string(testParam.rankId) + ".bin", agInShape);
-    std::vector<bfloat16> wLoraPtr =
+    std::vector<bfloat16> wLoraPtr = 
         ReadToVector<bfloat16>(GetGoldenDir() + "/w_lora_rank_" + std::to_string(testParam.rankId) + ".bin", wLoraShape);
-    std::vector<bfloat16> wOutPtr =
+    std::vector<bfloat16> wOutPtr = 
         ReadToVector<bfloat16>(GetGoldenDir() + "/w_out_rank_" + std::to_string(testParam.rankId) + ".bin", wOutShape);
-
-    Shape outShape = {b * s / testParam.rankSize, h};
-    Tensor out(dtype, outShape, "out");
 
     ProgramData::GetInstance().AppendInputs({RawTensorData::CreateTensor<bfloat16>(agIn, agInPtr)});
     ProgramData::GetInstance().AppendInputs({RawTensorData::CreateTensor<bfloat16>(wLora, wLoraPtr)});
     ProgramData::GetInstance().AppendInputs({RawTensorData::CreateTensor<bfloat16>(wOut, wOutPtr)});
     ProgramData::GetInstance().AppendOutputs({RawTensorData::CreateTensorZero(out)});
+    
+    return {agIn, wLora, wOut, out};
+}
+
+void TestAllGatherAttentionPostReducescatter(OpTestParam &testParam) {
+    constexpr size_t paramsSize = 7;
+    auto [b, s, n, kvLoraRank, vHeadDim, h, typeNum] = GetParams<paramsSize>(GetGoldenDir() + "/params.bin");
+    DataType dtype = GetDataTypeNum(typeNum);
+    auto [agIn, wLora, wOut, out] = InitializeTestData(testParam);
 
     FUNCTION("ALLGATHER_ATTNPOST_REDUCESCATTER", {agIn, wLora, wOut}, {out}) {
         Tensor agOut(dtype, {b * n * s, kvLoraRank}, "agOut");
@@ -59,8 +88,7 @@ void TestAllGatherAttentionPostReducescatter(OpTestParam &testParam)
             (void) unusedDynRankId;
             TileShape::Current().SetDistTile({64, b * n * s / testParam.rankSize / 64, 0}, {kvLoraRank, 1, 0}, 
                 {1, testParam.rankSize, 0});
-            Tensor fakeBarrierDummy(DT_INT32, {1, 1}, "fakeBarrierDummy");
-            ShmemAllGather(agIn, fakeBarrierDummy, testParam.group, agOut);
+            ShmemAllGather(agIn, agIn, testParam.group, agOut);
         }
         Tensor attnOut(dtype, {b * s, h}, "attnOut");
         LOOP("ATTNPOST", FunctionType::DYNAMIC_LOOP, batchId, LoopRange(1)) {
