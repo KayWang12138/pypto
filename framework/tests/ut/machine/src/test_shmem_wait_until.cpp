daed447399cbf44f63324d00058701e9e0da0f7c
luoxinhui666 <luoxinhui2@huawei.com>
2026-01-05 17:07:27 +0800

    feat(distributed): SetVecTile for ExpandFunction

--- framework/tests/ut/machine/src/test_shmem_wait_until.cpp
+++ framework/tests/ut/machine/src/test_shmem_wait_until.cpp
@@ -40,7 +40,7 @@ auto InitializeTensorInfo(const uint32_t rankSize) {
 auto InitializeAicpuCode(const uint32_t rankSize) {
     constexpr size_t codeSize = 17;
     auto data = std::make_unique<int32_t[]>(codeSize);
-    uint32_t initData[codeSize] = {153, 2, 2, 44, 4, 2, 18, 4, 0, 2, 1, 0, 4, rankSize, rankSize, 4, 8};
+    uint32_t initData[codeSize] = {153, 2, 2, 44, 4, 2, 18, 4, 0, 2, 1, 4, 0, 2, 1, 0, 4, rankSize, rankSize, 4, 8};
     std::copy(initData, initData + codeSize, data.get());
     npu::tile_fwk::dynamic::DevRelocVector<int32_t> aicpuCode(codeSize, data.get());
     return std::make_tuple(std::move(data), std::move(aicpuCode));
