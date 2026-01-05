8d18bb4bcdeeb49c49ab1fc0a598d05468470101
wangqun23 <wangqun23@huawei.com>
2026-01-05 11:59:21 +0800

    fix(pass): Fix the problem that occurs when axisCombine adds OP_BRCB but OP_BRCB's output is left without consumers

--- framework/tests/ut/passes/src/test_pad_local_buffer.cpp
+++ framework/tests/ut/passes/src/test_pad_local_buffer.cpp
@@ -967,15 +967,17 @@ TEST_F(TestPadLocalBuffer, axiscombine) {
     for (const auto &op : updatedOperations) {
         if (op.GetOpcode() == Opcode::OP_BRCB) {
             ++cnt;
-            if (op.HasAttr(OpAttributeKey::brcbIdx)) {
-                auto idx = op.GetIntAttribute(OpAttributeKey::brcbIdx) - 1;
-                auto tensor = op.GetIOperands()[idx];
-                EXPECT_TRUE(tensor != nullptr);
-                EXPECT_EQ(tensor->shape[0], K_4);
-                EXPECT_EQ(tensor->shape[1], K_8);
-                EXPECT_EQ(tensor->GetRawTensor()->GetRawShape()[0], K_8);
-                EXPECT_EQ(tensor->GetRawTensor()->GetRawShape()[1], K_8);
-            }
+            auto outputTensor = op.GetOOperands()[0];
+            EXPECT_TRUE(outputTensor->GetConsumers().size() != 0);
+        }
+        if (op.HasAttr(OpAttributeKey::brcbIdx)) {
+            auto idx = op.GetIntAttribute(OpAttributeKey::brcbIdx) - 1;
+            auto tensor = op.GetIOperands()[idx];
+            EXPECT_TRUE(tensor != nullptr);
+            EXPECT_EQ(tensor->shape[0], K_4);
+            EXPECT_EQ(tensor->shape[1], K_8);
+            EXPECT_EQ(tensor->GetRawTensor()->GetRawShape()[0], K_8);
+            EXPECT_EQ(tensor->GetRawTensor()->GetRawShape()[1], K_8);
         }
     }
     EXPECT_EQ(cnt, K_1);
