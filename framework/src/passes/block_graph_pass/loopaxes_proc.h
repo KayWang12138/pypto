80af847edbf102c167e3c2e324d6d783eda57a7e
jiachengtang <tangjiacheng7@huawei.com>
2026-01-06 20:55:35 +0800

    feat(pass): Add LoopAxesProc

--- framework/src/passes/block_graph_pass/loopaxes_proc.h
+++ framework/src/passes/block_graph_pass/loopaxes_proc.h
@@ -19,6 +19,7 @@
 #include "interface/function/function.h"
 #include "interface/operation/operation.h"
 #include "interface/tensor/logical_tensor.h"
+#include "passes/pass_interface/pass.h"
 
 namespace npu {
 namespace tile_fwk {
