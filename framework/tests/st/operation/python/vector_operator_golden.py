468ad4eb506d9680976186772b1847376d93851f
yang-fulai-3 <yangfulai3@huawei.com>
2026-01-06 19:33:19 +0800

    feat(operation): Add triu and tril

--- framework/tests/st/operation/python/vector_operator_golden.py
+++ framework/tests/st/operation/python/vector_operator_golden.py
@@ -1430,6 +1430,30 @@ def gen_cumsum_op_golden(case_name: str, output: Path, case_index: int = None) -
     return gen_op_golden("CumSum", cumsum_golden_func, output, case_index)
 
 
+def triu_golden_func(inputs: list, config: dict):
+    params = config.get("params")
+    diagonal = params["diagonal"]
+    if inputs[0].dtype == bfloat16:
+        input_tensor = torch.as_tensor(inputs[0].astype(np.float32)).to(torch.bfloat16)
+    else:
+        input_tensor = torch.from_numpy(inputs[0])
+    res = torch.triu(input_tensor, diagonal)
+    if inputs[0].dtype == bfloat16:
+        res = res.to(torch.float32).numpy().astype(bfloat16)
+        return [res]
+    
+    return [res.numpy()]
+
+@GoldenRegister.reg_golden_func(
+    case_names=[
+        "TestTriU/TriUOperationTest.TestTriU",
+    ]
+)
+def gen_triu_op_golden(case_name: str, output: Path, case_index: int = None) -> bool:
+    logging.debug("Case(%s), Golden creating...", case_name)
+    return gen_op_golden("TriU", triu_golden_func, output, case_index)
+
+
 def from_numpy(array: np.array):
     is_bfloat16 = array.dtype == bfloat16
     if is_bfloat16:
