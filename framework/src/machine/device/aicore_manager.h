d3c5976f72202293ec5e8edae7d6c843b0d9e03a
wan_jianfen <wanjianfen@hisilicon.com>
2026-01-06 20:00:02 +0800

    fix(machine): Change Ast to PyPto

--- framework/src/machine/device/aicore_manager.h
+++ framework/src/machine/device/aicore_manager.h
@@ -28,7 +28,6 @@
 #include "aicpu_task_manager.h"
 #include "interface/operation/opcode.h"
 #include "securec.h"
-#include "aicore_prof.h"
 #include "dynamic/device_utils.h"
 #include "aicore_dump.h"
 #include "interface/utils/common.h"
@@ -133,7 +132,7 @@ void SdmaPrefetch(DeviceTask *devTask);
 
 class AiCoreManager {
 public:
-    AiCoreManager(AicpuTaskManager &aicpuTaskManager) : aicpuTaskManager_(aicpuTaskManager), prof_(*this){};
+    AiCoreManager(AicpuTaskManager &aicpuTaskManager) : aicpuTaskManager_(aicpuTaskManager){};
     ~AiCoreManager(){};
 
     inline void InitTaskData(DeviceTaskCtrl *taskCtrl) {
@@ -445,7 +444,6 @@ private:
     StaticReadyCoreFunctionQueue *readyAicCoreFunctionQue_{nullptr};
     StaticReadyCoreFunctionQueue *readyAivCoreFunctionQue_{nullptr};
 
-    AiCoreProf prof_;
     AicoreDump aicoreDump_;
     int64_t dotStatus_{0};
     uint64_t waitTaskCnt_[AICORE_TYPE_NUM]{0,0};
