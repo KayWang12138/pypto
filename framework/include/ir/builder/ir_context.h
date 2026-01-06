1b5dbc93f92e695728fd15dad6107a6588d070a7
luohuan40 <luohuan40@huawei.com>
2026-01-06 15:52:33 +0800

    Modify UT test cases to adapt to the current stateless IR Builder.

--- framework/include/ir/builder/ir_context.h
+++ framework/include/ir/builder/ir_context.h
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file ir_context.h
+ * \brief
+ */
+
 #pragma once
 
 #include <cassert>
@@ -39,7 +54,7 @@ struct IRBuilderContext {
     }
 
     void PopScope() {
-        assert(!scopeStack.empty() && "IRBuilderContext::PopScope: scopeStack is empty");
+        // assert(!scopeStack.empty() && "IRBuilderContext::PopScope: scopeStack is empty");
 
         ScopeFrame frame = scopeStack.back();
         scopeStack.pop_back();
