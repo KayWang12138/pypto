6d87653d77ad88b0b463bf35166be528a55ebfe4
DCGDDD <yangxu14@huawei.com>
2026-01-06 14:30:49 +0800

    feat(utest): Support module-based compilation and execution of UTest

--- framework/tests/cmake/func.cmake
+++ framework/tests/cmake/func.cmake
@@ -110,8 +110,10 @@ function(PTO_Fwk_GTest_RunExe_GetPreExecSetup PY_CMD_SETUP PY_ENV_LINES BASH_CMD
 
     # 环境变量
     set(EnvLines)
-    # 处理变量 LD_LIBRARIES_EXT 及环境变量 LD_LIBRARY_PATH
-    set(LD_LIBRARY_PATH_EXT)
+    # 处理变量 LD_LIBRARIES_EXT 及环境变量 LD_LIBRARY_PATH 及 CMAKE_LIBRARY_OUTPUT_DIRECTORY
+    # 1. 当前 UTest/STest 已把动态库生成路径设置到 CMAKE_LIBRARY_OUTPUT_DIRECTORY 路径下, 此处需增加该路径配置;
+    # 2. LD_LIBRARY_PATH_EXT 处理保留, 已供后续其他场景使用;
+    set(LD_LIBRARY_PATH_EXT ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
     foreach (LIBRARY ${ARG_LD_LIBRARIES_EXT})
         add_dependencies(${ARG_TARGET} ${LIBRARY})
         list(APPEND LD_LIBRARY_PATH_EXT "$<TARGET_FILE_DIR:${LIBRARY}>")
@@ -301,4 +303,23 @@ function(PTO_Fwk_GTest_GetGTestFilterStr GTEST_FILTER_STR)
     )
     string(REPLACE "," ":" OutputVariable "${OutputVariable}")
     set(${GTEST_FILTER_STR} ${OutputVariable} PARENT_SCOPE)
+endfunction()
+
+function(PTO_Fwk_GTest_AddModuleDir)
+    cmake_parse_arguments(
+            ARG
+            ""
+            "DIR;MODULE_LIST"
+            ""
+            ""
+            ${ARGN}
+    )
+    if ((NOT ARG_MODULE_LIST) OR "${ARG_MODULE_LIST}x" STREQUAL "ONx")
+        set(_TestModuleList "ALL")
+    else ()
+        string(REPLACE "," ";" _TestModuleList "${ARG_MODULE_LIST}")
+    endif ()
+    if ("${_TestModuleList}" STREQUAL "ALL" OR ("${ARG_DIR}" IN ${_TestModuleList}))
+        add_subdirectory(${ARG_DIR})
+    endif()
 endfunction()
