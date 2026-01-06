1bb4092ba860826428ccab9c6a17047e2cbec264
yang-fulai-3 <yangfulai3@huawei.com>
2026-01-06 19:33:19 +0800

    feat(operation): Add triu and tril

--- framework/src/interface/operation/opcode.cpp
+++ framework/src/interface/operation/opcode.cpp
@@ -346,6 +346,9 @@ void OpcodeManager::RegisterVector() {
     RegisterInfo(Opcode::OP_CUM_SUM, OpCoreType::AIV, "CUM_SUM", {MemoryType::MEM_UB}, {MemoryType::MEM_UB},
         {"TileOp::TcumSum", PIPE_V, PIPE_V, CoreType::AIV}, OpCalcType::OTHER,
         {OP_ATTR_PREFIX + "axis", OP_ATTR_PREFIX + "flag", OpAttributeKey::excludeBufferReuse});
+    RegisterInfo(Opcode::OP_TRIUL, OpCoreType::AIV, "TRIUL", {MemoryType::MEM_UB}, {MemoryType::MEM_UB},
+        {"TileOp::TTriUL", PIPE_V, PIPE_V, CoreType::AIV}, OpCalcType::OTHER,
+        {OP_ATTR_PREFIX + "diagonal", OP_ATTR_PREFIX + "flag", OpAttributeKey::excludeBufferReuse});
     RegisterInfo(Opcode::OP_LOGICALAND, OpCoreType::AIV, "LOGICALAND", {MemoryType::MEM_UB, MemoryType::MEM_UB},
         {MemoryType::MEM_UB, MemoryType::MEM_UB}, {"TileOp::TlogicalAnd", PIPE_V, PIPE_V, CoreType::AIV},
         OpCalcType::ELMWISE, {}, TileShapeVerifier::Verify);
@@ -755,6 +758,7 @@ std::unordered_map<Opcode, std::string> SUPPORT_TILETENSOR_OPS{
     {  Opcode::OP_TRANSPOSE_MOVEOUT,  "TTransMoveOut"},
     {                Opcode::OP_ADD,           "TAdd"},
     {                Opcode::OP_SUB,           "TSub"},
+    {              Opcode::OP_TRIUL,         "TTriUL"},
     {                Opcode::OP_DIV,           "TDiv"},
     {                Opcode::OP_MUL,           "TMul"},
     {     Opcode::OP_GATHER_ELEMENT, "TgatherElement"},
