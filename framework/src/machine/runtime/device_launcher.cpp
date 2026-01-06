caa9bb8dc332d13d38efbc702299b887b9a95cb1
wan_jianfen <wanjianfen@hisilicon.com>
2026-01-06 20:00:02 +0800

    fix(machine): Change Ast to PyPto

--- framework/src/machine/runtime/device_launcher.cpp
+++ framework/src/machine/runtime/device_launcher.cpp
@@ -17,9 +17,13 @@
 #include "machine/runtime/device_launcher_binding.h"
 #include "machine/host/backend.h"
 #include "machine/runtime/host_prof.h"
+#include "cost_model/simulation/backend.h"
+
+
 namespace npu::tile_fwk::dynamic {
 namespace {
     constexpr uint32_t kMinDefaultDim = 20;
+    constexpr uint32_t BufferSize = 64;
 }
 int GetCfgBlockdim() {
 #ifdef BUILD_WITH_CANN
@@ -156,7 +160,7 @@ int DeviceLauncher::DeviceLaunchOnceWithDeviceTensorData(
         }
     }
     CheckDeviceId();
-    AstKernelArgs kArgs;
+    DeviceKernelArgs kArgs;
     DeviceLauncherConfigFillDeviceInfo(config);
     DeviceInitTilingData(DeviceMemoryUtils(), kArgs, function->GetDyndevAttribute()->devProgBinary, config, cachedOperator);
     DeviceRunCacheKernelSet(function, (uint8_t *)kArgs.cfgdata);
