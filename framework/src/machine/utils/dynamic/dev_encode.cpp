fc39ddff09a64c903f545f8e6e820e0f9fe6cd0d
heyufeng <heyufeng11@hisilicon.com>
2026-01-07 09:43:11 +0800

    a5 support atten&atten pre

--- framework/src/machine/utils/dynamic/dev_encode.cpp
+++ framework/src/machine/utils/dynamic/dev_encode.cpp
@@ -1379,6 +1379,7 @@ struct EncodeDevAscendFunctionInfo {
         });
 
         totalZeroPred = callopList.size();
+        AddDependOperandsToColorGraph(callopList, callopIndexDict);
         for (size_t index = 0; index < callopList.size(); index++) {
             if (callOpPredDict[callopList[index]] != 0) {
                 totalZeroPred = index;
