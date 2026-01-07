73427fbe15d42e35c6b530a72567956960ccb9f6
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-07 21:09:27 +0800

    feat(codegen): Support VF Fusion

---
+++ framework/src/codegen/stmt_mgr/codegen_for_block.h
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file codegen_for_block.h
+ * \brief
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "interface/operation/opcode.h"
+#include "interface/tensor/symbolic_scalar.h"
+
+namespace npu::tile_fwk {
+constexpr const int MAX_LOOP_DEPTH = 3;
+
+extern std::unordered_map<Opcode, std::string> SUPPORT_VF_FUSE_OPS;
+
+struct ForNode {
+    std::string loopVar;
+    SymbolicScalar start;
+    SymbolicScalar extent;
+    SymbolicScalar step;
+
+    std::string Print() const;
+    void PrintInit(std::ostringstream &os) const;
+    void PrintCond(std::ostringstream &os) const;
+    void PrintUpdate(std::ostringstream &os) const;
+};
+
+class ForBlockManager {
+public:
+    explicit ForBlockManager(const std::shared_ptr<SymbolManager> &symbolManager) : sm_(symbolManager) {
+        forNodes_.reserve(MAX_LOOP_DEPTH);
+    };
+    ~ForBlockManager() = default;
+
+    void UpdateAxesList(const std::vector<SymbolicScalar> &axesList);
+
+    void LoopStart() {
+        isLoopStart_ = true;
+        isInLoop_ = true;
+    }
+    void LoopEnd() { isLoopEnd_ = true; }
+
+    void OutLoop() {
+        isInLoop_ = false;
+        isLoopStart_ = false;
+        isLoopEnd_ = false;
+    }
+
+    bool IsInLoop() { return isInLoop_; }
+
+    void AddTensorInLoopBody(const std::string &tensorName) { tensorNeedSetAddr_.emplace_back(tensorName); }
+    void AddOpInLoopBody(const std::string &op) { opList_.emplace_back(op); }
+
+    std::string Print() const;
+
+private:
+    void PrintForHeader(std::ostringstream &os) const;
+    void PrintForBody(std::ostringstream &os) const;
+    void PrintForEnd(std::ostringstream &os) const;
+    void PrintOffsetDef(std::ostringstream &os) const;
+    void PrintSetAddrs(std::ostringstream &os) const;
+    void PrintSetAddrSingle(std::ostringstream &os, const std::string &tensor) const;
+    void PrintTileOps(std::ostringstream &os) const;
+
+    std::shared_ptr<SymbolManager> sm_;
+    std::vector<SymbolicScalar> axesList_;
+    std::vector<ForNode> forNodes_;
+    std::vector<std::string> tensorNeedSetAddr_;
+    std::vector<std::string> opList_;
+    bool isInLoop_{false};
+    bool isLoopStart_{false};
+    bool isLoopEnd_{false};
+};
+} // namespace npu::tile_fwk
--- framework/src/passes/block_graph_pass/codegen_preproc.cpp
+++ framework/src/passes/block_graph_pass/codegen_preproc.cpp
@@ -66,7 +66,8 @@ Status CodegenPreproc::SaveGmTensorParamIdxToOp(Function &func) const {
                 gmParamInCallFunc[addrPos].emplace_back(&op);
             }
         }
-        APASS_LOG_INFO_F(Elements::Operation, "%d:%sgmParamInCallFunc size: %zu", __LINE__, __FUNCTION__, gmParamInCallFunc.size());
+        APASS_LOG_INFO_F(
+            Elements::Operation, "%d:%sgmParamInCallFunc size: %zu", __LINE__, __FUNCTION__, gmParamInCallFunc.size());
         int tensorParamIdx{0};
         for (auto param : gmParamInCallFunc) {
             for (auto op : param.second) {
@@ -96,7 +97,9 @@ Status CodegenPreproc::ProcessAxis(Operation &op, std::vector<bool> attr, bool i
         }
     }
     if (attr.size() != operands.size()) {
-        APASS_LOG_ERROR_F(Elements::Operation, "%d %s attr size(%zu) is not equal to operands size(%zu), ProcessAxis failed.", op.GetOpMagic(), op.GetOpcodeStr().c_str(), attr.size(), operands.size());
+        APASS_LOG_ERROR_F(Elements::Operation,
+            "%d %s attr size(%zu) is not equal to operands size(%zu), ProcessAxis failed.", op.GetOpMagic(),
+            op.GetOpcodeStr().c_str(), attr.size(), operands.size());
         return FAILED;
     }
     for (size_t i = 0; i < operands.size(); ++i) {
@@ -149,7 +152,7 @@ Status CodegenPreproc::ForceCombineAxis(Function &func) const {
     return SUCCESS;
 }
 
-inline bool IsUBCopy(Operation& op) {
+inline bool IsUBCopy(Operation &op) {
     if (IsCopyIn(op.GetOpcode())) {
         auto outTensor = *(op.GetOOperands().begin());
         if (outTensor->GetMemoryTypeOriginal() == MemoryType::MEM_UB) {
@@ -185,7 +188,8 @@ Status CodegenPreproc::ForceCombineAxisForAxisCombine(Function &func) const {
             std::vector<bool> outputCombineAxis;
             for (size_t i = 0; i < op.GetOOperands().size(); ++i) {
                 LogicalTensors operands = op.GetOOperands();
-                if (operands[i]->tensor->rawshape.back() == 1 && OpcodeManager::Inst().GetOpCalcType(op.GetOpcode()) != OpCalcType::REDUCE) {
+                if (operands[i]->tensor->rawshape.back() == 1 &&
+                    OpcodeManager::Inst().GetOpCalcType(op.GetOpcode()) != OpCalcType::REDUCE) {
                     outputCombineAxis.push_back(true);
                 } else {
                     outputCombineAxis.push_back(false);
@@ -207,7 +211,7 @@ std::string CodegenPreproc::DumpOpList(Function &function) {
                 bool needAlloc = false;
                 op.oOperand[0]->GetAttr(OpAttributeKey::needAlloc, needAlloc);
                 ss << op.GetOpcodeStr() << "[" << op.GetOpMagic() << "], needAlloc: " << static_cast<int>(needAlloc)
-                    << ", memId: " << op.oOperand[0]->memoryrange.memId << "\n";
+                   << ", memId: " << op.oOperand[0]->memoryrange.memId << "\n";
             } else {
                 ss << op.GetOpcodeStr() << "[" << op.GetOpMagic() << "]" << "\n";
             }
@@ -215,6 +219,44 @@ std::string CodegenPreproc::DumpOpList(Function &function) {
         idx++;
     }
     return ss.str();
+}
+
+void CodegenPreproc::MarkForScope(Function &function) {
+    int64_t lastGroupId = INVALID_LOOP_GROUPID;
+    shared_ptr<Operation> lastOp = nullptr;
+    for (auto &subProgram : function.rootFunc_->programs_) {
+        for (auto &op : subProgram.second->Operations(false)) {
+            if (!op.HasAttr(OpAttributeKey::loopGroup)) {
+                continue;
+            }
+            curGroupId = op.GetIntAttribute(OpAttributeKey::loopGroup);
+            if (curGroupId == INVALID_LOOP_GROUPID) {
+                if (lastGroupId != INVALID_LOOP_GROUPID && lastOp != nullptr) {
+                    lastOp.SetAttribute(OpAttributeKey::loopGroupEnd, true);
+                    lastGroupId = INVALID_LOOP_GROUPID;
+                    lastOp = nullptr;
+                }
+                continue;
+            }
+
+            if (lastGroupId != curGroupId) {
+                op.SetAttribute(OpAttributeKey::loopGroupStart, true);
+                if (lastGroupId != INVALID_LOOP_GROUPID && lastOp != nullptr) {
+                    lastOp.SetAttribute(OpAttributeKey::loopGroupEnd, true);
+                }
+
+                lastGroupId = curGroupId;
+                lastOp = op.shared_from_this();
+                continue;
+            }
+        }
+        if (lastGroupId != INVALID_LOOP_GROUPID && lastOp != nullptr) {
+            subProgram.second->Operations.back().SetAttribute(OpAttributeKey::loopGroupEnd, true);
+        }
+        int64_t lastGroupId = INVALID_LOOP_GROUPID;
+        shared_ptr<Operation> lastOp = nullptr;
+    }
+}
 }
 
 void CodegenPreproc::SetNeedAllocAttr(Function &function) {
@@ -237,20 +279,23 @@ void CodegenPreproc::SetNeedAllocAttr(Function &function) {
 }
 
 Status CodegenPreproc::RunOnFunction(Function &function) {
-    APASS_LOG_INFO_F(Elements::Operation, "===============================================================> Start CodegenPreproc.");
+    APASS_LOG_INFO_F(
+        Elements::Operation, "===============================================================> Start CodegenPreproc.");
     for (auto &op : function.Operations()) {
         if (op.GetOpcode() == Opcode::OP_VIEW_TYPE) {
             op.SetOpCode(Opcode::OP_VIEW);
         }
     }
     if (SaveGmTensorParamIdxToOp(function) != SUCCESS) {
-        APASS_LOG_ERROR_F(Elements::Operation, "CodegenPreproc RunOnFunction failed at function SaveGmTensorParamIdxToOp.");
+        APASS_LOG_ERROR_F(
+            Elements::Operation, "CodegenPreproc RunOnFunction failed at function SaveGmTensorParamIdxToOp.");
         return FAILED;
     }
 
     if (ConfigManager::Instance().GetOperationConfig(KEY_COMBINE_AXIS, false)) {
         if (ForceCombineAxisForAxisCombine(function) != SUCCESS) {
-            APASS_LOG_ERROR_F(Elements::Operation, "CodegenPreproc RunOnFunction failed at function ForceCombineAxisForAxisCombine.");
+            APASS_LOG_ERROR_F(
+                Elements::Operation, "CodegenPreproc RunOnFunction failed at function ForceCombineAxisForAxisCombine.");
             return FAILED;
         }
     } else {
@@ -260,8 +305,11 @@ Status CodegenPreproc::RunOnFunction(Function &function) {
         }
     }
 
+    MarkForScope(function);
+
     SetNeedAllocAttr(function);
-    APASS_LOG_INFO_F(Elements::Operation, "===============================================================> Finish CodegenPreproc.");
+    APASS_LOG_INFO_F(
+        Elements::Operation, "===============================================================> Finish CodegenPreproc.");
     return SUCCESS;
 }
 
