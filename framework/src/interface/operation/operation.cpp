96edc2349caaf06b231e884fe14ca0bdfef1eace
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-07 21:09:27 +0800

    feat(codegen): Support VF Fusion

--- framework/src/interface/operation/operation.cpp
+++ framework/src/interface/operation/operation.cpp
@@ -67,6 +67,10 @@ const std::string OpAttributeKey::distOpAttr = "DIST_OP_ATTR";
 const std::string OpAttributeKey::subBlockIdx = "SUB_BLOCK_IDX";
 const std::string OpAttributeKey::brcbIdx = "brcb_idx";
 const std::string OpAttributeKey::quantFlag = "op_attr_vector_quant_flag";
+const std::string OpAttributeKey::loopGroup = "LOOP_GROUP";
+const std::string OpAttributeKey::loopAxes = "LOOP_AXES";
+const std::string OpAttributeKey::loopGroupStart = "LOOP_GROUP_START";
+const std::string OpAttributeKey::loopGroupEnd = "LOOP_GROUP_END";
 
 const std::string ConvOpAttributeKey::cin = "CIN";
 const std::string ConvOpAttributeKey::cout = "COUT";
