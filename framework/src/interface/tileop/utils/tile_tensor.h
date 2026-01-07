f2ab7f97103d559676c5dd65476a122e13b60185
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-07 21:09:27 +0800

    feat(codegen): Support VF Fusion

--- framework/src/interface/tileop/utils/tile_tensor.h
+++ framework/src/interface/tileop/utils/tile_tensor.h
@@ -18,6 +18,11 @@
 
 #include "common_type.h"
 
+template <typename... Indexs>
+using Offsets = Std::tuple<Indexs...>;
+
+using TileOffset = Offsets<size_t, size_t, size_t>;
+
 template <typename T, typename LA, Hardware FMT = Hardware::UB>
 struct TileTensor {
     using Type = T;
@@ -32,12 +37,20 @@ struct TileTensor {
 
     __aicore__ inline TileTensor(uint64_t addr) : addr_(addr) {}
     __aicore__ inline uint64_t GetAddr() { return addr_; }
+    __aicore__ inline void SetAddr(uint64_t addr) { addr_ = addr; }
     __aicore__ inline LA GetLayout() { return layout_; }
     __aicore__ inline const LA GetLayout() const { return layout_; }
     __aicore__ inline static constexpr bool IsStaticLayout() { return LayoutType::IsStaticLayout(); }
     __aicore__ inline constexpr Hardware GetPhyType() { return FORMAT; }
     __aicore__ inline Shape GetShape() { return layout_.GetShape(); }
     __aicore__ inline Stride GetStride() { return layout_.GetStride(); }
+
+    __aicore__ inline uint64_t GetLinearAddr(const TileOffset &offsets = TileOffset(0, 0, 0)) {
+        size_t offset = Std::get<DIM_1ST>(offsets) * layout_.template GetStrideDim<DIM_1ST, MAX_DIMS>();
+        offset += Std::get<DIM_2ND>(offsets) * layout_.template GetStrideDim<DIM_2ND, MAX_DIMS>();
+        offset += Std::get<DIM_3RD>(offsets) * layout_.template GetStrideDim<DIM_3RD, MAX_DIMS>();
+        return addr_ + offset * sizeof(Type);
+    }
 
 private:
     uint64_t addr_;
