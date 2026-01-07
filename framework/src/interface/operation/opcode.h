918d4410110a699cbaacc1fe8d8ff768a2c54574
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-07 21:09:27 +0800

    feat(codegen): Support VF Fusion

--- framework/src/interface/operation/opcode.h
+++ framework/src/interface/operation/opcode.h
@@ -610,6 +610,7 @@ inline bool IsOpCodeSupportMultiProducers(Opcode opCode) {
 }
 
 extern std::unordered_map<Opcode, std::string> SUPPORT_TILETENSOR_OPS;
+extern std::unordered_set<Opcode> SUPPORT_VF_FUSE_OPS;
 // NEXTNEXT: for test case use only
 inline void InsertTileTensorOp(Opcode opCode, const std::string &tileOpName) {
     SUPPORT_TILETENSOR_OPS.emplace(opCode, tileOpName);
