d1ac9f5824374820452bdf5472b780a6fca0a509
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-07 21:09:27 +0800

    feat(codegen): Support VF Fusion

--- framework/src/interface/operation/operation.h
+++ framework/src/interface/operation/operation.h
@@ -84,6 +84,10 @@ public:
     static const std::string subBlockIdx;
     static const std::string brcbIdx;
     static const std::string quantFlag;
+    static const std::string loopGroup;
+    static const std::string loopAxes;
+    static const std::string loopGroupStart;
+    static const std::string loopGroupEnd;
 };
 
 
