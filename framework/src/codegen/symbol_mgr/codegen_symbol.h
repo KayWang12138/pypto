33bd4edd5d2ffcdc07a104c2ba7d7275a976be99
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-07 21:09:27 +0800

    feat(codegen): Support VF Fusion

--- framework/src/codegen/symbol_mgr/codegen_symbol.h
+++ framework/src/codegen/symbol_mgr/codegen_symbol.h
@@ -36,6 +36,13 @@ const std::string COORD = "Coord";
 using BufferType = enum OperandType;
 using AllocKey = std::tuple<BufferType, int64_t /*RangeStart*/, int64_t /*RangeEnd*/>;
 
+struct ShapeInLoop {
+    size_t loopDepth{0};
+    std::vector<int64_t> originShape;
+    std::vector<int64_t> rawShape;
+    std::vector<SymbolicScalar> dynamicValidShape;
+};
+
 inline std::string GetLayoutType(BufferType bufType, int dim, bool isStatic) {
     std::string prefix = bufType == BUF_DDR ? "Dyn" : isStatic ? "Static" : "Local";
     std::ostringstream ss;
@@ -49,6 +56,7 @@ inline std::string GetLayoutType(BufferType bufType, int dim, bool isStatic) {
 struct TileTensor {
     int magic; // tensor magic numbuer
     int dim;
+    bool isStatic;
     DataType dtype;
     BufferType bufType;
     std::string bufVar;
@@ -58,7 +66,7 @@ struct TileTensor {
     std::vector<std::string> stride;
     std::vector<int64_t> rawShape;
     std::vector<int64_t> localBufOffset;
-    bool isStatic;
+    ShapeInLoop shapeInLoop;
 
     bool operator==(const TileTensor &other) const {
         return dim == other.dim && bufVar == other.bufVar && shape == other.shape && dtype == other.dtype &&
@@ -81,7 +89,8 @@ struct TileTensor {
             // cast local buffer pointer to uint64_t to adapt TileTensor mode
             oss << "uint64_t)";
             int64_t linearOffset = CalcLinearOffset(rawShape, localBufOffset);
-            if (linearOffset != 0) { // append linear offset, e.g. UBTileTensorFP32Dim2_1 ubTensor_1((uint64_t)((float *)UB_S0_E4096 + 32))
+            if (linearOffset != 0) { // append linear offset, e.g. UBTileTensorFP32Dim2_1 ubTensor_1((uint64_t)((float
+                                     // *)UB_S0_E4096 + 32))
                 oss << "((" << DataType2CCEStr(dtype) << " *)" << bufVar << " + " << linearOffset << ")";
             } else {
                 oss << bufVar;
@@ -149,8 +158,8 @@ struct TileTensorUsing {
     bool isStatic;
 
     bool operator==(const TileTensorUsing &other) const {
-        return dtype == other.dtype && bufType == other.bufType && originShape == other.originShape &&
-               rawShape == other.rawShape;
+        bool baseCompare = dtype == other.dtype && bufType == other.bufType && rawShape == other.rawShape;
+        return isStatic ? baseCompare && originShape == other.originShape : baseCompare;
     }
 
     std::string GenName() const {
@@ -225,12 +234,17 @@ public:
     std::string AddTileTensorUsing(const TileTensorUsing &tileTensorUsing);
     void AddTileTensor(const TileTensor &tileTensor);
     std::string QueryTileTensorByMagic(int magic);
-    // To be compatible with GM Tensor in Static Function Type like same ddr magic number with different parmaIdx & 'GMStackBase'
-    // e.g. ((__gm__ GMTensorInfo*)param + 1), ((__gm__ GMTensorInfo*)param + 2)
+    std::string QueryTileTensorInLoopByMagic(int magic);
+    std::string QueryTileTensorFullDimByMagic(int magic);
+    std::string QueryTileTensorFullDimByTensorInLoop(const std::string &tensorName);
+    // To be compatible with GM Tensor in Static Function Type like same ddr magic number with different parmaIdx &
+    // 'GMStackBase' e.g. ((__gm__ GMTensorInfo*)param + 1), ((__gm__ GMTensorInfo*)param + 2)
     std::string QueryTileTensorByBufVarName(const std::string &bufVarName);
 
     std::string GenUsingList();
     std::string GenTileTensorDefList();
+    void InForLoop() { isInForLoop_ = true; }
+    void OutForLoop() { isInForLoop_ = false; }
 
 private:
     std::shared_ptr<LogicalTensor> GetTensorByMagic(int magicNum) const;
@@ -248,7 +262,11 @@ private:
     std::unordered_map<TileTensor, std::string, TileTensorHash> tileTensor_;
     //<tensor magic, tensorName>
     std::unordered_map<int, std::string> tileTensorByMagic_;
+    std::unordered_map<int, std::string> tileTensorByMagicInLoop_;
+    // <tensor name in for loop, tensor name with full dim>
+    std::unordered_map<std::string, std::string> tensorNameInLoopToFullDim_;
     //<using type, TileTensorUsing>
     std::unordered_map<std::string, TileTensorUsing> tileTensorUsing_;
+    bool isInForLoop_{false};
 };
 } // namespace npu::tile_fwk
