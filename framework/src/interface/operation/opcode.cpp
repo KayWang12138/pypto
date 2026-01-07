bbca017268d33f46e952a4dbb80f64a3fc8b7c68
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-07 21:09:27 +0800

    feat(codegen): Support VF Fusion

--- framework/src/interface/operation/opcode.cpp
+++ framework/src/interface/operation/opcode.cpp
@@ -787,4 +787,23 @@ std::unordered_map<Opcode, std::string> SUPPORT_TILETENSOR_OPS{
     {        Opcode::OP_L0C_COPY_UB,       "TExtract"},
     {            Opcode::OP_VEC_DUP,        "TVecDup"},
 };
+
+std::unordered_set<Opcode> SUPPORT_VF_FUSE_OPS{
+    Opcode::OP_ADD,
+    Opcode::OP_SUB,
+    Opcode::OP_DIV,
+    Opcode::OP_MUL,
+    Opcode::OP_ADDS,
+    Opcode::OP_MULS,
+    Opcode::OP_SUBS,
+    Opcode::OP_DIVS,
+    Opcode::OP_RSQRT,
+    Opcode::OP_SQRT,
+    Opcode::OP_EXP,
+    Opcode::OP_MAXIMUM,
+    Opcode::OP_MINIMUM,
+    Opcode::OP_ROWSUM_SINGLE,
+    Opcode::OP_ROWMAX_SINGLE,
+    Opcode::OP_ROWMIN_SINGLE,
+};
 } // namespace npu::tile_fwk
