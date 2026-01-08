85fe5b8e2b0962ade92569b44d7e4f0022686e7d
陈展熹 <chenzhanxi1@huawei.com>
2026-01-08 21:40:28 +0800

    feat(opcode): Add opcode template

--- framework/include/ir/tile_graph.h
+++ framework/include/ir/tile_graph.h
@@ -20,6 +20,16 @@
 
 namespace pto {
 
+enum CastMode {
+    CAST_NONE = 0,
+    CAST_RINT = 1,  // round to nearest, tie to even
+    CAST_ROUND = 2, // round to nearest, tie away from zero
+    CAST_FLOOR = 3, // round to minus infinity
+    CAST_CEIL = 4,  // round to positive infinity
+    CAST_TRUNC = 5, // round to zero
+    CAST_ODD = 6   // round to odd (Von Neumann rounding)
+};
+
 #define DEFOP DEFOP_CLASS
 #include "tile_graph.def"
 #undef DEFOP
