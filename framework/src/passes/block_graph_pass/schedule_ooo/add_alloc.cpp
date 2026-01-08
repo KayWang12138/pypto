7c30fbb1b96662dc3231a1668e989f5da82fef7d
gggxinmeng <guoxinmeng@huawei.com>
2026-01-08 10:06:16 +0800

    feat(Pass): modify insert addalloc

--- framework/src/passes/block_graph_pass/schedule_ooo/add_alloc.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/add_alloc.cpp
@@ -51,7 +51,15 @@ Status AddAlloc::AddAndCheckAlloc(Function &function) {
         APASS_LOG_ERROR_F(Elements::Tensor, "GenAllocNode failed.");
         return FAILED;
     }
-    function.SortOperations();
+    std::vector<Operation *> newOperations;
+     for (auto& op : function.Operations(false).DuplicatedOpList()) {
+         if (op->GetOpcodeStr().find("ALLOC") != std::string::npos) {
+             newOperations.insert(newOperations.begin(), op);
+             continue;
+         }
+         newOperations.push_back(op);
+     }
+     function.ScheduleBy(newOperations);
     return SUCCESS;
 }
 
