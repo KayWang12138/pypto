9ed44df939bb34de30f89942062221826b991b67
Immutable <gaoqiancheng@huawei.com>
2026-01-08 14:42:28 +0800

    fix(Interface): Interpreter Adapt MatMul Op
    Change-Id: I536f3dd29c13c567e7e39f7e61c317ba5aefe912

--- framework/src/interface/interpreter/function.h
+++ framework/src/interface/interpreter/function.h
@@ -682,6 +682,9 @@ struct FunctionInterpreter {
             UpdateSymbolDict(loop->IterSymbolName(), idx);
             loopSymbolDict[loop->IterSymbolName()] = idx;
             Operation *callop = ExecuteFunctionLoopLookupSat(loop);
+            if (callop == nullptr) {
+                continue;
+            }
             Function *callee = GetCallee(callop);
 
             ExecuteHandleOperationBegin(callop);
