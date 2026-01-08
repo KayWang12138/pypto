c3731631ff8649c5db09f684f850398a85311338
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-01-08 16:07:32 +0800

    feat(pass): Adjust operation list sequence for vf fusion feature

---
+++ framework/src/passes/block_graph_pass/tune_tileopseq_for_vf.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file tune_tileopseq_for_vf.h
+ * \brief
+ */
+
+#ifndef TUNE_TILEOPSEQ_FOR_VF_H
+#define TUNE_TILEOPSEQ_FOR_VF_H
+
+#include "passes/pass_interface/pass.h"
+#include "interface/program/program.h"
+#include "interface/function/function.h"
+#include "passes/pass_utils/pass_utils.h"
+
+namespace npu::tile_fwk {
+class TuneTileOpSeqForVF : public Pass {
+public:
+    TuneTileOpSeqForVF() : Pass("TuneTileOpSeqForVF") {}
+    ~TuneTileOpSeqForVF() override = default;
+
+    Status RunOnFunction(Function &function) override;
+
+private:
+    Status ChangeOpSeq(std::vector<Operation *> &opList, std::vector<size_t> &tunedOpList, std::vector<size_t> &pipeVIdx, PipeSync &ps, bool isAIV1);
+};
+}
+#endif // TUNE_TILEOPSEQ_FOR_VF_H
