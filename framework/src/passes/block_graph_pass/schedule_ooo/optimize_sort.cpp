1820f180e3bc9ba699a5c35a0e7c4617df0f6bd0
gggxinmeng <guoxinmeng@huawei.com>
2026-01-08 09:11:53 +0800

    feat(Pass): Add compare index

--- framework/src/passes/block_graph_pass/schedule_ooo/optimize_sort.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/optimize_sort.cpp
@@ -186,7 +186,7 @@ Status OptimizeSort::PriorDFS(std::unordered_map<Opcode, int> preNodePriority) {
     for (size_t i = 0; i < operations.size(); i++) {
         visited[operations[i]] = false;
         if (outGraph[operations[i]].empty()) {
-            outNodeQueue.insert(outNodeQueue.begin(), operations[i]);
+            outNodeQueue.insert(operations[i]);
         }
     }
 
