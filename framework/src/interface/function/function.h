2e34c71bc91ce4911f30290e5a4885007ac0d917
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-01-09 20:32:07 +0800

    feat(pass): add tune sync

--- framework/src/interface/function/function.h
+++ framework/src/interface/function/function.h
@@ -30,6 +30,7 @@
 #include "interface/tensor/tensor_slot.h"
 #include "interface/cache/hash.h"
 #include "passes/pass_utils/pass_utils.h"
+#include "passes/block_graph_pass/insert_sync.h"
 
 namespace npu::tile_fwk {
 constexpr int FUNCTION_MAX_INCASTS = 10000;
@@ -484,6 +485,16 @@ public:
     std::map<uint64_t, Function*> programs_; // root function持有，所有异构的leaf function
     Function *rootFunc_ = nullptr; // TileGraph和RootGraph都需要保留，且需要映射关系
     ParamConfigs paramConfigs_;
+    // vf融合适配需要pass间传递的参数
+    std::unordered_map<PipeType, int> pipeEndTime; // function中每个pipe执行结束的时间
+    std::unordered_map<Operation *, Operation *> setWaitOpMap;
+    std::unordered_map<Operation *, Operation *> waitSetOpMap;
+    std::unordered_map<Operation *, Operation *> setOpMap;
+    std::unordered_map<Operation *, Operation *> opSetMap;
+    std::unordered_map<Operation *, Operation *> waitOpMap;
+    std::unordered_map<Operation *, Operation *> opWaitMap;
+    std::vector<PipeSync::IssueQueue> issueState;
+    std::vector<Operation *> oriOpList;
 
     Function(const Program &belongTo, const std::string &funcMagicName, const std::string &funcRawName,
         Function *parentFunc);
--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
@@ -504,9 +504,14 @@ Status OoOScheduler::RetireOpAndAwakeSucc(IssueEntryPtr issue, uint64_t& commitC
 
 Status OoOScheduler::RetireIssueStage(uint64_t& commitCnt, int& nextCycle) {
     for (auto& [pipeType, pipe] : issueQueues) {
-        (void)pipeType;
         if (!pipe.busy) {
             continue;
+        }
+        if (!pipeEndTime.count(pipeType)) {
+            pipeEndTime.emplace(pipeType, pipe.curOpRetireCycle);
+        } else {
+            auto curEndTime = pipeEndTime[pipeType];
+            pipeEndTime[pipeType] = std::max(curEndTime, pipe.curOpRetireCycle);
         }
         if (pipe.curOpRetireCycle <= clock) {   // 如果该pipe内当前正在执行op，在clock的时刻已经执行完毕。
             IssueEntryPtr issue = pipe.curIssue;
@@ -1017,6 +1022,7 @@ Status OoOScheduler::Schedule(const std::vector<Operation *> &operations) {
     }
     PrintOpList(newOperations_);
     function_.SetStackWorkespaceSize(workspaceOffset);
+    function_.pipeEndTime = pipeEndTime;
     return SUCCESS;
 }
 
