1f7b5ed63fe23c17883d279fb2fc7b06bc6c897a
zhaojunhan <zhaojunhan1@huawei.com>
2026-01-09 10:24:57 +0800

    feat(config): Convert the key of config to lowercase
    Co-authored-by: zhaojunhan<zhaojunhan1@huawei.com>
    
    !267 merge UpToLow into master
    
    feat(config): Convert the key of config to lowercase
    
    Created-by: zhaojunhan
    Commit-by: zhaojunhan
    Merged-by: cann-robot
    Description: ## 描述
    Convert the key of config to lowercase
    
    docs/tools/computation_graph/查看健康报告.md
    health_check改为小写，且在配置文件中的位置有变化
    
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：配置文件重构
    
    See merge request: cann/pypto!267

--- framework/src/interface/program/program.cpp
+++ framework/src/interface/program/program.cpp
@@ -257,7 +257,6 @@ bool Program::BeginFunction(const std::string &funcName,
         currentFunctionPtr_->GetGraphType() != GraphType::EXECUTE_GRAPH) {
         GetTensorSlotManager()->BeginScope(currentFunctionPtr_);
     }
-    SetParamConfig(currentFunctionPtr_);
 
 #if ENABLE_HIDDENLOOP
     // Begin new hidden loop for the new function
@@ -300,7 +299,7 @@ Operation *Program::FinishCurrentFunction(const std::shared_ptr<TensorSlotScope>
 
 // Helper function: Dump tensor graph if needed
 void Program::DumpTensorGraphIfNeeded(Function *result) {
-    if (config::GetPassDefaultConfig("print_graph", false) &&
+    if (config::GetPassDefaultConfig(KEY_PRINT_GRAPH, false) &&
         result->IsGraphType(GraphType::TENSOR_GRAPH)) {
         result->DumpJsonFile(config::LogTensorGraphFolder() + "/" + result->GetRawName() + ".json");
         result->DumpFile(config::LogTensorGraphFolder() + "/" + result->GetRawName() + ".tifwkgr");
@@ -356,6 +355,8 @@ std::tuple<Function*, Operation *, bool> Program::EndFunction(const std::string 
     Operation *callop = FinishCurrentFunction(scope, generateCall);
     bool hit = QueryAndUpdateCurrentFunction();
     auto result = currentFunctionPtr_;
+
+    SetParamConfig(currentFunctionPtr_);
 
     DumpTensorGraphIfNeeded(result);
     PopStackAndUpdateCurrent();
@@ -441,8 +442,8 @@ void TraverAndDumpParent(Function *func, Json &progDump) {
 Json Program::DumpJson(Function *mainFunc) const {
     Json progDump;
     progDump["version"] = T_VERSION;
-    progDump["pass_thread_num"] = config::GetPassGlobalConfig("pass_thread_num", 1);
-    progDump["enable_cvfuse"] = config::GetPassGlobalConfig("enable_cv_fuse", false);
+    progDump["pass_thread_num"] = config::GetPassGlobalConfig(KEY_PASS_THREAD_NUM, 1);
+    progDump["enable_cvfuse"] = config::GetPassGlobalConfig(KEY_ENABLE_CV_FUSE, false);
     if (mainFunc == nullptr) {
         std::shared_ptr<npu::tile_fwk::Function> dyndevFunc = nullptr;
         std::vector<std::shared_ptr<npu::tile_fwk::Function>> rootFuncs;
