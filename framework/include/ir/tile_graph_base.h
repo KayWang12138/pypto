70ccf2b44debb7832a8b04d8de93628933d7e425
yinjie27 <yinjie27@huawei.com>
2026-01-09 16:55:22 +0800

    feat(codegen): Add source_cpp ir serializer as codegen

--- framework/include/ir/tile_graph_base.h
+++ framework/include/ir/tile_graph_base.h
@@ -45,7 +45,7 @@ public:
 class ElementWiseUnaryTileBaseOp : public ElementWiseTileBaseOp {
 public:
     ElementWiseUnaryTileBaseOp(Opcode opcode, TileValuePtr input, TileValuePtr output)
-        : ElementWiseTileBaseOp(opcode, {ValueCast<Value>(input)}, {ValueCast<Value>(output)}) {}
+        : ElementWiseTileBaseOp(opcode, {ObjectCast<Value>(input)}, {ObjectCast<Value>(output)}) {}
 };
 
 class ElementWiseBinaryTileBaseOp : public ElementWiseTileBaseOp {
@@ -61,7 +61,7 @@ public:
 class ElementWiseScalarMixBinaryTileBaseOp : public ElementWiseTileBaseOp {
 public:
     ElementWiseScalarMixBinaryTileBaseOp(Opcode opcode, TileValuePtr lhs, ScalarValuePtr rhs, TileValuePtr output)
-        : ElementWiseTileBaseOp(opcode, {ValueCast<Value>(lhs), ValueCast<Value>(rhs)}, {ValueCast<Value>(output)}) {}
+        : ElementWiseTileBaseOp(opcode, {ObjectCast<Value>(lhs), ObjectCast<Value>(rhs)}, {ObjectCast<Value>(output)}) {}
 };
 
 class ReduceTileBaseOp : public TileBaseOp {
@@ -71,12 +71,15 @@ class BroadcastTileBaseOp : public TileBaseOp {
 };
 
 class DataCopyTileBaseOp : public TileBaseOp {
+public:
+    DataCopyTileBaseOp(Opcode opcode, ValuePtr src, std::vector<ScalarValuePtr> offset, ValuePtr dst)
+      : TileBaseOp(opcode, ValueUtils::Join(src, offset), {dst}) {}
 };
 
 class MatmulTileBaseOp : public TileBaseOp {
 public:
     MatmulTileBaseOp(Opcode opcode, TileValuePtr input, std::vector<ScalarValuePtr> offsets, TileValuePtr output)
-        : TileBaseOp(opcode, {ValueCast<Value>(input)}, {ValueCast<Value>(output)}),  offsets_(offsets) {}
+        : TileBaseOp(opcode, {ObjectCast<Value>(input)}, {ObjectCast<Value>(output)}),  offsets_(offsets) {}
 
     ScalarValuePtr GetOffset(size_t index) const;
 
@@ -92,13 +95,19 @@ public:
                             TileValuePtr out)
         : TileBaseOp(opcode, {lhs, rhs}, {out}) {
     }
+class DataCopyInTileBaseOp : public DataCopyTileBaseOp {
+public:
+    DataCopyInTileBaseOp(Opcode opcode, TensorValuePtr src, std::vector<ScalarValuePtr> offset, TileValuePtr dst)
+      : DataCopyTileBaseOp(opcode, ObjectCast<Value>(src), offset, ObjectCast<Value>(dst)) {}
 };
 
-class SysBaseOp : public ScalarBaseOp {
+class DataCopyOutTileBaseOp : public DataCopyTileBaseOp {
 public:
-    std::string GetName() const;
-private:
-    std::string name_;
+    DataCopyOutTileBaseOp(Opcode opcode, TileValuePtr src, std::vector<ScalarValuePtr> offset, TensorValuePtr dst)
+      : DataCopyTileBaseOp(opcode, ObjectCast<Value>(src), offset, ObjectCast<Value>(dst)) {}
+};
+
+class MatmulTileBaseOp : public TileBaseOp {
 };
 
 class CustomTileBaseOp : public TileBaseOp {
