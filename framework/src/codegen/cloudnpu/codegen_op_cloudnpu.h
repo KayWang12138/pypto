1e9fffd3faa091673efa0a11f27ed30a635437e9
wkkk0528 <wangkai578@huawei.com>
2026-01-09 12:42:35 +0800

    feat(interface): TRange of pto-isa

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -90,7 +90,7 @@ public:
     std::string GenGatherElementOp() const;
 
     std::string GenRangeOp() const;
-    std::string PrintRangeTileTensor(std::string startVal, std::string stepVal) const;
+    std::string PrintRangeTileTensor(std::string startVal, std::string stepVal, std::string &tileIdxExpr) const;
     std::string GenL0CToUBTileTensor() const;
 
     std::string GenScatterElementSOp() const;
--- framework/src/interface/tileop/TileOpImpl.h
+++ framework/src/interface/tileop/TileOpImpl.h
@@ -46,6 +46,7 @@
 #include "vector/pair_binary.h"
 #include "vector/where.h"
 #include "vector/vector_dup.h"
+#include "vector/range.h"
 #endif
 
 #endif
