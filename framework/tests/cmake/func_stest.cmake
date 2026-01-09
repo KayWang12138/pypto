6ce259efdd72cbd9a7ab2ec8e51393e3be0e8faa
DCGDDD <yangxu14@huawei.com>
2026-01-09 15:54:40 +0800

    feat(utest): Optimize the time consumption during the compilation phase

--- framework/tests/cmake/func_stest.cmake
+++ framework/tests/cmake/func_stest.cmake
@@ -49,9 +49,6 @@ function(PTO_Fwk_STest_AddLib)
                 ${PTO_Fwk_STestNamePrefix}_utils
                 GTest::gtest
     )
-    # 后检查
-    PTO_Fwk_AnalysisTargetHeaderFiles(TARGET ${ARG_TARGET})
-    # STest 暂不支持并行执行
     set(PTO_Fwk_STestCaseLibraries            ${PTO_Fwk_STestCaseLibraries}            ${ARG_TARGET}            CACHE INTERNAL "" FORCE)
     set(PTO_Fwk_STestCaseLdLibrariesExt       ${PTO_Fwk_STestCaseLdLibrariesExt}       ${ARG_LD_LIBRARIES_EXT}  CACHE INTERNAL "" FORCE)
     set(PTO_Fwk_STestCaseGoldenScriptPathList ${PTO_Fwk_STestCaseGoldenScriptPathList} ${ARG_GOLDEN_SCRIPT_DIR} CACHE INTERNAL "" FORCE)
@@ -300,6 +297,7 @@ function(PTO_Fwk_STest_AddExe_RunExe)
     execute_process(COMMAND touch ${_Sources})
     list(REMOVE_DUPLICATES PTO_Fwk_STestCaseLibraries)
     set(PTO_Fwk_Libraries
+            tile_fwk_simulation_platform
             tile_fwk_interface
             tile_fwk_codegen
             tile_fwk_compiler
@@ -311,9 +309,8 @@ function(PTO_Fwk_STest_AddExe_RunExe)
     PTO_Fwk_GTest_AddExe(
             TARGET                      ${ARG_TARGET}
             SOURCES                     ${_Sources}
-            PRIVATE_LINK_LIBRARIES      ${PTO_Fwk_STestNamePrefix}_intf_pub ${PTO_Fwk_Libraries} ${PTO_Fwk_STestCaseLibraries}
+            PRIVATE_LINK_LIBRARIES      ${PTO_Fwk_STestNamePrefix}_utils ${PTO_Fwk_Libraries} ${PTO_Fwk_STestCaseLibraries}
     )
-    add_dependencies(${ARG_TARGET} tile_fwk_server)
 
     #
     # 执行
