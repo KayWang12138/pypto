aa04665ec03c1eed1ec0cd2b59b934ec69b193bc
jiaweizhuang <zhuangjiawei@hisilicon.com>
2026-01-09 12:40:08 +0000

    desired ast transform feature

---
+++ examples/ir_builder/builder_api_level_2.py
@@ -0,0 +1,84 @@
+"""
+Simplify builder_api_level_1.py, by the following ast-transforms on `create_function`:
+
+
+1) Mapping native `for` statement
+
+```
+for i in pto.block.loop(0, batch, step=1, unroll=4):
+    (loop body using `i`)
+```
+
+to:
+```
+i = builder.create_scalar(ctx, ir.DataType.int32, "i")
+constant0 = builder.create_const(ctx, 0, "const_0")
+constant1 = builder.create_const(ctx, 1, "const_1")
+
+fs = builder.create_for(ctx, i, constant0, batch, constant1)
+fs.properties()["unroll"] = "4"
+
+with for_scope(builder, ctx, fs):
+    (loop body using `i`)
+```
+
+
+2) Mapping native `if`/`else`
+
+```
+if i:
+    (if body)
+else:
+    (else body)
+```
+
+to:
+```
+ifs = builder.create_if(ctx, i)
+
+with if_then_scope(builder, ctx, ifs):
+    (if body)
+
+with if_else_scope(builder, ctx, ifs):
+    (else body)
+
+builder.exit_if(ctx, ifs)  # this auto-inserted by transform
+```
+
+
+3) Mapping tile computation
+
+Maps:
+```
+res_loop_x = pto.block.Tile(tile_shape, ir.DataType.float, "outputX")
+pto.block.add(res_loop_x, scale1, out=res_loop_x)
+```
+
+or even simpler:
+```
+res_loop_x = pto.block.Tile(tile_shape, ir.DataType.float, "outputX")
+res_loop_x += scale1
+```
+
+to:
+```
+res_loop_x = builder.create_tile(ctx, tile_shape, ir.DataType.float, "outputX")
+add_op_x = builder.create_binary_scalar_op(ir.Opcode.OP_ADDS, res_loop_x, scale1, res_loop_x)
+builder.emit(ctx, add_op_x)
+```
+
+Another example, from
+```
+res_if_y = pto.block.Tile(tile_shape, ir.DataType.float, "outputY")
+res_if_y = res_loop_y * scale2
+```
+
+to 
+```
+res_if_y = builder.create_tile(ctx, tile_shape, ir.DataType.float, "outputY")
+mul_op_y = builder.create_binary_scalar_op(ir.Opcode.OP_MULS, res_loop_y, scale2, res_if_y)
+builder.emit(ctx, mul_op_y)
+```
+
+"""
+
