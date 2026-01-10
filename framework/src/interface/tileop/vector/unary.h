6ad624fc1540882a1dc9fae7a67b1f9570ea69f2
liuyang454 <liuyang454@huawei.com>
2026-01-10 09:47:17 +0800

    feat(interface): Support ptobrcinline

--- framework/src/interface/tileop/vector/unary.h
+++ framework/src/interface/tileop/vector/unary.h
@@ -33,25 +33,49 @@ TILEOP void UnaryComputeImpl(T0 dst, T1 src) {
         pto::TSQRT(dst, src);
         return;
     }
+    if constexpr (op == UnaryOp::BRCB) {
+        pto::TROWEXPAND(dst, src);
+        return;
+    }
 }
 
 template <UnaryOp op, typename T0, typename T1>
 TILEOP void UnaryCompute(T0 dst, T1 src) {
-    if constexpr (TileOp::IsConstContinous<T0, T1>() == true) {
-        auto dstTile = PtoTile<T0, pto::BLayout::RowMajor, true>().Data();
-        auto srcTile = PtoTile<T1, pto::BLayout::RowMajor, true>().Data();
-        pto::TASSIGN(dstTile, (uint64_t)dst.GetAddr());
-        pto::TASSIGN(srcTile, (uint64_t)src.GetAddr());
-        UnaryComputeImpl<op>(dstTile, srcTile);
-        return;
-    }
+    // if constexpr (TileOp::IsConstContinous<T0, T1>() == true) {
+    //     auto dstTile = PtoTile<T0, pto::BLayout::RowMajor, true>().Data();
+    //     auto srcTile = PtoTile<T1, pto::BLayout::RowMajor, true>().Data();
+    //     pto::TASSIGN(dstTile, (uint64_t)dst.GetAddr());
+    //     pto::TASSIGN(srcTile, (uint64_t)src.GetAddr());
+    //     UnaryComputeImpl<op>(dstTile, srcTile);
+    //     return;
+    // }
     const auto dstLayout = dst.GetLayout();
     auto shape0 = dstLayout.template GetShapeDim<DIM_1ST, MAX_DIMS>();
     auto shape1 = dstLayout.template GetShapeDim<DIM_2ND, MAX_DIMS>();
     auto shape2 = dstLayout.template GetShapeDim<DIM_3RD, MAX_DIMS>();
 
-    auto dstTile = PtoTile<T0>(dst);
-    auto srcTile = PtoTile<T1>(src);
+    if constexpr (op == UnaryOp::BRCB) {
+        constexpr size_t expectSize = 5;
+        constexpr auto srcTileH = TileOp::GetTensorTileShapeDim<T1, DIM_4TH, expectSize>();
+        constexpr auto srcTileW = TileOp::GetTensorTileShapeDim<T1, DIM_5TH, expectSize>();
+        constexpr auto dstTileH = TileOp::GetTensorTileShapeDim<T0, DIM_4TH, expectSize>();
+        constexpr auto dstTileW = TileOp::GetTensorTileShapeDim<T0, DIM_5TH, expectSize>();
+
+        using DstTileDefine =pto::Tile<pto::TileType::Vec, typename T0::Type, dstTileH, dstTileW, pto::BLayout::RowMajor>;
+        using SrcTileDefine = typename std::conditional<(srcTileW == 1), 
+            pto::Tile<pto::TileType::Vec, typename T1::Type, srcTileH, srcTileW, pto::BLayout::ColMajor>,
+            pto::Tile<pto::TileType::Vec, typename T1::Type, srcTileH, srcTileW, pto::BLayout::RowMajor>>::type;
+
+        // const auto srcLayout = src.GetLayout();
+        // auto srcShape3 = srcLayout.template GetShapeDim<3, expectSize>();
+        // auto srcShape4 = srcLayout.template GetShapeDim<4, expectSize>();
+        SrcTileDefine srcTile;
+        DstTileDefine dstTile;
+    } else {
+        auto dstTile = PtoTile<T0>(dst);
+        auto srcTile = PtoTile<T1>(src);
+    }
+
     for (size_t n0Index = 0; n0Index < shape0; ++n0Index) {
         for (size_t n1Index = 0; n1Index < shape1; ++n1Index) {
             for (size_t n2Index = 0; n2Index < shape2; ++n2Index) {
@@ -77,5 +101,10 @@ TILEOP void TRsqrt(T0 dst, T1 src) {
 template <typename T0, typename T1>
 TILEOP void TSqrt(T0 dst, T1 src) {
     UnaryCompute<UnaryOp::SQRT>(dst, src);
+}
+
+template <typename T0, typename T1>
+TILEOP void Tbrcb(T0 dst, T1 src) {
+    UnaryCompute<UnaryOp::BRCB>(dst, src);
 }
 #endif
