be5ca5a1945e8aff203b22e043ddfb7fdb02d264
jiachengtang <tangjiacheng7@huawei.com>
2026-01-12 09:44:44 +0800

    refactor(pass): update mixdependencyanalyzer

--- framework/src/passes/block_graph_pass/mix_subgraph_split/mix_dependency_analyzer.h
+++ framework/src/passes/block_graph_pass/mix_subgraph_split/mix_dependency_analyzer.h
@@ -26,6 +26,7 @@
 #include "interface/tensor/logical_tensor.h"
 #include "tilefwk/tilefwk.h"
 #include "passes/tile_graph_pass/subgraph_to_function.h"
+#include "passes/block_graph_pass/mix_subgraph_split/mix_subgraph_split_utils.h"
 
 namespace npu {
 namespace tile_fwk {
---
+++ framework/src/passes/block_graph_pass/mix_subgraph_split/mix_subgraph_split_utils.h
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file mix_dependency_analyzer.h
+ * \brief 用于提供所需的接口
+ */
+
+#ifndef MIX_SUBGRAPH_SPLIT_UTILS_H
+#define MIX_SUBGRAPH_SPLIT_UTILS_H
+
+#include <set>
+#include <vector>
+#include <unordered_map>
+#include "passes/pass_interface/pass.h"
+#include "interface/function/function.h"
+#include "interface/operation/operation.h"
+#include "interface/program/program.h"
+#include "interface/tensor/logical_tensor.h"
+#include "tilefwk/tilefwk.h"
+#include "passes/tile_graph_pass/subgraph_to_function.h"
+#include "passes/block_graph_pass/mix_subgraph_split/mix_subgraph_split_utils.h"
+
+namespace npu {
+namespace tile_fwk {  
+// Mix子图内部独立子图的信息
+struct InternalComponentInfo {
+    int internalSubgraphID;  // mix子图内部的子图ID(cube/vector组件ID)
+    std::vector<Operation*> operations; // 包含的op
+    std::string suffix;
+    AIVCore aivCore;
+    
+    InternalComponentInfo(int id, const std::string& suf = "")
+        : internalSubgraphID(id), suffix(suf), aivCore(AIVCore::UNSPECIFIED) {}
+        
+    InternalComponentInfo(int id, const std::string& suf, AIVCore aiv)
+        : internalSubgraphID(id), suffix(suf), aivCore(aiv) {}
+};
+
+// 内部依赖信息结构
+struct InternalDependencyInfo {
+    int srcComp;  // 源scope索引
+    int dstComp;  // 目标scope索引
+    LogicalTensorPtr dummyTensor;  // 用于表示依赖的dummy tensor
+    uint64_t dummyTensorMagic;  // dummy tensor的magic值
+    bool isSameType;  // 是否是同类型scope依赖
+    ComponentType compType;  // scope类型（C或V）
+    
+    InternalDependencyInfo(int src, int dst, ComponentType type)
+        : srcComp(src), dstComp(dst), dummyTensor(nullptr), 
+          dummyTensorMagic(0), isSameType(true), compType(type) {}
+};
+} // namespace tile_fwk
+} // namespace npu
+
+#endif // MIX_SUBGRAPH_SPLIT_UTILS_H
