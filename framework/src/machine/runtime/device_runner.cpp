5febca6833c12113c75c7e1a022a40e815b1fdf1
gaoxingwang <camuel.gao@huawei.com>
2026-01-12 12:01:19 +0800

    perf(machine): Host perf analysis and optimize

--- framework/src/machine/runtime/device_runner.cpp
+++ framework/src/machine/runtime/device_runner.cpp
@@ -40,6 +40,7 @@
 #include "machine/platform/platform_manager.h"
 #include "machine/runtime/device_error_tracking.h"
 #include "nlohmann/json.hpp"
+#include "machine/runtime/perf_analysis.h"
 
 using json = nlohmann::json;
 extern char _binary_kernel_o_start[];
@@ -575,13 +576,13 @@ int DeviceRunner::launchDynamicAiCpuInit(rtStream_t aicpuStream, AstKernelArgs *
 }
 
 int DeviceRunner::RunPrepare() {
-   for (uint32_t i = 0; i < args_.nrAic + args_.nrAiv; i++) {
-        rtMemcpy((reinterpret_cast<uint8_t *>(args_.sharedBuffer + sizeof(uint64_t) * SHAK_BUF_DFX_DATA_INDEX)) + i * SHARED_BUFFER_SIZE,
-            sizeof(uint64_t),
-            reinterpret_cast<uint8_t *>(&perfData_[i]),
-            sizeof(uint64_t),
-            RT_MEMCPY_HOST_TO_DEVICE);
-    }
+//    for (uint32_t i = 0; i < args_.nrAic + args_.nrAiv; i++) {
+//         rtMemcpy((reinterpret_cast<uint8_t *>(args_.sharedBuffer + sizeof(uint64_t) * SHAK_BUF_DFX_DATA_INDEX)) + i * SHARED_BUFFER_SIZE,
+//             sizeof(uint64_t),
+//             reinterpret_cast<uint8_t *>(&perfData_[i]),
+//             sizeof(uint64_t),
+//             RT_MEMCPY_HOST_TO_DEVICE);
+//     }
     if (isCapture_) {
         aclmdlRICaptureMode mode = ACL_MODEL_RI_CAPTURE_MODE_GLOBAL;
         aclmdlRICaptureThreadExchangeMode(&mode);
@@ -643,6 +644,8 @@ int DeviceRunner::DynamicKernelLaunch(rtStream_t aicpuStream, rtStream_t aicoreS
     }
     ReportHostProfInfo(startTime, 1, MSPROF_GE_TASK_TYPE_AI_CPU);
 
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_LAUNCH_AICPU_INIT);
+
     startTime = MsprofSysCycleTime();
     rc = launchDynamicAiCpu(aicpuStream, kernelArgs);
     if (rc < 0) {
@@ -651,6 +654,8 @@ int DeviceRunner::DynamicKernelLaunch(rtStream_t aicpuStream, rtStream_t aicoreS
     }
     ReportHostProfInfo(startTime, aicpuNum_, MSPROF_GE_TASK_TYPE_AI_CPU);
 
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_LAUNCH_AICPU_RUN);
+
     startTime = MsprofSysCycleTime();
     rc = launchDynamicAiCore(aicoreStream, kernelArgs);
     if (rc < 0) {
@@ -658,6 +663,8 @@ int DeviceRunner::DynamicKernelLaunch(rtStream_t aicpuStream, rtStream_t aicoreS
         return rc;
     }
     ReportHostProfInfo(startTime, blockdim, MSPROF_GE_TASK_TYPE_MIX_AIC, true);
+
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_LAUNCH_AICORE);
     return rc;
 }
 
@@ -708,13 +715,20 @@ int DeviceRunner::DynamicSeparateLaunch(rtStream_t aicpuStream, rtStream_t ctrlS
     return rc;
 }
 
+
 int DeviceRunner::DynamicLaunch(rtStream_t aicpuStream, rtStream_t ctrlStream, rtStream_t aicoreStream, int64_t taskId,
     AstKernelArgs *kernelArgs, int blockdim, int launchAicpuNum) {
     InitializeErrorCallback();
+    
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_INIT_ERR_CALLBACK);
+
     if (!g_IsFirstInit) {
         InitAiCpuSoBin();
     }
     g_IsFirstInit = true;
+    
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_INIT_AICPUSO);
+
     #ifdef BUILD_WITH_NEW_CANN
     if (!g_IsNullLaunched) {
         auto ret = LoadAicpuOp::GetInstance().LaunchBuiltInOp(aicpuStream, kernelArgs, 1, "PyptoNull");
@@ -745,15 +759,24 @@ int DeviceRunner::DynamicLaunch(rtStream_t aicpuStream, rtStream_t ctrlStream, r
     localArgs.generalAddr = kernelArgs->opMetaAddrs.generalAddr;
     localArgs.stitchPoolAddr = kernelArgs->opMetaAddrs.stitchPoolAddr;
     localArgs.isGETensorList = kernelArgs->toSubMachineConfig.isGETensorList;
-    int rc = rtMemcpy(kernelArgs->cfgdata, sizeof(localArgs), &localArgs, sizeof(localArgs), RT_MEMCPY_HOST_TO_DEVICE);
+
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_INIT_KERNEL_ARGS);
+
+    int rc = rtMemcpyAsync(kernelArgs->cfgdata, sizeof(localArgs), &localArgs, sizeof(localArgs), RT_MEMCPY_HOST_TO_DEVICE, aicpuStream);
     if (rc != 0) {
         ALOG_ERROR_F("Copy args failed %p rc %d\n", kernelArgs->cfgdata, rc);
         return rc;
     }
+
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_INIT_H2D_MEMCPY);
+
     if (RunPrepare() < 0) {
         ALOG_ERROR_F("Prepare failed %d\n", rc);
         return rc;
     }
+
+    HOST_PERF_TRACE(TracePhase::RUN_DEV_KERNEL_INIT_RUN_PREPARE);
+
     if (ctrlStream == nullptr) {
         return DynamicKernelLaunch(aicpuStream, aicoreStream, kernelArgs, blockDim_);
     } else {
--- python/src/bindings/runtime.cpp
+++ python/src/bindings/runtime.cpp
@@ -20,6 +20,7 @@
 #include "interface/interpreter/raw_tensor_data.h"
 #include "machine/runtime/device_launcher_binding.h"
 #include "machine/runtime/emulation_launcher.h"
+#include "machine/runtime/perf_analysis.h"
 
 using namespace npu::tile_fwk;
 using namespace npu::tile_fwk::dynamic;
@@ -108,6 +109,10 @@ std::string DeviceRunOnceDataFromHost(
 std::string OperatorDeviceRunOnceDataFromDevice([[maybe_unused]] py::int_ pythonOperatorPython,
     [[maybe_unused]] const std::vector<DeviceTensorData> &inputs, [[maybe_unused]] const std::vector<DeviceTensorData> &outputs,
     [[maybe_unused]] py::int_ incomingStreamPython, [[maybe_unused]] py::int_ workspaceData) {
+
+    PerfAnalysis::Get().TraceInit();
+    HOST_PERF_EVT_BEGIN(EventPhase::RUN_DEV);
+
 #ifdef BUILD_WITH_CANN
     auto opAddr = static_cast<uintptr_t>(pythonOperatorPython);
     if (opAddr == 0) {
@@ -154,6 +159,8 @@ std::string OperatorDeviceRunOnceDataFromDevice([[maybe_unused]] py::int_ python
         return "device run failed";
     }
 #endif
+
+    HOST_PERF_EVT_END(EventPhase::RUN_DEV);
     return "";
 }
 
@@ -190,6 +197,9 @@ void DeviceFini() {
 }
 
 uintptr_t OperatorBegin() {
+    PerfAnalysis::Get().TraceInit();
+    HOST_PERF_EVT_BEGIN(EventPhase::COMPILE);
+
     ExportedOperator *op = ExportedOperatorBegin();
     auto opAddr = reinterpret_cast<uintptr_t>(op);
     return opAddr;
@@ -199,12 +209,16 @@ std::string OperatorEnd(uintptr_t opAddr) {
     ExportedOperator *op = reinterpret_cast<ExportedOperator *>(opAddr);
     ExportedOperatorEnd(op);
 
+    HOST_PERF_EVT_END(EventPhase::COMPILE);
     return "";
 }
 
 std::string BuildCache(uintptr_t opAddr, const std::vector<DeviceTensorData> &inputList,
         const std::vector<DeviceTensorData> &outputList) {
     ExportedOperator *op = reinterpret_cast<ExportedOperator *>(opAddr);
+
+    PerfAnalysis::Get().TraceInit();
+    HOST_PERF_EVT_BEGIN(EventPhase::BUILD_CTRL_CACHE);
 
     if (config::GetRuntimeOption<int64_t>(STITCH_CFGCACHE_SIZE) != 0) {
         DeviceLauncherConfig config;
@@ -214,6 +228,7 @@ std::string BuildCache(uintptr_t opAddr, const std::vector<DeviceTensorData> &in
         }
     }
 
+    HOST_PERF_EVT_END(EventPhase::BUILD_CTRL_CACHE);
     return "";
 }
 
