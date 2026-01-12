ee1c20c88b53b5456b221173cb54970dc1e9a8b6
gggxinmeng <guoxinmeng@huawei.com>
2026-01-12 14:32:59 +0800

    fix(Pass): Use optimize sort and modify base init

--- framework/src/passes/block_graph_pass/schedule_ooo/schedule_base.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/schedule_base.h
@@ -75,8 +75,8 @@ public:
     std::unordered_map<MemoryType, int64_t> localMemSize; //内存剩余情况
     std::unordered_map<MemoryType, int64_t> localMemoryCurrentSize;
     std::unordered_map<int, LocalBufferPtr> localBufferMap; //memid:local
-    std::map<const Operation*, std::vector<Operation*>> opConsumers;
-    std::map<const Operation*, std::vector<Operation*>> opProducers;
+    std::map<Operation*, std::vector<Operation*>> opConsumers;
+    std::map<Operation*, std::vector<Operation*>> opProducers;
 
     //  初始依赖的list序列
     std::vector<Operation*> operations;
@@ -245,12 +245,6 @@ public:
     }
 
     void AddDependency(Operation* preOp, Operation* postOp, bool isAlloc) {
-        if (std::find(operations.begin(), operations.end(), preOp) == operations.end()) {
-            return;
-        }
-        if (std::find(operations.begin(), operations.end(), postOp) == operations.end()) {
-            return;
-        }
         if (isAlloc || (!IsOpAlloc(preOp) && !IsOpAlloc(postOp))) {
             outGraph[preOp].insert(postOp);
             inGraph[postOp].insert(preOp);
@@ -258,18 +252,18 @@ public:
     }
 
     void FindDependencies(Operation* op) {
-        for (auto &producer : op->ProducerOps()) {
+        for (auto &producer : opProducers[op]) {
             if (producer->GetOpcode() == Opcode::OP_VIEW) {
-                for (auto viewProducer : producer->ProducerOps()) {
+                for (auto viewProducer : opProducers[producer]) {
                     AddDependency(viewProducer, op, false);
                 }
                 continue;
             }
             AddDependency(producer, op, false);
         }
-        for (auto &consumer : op->ConsumerOps()) {
+        for (auto &consumer : opConsumers[op]) {
             if (consumer->GetOpcode() == Opcode::OP_VIEW) {
-                for (auto viewConsumer : consumer->ConsumerOps()) {
+                for (auto viewConsumer : opConsumers[consumer]) {
                     AddDependency(op, viewConsumer, false);
                 }
             }
