dd2b5ee70b77dfc26a1db3cc0c516c6aa714d08a
yanghaoran29 <yanghaoran29@huawei.com>
2026-01-12 16:11:40 +0800

    Remove IRBuilder's dependency on module
    Co-authored-by: yanghaoran29<yanghaoran29@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !317 merge 0112 into master
    
    fix(IR): Remove IRBuilder's dependency on module
    
    Created-by: yanghaoran29
    Commit-by: yanghaoran29
    Merged-by: cann-robot
    Description: ## 描述
    删除IRBuilder的module依赖
    
    ## 关联的Issue
    无
    
    ## 测试
    保留现有测试样例
    
    ## 文档更新
    更新文档中使用了IRBuilder构造函数的地方
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [x] 文档更新
    - [x] 其他，请描述：修改代码实现方式，减少不必要的依赖
    
    
    
    
    See merge request: cann/pypto!317

--- framework/tests/ut/interface/src/ir/test_ir_builder.cpp
+++ framework/tests/ut/interface/src/ir/test_ir_builder.cpp
@@ -34,7 +34,7 @@ namespace pto{
 TEST(IRTEST, TestBuilder) {
     // ===== Module =====
     auto module = std::make_shared<ProgramModule>("main");
-    IRBuilder builder(module);
+    IRBuilder builder;
     IRBuilderContext ctx;
 
     // ===== Signature =====
@@ -51,7 +51,9 @@ TEST(IRTEST, TestBuilder) {
     sig.arguments = { inputTensor, scale1, result };
 
     // ===== Function =====
-    auto func = builder.CreateFunction("test_value", FunctionKind::ControlFlow, sig, /*setAsEntry=*/true);
+    auto func = builder.CreateFunction("test_value", FunctionKind::ControlFlow, sig);
+    module->AddFunction(func);
+    module->SetProgramEntry(func);
 
     // enter func scope + create an initial block as insertion point
     builder.EnterFunctionBody(ctx, func);
@@ -95,7 +97,7 @@ TEST(IRTEST, TestBuilder) {
 TEST(IRTEST, TestControlFlow) {
     // ===== Module =====
     auto module = std::make_shared<ProgramModule>("main");
-    IRBuilder builder(module);
+    IRBuilder builder;
     IRBuilderContext ctx;
 
     // ===== Signature =====
@@ -121,9 +123,10 @@ TEST(IRTEST, TestControlFlow) {
     sig.results.push_back(std::make_shared<ScalarValue>(DataType::INT32));
 
     // ===== Function =====
-    auto func = builder.CreateFunction("test_control", FunctionKind::ControlFlow, sig, /*setAsEntry=*/false);
+    auto func = builder.CreateFunction("test_control", FunctionKind::ControlFlow, sig);
+    module->AddFunction(func);
     module->SetProgramEntry(func);
-        // 进入函数体作用域
+        // 进入函数体作用域 
     builder.EnterFunctionBody(ctx, func);
 
     // for i = 0 to batch step 1
