8e40c0a40ecb8ee94493b36fc6268dea9929aab1
zhaosenqiu <zhaosenqiu@huawei.com>
2026-01-12 16:36:54 +0800

    冗余代码删除，L0C2L1已经被允许拥有copyAttr属性

--- framework/src/interface/operation/operation.h
+++ framework/src/interface/operation/operation.h
@@ -391,9 +391,6 @@ public:
                 ASSERT(std::dynamic_pointer_cast<ConvertOpAttribute>(opAttribute_) != nullptr);
                 break;
             }
-            case Opcode::OP_L0C_TO_L1:
-                ASSERT(std::dynamic_pointer_cast<CopyOpAttribute>(opAttribute_) != nullptr);
-                break;
             default: ASSERT(opAttribute_ == nullptr);
         }
     }
