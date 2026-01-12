5365bc09d51c1c4ebf4b7c45188b77294d75ee48
jiaweizhuang <zhuangjiawei@hisilicon.com>
2026-01-12 16:02:15 +0000

    test_control_flow_closure

--- python/tests/ut/ir/test_builder_helper.py
+++ python/tests/ut/ir/test_builder_helper.py
@@ -13,6 +13,11 @@ from pypto.blockgraph.builder_helper import BlockBuilderHelper
 
 
 def test_control_flow():
+    """
+    1:1 ported from `test_control_flow` in `test_ir_binding.py`
+    just hide `builder` and `ctx` behind ``BlockBuilderHelper
+    """
+
     # ===== Module =====
     module = ir.module("main")
     builder = ir.IrBuilder()
@@ -104,6 +109,88 @@ def test_control_flow():
 
         block.create_return([constant0])
 
+    # TODO: assert IR module structure
+    print(f"Module: {module}\nEntry: {module.entry}\nFunctions: {module.functions}")
+
+
+def test_control_flow_closure():
+    """
+    Rearrange `test_control_flow_rearrange` to a more functional style.
+    Further ast transforms will work on `create_function` level, not module level.
+    """
+    module = ir.module("main")
+    builder = ir.IrBuilder()
+    ctx = ir.IrBuilderContext()
+    block = BlockBuilderHelper(builder, ctx)
+
+    batch = ir.Scalar(ir.DataType.int32, None, "batch")
+    constant128 = ir.Scalar(ir.DataType.int64, 128, "const_128")
+    tensor_shape = [batch, constant128]
+    tile_shape = [128, 128]
+
+    # NOTE: shape parameter `tensor_shape`, `tile_shape`, `batch` are passed via closure
+    def create_function(
+        block=block,
+        name="test_control",
+        function_kind=ir.FunctionKind.ControlFlow
+    ):
+        input_x = ir.Tensor(tensor_shape, ir.DataType.float,
+                            "inputX", ir.Format.ND)
+        input_y = ir.Tensor(tensor_shape, ir.DataType.float,
+                            "inputY", ir.Format.ND)
+        scale1 = ir.Scalar(ir.DataType.float, None, "scale1")
+        scale2 = ir.Scalar(ir.DataType.float, None, "scale2")
+        result_x = ir.Tensor(tensor_shape, ir.DataType.float,
+                            "outputX", ir.Format.ND)
+        result_y = ir.Tensor(tensor_shape, ir.DataType.float,
+                            "outputY", ir.Format.ND)
+
+        sig = ir.FunctionSignature()
+        sig.arguments = [input_x, input_y, scale1, scale2, result_x, result_y]
+        sig.returns = [ir.Scalar(ir.DataType.int32, None)]
+
+        func = block.create_function(name, function_kind, sig)
+        with block.function_scope(func):
+
+            # for i = 0 to batch step 1
+            i = block.Scalar(ir.DataType.int32, "i")
+            constant0 = block.Const(0, "const_0")
+            constant1 = block.Const(1, "const_1")
+            fs = block.ForNode(i, constant0, batch, constant1, unroll=4)
+            with block.for_scope(fs):
+                res_loop_x = block.Tile(tile_shape, ir.DataType.float, "outputX")
+                add_op_x = block.adds(res_loop_x, scale1, out=res_loop_x)
+
+                res_loop_y = block.Tile(tile_shape, ir.DataType.float, "outputY")
+                add_op_y = block.adds(res_loop_y, scale2, out=res_loop_y)
+
+                ifs = block.IfNode(i)
+                with block.if_then_scope(ifs):
+                    res_if_x = block.Tile(tile_shape, ir.DataType.float, "outputX")
+                    mul_op_x = block.muls(res_loop_x, scale1, out=res_if_x)
+
+                with block.if_else_scope(ifs):
+                    res_if_y = block.Tile(tile_shape, ir.DataType.float, "outputY")
+                    mul_op_y = block.muls(res_loop_y, scale2, out=res_if_y)
+
+                block.exit_if(ifs)
+
+            block.create_return([constant0])
+
+        return func
+
+    func = create_function(
+        block,
+        name="test_control",
+        function_kind=ir.FunctionKind.ControlFlow
+    )
+    module.add_function(func)
+    module.entry = func
+
+    # TODO: assert IR module structure
+    print(f"Module: {module}\nEntry: {module.entry}\nFunctions: {module.functions}")
+
 
 if __name__ == "__main__":
     test_control_flow()
+    test_control_flow_closure()
