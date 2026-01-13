b5b29b5a6f0f249881511e2eef783b6b32dfa5a2
mengziyu <mengziyu1@huawei.com>
2026-01-13 17:56:42 +0800

    feat(distributed): Optimize the code of the testing framework

--- framework/tests/cmake/scripts/helper/test_case_launcher.py
+++ framework/tests/cmake/scripts/helper/test_case_launcher.py
@@ -45,17 +45,18 @@ class TestCaseLauncher:
         self.executable_path = config.executable_path
 
     def tear_up(self):
-        if os.path.exists(self.log_path):
-            os.system(f"rm -rf {self.log_path}")
-        os.mkdir(self.log_path)
-        if os.path.exists(self.plog_cache_path):
-            os.system(f"rm -rf {self.plog_cache_path}")
-        os.mkdir(self.plog_cache_path)
-        if os.path.exists(self.report_file):
-            os.remove(self.report_file)
+        # if os.path.exists(self.log_path):
+        #     os.system(f"rm -rf {self.log_path}")
+        # os.mkdir(self.log_path)
+        # if os.path.exists(self.plog_cache_path):
+        #     os.system(f"rm -rf {self.plog_cache_path}")
+        # os.mkdir(self.plog_cache_path)
+        # if os.path.exists(self.report_file):
+        #     os.remove(self.report_file)
 
         os.environ["TILE_FWK_DEVICE_ID"] = f"{self.device}"
         os.environ["ASCEND_PROCESS_LOG_PATH"] = self.plog_cache_path
+        os.environ["JSON_PATH"] = self.json_path
 
         if not self.python:
             golden_dir = str(self.golden_script.parent)
@@ -70,6 +71,7 @@ class TestCaseLauncher:
     def tear_down(self):
         del os.environ["TILE_FWK_DEVICE_ID"]
         del os.environ["ASCEND_PROCESS_LOG_PATH"]
+        del os.environ["JSON_PATH"]
 
     def compile_if_need(self):
         clean_str = "-c" if self.clean else ""
@@ -79,7 +81,7 @@ class TestCaseLauncher:
         elif not self.distributed_op:
             cmd += " -f=cpp -s='TestAdd/AddOperationTest.TestAdd/*' --disable_auto_execute"
         else:
-            cmd += " -f=cpp --stest_distributed='TestAllgather/DistributedTest.TestAllgather/*' --disable_auto_execute"
+            cmd += " -f=cpp --stest_distributed='TestDistributedOps/DistributedTest.TestOps/*' --disable_auto_execute"
         TestCaseShellActuator.run(cmd)
 
         if self.python:
@@ -177,7 +179,7 @@ class TestCaseLauncher:
             if not self.distributed_op:
                 test_case = f"Test{case_op}/{case_op}OperationTest.Test{case_op}/{index}"
             else:
-                test_case = f"Test{case_op}/DistributedTest.Test{case_op}/{index}"
+                test_case = f"TestDistributedOps/DistributedTest.TestOps/{index}"
             golden_path = f"{self.work_path}/build/output/bin/golden/{test_case}"
             if os.path.exists(golden_path + "/golden_desc.json"):
                 os.remove(golden_path + "/golden_desc.json")
