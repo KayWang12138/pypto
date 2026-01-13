2249c54ce52eb867d3e3feecdc6583855b83c4d8
arthur_morgan_hw <lipeijie@huawei.com>
2026-01-13 11:24:14 +0800

    feat(machine): Remove BUILD_WITH_CANN_SUB

--- framework/tests/st/distributed/ops/src/distributed_op_test_common.cpp
+++ framework/tests/st/distributed/ops/src/distributed_op_test_common.cpp
@@ -14,11 +14,8 @@
  */
 
 #include "distributed_op_test_common.h"
-#ifdef BUILD_WITH_CANN_SUB
 #include<hccl/hcom.h>
-#else
 #include "hcom.h"
-#endif
 
 extern "C" HcclResult HcclAllocComResourceByTiling(HcclComm comm, void *stream, void *mc2Tiling, void **commContext);
 
