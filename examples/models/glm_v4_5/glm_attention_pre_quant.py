6e48bdd8822faf3c797e23309f66ab2379ed872c
junjieHuang <huangjunjie38@h-partners.com>
2026-01-13 19:09:28 +0800

    refactor(config): Reimplement the interface of set_host_options, such as only_tensor_graph, only_host_compiled, and only_codegen.

--- examples/models/glm_v4_5/glm_attention_pre_quant.py
+++ examples/models/glm_v4_5/glm_attention_pre_quant.py
@@ -232,8 +232,7 @@ def rope_data(x1, x2, cos, sin, tile_shape):
     runtime_options={"stitch_function_num_initial": 128, 
     "stitch_function_outcast_memory": 1024,
     "stitch_function_inner_memory": 1024,
-    "stitch_cfgcache_size": 3000000},
-    host_options={"only_codegen": True},
+    "stitch_cfgcache_size": 3000000}
 )
 def quant_attention_pre_kernel(x, residual_input, x_gamma, x_bias,
                                x_scale, x_offset, weight, quant_bias,
