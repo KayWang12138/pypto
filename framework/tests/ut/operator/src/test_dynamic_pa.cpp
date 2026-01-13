541cb1b450ce3ec2f891001ddcd94e15757bba02
y00938749 <yefan29@huawei.com>
2026-01-13 14:56:14 +0800

    feat(ut): Modify ut executed sequence

--- framework/tests/ut/operator/src/test_dynamic_pa.cpp
+++ framework/tests/ut/operator/src/test_dynamic_pa.cpp
@@ -16,6 +16,7 @@
 #include <gtest/gtest.h>
 #include "operator/models/deepseek/page_attention.h"
 #include "interface/configs/config_manager.h"
+#include "interface/utils/test_cost_macro.h"
 
 using namespace npu::tile_fwk;
 
@@ -169,7 +170,7 @@ TEST_F(DynamicPATest, dynamic_pa_low_lantency_pass_unroll) {
         tileConfig, maxUnrollTimes);
 }
 
-TEST_F(DynamicPATest, dynamic_pa_low_lantency_manual_unroll) {
+TEST_F_WITH_COST(DynamicPATest, dynamic_pa_low_lantency_manual_unroll, 135) {
     config::SetHostOption(ONLY_CODEGEN, true);
     config::SetPassDefaultConfig(KEY_PRINT_GRAPH, true);
     std::vector<uint8_t> devProgBinary;
