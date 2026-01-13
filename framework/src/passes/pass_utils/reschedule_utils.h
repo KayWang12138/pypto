83cd681aa670b4bb643639822fcbdb3e271eb050
zhaoshenjie <zhaoshenjie@huawei.com>
2026-01-14 00:50:26 +0800

    Pass cast sets by reference

--- framework/src/passes/pass_utils/reschedule_utils.h
+++ framework/src/passes/pass_utils/reschedule_utils.h
@@ -54,8 +54,9 @@ public:
     static unsigned long ComputeOperationHash(const Operation *op);
 
     static void UpdateTensorConsProd(Function *funcPtr);
-    static void ClearInputConsProd(Operation &op, Function *funcPtr, std::unordered_set<LogicalTensorPtr> incastSet);
-    static void ClearOutputConsProd(Operation &op, Function *funcPtr, std::unordered_set<LogicalTensorPtr> outcastSet);
+    static void ClearInputConsProd(Operation &op, Function *funcPtr, const std::unordered_set<LogicalTensorPtr> &incastSet);
+    static void ClearOutputConsProd(Operation &op, Function *funcPtr, const std::unordered_set<LogicalTensorPtr> &outcastSet);
+    static void FindOtherGraphOp(Operation *op, Function *funcPtr, std::unordered_set<Operation*> &otherGraphOp);
     static void PrintColorNode(Function &func);
 };
 }  // namespace npu::tile_fwk
