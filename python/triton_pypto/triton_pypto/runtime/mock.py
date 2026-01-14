1a2421f4165bc61ffc5089579364feb16f14a8fe
Vlasov Maxim <vlasov.maxim@huawei-partners.com>
2026-01-14 13:45:17 +0300

    Add Triton-to-PyPTO prototype
    Signed-off-by: Vlasov Maxim <vlasov.maxim@huawei-partners.com>
    Signed-off-by: Kuznetsov Artyom <kuznetsov.artyom@h-partners.com>
    Co-authored-by: vla5924 <vlasov.maxim@huawei-partners.com>
    Co-authored-by: artyomka <kuznetsov.artyom@h-partners.com>

---
+++ python/triton_pypto/triton_pypto/runtime/mock.py
@@ -0,0 +1,37 @@
+from ..language import operations as l
+
+
+def mock():
+    import triton.language as tl
+    mocks = (
+        l.abs,
+        l.arange,
+        l.cdiv,
+        l.clamp,
+        l.dot,
+        l.exp,
+        l.expand_dims,
+        l.load,
+        l.log,
+        l.make_block_ptr,
+        l.max,
+        l.maximum,
+        l.minimum,
+        l.num_programs,
+        l.permute,
+        l.program_id,
+        l.range,
+        l.ravel,
+        l.reshape,
+        l.rsqrt,
+        l.sigmoid,
+        l.sqrt,
+        l.static_range,
+        l.store,
+        l.sum,
+        l.trans,
+        l.where,
+        l.zeros,
+    )
+    for m in mocks:
+        setattr(tl, m.__name__, m)
