170d86bc096b3580e75381692b33621fbd4d93be
wuchenhao123 <wuchenhao6@huawei.com>
2026-01-14 10:28:01 +0800

    临时规避hcclcontext拷贝

--- framework/src/machine/runtime/device_launcher.h
+++ framework/src/machine/runtime/device_launcher.h
@@ -226,12 +226,20 @@ public:
 
      static void DeviceInitDistributedContextToHost(const std::vector<std::string> &groupNames,
         const std::vector<uint8_t> &devProgData) {
+        auto *devProg = reinterpret_cast<DevAscendProgram *>(const_cast<uint8_t*>(devProgData.data()));
+        if (devProg->hcclContext[0] != 0) {
+            return;
+        }
         auto hcclContext = DistributedContext::GetHcclContextToHost(groupNames);
         PrepareHcclContext(hcclContext, devProgData);
     }
 
     static void DeviceInitDistributedContext(const std::vector<std::string> &groupNames,
         const std::vector<uint8_t> &devProgData) {
+        auto *devProg = reinterpret_cast<DevAscendProgram *>(const_cast<uint8_t*>(devProgData.data()));
+        if (devProg->hcclContext[0] != 0) {
+            return;
+        }
  	    auto hcclContext = DistributedContext::GetHcclContext(groupNames);
         PrepareHcclContext(hcclContext, devProgData);
     }
