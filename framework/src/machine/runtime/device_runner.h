d044bb3c341336887336f9aafb6cc7e1bd540770
wan_jianfen <wanjianfen@hisilicon.com>
2026-01-14 08:09:28 +0000

    fix(machine): Add aicpu perf

--- framework/src/machine/runtime/device_runner.h
+++ framework/src/machine/runtime/device_runner.h
@@ -100,6 +100,8 @@ private:
     void ReportHostProfInfo(uint64_t startTime, uint32_t blockDim, uint16_t taskType, bool isCore = false);
     int DynamicKernelLaunch(rtStream_t aicpuStream, rtStream_t aicoreStream, DeviceKernelArgs *kernelArgs, int blockdim);
     int DynamicSeparateLaunch(rtStream_t aicpuStream, rtStream_t ctrlStream, rtStream_t aicoreStream, DeviceKernelArgs *kernelArgs, int blockdim);
+    int ConstrutDeviceArgs(DeviceArgs &args, const std::vector<int64_t> &regs, const std::vector<int64_t> &regsPmu);
+    void PrepareLaunchArgs(DeviceArgs &localArgs, DeviceKernelArgs *kernelArgs, int64_t taskId, int blockdim, int launchAicpuNum);
 private:
     int devId_;
     int aicpuNum_{5};
