86a49a67e818ac5dfb4413d24b22a87287a96925
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-01-14 10:16:59 +0800

    feat(pass): bugfix

--- framework/src/passes/block_graph_pass/insert_sync.cpp
+++ framework/src/passes/block_graph_pass/insert_sync.cpp
@@ -722,11 +722,12 @@ Status PipeSync::InjectWaitFlag(Function &function, size_t idx, std::vector<Inde
         if (setPipeReal.core != currPipeReal.core) {
             crossCoreFreeEventId_[{setWaitCoreType.second, setWaitCoreType.first}].push_back(eventId);
         }
-        // 记录set op 和 wait op的对应关系 及 wait op 和 waitflag的对应关系
+        // 记录set op 和 wait op的对应关系
         setWaitOpMap.emplace(oriOpList_[ele], oriOpList_[idx]);
         waitSetOpMap.emplace(oriOpList_[idx], oriOpList_[ele]);
-        waitOpMap.emplace(&syncOp, oriOpList_[idx]);
-        opWaitMap.emplace(oriOpList_[idx], &syncOp);
+        // 记录 set op 和 waitflag的对应关系
+        waitOpMap.emplace(&syncOp, oriOpList_[ele]);
+        opWaitMap.emplace(oriOpList_[ele], &syncOp);
     }
     return SUCCESS;
 }
@@ -754,9 +755,9 @@ Status PipeSync::InjectSetFlag(Function &function, size_t idx, std::vector<Index
                 syncOp.GetOpMagic(), syncOp.GetOpcodeStr().c_str(), GetPipeTypeDict().Find(syncOp.syncQueue_.pipeId_).c_str(),
                 GetPipeTypeDict().Find(syncOp.syncQueue_.trigPipeId_).c_str(), syncOp.syncQueue_.eventId_);
             setWaitPairMap_[{idx, ele}] = eventId;
-            // 记录set op 和 setflag的对应关系
-            setOpMap.emplace(&syncOp, oriOpList_[idx]);
-            opSetMap.emplace(oriOpList_[idx], &syncOp);
+            // 记录wait op 和 setflag的对应关系
+            setOpMap.emplace(&syncOp, oriOpList_[ele]);
+            opSetMap.emplace(oriOpList_[ele], &syncOp);
             continue;
         }
         syncOp.SetAsDeleted();
