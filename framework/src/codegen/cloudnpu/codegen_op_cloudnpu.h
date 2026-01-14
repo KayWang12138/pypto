487f52e0dbeb9e9320ea0d2cd062e3ce04bce513
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-14 20:24:38 +0800

    fix(codegen): Support for forcing axis merging under TileTensor mode

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -150,6 +150,8 @@ public:
     void UpdateSaturateStatus(FloatSaturateStatus &fs);
 
 private:
+    std::string QueryTileTensorByMagic(int magic);
+
     std::string GenTemplateParamsForPutAndGet() const;
     std::string GenTemplateParamsForSignal() const;
     std::string GenTemplateParamsForMoeDistributedCombineSend() const;
