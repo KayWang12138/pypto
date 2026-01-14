3b83a6ca2435d23e70f3b123c0e363dd46994cea
DCGDDD <yangxu14@huawei.com>
2026-01-14 12:40:32 +0800

    feat(build): The coverage generation process supports parallel processing

--- framework/tests/cmake/scripts/python/gen_coverage.py
+++ framework/tests/cmake/scripts/python/gen_coverage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
 # CANN Open Software License Agreement Version 2.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -11,117 +11,158 @@
 """生成覆盖率
 """
 import argparse
-import dataclasses
 import logging
+import math
+import os
+import re
 import subprocess
+from multiprocessing import cpu_count
 from pathlib import Path
-from typing import Optional, List
+from typing import Optional
+from datetime import datetime, timezone
 
 
 class GenCoverage:
-    @dataclasses.dataclass
-    class Param:
-        source_dir: Optional[Path] = None
-        data_dir: Optional[Path] = None
-        info_file: Optional[Path] = None
-        info_file_filtered: Optional[Path] = None
-        html_report_dir: Optional[Path] = None
-        filter_str: str = ''
 
-        def init_filter_str(self, fs: Optional[List[List[Path]]]):
-            if not fs:
-                return
-            for fl in fs:
-                f = fl[0]
-                if not f.exists():
-                    continue
-                if f.is_dir():
-                    self.filter_str += f" {f}/*"
+    class FilterPathAction(argparse.Action):
+        """自定义 Action: 解析 filter 参数时校验路径并格式化"""
+        def __call__(self, parser, namespace, values, option_string=None):
+            # 获取当前已收集的列表(初始为 None)
+            cur_values = getattr(namespace, self.dest, None) or []
+            path = Path(values)
+            # 仅保留存在的路径，并格式化(目录加 /*)
+            if path.exists():
+                if path.is_dir():
+                    cur_values.append(f" {path}/*")
                 else:
-                    self.filter_str += f" {f}"
+                    cur_values.append(f" {path}")
+            # 更新命名空间的值
+            setattr(namespace, self.dest, cur_values)
+
+    def __init__(self, args):
+        self.src_root: Optional[Path] = Path(args.source[0]).resolve() if args.source else None
+        self.data_dir: Path = Path(args.data[0]).resolve()
+        self.info_file = Path(args.info[0]).resolve() if args.info else Path(self.data_dir, 'cov_result/coverage.info')
+        self.filter_str: str = " ".join(args.filter)
+        self.html_report = Path(args.html[0]).resolve() if args.html else Path(self.info_file.parent, "html_report")
+        self.job_num: int = self.get_job_num(args=args)
+        # 合法性检查
+        self.lcov_version: str = ""
+        self.lcov_support_job_num: bool = False
+        self.chk_env()
+        if not self.data_dir.exists():
+            raise ValueError(f"The dir({self.data_dir}) required to find the .da files not exist.")
+        self.info_file.parent.mkdir(parents=True, exist_ok=True)
+        self.html_report.mkdir(parents=True, exist_ok=True)
+
+    def __str__(self) -> str:
+        desc = f"\nGenerateCoverage"
+        desc += f"\n    SrcRoot    : {self.src_root}"
+        desc += f"\n    DataDir    : {self.data_dir}"
+        desc += f"\n    InfoFile   : {self.info_file}"
+        desc += f"\n    Filter     : {self.filter_str}"
+        desc += f"\n    HtmlReport : {self.html_report}"
+        desc += f"\n    JobNum     : {self.job_num}"
+        desc += f"\n    lcov       : {self.lcov_version} ({self.lcov_support_job_num})"
+        desc += f"\n"
+        return desc
+
+    @classmethod
+    def reg_args(cls, parser):
+        parser.add_argument("-s", "--source",
+                            required=False, nargs=1, type=Path,
+                            help="Specify the source base directory.")
+        parser.add_argument("-d", "--data",
+                            required=True, nargs=1, type=Path,
+                            help="Specify the *.da's base directory.")
+        parser.add_argument("-i", "--info_file", dest="info",
+                            required=False, nargs=1, type=Path,
+                            help="Specify coverage info file path.")
+        parser.add_argument("-f", "--filter",
+                            required=False, action=cls.FilterPathAction, type=str,
+                            help="Specify filter file/dir in coverage info.")
+        parser.add_argument("--html_report", dest="html",
+                            required=False, nargs=1, type=Path,
+                            help="Specify coverage html report dir.")
+        parser.add_argument("-j", "--job_num",
+                            nargs="?", type=int, default=None,
+                            help="Specify parallel job num.")
+
+    @classmethod
+    def get_job_num(cls, args):
+        if args.job_num:
+            job_num = args.job_num
+        else:
+            if os.environ.get("CMAKE_BUILD_PARALLEL_LEVEL", 0):
+                job_num = int(os.environ.get("CMAKE_BUILD_PARALLEL_LEVEL"), 0)
+            elif os.environ.get("PYPTO_UTEST_PARALLEL_NUM", 0):
+                job_num = int(os.environ.get("PYPTO_UTEST_PARALLEL_NUM", 0))
+            else:
+                job_num = int(math.ceil(float(cpu_count()) * 0.8))    # use 0.8 cpu
+        job_num = min(max(int(job_num), 1), cpu_count(), 48)
+        return job_num
 
     @classmethod
     def main(cls):
         # 参数注册
         parser = argparse.ArgumentParser(description="Generate Coverage", epilog="Best Regards!")
-        parser.add_argument("-s", "--source_base_dir", required=False,
-                            nargs=1, type=Path, help="Explicitly specify the source base directory.")
-        parser.add_argument("-c", "--coverage_data_dir", required=True,
-                            nargs=1, type=Path, help="Explicitly specify the *.da's base directory.")
-        parser.add_argument("-i", "--info_file", required=False,
-                            nargs=1, type=Path, help="Explicitly specify coverage info file path.")
-        # 考虑最低支持 Python 版本为 3.7, 此处用 append 而非 extend
-        parser.add_argument("-f", "--filter", required=False, action='append',
-                            nargs='*', type=Path, help="Explicitly specify filter file/dir in coverage info.")
-        parser.add_argument("--html_report", required=False,
-                            nargs=1, type=Path, help="Explicitly specify coverage html report dir.")
-        # 参数解析, 默认值处理
-        p = cls.Param()
-        args = parser.parse_args()
-        p.source_dir = Path(args.source_base_dir[0]).absolute() if args.source_base_dir else None
-        p.data_dir = Path(args.coverage_data_dir[0]).absolute()
-        p.info_file = args.info_file[0] if args.info_file else Path(p.data_dir, 'cov_result/coverage.info')
-        p.info_file = Path(p.info_file).absolute()
-        p.info_file_filtered = Path(p.info_file.parent, f"{p.info_file.stem}_filtered{p.info_file.suffix}")
-        p.html_report_dir = args.html_report[0] if args.html_report else Path(p.info_file.parent, "html_report")
-        p.html_report_dir = Path(p.html_report_dir).absolute()
-        p.init_filter_str(fs=args.filter)
-        logging.debug("filter_str=%s", p.filter_str)
-        # 参数检查
-        if not p.data_dir.exists():
-            logging.error("The dir(%s) required to find the .da files not exist.", p.data_dir)
-            return
-        if not p.info_file.exists():
-            p.info_file.parent.mkdir(parents=True, exist_ok=True)
-        if not p.html_report_dir.exists():
-            p.html_report_dir.mkdir(parents=True, exist_ok=True)
-        # 环境检查
-        if not cls._chk_env():
-            return
-        # 生成覆盖率数据
-        cls._gen_cov(param=p)
+        cls.reg_args(parser=parser)
+        # 参数处理
+        ctrl = GenCoverage(args=parser.parse_args())
+        logging.info("%s", ctrl)
+        # 流程处理
+        ctrl.process()
 
-    @classmethod
-    def _chk_env(cls):
+    def chk_env(self):
         try:
             ret = subprocess.run('lcov --version'.split(), capture_output=True, check=True, encoding='utf-8')
             ret.check_returncode()
-        except FileNotFoundError:
-            logging.error("lcov is required to generate coverage data, please install.")
-            return False
+            # 提取版本号
+            version_output = ret.stdout.strip()
+            version_match = re.search(r'version (\d+\.\d+)', version_output)
+            if not version_match:
+                raise RuntimeError(f"Can't get version from {version_output}")
+            self.lcov_version = version_match.group(1)
+            self.lcov_support_job_num = False
+            # 验证版本是否 ≥2.1(支持 -j 参数)
+            version_parts = list(map(int, self.lcov_version.split('.')))
+            if version_parts[0] > 2 or (version_parts[0] == 2 and version_parts[1] >= 1):
+                self.lcov_support_job_num = True
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f"lcov is required to generate coverage data, please install.") from e
         try:
             ret = subprocess.run('genhtml --version'.split(), capture_output=True, check=True, encoding='utf-8')
             ret.check_returncode()
-        except FileNotFoundError:
-            logging.error("genhtml is required to generate coverage html report, please install.")
-            return False
-        return True
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f"genhtml is required to generate coverage html report, please install.") from e
 
-    @classmethod
-    def _gen_cov(cls, param: Param):
+    def process(self):
         """使用 lcov 生成覆盖率
         """
-        # 生成覆盖率
-        cmd = f"lcov -c -d {param.data_dir} -o {param.info_file}"
-        logging.info("[BGN] generated origin coverage file, cmd=%s", cmd)
+        # 生成覆盖率原始统计文件
+        cmd = f"lcov -c -d {self.data_dir} -o {self.info_file}"
+        cmd += f" -j {self.job_num}" if self.lcov_support_job_num else ""
         ret = subprocess.run(cmd.split(), capture_output=False, check=True, encoding='utf-8')
         ret.check_returncode()
-        logging.info("[END] generated origin coverage file %s", param.info_file)
+        logging.info("Generated origin coverage file %s, cmd: %s", self.info_file, cmd)
         # 滤掉某些文件/路径的覆盖率信息
-        cmd = f"lcov --remove {param.info_file} {param.filter_str} -o {param.info_file_filtered}"
-        logging.info("[BGN] generated filtered coverage file, cmd=%s", cmd)
+        info_file_filtered = Path(self.info_file.parent, f"{self.info_file.stem}_filtered{self.info_file.suffix}")
+        cmd = f"lcov --remove {self.info_file} {self.filter_str} -o {info_file_filtered}"
         ret = subprocess.run(cmd.split(), capture_output=False, check=True, encoding='utf-8')
         ret.check_returncode()
-        logging.info("[END] generated filtered coverage file %s", param.info_file_filtered)
+        logging.info("Generated filtered coverage file %s, cmd: %s", info_file_filtered, cmd)
         # 生成 html 报告
-        sub_cmd_prefix = f"-p {param.source_dir}" if param.source_dir else ""
-        cmd = f'genhtml {param.info_file_filtered} {sub_cmd_prefix} -o {param.html_report_dir}'
-        logging.info("[BGN] generated filtered coverage html report, cmd=%s", cmd)
+        prefix = f"-p {self.src_root}" if self.src_root else ""
+        cmd = f'genhtml {info_file_filtered} {prefix} -o {self.html_report}'
+        cmd += f" -j {self.job_num}" if self.lcov_support_job_num else ""
         ret = subprocess.run(cmd.split(), capture_output=False, check=True, encoding='utf-8')
         ret.check_returncode()
-        logging.info("[END] generated filtered coverage html report. %s", param.html_report_dir)
+        logging.info("Generated filtered coverage html report in %s, cmd: %s", self.html_report, cmd)
 
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s: %(message)s', level=logging.INFO)
+    ts = datetime.now(tz=timezone.utc)
     GenCoverage.main()
+    duration = int((datetime.now(tz=timezone.utc) - ts).seconds)
+    logging.info("Generate Coverage use %s secs.", duration)
