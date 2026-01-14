98fcad4fba46e328261de018282cd919b06cf10a
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-14 20:24:38 +0800

    fix(codegen): Support for forcing axis merging under TileTensor mode

--- framework/src/codegen/symbol_mgr/codegen_symbol.h
+++ framework/src/codegen/symbol_mgr/codegen_symbol.h
@@ -47,6 +47,7 @@ inline std::string GetLayoutType(BufferType bufType, int dim, bool isStatic) {
 // UBTileTensorFP32Dim2 ubTile_0((__ubuf__ float*)UB_S0_E16384, DimLayout2(Shape<int, int>(sym_18_dim_0, sym_18_dim_1),
 // Stride<int, int>(64, 1)));
 struct TileTensor {
+    bool isStatic;
     int magic; // tensor magic numbuer
     int dim;
     DataType dtype;
@@ -57,8 +58,9 @@ struct TileTensor {
     std::vector<std::string> shape; // valid shape
     std::vector<std::string> stride;
     std::vector<int64_t> rawShape;
+    std::vector<int64_t> originShape;          // for static functionType
+    std::vector<SymbolicScalar> dynValidShape; // for dynamic functionType
     std::vector<int64_t> localBufOffset;
-    bool isStatic;
 
     bool operator==(const TileTensor &other) const {
         return dim == other.dim && bufVar == other.bufVar && shape == other.shape && dtype == other.dtype &&
@@ -81,7 +83,8 @@ struct TileTensor {
             // cast local buffer pointer to uint64_t to adapt TileTensor mode
             oss << "uint64_t)";
             int64_t linearOffset = CalcLinearOffset(rawShape, localBufOffset);
-            if (linearOffset != 0) { // append linear offset, e.g. UBTileTensorFP32Dim2_1 ubTensor_1((uint64_t)((float *)UB_S0_E4096 + 32))
+            if (linearOffset != 0) { // append linear offset, e.g. UBTileTensorFP32Dim2_1 ubTensor_1((uint64_t)((float
+                                     // *)UB_S0_E4096 + 32))
                 oss << "((" << DataType2CCEStr(dtype) << " *)" << bufVar << " + " << linearOffset << ")";
             } else {
                 oss << bufVar;
@@ -224,9 +227,9 @@ public:
 
     std::string AddTileTensorUsing(const TileTensorUsing &tileTensorUsing);
     void AddTileTensor(const TileTensor &tileTensor);
-    std::string QueryTileTensorByMagic(int magic);
-    // To be compatible with GM Tensor in Static Function Type like same ddr magic number with different parmaIdx & 'GMStackBase'
-    // e.g. ((__gm__ GMTensorInfo*)param + 1), ((__gm__ GMTensorInfo*)param + 2)
+    std::vector<TileTensor> QueryTileTensorByMagic(int magic);
+    // To be compatible with GM Tensor in Static Function Type like same ddr magic number with different parmaIdx &
+    // 'GMStackBase' e.g. ((__gm__ GMTensorInfo*)param + 1), ((__gm__ GMTensorInfo*)param + 2)
     std::string QueryTileTensorByBufVarName(const std::string &bufVarName);
 
     std::string GenUsingList();
@@ -246,8 +249,8 @@ private:
     std::unordered_map<int, std::shared_ptr<LogicalTensor>> tensorMap_;
     //<TileTensor, tensorName>
     std::unordered_map<TileTensor, std::string, TileTensorHash> tileTensor_;
-    //<tensor magic, tensorName>
-    std::unordered_map<int, std::string> tileTensorByMagic_;
+    //<tensor magic, TileTensor>
+    std::multimap<int, TileTensor> tileTensorByMagic_;
     //<using type, TileTensorUsing>
     std::unordered_map<std::string, TileTensorUsing> tileTensorUsing_;
 };
