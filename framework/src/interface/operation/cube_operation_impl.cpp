f2e7ae22345e7445d8afc6823ce9b286ef3737ac
wang-zhiyue2 <wangzhiyue2@huawei.com>
2026-01-14 14:31:23 +0800

    fix(operation): add kL0 restrict

--- framework/src/interface/operation/cube_operation_impl.cpp
+++ framework/src/interface/operation/cube_operation_impl.cpp
@@ -669,6 +669,11 @@ void CheckCubeTiling(const Tensor &operand1, const Tensor &operand2) {
             << " bytes, the length must be aligned to 32 bytes" << std::endl;
     });
     OP_CHECK(true, {
+        ASSERT(kL0 % ALIGN_SIZE_16 == 0)
+            << "Current element count of kL0: " << kL0
+            << ", the element count must be aligned to 16" << std::endl;
+    });
+    OP_CHECK(true, {
         ASSERT(nL0 * BytesOf(operand2.GetDataType()) % ALIGN_SIZE_32 == 0)
             << "Current length of nL0: " << (kL0 * BytesOf(operand1.GetDataType()))
             << " bytes, the length must be aligned to 32 bytes" << std::endl;
