27f2533220a57ae2c88f2cf50382b95aea5420fe
junnyleo <lijun580@huawei.com>
2026-01-15 19:36:19 +0800

    feat(Operation): Add TIndexAdd pto instruction

--- framework/src/interface/tileop/TileOpImpl.h
+++ framework/src/interface/tileop/TileOpImpl.h
@@ -41,6 +41,7 @@
 #include "vector/sort.h"
 #include "vector/mte.h"
 #include "vector/gather.h"
+#include "vector/indexadd.h"
 #include "vector/expand.h"
 #include "vector/extract.h"
 #include "vector/pair_binary.h"
