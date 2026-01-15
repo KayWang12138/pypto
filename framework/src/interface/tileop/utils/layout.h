1abbdb37fbf45a1fdb2d69714aed3498dad34d43
l00692226 <luohui46@huawei.com>
2026-01-15 14:34:59 +0800

    feat(operation): A5 support last use

--- framework/src/interface/tileop/utils/layout.h
+++ framework/src/interface/tileop/utils/layout.h
@@ -30,6 +30,9 @@ namespace TileOp {
 template <typename CoordType, typename ShapeType, typename StrideType>
 __aicore__ inline constexpr auto Crd2Idx(const CoordType &coord, const ShapeType &shape, const StrideType &stride);
 
+template <typename... LastUses>
+using LastUse = Std::tuple<LastUses...>;
+
 template <typename... Shapes>
 using Shape = Std::tuple<Shapes...>;
 
@@ -354,6 +357,22 @@ using DynLayout2Dim = TileOp::Layout<Shape2Dim, Stride2Dim, TileOp::TileShape<si
 using DynLayout3Dim = TileOp::Layout<Shape3Dim, Stride3Dim, TileOp::TileShape<size_t, size_t, size_t>>;
 using DynLayout4Dim = TileOp::Layout<Shape4Dim, Stride4Dim, TileOp::TileShape<size_t, size_t, size_t, size_t>>;
 using DynLayout5Dim = TileOp::Layout<Shape5Dim, Stride5Dim, TileOp::TileShape<size_t, size_t, size_t, size_t, size_t>>;
+
+// common lastuse
+template <size_t TileW>
+using LastUse1Dim = TileOp::LastUse<Std::Int<TileW>>;
+
+template <size_t TileH, size_t TileW>
+using LastUse2Dim = TileOp::LastUse<Std::Int<TileH>, Std::Int<TileW>>;
+
+template <size_t TileD, size_t TileH, size_t TileW>
+using LastUse3Dim = TileOp::LastUse<Std::Int<TileD>, Std::Int<TileH>, Std::Int<TileW>>;
+
+template <size_t TileN, size_t TileD, size_t TileH, size_t TileW>
+using LastUse4Dim = TileOp::LastUse<Std::Int<TileN>, Std::Int<TileD>, Std::Int<TileH>, Std::Int<TileW>>;
+
+template <size_t TileS, size_t TileN, size_t TileD, size_t TileH, size_t TileW>
+using LastUse5Dim = TileOp::LastUse<Std::Int<TileS>, Std::Int<TileN>, Std::Int<TileD>, Std::Int<TileH>, Std::Int<TileW>>;
 
 // common Local layouts
 template <size_t TileW>
