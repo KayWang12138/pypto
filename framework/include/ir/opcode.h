3f0ae8da44625c50298995c78bd5b223e8388887
YunjiQin <qinyunji1@huawei.com>
2026-01-15 16:47:45 +0800

    add property of operation

--- framework/include/ir/opcode.h
+++ framework/include/ir/opcode.h
@@ -16,6 +16,7 @@
 #pragma once
 
 #include "utils_defop.h"
+#include "value.h"
 
 namespace pto {
 
@@ -39,5 +40,48 @@ enum class Opcode : int64_t {
 };
 
 std::string GetOpcodeName(Opcode opcode);
+
+enum class CoreType { AIV = 0, AIC = 1, MIX = 2, AICPU = 3, HUB = 4, GMATOMIC = 5, INVALID = 20 };
+
+enum class OpCalcType {
+    ELMWISE,
+    CAST,
+    BROADCAST,
+    OTHER,
+    REDUCE,
+    MATMUL,
+    CONV,
+    MOVE_IN,
+    MOVE_OUT,
+    MOVE_LOCAL,
+    SYNC,        // 同步
+    DISTRIBUTED, // 通信
+    SYS,         // 框架
+    CALC_TYPE_BOTTOM
+};
+
+// hardware pipeline
+enum PipeType {
+    PIPE_S = 0, // Scalar Pipe
+    PIPE_V,     // Vector Pipe, including{VectorOP write UB,  L0C->UB write}
+    PIPE_M,     // Matrix Pipe, including{}
+    PIPE_MTE1,  // L1->L0{A,B}
+    PIPE_MTE2,  // OUT ->{L1, L0{A,B}, UB}
+    PIPE_MTE3,  // UB ->{OUT,L1}
+    PIPE_ALL,
+    PIPE_MTE4 = 7, // MOV_UB_TO_OUT
+    PIPE_MTE5 = 8, // MOV_OUT_TO_UB
+    PIPE_V2 = 9,   // Lower priority vector pipe,
+    PIPE_FIX = 10, // {L0C} ->{L1,UB,L1UB}
+};
+
+struct OpClassInfo {
+    std::vector<MemSpaceKind> inputsMemType_;
+    std::vector<MemSpaceKind> outputsMemType_;
+    PipeType pipeIdStart_;
+    PipeType pipeIdEnd_;
+    CoreType coreType_;
+    OpCalcType calcType_;
+};
 
 } // namespace pto
