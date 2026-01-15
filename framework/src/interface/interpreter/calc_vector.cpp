b73bebe7513501248a2a06b07017cd965b779268
liusiwei5 <liusiwei5@huawei.com>
2026-01-15 14:41:08 +0800

    r

--- framework/src/interface/interpreter/calc_vector.cpp
+++ framework/src/interface/interpreter/calc_vector.cpp
@@ -391,7 +391,7 @@ void ExecuteOpIndexPut(ExecuteOperationContext *ctx) {
         auto indicesTemp = ctx->ioperandDataViewList->at(i);
         indices.push_back(indicesTemp);
     }
-    bool accumulate = ctx->op->GetIntAttribute(OpAttributeKey::accumulate);
+    bool accumulate = ctx->op->GetBoolAttribute(OpAttributeKey::accumulate);
     calc::IndexPut(out, self, indices, values, accumulate);
 }
 REGISTER_CALC_OP(OP_INDEX_PUT, Opcode::OP_INDEX_PUT, ExecuteOpIndexPut);
