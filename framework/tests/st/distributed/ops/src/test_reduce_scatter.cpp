337466bb7f4e3f4f37107c354a23ce3f176b6c06
luozhonglin <luozhonglin1@huawei.com>
2026-01-15 19:49:16 +0800

    feat(distributed): CodeCheck

--- framework/tests/st/distributed/ops/src/test_reduce_scatter.cpp
+++ framework/tests/st/distributed/ops/src/test_reduce_scatter.cpp
@@ -77,19 +77,18 @@ void TestShmemReduceScatterParaWithShmem(OpTestParam &testParam)
 
     Shape shmemDataShape = {1, rowOut, col};
     FUNCTION("ShmemReduceScatter", {in}, {out}) {
-        TileShape::Current().SetVecTile({tileRow, tileCol});
-        Tensor shmemData;
-        Tensor shmemSignal;
         DataType shmemDataType = in.GetDataType();
         if ((shmemDataType == DT_BF16) || (shmemDataType == DT_FP16)) {
             shmemDataType = DT_FP32;
         }
+        Tensor shmemData;
+        Tensor shmemSignal;
         LOOP("CreateShmemTensor", FunctionType::DYNAMIC_LOOP, index, LoopRange(1)) {
             (void)index;
             CreateShmemData(testParam.group, testParam.rankSize, shmemDataType, shmemDataShape, shmemData);
             CreateShmemSignal(testParam.group, shmemData, shmemSignal);
         }
-        Tensor predToken(DT_INT32, {1, 1}, "predToken");
+        TileShape::Current().SetVecTile({tileRow, tileCol});
         ReduceScatter(in, in, testParam.group, shmemData, shmemSignal, DistReduceType::DIST_REDUCE_ADD, out);
     }
     DeviceLauncherConfig config;
