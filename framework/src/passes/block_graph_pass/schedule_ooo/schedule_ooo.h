e3a9c97226ccd7cfedbc41f6557eaa8420290370
gengrunze <gengrunze@huawei.com>
2026-01-15 09:19:41 +0800

    feat(pass): Set Op Last Use Attribute.

--- framework/src/passes/block_graph_pass/schedule_ooo/schedule_ooo.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/schedule_ooo.h
@@ -39,10 +39,12 @@ private:
     Status SortAndLatencyEstimate(std::vector<Operation*> &opList, std::vector<Operation*> &taskOpList,
         int &latency);
     void OoOHealthCheck(OoOScheduler &oooSchedule, Function &function, std::pair<uint64_t, Function*> &program);
+    Status RecordLastUseMemory(Function &function);
     Status A23Schedule(std::vector<Operation*> &opList, Function &function, std::pair<uint64_t, Function*> &program, int &maxWorkeSpaceSize);
     Status A5Schedule(std::vector<Operation*> &opList, Function &function, std::pair<uint64_t, Function*> &program, int &maxWorkeSpaceSize);
     std::vector<Function *> oriFunctions;
     std::map<uint64_t, OoOScheduler> schedulerMap;
+    std::unordered_map<int, std::pair<Operation *, size_t>> lastUseMap_;
     OoOScheduleChecker checker;
 };
 } // namespace npu::tile_fwk
