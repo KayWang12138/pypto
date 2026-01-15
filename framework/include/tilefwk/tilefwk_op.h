993f6cea13e9b97aa25379d7b1c374402cd8c374
陈一 <chenyi138@huawei.com>
2026-01-15 20:31:42 +0800

    feat(distributed): Implement the Combine operator using shmem

--- framework/include/tilefwk/tilefwk_op.h
+++ framework/include/tilefwk/tilefwk_op.h
@@ -398,6 +398,11 @@ enum class DistReduceType {
     DIST_REDUCE_MIN,
 };
 
+enum class AtomicType {
+    SET,
+    ADD
+};
+
 struct MoeConfig {
     int32_t routedExpertNum{0};
     int32_t expertNumPerRank{0};
@@ -418,9 +423,18 @@ void TwoShotAllReduce(const Tensor& predToken, const Tensor& in, const char* gro
 void MoeDistributedCombine(const Tensor& expandX, const Tensor& assistInfoForCombine, const Tensor& recvCounts,
     const Tensor& expertScales, const char* group, uint32_t epWorldSize, uint32_t moeExpertNum,
     uint32_t sharedExpertNum, uint32_t sharedExpertRankNum, Tensor& out);
-void CreateShmemData(const char *group, int64_t worldSize, DataType dataType,
-    const Shape &shape, Tensor &shmemTensor, uint64_t memType = 0);
-void CreateShmemSignal(const char *group, Tensor &shmemData, Tensor &shmemSignal);
+void MoeDistributedCombineV2(const Tensor& expandX, const Tensor& assistInfoForCombine, const Tensor& recvCounts,
+    const Tensor& expertScales, const char* group, uint32_t epWorldSize, uint32_t moeExpertNum,
+    uint32_t sharedExpertNum, uint32_t sharedExpertRankNum, Tensor& out);
+Tensor CreateShmemData(const char* group, int64_t worldSize, DataType dataType, const Shape& shape,
+    uint64_t memType = 0);
+Tensor CreateShmemSignal(const char* group, Tensor& shmemData);
+Tensor ShmemPut(const Tensor& in, const Tensor& shmemDataTile, const Tensor& barrierDummy,
+    AtomicType atomicType = AtomicType::SET);
+Tensor ShmemSignal(const Tensor& dummy, const Tensor& shmemSignalTile, AtomicType atomicType);
+Tensor WaitUntil(const Tensor& dummyIn, const Tensor& shmemSignalTile, int32_t expectedSum, bool resetSignal = false);
+Tensor ShmemGet(const Tensor& dummy, const Tensor& shmemDataTile, DataType nonShmemDataType = DataType::DT_BOTTOM,
+    AtomicType atomicType = AtomicType::SET);
 } // namespace Distributed
 std::tuple<Tensor, Tensor> TopKSort(const Tensor &x, int idxStart);
 std::tuple<Tensor, Tensor> TopKSort(const Tensor &x, const SymbolicScalar &idxStart);
