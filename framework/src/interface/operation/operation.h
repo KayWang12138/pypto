a8bfda116aa7325e36e97c2930eef8b96efd3baa
gengrunze <gengrunze@huawei.com>
2026-01-15 15:16:36 +0800

    feat(pass): Set Op Last Use Attribute.

--- framework/src/interface/operation/operation.h
+++ framework/src/interface/operation/operation.h
@@ -84,7 +84,7 @@ public:
     static const std::string subBlockIdx;
     static const std::string brcbIdx;
     static const std::string quantFlag;
-    static const std::string inputLastUse;
+    static const std::string lastUse;
 };
 
 
