7c23d16aef99f3c584a78372a2afddc93be8766c
luohuan40 <luohuan40@huawei.com>
2026-01-16 17:25:13 +0800

    1. Adjust the logic for determining printer redundancy

--- framework/src/interface/ir/transform/printer.cpp
+++ framework/src/interface/ir/transform/printer.cpp
@@ -19,19 +19,15 @@
 #include "ir/value.h"
 #include "ir/utils.h"
 
-#include <unordered_set>
-
 namespace pto {
 
 // ===== program =====
 void IRPrinter::VisitImplProgram(ProgramModulePtr &pm) {
-    if (!pm)
-        return;
+    if (!pm)return;
 
     // Print module header
     PrintIndent();
     os_ << "program.module " << pm->GetPrefixedName() << " {\n";
-
     // Print program.entry + program-level attrs
     Indent();
 
@@ -41,29 +37,12 @@ void IRPrinter::VisitImplProgram(ProgramModulePtr &pm) {
     }
 
     const auto &attrs = pm->Attributes();
-    if (!attrs.empty()) {
-        for (const auto &kv : attrs) {
-            PrintIndent();
-            os_ << "attr " << kv.first << " = " << kv.second << "\n";
-        }
+    for (const auto &kv : attrs) {
+        PrintIndent();
+        os_ << "attr " << kv.first << " = " << kv.second << "\n";
     }
-
-    // Visit all functions (dedup entry to avoid double-print)
-    std::unordered_set<const Function *> visited;
-
-    if (auto entry = pm->GetProgramEntry()) {
-        if (visited.insert(entry.get()).second) {
-            FunctionPtr f = entry;
-            VisitImplFunction(f);
-            os_ << "\n";
-        }
-    }
-
+    
     for (const auto &func : pm->GetFunctions()) {
-        if (!func)
-            continue;
-        if (!visited.insert(func.get()).second)
-            continue;
         FunctionPtr f = func;
         VisitImplFunction(f);
         os_ << "\n";
@@ -77,9 +56,6 @@ void IRPrinter::VisitImplProgram(ProgramModulePtr &pm) {
 
 // ===== function =====
 void IRPrinter::VisitImplFunction(FunctionPtr &func) {
-    if (!func)
-        return;
-
     // Print function header
     PrintIndent();
     os_ << "func.func " << func->GetPrefixedName() << "(";
@@ -87,34 +63,25 @@ void IRPrinter::VisitImplFunction(FunctionPtr &func) {
     const auto &signature = func->GetSignature();
     for (size_t i = 0; i < signature.arguments.size(); ++i) {
         const auto &arg = signature.arguments[i];
-        if (arg) {
-            PrintValue(arg);
-        }
+        PrintValue(arg);
         if (i + 1 < signature.arguments.size()) {
             os_ << ", ";
         }
     }
     os_ << ")";
-
-    if (!signature.results.empty()) {
-        os_ << " -> (";
-        for (size_t i = 0; i < signature.results.size(); ++i) {
-            if (signature.results[i]) {
-                PrintType(signature.results[i]);
-            }
-            if (i + 1 < signature.results.size()) {
-                os_ << ", ";
-            }
+    
+    os_ << " -> (";
+    for (size_t i = 0; i < signature.results.size(); ++i) {
+        PrintType(signature.results[i]);
+        if (i + 1 < signature.results.size()) {
+            os_ << ", ";
         }
-        os_ << ")";
     }
-    os_ << " {\n";
-
+    os_ << ") {\n";
     // Print function body statements
     Indent();
-    if (auto compound = func->GetCompound()) {
-        VisitImplStmt(compound);
-    }
+    auto compound = func->GetCompound();
+    VisitImplStmt(compound);
     Dedent();
 
     // Closing brace
@@ -133,9 +100,6 @@ void IRPrinter::VisitImplFunction(FunctionPtr &func) {
 // ===== statements =====
 
 void IRPrinter::VisitImplStmt(OpStatementPtr &st) {
-    if (!st)
-        return;
-
     PrintIndent();
     os_ << "statement.op {\n";
 
@@ -143,8 +107,6 @@ void IRPrinter::VisitImplStmt(OpStatementPtr &st) {
     Indent();
     const auto &operations = st->Operations();
     for (const auto &op : operations) {
-        if (!op)
-            continue;
         PrintIndent();
         OperationPtr opPtr = op;
         VisitOp(opPtr);
@@ -156,43 +118,32 @@ void IRPrinter::VisitImplStmt(OpStatementPtr &st) {
 }
 
 void IRPrinter::VisitImplStmt(IfStatementPtr &is) {
-    if (!is)
-        return;
-
     PrintIndent();
 
     const auto &results = is->Results();
-    if (!results.empty()) {
-        for (size_t i = 0; i < results.size(); ++i) {
-            if (results[i]) {
-                PrintSSAName(results[i]);
-            }
-            if (i + 1 < results.size()) {
-                os_ << ", ";
-            }
+    for (size_t i = 0; i < results.size(); ++i) {
+        PrintSSAName(results[i]);
+        if (i + 1 < results.size()) {
+            os_ << ", ";
         }
-        os_ << " = ";
     }
-
+    os_ << " = ";
+    
     os_ << "statement.if ";
-    if (auto cond = is->GetCondition()) {
-        PrintSSAName(cond);
-    }
+    PrintSSAName(is->GetCondition());
     os_ << " {\n";
 
     Indent();
-    if (auto thenCompound = is->GetThenCompound()) {
-        VisitImplStmt(thenCompound);
-    }
+    auto thenCompound = is->GetThenCompound();
+    VisitImplStmt(thenCompound);
     Dedent();
 
     PrintIndent();
     os_ << "} else {\n";
 
     Indent();
-    if (auto elseCompound = is->GetElseCompound()) {
-        VisitImplStmt(elseCompound);
-    }
+    auto elseCompound = is->GetElseCompound();
+    VisitImplStmt(elseCompound);
     Dedent();
 
     PrintIndent();
@@ -200,103 +151,63 @@ void IRPrinter::VisitImplStmt(IfStatementPtr &is) {
 }
 
 void IRPrinter::VisitImplStmt(ForStatementPtr &fs) {
-    if (!fs)
-        return;
-
     PrintIndent();
-
     const auto &results = fs->Results();
-    if (!results.empty()) {
-        for (size_t i = 0; i < results.size(); ++i) {
-            if (results[i]) {
-                PrintSSAName(results[i]);
-            }
-            if (i + 1 < results.size()) {
-                os_ << ", ";
-            }
+    
+    for (size_t i = 0; i < results.size(); ++i) {
+        PrintSSAName(results[i]);
+        if (i + 1 < results.size()) {
+            os_ << ", ";
         }
-        os_ << " = ";
     }
+    os_ << " = ";
 
     os_ << "statement.for ";
-    if (auto iv = fs->GetIterationVar()) {
-        PrintSSAName(iv);
-    } else {
-        os_ << "<null>";
-    }
-
+    PrintSSAName(fs->GetIterationVar());
     os_ << " = ";
-    if (auto range = fs->GetRange()) {
-        if (auto start = range->GetStart())
-            PrintSSAName(start);
-        else
-            os_ << "<null>";
-
-        os_ << " to ";
-
-        if (auto end = range->GetEnd())
-            PrintSSAName(end);
-        else
-            os_ << "<null>";
-
-        os_ << " step ";
-
-        if (auto step = range->GetStep())
-            PrintSSAName(step);
-        else
-            os_ << "<null>";
-    } else {
-        os_ << "<null> to <null> step <null>";
-    }
+    auto range = fs->GetRange();
+    PrintSSAName(range->GetStart());
+    os_ << " to ";
+    PrintSSAName(range->GetEnd());
+    os_ << " step ";
+    PrintSSAName(range->GetStep());
 
     const auto &iterArgs = fs->IterArgs();
-    if (!iterArgs.empty()) {
-        os_ << "\n";
-        PrintIndentWithExtra(2);
-        os_ << "iter_args(";
-        for (size_t i = 0; i < iterArgs.size(); ++i) {
-            const auto &arg = iterArgs[i];
-            if (arg.value) {
-                PrintSSAName(arg.value);
-                os_ << " = ";
-            } else {
-                os_ << "<no-value> = ";
-            }
-
-            if (arg.initValue) {
-                PrintValue(arg.initValue);
-            } else {
-                os_ << "<null> : <unknown>";
-            }
-
-            if (i + 1 < iterArgs.size()) {
-                os_ << ", ";
-            }
+    
+    os_ << "\n";
+    PrintIndentWithExtra(2);
+    os_ << "iter_args(";
+    for (size_t i = 0; i < iterArgs.size(); ++i) {
+        const auto &arg = iterArgs[i];
+        PrintSSAName(arg.value);
+        os_ << " = ";
+        PrintValue(arg.initValue);
+        
+        if (i + 1 < iterArgs.size()) {
+            os_ << ", ";
         }
-        os_ << ")";
     }
+    os_ << ")";
+    
 
     const auto &attrs = fs->Attributes();
-    if (!attrs.empty()) {
-        os_ << "\n";
-        PrintIndentWithExtra(2);
-        os_ << "attributes {";
-        bool first = true;
-        for (const auto &kv : attrs) {
-            if (!first)
-                os_ << ", ";
-            os_ << kv.first << " = " << kv.second;
-            first = false;
-        }
-        os_ << "}";
+    os_ << "\n";
+    PrintIndentWithExtra(2);
+    os_ << "attributes {";
+    bool first = true;
+    for (const auto &kv : attrs) {
+        if (!first)
+            os_ << ", ";
+        os_ << kv.first << " = " << kv.second;
+        first = false;
     }
+    os_ << "}";
 
     os_ << " {\n";
 
     Indent();
-    if (auto compound = fs->GetCompound()) {
-        VisitImplStmt(compound);
-    }
+    auto compound = fs->GetCompound();
+    VisitImplStmt(compound);
     Dedent();
 
     PrintIndent();
@@ -304,9 +215,6 @@ void IRPrinter::VisitImplStmt(ForStatementPtr &fs) {
 }
 
 void IRPrinter::VisitImplStmt(YieldStatementPtr &ys) {
-    if (!ys)
-        return;
-
     PrintIndent();
     os_ << "statement.yield";
     const auto &values = ys->Values();
@@ -318,9 +226,6 @@ void IRPrinter::VisitImplStmt(YieldStatementPtr &ys) {
 }
 
 void IRPrinter::VisitImplStmt(ReturnStatementPtr &rs) {
-    if (!rs)
-        return;
-
     PrintIndent();
     os_ << "statement.return";
     const auto &values = rs->Values();
@@ -332,36 +237,30 @@ void IRPrinter::VisitImplStmt(ReturnStatementPtr &rs) {
 }
 
 void IRPrinter::VisitImplOp(OperationPtr &op) {
-    if (!op)
-        return;
 
     // ===== results =====
-    if (op->GetNumOutputOperand()) {
-        for (size_t i = 0; i < op->GetNumOutputOperand(); ++i) {
-            PrintSSAName(op->GetOutputOperand(i));
-            if (i + 1 < op->GetNumOutputOperand()) {
-                os_ << ", ";
-            }
+    for (size_t i = 0; i < op->GetNumOutputOperand(); ++i) {
+        PrintSSAName(op->GetOutputOperand(i));
+        if (i + 1 < op->GetNumOutputOperand()) {
+            os_ << ", ";
         }
-        os_ << " = ";
     }
-
+    os_ << " = ";
+    
     // ===== op name =====
     // Determine dialect prefix based on output type
     // - Tensor outputs -> tensor. prefix (Python frontend operations)
     // - Tile outputs -> tile. prefix (compiler optimization operations)
     // - Scalar outputs -> tensor. prefix (element-wise operations)
     std::string opcodeName = GetOpcodeName(op->GetOpcode());
-    if (op->GetNumOutputOperand()) {
-        const auto &firstOutput = op->GetOutputOperand(0);
-        if (firstOutput->GetValueKind() == ValueKind::Tile) {
-            os_ << "tile." << opcodeName;
-        } else {
-            // Tensor or Scalar -> use tensor. prefix
-            os_ << "tensor." << opcodeName;
-        }
-    } else {
-        // No outputs (shouldn't happen, but fallback to tensor.)
+    
+    const auto &firstOutput = op->GetOutputOperand(0);
+    if (firstOutput->GetValueKind() == ValueKind::Tile) {
+        os_ << "tile." << opcodeName;
+    } else if (firstOutput->GetValueKind() == ValueKind::Scalar){
+        // Tensor or Scalar -> use tensor. prefix
+        os_ << "scalar." << opcodeName;
+    }else {
         os_ << "tensor." << opcodeName;
     }
 
@@ -384,45 +283,33 @@ void IRPrinter::VisitImplOp(OperationPtr &op) {
     }
     os_ << ")";
 
-    os_ << " -> ";
-    if (op->GetNumOutputOperand() == 1) {
-        PrintType(op->GetOutputOperand(0));
-    } else {
-        os_ << "(";
-        for (size_t i = 0; i < op->GetNumOutputOperand(); ++i) {
-            PrintType(op->GetOutputOperand(i));
-            if (i + 1 < op->GetNumOutputOperand()) {
-                os_ << ", ";
-            }
+    os_ << " -> (";
+    for (size_t i = 0; i < op->GetNumOutputOperand(); ++i) {
+        PrintType(op->GetOutputOperand(i));
+        if (i + 1 < op->GetNumOutputOperand()) {
+            os_ << ", ";
         }
-        os_ << ")";
     }
+    os_ << ")";
+    
 
     os_ << "\n";
 }
 
 // ===== values =====
 void IRPrinter::VisitImplValue(ScalarValuePtr &value) {
-    if (!value)
-        return;
     PrintValue(value);
 }
 
 void IRPrinter::VisitImplValue(TileValuePtr &value) {
-    if (!value)
-        return;
     PrintValue(value);
 }
 
 void IRPrinter::VisitImplValue(TensorValuePtr &value) {
-    if (!value)
-        return;
     PrintValue(value);
 }
 
 void IRPrinter::VisitImplValue(ValuePtr &value) {
-    if (!value)
-        return;
     PrintValue(value);
 }
 
@@ -448,11 +335,7 @@ void IRPrinter::PrintValueList(const std::vector<ValuePtr> &vals) {
     for (size_t i = 0; i < vals.size(); ++i) {
         if (i > 0)
             os_ << ", ";
-        if (vals[i]) {
-            PrintSSAName(vals[i]);
-        } else {
-            os_ << "<null>";
-        }
+        PrintSSAName(vals[i]);
     }
 }
 
@@ -473,9 +356,6 @@ const char *IRPrinter::ToString(FunctionKind kind) {
 
 // ===== value printing methods =====
 void IRPrinter::PrintSSAName(ValuePtr value) {
-    if (!value)
-        return;
-
     if (auto scalar = std::dynamic_pointer_cast<ScalarValue>(value)) {
         switch (scalar->GetScalarValueKind()) {
             case ScalarValueKind::Immediate:
@@ -495,23 +375,14 @@ void IRPrinter::PrintSSAName(ValuePtr value) {
 }
 
 void IRPrinter::PrintValue(ValuePtr value) {
-    if (!value)
-        return;
     PrintSSAName(value);
     os_ << ": ";
     PrintType(value);
 }
 
 void IRPrinter::PrintType(ValuePtr value) {
-    if (!value)
-        return;
-
     if (auto scalar = std::dynamic_pointer_cast<ScalarValue>(value)) {
-        if (auto type = scalar->GetType()) {
-            type->Print(os_);
-        } else {
-            os_ << "unknown";
-        }
+        scalar->GetType()->Print(os_);
     } else if (auto tensor = std::dynamic_pointer_cast<TensorValue>(value)) {
         os_ << "tensor<";
         // ====== shape ======
