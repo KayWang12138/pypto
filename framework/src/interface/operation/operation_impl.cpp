8ba3971d46ac4b80958d2b3b0d95cd7429e22d2f
gaoqiancheng <gaoqiancheng@huawei.com>
2026-01-16 10:16:36 +0800

    feat(Operation): Allow unsqueeze to support the transfer of validShape.

--- framework/src/interface/operation/operation_impl.cpp
+++ framework/src/interface/operation/operation_impl.cpp
@@ -254,6 +254,11 @@ Tensor Unsqueeze(const Tensor &old, int unsqueezeDimNum) {
     }
     std::vector<int64_t> newShape(old.GetStorage()->shape);
     newShape.insert(newShape.begin() + unsqueezeDim, 1);
+    auto validShape = old.GetStorage()->GetDynValidShape();
+    if (!validShape.empty()) {
+        validShape.insert(validShape.begin() + unsqueezeDim, 1);
+        return Reshape(old, newShape, validShape);
+    }
     return Reshape(old, newShape);
 }
 
