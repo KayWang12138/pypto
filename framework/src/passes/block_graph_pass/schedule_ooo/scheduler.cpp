7ba6d6300b28015aeb58e4c821b7881e54521fe3
gggxinmeng <guoxinmeng@huawei.com>
2026-01-16 14:17:15 +0800

    fix(Pass): Modify the depth calculation function

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
@@ -1024,7 +1024,7 @@ Status OoOScheduler::Schedule(const std::vector<Operation *> &operations) {
     PrintOpList(operations);
     if (Init(operations) != SUCCESS) { 
         APASS_LOG_ERROR_F(Elements::Operation, "Init failed!"); 
-        return FAILED; 
+        return FAILED;
     }
     // 生成spill指令
     if (GenSpillSchedule() != SUCCESS) { 
