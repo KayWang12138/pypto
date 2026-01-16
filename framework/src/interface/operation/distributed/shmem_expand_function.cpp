4ea73e6ceec20bbe0a8e506c926e04e2457b5fd1
luoxinhui666 <luoxinhui2@huawei.com>
2026-01-16 17:32:25 +0800

    fix(distributed): Fix calculation for tile info and double buffer for shmemPut or shmemGet

--- framework/src/interface/operation/distributed/shmem_expand_function.cpp
+++ framework/src/interface/operation/distributed/shmem_expand_function.cpp
@@ -129,7 +129,7 @@ Shape GetCopyBufferShape(DataType nonShmemDtype, DataType shmemDtype, Shape tile
 LogicalTensorPtr CreateAdaptiveUbTensor(Function& function, const Shape& shape, DataType ubType, DataType castType)
 {
     Shape ubShape;
-    int64_t ubLen = AlignUp(shape[0] * shape[1] * BytesOf(ubType), UB_ALIGIN_SIZE) / BytesOf(ubType);
+    int64_t ubLen = shape[0] * AlignUp(shape[1] * BytesOf(ubType), UB_ALIGIN_SIZE) / BytesOf(ubType);
     if (!shouldConvertDtype(ubType, castType)) {
         ubShape = {ubLen * 2};
     } else {
@@ -250,6 +250,8 @@ void TiledShmemSignal(Function& function, const TileShape& tileShape,
             {dummyOutTile, ubTensor});
         DistOpAttr distOpAttr;
         op.GetAttr(OpAttributeKey::distOpAttr, distOpAttr);
+        distOpAttr.tileRowShape = tileShape.GetVecTile()[0];
+        distOpAttr.tileColShape = tileShape.GetVecTile()[1];
         tileOp.SetAttr(OpAttributeKey::distOpAttr, distOpAttr);
         tileOp.SetAttr(OpAttributeKey::dontTouch, true);
         tileIndex++;
@@ -296,6 +298,8 @@ void TiledShmemWaitUntil(Function& function, const TileShape& tileShape,
             {dummyTile});
         DistOpAttr distOpAttr;
         op.GetAttr(OpAttributeKey::distOpAttr, distOpAttr);
+        distOpAttr.aicpuOpParams.push_back(tileIndex);
+        distOpAttr.aicpuOpParams.push_back(tileRowNum * tileColNum);
         tileOp.SetAttr(OpAttributeKey::distOpAttr, distOpAttr);
     });
 }
