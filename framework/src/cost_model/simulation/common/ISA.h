39ce470579b4364a5fdc237b99465bd842d7aca8
dingsinuo <dingsinuo@huawei.com>
2026-01-17 17:50:27 +0800

    feat(operation): Add the BitwiseAnd/Ands/Or/Ors/Xor/Xors/Not methods

--- framework/src/cost_model/simulation/common/ISA.h
+++ framework/src/cost_model/simulation/common/ISA.h
@@ -38,6 +38,7 @@ const std::map<std::string, CorePipeType> SCHED_CORE_PIPE_TYPE {
     {"NEG", CorePipeType::PIPE_VECTOR_ALU},
     {"RSQRT", CorePipeType::PIPE_VECTOR_ALU},
     {"SQRT", CorePipeType::PIPE_VECTOR_ALU},
+    {"BITWISENOT", CorePipeType::PIPE_VECTOR_ALU},
     {"RECIPROCAL", CorePipeType::PIPE_VECTOR_ALU},
     {"CAST", CorePipeType::PIPE_VECTOR_ALU},
     {"EXPAND", CorePipeType::PIPE_VECTOR_ALU},
@@ -71,6 +72,9 @@ const std::map<std::string, CorePipeType> SCHED_CORE_PIPE_TYPE {
     {"LN", CorePipeType::PIPE_VECTOR_ALU},
     {"HUB", CorePipeType::PIPE_VECTOR_ALU},
     {"BRCB", CorePipeType::PIPE_VECTOR_ALU},
+    {"BITWISEANDS", CorePipeType::PIPE_VECTOR_ALU},
+    {"BITWISEORS", CorePipeType::PIPE_VECTOR_ALU},
+    {"BITWISEXORS", CorePipeType::PIPE_VECTOR_ALU},
     // Binary Vector
     {"ADD", CorePipeType::PIPE_VECTOR_ALU},
     {"SUB", CorePipeType::PIPE_VECTOR_ALU},
@@ -116,6 +120,9 @@ const std::map<std::string, CorePipeType> SCHED_CORE_PIPE_TYPE {
     {"WHERE_ST", CorePipeType::PIPE_VECTOR_ALU},
     {"WHERE_SS", CorePipeType::PIPE_VECTOR_ALU},
     {"LOGICALAND", CorePipeType::PIPE_VECTOR_ALU},
+    {"BITWISEAND", CorePipeType::PIPE_VECTOR_ALU},
+    {"BITWISEOR", CorePipeType::PIPE_VECTOR_ALU},
+    {"BITWISEXOR", CorePipeType::PIPE_VECTOR_ALU},
     // Cube
     {"A_MUL_B", CorePipeType::PIPE_CUBE},
     {"A_MULACC_B", CorePipeType::PIPE_CUBE},
