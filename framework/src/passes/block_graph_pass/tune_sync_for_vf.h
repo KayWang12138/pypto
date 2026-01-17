50d4b19305997a896e686d579fa6ab1bcc2d15c4
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-01-17 15:50:49 +0800

    feat(pass): fix codecheck3 for tunesyncseq

--- framework/src/passes/block_graph_pass/tune_sync_for_vf.h
+++ framework/src/passes/block_graph_pass/tune_sync_for_vf.h
@@ -44,10 +44,12 @@ private:
     bool NeedAdjustOpSeq(Function *subGraphFunc, const std::vector<Operation *> &setFlagList,
         const std::vector<Operation *> &waitFlagList, size_t left, size_t right);
     void AddVecTileopsToGroup(int &groupNum, size_t left, size_t right);
-    size_t MoveOpsForMerge(size_t vecTileOp0Idx, size_t vecTileOp1Idx, int groupNum);
+    size_t MoveOpsForMerge(size_t vecTileOp0Idx, size_t vecTileOp1Idx, int groupNum,
+        std::vector<Operation *> &setFlagList, std::vector<Operation *> &waitFlagList);
     Status UpdatePipeVTime(Operation *vecTileOp1, int groupNum, size_t mergedSize, int &curVFStartTime, int &curVecTileOp1EndTime);
     Status UpdateSetPipeTime(Function *subGraphFunc, std::vector<Operation *> &setFlagList, const int &curVecTileOp1EndTime);
-    Status UpdateWaitPipeTime(Function *subGraphFunc, std::vector<Operation *> &waitFlagList, const int &curVFStartTime, int &maxMoveBackDist);
+    Status UpdateWaitPipeTime(Function *subGraphFunc, std::vector<Operation *> &waitFlagList,
+        const int &curVFStartTime, int &maxMoveBackDist);
     Status MoveBackPipeVOps(int groupNum, const int &maxMoveBackDist);
     std::vector<Operation *> opList_;
     std::vector<std::vector<Operation *>> mergedOps;
