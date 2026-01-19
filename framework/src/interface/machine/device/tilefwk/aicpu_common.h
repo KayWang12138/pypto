546dd3f2050d74d2264a34dabe171fb9dd044d9d
wan_jianfen <wanjianfen@hisilicon.com>
2026-01-19 11:41:05 +0000

    fix(machine): Remove pypto launch init

--- framework/src/interface/machine/device/tilefwk/aicpu_common.h
+++ framework/src/interface/machine/device/tilefwk/aicpu_common.h
@@ -147,7 +147,8 @@ struct DeviceArgs {
     uint64_t startArgsAddr{0}; // DevStartArgs addr
     uint64_t taskQueue{0};     // task queue between ctrl and sche
     uint64_t taskCtrl{0};      // task ctrl between ctrl and sche
-    uint32_t scheCpuNum{0};    // sche cpu num calc by host
+    uint32_t scheCpuNum: 16;    // sche cpu num calc by host
+    uint32_t isNeedLaunchInit: 16;    // only init once
     uint32_t enableCtrl : 2;    // if enable builtin ctrl
     uint32_t validGetPgMask : 2; // mark pgmask is invalid
     uint32_t disableSync : 2;    // close ctrl and sche soft sync
