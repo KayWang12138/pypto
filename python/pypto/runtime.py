ba77b1d27987288499ce0a7480a68a6de1593a94
HuangYuqian <huangyuqian2@huawei.com>
2026-01-19 21:19:52 +0800

    dfx

--- python/pypto/runtime.py
+++ python/pypto/runtime.py
@@ -239,14 +239,14 @@ class _JIT:
                 pypto_impl.BuildCache(self._handler, in_out_tensors_data, [])
                 import shutil
                 try:
- 	                shutil.rmtree(
- 	                    self._output_path,
- 	                    ignore_errors=True,  # 忽略删除失败的文件（如只读文件）
- 	                    onerror=None         # 关闭错误回调（减少函数调用开销）
- 	                )
- 	            except FileNotFoundError:
- 	                pass
-            else:
+                    shutil.rmtree(
+                        self._output_path,
+                        ignore_errors=True,  # 忽略删除失败的文件（如只读文件）
+                        onerror=None         # 关闭错误回调（减少函数调用开销）
+                    )
+                except FileNotFoundError:
+                    pass
+                else:
                 pypto_impl.ResetLog(self._output_path)
                 self._handler = self._handler_cache.get(input_hash)
             # dispatch run mode based on ASCEND_HOME_PATH or run_mode
