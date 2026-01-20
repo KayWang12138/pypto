06e2f0217979b5e3eefd51b2df08fa8ba3bb6988
wuchenhao123 <wuchenhao6@huawei.com>
2026-01-20 20:03:21 +0800

    fix(distributed): Fix distributed UT occasional problem

--- framework/src/machine/device/distributed/shmem_wait_until.cpp
+++ framework/src/machine/device/distributed/shmem_wait_until.cpp
@@ -31,19 +31,27 @@
 
 namespace npu::tile_fwk::Distributed {
 
-inline bool SignalTileOp::PollCompleted() const {
-    if (addr_[0] == expectedSum_) {
-        if (resetSignal_) {
-            addr_[0] = 0;
+inline bool SignalTileOp::PollCompleted() const 
+{
+    if constexpr (npu::tile_fwk::dynamic::IsDeviceMode()) {
+        if (addr_[0] == expectedSum_) {
+            if (resetSignal_) {
+                addr_[0] = 0;
+            }
+            return true;
         }
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
 
 int32_t ShmemWaitUntil::PollCompleted(npu::tile_fwk::dynamic::AiCoreManager &aicoreManager)
 {
     return runingTaskQueue_.PollCompleted([&](SignalTileOp* task) {
+    if constexpr (npu::tile_fwk::dynamic::IsDeviceMode()) {
+            (void)task;
+ 	        return dynamic::DEVICE_MACHINE_OK;
+        }
         return aicoreManager.ProcessCompletedAicpuTask(task->taskId_);
     });
 }
