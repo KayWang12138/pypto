59ede2333a73301c9bc2a4158a6b064623134ba8
zhiweiyuan <zhiweiyuan@huawei.com>
2026-01-20 19:43:23 +0800

    feat(simulation): Add pv model
    Signed-off-by: zhiweiyuan <zhiweiyuan@huawei.com>

--- framework/src/cost_model/simulation/pv/PvModelFactory.cpp
+++ framework/src/cost_model/simulation/pv/PvModelFactory.cpp
@@ -31,10 +31,6 @@ namespace CostModel
         using CreateFunc = std::shared_ptr<PvModel>(*)();
         std::string funcName = "CreatePvModelImpl" + arch;
         auto createFunc = (CreateFunc)(dlsym(handle, funcName.c_str()));
-        if (!createFunc) {
-            dlclose(handle);
-            throw std::runtime_error("can not find the factory func: " + arch);
-        }
 
         // 创建对象并返回
         return createFunc();
@@ -45,17 +41,13 @@ namespace CostModel
         std::string soPath = "libtile_fwk_simulation_pv.so";
         void* handle = dlopen(soPath.c_str(), RTLD_LAZY);
         if (!handle) {
-            throw std::runtime_error("can not load library: ");
+            throw std::runtime_error("can not load library: " + soPath);
         }
 
         // 获取工厂函数符号
         using CreateFunc = std::shared_ptr<DynPvModel>(*)();
         std::string funcName = "CreateDynPvModelImpl";
         auto createFunc = (CreateFunc)(dlsym(handle, funcName.c_str()));
-        if (!createFunc) {
-            dlclose(handle);
-            throw std::runtime_error("CreateDynPvModelImpl function not found.");
-        }
 
         // 创建对象并返回
         return createFunc();
