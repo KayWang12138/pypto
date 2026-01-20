c27069b623d994a11c8eb7c9a0f50605f49b8582
wangqun23 <wangqun23@huawei.com>
2026-01-20 14:41:51 +0800

    test(pass): test replace add copy op

--- framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/pre_graph.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/pre_graph.cpp
@@ -71,8 +71,6 @@ Status PreGraphProcess::RunOnFunction(Function &function) {
             setCopyAttr.ProcessMoveInOperation(op);
         }
     }
-    RemoveRedundantAssemble removeRedundantAssemble;
-    removeRedundantAssemble.DeleteRedundantAssemble(function);
     CubeProcess cubeProcess;
     if (cubeProcess.UpdateCubeOp(function) != SUCCESS) {
         APASS_LOG_ERROR_F(Elements::Function, "Update Cube attr failed.");
