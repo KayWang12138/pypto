58ba23784e4dad73551e36009810e7b67a2af737
gggxinmeng <guoxinmeng@huawei.com>
2026-01-20 16:38:43 +0800

    fix(Pass): Modify mixSchedule bug
    Co-authored-by: gggxinmeng<guoxinmeng@huawei.com>
    
    !507 merge fixMixBug2 into master
    
    fix(Pass): Modify mixSchedule bug
    
    Created-by: gggxinmeng
    Commit-by: gggxinmeng
    Merged-by: cann-robot
    Description: 修改Mix场景下出现的问题。
    问题描述：
    
    存在的一个切图情形：tensor没有生产者op，需要在estimate_latency和scheduler文件中的GenSpill和MainLoop流程中对此类情形下的tensor进行提前识别并申请Alloc，需要注意其中view节点或级联view场景下的issueEntries中不包含viewOp情况的额外处理;
    计算tensor的大小时，统一使用rawshape进行计算;
    LatencyEstimatorMainLoop中无前继节点的op无法执行：修改其插入过程应该在初始化opQueue之后。
    
    See merge request: cann/pypto!507

--- framework/src/interface/operation/distributed/shmem_expand_function.cpp
+++ framework/src/interface/operation/distributed/shmem_expand_function.cpp
@@ -160,7 +160,7 @@ Shape GetCopyBufferShape(DataType nonShmemDtype, DataType shmemDtype, Shape tile
 LogicalTensorPtr CreateAdaptiveUbTensor(Function& function, const Shape& shape, DataType ubType, DataType castType)
 {
     Shape ubShape;
-    int64_t ubLen = AlignUp(shape[0] * shape[1] * BytesOf(ubType), UB_ALIGIN_SIZE) / BytesOf(ubType);
+    int64_t ubLen = shape[0] * AlignUp(shape[1] * BytesOf(ubType), UB_ALIGIN_SIZE) / BytesOf(ubType);
     if (!shouldConvertDtype(ubType, castType)) {
         ubShape = {ubLen * 2};
     } else {
@@ -263,6 +263,8 @@ void TiledShmemSignal(Function& function, const TileShape& tileShape,
 
         DistOpAttr distOpAttr;
         op.GetAttr(OpAttributeKey::distOpAttr, distOpAttr);
+        distOpAttr.tileRowShape = tileShape.GetVecTile()[0];
+        distOpAttr.tileColShape = tileShape.GetVecTile()[1];
         tileOp.SetAttr(OpAttributeKey::distOpAttr, distOpAttr);
         tileOp.SetAttr(OpAttributeKey::dontTouch, true);
     });
@@ -278,11 +280,16 @@ void TiledShmemWaitUntil(Function& function, const TileShape& tileShape,
     auto shmemSignal = iOperand[1];
     auto dummy = oOperand[0];
 
+    int32_t totalRowShape = shmemSignal->shape[shmemSignal->shape.size() - 2];
+    int32_t totalColShape = shmemSignal->shape[shmemSignal->shape.size() - 1];
+    int32_t tileRowShape = tileShape.GetVecTile()[0];
+    int32_t tileColShape = tileShape.GetVecTile()[1];
+    int32_t tileRowNum = totalRowShape / tileRowShape + (totalRowShape % tileRowShape == 0 ? 0 : 1);
+    int32_t tileColNum = totalColShape / tileColShape + (totalColShape % tileColShape == 0 ? 0 : 1);
+
     DummyTileFunc dummyInTileFunc = GetDummyTileFunc(dummyIn, shmemSignal, tileShape.GetVecTile(), function);
     DummyTileFunc dummyTileFunc = GetDummyTileFunc(dummy, shmemSignal, tileShape.GetVecTile(), function);
     DfsTiling(tileShape.GetVecTile(), shmemSignal, [&](uint32_t tileIndex, Input& input) {
-        ASSERT(tileIndex <= MAX_TILE_NUM / shmemSignal->shape[0]) << "ShmemWaitUntil totalTileNum is invalid";
-
         auto dummyInTile = dummyInTileFunc(tileIndex);
         std::vector<int64_t>& shmemSignalTileShape = input.tileInfo.shape;
         std::vector<int64_t>& shmemSignalTileOffset = input.tileInfo.offset;
@@ -293,6 +300,8 @@ void TiledShmemWaitUntil(Function& function, const TileShape& tileShape,
 
         DistOpAttr distOpAttr;
         op.GetAttr(OpAttributeKey::distOpAttr, distOpAttr);
+        distOpAttr.aicpuOpParams.push_back(tileIndex);
+        distOpAttr.aicpuOpParams.push_back(tileRowNum * tileColNum);
         tileOp.SetAttr(OpAttributeKey::distOpAttr, distOpAttr);
     });
 }
