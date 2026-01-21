5798bfe6f067ad0f49a8e009b070c5ea404282ca
bytecode <dubingjia1@huawei.com>
2026-01-21 20:53:47 +0800

    fix assemble

--- framework/tests/ut/passes/src/test_pre_graph.cpp
+++ framework/tests/ut/passes/src/test_pre_graph.cpp
@@ -838,6 +838,79 @@ TEST_F(PreGraphTest, TestProcessReshape) {
     EXPECT_EQ(reshapeCnt, 2);
 }
 
+void CompareOpImmediateVector(const std::vector<OpImmediate> &result, const std::vector<int64_t> &expect) {
+    EXPECT_EQ(result.size(), expect.size());
+    for (size_t idx = 0; idx < result.size(); ++idx) {
+        EXPECT_EQ(result[idx].Dump(), std::to_string(expect[idx]));
+    }
+}
+
+TEST_F(PreGraphTest, TestRemoveRedundantAssemble) {
+    ComputationalGraphBuilder G;
+    // add tensor
+    G.AddTensor(DataType::DT_FP16, {128, 256}, "t2");
+    std::vector<std::vector<int64_t>> offsets = {
+        { 0,   0},
+        { 0, 128},
+        {64,   0},
+        {64, 128}
+    };
+    for (size_t i = 0; i < 4; i++) {
+        std::string tensorName = "t1" + std::to_string(i);
+        std::string copyName = "COPYOUT" + std::to_string(i);
+        G.AddTensor(DataType::DT_FP16, {64, 128}, tensorName);
+        G.AddOp(Opcode::OP_COPY_OUT, {tensorName}, {"t2"}, copyName);
+        auto copy = G.GetOp(copyName);
+        auto copyAttr = std::make_shared<CopyOpAttribute>(MemoryType::MEM_L0C, OpImmediate::Specified(offsets[i]),
+            OpImmediate::Specified(std::vector<int64_t>{64, 128}),
+            OpImmediate::Specified(std::vector<int64_t>{64, 128}));
+        copy->SetOpAttribute(copyAttr);
+    }
+
+    G.AddTensor(DataType::DT_FP16, {1, 128, 256}, "t3");
+    G.AddTensor(DataType::DT_FP16, {3, 128, 256}, MemoryType::MEM_DEVICE_DDR, "t4");
+    auto tensor3 = G.GetTensor("t3");
+    auto tensor4 = G.GetTensor("t4");
+    tensor3->tensor = tensor4->tensor;
+    // add op
+
+    G.AddOp(Opcode::OP_RESHAPE, {"t2"}, {"t3"}, "RESHAPE");
+    G.AddOp(Opcode::OP_ASSEMBLE, {"t3"}, {"t4"}, "ASSEMBLE");
+    std::vector<int64_t> offset = {0, 0, 0};
+    auto assemble = G.GetOp("ASSEMBLE");
+    auto assembleAttr = std::make_shared<AssembleOpAttribute>(MemoryType::MEM_DEVICE_DDR, std::vector<int64_t>{2, 0, 0},
+        OpImmediate::ToSpecified(OpImmediate::Specified(std::vector<int64_t>{2, 0, 0})));
+    assemble->SetOpAttribute(assembleAttr);
+    // set incast and outcast
+    G.SetOutCast({"t4"});
+    // run pass
+    Function *function = G.GetFunction();
+    EXPECT_NE(function, nullptr);
+    function->DumpJsonFile();
+    PreGraphProcess passLocal;
+    EXPECT_EQ(passLocal.Run(*function, "", "", 0), SUCCESS);
+    function->DumpJsonFile();
+    // check after pass
+    // auto opList = function->Operations();
+    // int64_t viewCnt = 0;
+    // for (const auto &op : opList) {
+    //     if (op.GetOpcode() == Opcode::OP_VIEW) {
+    //         ++viewCnt;
+    //     }
+    // }
+    // EXPECT_EQ(viewCnt, 0);
+
+    // auto copyOut = G.GetOp("COPYOUT");
+    // std::shared_ptr<CopyOpAttribute> copyAttr = std::static_pointer_cast<CopyOpAttribute>(copyOut->GetOpAttribute());
+    // auto newDynOffset = copyAttr->GetToOffset();
+    // CompareOpImmediateVector(newDynOffset, std::vector<int64_t>{2 * 64, 0});
+
+    // auto newRawShape = copyAttr->GetRawShape();
+    // CompareOpImmediateVector(newRawShape, std::vector<int64_t>{64, 1, 4, 128});
+    // auto t = G.GetTensor("t3");
+    // EXPECT_EQ(t->GetShape(), (std::vector<int64_t>{64, 1, 4, 128}));
+} // namespace tile_fwk
+
 } // namespace tile_fwk
 } // namespace npu
 #undef private
