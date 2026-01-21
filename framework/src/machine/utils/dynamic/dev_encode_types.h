1dbec2c8e6b76438e244b8fd34aa43e0a70b6855
W4termy <wangmingyang21@huawei.com>
2026-01-21 16:55:11 +0800

    fix(machine): Fix a bug that negative numbers were incorrectly align up

--- framework/src/machine/utils/dynamic/dev_encode_types.h
+++ framework/src/machine/utils/dynamic/dev_encode_types.h
@@ -84,6 +84,10 @@ inline bool IsTaskFinish(uint32_t id, uint32_t finValue) {
 }
 
 #define ALIGN_UP(val, align)            (((val) + (align) - 1) & ~((align) - 1))
+ inline int64_t AlignUp(int64_t val, int64_t align)
+{
+    return (((val) + (align) - 1) & ~((align) - 1));
+}
 
 using uintdevptr_t = uint64_t;
 using intdevptr_t = int64_t;
@@ -94,12 +98,12 @@ inline void HostAssign(T *&ptr, uintdevptr_t offset) {
 }
 template <typename T>
 inline void DeviceReloc(T *&ptr, intdevptr_t shift) {
-    ptr = reinterpret_cast<T *>(reinterpret_cast<uintdevptr_t>(ptr) + shift);
+    ptr = reinterpret_cast<T *>(reinterpret_cast<intdevptr_t>(ptr) + shift);
 }
 template <typename T>
 inline void DeviceRelocMaybeNull(T *&ptr, intdevptr_t shift) {
     if (ptr != nullptr) {
-        ptr = reinterpret_cast<T *>(reinterpret_cast<uintdevptr_t>(ptr) + shift);
+        ptr = reinterpret_cast<T *>(reinterpret_cast<intdevptr_t>(ptr) + shift);
     }
 }
 
@@ -197,8 +201,8 @@ struct DevRelocVector {
         size_ = size;
         offset = reinterpret_cast<uintdevptr_t>(data_ + size);
     }
-    void DeviceRelocData(intdevptr_t shift) { DeviceReloc(data_, ALIGN_UP(shift, alignof(T))); }
-    void DeviceRelocDataMaybeNull(intdevptr_t shift) { DeviceRelocMaybeNull(data_, ALIGN_UP(shift, alignof(T))); }
+    void DeviceRelocData(intdevptr_t shift) { DeviceReloc(data_, AlignUp(shift, alignof(T))); }
+    void DeviceRelocDataMaybeNull(intdevptr_t shift) { DeviceRelocMaybeNull(data_, AlignUp(shift, alignof(T))); }
     uintdevptr_t End() const { return reinterpret_cast<uintdevptr_t>(data_ + size_); }
 
     static uint64_t ElementSize() { return sizeof(T); }
