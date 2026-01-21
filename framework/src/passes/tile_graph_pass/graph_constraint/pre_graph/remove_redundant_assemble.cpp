98c0bc196e98cecf43bab1d79c67c9bc159c9d84
bytecode <dubingjia1@huawei.com>
2026-01-21 11:42:41 +0800

    back2

--- framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
@@ -545,9 +545,6 @@ Status RemoveRedundantAssemble::DeleteRedundantAssemble(Function &function) cons
         APASS_LOG_ERROR_F(Elements::Function, "RemoveViewMultiReshape failed.");
         return FAILED;
     }
-
-    std::string dbjfile = "/home/d00899108/bluecode/view0117";
-    function.DumpJsonFile(dbjfile + "/0json/tmp-before8.json");
     if (ProcessView(function) != SUCCESS) {
         APASS_LOG_ERROR_F(Elements::Function, "ProcessView failed.");
         return FAILED;
