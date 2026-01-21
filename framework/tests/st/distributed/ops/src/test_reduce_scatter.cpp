27387e2bf2f860662804cd4b7ee7ecdd4548c947
luozhonglin <luozhonglin1@huawei.com>
2026-01-21 11:59:46 +0800

    fix(distributed): Shmem Tensor add validate

--- framework/tests/st/distributed/ops/src/test_reduce_scatter.cpp
+++ framework/tests/st/distributed/ops/src/test_reduce_scatter.cpp
@@ -25,7 +25,6 @@ namespace Distributed {
 
 template<typename T>
 void TestReduceScatter(OpTestParam& testParam)
-void TestReduceScatter(OpTestParam& testParam)
 {
     constexpr size_t paramsSize = 5;
     auto [row, col, typeNum, tileRow, tileCol] = GetParams<paramsSize>(GetGoldenDir() + "/params.bin");
