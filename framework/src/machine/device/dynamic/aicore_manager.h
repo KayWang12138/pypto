e57f8da9bc929010eb430d7c62de4a95ed497269
luozhonglin <luozhonglin1@huawei.com>
2026-01-21 16:13:26 +0800

    feat(distributed): Start time as params

--- framework/src/machine/device/dynamic/aicore_manager.h
+++ framework/src/machine/device/dynamic/aicore_manager.h
@@ -186,8 +186,8 @@ public:
         }
     }
 
-    inline int RunTask(DeviceTaskCtrl *taskCtrl) {
-        auto ret = ExecuteTask(taskCtrl);
+    inline int RunTask(DeviceTaskCtrl *taskCtrl, DeviceArgs *deviceArgs) {
+        auto ret = ExecuteTask(taskCtrl, deviceArgs);
         wrapManager_.Deinit();
         if (unlikely(ret != DEVICE_MACHINE_OK)) {
             DEV_ERROR("Aicpu %d proc finish %lu %lu %lu, but timeout !.", aicpuIdx_,
@@ -197,8 +197,8 @@ public:
         return ret;
     }
 
-    inline int32_t ExecuteTask(DeviceTaskCtrl *taskCtrl) {
-        int32_t ret = ProcessTask(taskCtrl);
+    inline int32_t ExecuteTask(DeviceTaskCtrl *taskCtrl, DeviceArgs *deviceArgs) {
+        int32_t ret = ProcessTask(taskCtrl, deviceArgs);
         if (unlikely(ret != DEVICE_MACHINE_OK)) {
             return ret;
         }
@@ -219,7 +219,7 @@ public:
         return ret;
     }
 
-    inline int32_t ProcessTask(DeviceTaskCtrl *taskCtrl) {
+    inline int32_t ProcessTask(DeviceTaskCtrl *taskCtrl, [[maybe_unused]]DeviceArgs *deviceArgs) {
         int32_t ret = DEVICE_MACHINE_OK;
         seq = taskCtrl->taskId;
         DEV_INFO("receive new task %lu.", taskCtrl->taskId);
@@ -235,8 +235,8 @@ public:
         }
 
         if (IsNeedProcAicpuTask()) {
-            const bool profSwitch = aicoreProf_.ProfIsEnable();
-            ret = aicpuTaskManager_.Init(reinterpret_cast<DynDeviceTask *>(curDevTask_), sharedBuffer_, profSwitch);
+            const bool profSwitch = aicoreProf_.ProfIsEnable()
+            ret = aicpuTaskManager_.Init(reinterpret_cast<DynDeviceTask *>(curDevTask_), deviceArgs, profSwitch);
             if (unlikely(ret != DEVICE_MACHINE_OK)) {
                 return ret;
             }
@@ -361,7 +361,7 @@ public:
             PerfMtTrace(PERF_TRACE_DEV_TASK_RCV, aicpuIdx_);
             PROF_STAGE_BEGIN_MTSAFE(PERF_EVT_STAGE_SCHEDULE, threadIdx, "dispatch.before\n");
             PerfMtBegin(PERF_EVT_RUN_TASK, threadIdx);
-            ret = RunTask(taskCtrl);
+            ret = RunTask(taskCtrl, deviceArgs);
             lastDevTaskFinCycle = GetCycles();
             PerfMtEnd(PERF_EVT_RUN_TASK, threadIdx);
             DEV_DEBUG("Run task finish taskid=%d ret %d.", curTaskId_, ret);
@@ -376,10 +376,6 @@ public:
     }
 
     int32_t ProcessCompletedAicpuTask(uint64_t taskId) {
-        if (aicoreProf_.ProfIsEnable()) {
-            aicpuTaskManager_.FillTaskEndTimes(taskId);
-        }
-        
         int32_t ret = ResolveDepDyn(taskId);
         if (unlikely(ret != DEVICE_MACHINE_OK)) {
             return ret;
@@ -1328,7 +1324,6 @@ private:
         pendingResolveIndexList_.fill(0);
         taskDfxStatPos_.fill(REG_LOW_TASK_PING);
         isSendStop = false;
-        sharedBuffer_ = deviceArgs->sharedBuffer;
 
         wrapManager_.InitArchInfo(deviceArgs->archInfo);
         if (deviceArgs->machineConfig != static_cast<uint8_t>(MachineScheduleConfig::DEFAULT_SCH)) {
@@ -1657,7 +1652,6 @@ private:
     int aicEnd_{0};
     int aivStart_{0};
     int aivEnd_{0};
-    uint64_t sharedBuffer_{0};
     uint64_t procAicCoreFunctionCnt_{0};
     uint64_t procAivCoreFunctionCnt_{0};
     uint64_t procAicpuFunctionCnt_{0};
