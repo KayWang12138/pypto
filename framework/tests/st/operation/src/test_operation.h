68214137420e2d25bc3673001e7b3324014213d1
陈展熹 <chenzhanxi1@huawei.com>
2026-01-21 15:14:29 +0800

    feat(Operation): Support cast fp8

--- framework/tests/st/operation/src/test_operation.h
+++ framework/tests/st/operation/src/test_operation.h
@@ -190,6 +190,18 @@ private:
                 case DataType::DT_UINT64:
                     readGoldenCmp<uint64_t>(tensor, testCase.goldenPaths[i], i, 0);
                     break;
+                case DataType::DT_HF8:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
+                case DataType::DT_FP8E4:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
+                case DataType::DT_FP8E5:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
+                case DataType::DT_FP8E8:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
                 default:
                     ASSERT_TRUE(false) << "no support dtype " << tensor.GetDataType();
                     break;
