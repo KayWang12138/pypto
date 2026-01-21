efb48f54f3bc6ebf56d9caddcbfd1e00ec2c14f7
陈展熹 <chenzhanxi1@huawei.com>
2026-01-21 15:14:29 +0800

    feat(Operation): Support cast fp8

--- framework/tests/st/operation/src/test_operation.h
+++ framework/tests/st/operation/src/test_operation.h
@@ -190,6 +190,18 @@ private:
                 case DataType::DT_UINT64:
                     readGoldenCmp<uint64_t>(tensor, testCase.goldenPaths[i], i, 0);
                     break;
+                case DataType::DT_HF8:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
+                case DataType::DT_FP8E4M3:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
+                case DataType::DT_FP8E5M2:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
+                case DataType::DT_FP8E8M0:
+                    readGoldenCmp<uint8_t>(tensor, testCase.goldenPaths[i], i, 0);
+                    break;
                 default:
                     ASSERT_TRUE(false) << "no support dtype " << tensor.GetDataType();
                     break;
@@ -337,6 +349,9 @@ static DataType GetDataType(const std::string &name) {
         {"uint64", DataType::DT_UINT64},
         {  "bool",   DataType::DT_BOOL},
         {"double", DataType::DT_DOUBLE},
+        {"fp8e4m3", DataType::DT_FP8E4M3},
+        {"fp8e5m2", DataType::DT_FP8E5M2},
+        {"fp8e8m0", DataType::DT_FP8E8M0},
     };
     if (name_to_dtype.find(name) == name_to_dtype.end()) {
         ALOG_ERROR << "Not support type " << name << " yet, return fp32 as default.";
