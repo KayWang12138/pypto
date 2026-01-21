7ca715d41f138afe115c0f6bf15b2bf18b7c6bd5
junjieHuang <huangjunjie38@h-partners.com>
2026-01-21 15:13:56 +0800

    fix(interface): Standardize the log format.

--- framework/tests/ut/interface/src/utils/test_file.cpp
+++ framework/tests/ut/interface/src/utils/test_file.cpp
@@ -20,6 +20,14 @@
 
 using namespace npu::tile_fwk;
 
+class FileTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
 TEST(FileTest, NullptrTest) {
     uint8_t a = 255;
     std::vector<uint8_t> data{a};
@@ -27,4 +35,23 @@ TEST(FileTest, NullptrTest) {
 
     bool ret = SaveFile("", &a, 1);
     EXPECT_EQ(ret, false);
+}
+
+TEST(FileTest, ReadBytesFromFileTest) {
+    char a = 'a';
+    std::vector<char> data{a};
+
+    bool ret = ReadBytesFromFile("", data);
+    EXPECT_EQ(ret, false);
+
+    ret = ReadBytesFromFile("/not_exist/test.log", data);
+    EXPECT_EQ(ret, false);
+}
+
+TEST(FileTest, LoadFileTest) {
+    uint8_t a = 255;
+    std::vector<uint8_t> data{a};
+
+    data = LoadFile("");
+    EXPECT_EQ(data.size(), 0);
 }
