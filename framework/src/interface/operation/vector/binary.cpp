05f3ffca13211289a2582f5cb6bd4201e395004a
王润法 <wangrunfa1@huawei.com>
2026-01-21 16:56:03 +0800

    add Fmod

--- framework/src/interface/operation/vector/binary.cpp
+++ framework/src/interface/operation/vector/binary.cpp
@@ -108,7 +108,7 @@ void TiledBinaryOperation(Function &function, const TileShape &tileShape, size_t
                 tmpShape = input1.tileInfo.shape;
             }
             auto alignSize = BLOCK_SIZE / BytesOf(input2.tensor->Datatype());
-            tmpShape[input1.tileInfo.shape.size() - 1] = (tmpShape[input1.tileInfo.shape.size() - 1] + alignSize -1) / alignSize * alignSize;
+            tmpShape[tmpShape.size() - 1] = (tmpShape[tmpShape.size() - 1] + alignSize -1) / alignSize * alignSize;
             int64_t tmpSize = 1;
             for (int64_t num : tmpShape){
                 tmpSize *= num;
@@ -233,6 +233,29 @@ void TiledBinaryOperationScalar(Function &function, const TileShape &tileShape, 
     if (cur == input1.tensor->GetShape().size()) {
         auto inputTile1 = input1.tensor->View(function, input1.tileInfo.shape, input1.tileInfo.offset);
         auto resultTile = result->View(function, resultTileInfo.shape, resultTileInfo.offset);
+        auto opName = GetBinaryOpName<T>();
+        if (opName == "MOD") {
+            std::vector<int64_t> tmpShape;
+            if (input1.tileInfo.shape.size() >= 2) {
+                tmpShape.assign(input1.tileInfo.shape.end() - 2, input1.tileInfo.shape.end());
+            } else {
+                tmpShape = input1.tileInfo.shape;
+            }
+            auto alignSize = BLOCK_SIZE / BytesOf(input1.tensor->Datatype());
+            tmpShape[tmpShape.size() - 1] = (tmpShape[tmpShape.size() - 1] + alignSize -1) / alignSize * alignSize;
+            int64_t tmpSize = 1;
+            for (int64_t num : tmpShape) {
+                tmpSize *= num;
+            }
+            size_t totalBytes = BytesOf(DataType::DT_FP32) * tmpSize * 4;
+            std::vector<int64_t> tmpTensorShape({static_cast<int64_t>(totalBytes)});
+            auto tmpTensor = std::make_shared<LogicalTensor>(function, DT_UINT8, tmpTensorShape);
+
+            auto &op = function.AddOperation(GetBinaryOpNameCode<T, true>(), {inputTile1}, {resultTile, tmpTensor});
+            op.SetAttribute(OpAttributeKey::scalar, value);
+            op.SetAttribute(OP_ATTR_PREFIX + "reverseOperand", reverseOperand);
+            return;
+        }
         // 确认接口
         auto &op = function.AddOperation(GetBinaryOpNameCode<T, true>(), {inputTile1}, {resultTile});
         op.SetAttribute(OpAttributeKey::scalar, value);
