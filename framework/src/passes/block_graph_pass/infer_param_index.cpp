da6548fdd421054c3f4078670f030b9c5e8a7018
wangqun23 <wangqun23@huawei.com>
2026-01-22 11:19:49 +0800

    fix(test): Fix the problem the valid shape is disable in frontend

--- framework/src/passes/block_graph_pass/infer_param_index.cpp
+++ framework/src/passes/block_graph_pass/infer_param_index.cpp
@@ -92,12 +92,12 @@ Status InferParamIndex::ResetDynValidShape(Function& function) {
             return FAILED;
         }
         // 清空view和assemble的属性中的dynvalidshape，以便后续重新推导符号化的dynvalidshape
-        if (op.GetOpcode() == Opcode::OP_VIEW) {
-            if (ResetViewDynValidShape(op) != SUCCESS) {
-                APASS_LOG_ERROR_F(Elements::Operation, "Fail to reset the output operand shape of VIEW operation %d in function %s. %s", op.GetOpMagic(), function.GetRawName().c_str(), GetFormatBacktrace(op).c_str());
-                return FAILED;
-            }
-        }
+        // if (op.GetOpcode() == Opcode::OP_VIEW) {
+        //     if (ResetViewDynValidShape(op) != SUCCESS) {
+        //         APASS_LOG_ERROR_F(Elements::Operation, "Fail to reset the output operand shape of VIEW operation %d in function %s. %s", op.GetOpMagic(), function.GetRawName().c_str(), GetFormatBacktrace(op).c_str());
+        //         return FAILED;
+        //     }
+        // }
         if (op.GetOpcode() == Opcode::OP_ASSEMBLE) {
             if (ResetAssembleDynValidShape(op) != SUCCESS) {
                 APASS_LOG_ERROR_F(Elements::Operation, "Fail to reset the output operand shape of ASSEMBLE operation %d in function %s. %s", op.GetOpMagic(), function.GetRawName().c_str(), GetFormatBacktrace(op).c_str());
