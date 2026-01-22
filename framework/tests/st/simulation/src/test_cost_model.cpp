fe277a8117ecf74e62dd71959367a8025a90b6f8
zhiweiyuan <zhiweiyuan@huawei.com>
2026-01-22 11:29:16 +0800

    feat(simulation): Add pv model
    Signed-off-by: zhiweiyuan <zhiweiyuan@huawei.com>

--- framework/tests/st/simulation/src/test_cost_model.cpp
+++ framework/tests/st/simulation/src/test_cost_model.cpp
@@ -183,7 +183,6 @@ void CostModelTestLoopViewAssemble(const Tensor &t0, const Tensor &t1, const Ten
 }
 
 TEST_F(CostModelDynTest, TestDD) {
-    config::SetHostOption(ONLY_CODEGEN, true);
     config::SetRuntimeOption(CFG_RUN_MODE, CFG_RUN_MODE_SIM);
     config::SetHostOption(COMPILE_STAGE, GEN_KERNEL_CODE);
     constexpr int tilingX = 32;
@@ -229,7 +228,6 @@ TEST_F(CostModelDynTest, TestDD) {
 }
 
 TEST_F(CostModelDynTest, TestGG) {
-    config::SetHostOption(ONLY_CODEGEN, true);
     config::SetRuntimeOption(CFG_RUN_MODE, CFG_RUN_MODE_SIM);
     config::SetCodeGenConfig(KEY_CODEGEN_SUPPORT_TILE_TENSOR, false);
     constexpr int tilingX = 32;
