1ebefb0cdfcca10d928f907c77d002464f978566
Bee.Hujun <Bee.hujun@huawei.com>
2026-01-22 11:52:09 +0800

    feat(codegen): revert support for TileTensor type in main block

--- framework/src/codegen/symbol_mgr/codegen_symbol.h
+++ framework/src/codegen/symbol_mgr/codegen_symbol.h
@@ -55,7 +55,6 @@ inline std::string GetLayoutType(BufferType bufType, int dim, bool isStatic) {
 // Stride<int, int>(64, 1)));
 struct TileTensor {
     bool isStatic;
-    bool isMainBlock;
     int magic; // tensor magic numbuer
     int dim;
     DataType dtype;
@@ -98,7 +97,7 @@ struct TileTensor {
             }
         }
 
-        if ((isStatic || isMainBlock) && bufType != BUF_DDR) {
+        if (isStatic && bufType != BUF_DDR) {
             return "(" + oss.str() + ")";
         }
         params.emplace_back(oss.str());
@@ -160,7 +159,6 @@ struct TileTensorUsing {
     std::vector<int64_t> originShape; // only used for static shape
     std::vector<int64_t> rawShape;
     bool isStatic;
-    bool isMainBlock;
 
     bool operator==(const TileTensorUsing &other) const {
         bool baseCompare = dtype == other.dtype && bufType == other.bufType && rawShape == other.rawShape;
@@ -184,7 +182,7 @@ struct TileTensorUsing {
             ss << GetAddrTypeByOperandType(bufType) << " ";
         }
         ss << DataType2CCEStr(dtype) << ", ";
-        ss << GetLayoutType(bufType, dim, isStatic || isMainBlock);
+        ss << GetLayoutType(bufType, dim, isStatic);
         if (bufType != BUF_DDR) {
             ss << GetLayoutParams();
         }
@@ -197,7 +195,7 @@ private:
     std::string GetLayoutParams() const {
         std::vector<int64_t> params;
         params.reserve(dim * SHAPE_KIND);
-        if (isStatic || isMainBlock) {
+        if (isStatic) {
             params.insert(params.end(), originShape.begin(), originShape.end());
         }
         params.insert(params.end(), rawShape.begin(), rawShape.end());
