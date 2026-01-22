a7689cbb110ad011c69b9784d064818340227a03
duanhaotian <duanhaotian@huawei.com>
2026-01-22 09:37:44 +0800

    fix(machine): Update maxDynamicAssembleOutcastMem on runtime

--- framework/src/machine/utils/dynamic/dev_encode.cpp
+++ framework/src/machine/utils/dynamic/dev_encode.cpp
@@ -961,7 +961,12 @@ struct EncodeDevAscendFunctionInfo {
             cellMatchSize,
             IntVecToStr(ShapeToVector(cellMatchShape)).c_str(),
             IntVecToStr(StrideToVector(cellMatchStride)).c_str());
-        ASSERT(cellMatchStride[0] < MAX_CELLMATCHSSTRIDE) << " Assemble outcast " << tensor->magic << "raw" << tensor->GetRawMagic()
+        if(cellMatchStride[0] > MAX_CELLMATCHSSTRIDE) {
+ 	             ALOG_ERROR_F("Assemble out-cast %d raw %d stitch results in excessive memory consumption "
+ 	                          "Please appropriately configure the view shape and tile shape, and ensure aligned with the input shape. ",
+ 	                          tensor->magic, tensor->GetRawMagic());
+ 	    }
+        ASSERT(cellMatchStride[0] < MAX_CELLMATCHSSTRIDE) << " Assemble outcast " << tensor->magic << " raw " << tensor->GetRawMagic()
  	         <<"stitch results in excessive memory consumption," 
  	         << "Please appropriately configure the view shape and tile shape, and ensure aligned with the input shape."; 
     }
