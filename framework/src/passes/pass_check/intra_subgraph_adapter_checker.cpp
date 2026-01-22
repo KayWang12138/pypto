abe8e3d0b4dafe63b7a42464b703fb005f834680
bytecode <dubingjia1@huawei.com>
2026-01-22 19:19:36 +0800

    graphpartition-postcheck

---
+++ framework/src/passes/pass_check/intra_subgraph_adapter_checker.cpp
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file intra_subgraph_adapter_checker.cpp
+ * \brief
+ */
+
+#include "intra_subgraph_adapter_checker.h"
+#include <queue>
+#include <set>
+#include "passes/pass_log/pass_log.h"
+#define MODULE_NAME "IntraSubgraphAdapterChecker"
+
+namespace npu {
+namespace tile_fwk {
+
+Status IntraSubgraphAdapterChecker::PostSubgraphCheck(const std::vector<std::vector<Operation *>> &subgraphs) {
+    for (auto subgraph : subgraphs) {
+        if (subgraph.empty()) {
+            continue;
+        }
+        int32_t aicCount = 0;
+        int32_t aivCount = 0;
+        int32_t aicMemoryCount = 0;
+        int32_t aivMemoryCount = 0;
+        std::unordered_set<std::shared_ptr<LogicalTensor>> tensorList;
+        int32_t subgraphId = subgraph[0]->GetSubgraphID();
+        for (auto &op : subgraph) {
+            for (auto iTensor : op->GetIOperands()) {
+                if (tensorList.find(iTensor) != tensorList.end()) {
+                    continue;
+                }
+                tensorList.insert(iTensor);
+                if (iTensor->GetMemoryTypeOriginal() == MemoryType::MEM_L1 ||
+                    iTensor->GetMemoryTypeOriginal() == MemoryType::MEM_L0A ||
+                    iTensor->GetMemoryTypeOriginal() == MemoryType::MEM_L0B ||
+                    iTensor->GetMemoryTypeOriginal() == MemoryType::MEM_L0C) {
+                    aicMemoryCount++;
+                    continue;
+                }
+                if (iTensor->GetMemoryTypeOriginal() == MemoryType::MEM_UB) {
+                    aivMemoryCount++;
+                }
+            }
+            if (op->HasAttr(OpAttributeKey::isCube) && op->GetBoolAttribute(OpAttributeKey::isCube)) {
+                aicCount++;
+                continue;
+            }
+            aivCount++;
+        }
+        if (aicCount > 0 && aivCount > 0) {
+            APASS_LOG_ERROR_F(Elements::Operation, "Subgraph %d has both AIV and AIC operation.", subgraphId);
+            return FAILED;
+        }
+        if (aicMemoryCount > 0 && aivMemoryCount > 0) {
+            std::cout << aicMemoryCount << "," << aivMemoryCount << std::endl;
+            APASS_LOG_ERROR_F(Elements::Operation, "Subgraph %d has both ub and l0/l1 memory type tensor.", subgraphId);
+            return FAILED;
+        }
+    }
+    return SUCCESS;
+}
+
+Status IntraSubgraphAdapterChecker::DoPostCheck(Function &function) {
+    APASS_LOG_INFO_F(Elements::Function, "PostCheck for IntraSubgraphAdapter.");
+    std::vector<std::vector<Operation *>> subgraphs(function.GetTotalSubGraphCount());
+    for (auto &op : function.Operations()) {
+        int32_t curSubgraphID = op.GetSubgraphID();
+        if (curSubgraphID == -1) {
+            APASS_LOG_ERROR_F(Elements::Operation,
+                "Operation (opmagic: %d) is not in any subgraph; Please review the error messages generated during the "
+                "processing procedure.%s",
+                op.GetOpMagic(), GetFormatBacktrace(op).c_str());
+            return FAILED;
+        }
+        if (curSubgraphID < 0 || curSubgraphID >= static_cast<int32_t>(function.GetTotalSubGraphCount())) {
+            std::cout << op.GetOpcodeStr() << op.GetOpMagic() << "," << curSubgraphID << ","
+                      << function.GetTotalSubGraphCount() << std::endl;
+            APASS_LOG_ERROR_F(Elements::Operation,
+                "Operation (opmagic: %d) has illegal SubgraphID; Please review the error messages generated during the "
+                "processing procedure.%s",
+                op.GetOpMagic(), GetFormatBacktrace(op).c_str());
+            return FAILED;
+        }
+        subgraphs[curSubgraphID].push_back(&op);
+    }
+    if (PostSubgraphCheck(subgraphs) != SUCCESS) {
+        APASS_LOG_ERROR_F(Elements::Function, "Subgraph post check failed.");
+        return FAILED;
+    }
+
+    return SUCCESS;
+}
+} // namespace tile_fwk
+} // namespace npu
