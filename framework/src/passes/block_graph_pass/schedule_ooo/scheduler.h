c88be369decd73da77fd3f3cb0254aea6ef5c4b4
gggxinmeng <guoxinmeng@huawei.com>
2026-01-22 14:52:58 +0800

    feat(Pass): Change the coreLocation of the new op

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
@@ -79,7 +79,7 @@ const std::map<OpCoreType, int> CORE_INIT_CONFIGS_NON_MIX = {
 const std::unordered_map<OpCoreType, std::pair<OpCoreType, int>> opCoreTypeMap {
     {OpCoreType::AIV, std::make_pair(OpCoreType::AIV, 0)},
     {OpCoreType::AIC, std::make_pair(OpCoreType::AIC, 0)}
-}
+};
 
 struct IssueEntry {
     Operation &tileOp;
