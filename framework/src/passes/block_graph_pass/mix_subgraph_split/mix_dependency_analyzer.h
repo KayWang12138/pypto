658ca468bc7ed6778355b35904955182ff624edc
jiachengtang <tangjiacheng7@huawei.com>
2026-01-22 16:46:31 +0800

    refactor(pass): MixSubgraphSplit refactoring

--- framework/src/passes/block_graph_pass/mix_subgraph_split/mix_dependency_analyzer.h
+++ framework/src/passes/block_graph_pass/mix_subgraph_split/mix_dependency_analyzer.h
@@ -106,6 +106,8 @@ private:
     // 判断是否包含对应tensor
     bool ContainsTensor(const std::vector<SimpleTensorParam> &tensors, const LogicalTensorPtr &tensor) const;
     // 构建可达阵的转置（即反向的可达阵）
+    
+    void Reset();
     std::vector<std::vector<bool>> Transpose(const std::vector<std::vector<bool>> &matrix);
 
     int maxComponent;
