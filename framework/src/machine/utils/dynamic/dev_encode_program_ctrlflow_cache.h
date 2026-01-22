504615b11a66360cf6952ffc43ca1db3fdc75464
duanhaotian <duanhaotian@huawei.com>
2026-01-22 09:37:44 +0800

    fix(machine): Update maxDynamicAssembleOutcastMem on runtime

--- framework/src/machine/utils/dynamic/dev_encode_program_ctrlflow_cache.h
+++ framework/src/machine/utils/dynamic/dev_encode_program_ctrlflow_cache.h
@@ -30,7 +30,7 @@ namespace npu::tile_fwk::dynamic {
 #define INVALID_STITCH_IDX      (static_cast<uint32_t>(-1))
 
 constexpr size_t READY_QUEUE_SIZE = 3UL;
-inline constexpr size_t MAX_CACHED_FUNC_NUM = 128;
+inline constexpr size_t MAX_CACHED_FUNC_NUM = 512;
 
 struct ReadyQueueCache {
     uint32_t coreFunctionCnt;
