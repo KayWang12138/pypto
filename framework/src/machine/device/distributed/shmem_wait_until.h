a4cb2ea2c73fb04e8040375ca55367be6093f01c
luozhonglin <luozhonglin1@huawei.com>
2026-01-22 15:58:02 +0800

    feat(distributed): CodeCheck

--- framework/src/machine/device/distributed/shmem_wait_until.h
+++ framework/src/machine/device/distributed/shmem_wait_until.h
@@ -35,12 +35,10 @@ struct SignalTileOp {
 
     SignalTileOp* next{nullptr};
     uint64_t taskId_{0};
-    int32_t* addr_;
+    int32_t* addr_{nullptr};
     int32_t expectedSum_{0};
     bool resetSignal_{false};
-    uint64_t taskStatTime_{0};
-    uint64_t taskEndTime_{0};
-    Metrics* dumpData{0};
+    TaskStat* dumpData{nullptr};
 };
 
 class HashMap {
@@ -152,7 +150,7 @@ public:
                 if (ret != dynamic::DEVICE_MACHINE_OK) {
                     return ret;
                 }
-                task->taskEndTime_ = dynamic::GetCycles();
+                task->dumpData->execEnd = dynamic::GetCycles();
             }
         }
         return dynamic::DEVICE_MACHINE_OK;
@@ -179,7 +177,8 @@ public:
             DEV_ERROR("There is no this taskId: %lu", taskId);
             return dynamic::DEVICE_MACHINE_ERROR;
         }
-        task->taskStatTime_ = dynamic::GetCycles();
+        task->dumpData->taskId = static_cast<int32_t>(taskId);
+        task->dumpData->execStart = dynamic::GetCycles();
         return runingTaskQueue_.Enqueue(task);
     }
 
