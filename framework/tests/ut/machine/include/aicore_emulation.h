7b73ae334fea8ffb45763b9c0e34ad003de65d25
yinjie27 <yinjie27@huawei.com>
2026-01-23 19:59:57 +0800

    feat(machine): Make machine modularity and unit test it

---
+++ framework/tests/ut/machine/include/aicore_emulation.h
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file aicore_emulation.h
+ * \brief
+ */
+
+#ifndef AICORE_EMULATION_H
+#define AICORE_EMULATION_H
+
+#include <chrono>
+#include <memory>
+
+#include "tilefwk/aikernel_define.h"
+
+namespace npu::tile_fwk::machine {
+
+class AicoreEmulationBase {
+public:
+    uint64_t GetSysCnt() {
+        auto now = std::chrono::high_resolution_clock::now();
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
+        return ns;
+    }
+
+    virtual ~AicoreEmulationBase() = default;
+
+    virtual int64_t GetCoreId() {
+        return 0;
+    }
+
+    virtual int GetBlockIdx() {
+        return 0;
+    }
+
+    virtual void SetCond(uint64_t cond) {
+        UNUSED(cond);
+    }
+
+    virtual uint64_t GetDataMainBase() {
+        return 0;
+    }
+};
+
+class AicoreEmulationManager {
+public:
+    static AicoreEmulationManager &GetInstance();
+
+    AicoreEmulationManager() {
+        base_ = std::make_shared<AicoreEmulationBase>();
+    }
+
+    void RegisterAicoreEmulation(std::shared_ptr<AicoreEmulationBase> base) { base_ = base; }
+
+    std::shared_ptr<AicoreEmulationBase> GetEmulation() { return base_; }
+private:
+    std::shared_ptr<AicoreEmulationBase> base_;
+};
+
+}
+
+#define dcci(...)
+#define dsb(...)
+#define set_flag(...)
+#define wait_flag(...)
+#define set_mask_norm(...)
+
+uint64_t get_sys_cnt() {
+    auto cnt = npu::tile_fwk::machine::AicoreEmulationManager::GetInstance().GetEmulation()->GetSysCnt();
+    return cnt;
+}
+
+int64_t get_coreid() {
+    auto coreid = npu::tile_fwk::machine::AicoreEmulationManager::GetInstance().GetEmulation()->GetCoreId();
+    return coreid;
+}
+
+int get_block_idx() {
+    auto blockIdx = npu::tile_fwk::machine::AicoreEmulationManager::GetInstance().GetEmulation()->GetBlockIdx();
+    return blockIdx;
+}
+
+void set_cond(uint64_t cond) {
+    npu::tile_fwk::machine::AicoreEmulationManager::GetInstance().GetEmulation()->SetCond(cond);
+}
+
+uint64_t GetDataMainBase() {
+    auto mainBase = npu::tile_fwk::machine::AicoreEmulationManager::GetInstance().GetEmulation()->GetDataMainBase();
+    return mainBase;
+}
+
+#include "tilefwk/aicore_entry.h"
+
+#endif
