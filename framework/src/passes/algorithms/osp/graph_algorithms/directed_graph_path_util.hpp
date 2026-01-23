3c4a772e6c25b5edcf9c4f2ee7a9f2f6f16d024e
raphaelsteiner <raphael.steiner@huawei.com>
2026-01-23 14:52:23 +0000

    graph algorithms

---
+++ framework/src/passes/algorithms/osp/graph_algorithms/directed_graph_path_util.hpp
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file directed_graph_path_util.hpp
+ * \brief
+ */
+
+#ifndef PASS_OSP_DIRECTED_GRAPH_PATH_UTIL_H
+#define PASS_OSP_DIRECTED_GRAPH_PATH_UTIL_H
+
+#include <map>
+#include <queue>
+#include <set>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "passes/algorithms/osp/graph_algorithms/directed_graph_top_sort.hpp"
+
+namespace npu::tile_fwk {
+namespace osp {
+
+template <typename GraphT, typename T = unsigned>
+std::vector<T> GetBottomNodeDistance(const GraphT &graph) {
+    static_assert(std::is_integral_v<T>, "T must be of integral type");
+
+    std::vector<T> bottomDistance(graph.NumVertices(), 0);
+
+    const auto topOrder = GetTopOrder(graph);
+    for (std::size_t i = topOrder.size() - 1; i < topOrder.size(); i--) {
+        T maxTemp = 0;
+        for (const auto &j : graph.Children(topOrder[i])) {
+            maxTemp = std::max(maxTemp, bottomDistance[j]);
+        }
+        bottomDistance[topOrder[i]] = ++maxTemp;
+    }
+    return bottomDistance;
+}
+
+template <typename GraphT, typename T = unsigned>
+std::vector<T> GetTopNodeDistance(const GraphT &graph) {
+    static_assert(std::is_integral_v<T>, "T must be of integral type");
+
+    std::vector<T> topDistance(graph.NumVertices(), 0);
+
+    for (const auto &vertex : BfsTopSortView(graph)) {
+        T maxTemp = 0;
+        for (const auto &j : graph.Parents(vertex)) {
+            maxTemp = std::max(maxTemp, topDistance[j]);
+        }
+        topDistance[vertex] = ++maxTemp;
+    }
+    return topDistance;
+}
+
+}    // namespace osp
+}    // namespace npu::tile_fwk
