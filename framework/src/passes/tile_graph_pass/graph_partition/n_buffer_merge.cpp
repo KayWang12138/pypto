b7e8775e5bb6026958865b21a255bc13f4512289
zhangxiangjie <zhangxiangjie5@huawei.com>
2026-01-23 16:24:19 +0800

    代码优化

--- framework/src/passes/tile_graph_pass/graph_partition/n_buffer_merge.cpp
+++ framework/src/passes/tile_graph_pass/graph_partition/n_buffer_merge.cpp
@@ -228,8 +228,8 @@ Status NBufferMerge::Init(Function &func) {
     return SUCCESS;
 }
 
-std::map<int, size_t> NBufferMerge::GetIsoColorMergeNum(const std::map<uint64_t, std::vector<int>> &hashMap) const {
-    std::map<int, size_t> hashCoreNum;
+std::map<uint64_t, size_t> NBufferMerge::GetIsoColorMergeNum(const std::map<uint64_t, std::vector<int>> &hashMap) const {
+    std::map<uint64_t, size_t> hashCoreNum;
     for (const auto& entry : hashMap) {
         if (entry.first == 0 || entry.second.empty()) {
             continue;
--- framework/src/passes/tile_graph_pass/graph_partition/n_buffer_merge.h
+++ framework/src/passes/tile_graph_pass/graph_partition/n_buffer_merge.h
@@ -44,7 +44,7 @@ private:
     Status CheckAndFixColorOrder(OperationsViewer &opOriList,
                                int &color1, std::vector<int> &colorCycles1,
                                std::vector<std::vector<int>> &colorNode1);
-    std::map<int, size_t> GetIsoColorMergeNum(const std::map<uint64_t, std::vector<int>> &hashMap) const;
+    std::map<uint64_t, size_t> GetIsoColorMergeNum(const std::map<uint64_t, std::vector<int>> &hashMap) const;
     std::vector<std::vector<int>> SortColorWithInput(std::vector<int> &colorValues) const;
     Status MergeProcess(const OperationsViewer &opOriList,
                         std::map<uint64_t, std::vector<int>> &hashMap,
