0c902218bc3a0d9a7b6fbfa1e200574d3c925ac7
zhangxiangjie <zhangxiangjie5@huawei.com>
2026-01-23 09:38:28 +0800

    优化代码

--- framework/src/passes/block_graph_pass/insert_sync.cpp
+++ framework/src/passes/block_graph_pass/insert_sync.cpp
@@ -698,7 +698,7 @@ Status PipeSync::PopFromQueue(IssueQueue &issueQ, std::vector<size_t> &poped, bo
 Status PipeSync::InjectWaitFlag(Function &function, size_t idx, std::vector<IndexOp> &syncedOpLog) {
     PipeCore currPipe = depOps_[idx].selfPipeCore;
     // serch the waitpipe of current op
-    uint64_t waitIdx;
+    uint64_t waitIdx = idx == 0 ? 0 : idx * SEQUENCE_IDX - HALF_SEQUENCE_IDX;
     if (idx == 0) {
         waitIdx = 0;
     }
