cec7a5e747c96346e73fc57930a5fe64ccfe2cde
yinjie27 <yinjie27@huawei.com>
2026-01-23 19:59:57 +0800

    feat(machine): Make machine modularity and unit test it

--- framework/src/interface/machine/device/tilefwk/core_func_data.h
+++ framework/src/interface/machine/device/tilefwk/core_func_data.h
@@ -19,7 +19,7 @@
 #define CORE_FUNC_DATA_H
 
 #include <cstdint>
-#include "tilefwk/aicore_data.h"
+#include "tilefwk/aikernel_data.h"
 
 inline constexpr uint32_t DIST_COMM_GROUP_NUM = 8;
 inline constexpr size_t MAX_CACHED_FUNC_NUM = 128;
@@ -162,32 +162,6 @@ struct BaseArgs {
 #pragma pack ()
 
 using predcount_t = uint16_t;
-
-struct DynFuncBin {
-    uint32_t coreType;
-    uint32_t psgId;
-    uint64_t funcHash;
-    int32_t wrapVecId {-1};
-    uint32_t mixResourceType {0};
-};
-
-struct DynFuncHeader {
-    uint64_t seqNo;
-    uint32_t funcNum;
-    uint32_t funcSize;
-    __gm__ DynFuncBin *cceBinary;
-
-    uint64_t GetIndex() {
-        return seqNo;
-    }
-
-    inline DynFuncData &At(int index) {
-        return (reinterpret_cast<DynFuncData *>(this + 1))[index];
-    }
-    inline uint32_t Size() {
-        return funcNum;
-    }
-};
 
 #pragma pack ()
 
