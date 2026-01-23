fb9cd06767ed3339f645029885381e4c0282d4b1
tsungl4 <lidongsheng4@huawei.com>
2026-01-23 14:32:34 +0800

    perf(python): Change pypto enum from Enum to IntEnum

--- python/src/bindings/enum.cpp
+++ python/src/bindings/enum.cpp
@@ -12,137 +12,149 @@
  * \file enum.cpp
  * \brief
  */
-
 #include "pybind_common.h"
 
 using namespace npu::tile_fwk;
 
 namespace pypto {
+
+struct EnumItem {
+    const char *name;
+    int value;
+    template <typename T>
+    EnumItem(const char *tname, T tvalue) : name(tname), value((int)tvalue) {}
+};
+
+static void bind_enum(py::object &m, const char *name, const std::initializer_list<EnumItem> &input) {
+    py::dict items;
+    for (auto &item : input) {
+        items[item.name] = item.value;
+    }
+    auto IntEnum = py::module::import("enum").attr("IntEnum");
+    m.attr(name) = IntEnum(name, items);
+}
+
 void bind_enum(py::module &m){
-    py::enum_<DataType>(m, "DataType")
-        .value("DT_INT4", DataType::DT_INT4)
-        .value("DT_INT8", DataType::DT_INT8)
-        .value("DT_INT16", DataType::DT_INT16)
-        .value("DT_INT32", DataType::DT_INT32)
-        .value("DT_INT64", DataType::DT_INT64)
-        .value("DT_FP8", DataType::DT_FP8)
-        .value("DT_FP16", DataType::DT_FP16)
-        .value("DT_FP32", DataType::DT_FP32)
-        .value("DT_BF16", DataType::DT_BF16)
-        .value("DT_HF4", DataType::DT_HF4)
-        .value("DT_HF8", DataType::DT_HF8)
-        .value("DT_FP8E4M3", DataType::DT_FP8E4M3)
-        .value("DT_FP8E5M2", DataType::DT_FP8E5M2)
-        .value("DT_UINT8", DataType::DT_UINT8)
-        .value("DT_UINT16", DataType::DT_UINT16)
-        .value("DT_UINT32", DataType::DT_UINT32)
-        .value("DT_UINT64", DataType::DT_UINT64)
-        .value("DT_BOOL", DataType::DT_BOOL)
-        .value("DT_DOUBLE", DataType::DT_DOUBLE)
-        .value("DT_BOTTOM", DataType::DT_BOTTOM)
-        .export_values();
+    bind_enum(m, "DataType", {
+        {"DT_INT4", DataType::DT_INT4},
+        {"DT_INT8", DataType::DT_INT8},
+        {"DT_INT16", DataType::DT_INT16},
+        {"DT_INT32", DataType::DT_INT32},
+        {"DT_INT64", DataType::DT_INT64},
+        {"DT_FP8", DataType::DT_FP8},
+        {"DT_FP16", DataType::DT_FP16},
+        {"DT_FP32", DataType::DT_FP32},
+        {"DT_BF16", DataType::DT_BF16},
+        {"DT_HF4", DataType::DT_HF4},
+        {"DT_HF8", DataType::DT_HF8},
+        {"DT_FP8E4M3", DataType::DT_FP8E4M3},
+        {"DT_FP8E5M2", DataType::DT_FP8E5M2},
+        {"DT_UINT8", DataType::DT_UINT8},
+        {"DT_UINT16", DataType::DT_UINT16},
+        {"DT_UINT32", DataType::DT_UINT32},
+        {"DT_UINT64", DataType::DT_UINT64},
+        {"DT_BOOL", DataType::DT_BOOL},
+        {"DT_DOUBLE", DataType::DT_DOUBLE},
+        {"DT_BOTTOM", DataType::DT_BOTTOM},
+    });
 
-    py::enum_<NodeType>(m, "NodeType")
-        .value("LOCAL", NodeType::LOCAL)
-        .value("INCAST", NodeType::INCAST)
-        .value("OUTCAST", NodeType::OUTCAST)
-        .export_values();
+    bind_enum(m, "NodeType", {
+        {"LOCAL", NodeType::LOCAL},
+        {"INCAST", NodeType::INCAST},
+        {"OUTCAST", NodeType::OUTCAST},
+    });
 
-    py::enum_<TileOpFormat>(m, "TileOpFormat")
-        .value("TILEOP_ND", TileOpFormat::TILEOP_ND)
-        .value("TILEOP_NZ", TileOpFormat::TILEOP_NZ)
-        .value("TILEOP_FORMAT_NUM", TileOpFormat::TILEOP_FORMAT_NUM)
-        .export_values();
+    bind_enum(m, "TileOpFormat", {
+        {"TILEOP_ND", TileOpFormat::TILEOP_ND},
+        {"TILEOP_NZ", TileOpFormat::TILEOP_NZ},
+        {"TILEOP_FORMAT_NUM", TileOpFormat::TILEOP_FORMAT_NUM},
+    });
 
-    py::enum_<CachePolicy>(m, "CachePolicy")
-        .value("NONE_CACHEABLE", CachePolicy::NONE_CACHEABLE)
-        .value("MAX_NUM", CachePolicy::MAX_NUM)
-        .export_values();
+    bind_enum(m, "CachePolicy", {
+        {"NONE_CACHEABLE", CachePolicy::NONE_CACHEABLE},
+        {"MAX_NUM", CachePolicy::MAX_NUM},
+    });
 
-    py::enum_<ReduceMode>(m, "ReduceMode")
-        .value("ATOMIC_ADD", ReduceMode::ATOMIC_ADD)
-        .export_values();
+    bind_enum(m, "ReduceMode", {
+        {"ATOMIC_ADD", ReduceMode::ATOMIC_ADD},
+    });
 
-    py::enum_<ScatterMode>(m, "ScatterMode")
-        .value("NONE", ScatterMode::NONE)
-        .value("ADD", ScatterMode::ADD)
-        .value("MULTIPLY", ScatterMode::MULTIPLY)
-        .export_values();
+    bind_enum(m, "ScatterMode", {
+        {"NONE", ScatterMode::NONE},
+        {"ADD", ScatterMode::ADD},
+        {"MULTIPLY", ScatterMode::MULTIPLY},
+    });
 
-    py::enum_<MemoryType>(m, "MemoryType")
-        .value("MEM_UB", MemoryType::MEM_UB)
-        .value("MEM_L1", MemoryType::MEM_L1)
-        .value("MEM_L0A", MemoryType::MEM_L0A)
-        .value("MEM_L0B", MemoryType::MEM_L0B)
-        .value("MEM_L0C", MemoryType::MEM_L0C)
-        .value("MEM_L2", MemoryType::MEM_L2)
-        .value("MEM_L3", MemoryType::MEM_L3)
-        .value("MEM_DEVICE_DDR", MemoryType::MEM_DEVICE_DDR)
-        .value("MEM_HOST1", MemoryType::MEM_HOST1)
-        .value("MEM_FAR1", MemoryType::MEM_FAR1)
-        .value("MEM_FAR2", MemoryType::MEM_FAR2)
-        .value("MEM_UNKNOWN", MemoryType::MEM_UNKNOWN)
-        .export_values();
+    bind_enum(m, "MemoryType", {
+        {"MEM_UB", MemoryType::MEM_UB},
+        {"MEM_L1", MemoryType::MEM_L1},
+        {"MEM_L0A", MemoryType::MEM_L0A},
+        {"MEM_L0B", MemoryType::MEM_L0B},
+        {"MEM_L0C", MemoryType::MEM_L0C},
+        {"MEM_L2", MemoryType::MEM_L2},
+        {"MEM_L3", MemoryType::MEM_L3},
+        {"MEM_DEVICE_DDR", MemoryType::MEM_DEVICE_DDR},
+        {"MEM_HOST1", MemoryType::MEM_HOST1},
+        {"MEM_FAR1", MemoryType::MEM_FAR1},
+        {"MEM_FAR2", MemoryType::MEM_FAR2},
+        {"MEM_UNKNOWN", MemoryType::MEM_UNKNOWN},
+    });
 
-    py::enum_<FunctionType>(m, "FunctionType")
-        .value("EAGER", FunctionType::EAGER)
-        .value("STATIC", FunctionType::STATIC)
-        .value("DYNAMIC", FunctionType::DYNAMIC)
-        .value("DYNAMIC_LOOP", FunctionType::DYNAMIC_LOOP)
-        .value("DYNAMIC_LOOP_PATH", FunctionType::DYNAMIC_LOOP_PATH)
-        .value("INVALID", FunctionType::INVALID)
-        .value("MAX", FunctionType::MAX)
-        .export_values();
+    bind_enum(m, "FunctionType", {
+        {"STATIC", FunctionType::STATIC},
+        {"DYNAMIC", FunctionType::DYNAMIC},
+        {"DYNAMIC_LOOP", FunctionType::DYNAMIC_LOOP},
+        {"DYNAMIC_LOOP_PATH", FunctionType::DYNAMIC_LOOP_PATH},
+    });
 
-    py::enum_<GraphType>(m, "GraphType")
-        .value("TENSOR_GRAPH", GraphType::TENSOR_GRAPH)
-        .value("TILE_GRAPH", GraphType::TILE_GRAPH)
-        .value("EXECUTE_GRAPH", GraphType::EXECUTE_GRAPH)
-        .value("BLOCK_GRAPH", GraphType::BLOCK_GRAPH)
-        .value("LEAF_VF_GRAPH", GraphType::LEAF_VF_GRAPH)
-        .value("INVALID", GraphType::INVALID)
-        .export_values();
+    bind_enum(m, "GraphType", {
+        {"TENSOR_GRAPH", GraphType::TENSOR_GRAPH},
+        {"TILE_GRAPH", GraphType::TILE_GRAPH},
+        {"EXECUTE_GRAPH", GraphType::EXECUTE_GRAPH},
+        {"BLOCK_GRAPH", GraphType::BLOCK_GRAPH},
+        {"LEAF_VF_GRAPH", GraphType::LEAF_VF_GRAPH},
+    });
 
-    py::enum_<CastMode>(m, "CastMode")
-        .value("CAST_NONE", CastMode::CAST_NONE)
-        .value("CAST_RINT", CastMode::CAST_RINT)
-        .value("CAST_ROUND", CastMode::CAST_ROUND)
-        .value("CAST_FLOOR", CastMode::CAST_FLOOR)
-        .value("CAST_CEIL", CastMode::CAST_CEIL)
-        .value("CAST_TRUNC", CastMode::CAST_TRUNC)
-        .value("CAST_ODD", CastMode::CAST_ODD)
-        .export_values();
+    bind_enum(m, "CastMode", {
+        {"CAST_NONE", CastMode::CAST_NONE},
+        {"CAST_RINT", CastMode::CAST_RINT},
+        {"CAST_ROUND", CastMode::CAST_ROUND},
+        {"CAST_FLOOR", CastMode::CAST_FLOOR},
+        {"CAST_CEIL", CastMode::CAST_CEIL},
+        {"CAST_TRUNC", CastMode::CAST_TRUNC},
+        {"CAST_ODD", CastMode::CAST_ODD},
+    });
 
-    py::enum_<TileType>(m, "TileType")
-        .value("VEC", TileType::VEC)
-        .value("CUBE", TileType::CUBE)
-        .value("DIST", TileType::DIST)
-        .value("MAX", TileType::MAX)
-        .export_values();
+    bind_enum(m, "TileType", {
+        {"VEC", TileType::VEC},
+        {"CUBE", TileType::CUBE},
+        {"DIST", TileType::DIST},
+        {"MAX", TileType::MAX},
+    });
 
-    py::enum_<OpType>(m, "OpType")
-        .value("EQ", OpType::EQ)
-        .value("NE", OpType::NE)
-        .value("LT", OpType::LT)
-        .value("LE", OpType::LE)
-        .value("GT", OpType::GT)
-        .value("GE", OpType::GE)
-        .export_values();
+    bind_enum(m, "OpType", {
+        {"EQ", OpType::EQ},
+        {"NE", OpType::NE},
+        {"LT", OpType::LT},
+        {"LE", OpType::LE},
+        {"GT", OpType::GT},
+        {"GE", OpType::GE},
+    });
 
-    py::enum_<OutType>(m, "OutType")
-        .value("BOOL", OutType::BOOL)
-        .value("BIT", OutType::BIT)
-        .export_values();
+    bind_enum(m, "OutType", {
+        {"BOOL", OutType::BOOL},
+        {"BIT", OutType::BIT},
+    });
 
-    py::enum_<Matrix::ReLuType>(m, "ReLuType")
-        .value("NO_RELU", Matrix::ReLuType::NoReLu)
-        .value("RELU", Matrix::ReLuType::ReLu)
-        .export_values();
+    bind_enum(m, "ReLuType", {
+        {"NO_RELU", Matrix::ReLuType::NoReLu},
+        {"RELU", Matrix::ReLuType::ReLu},
+    });
 
-    py::enum_<LogBaseType>(m, "LogBaseType")
-        .value("LOG_E", LogBaseType::LOG_E)
-        .value("LOG_2", LogBaseType::LOG_2)
-        .value("LOG_10", LogBaseType::LOG_10)
-        .export_values();
+    bind_enum(m, "LogBaseType", {
+        {"LOG_E", LogBaseType::LOG_E},
+        {"LOG_2", LogBaseType::LOG_2},
+        {"LOG_10", LogBaseType::LOG_10},
+    });
 }
 }
