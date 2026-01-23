6d63a979695b0d3434802cbe60bf311fb48c4a5d
zhaosenqiu <zhaosenqiu@huawei.com>
2026-01-23 15:59:32 +0800

    fix(pass): Set tensor after assemble to DDR when from L0C and not to L1.

--- framework/src/passes/tile_graph_pass/data_path/assign_memory_type.cpp
+++ framework/src/passes/tile_graph_pass/data_path/assign_memory_type.cpp
@@ -499,7 +499,13 @@ void AssignMemoryType::ProcesSmallTileToLargeTile(Function &function) {
         auto oOperand = op.GetOOperands().front();
         auto iOperand = op.GetIOperands().front();
         if(iOperand->GetMemoryTypeOriginal() == MEM_L0C) {
-            if (!IsDimMultiple(oOperand->GetShape(), iOperand->GetShape())){
+            bool isToL1 = true;
+            auto toBeMap = inserter.GetMemoryTypeFromTensorTobeMap(oOperand);
+            for (const auto &[_, toBeType] : toBeMap) {
+                isToL1 = false;
+                break;
+            }
+            if (!isToL1 || !IsDimMultiple(oOperand->GetShape(), iOperand->GetShape())){
                 oOperand->SetMemoryTypeOriginal(MEM_DEVICE_DDR, true);
             }
         }
