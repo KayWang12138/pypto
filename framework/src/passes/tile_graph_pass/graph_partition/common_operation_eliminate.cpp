42ddba2371e19caa7f329071066bb4ceee2ce8b8
xujiayi1999 <xujiayi28@huawei.com>
2026-01-24 16:43:42 +0800

    fix(pass): Fix COE hash compute and sort algorithm

--- framework/src/passes/tile_graph_pass/graph_partition/common_operation_eliminate.cpp
+++ framework/src/passes/tile_graph_pass/graph_partition/common_operation_eliminate.cpp
@@ -258,22 +258,21 @@ bool CommonOperationEliminate::OpAlreadyExist(const std::pair<LogicalTensor*, st
         if (cur == nullptr) continue;
         std::shared_ptr<LogicalTensor> old_ptr(oldtensors, [](LogicalTensor*){});
         std::shared_ptr<LogicalTensor> new_ptr(newtensors, [](LogicalTensor*){});
-        if (cur->GetOpAttribute() == nullptr) {
-            continue;
-        }
         cur->ReplaceInput(new_ptr, old_ptr);
         auto attptr = cur->GetOpAttribute().get();
-        if (auto viewOpAttribute = dynamic_cast<ViewOpAttribute*>(attptr)) {
+        if (cur->GetOpcode() == Opcode::OP_VIEW) {
+            if (auto viewOpAttribute = dynamic_cast<ViewOpAttribute*>(attptr)) {
             // VIEW操作的offset要相应被修改。
-            UpdateView(viewOpAttribute, old_ptr, new_ptr);
-            continue;
-        }
-        if (auto copyOpAttribute = dynamic_cast<CopyOpAttribute*>(attptr)) {
+                UpdateView(viewOpAttribute, old_ptr, new_ptr);
+                continue;
+            }
+        } else if (cur->GetOpcode() == Opcode::OP_COPY_IN) { 
+            if (auto copyOpAttribute = dynamic_cast<CopyOpAttribute*>(attptr)) {
             // CopyIn操作的offset要相应被修改。
-            UpdateCopy(copyOpAttribute, old_ptr, new_ptr);
-            continue;
+                UpdateCopy(copyOpAttribute, old_ptr, new_ptr);
+                continue;
+            }
         }
-
     }
     oldtensors->GetConsumers().clear();
     ALOG_DEBUG_F("In CommonOperationEliminate, Tensor %d producergroup is marked as redundant.", oldtensors->GetMagic());
