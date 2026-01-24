bb9cb2e09bb9ec875e4d087296849f66f6bec0f1
丁波 <dingbo24@h-partners.com>
2026-01-24 15:14:06 +0800

    fix(interpreter): Update precision compare method

--- framework/src/interface/interpreter/function.cpp
+++ framework/src/interface/interpreter/function.cpp
@@ -373,6 +373,17 @@ void FunctionInterpreter::DumpPassTensorDiff(
     if (captureExecution->GetFrameList().size() != captureGolden->GetFrameList().size())
         return;
 
+    std::vector<double> tolerance = config::GetVerifyOption<std::vector<double>>(KEY_PASS_VERIFY_ERROR_TOL);
+    float rtol = 0;
+    float atol = 0;
+    if (tolerance.size() < 2) {
+        rtol = static_cast<float>(1e-3);
+        atol = static_cast<float>(1e-3);
+    } else {
+        rtol = static_cast<float>(tolerance[0]);
+        atol = static_cast<float>(tolerance[1]);
+    }
+
     std::string dumpStyleFilePath = execDumpDir + "/entry_" + std::to_string(captureIndex) + ".css";
     execDumpStyleFile = fopen(dumpStyleFilePath.c_str(), "w");
     for (size_t idx = 0; idx < captureGolden->GetFrameList().size(); idx++) {
@@ -398,7 +409,7 @@ void FunctionInterpreter::DumpPassTensorDiff(
             if (!dataViewExecution || !dataViewGolden) {
                 continue;
             }
-            auto compare = FlowVerifier::VerifyResult(dataViewGolden, dataViewExecution, static_cast<float>(1e-5));
+            auto compare = FlowVerifier::VerifyResult(dataViewGolden, dataViewExecution, rtol, atol);
             std::string tensorId = GetDumpTensorId(frameExecution, tensor);
             if (compare.Check()) {
                 fprintf(execDumpStyleFile, "#%s { background-color: LightGreen; }\n", tensorId.c_str());
