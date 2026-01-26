fef708cdd314a070e1dff5354c6715f6117c35dc
yinjie27 <yinjie27@huawei.com>
2026-01-26 15:46:38 +0800

    feat(machine): Export machine ut interface for control

--- framework/src/machine/runtime/device_launcher.cpp
+++ framework/src/machine/runtime/device_launcher.cpp
@@ -229,7 +229,7 @@ int DeviceLauncher::DeviceRunOnce(Function *function, DevControlFlowCache* hostC
     if (hostCtrlCache) {
         devCtrlCache = devMemory.CopyToDev(reinterpret_cast<uint8_t *>(hostCtrlCache), hostCtrlCache->allCacheSize, nullptr);
     }
-    
+
     int rc = DeviceLaunchOnceWithDeviceTensorData(function, inputDeviceDataList, outputDeviceDataList,
         aicpuStream, aicoreStream, true, nullptr, reinterpret_cast<DevControlFlowCache*>(devCtrlCache), config);
     CopyFromDev(DeviceMemoryUtils(), outputDataList);
@@ -325,6 +325,7 @@ int ExportedOperatorDeviceLaunchOnceWithDeviceTensorData(
     (void)aicpuStream;
     (void)aicoreStream;
     (void)streamSynchronize;
+    (void)devCtrlCache;
     (void)config;
     return 0;
 #endif
