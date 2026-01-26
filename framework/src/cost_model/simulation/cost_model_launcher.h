b60b18590f225f9a2da41a533c9bd53d9a7a5629
yinjie27 <yinjie27@huawei.com>
2026-01-26 22:49:50 +0800

    feat(machine): Multiple metadata cache

--- framework/src/cost_model/simulation/cost_model_launcher.h
+++ framework/src/cost_model/simulation/cost_model_launcher.h
@@ -385,7 +385,7 @@ private:
         devProg->devArgs.startArgsAddr = (uint64_t)pv_->AllocWorkspaceDev(DEV_ARGS_SIZE);
         devProg->workspaceSize = devProg->memBudget.Total();
         devProg->devArgs.scheCpuNum = 1;
-        AssignMetaAddr(kArgs, devMem, devProg, nullptr);
+        AssignMetaAddr(devMem, kArgs, devProg, nullptr);
         for (auto &input: inputs) {
             if (input)
                 input->SetDevPtr(nullptr);
