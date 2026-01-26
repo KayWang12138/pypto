042f9dfb955a232355987df7ef8429290770d37e
hwwangmingjun <wangmingjun13@huawei.com>
2026-01-26 17:59:06 +0800

    modify review comments

--- framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
@@ -264,7 +264,7 @@ Status ProcessReshape(Function &function, Operation *&operation) {
         }
         consumer->ReplaceInput(dst, oOperand);
         auto &newReshapeOp = function.AddRawOperation(Opcode::OP_RESHAPE, {iOperand}, {dst});
-        const std::shared_ptr<OpAttribute> oriReshapeAttr = operation->GetOpAttribute()->clone();
+        const std::shared_ptr<OpAttribute> oriReshapeAttr = operation->GetOpAttribute()->Clone();
         if (oriReshapeAttr != nullptr) {
             newReshapeOp.SetOpAttribute(oriReshapeAttr);
         }
