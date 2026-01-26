00eac5109a6c640e5827e01a7da26d1cd91ceb5a
anyangle <anyangle@huawei.com>
2026-01-26 17:42:27 +0800

    code review 2

--- framework/src/machine/runtime/pmu_common.cpp
+++ framework/src/machine/runtime/pmu_common.cpp
@@ -1,11 +1,16 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file pmu_common.cpp
+ * \brief
  */
 
 #include "machine/runtime/pmu_common.h"
@@ -75,7 +80,9 @@ void SetPmuEventTypeDAV3510(int32_t profPmuType, std::vector<int64_t> &pmuEvtTyp
 }
 }
 
-void GetPmuEventType(const ArchInfo& archInfo, std::vector<int64_t>& pmuEvtType) {
+void PmuCommon::InitPmuEventType(const ArchInfo& archInfo, std::vector<int64_t>& pmuEvtType) {
+    size_t pmuEvtTypeSize = archInfo == ArchInfo::DAV_2201 ? PMU_EVENT_TYPE_MAX_DAV2201 : PMU_EVENT_TYPE_MAX_DAV3510;
+    pmuEvtType.resize(pmuEvtTypeSize, 0x0);
     // 获取pmu事件类型环境变量获取方式
     std::string eventTypeStr = GetEnvVar("PROF_PMU_EVENT_TYPE");
     if (eventTypeStr.empty()) {
--- framework/src/machine/runtime/pmu_common.h
+++ framework/src/machine/runtime/pmu_common.h
@@ -1,11 +1,16 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file pmu_common.h
+ * \brief
  */
 
 #ifndef SRC_MACHINE_RUNTIME_PMU_COMMON_H
@@ -30,7 +35,10 @@ constexpr int32_t L2_CACHE = 8;
 constexpr int PMU_EVENT_TYPE_MAX_DAV2201 = 8;
 constexpr int PMU_EVENT_TYPE_MAX_DAV3510 = 10;
 
-void GetPmuEventType(const ArchInfo& archInfo, std::vector<int64_t>& pmuEvtType);
+class PmuCommon {
+public:
+    static void InitPmuEventType(const ArchInfo& archInfo, std::vector<int64_t>& pmuEvtType);
+};
 
 } // namespace npu::tile_fwk
 
