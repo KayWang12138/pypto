d8c773723c2a3f797225bf3ec2bcf80b2b156cbb
heyufeng <heyufeng11@hisilicon.com>
2026-01-26 17:11:10 +0800

    update 0126

--- framework/src/interface/tileop/vector/expand.h
+++ framework/src/interface/tileop/vector/expand.h
@@ -81,19 +81,17 @@ TILEOP void TExpand(T0 dst, T1 src) {
         for (size_t n0Index = 0; n0Index < dstShape0; ++n0Index) {
             for (size_t n1Index = 0; n1Index < dstShape1; ++n1Index) {
                 for (size_t n2Index = 0; n2Index < dstShape2; ++n2Index) {
+                    using dstTileDefine =
+ 	                    pto::Tile<pto::TileType::Vec, DstDtype, dstTileH, dstTileW, pto::BLayout::RowMajor, -1, -1>;
+ 	                using srcTileDefine =
+ 	                    pto::Tile<pto::TileType::Vec, SrcDtype, srcTileH, srcTileW, pto::BLayout::RowMajor, -1, -1>;
+ 	                dstTileDefine dstTile(dstShape3, dstShape4);
+ 	                srcTileDefine srcTile(srcShape3, srcShape4);
                     auto dstOffset = n0Index * dstStride0 + n1Index * dstStride1 + n2Index * dstStride2;
                     auto srcOffset = n0Index * srcStride0 + n1Index * srcStride1 + n2Index * srcStride2;
-                    using dstTileDefine =
-                        pto::Tile<pto::TileType::Vec, DstDtype, 1, dstTileW, pto::BLayout::RowMajor, -1, -1>;
-                    using srcTileDefine =
-                        pto::Tile<pto::TileType::Vec, SrcDtype, 1, srcTileW, pto::BLayout::RowMajor, -1, -1>;
-                    dstTileDefine dstTile(1, dstShape4);
-                    srcTileDefine srcTile(1, srcShape4);
+                    pto::TASSIGN(dstTile, (uint64_t)(dst.GetAddr() + dstOffset * typeSize));
                     pto::TASSIGN(srcTile, (uint64_t)(src.GetAddr() + srcOffset * typeSize));
-                    for (unsigned i = 0; i < dstShape3; i++) {
-                        pto::TASSIGN(dstTile, (uint64_t)(dst.GetAddr() + (dstOffset + i * dstTileW) * typeSize));
-                        [[pto::last_use(n1, n2)]]pto::TMOV(dstTile, srcTile);
-                    }
+                    [[pto::last_use(n1, n2)]]pto::TCOLEXPAND(dstTile, srcTile);
                 }
             }
         }
