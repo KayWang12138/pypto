88be2d92a80a4954b78f71ba8759783ba8f9396f
wang-zhiyue2 <wangzhiyue2@huawei.com>
2026-01-26 16:03:19 +0800

    feat(operation): Add PTO gather_in_L1

--- framework/src/interface/tileop/cube/cube_pto.h
+++ framework/src/interface/tileop/cube/cube_pto.h
@@ -516,5 +516,45 @@ TILEOP void TStore(T &dst, U &src, V &fixbuf, const Coord &coord, const int64_t 
         }
     }
 }
+
+template <int64_t blockSize, typename DstT, typename SrcT, typename BlockT, typename OffsetT, typename SrcCoord,
+    typename OffsetCoord, typename BlockCoord>
+TILEOP void TGatherInL1(DstT dst, SrcT src, BlockT block, OffsetT offset, SrcCoord srcCoord,
+    OffsetCoord offsetCoord, BlockCoord blockCoord)
+{
+    constexpr auto shapeSize = Std::tuple_size<typename DstT::Shape>::value;
+    constexpr int64_t c0Size = BLOCK_ALIGN_BYTE / sizeof(typename DstT::Type);
+    int64_t srcShape1 = GetShape<1>(src);
+    int64_t srcStride0 = GetStride<0>(src);
+    int64_t srcStride1 = GetStride<1>(src);
+    int64_t dstShape0 = GetShape<0>(dst);
+    int64_t dstShape1 = GetShape<1>(dst);
+    int64_t blockShape1 = GetShape<1>(block);
+    int64_t offsetShape1 = GetShape<1>(offset);
+    uint64_t srcColumnStartOffset = srcCoord.GetValue();
+    uint64_t offsetsRowStartOffset = offsetCoord.GetValue();
+    uint64_t offsetsColumnStartOffset = static_cast<const Std::tuple<size_t> &>(offsetCoord).GetValue();
+    uint64_t offsetsStartOffset = offsetsRowStartOffset * offsetShape1 + offsetsColumnStartOffset;
+    uint64_t GMBlockTableOffset0 = blockCoord.GetValue();
+    uint64_t GMBlockTableOffset1 = static_cast<const Std::tuple<size_t> &>(blockCoord).GetValue();
+    uint64_t GMBlockTableOffset = GMBlockTableOffset0 * blockShape1 + GMBlockTableOffset1;
+    constexpr auto staticL1H = Std::tuple_element<shapeSize - SHAPE_DIM2, typename DstT::TileShape>::type::value;
+    constexpr auto staticL1W = Std::tuple_element<shapeSize - 1, typename DstT::TileShape>::type::value;
+    using shapeDim2 = pto::Shape<1, 1, 1, 1, -1>;
+    using strideDim2 = pto::Stride<1, 1, 1, -1, -1>;
+    using globalData = pto::GlobalTensor<typename SrcT::Type, shapeDim2, strideDim2, pto::Layout::ND>;
+    for (int64_t i = 0; i < dstShape0; i++) {
+        uint64_t gatherOffset = offset.GetAddr()[i + offsetsStartOffset];
+        gatherOffset = CalaOffset2PageAttention<uint64_t, typename BlockT::Type, blockSize>
+            (block.GetAddr() + GMBlockTableOffset, gatherOffset);
+        globalData src0Global((__gm__ typename SrcT::Type *)(src.GetAddr() + gatherOffset * srcShape1 + srcColumnStartOffset),
+            shapeDim2(staticL1W), strideDim2(srcStride0, srcStride1));
+        using tileData = pto::Tile<pto::TileType::Mat, typename DstT::Type, staticL1H, staticL1W, pto::BLayout::ColMajor, -1,
+            -1, pto::SLayout::RowMajor>;
+        tileData dstL1(dstShape0, dstShape1);
+        pto::TASSIGN(dstL1, (uint64_t)((__cbuf__ typename DstT::Type *)dst.GetAddr() + i * c0Size));
+        pto::TLOAD(dstL1, src0Global);
+    }
+}
 } // namespace TileOp
 #endif // TILEOP_TILE_OPERATOR_CUBE_PTO__H
