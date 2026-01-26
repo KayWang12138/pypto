bd084f12a7b3bf3bc8ec303a853ccfddf6eee597
yinjie27 <yinjie27@huawei.com>
2026-01-26 14:42:58 +0800

    feat(machine): Add checker for aicore entry emulation

--- framework/tests/ut/machine/src/dynamic/test_aicore_entry.cpp
+++ framework/tests/ut/machine/src/dynamic/test_aicore_entry.cpp
@@ -60,6 +60,93 @@ private:
     std::vector<KernelSharedBuffer> sharedBuffer_;
 };
 
+struct MultipleCore : ThreadAicoreEmulation {
+    MultipleCore(std::shared_ptr<MemoryEmulation> memory_)
+        : memory(memory_) {
+        memset_s(&devArgs, sizeof(devArgs), 0, sizeof(devArgs));
+        devArgs.sharedBuffer = (uint64_t)(uintptr_t)memory->GetSharedBuffer();
+    }
+
+    void WaitAndStartKernelEntry() {
+        while (GetAicoreInfoByThread() == nullptr) {
+            std::this_thread::sleep_for(std::chrono::seconds(0));
+        }
+        KernelEntry(0, 0, 0, 0, 0, (uint64_t)(uintptr_t)&devArgs);
+    }
+
+    static void StartKernelEntry(std::shared_ptr<MultipleCore> aicore) {
+        aicore->WaitAndStartKernelEntry();
+    }
+
+    virtual void AicoreCallSubFuncTask(uint64_t funcIdx, CoreFuncParam *param, int64_t gmStackAddr, __gm__ int64_t *hcclContext) override {
+        UNUSED(funcIdx); UNUSED(param); UNUSED(gmStackAddr); UNUSED(hcclContext);
+        std::lock_guard<std::mutex> guard(traceMutex);
+        traceList.emplace_back(GetAicoreInfoByThread()->GetCoreIdx(), funcIdx, *param);
+    }
+
+    void MainLoop() {
+        const int rootCount = 0x2;
+        const int attrCount = 0x100;
+        const int leafCount = 0x40;
+        const uint64_t attrListBase = 0x100000000;
+        for (size_t k = 0; k < attrCount; k++) {
+            devFuncAttrList.push_back(attrListBase + k);
+        }
+        for (size_t k = 0; k < leafCount; k++) {
+            devFuncAttrOffsetList.push_back(k * attrCount / leafCount);
+        }
+
+        dynFuncDataList.resize(sizeof(DynFuncHeader) + sizeof(DynFuncData) * rootCount);
+        DynFuncHeader *dataList = reinterpret_cast<DynFuncHeader *>(dynFuncDataList.data());
+        dataList->funcNum = rootCount;
+        dataList->seqNo = 0;
+        for (size_t k = 0; k < dataList->funcNum; k++) {
+            dataList->At(k).opAttrs = devFuncAttrList.data();
+            dataList->At(k).opAtrrOffsets = devFuncAttrOffsetList.data();
+        }
+
+        KernelSharedBuffer *buffer = memory->GetSharedBuffer();
+        for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
+            buffer[i].args.shakeBufferCpuToCore[CPU_TO_CORE_SHAK_BUF_COREFUNC_DATA_INDEX] = (uintptr_t)dataList;
+        }
+
+        for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
+            AicpuSetData(i, MakeTaskID(i % 0x2, i) + 1);
+        }
+
+        for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
+            while ((AicpuGetCond(i) & AICORE_FIN_MASK) == 0) {
+                std::this_thread::sleep_for(std::chrono::seconds(0));
+            }
+        }
+
+        for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
+            AicpuSetData(i, AICORE_FUNC_STOP + 1);
+        }
+
+        for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
+            buffer[i].args.waveBufferCpuToCore[0] = AICORE_SAY_GOODBYE;
+        }
+    }
+
+    DynFuncHeader *GetDataList() { return reinterpret_cast<DynFuncHeader *>(dynFuncDataList.data()); }
+public:
+    std::shared_ptr<MemoryEmulation> memory;
+    DeviceArgs devArgs;
+    std::vector<uint8_t> dynFuncDataList;
+    std::vector<uint64_t> devFuncAttrList;
+    std::vector<int32_t> devFuncAttrOffsetList;
+
+    std::mutex traceMutex;
+    struct Trace {
+        int coreIdx;
+        int funcIdx;
+        CoreFuncParam param;
+        Trace(int coreIdx_, int funcIdx_, CoreFuncParam param_) : coreIdx(coreIdx_), funcIdx(funcIdx_), param(param_) {}
+    };
+    std::vector<Trace> traceList;
+};
+
 TEST_F(AicoreTest, MultipleCore) {
     const int aicpuCount = 0x4;
     const int aicCount = 0x0;
@@ -67,97 +154,12 @@ TEST_F(AicoreTest, MultipleCore) {
     std::shared_ptr<MemoryEmulation> memory = std::make_shared<MemoryEmulation>(aicpuCount, aicCount, aivCount);
     memory->Setup();
 
-    struct Aicore : ThreadAicoreEmulation {
-        Aicore(std::shared_ptr<MemoryEmulation> memory_)
-          : memory(memory_) {
-            memset_s(&devArgs, sizeof(devArgs), 0, sizeof(devArgs));
-            devArgs.sharedBuffer = (uint64_t)(uintptr_t)memory->GetSharedBuffer();
-        }
-
-        void WaitAndStartKernelEntry() {
-            while (GetAicoreInfoByThread() == nullptr) {
-                std::this_thread::sleep_for(std::chrono::seconds(0));
-            }
-            KernelEntry(0, 0, 0, 0, 0, (uint64_t)(uintptr_t)&devArgs);
-        }
-
-        static void StartKernelEntry(std::shared_ptr<Aicore> aicore) {
-            aicore->WaitAndStartKernelEntry();
-        }
-
-        virtual void AicoreCallSubFuncTask(uint64_t funcIdx, CoreFuncParam *param, int64_t gmStackAddr, __gm__ int64_t *hcclContext) override {
-            UNUSED(funcIdx); UNUSED(param); UNUSED(gmStackAddr); UNUSED(hcclContext);
-            std::lock_guard<std::mutex> guard(traceMutex);
-            traceList.emplace_back(GetAicoreInfoByThread()->GetCoreIdx(), funcIdx, *param);
-        }
-
-        void MainLoop() {
-            const int rootCount = 0x2;
-            const int attrCount = 0x100;
-            const int leafCount = 0x40;
-            const uint64_t attrListBase = 0x100000000;
-            for (size_t k = 0; k < attrCount; k++) {
-                devFuncAttrList.push_back(attrListBase + k);
-            }
-            for (size_t k = 0; k < leafCount; k++) {
-                devFuncAttrOffsetList.push_back(k * attrCount / leafCount);
-            }
-
-            dynFuncDataList.resize(sizeof(DynFuncHeader) + sizeof(DynFuncData) * rootCount);
-            DynFuncHeader *dataList = reinterpret_cast<DynFuncHeader *>(dynFuncDataList.data());
-            dataList->funcNum = rootCount;
-            dataList->seqNo = 0;
-            for (size_t k = 0; k < dataList->funcNum; k++) {
-                dataList->At(k).opAttrs = devFuncAttrList.data();
-                dataList->At(k).opAtrrOffsets = devFuncAttrOffsetList.data();
-            }
-
-            KernelSharedBuffer *buffer = memory->GetSharedBuffer();
-            for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
-                buffer[i].args.shakeBufferCpuToCore[CPU_TO_CORE_SHAK_BUF_COREFUNC_DATA_INDEX] = (uintptr_t)dataList;
-            }
-
-            for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
-                AicpuSetData(i, MakeTaskID(i % 0x2, i) + 1);
-            }
-
-            for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
-                while ((AicpuGetCond(i) & AICORE_FIN_MASK) == 0) {
-                    std::this_thread::sleep_for(std::chrono::seconds(0));
-                }
-            }
-
-            for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
-                AicpuSetData(i, AICORE_FUNC_STOP + 1);
-            }
-
-            for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
-                buffer[i].args.waveBufferCpuToCore[0] = AICORE_SAY_GOODBYE;
-            }
-        }
-    private:
-        std::shared_ptr<MemoryEmulation> memory;
-        DeviceArgs devArgs;
-        std::vector<uint8_t> dynFuncDataList;
-        std::vector<uint64_t> devFuncAttrList;
-        std::vector<int32_t> devFuncAttrOffsetList;
-
-        std::mutex traceMutex;
-        struct Trace {
-            int coreIdx;
-            int funcIdx;
-            CoreFuncParam param;
-            Trace(int coreIdx_, int funcIdx_, CoreFuncParam param_) : coreIdx(coreIdx_), funcIdx(funcIdx_), param(param_) {}
-        };
-        std::vector<Trace> traceList;
-    };
-
-    auto aicore = std::make_shared<Aicore>(memory);
+    auto aicore = std::make_shared<MultipleCore>(memory);
     AicoreEmulationManager::GetInstance().SetupAicoreEmulation(std::static_pointer_cast<AicoreEmulationBase>(aicore));
 
     std::vector<std::shared_ptr<std::thread>> threadList;
     for (int i = 0; i < aicCount + aivCount; i++) {
-        std::shared_ptr<std::thread> thread = std::make_shared<std::thread>(Aicore::StartKernelEntry, aicore);
+        std::shared_ptr<std::thread> thread = std::make_shared<std::thread>(MultipleCore::StartKernelEntry, aicore);
         aicore->AppendAicore(thread, i, i);
         threadList.push_back(thread);
     }
@@ -170,6 +172,12 @@ TEST_F(AicoreTest, MultipleCore) {
     for (int i = 0; i < memory->GetAicCount() + memory->GetAivCount(); i++) {
         // normal exit
         EXPECT_EQ(buffer[i].args.shakeBuffer[2], STAGE_GET_NEXT_TASK_STOP);
+    }
+
+    EXPECT_EQ(aicCount + aivCount, aicore->traceList.size());
+    DynFuncHeader *dataList = aicore->GetDataList();
+    for (int i = 0; i < aicCount + aivCount; i++) {
+        EXPECT_EQ(dataList->At(aicore->traceList[i].coreIdx % 2), aicore->traceList[i].param.funcData);
     }
 
     AicoreEmulationManager::GetInstance().Reset();
