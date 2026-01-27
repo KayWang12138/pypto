1ed90112f742784ed57370479e175574f5bb506f
junnyleo <lijun580@huawei.com>
2026-01-27 18:53:50 +0800

    feat(Operation): Add triu and tril operations

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -120,6 +120,7 @@ public:
     std::string PrintIndexOutCastTileTensor() const;
 
     std::string GenCumSumOp() const;
+    std::string GenTriULOp() const;
     std::string PrintGatherDynamicUnaligned() const;
     std::string PrintGatherLayout() const;
     std::string GenGatherOp() const;
@@ -400,6 +401,8 @@ private:
     std::string PrintIndexPut(const PrintIndexPutParam &param) const;
     std::string PrintIndexPutLayout(size_t indicesSize, bool accumulate) const;
     std::string PrintIndexPutDynamicUnaligned(const PrintIndexPutParam &param) const;
+
+    std::string PrintTriULTileTensor(std::string diagonal, bool isUpper) const;
 
     std::string PrintCumSumDynamicUnaligned(const PrintCumSumParam &param) const;
     std::string PrintCumSumTileTensor(int axis) const;
