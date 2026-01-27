b5a02016d8ca2d233089012bac1dea442bbc9a91
tsungl4 <lidongsheng4@huawei.com>
2026-01-27 22:55:14 +0800

    feat(runtime): Support triple stream schedule

--- framework/src/machine/device/dynamic/device_ctrl.h
+++ framework/src/machine/device/dynamic/device_ctrl.h
@@ -274,9 +274,9 @@ public:
         if (kargs == nullptr) {
             return -1;
         }
-        if (kargs->inputs == nullptr || kargs->outputs == nullptr || kargs->cfgdata == nullptr) {
-            DEV_ERROR("Args has null in inputs[%p] outputs[%p] work[%p] or cfg[%p].\n", kargs->inputs,
-                    kargs->outputs, kargs->workspace, kargs->cfgdata);
+        if (kargs->inputs == nullptr || kargs->cfgdata == nullptr) {
+            DEV_ERROR("Args has null in inputs[%p] work[%p] or cfg[%p].\n", kargs->inputs,
+                    kargs->workspace, kargs->cfgdata);
             return -1;
         }
         InitDyn(kargs);
