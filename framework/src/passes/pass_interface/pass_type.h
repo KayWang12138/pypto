c3ae1ae47b49a5cf26a17bc46960d55994ed42d1
heyufeng <heyufeng11@hisilicon.com>
2026-01-27 16:36:50 +0800

    feat(pass, codegen, interface):VF support for A5

--- framework/src/passes/pass_interface/pass_type.h
+++ framework/src/passes/pass_interface/pass_type.h
@@ -67,6 +67,11 @@ enum class PassName {
     MIX_SUBGRAPH_SPLIT,
     CODEGEN_PREPROC,
     DYN_ATTR_TO_STATIC,
+<<<<<<< HEAD
+=======
+    TUNE_TILEOP_SEQ_FOR_VF,
+    TUNE_SYNC_FOR_VF,
+>>>>>>> 418c79e (feat(pass, codegen, interface):VF support for A5)
     LOOPAXES_PROC,
     NOT_DEFINED
 };
@@ -114,6 +119,8 @@ inline constexpr const char *PassNameStr(PassName name){
         case PassName::MIX_SUBGRAPH_SPLIT: return "MixSubgraphSplit";
         case PassName::CODEGEN_PREPROC: return "CodegenPreproc";
         case PassName::DYN_ATTR_TO_STATIC: return "DynAttrToStatic";
+        case PassName::TUNE_TILEOP_SEQ_FOR_VF: return "TuneTileOpSeqForVF";
+        case PassName::TUNE_SYNC_FOR_VF: return "TuneSyncForVF";
         case PassName::LOOPAXES_PROC: return "LoopaxesProc";
         case PassName::NOT_DEFINED: return "NotDefined";
         default: 
