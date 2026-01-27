b39e7297248dff50bdc594b93160d620bee9a8f0
xuyongxin <xuyongxin6@huawei.com>
2026-01-27 09:51:59 +0800

    refactor(pass): MixSubgraphSplit refactoring

--- framework/tests/ut/passes/src/test_mix_call_operation_builder.cpp
+++ framework/tests/ut/passes/src/test_mix_call_operation_builder.cpp
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
 /*!
  * \file test_mix_call_operation_builder.cpp
  * \brief Unit test for MixCallOperationBuilder
@@ -393,12 +403,8 @@ private:
     }
     
     // 创建CopyIn操作
-    void createCopyInOperation(
-        const std::shared_ptr<Function>& func,
-        int internalSubgraphId,
-        const std::shared_ptr<LogicalTensor>& input,
-        const std::shared_ptr<LogicalTensor>& output,
-        int offset)
+    void createCopyInOperation(const std::shared_ptr<Function>& func, int internalSubgraphId,
+        const std::shared_ptr<LogicalTensor>& input, const std::shared_ptr<LogicalTensor>& output, int offset)
     {
         const std::vector<int64_t> shape = {MS_NUM16, MS_NUM16};
         auto shapeImme = OpImmediate::Specified(shape);
@@ -431,12 +437,8 @@ private:
     }
     
     // 创建CopyOut操作
-    void createCopyOutOperation(
-        const std::shared_ptr<Function>& func,
-        int internalSubgraphId,
-        const std::shared_ptr<LogicalTensor>& input,
-        const std::shared_ptr<LogicalTensor>& output,
-        int offset)
+    void createCopyOutOperation(const std::shared_ptr<Function>& func, int internalSubgraphId,
+        const std::shared_ptr<LogicalTensor>& input, const std::shared_ptr<LogicalTensor>& output, int offset)
     {
         const std::vector<int64_t> shape = {MS_NUM16, MS_NUM16};
         auto shapeImme = OpImmediate::Specified(shape);
@@ -610,7 +612,7 @@ TEST_F(MixCallOperationBuilderTest, TestSameWrapIdForSameOriginalCallOp)
     // 使用辅助函数创建叶子函数和指针
     std::vector<std::shared_ptr<Function>> leafFuncs;
     std::vector<Function*> newFunctions;
-    createLeafFunctionsAndPointers(2, "leaf", leafFuncs, newFunctions);
+    createLeafFunctionsAndPointers(MS_NUM2, "leaf", leafFuncs, newFunctions);
     
     // 使用辅助函数创建程序ID
     std::vector<uint64_t> newProgramIDs = createProgramIds(components.size());
@@ -667,7 +669,7 @@ TEST_F(MixCallOperationBuilderTest, TestDifferentWrapIdForDifferentOriginalCallO
     // 使用辅助函数创建叶子函数和指针
     std::vector<std::shared_ptr<Function>> leafFuncs;
     std::vector<Function*> newFunctions;
-    createLeafFunctionsAndPointers(2, "leaf", leafFuncs, newFunctions);
+    createLeafFunctionsAndPointers(MS_NUM2, "leaf", leafFuncs, newFunctions);
     
     // 使用辅助函数创建程序ID
     std::vector<uint64_t> newProgramIDs = createProgramIds(components.size());
@@ -775,7 +777,7 @@ TEST_F(MixCallOperationBuilderTest, TestInternalDependencies)
     // 使用辅助函数创建叶子函数和指针
     std::vector<std::shared_ptr<Function>> leafFuncs;
     std::vector<Function*> newFunctions;
-    createLeafFunctionsAndPointers(2, "leaf", leafFuncs, newFunctions);
+    createLeafFunctionsAndPointers(MS_NUM2, "leaf", leafFuncs, newFunctions);
     
     std::vector<InternalDependencyInfo> internalDeps = {
         {COMPONENT_ID_0, COMPONENT_ID_1, ComponentType::C_SCOPE}
