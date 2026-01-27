b10907400b2acc025783b468c6e1f93c76bcf3ba
heyufeng <heyufeng11@hisilicon.com>
2026-01-27 16:54:16 +0800

    feat(pass, codegen, interface):VF support for A5

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
@@ -380,6 +380,9 @@ Status OoOScheduler::LaunchIssueStage(int& nextCycle) {
             continue;
         }
         IssueEntryPtr issue = pipe.PopFront();
+        //标注op的生命周期
+        issue->tileOp.cycleStart = clock;
+        issue->tileOp.cycleEnd = clock + issue->tileOp.GetLatency();
         pipe.busy = true;
         pipe.curIssue = issue;
         pipe.curOpRetireCycle = clock + issue->tileOp.GetLatency();
@@ -508,9 +511,14 @@ Status OoOScheduler::RetireOpAndAwakeSucc(IssueEntryPtr issue, uint64_t& commitC
 
 Status OoOScheduler::RetireIssueStage(uint64_t& commitCnt, int& nextCycle) {
     for (auto& [pipeType, pipe] : issueQueues) {
-        (void)pipeType;
         if (!pipe.busy) {
             continue;
+        }
+        if (!pipeEndTime.count(pipeType)) {
+            pipeEndTime.emplace(pipeType, pipe.curOpRetireCycle);
+        } else {
+            auto curEndTime = pipeEndTime[pipeType];
+            pipeEndTime[pipeType] = std::max(curEndTime, pipe.curOpRetireCycle);
         }
         if (pipe.curOpRetireCycle <= clock) {   // 如果该pipe内当前正在执行op，在clock的时刻已经执行完毕。
             IssueEntryPtr issue = pipe.curIssue;
@@ -1049,6 +1057,7 @@ Status OoOScheduler::Schedule(const std::vector<Operation *> &operations) {
     }
     PrintOpList(newOperations_);
     function_.SetStackWorkespaceSize(workspaceOffset);
+    function_.pipeEndTime = pipeEndTime;
     return SUCCESS;
 }
 
