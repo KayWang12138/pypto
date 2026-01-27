6d32cc10dd66c37976374becb23884eec95c4ff9
mengziyu <mengziyu1@huawei.com>
2026-01-27 10:34:41 +0800

    feat(distributed): Optimize code

--- framework/src/codegen/cloudnpu/codegen_distributed.cpp
+++ framework/src/codegen/cloudnpu/codegen_distributed.cpp
@@ -25,7 +25,6 @@ namespace npu::tile_fwk {
 
 using AtomicType = npu::tile_fwk::Distributed::AtomicType;
 using DistOpAttr = npu::tile_fwk::Distributed::DistOpAttr;
-constexpr int32_t GM2UB_SHMEMDATA_INDEX = 2;
 
 void CheckInRange(int64_t value)
 {
@@ -82,7 +81,7 @@ std::string CodeGenOpCloudNPU::GenTemplateParamsForPutAndGet() const
     static const std::unordered_map<Opcode, std::array<int32_t, 3>> opcodeIndexMap = {
         {Opcode::OP_SHMEM_PUT, {3, 4, 3}},
         {Opcode::OP_SHMEM_GET, {0, 3, 3}},
-        {Opcode::OP_SHMEM_PUT_UB2GM, {1, GM2UB_SHMEMDATA_INDEX, 1}},
+        {Opcode::OP_SHMEM_PUT_UB2GM, {2, 3, 1}},
         {Opcode::OP_SHMEM_GET_GM2UB, {0, 3, 3}}
     };
     auto [nonShmemDataIndex, shmemDataIndex, shapeIndex] = opcodeIndexMap.at(opCode);
@@ -239,8 +238,8 @@ std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapesForShmemPutAndGet() const
 std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapesForShmemPutAndGetUB() const
 {
     std::ostringstream oss;
-    int32_t nonShmemDataIndex = (opCode == Opcode::OP_SHMEM_PUT_UB2GM) ? 1 : 0;
-    int32_t shmemDataIndex = 2;
+    int32_t nonShmemDataIndex = 2;
+    int32_t shmemDataIndex = 3;
     int32_t nonShmemDataDim = 2;
     int32_t shmemDataDim = 4;
     oss << ", " << GenOffsetsAndRawShapes(nonShmemDataIndex, nonShmemDataDim)
@@ -394,7 +393,7 @@ std::string CodeGenOpCloudNPU::GenDistOp() const
     static const std::unordered_map<Opcode, std::unordered_set<int32_t>> skipIndexMap = {
         {Opcode::OP_SHMEM_PUT, {0, 2}},
         {Opcode::OP_SHMEM_GET, {2}},
-        {Opcode::OP_SHMEM_PUT_UB2GM, {0, 3}},
+        {Opcode::OP_SHMEM_PUT_UB2GM, {0, 1}},
         {Opcode::OP_SHMEM_GET_GM2UB, {2}},
         {Opcode::OP_SHMEM_SIGNAL, {0, 2}},
         {Opcode::OP_SHMEM_SET, {0, 2}},
