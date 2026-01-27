f4eefb2c9ca6b8610fab10c3c1a698392e863d93
zhuqingbo <zhuqingbo@huawei.com>
2026-01-27 18:43:04 +0800

    feat(interpreter): Dump leaf func input and out tensors

--- python/tests/st/interface/test_tensor_dump.py
+++ python/tests/st/interface/test_tensor_dump.py
@@ -17,43 +17,10 @@ import torch_npu
 import pypto
 import numpy as np
 
-verify_options = {"enable_pass_verify": True,
-                  "pass_verify_save_tensor": True,
-                 }
-
-
-@pypto.jit(verify_options=verify_options)
-def add(a, b, c):
-    for _ in pypto.loop(1):
-        pypto.set_vec_tile_shapes(16, 16)
-        c[:] = a + b
-
-
-def test_verify_full_options():
-    device_id = int(os.environ.get('TILE_FWK_DEVICE_ID', 0))
-    torch.npu.set_device(device_id)
-
-    a = torch.ones((64, 64))
-    b = torch.ones((64, 64))
-    c = torch.zeros((64, 64))
-
-    golden = a + b
-    inputs = [a, b]
-    outputs = [c]
-
-    inputs = [a.to(f"npu:{device_id}"), b.to(f"npu:{device_id}")]
-    outputs = [c.to(f"npu:{device_id}")]
-    pto_inputs = [pypto.from_torch(tensor, f"IN_{idx}") for idx, tensor in enumerate(inputs)]
-    pto_outputs = [pypto.from_torch(tensor, f"OUT_{idx}") for idx, tensor in enumerate(outputs)]
-
-    add(*pto_inputs, *pto_outputs)
-
-    assert torch.allclose(outputs[0].cpu(), golden)
-
 
 @pypto.jit(verify_options={"enable_pass_verify": True,
                            "pass_verify_save_tensor": True,
-                           "pass_verify_pass_filter": ["RemoveRedundantReshape", "ExpandFunction", "DuplicateOp1"]
+                           "pass_verify_pass_filter": ["CodegenPreproc"]
                         })
 def add_dyn(x, y, out):
     first_dim, second_dim = x.shape
@@ -82,14 +49,14 @@ def add_dyn(x, y, out):
                 [b_idx * first_view_shape, s_idx * second_view_shape],
                 out,
             )
-            del res, tile_tensor_0, tile_tensor_1
 
 
-def test_verify_dyn():
+def test_tensor_dump():
     shape = [72, 144]
     a = torch.rand(shape, dtype=torch.float16)
     b = torch.rand(shape, dtype=torch.float16)
     c = torch.zeros(shape, dtype=torch.float16)
+    os.environ["PTO_DATADUMP_ENABLE"] = "true"
 
     device_id = os.environ.get('TILE_FWK_DEVICE_ID', 0)
     torch.npu.set_device(int(device_id))
@@ -105,4 +72,5 @@ def test_verify_dyn():
     pto_outputs = [pypto.from_torch(tensor, f"OUT_{idx}") for idx, tensor in enumerate(outputs)]
 
     add_dyn(*pto_inputs, *pto_outputs)
+    assert os.path.exists("output/dump_tensor")
 
