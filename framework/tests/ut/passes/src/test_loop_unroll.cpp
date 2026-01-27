cf1451762eaa08ce68d5be9b482fd1145dd4dfb9
songyangyang18 <songyangyang18@huawei.com>
2026-01-27 11:22:19 +0800

    refactor(machine): Remove old configs.

--- framework/tests/ut/passes/src/test_loop_unroll.cpp
+++ framework/tests/ut/passes/src/test_loop_unroll.cpp
@@ -37,7 +37,7 @@ public:
     void SetUp() override {
         Program::GetInstance().Reset();
         config::Reset();
-        config::SetHostOption(COMPILE_STAGE, HOST_COMPILE_END);
+        config::SetRuntimeOption(CFG_RUN_MODE, COMPILE_STAGE3);
         config::SetHostConfig(KEY_STRATEGY, "FunctionUnroll");
         config::SetPlatformConfig(KEY_ENABLE_COST_MODEL, false);
         config::SetHostOption(COMPILE_STAGE, GEN_TENSOR_GRAPH);
