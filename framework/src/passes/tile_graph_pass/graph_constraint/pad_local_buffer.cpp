74cb33d497558d07163ce7ea9561f230da699f8a
gengrunze <gengrunze@huawei.com>
2026-01-27 15:19:06 +0800

    feat(pass): Fit New Op Matmul_MX in Pass

--- framework/src/passes/tile_graph_pass/graph_constraint/pad_local_buffer.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pad_local_buffer.cpp
@@ -103,12 +103,12 @@ void PadLocalBuffer::PadMatmul(Operation &op, LogicalTensorPtr &in) {
     第二种：iOperands (dtype:int8) -> Matmul系列(A_MUL_B, AT_MUL_B, A_MUL_BT, AT_MUL_BT, A_MULACC_B) -> in (dtype:fp16/int32) -> iOperands (dtype:fp16/int32) -> COPY_OUT
     这种情况是COPY_OUT需要根据in的producer的iOperands来进行判断，所以会需要获取到in的producer的iOperands的数据类型。
     */
-    if (op.GetOpcode() == Opcode::OP_L1_COPY_IN_A_SCALE) {
+    if (op.GetOpcode() == Opcode::OP_L1_TO_L0A_SCALE) {
         in->shape[lowIndex] = Pad(in->shape[lowIndex], CUBE_PAD_VALUE);
         in->tensor->oriRawshape = in->tensor->rawshape;
         in->tensor->rawshape[lowIndex] = Pad(in->tensor->oriRawshape[lowIndex], CUBE_PAD_VALUE);
         return;
-    } else if (op.GetOpcode() == Opcode::OP_L1_COPY_IN_B_SCALE) {
+    } else if (op.GetOpcode() == Opcode::OP_L1_TO_L0B_SCALE) {
         in->shape[highIndex] = Pad(in->shape[highIndex], CUBE_PAD_VALUE);
         in->tensor->oriRawshape = in->tensor->rawshape;
         in->tensor->rawshape[highIndex] = Pad(in->tensor->oriRawshape[highIndex], CUBE_PAD_VALUE);
@@ -408,9 +408,9 @@ void PadLocalBuffer::DoPadding(Function &function) {
             }
             visited.emplace(in);
             if (IsMatmul(in)) {
-                if (op.GetOpcode() == Opcode::OP_L1_TO_L0B_SCALE) {
-                    continue;
-                }
+                // if (op.GetOpcode() == Opcode::OP_L1_TO_L0B_SCALE) {
+                //     continue;
+                // }
                 PadMatmul(op, in);
                 continue;
             }
