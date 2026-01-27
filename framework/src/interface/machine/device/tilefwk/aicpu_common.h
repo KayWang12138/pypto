6e294f21fef2953e6d136eea026b0acecb890100
yang-zhipeng19 <yangzhipeng19@huawei.com>
2026-01-27 11:14:27 +0800

    feat(machine): Bind the aicpu and aicore to the same die for DAV3510

--- framework/src/interface/machine/device/tilefwk/aicpu_common.h
+++ framework/src/interface/machine/device/tilefwk/aicpu_common.h
@@ -148,6 +148,8 @@ struct DeviceArgs {
     uint64_t taskQueue{0};     // task queue between ctrl and sche
     uint64_t taskCtrl{0};      // task ctrl between ctrl and sche
     uint32_t scheCpuNum{0};    // sche cpu num calc by host
+    uint32_t launchScheCpuNum{0};    // max launch sche cpu num by host
+    uint32_t launchAicpu{0};
     uint32_t enableCtrl : 2;    // if enable builtin ctrl
     uint32_t validGetPgMask : 2; // mark pgmask is invalid
     uint32_t disableSync : 28;    // close ctrl and sche soft sync
