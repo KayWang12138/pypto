bfe46c8e9b93d62c8d4c296b6003218255213b44
gggxinmeng <guoxinmeng@huawei.com>
2026-01-27 14:33:27 +0800

    feat(Pass): Modify conflict

--- framework/tests/ut/passes/src/test_schedule_ooo.cpp
+++ framework/tests/ut/passes/src/test_schedule_ooo.cpp
@@ -1647,8 +1647,8 @@ TEST_F(ScheduleOoOTest, TestSpillOnBlockFailedAtL0) {
     auto corePair = opCoreTypeMap.at(OpCoreType::AIC);
     oooSchedule.allocIssueQueue[corePair.first][corePair.second][MemoryType::MEM_L0A].Insert(AllocL0AIssue);
     oooSchedule.allocIssueQueue[corePair.first][corePair.second][MemoryType::MEM_L0B].Insert(AllocL0BIssue);
-    oooSchedule.tensorOccupyMap[corePair.first][corePair.second][MemoryType::MEM_L0A].emplace(1, L1toL0AIssue);
-    oooSchedule.tensorOccupyMap[corePair.first][corePair.second][MemoryType::MEM_L0B].emplace(2, L1toL0BIssue);
+    oooSchedule.tensorOccupyMap[MemoryType::MEM_L0A].emplace(1, L1toL0AIssue);
+    oooSchedule.tensorOccupyMap[MemoryType::MEM_L0B].emplace(2, L1toL0BIssue);
     oooSchedule.localBufferMap[1] = std::make_shared<LocalBuffer>(1, 32768, MemoryType::MEM_L0A);
     oooSchedule.localBufferMap[2] = std::make_shared<LocalBuffer>(2, 32768, MemoryType::MEM_L0B);
     oooSchedule.localBufferMap[3] = std::make_shared<LocalBuffer>(3, 32768, MemoryType::MEM_L0A);
