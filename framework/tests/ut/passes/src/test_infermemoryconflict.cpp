97c3cf5c646a8d123a0f28a7728fbbdb763c873a
songyangyang18 <songyangyang18@huawei.com>
2026-01-27 11:22:19 +0800

    refactor(machine): Remove old configs.

--- framework/tests/ut/passes/src/test_infermemoryconflict.cpp
+++ framework/tests/ut/passes/src/test_infermemoryconflict.cpp
@@ -53,7 +53,7 @@ public:
     void SetUp() override {
         Program::GetInstance().Reset();
         config::Reset();
-        config::SetHostOption(COMPILE_STAGE, HOST_COMPILE_END);
+        config::SetRuntimeOption(CFG_RUN_MODE, COMPILE_STAGE3);
         config::SetHostConfig(KEY_STRATEGY, "InferMemoryTestStrategy");
         config::SetPlatformConfig(KEY_ENABLE_COST_MODEL, false);
     }
