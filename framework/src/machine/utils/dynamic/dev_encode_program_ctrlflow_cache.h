09da0ce69f675d4653d59b0e521d4f40a39bd8a1
mengziyu <mengziyu1@huawei.com>
2026-01-28 16:14:54 +0800

    feat(distributed): Adapt distributed operators to the control flow cache feature.

--- framework/src/machine/utils/dynamic/dev_encode_program_ctrlflow_cache.h
+++ framework/src/machine/utils/dynamic/dev_encode_program_ctrlflow_cache.h
@@ -27,6 +27,7 @@ namespace npu::tile_fwk::dynamic {
 #define ADDRESS_CACHE_KIND_WORKSPACE         0
 #define ADDRESS_CACHE_KIND_INPUT             1
 #define ADDRESS_CACHE_KIND_OUTPUT            2
+#define ADDRESS_CACHE_KIND_COMM              3
 #define INVALID_STITCH_IDX      (static_cast<uint32_t>(-1))
 
 constexpr size_t READY_QUEUE_SIZE = 3UL;
@@ -191,6 +192,7 @@ struct DevControlFlowCache {
     DevRelocVector<uint8_t> cacheData;
 
     uint64_t workspaceAddr;
+    uint64_t hcclContextAddr{0};
 #define ctrlFlowLastField         cacheData
     uint64_t dataSize;
     uint8_t data[0];
@@ -491,6 +493,8 @@ struct DevControlFlowCache {
         uint64_t addr = desc.GetAddressValue();
         if (cacheInputOutputDict.count(addr)) {
             resultDesc = cacheInputOutputDict[addr];
+        } else if (addr & (1UL << 54)) {
+ 	        resultDesc = AddressDescriptor::MakeCache(ADDRESS_CACHE_KIND_COMM, addr);
         } else {
             relocWorkspace.Reloc(addr);
             resultDesc = AddressDescriptor::MakeCache(ADDRESS_CACHE_KIND_WORKSPACE, addr);
@@ -513,6 +517,9 @@ struct DevControlFlowCache {
                 break;
             case ADDRESS_CACHE_KIND_OUTPUT:
                 resultAddr = devStartArgs->GetOutputTensor(desc.cacheValue).address;
+                break;
+            case ADDRESS_CACHE_KIND_COMM:
+                resultAddr = desc.cacheValue;
                 break;
             default:
                 DEV_ERROR("[RelocDescFromCache] Invalid kind: %lu\n", (unsigned long)desc.cacheKind);
@@ -645,7 +652,7 @@ struct DevControlFlowCache {
             }
         }
     }
-
+ 	 
     void IncastOutcastAddrReloc(
             uint64_t srcWorkspace, uint64_t dstWorkspace,
             DevStartArgsBase *devStartArgs) {
@@ -675,7 +682,7 @@ struct DevControlFlowCache {
                     }
                     for (uint64_t i = 0; i < duppedData->GetOutcastSize(); i++) {
                         AddressDescriptor *addr = reinterpret_cast<AddressDescriptor *>(dynDataBackup->rawTensorAddrBackup + duppedData->GetIncastSize() + i);
-                        RelocDescToCache(*addr, relocWorkspace, cacheInputOutputDict);
+ 	                    RelocDescToCache(*addr, relocWorkspace, cacheInputOutputDict);
                     }
                 } else {
                     // Device: addr uses actual
