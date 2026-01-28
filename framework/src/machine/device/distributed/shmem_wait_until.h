4b77a3899f465ee1517460379b916dea59fa3285
codehjp423 <hanjunpei@huawei.com>
2026-01-28 19:28:45 +0800

    feat(distributed): Take hcclcontext as part of start args

--- framework/src/machine/device/distributed/shmem_wait_until.h
+++ framework/src/machine/device/distributed/shmem_wait_until.h
@@ -169,7 +169,7 @@ public:
     inline void Init(npu::tile_fwk::dynamic::DynDeviceTask *dynDeviceTask) {
         dynDeviceTask_ = dynDeviceTask;
         funcDataList_ = reinterpret_cast<DynFuncData*>(&dynDeviceTask->GetDynFuncDataList()->At(0));
-        hcclContextAddr_ = funcDataList_->hcclContext;
+        hcclContextAddr_ = funcDataList_->startArgs->commContexts;
         hashMap_.Init();
     }
 
@@ -215,7 +215,7 @@ private:
 
     npu::tile_fwk::dynamic::DynDeviceTask *dynDeviceTask_;
     npu::tile_fwk::DynFuncData *funcDataList_;
-    uint64_t *hcclContextAddr_;
+    int64_t *hcclContextAddr_;
     AicpuParamInfo paramInfo_;
 
     uint64_t GetRawAddr(const uint64_t addr, const uint64_t dstRankId);
