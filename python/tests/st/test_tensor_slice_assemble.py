5b7eb46d74bf05c4ccfcc8cb076b2620606045ed
guoliwei <guoliwei10@huawei.com>
2026-01-29 11:38:40 +0800

    feat(operation): Add Ceil Floor Trunc  operation
    feat(operation): Add Ceil Floor Trunc operation

--- python/tests/st/test_tensor_slice_assemble.py
+++ python/tests/st/test_tensor_slice_assemble.py
@@ -11,10 +11,12 @@
 """
 """
 import os
+import math
 import pypto
 import pytest
 import torch
 import torch_npu
+import math
 
 
 def test_slice_neg_index():
@@ -89,7 +91,7 @@ def test_2d_assemble_to_3d():
 
     with pypto.function("SLICE_NEG_INDEX", x, res):
         for b_idx in pypto.loop(res.shape[0], name="LOOP_L0", idx_name="a_idx"):
-            s_loop = pypto.ceil(res.shape[1], x.shape[0])
+            s_loop = math.ceil(res.shape[1] / x.shape[0])
             for s_idx in pypto.loop(s_loop, name="LOOP_L1", idx_name="s_idx"):
                 pypto.set_vec_tile_shapes(4, 4, 8)
                 offset = s_idx * x.shape[1]
