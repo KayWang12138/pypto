655320241ede91c42c12153557fe61b35e20cf0f
zhaoshenjie <zhaoshenjie@huawei.com>
2026-01-29 14:25:00 +0800

    fix(Pass): Optimize IsInIssueEntries complexity

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
@@ -192,6 +192,9 @@ void OoOScheduler::PrintSpillFailedInfo(IssueEntryPtr allocIssue, MemoryType buf
 }
 
 void OoOScheduler::PrintDependencies() {
+    if (static_cast<int>(LoggerManager::GetManager().level) > static_cast<int>(LoggerLevel::DEBUG)) {
+        return;
+    }
     for (const auto &issue : issueEntries) {
         APASS_LOG_DEBUG_F(Elements::Operation, "%s, latency: %d.", issue->GetOpInfo().c_str(), issue->tileOp.GetLatency());
         for (const auto &preId : issue->predecessors) {
@@ -262,6 +265,7 @@ void OoOScheduler::InsertIssueEntries(IssueEntryPtr insertIssue) {
         }
     }
     auto insertPos = issueEntries.insert(it++, insertIssue);
+    issueEntriesOpMagic.insert(insertIssue->tileOp.GetOpMagic());
     for (auto adjustIt = insertPos + 1; adjustIt != issueEntries.end(); adjustIt++) {
         if ((*adjustIt)->execOrder >= insertIssue->execOrder) {
             (*adjustIt)->execOrder++;
@@ -609,13 +613,8 @@ void OoOScheduler::LaunchReadyIssue() {
     }
 }
 
-bool OoOScheduler::IsInissueEntries(Operation* op) {
-    for (auto &issue : issueEntries) {
-        if (issue->tileOp.GetOpMagic() == op->GetOpMagic()) {
-            return true;
-        }
-    }
-    return false;
+bool OoOScheduler::IsInIssueEntries(Operation* op) {
+    return issueEntriesOpMagic.count(op->GetOpMagic());
 }
 
 Status OoOScheduler::ScheduleMainLoop() {
@@ -1076,6 +1075,7 @@ Status OoOScheduler::InitIssueEntry(Operation* op, const std::unordered_map<Oper
         return FAILED;
     }
     issueEntries.emplace_back(issue);
+    issueEntriesOpMagic.insert(issue->tileOp.GetOpMagic());
     if (InitIssueCoreType(issue, op, opCoreMap) != SUCCESS) {
         APASS_LOG_ERROR_F(Elements::Operation, "IssueEntry %s init coreType failed!", issue->GetOpInfo().c_str());
         return FAILED;
--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
@@ -155,6 +155,7 @@ struct SpillInfo {
 class OoOScheduler {
 private:
     std::vector<IssueEntryPtr> issueEntries;
+    std::unordered_set<int> issueEntriesOpMagic;
     std::unordered_map<int, IssueEntryPtr> issueEntryMap;
 
     std::unordered_map<OpCoreType, std::vector<int>> CORE_INIT_CONFIGS;
@@ -225,7 +226,7 @@ private:
     Status GenSpillSchedule();
     Status ExecuteAllocIssue(IssueEntryPtr issue, size_t &pcIdx);
     Status RetireIssue(IssueEntryPtr issue);
-    bool IsInissueEntries(Operation* op);
+    bool IsInIssueEntries(Operation* op);
     Status ScheduleMainLoop();
     void LaunchReadyIssue();
     Status RetireCoreIssue(OpCoreType coreType, int idx, uint64_t& commitCnt, int& nextCycle);
