7cb5a59244fcc4043a5a6444d392bffc2d0b9ed6
zhangxiangjie <zhangxiangjie5@huawei.com>
2026-01-29 11:59:26 +0800

    处理计算hash逻辑异常导致的精度问题

--- framework/src/interface/function/function.cpp
+++ framework/src/interface/function/function.cpp
@@ -1238,7 +1238,7 @@ void Function::ProducerMagicLookup(const Function *function, const LogicalTensor
                 if (!op->oOperand[0]->isSubGraphBoundary) {
                     ss << " " << op->GetOpAttribute()->Dump();
                 }
-            } else if ((!IsCopyIn(op->GetOpcode()) && !IsCopyOut(op->GetOpcode())) ||
+            } else if (IsCopyIn(op->GetOpcode()) || (!IsCopyIn(op->GetOpcode()) && !IsCopyOut(op->GetOpcode())) ||
                 function->GetGraphType() != GraphType::BLOCK_GRAPH) {
                 ss << " " << op->GetOpAttribute()->Dump();
             }
