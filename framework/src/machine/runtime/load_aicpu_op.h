1affd98b3ded0fcc37e36dd9261a6506bd3e156c
wan_jianfen <wanjianfen@hisilicon.com>
2026-01-29 19:56:42 +0800

    fix(machine): Change custom to kfc aicpu

--- framework/src/machine/runtime/load_aicpu_op.h
+++ framework/src/machine/runtime/load_aicpu_op.h
@@ -20,6 +20,10 @@
 #include <vector>
 #include <memory>
 
+#ifdef BUILD_WITH_NEW_CANN
+#include "rts/rts_kernel.h"
+#endif
+
 #ifdef BUILD_WITH_CANN
 #include "runtime/mem.h"
 #include "machine/utils/machine_ws_intf.h"
