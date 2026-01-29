f401943e1b871d37fe3f4143f82cdf688c89576d
xiefangming <xiefangming@huawei.com>
2026-01-29 14:10:56 +0800

    feat(operation): Add the BitwiseAnd/Ands/Or/Ors/Xor/Xors/Not methods
    Co-authored-by: xiefangming<xiefangming@huawei.com>
    
    !588 merge bitwiseshiftplanb into master
    
    feat(operation): Add BitwiseShift operation
    
    Created-by: xiefangming
    Commit-by: xiefangming
    Merged-by: cann-robot
    Description: Add BitwiseRightShift and BitwiseLeftShift operation
    
    See merge request: cann/pypto!588

--- framework/src/interface/interpreter/calc.h
+++ framework/src/interface/interpreter/calc.h
@@ -52,6 +52,9 @@ inline void Rsqrt(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
 inline void Sqrt(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
     GetCalcOps()->Sqrt(out, self);
 }
+inline void BitwiseNot(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
+    GetCalcOps()->BitwiseNot(out, self);
+}
 inline void Abs(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
     GetCalcOps()->Abs(out, self);
 }
@@ -110,6 +113,15 @@ inline void DivS(LogicalTensorDataPtr out, LogicalTensorDataPtr self, const Elem
 inline void FmodS(LogicalTensorDataPtr out, LogicalTensorDataPtr self, const Element &scalar, bool reverse = false) {
     GetCalcOps()->FmodS(out, self, scalar, reverse);
 }
+inline void BitwiseAndS(LogicalTensorDataPtr out, LogicalTensorDataPtr self, const Element &scalar, bool reverse = false) {
+    GetCalcOps()->BitwiseAndS(out, self, scalar, reverse);
+}
+inline void BitwiseOrS(LogicalTensorDataPtr out, LogicalTensorDataPtr self, const Element &scalar, bool reverse = false) {
+    GetCalcOps()->BitwiseOrS(out, self, scalar, reverse);
+}
+inline void BitwiseXorS(LogicalTensorDataPtr out, LogicalTensorDataPtr self, const Element &scalar, bool reverse = false) {
+    GetCalcOps()->BitwiseXorS(out, self, scalar, reverse);
+}
 inline void Add(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr other) {
     GetCalcOps()->Add(out, self, other);
 }
@@ -154,6 +166,15 @@ inline void SBitwiseRightShift(LogicalTensorDataPtr out, const Element &scalar, 
 }
 inline void SBitwiseLeftShift(LogicalTensorDataPtr out, const Element &scalar, LogicalTensorDataPtr other) {
     GetCalcOps()->SBitwiseLeftShift(out, scalar, other);
+}
+inline void BitwiseAnd(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr other) {
+    GetCalcOps()->BitwiseAnd(out, self, other);
+}
+inline void BitwiseOr(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr other) {
+    GetCalcOps()->BitwiseOr(out, self, other);
+}
+inline void BitwiseXor(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr other) {
+    GetCalcOps()->BitwiseXor(out, self, other);
 }
 /* used by reducc op, if shape are not same, need masked */
 inline void PairSum(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr other) {
