1eb7b28edbd2ded71a854e46977816ca84f99bdf
zhuqingbo <zhuqingbo@huawei.com>
2026-01-30 09:08:27 +0800

    feat(interpreter): Dump leaf func input and out tensors

--- framework/src/interface/interpreter/function.h
+++ framework/src/interface/interpreter/function.h
@@ -259,6 +259,7 @@ enum class OpInfoCsvHeader {
     rootFuncID,
     funcID,
     verifyType,
+    callopMagic,
     loopInfo,
     opMagic,
     opCode,
@@ -303,8 +304,8 @@ struct FunctionInterpreter {
 
         std::string dumpFilePath = dumpPath + "verify_result.csv";
         execResultFile = fopen(dumpFilePath.c_str(), "w");
-        std::vector<std::string> csvHeader = {"No.", "rootFuncID", "funcID", "verifyType", "LoopInfo", "opMagic", "opCode", 
-            "rawTensorMagic", "tensorMagic", "offset", "inputShape", "inputValidShape", "inputDtype", "inputTensors", 
+        std::vector<std::string> csvHeader = {"No.", "rootFuncID", "funcID", "verifyType", "callopMagic", "loopInfo", "opMagic",
+            "opCode", "rawTensorMagic", "tensorMagic", "offset", "inputShape", "inputValidShape", "inputDtype", "inputTensors", 
             "outputShape", "outputValidShape", "outputDynValidShape", "outputDtype",
             "outputTensor", "verifyResult", "maxAbsDiff", "maxRelDiff", "errorCount", "errorRatio"};
         WriteCsvRow(csvHeader);
