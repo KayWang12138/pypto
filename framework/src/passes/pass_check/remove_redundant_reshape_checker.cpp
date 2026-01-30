ff6025768b82279f9b1798d999474fefdda3e08c
xujiayi1999 <xujiayi28@huawei.com>
2026-01-30 11:27:59 +0800

    fix(pass): RemoveRedundantReshape adds Postcheck

--- framework/src/passes/pass_check/remove_redundant_reshape_checker.cpp
+++ framework/src/passes/pass_check/remove_redundant_reshape_checker.cpp
@@ -1,17 +1,17 @@
-/**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ /**
+* Copyright (c) 2025 Huawei Technologies Co., Ltd.
+* This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+* CANN Open Software License Agreement Version 2.0 (the "License").
+* Please refer to the License for details. You may not use this file except in compliance with the License.
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+* See LICENSE in the root of the software repository for the full text of the License.
+*/
 
 /*!
- * \file remove_redundant_reshape_checker.cpp
- * \brief
- */
+* \file remove_redundant_reshape_checker.cpp
+* \brief
+*/
 
 #include "remove_redundant_reshape_checker.h"
 #include "passes/pass_log/pass_log.h"
@@ -38,6 +38,18 @@ Status RemoveRedundantReshapeChecker::DoPreCheck(Function &function) {
     }
     return SUCCESS;
 }
+
+Status RemoveRedundantReshapeChecker::DoPostCheck(Function &function) {
+    APASS_LOG_INFO_F(Elements::Operation, "PostCheck for RemoveRedundantShape.");
+    for (const auto &op : function.Operations().DuplicatedOpList()) {
+        if (ProcessPostCheck(op)) {
+            APASS_LOG_ERROR_F(Elements::Operation, "Postcheck RemoveRedundantShape failed. %s", GetFormatBacktrace(*op).c_str());
+            return FAILED;
+        }
+    }
+    return SUCCESS;
+}
+
 Status RemoveRedundantReshapeChecker::ProcessPreCheck(const Operation *op) {
     if (op->GetOpcode() == Opcode::OP_RESHAPE) {
         auto in = op->iOperand.front();
@@ -62,5 +74,40 @@ Status RemoveRedundantReshapeChecker::PreCheckReshape(const LogicalTensorPtr &in
     }
     return SUCCESS;
 }
+
+Status RemoveRedundantReshapeChecker::ProcessPostCheck(const Operation *op) {
+    if (op->GetOpcode() == Opcode::OP_RESHAPE) {
+        auto in = op->iOperand.front();
+        if (PostCheckReshape(in) != SUCCESS) {
+            APASS_LOG_ERROR_F(Elements::Operation, "Postcheck of reshape op[%d] failed. %s", 
+            op->GetOpMagic(), GetFormatBacktrace(*op).c_str());
+            return FAILED;
+        }
+    }
+    return SUCCESS;
+}
+
+bool CheckForConsecutiveReshape(const Operation *childOp){
+    for (auto &consumer : childOp->GetOOperands()[0]->GetConsumers()){
+        if (consumer->GetOpcode() == Opcode::OP_RESHAPE){
+            return true;
+        }
+    }
+    return false;   
+}
+
+// Postcheck for reshape
+Status RemoveRedundantReshapeChecker::PostCheckReshape(const LogicalTensorPtr &in) {
+    for (auto &childOp : in->GetConsumers()) {
+        if (childOp->GetOpcode() == Opcode::OP_RESHAPE) {
+            if (CheckForConsecutiveReshape(childOp)) {
+                APASS_LOG_ERROR_F(Elements::Operation, "PostCheckReshape failed: Found consecutive reshape ops.");
+                return FAILED;
+            }
+        }
+    }
+    return SUCCESS;
+}
+
 } // namespace tile_fwk
 } // namespace npu
