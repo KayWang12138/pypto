305586143ddcae0369f0e1d463b933b5b1b5b707
gggxinmeng <guoxinmeng@huawei.com>
2026-01-30 16:05:03 +0800

    fix(Pass): Increase ut coverage rate

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
@@ -205,8 +205,6 @@ private:
     Status InitIssueEntry(Operation* op, const std::unordered_map<Operation*, std::pair<OpCoreType, int>> &opCoreMap);
     void InitCoreConfig(const std::vector<Operation *> &operations);
     Status InitIssueCoreType(IssueEntryPtr issue, Operation* op, const std::unordered_map<Operation*, std::pair<OpCoreType, int>> &opCoreMap);
-    void InitUsedCore();
-    void UpdateUsedCore(IssueEntryPtr issue);
     void InitMemorySize();
     Status CheckOpBufferSize(Operation *op);
     std::string dumpOpInfo(Operation &op);
