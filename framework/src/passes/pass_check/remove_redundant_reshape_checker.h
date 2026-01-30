e3aef1159e0676875df211b3e77608d247c68847
xujiayi1999 <xujiayi28@huawei.com>
2026-01-30 11:36:18 +0800

    fix(pass): RemoveRedundantReshape adds Postcheck

--- framework/src/passes/pass_check/remove_redundant_reshape_checker.h
+++ framework/src/passes/pass_check/remove_redundant_reshape_checker.h
@@ -1,17 +1,17 @@
- /**
-* Copyright (c) 2025 Huawei Technologies Co., Ltd.
-* This program is free software, you can redistribute it and/or modify it under the terms and conditions of
-* CANN Open Software License Agreement Version 2.0 (the "License").
-* Please refer to the License for details. You may not use this file except in compliance with the License.
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
-* INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
-* See LICENSE in the root of the software repository for the full text of the License.
-*/
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 /*!
-* \file remove_redundant_reshape_checker.h
-* \brief
-*/
+ * \file remove_redundant_reshape_checker.h
+ * \brief
+ */
 
 #ifndef REMOVE_REDUNDANT_RESHAPE_CHECKER_H
 #define REMOVE_REDUNDANT_RESHAPE_CHECKER_H
@@ -36,3 +36,4 @@ private:
 };
 } // namespace tile_fwk
 } // namespace npu
+#endif  // REMOVE_REDUNDANT_RESHAPE_CHECKER_H
