4fa3bd0370c60154d0bf62444dde6ba6dd9bec5d
yr12318 <yangrui231@huawei.com>
2026-01-30 16:44:08 +0800

    fix(operation): Switch cube operation to pto lib.

--- framework/src/interface/tileop/cube/cube_pto.h
+++ framework/src/interface/tileop/cube/cube_pto.h
@@ -51,13 +51,28 @@ INLINE int64_t CalNZOffset(const int64_t &srcShape0, const int64_t &srcShape1, c
     return gmOffset;
 }
 
+template <typename T, typename U>
+INLINE bool CheckShapeValid(const T &dst, const U &src) {
+    int64_t dstShape0 = GetShape<0>(dst);
+    int64_t dstShape1 = GetShape<1>(dst);
+    int64_t srcShape0 = GetShape<0>(src);
+    int64_t srcShape1 = GetShape<1>(src);
+    if (dstShape0 == 0 || dstShape1 == 0 || srcShape0 == 0 || srcShape1 == 0) {
+        return false;
+    }
+    return true;
+}
+
 // Copy data from DDR to L1
 template <CopyInMode mode, typename Coord, typename T, typename U>
 TILEOP void TLoad(T &dst, U &src, const Coord &coord, const int64_t &curH, const int64_t &curW) {
     constexpr auto shapeSize = Std::tuple_size<typename T::Shape>::value;
     static_assert(shapeSize == SHAPE_DIM2 && Std::tuple_size<Coord>::value == SHAPE_DIM2, "Shape Size should be 2 Dim");
-    uint16_t offset0 = coord.GetValue();
-    uint16_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
+    if (!CheckShapeValid(dst, src)) {
+        return;
+    }
+    int64_t offset0 = coord.GetValue();
+    int64_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
 
     static_assert(T::FORMAT == Hardware::L1 && U::FORMAT == Hardware::GM,
         "[TLoad Error]: Dst format shoulde be L1 and Src format shoulde be GM");
@@ -154,6 +169,9 @@ INLINE void TLoadND2ND(T &dst, U &src, const int64_t &offset0, const int64_t &of
 // Copy data from UB to UB with ND -> NZ format
 template <typename T, typename U>
 TILEOP void TMoveND2NZ(T &dst, U &src) {
+    if (!CheckShapeValid(dst, src)) {
+        return;
+    }
     constexpr int64_t shapeSize = Std::tuple_size<typename T::Shape>::value;
     static_assert(shapeSize == SHAPE_DIM2, "Shape Size should be 2 Dim");
     static_assert(T::FORMAT == Hardware::UB && U::FORMAT == Hardware::UB);
@@ -178,6 +196,9 @@ TILEOP void TMoveND2NZ(T &dst, U &src) {
 // Copy data from UB to L1 with NZ -> NZ format
 template <typename Coord, typename T, typename U>
 TILEOP void TExtract(T &dst, U &src, const Coord &coord) {
+    if (!CheckShapeValid(dst, src)) {
+        return;
+    }
     constexpr int64_t shapeSize = Std::tuple_size<typename T::Shape>::value;
     constexpr int64_t c0Size = BLOCK_ALIGN_BYTE / sizeof(typename U::Type);
     static_assert(shapeSize == SHAPE_DIM2 && Std::tuple_size<Coord>::value == SHAPE_DIM2, "Shape Size should be 2 Dim");
@@ -265,6 +286,9 @@ TILEOP void TInsertL0CToL1(
 // Copy data from L0C to L1 with quantization ability
 template <typename config, typename Coord, typename T, typename U, typename V>
 TILEOP void TExtract(T &dst, U &src, V &fixbuf, const Coord &l1Coord, const Coord &l0cCoord, uint64_t scaleValue = 0) {
+    if (!CheckShapeValid(dst, src) || !CheckShapeValid(dst, fixbuf)) {
+        return;
+    }
     constexpr int64_t shapeSize = Std::tuple_size<typename T::Shape>::value;
     static_assert(shapeSize == SHAPE_DIM2 && Std::tuple_size<Coord>::value == SHAPE_DIM2, "Shape Size should be 2 Dim");
     static_assert(U::FORMAT == Hardware::L0C && T::FORMAT == Hardware::L1);
@@ -357,10 +381,13 @@ INLINE void TExtractL1ToBTOrFB(T &dst, U &src) {
 // Copy data from L1 to L0A/L0B
 template <bool isTrans, typename Coord, typename T, typename U>
 TILEOP void TExtract(T &dst, U &src, const Coord &coord) {
+    if (!CheckShapeValid(dst, src)) {
+        return;
+    }
     constexpr auto shapeSize = Std::tuple_size<typename T::Shape>::value;
     static_assert(shapeSize == SHAPE_DIM2 && Std::tuple_size<Coord>::value == SHAPE_DIM2, "Shape Size should be 2 Dim");
-    uint16_t offset0 = coord.GetValue();
-    uint16_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
+    int64_t offset0 = coord.GetValue();
+    int64_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
     if constexpr ((T::FORMAT == Hardware::L0A || T::FORMAT == Hardware::L0B) && U::FORMAT == Hardware::L1) {
         TExtractL1ToL0<isTrans>(dst, src, offset0, offset1);
     }
@@ -373,12 +400,15 @@ TILEOP void TExtract(T &dst, U &src, const Coord &coord) {
 // Copy data from L0C to UB
 template <CopyOutMode mode, typename Coord, typename T, typename U>
 TILEOP void TExtract(T &dst, U &src, const Coord &coord, int16_t subblockId) {
+    if (!CheckShapeValid(dst, src)) {
+        return;
+    }
     constexpr auto shapeSize = Std::tuple_size<typename T::Shape>::value;
     constexpr int64_t c0Size = BLOCK_ALIGN_BYTE / sizeof(typename U::Type);
     static_assert(shapeSize == SHAPE_DIM2 && Std::tuple_size<Coord>::value == SHAPE_DIM2, "Shape Size should be 2 Dim");
     if constexpr (T::FORMAT == Hardware::UB && U::FORMAT == Hardware::L0C) {
-        uint16_t offset0 = coord.GetValue();
-        uint16_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
+        int64_t offset0 = coord.GetValue();
+        int64_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
         constexpr auto staticUBH = Std::tuple_element<shapeSize - SHAPE_DIM2, typename T::TileShape>::type::value;
         constexpr auto staticUBW = Std::tuple_element<shapeSize - 1, typename T::TileShape>::type::value;
         constexpr auto staticL0CH = Std::tuple_element<shapeSize - SHAPE_DIM2, typename U::TileShape>::type::value;
@@ -403,7 +433,7 @@ TILEOP void TExtract(T &dst, U &src, const Coord &coord, int16_t subblockId) {
 }
 
 template <bool isZeroC, typename T, typename U, typename V>
-TILEOP void Matmul(T &c, U &a, V &b) {
+TILEOP void TMatmul(T &c, U &a, V &b) {
     constexpr auto shapeSizeA = Std::tuple_size<typename U::Shape>::value;
     constexpr auto shapeSizeB = Std::tuple_size<typename V::Shape>::value;
     constexpr auto shapeSizeC = Std::tuple_size<typename T::Shape>::value;
@@ -420,6 +450,9 @@ TILEOP void Matmul(T &c, U &a, V &b) {
     int64_t validM = GetShape<0>(a);
     int64_t validK = GetShape<1>(a);
     int64_t validN = GetShape<1>(b);
+    if (validM == 0 || validK == 0 || validN == 0) {
+        return;
+    }
 
     using tileL0ATensor = pto::TileLeft<typename U::Type, staticL0AH, staticL0AW, -1, -1>;
     using tileL0BTensor = pto::TileRight<typename V::Type, staticL0BH, staticL0BW, -1, -1>;
@@ -445,7 +478,7 @@ TILEOP void Matmul(T &c, U &a, V &b) {
 }
 
 template <typename T0, typename T1, typename T2, typename T3>
-TILEOP void Matmul(T0 &c, T1 &a, T2 &b, T3 &bias) {
+TILEOP void TMatmul(T0 &c, T1 &a, T2 &b, T3 &bias) {
     constexpr auto shapeSizeA = Std::tuple_size<typename T1::Shape>::value;
     constexpr auto shapeSizeB = Std::tuple_size<typename T2::Shape>::value;
     constexpr auto shapeSizeC = Std::tuple_size<typename T0::Shape>::value;
@@ -468,6 +501,9 @@ TILEOP void Matmul(T0 &c, T1 &a, T2 &b, T3 &bias) {
     int64_t validM = GetShape<0>(a);
     int64_t validK = GetShape<1>(a);
     int64_t validN = GetShape<1>(b);
+    if (validM == 0 || validK == 0 || validN == 0) {
+        return;
+    }
 
     validM = (validM + BLOCK_CUBE_M_N - 1) / BLOCK_CUBE_M_N * BLOCK_CUBE_M_N;
     tileL0ATensor l0a(validM, validK);
@@ -498,6 +534,9 @@ INLINE void TStoreExecute(globalData dstGlobal, tileData srcL0C, V &fixbuf, uint
                 pto::Tile<pto::TileType::Scaling, uint64_t, tileH, tileW, pto::BLayout::RowMajor, -1, -1>;
             int64_t fixShape0 = GetShape<0>(fixbuf);
             int64_t fixShape1 = GetShape<1>(fixbuf);
+            if (fixShape0 == 0 || fixShape1 == 0) {
+                return;
+            }
             fpTileData fpData(fixShape0, fixShape1);
             pto::TASSIGN(fpData, (uint64_t)fixbuf.GetAddr());
             pto::TSTORE_FP<tileData, globalData, fpTileData,
@@ -574,10 +613,13 @@ INLINE void TStoreNZ2NZ(T &dst, U &src, V &fixbuf, const int64_t &offset0, const
 template <typename config, typename Coord, typename T, typename U, typename V>
 TILEOP void TStore(
     T &dst, U &src, V &fixbuf, const Coord &coord, const int64_t &curH, const int64_t &curW, uint64_t scaleValue = 0) {
+    if (!CheckShapeValid(dst, src)) {
+        return;
+    }
     constexpr auto shapeSize = Std::tuple_size<typename T::Shape>::value;
     static_assert(shapeSize == SHAPE_DIM2 && Std::tuple_size<Coord>::value == SHAPE_DIM2, "Shape Size should be 2 Dim");
-    uint16_t offset0 = coord.GetValue();
-    uint16_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
+    int64_t offset0 = coord.GetValue();
+    int64_t offset1 = static_cast<const Std::tuple<size_t> &>(coord).GetValue();
     if constexpr (U::FORMAT == Hardware::L0C && T::FORMAT == Hardware::GM) {
         if constexpr (config::kMode == CopyOutMode::NZ2ND) {
             TStoreNZ2ND<config>(dst, src, fixbuf, offset0, offset1, scaleValue);
