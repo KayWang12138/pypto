4b97345c3da90cee19059ad4fc04e4f0c14fe947
junnyleo <lijun580@huawei.com>
2026-01-31 17:13:46 +0800

    fix python

--- python/pypto/tensor.py
+++ python/pypto/tensor.py
@@ -604,8 +604,18 @@ class Tensor:
         return pypto.triu(self, diagonal)
 
     @source_location
+    def triu_(self: 'Tensor', diagonal: 'int | SymbolicScalar') -> 'Tensor':
+        self.move(pypto.triu(self, diagonal))
+        return self
+    
+    @source_location
     def tril(self: 'Tensor', diagonal: 'int | SymbolicScalar') -> 'Tensor':
         return pypto.tril(self, diagonal)
+
+    @source_location
+    def tril_(self: 'Tensor', diagonal: 'int | SymbolicScalar') -> 'Tensor':
+        self.move(pypto.tril(self, diagonal))
+        return self
 
     @source_location
     def expand_clone(self, shape: List[int], *,
