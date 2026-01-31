4425082326b5c96d0018e2f036b9e7dc19139318
wang-zhiyue2 <wangzhiyue2@huawei.com>
2026-01-31 12:12:28 +0800

    fix(operation): Add constrain on python tensor.

--- python/pypto/converter.py
+++ python/pypto/converter.py
@@ -88,6 +88,12 @@ def from_torch(tensor, name: str = "", dynamic_axis: Optional[List[int]] = None,
             if torch_npu.get_npu_format(tensor) == 29:
                 tensor_format = TileOpFormat.TILEOP_NZ
 
+    block_align_bytes = 32
+    inner_axis_check = tensor.dim() > 0 and isinstance(tensor.shape[-1], int) and \
+        (tensor.shape[-1] * tensor.element_size()) % block_align_bytes != 0
+    if tensor_format == TileOpFormat.TILEOP_NZ and inner_axis_check:
+        raise RuntimeError("NZ format inner axis must be aligned to 32B.")
+
     dtype = _dtype_from(tensor.dtype)
     if tensor.dim() == 0:
         return Tensor(
