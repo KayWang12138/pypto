d9345516483301265a2a19c3f46bc80d49a46007
lijunze <lijunze3@huawei.com>
2026-01-31 16:04:56 +0800

    fix(codegen): Add test cases to verify

--- framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_vector/test_codegen_dyn_round.cpp
+++ framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_vector/test_codegen_dyn_round.cpp
@@ -95,7 +95,7 @@ TEST_F(TestCodegenDynRound, TestDynOpRound) {
     codeGen.GenCode(*function, {});
     std::string res = GetResultFromCpp(*function);
     std::string expect =
-        R"!!!(TFloor(ubTensor_3, ubTensor_4, ubTensor_1, 10);
+        R"!!!(TRound(ubTensor_3, ubTensor_4, ubTensor_1, 10);
 )!!!";
     CheckStringExist(expect, res);
 }
