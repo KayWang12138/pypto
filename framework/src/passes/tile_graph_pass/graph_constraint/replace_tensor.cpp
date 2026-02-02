16fb5ba0e46311df208a79f824a3d7581633435a
wangqun23 <wangqun23@huawei.com>
2026-02-02 14:22:00 +0800

    fix(pass): Fix the problem of false alarms from the postcheck of ReplaceTensor--assemble.

--- framework/src/passes/tile_graph_pass/graph_constraint/replace_tensor.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/replace_tensor.cpp
@@ -841,11 +841,9 @@ void ReplaceTensor::InsertAssembleCopy(Function &function) {
             visitedAssOps.insert(op.GetOpMagic());
             auto assembleIn = op.GetIOperands()[0];
             auto consumers = assembleIn->GetConsumers();
-            if (consumers.size() <= 1) { 
-                continue;
-            }
+
             for (auto &con : consumers) {
-                if (con->GetOpcode() == Opcode::OP_ASSEMBLE) {
+                if (con->GetOpMagic() != op.GetOpMagic() && con->GetOpcode() == Opcode::OP_ASSEMBLE) {
                     visitedAssOps.insert(con->GetOpMagic());
                     needInsertCopyAssOps.insert(con);
                 }
