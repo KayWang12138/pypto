7bf849cdba62bf717341c67a05f5a43e92784853
weixin_44864090 <taojianlong1@huawei.com>
2026-02-02 18:33:38 +0800

    feat(operation): Add testcase for isfinite operation.

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -401,6 +401,8 @@ private:
     std::string PrintRowSumlineDynamicUnaligned(const PrintUnaryTmpBuffParam &param) const;
     std::string PrintRowSumlineStatic(const PrintUnaryTmpBuffParam &param) const;
 
+    std::string PrintIsFinite(const PrintUnaryTmpBuffParam &param) const;
+
     std::string PrintExtractStatic() const;
     std::string PrintExtractDynamicUnaligned() const;
     std::string PrintExtractTileTensor() const;
