99b3dc1961ade4e99b079eb17ca9ab8702887755
zhoumin_ <zhoumin107@huawei.com>
2026-02-02 16:09:43 +0800

    operation check

--- framework/include/tilefwk/tile_shape.h
+++ framework/include/tilefwk/tile_shape.h
@@ -57,13 +57,7 @@ struct CubeTile {
 
     std::string ToString() const;
 };
-
-/**
- * @brief ConvTile tile for conv operation
- *
- */
-struct ConvTile {
-    struct TileL1Info {
+struct TileL1Info {
         int64_t tileHin;
         int64_t tileHout;
         int64_t tileWin;
@@ -74,11 +68,15 @@ struct ConvTile {
         int64_t tileN;
     };
     
-    struct TileL0Info {
-        int64_t tileM;
-        int64_t tileN;
-        };
-        
+struct TileL0Info {
+    int64_t tileM;
+    int64_t tileN;
+    };
+/**
+ * @brief ConvTile tile for conv operation
+ *
+ */
+struct ConvTile {
     TileL1Info tileL1Info;
     TileL0Info tileL0Info;
     bool setL0Tile{false};
