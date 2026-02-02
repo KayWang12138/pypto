ce441a2429bb0e46e0b91c8b6a8360b1de9e411d
2501_92924845 <niulinkai@huawei.com>
2026-02-02 16:41:21 +0800

    fix(pass):Add MergeViewAssemble functionality in RemoveRedundantOp

--- framework/src/passes/tile_graph_pass/graph_optimization/remove_redundant_op.cpp
+++ framework/src/passes/tile_graph_pass/graph_optimization/remove_redundant_op.cpp
@@ -16,6 +16,7 @@
 #include "remove_redundant_op.h"
 #include "passes/pass_check/remove_redundant_op_checker.h"
 #include "passes/pass_utils/dead_operation_eliminate.h"
+#include "passes/pass_utils/merge_view_assemble_impl.h"
 #include "passes/pass_log/pass_log.h"
 
 #define MODULE_NAME "RemoveRedundantOp"
@@ -109,6 +110,13 @@ Status RemoveRedundantOp::RunOnFunction(Function &function) {
             return FAILED;
         }
         iterTime++;
+    }
+    MergeViewAssembleImpl MergeViewAssembleImpl;
+    Status status = MergeViewAssembleImpl.Process(function);
+    if (status != SUCCESS)
+    {
+        APASS_LOG_ERROR_F(Elements::Function, "Merge assemble and view failed.");
+        return status;
     }
     APASS_LOG_INFO_F(Elements::Function, "===> End RemoveRedundantOp");
     return SUCCESS;
