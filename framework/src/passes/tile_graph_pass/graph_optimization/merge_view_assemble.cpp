ca3bb56c34669f67528530b42e77d3dbe9dd3f95
2501_92924845 <niulinkai@huawei.com>
2026-02-02 16:13:58 +0800

    fix logic for merge view assemble

--- framework/src/passes/tile_graph_pass/graph_optimization/merge_view_assemble.cpp
+++ framework/src/passes/tile_graph_pass/graph_optimization/merge_view_assemble.cpp
@@ -16,6 +16,7 @@
 #include "merge_view_assemble.h"
 #include "interface/operation/attribute.h"
 #include "passes/pass_utils/dead_operation_eliminate.h"
+#include "passes/pass_utils/merge_view_assemble_impl.h"
 #include "passes/pass_log/pass_log.h"
 
 #define MODULE_NAME "MergeViewAssemble"
