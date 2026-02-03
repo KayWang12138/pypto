b2bab5fba8e1976cafd7b8929f3e59be199da2a7
jinyijian <jinyijian1@huawei.com>
2026-02-03 09:45:01 +0800

    conv_python fix

--- examples/conv_example.py
+++ examples/conv_example.py
@@ -12,7 +12,6 @@ import numpy as np
 import pypto
 from pypto import (
     tensor, view, function,
-    set_vec_tile_shapes,
     set_conv_tile_shapes,
 )
 from pypto.symbolic_scalar import SymInt
@@ -37,25 +36,64 @@ tileL0Info = pypto_impl.TileL0Info(
     tileN=4          
 )
 
+def run_mm():
+    device_id = int(os.environ.get("TILE_FWK_DEVICE_ID", 0))
+    torch.npu.set_device(device_id)
+    pypto.runtime._device_init()
+    a = pypto.tensor([16, 32], pypto.DT_FP16, "tensor_a")
+    b = pypto.tensor([32, 64], pypto.DT_FP16, "tensor_b")
+    c = pypto.tensor([16, 64], pypto.DT_FP16, "tensor_c")
+    with function("Matmul", a, b, c):
+        pypto.set_debug_options(compile_debug_mode=1)
+        pypto.set_cube_tile_shapes([16, 16], [16, 16], [16, 16])
+        res = pypto.matmul(a, b, pypto.DT_FP16)
+        print(res.shape)
+
 
 @pypto.jit
 def conv_kernel(a, b, c, d):
-    pypto.set_conv_tile_shapes(tileL1Info, tileL0Info)
-    conv_tile = pypto.get_conv_tile_shapes()
-    print(conv_tile)
-    print(f"TileL1Info: Hin={conv_tile[0].tileHin}, Hout={conv_tile[0].tileHout}," 
-          f" Win={conv_tile[0].tileWin}, Wout={conv_tile[0].tileWout},"
-          f" CinFmap={conv_tile[0].tileCinFmap}, CinWeight={conv_tile[0].tileCinWeight},"
-          f" Cout={conv_tile[0].tileCout}, N={conv_tile[0].tileN}")
-    print(f"TileL0Info: H={conv_tile[1].tileH}, W={conv_tile[1].tileW}, K={conv_tile[1].tileK}, N={conv_tile[1].tileN}")
-    print(f"SetL0Tile flag: {conv_tile[2]}")
+    # pypto.set_conv_tile_shapes()
     pypto.set_debug_options(compile_debug_mode=1)
-    d[:] = pypto.conv(a, b, pypto.DT_FP16, c, [1,1,1,1],[0,0,0,0],[1,1,1,1],1) # 将x+1的结果写入函数参数y的原有内存空间
+    d[:] = pypto.conv(a, b, pypto.DT_FP16, [1,1], [0,0,0,0], [1,1], 1) # 将x+1的结果写入函数参数y的原有内存空间
 
-torch.npu.set_device(0)
-a = torch.ones([1, 2, 8, 8, 16], dtype=torch.float16)
-b = torch.ones([2, 2, 16, 16], dtype=torch.float16)
-c = torch.ones([32], dtype=torch.float16)
-d = torch.empty([64, 32], dtype=torch.float16)
-conv_kernel(pypto.from_torch(a), pypto.from_torch(b), pypto.from_torch(a), pypto.from_torch(b))
-print(y) # 输出x + 1的结果
+@pypto.jit
+def mm_kernel(A, B, C):
+    pypto.set_debug_options(compile_debug_mode=1)
+    pypto.set_cube_tile_shapes([16, 16], [16, 16], [16, 16])
+    C[:] = pypto.matmul(A, B, pypto.DT_FP16)
+
+
+def conv2d_a2a3_test():
+    torch.npu.set_device(0)
+    a = torch.ones([1, 2, 8, 8, 16], dtype=torch.float16)
+    b = torch.ones([2, 2, 16, 16], dtype=torch.float16)
+    c = torch.ones([32], dtype=torch.float16)
+    d = torch.empty([1, 2, 8, 8, 16], dtype=torch.float16)
+    conv_kernel(pypto.from_torch(a), pypto.from_torch(b), pypto.from_torch(c), pypto.from_torch(d))
+
+
+def mm_test():
+    A = torch.ones([16, 32], dtype=torch.float16)
+    B = torch.ones([32, 64], dtype=torch.float16)
+    C = torch.ones([16, 64], dtype=torch.float16)
+    # mm_kernel(pypto.from_torch(A), pypto.from_torch(B), pypto.from_torch(C))
+    # print(d)
+
+def conv1d_a5_test():
+    torch.npu.set_device(0)
+    a = torch.ones([1, 32, 8], dtype=torch.float16)
+    b = torch.ones([32, 32, 1], dtype=torch.float16)
+    c = torch.ones([32], dtype=torch.float16)
+    d = torch.empty([1, 32, 8], dtype=torch.float16)
+    conv_kernel(pypto.from_torch(a), pypto.from_torch(b), pypto.from_torch(c), pypto.from_torch(d))
+
+
+def conv2d_a5_test():
+    torch.npu.set_device(0)
+    a = torch.ones([1, 32, 8, 8], dtype=torch.float16)
+    b = torch.ones([32, 32, 1, 1], dtype=torch.float16)
+    c = torch.ones([32], dtype=torch.float16)
+    d = torch.empty([1, 32, 8, 8], dtype=torch.float16)
+    conv_kernel(pypto.from_torch(a), pypto.from_torch(b), pypto.from_torch(c), pypto.from_torch(d))
+
+conv2d_a5_test()
