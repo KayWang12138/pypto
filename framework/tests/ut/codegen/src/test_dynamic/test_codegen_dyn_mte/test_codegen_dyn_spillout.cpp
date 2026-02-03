eae9caad28eae06f222ffbc9303516a38418f13c
Bee.Hujun <Bee.hujun@huawei.com>
2026-02-03 17:40:30 +0800

    feat(codegen): Support L1 spill for TileTensor mode part2

--- framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_mte/test_codegen_dyn_spillout.cpp
+++ framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_mte/test_codegen_dyn_spillout.cpp
@@ -24,13 +24,14 @@
 #include "codegen/symbol_mgr/codegen_symbol.h"
 #include "codegen/cloudnpu/codegen_op_cloudnpu.h"
 #include "codegen/cloudnpu/codegen_cloudnpu.h"
+#include "test_codegen_common.h"
 #include "test_codegen_utils.h"
 
 namespace npu::tile_fwk {
 
 class TestCodegenDynSpillOut : public ::testing::Test {
 public:
-    static void SetUpTestCase() { config::SetCodeGenConfig(KEY_CODEGEN_SUPPORT_TILE_TENSOR, false); }
+    static void SetUpTestCase() {}
 
     static void TearDownTestCase() { config::SetCodeGenConfig(KEY_CODEGEN_SUPPORT_TILE_TENSOR, true); }
 
@@ -45,6 +46,8 @@ public:
 };
 
 TEST_F(TestCodegenDynSpillOut, UBSpillOut) {
+    config::SetCodeGenConfig(KEY_CODEGEN_SUPPORT_TILE_TENSOR, false);
+
     const std::vector<int64_t> shape = {64, 64};
     auto shapeImme = OpImmediate::Specified(shape);
     TileShape::Current().SetVecTile(shape);
@@ -130,5 +133,56 @@ TEST_F(TestCodegenDynSpillOut, L1SpillOut) {
     cop.originShape[1] = shape;
 
     cop.GenOpCode();
+}
+
+TEST_F(TestCodegenDynSpillOut, L1SpillTileTensor) {
+    config::SetCodeGenConfig(KEY_CODEGEN_NEED_COMPILE, false);
+
+    const std::vector<int64_t> shape = {64, 64};
+    auto shapeImme = OpImmediate::Specified(shape);
+    TileShape::Current().SetVecTile(shape);
+
+    InsertTileTensorOp(Opcode::OP_L1_COPY_IN, "TLoad");
+    InsertTileTensorOp(Opcode::OP_L1_COPY_OUT, "TStore");
+
+    Tensor inputA(DT_FP32, shape, "A");
+    Tensor inputB(DT_FP32, shape, "B");
+    Tensor output(DT_FP32, shape, "C");
+
+    std::string funcName = "L1SpillTileTensor";
+    FUNCTION(funcName, {inputA, inputB, output}) {
+        LOOP(funcName, FunctionType::DYNAMIC_LOOP, i, LoopRange(1)) {
+            (void)i;
+            output = Add(inputA, inputB);
+        }
+    }
+    auto function =
+        Program::GetInstance().GetFunctionByRawName(FUNCTION_PREFIX + funcName + SUB_FUNC_SUFFIX + HIDDEN_FUNC_SUFFIX);
+    function->SetUnderDynamicFunction(true);
+
+    const std::vector<SymbolicScalar> dynValidShape = {64, 64};
+    auto ddrTensor = CreateLogicalTensor({*function, DataType::DT_FP32, MemoryType::MEM_DEVICE_DDR, shape, "L1SpillOut",
+        SYMBOL_STACK_BASE, dynValidShape});
+    ddrTensor->UpdateDynValidShape(dynValidShape);
+    auto l1Tensor = CreateLogicalTensor({*function, DataType::DT_FP32, MemoryType::MEM_L1, shape, dynValidShape});
+
+    auto &op = function->rootFunc_->programs_[0]->AddOperation(Opcode::OP_COPY_OUT, {l1Tensor}, {ddrTensor});
+    op.SetOpAttribute(std::make_shared<CopyOpAttribute>(MEM_L1, OpImmediate::Specified({0, 0}), shapeImme, shapeImme));
+    op.SetAttribute("GmTensorParamIdxInCallFunc", 0);
+
+    auto &op2 = function->rootFunc_->programs_[0]->AddOperation(Opcode::OP_COPY_IN, {ddrTensor}, {l1Tensor});
+    op2.SetOpAttribute(std::make_shared<CopyOpAttribute>(OpImmediate::Specified({0, 0}), MEM_L1, shapeImme, shapeImme));
+    op2.SetAttribute("GmTensorParamIdxInCallFunc", 0);
+
+    CodeGenCtx ctx;
+    CodeGenCloudNPU codegen(ctx);
+    codegen.GenCode(*function, {});
+    const std::string res = GetResultFromCpp(*function);
+    std::string expect =
+        R"!!!(TStore<TileOp::TStoreConfig<CopyOutMode::ND2ND, 0, 0>>(gmTensor_9, l1Tensor_10, Coord2Dim(0, 0));)!!!";
+    CheckStringExist(expect, res);
+
+    expect = R"!!!(TLoad<CopyInMode::ND2ND>(l1Tensor_10, gmTensor_9, Coord2Dim(0, 0), 64, 64);)!!!";
+    CheckStringExist(expect, res);
 }
 } // namespace npu::tile_fwk
