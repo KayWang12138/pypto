9f0a051e7906774c8d3aad0583b9a59d638b5d23
guhuangxing <guhuangxing@hisilicon.com>
2026-02-03 17:01:27 +0800

    slove unused variable

--- framework/include/ir/expr.h
+++ framework/include/ir/expr.h
@@ -372,8 +372,8 @@ class Call : public Expr {
    * @return true if the kwarg exists
    */
   bool HasKwarg(const std::string& key) const {
-    for (const auto& [k, _] : kwargs_) {
-      if (k == key) {
+    for (const auto& kv : kwargs_) {
+      if (kv.first == key) {
         return true;
       }
     }
