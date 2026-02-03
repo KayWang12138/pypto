920eacfbc9b6de330eceb8d006d307dcbf38c86b
yanggen <yanggen4@hisilicon.com>
2026-02-03 22:21:46 +0800

    feat(mahcine): Die-to-Die schedule
    Signed-off-by: yanggen <yanggen4@hisilicon.com>

--- framework/src/machine/device/dynamic/wrap_manager.h
+++ framework/src/machine/device/dynamic/wrap_manager.h
@@ -69,6 +69,10 @@ public:
     SendTaskToAiCoreFunc SendTaskToAiCore;
     bool isSupportMixSche {false};
 
+    // for die-to-die shchedule
+    ReadyCoreFunctionQueue* readyDieAicFunctionQue_[DIE_NUM] = {nullptr};
+    ReadyCoreFunctionQueue* readyDieAivFunctionQue_[DIE_NUM] = {nullptr};
+
     inline void InitArchInfo(ArchInfo info) {
         isSupportMixSche = (info == ArchInfo::DAV_3510);
     }
@@ -103,6 +107,10 @@ public:
             free(wrapQueueForThread_.elem);
             wrapQueueForThread_.elem = nullptr;
         }
+    }
+
+    inline bool GeIsMixArch() {
+        return isSupportMixSche;
     }
 
     inline bool GetWrapCoreAvailable(int coreIdx) {
@@ -457,6 +465,31 @@ public:
                 wrapCoreStatus_[coreIdx] = 0;
             }
         }
+    }
+
+    // for die-to-die schedule
+    inline void SetDieReadyQueue(const struct DieReadyQueueData dieReadyFunctionQue) {
+        for (size_t i = 0 ; i < DIE_NUM ; i++) {
+           readyDieAivFunctionQue_[i] =  reinterpret_cast<ReadyCoreFunctionQueue *>(dieReadyFunctionQue.readyDieAivCoreFunctionQue[i]);
+           readyDieAicFunctionQue_[i] =  reinterpret_cast<ReadyCoreFunctionQueue *>(dieReadyFunctionQue.readyDieAicCoreFunctionQue[i]);
+        }
+    }
+
+    inline ReadyCoreFunctionQueue* GetDieReadyQueue(CoreType type, DieId dieId) {
+        ReadyCoreFunctionQueue* readyQueue = nullptr;
+        if (!isSupportMixSche || dieId == DieId::DIE_MIX || dieId == DieId::DIE_UNKNOW) {
+            return readyQueue;
+        }
+
+#ifdef SUPPORT_DIE_TO_DIE_SCHE
+        size_t dieIndex = static_cast<size_t>(dieId);
+        if (type == CoreType::AIC) {
+            readyQueue = readyDieAicFunctionQue_[dieIndex];
+        } else if (type == CoreType::AIV) {
+            readyQueue = readyDieAivFunctionQue_[dieIndex];
+        }
+#endif
+        return readyQueue;
     }
 };
 }
