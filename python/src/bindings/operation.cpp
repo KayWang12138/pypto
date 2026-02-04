097d269d045b26d816c452ea4cffba746b9ca97e
xinweiliu <liuxinwei12@huawei.com>
2026-02-04 17:48:02 +0800

    fix pybind

--- python/src/bindings/operation.cpp
+++ python/src/bindings/operation.cpp
@@ -376,17 +376,6 @@ void bind_operation(py::module &m) {
         py::arg("paddings"), py::arg("dilations"), py::arg("extend_params"), py::arg("groups") = 1,
         "Convolution forward with extend param.");
     m.def(
-        "Conv",
-        [](DataType out_type, const Tensor &tensor_input, const Tensor &tensor_weight, const Tensor &tensor_bias,
-            const std::vector<int64_t> &strides, const std::vector<int64_t> &paddings, const std::vector<int64_t> &dilations,
-            const int64_t groups) {
-            return Conv::Conv(out_type, tensor_input, tensor_weight, tensor_bias, strides, paddings,
-                dilations, groups);
-        },
-        py::arg("out_type"), py::arg("tensor_input"), py::arg("tensor_weight"), py::arg("tensor_bias"),
-        py::arg("strides"), py::arg("paddings"), py::arg("dilations"), py::arg("groups"),
-        "Convolution forward with Transpose is false.");
-    m.def(
         "BatchMatmul",
         [](DataType out_type, const Tensor &tensor_a, const Tensor &tensor_b, bool a_trans, bool b_trans,
             bool c_matrix_nz) {
