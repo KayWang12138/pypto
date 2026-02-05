b1dab5a68a4765ad8e8672b3b5d1d2c2dc355f16
qq_52905393 <liuzhen180@huawei.com>
2026-02-05 20:46:08 +0800

    fix(machine): Revert remove alloc thread idx

--- framework/src/machine/device/dynamic/device_utils.h
+++ framework/src/machine/device/dynamic/device_utils.h
@@ -58,7 +58,6 @@ constexpr int32_t DEVICE_MACHINE_OK = 0;
 constexpr int32_t DEVICE_MACHINE_FINISHED = 1;
 constexpr int32_t TIME_OUT_THRESHOLD = 1000000; // 超时阈值 1s
 constexpr int32_t DFX_TIME_OUT_THRESHOLD = 50000000; // 超时阈值 50s
-constexpr uint32_t CTRL_CPU_THREAD_IDX = 0;
 constexpr int32_t START_AICPU_NUM = 3;
 constexpr uint64_t NUM_FIFTY = 50;
 constexpr uint64_t US_PER_SEC = 1000000;
