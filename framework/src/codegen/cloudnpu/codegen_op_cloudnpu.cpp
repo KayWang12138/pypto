566450013bc54a6b62cec294f36bcdea51e77da0
yang-fulai-3 <yangfulai3@huawei.com>
2026-02-05 14:28:24 +0800

    feat(operation): Add new operation gcd

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
@@ -130,6 +130,7 @@ CodeGenOpCloudNPU::CodeGenOpCloudNPU(const std::shared_ptr<SymbolManager> &symbo
           {Opcode::OP_PAIRMIN, [this]() { return GenBinaryOp(); }},
           {Opcode::OP_BITWISEAND, [this]() { return GenBinaryOp(); }},
           {Opcode::OP_BITWISEOR, [this]() { return GenBinaryOp(); }},
+          {Opcode::OP_GCD, [this]() { return GenBinaryOp(); }},
 
           // binary op: vector operations with tmp
           {Opcode::OP_MOD, [this]() { return GenBinaryOp(); }},
@@ -146,6 +147,7 @@ CodeGenOpCloudNPU::CodeGenOpCloudNPU(const std::shared_ptr<SymbolManager> &symbo
           {Opcode::OP_DIV_BRC, [this]() { return GenBinaryWithBrc(); }},
           {Opcode::OP_MAX_BRC, [this]() { return GenBinaryWithBrc(); }},
           {Opcode::OP_MIN_BRC, [this]() { return GenBinaryWithBrc(); }},
+          {Opcode::OP_GCD_BRC, [this]() { return GenBinaryWithBrc(); }},
 
           // binary op: vector scalar
           {Opcode::OP_ADDS, [this]() { return GenVectorScalarOp(); }},
@@ -158,6 +160,7 @@ CodeGenOpCloudNPU::CodeGenOpCloudNPU(const std::shared_ptr<SymbolManager> &symbo
           {Opcode::OP_BITWISEORS, [this]() { return GenVectorScalarOp(); }},
           {Opcode::OP_BITWISERIGHTSHIFTS, [this]() { return GenVectorScalarOp(); }},
           {Opcode::OP_BITWISELEFTSHIFTS, [this]() { return GenVectorScalarOp(); }},
+          {Opcode::OP_GCDS, [this]() { return GenVectorScalarOp(); }},
 
           // binary op: vector scalar with tmp
           {Opcode::OP_MODS, [this]() { return GenVectorScalarOp(); }},
