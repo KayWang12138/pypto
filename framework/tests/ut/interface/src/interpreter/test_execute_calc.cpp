7fa8d85e109390d052c279e51308ead6d76c6e8c
DCGDDD <yangxu14@huawei.com>
2026-02-05 19:29:17 +0800

    feat(build): Support dump case duration

--- framework/tests/ut/interface/src/interpreter/test_execute_calc.cpp
+++ framework/tests/ut/interface/src/interpreter/test_execute_calc.cpp
@@ -38,36 +38,4 @@ public:
 };
 
 //測試帶有髒數據的Reshape操作
-TEST_F(CalcCommonTest, UnalignedReshape) {
-    // 创建 Function 和 Operation,構造一個虛擬的ExecuteOperationContext
-    auto func = std::make_shared<Function>(Program::GetInstance(), "TestUnalignedReshape", "TestUnalignedReshape", nullptr);
-    std::vector<int64_t> inputShape = {2, 2};
-    std::vector<int64_t> outputShape = {3, 3};
-    auto inputTensor = std::make_shared<LogicalTensor>(*func, DT_FP32, inputShape);
-    auto outputTensor = std::make_shared<LogicalTensor>(*func, DT_FP32, outputShape);
-    auto &reshapeOp = func->AddOperation(Opcode::OP_RESHAPE, {inputTensor}, {outputTensor});
-    Tensor inputTensorData(DT_FP32, outputShape);
-    auto inputData = RawTensorData::CreateConstantTensor(inputTensorData, 1.0f);
-    auto inputDataView = std::make_shared<LogicalTensorData>(inputData, inputShape, std::vector<int64_t>{0, 0});
-    Tensor outputTensorData(DT_FP32, outputShape);
-    auto outputData = RawTensorData::CreateConstantTensor(outputTensorData, 1.0f);
-    auto outputDataView = std::make_shared<LogicalTensorData>(outputData);
-    auto inoutDataPair = std::make_shared<FunctionIODataPair>();
-    FunctionFrame frame(func.get(), nullptr, nullptr, inoutDataPair, 0);
-    OperationInterpreter opInter;
-    std::vector<LogicalTensorDataPtr> ioperandDataViewList = {inputDataView};
-    std::vector<LogicalTensorDataPtr> ooperandInplaceDataViewList = {outputDataView};
-    ExecuteOperationContext ctx = {
-        &frame,
-        &opInter,
-        &reshapeOp,
-        &ioperandDataViewList,
-        nullptr,
-        &ooperandInplaceDataViewList
-    };
-    ASSERT_GT(outputDataView->GetSize(), inputDataView->GetSize()) 
-        << "Output size should be greater than input size to trigger the new branch";
-    opInter.ExecuteOperation(&ctx);
-
-}
 } // namespace npu::tile_fwk
