162fdfb92f8e5abc442b5ba42481901bfe8f387e
qq_52905393 <liuzhen180@huawei.com>
2026-02-05 20:46:08 +0800

    fix(machine): Revert remove alloc thread idx

--- framework/src/machine/runtime/device_launcher.cpp
+++ framework/src/machine/runtime/device_launcher.cpp
@@ -611,7 +611,7 @@ int DeviceLauncher::LaunchAicpuKernel(rtAicpuArgsEx_t &rtArgs, bool tripleStream
         args->kArgs.parameter.runMode = RUN_SPLITTED_STREAM_SCHE;
         startTime = MsprofSysCycleTime();
         ret = rtAicpuKernelLaunchExWithArgs(
-            rtKernelType_t::KERNEL_TYPE_AICPU_KFC, "AST_DYN_AICPU", 3, &rtArgs, nullptr, schedStream, 0);
+            rtKernelType_t::KERNEL_TYPE_AICPU_KFC, "AST_DYN_AICPU", 5, &rtArgs, nullptr, schedStream, 0);
         devRunner.ReportHostProfInfo(startTime, 3, MSPROF_GE_TASK_TYPE_AI_CPU, false);
         return ret;
     } else {
@@ -619,8 +619,8 @@ int DeviceLauncher::LaunchAicpuKernel(rtAicpuArgsEx_t &rtArgs, bool tripleStream
         args->kArgs.parameter.runMode = RUN_UNIFIED_STREAM;
         auto startTime = MsprofSysCycleTime();
         ret = rtAicpuKernelLaunchExWithArgs(
-            rtKernelType_t::KERNEL_TYPE_AICPU_KFC, "AST_DYN_AICPU", nrAicpu, &rtArgs, nullptr, schedStream, 0);
-        devRunner.ReportHostProfInfo(startTime, nrAicpu, MSPROF_GE_TASK_TYPE_AI_CPU, false);
+            rtKernelType_t::KERNEL_TYPE_AICPU_KFC, "AST_DYN_AICPU", devArgs.nrAicpu, &rtArgs, nullptr, schedStream, 0);
+        devRunner.ReportHostProfInfo(startTime, devArgs.nrAicpu, MSPROF_GE_TASK_TYPE_AI_CPU, false);
         return ret;
     }
 #else
