53b375a51d5831821f089bfdd936a44a7fe5a459
gengrunze <gengrunze@huawei.com>
2026-02-05 21:25:58 +0800

    fix(pass): Judge A2A3 & A5 in InsertOpForViewAssemble

--- framework/src/passes/tile_graph_pass/graph_optimization/insert_op_for_viewassemble.cpp
+++ framework/src/passes/tile_graph_pass/graph_optimization/insert_op_for_viewassemble.cpp
@@ -118,17 +118,25 @@ Status InsertOpForViewAssemble::JudgedViewAssemble(Function &function) {
             continue;
         }
         auto &prodOp = *op.GetIOperands()[0]->GetProducers().begin();
-        if (op.GetOOperands()[0]->GetMemoryTypeOriginal() != op.GetIOperands()[0]->GetMemoryTypeOriginal()) {
-            if (notProcessOut_.find(op.GetOOperands()[0]) == notProcessOut_.end()) {
-                notProcessOut_.insert(op.GetOOperands()[0]);
+        if (Platform::Instance().GetSoc().GetNPUArch() != NPUArch::DAV_3510) {
+            if (op.GetOOperands()[0]->GetMemoryTypeOriginal() != op.GetIOperands()[0]->GetMemoryTypeOriginal()) {
+                if (notProcessOut_.find(op.GetOOperands()[0]) == notProcessOut_.end()) {
+                    notProcessOut_.insert(op.GetOOperands()[0]);
+                }
+                continue;
             }
-            continue;
-        }
-        if (prodOp->GetOpcode() == Opcode::OP_VIEW && assembleOutSet_.find(op.GetOOperands()[0]) == assembleOutSet_.end() &&
-            prodOp->GetIOperands()[0]->GetMemoryTypeOriginal() == prodOp->GetOOperands()[0]->GetMemoryTypeOriginal() && 
-            notProcessOut_.find(op.GetOOperands()[0]) == notProcessOut_.end()) {
-            assembleOutSet_.insert(op.GetOOperands()[0]);
-            APASS_LOG_INFO_F(Elements::Operation, "assembleOutSet_ insert oOperand %d", op.GetOOperands()[0]->GetMagic());
+            if (prodOp->GetOpcode() == Opcode::OP_VIEW && assembleOutSet_.find(op.GetOOperands()[0]) == assembleOutSet_.end() &&
+                prodOp->GetIOperands()[0]->GetMemoryTypeOriginal() == prodOp->GetOOperands()[0]->GetMemoryTypeOriginal() && 
+                notProcessOut_.find(op.GetOOperands()[0]) == notProcessOut_.end()) {
+                assembleOutSet_.insert(op.GetOOperands()[0]);
+                APASS_LOG_INFO_F(Elements::Operation, "assembleOutSet_ insert oOperand %d", op.GetOOperands()[0]->GetMagic());
+            }
+        } else {
+            if (prodOp->GetOpcode() == Opcode::OP_VIEW && assembleOutSet_.find(op.GetOOperands()[0]) == assembleOutSet_.end() &&
+            prodOp->GetIOperands()[0]->GetMemoryTypeOriginal() == prodOp->GetOOperands()[0]->GetMemoryTypeOriginal()) {
+                assembleOutSet_.insert(op.GetOOperands()[0]);
+                APASS_LOG_INFO_F(Elements::Operation, "assembleOutSet_ insert oOperand %d", op.GetOOperands()[0]->GetMagic());
+            }
         }
     }
     for (auto assembleOut : assembleOutSet_) {
