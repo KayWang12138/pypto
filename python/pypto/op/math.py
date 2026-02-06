5c5016a9e52f20579d4000f1a81b1c26a00654d5
姜昊 <jianghao131@huawei.com>
2026-02-06 10:40:56 +0800

    feat(operation): Add Hypot operation

--- python/pypto/op/math.py
+++ python/pypto/op/math.py
@@ -229,6 +229,48 @@ def div(input: Tensor, other: Union[Tensor, float]) -> Tensor:
 
 
 @op_wrapper
+def hypot(input: Tensor, other: Tensor) -> Tensor:
+    """Computes the element-wise division of `input` and `other`.
+
+    This function calculates the formula: `out = input / other`.
+    It supports broadcasting between the input tensors.
+
+    Parameters
+    ----------
+    input : Tensor
+        The first input tensor.
+    other : Tensor or Number
+        The second input tensor or a scalar to divide.
+
+    Returns
+    -------
+    Tensor
+        A new tensor containing the element-wise division.
+
+    Raises
+    ------
+    RuntimeError
+        If the two tensors are not broadcastable to a common shape.
+
+    See Also
+    --------
+    sub : The inverse operation, element-wise subtraction.
+    mul : Element-wise multiplication.
+
+    Examples
+    --------
+    a = pypto.tensor([1, 3], pypto.DT_FP32)
+    b = pypto.tensor([1, 3], pypto.DT_FP32)
+    out = pypto.div(a, b)
+
+    Input a:    [[2.0 4.0 6.0]]
+    Input b:    [[2.0 2.0 2.0]]
+    Output out: [[1.0 2.0 3.0]]
+    """
+    return pypto_impl.Hypot(input, other)
+
+
+@op_wrapper
 def fmod(input: Tensor, other: Union[Tensor, float]) -> Tensor:
     """Computes the element-wise modulus of `input` and `other`.
 
