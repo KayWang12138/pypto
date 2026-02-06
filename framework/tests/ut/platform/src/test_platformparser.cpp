e9677b4c167f56d0cda85634cffca4761ca9d0da
jiachengtang <tangjiacheng7@huawei.com>
2026-02-06 10:47:59 +0800

    refactor(platform): Refactoring platform

--- framework/tests/ut/platform/src/test_platformparser.cpp
+++ framework/tests/ut/platform/src/test_platformparser.cpp
@@ -14,9 +14,8 @@
  */
 
 #include "gtest/gtest.h"
-#include "tilefwk/tilefwk.h"
 #include "tilefwk/platform.h"
-#include "interface/utils/file_utils.h"
+#include "platform/parser/platform_parser.h"
 
 using namespace npu::tile_fwk;
 const std::string archInfo = "ArchInfo";
