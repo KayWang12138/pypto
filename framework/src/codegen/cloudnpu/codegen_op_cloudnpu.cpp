e8c9f57a9d35dfc08584fa2f682ebfa8737e966f
姜昊 <jianghao131@huawei.com>
2026-02-06 10:40:56 +0800

    feat(operation): Add Hypot operation

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
@@ -202,6 +202,9 @@ CodeGenOpCloudNPU::CodeGenOpCloudNPU(const std::shared_ptr<SymbolManager> &symbo
           // cmp op
           {Opcode::OP_CMP, [this]() { return GenCmpOp(); }},
           {Opcode::OP_CMPS, [this]() { return GenCmpOp(); }},
+
+          // hypot op
+          {Opcode::OP_HYPOT, [this]() { return GenHypotOp(); }},
       }),
       sortOps_({
           // sort
