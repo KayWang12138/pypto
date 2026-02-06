a21ab06e662e91c94084236833a2cdc519390c07
zhangxiangjie <zhangxiangjie5@huawei.com>
2026-02-06 18:48:11 +0800

    还原function

--- framework/tests/ut/passes/src/test_n_buffer_merge.cpp
+++ framework/tests/ut/passes/src/test_n_buffer_merge.cpp
@@ -93,7 +93,7 @@ TEST_F(NBufferMergeTest, TestNBufferMerge) {
     nbufferPass.PostCheck(*currFunctionPtr);
 }
 
-TEST_F(NBufferMergeTest, TestMulityInputOutput) {
+TEST_F(NBufferMergeTest, TestMulityInputOutputMode3) {
     auto currFunctionPtr = std::make_shared<Function>(Program::GetInstance(), "TestNBufferMerge", "TestNBufferMerge", nullptr);
     EXPECT_TRUE(currFunctionPtr != nullptr);
 
@@ -147,6 +147,41 @@ TEST_F(NBufferMergeTest, TestMulityInputOutput) {
     EXPECT_EQ(NBM.RunOnFunction(*currFunctionPtr), SUCCESS);
     EXPECT_EQ(currFunctionPtr->GetTotalSubGraphCount(), 2);
     currFunctionPtr->DumpJsonFile("./config/pass/json/nBufferMerge_mulity_input_after.json");
+}
+
+TEST_F(NBufferMergeTest, TestMode4) {
+    ComputationalGraphBuilder G;
+    std::vector<int64_t> tileShape{16, 16};
+    const int vecParallelNum = 6;
+    const int result = 11;
+    const int manualMode = 4;
+    EXPECT_EQ(G.AddTensors(DataType::DT_FP32, tileShape, {"incast0", "incast1", "outcast"}), true);
+    EXPECT_EQ(G.AddOps({Opcode::OP_COPY_IN}, {{"incast0"}}, {{"incast1"}}, {"copy_in"}, true), true);
+    G.GetOp("copy_in")->UpdateSubgraphID(0);
+    const int subGraphNum = 20;
+    for (int i = 1; i < subGraphNum; i++) {
+        std::string strID = std::to_string(i);
+        EXPECT_EQ(G.AddTensors(DataType::DT_FP32, tileShape, {"tensor1" + strID, "tensor2" + strID, "tensor3" + strID}), true);
+        std::vector<Opcode> opLists{Opcode::OP_ABS, Opcode::OP_EXP, Opcode::OP_ADDS, Opcode::OP_ASSEMBLE};
+        std::vector<std::vector<std::string>> iOperands{{"incast1"}, {"tensor1" + strID}, {"tensor2" + strID}, {"tensor3" + strID}};
+        std::vector<std::vector<std::string>> oOperands{{"tensor1" + strID}, {"tensor2" + strID}, {"tensor3" + strID}, {"outcast"}};
+        std::vector<std::string> opNames{"ABS_" + strID, "EXP_" + strID, "ADDS_" + strID, "ASSEMBLE_" + strID};
+        EXPECT_EQ(G.AddOps(opLists, iOperands, oOperands, opNames, true), true);
+        G.GetOp("ABS_" + strID)->UpdateSubgraphID(i);
+        G.GetOp("EXP_" + strID)->UpdateSubgraphID(i);
+        G.GetOp("ADDS_" + strID)->UpdateSubgraphID(i);
+        G.GetOp("ASSEMBLE_" + strID)->UpdateSubgraphID(i);
+    }
+    EXPECT_EQ(G.SetInCast({"incast0"}), true);
+    EXPECT_EQ(G.SetOutCast({"outcast"}), true);
+    Function *function = G.GetFunction();
+    function->paramConfigs_.vecNBuffermode = manualMode;
+    function->paramConfigs_.vecNBufferSetting = {{-1, 4}, {1, 2}};
+    function->paramConfigs_.mgVecParallelLb = vecParallelNum;
+    function->SetTotalSubGraphCount(subGraphNum);
+    NBufferMerge NBM;
+    EXPECT_EQ(NBM.RunOnFunction(*function), SUCCESS);
+    EXPECT_EQ(function->GetTotalSubGraphCount(), result);
 }
 
 TEST_F(NBufferMergeTest, TestInvalidMode) {
