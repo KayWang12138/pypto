a9b58f4ff45c8b1354c03b8ff36b6e5bccc206a0
陈展熹 <chenzhanxi1@huawei.com>
2026-02-07 15:32:08 +0800

    feat(Operation): Add Sign OP

--- framework/src/interface/interpreter/calculator/calc_torch.cpp
+++ framework/src/interface/interpreter/calculator/calc_torch.cpp
@@ -147,6 +147,13 @@ static void Neg(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
     ToOperand(tout.second, tout.first, out->GetData()->GetDataType());
 }
 
+static void Sign(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
+    auto tout = From(out);
+    auto tself = From(self);
+    torch::sign_out(tout.second, tself.second);
+    ToOperand(tout.second, tout.first, out->GetData()->GetDataType());
+}
+
 static void Ceil(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
     auto tout = From(out);
     auto tself = From(self);
@@ -1692,6 +1699,7 @@ static struct CalcOps calcOps = {
     .Exp = Exp,
     .Neg = Neg,
     .Rsqrt = Rsqrt,
+    .Sign = Sign,
     .Sqrt = Sqrt,
     .Ceil = Ceil,
     .Floor = Floor,
