e583b96315ae1f39e02459dacd97e92357249e4b
yyyfff <yefan29@huawei.com>
2026-02-09 11:25:07 +0800

    feat(tests): Modify ut tests to new frontend.jit

---
+++ python/tests/ut/interface/test_compile_stage_new_front.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+import os
+import pypto
+import torch
+
+
+def compile_add(a, b, comp_stage):
+    @pypto.frontend.jit(
+        runtime_options={"run_mode": 1}
+    )
+    def add_kernel(a: pypto.Tensor((4, 4), pypto.DT_FP32),
+                b: pypto.Tensor((4, 4), pypto.DT_FP32)) -> pypto.Tensor((4, 4), pypto.DT_FP32):
+        pypto.set_vec_tile_shapes(4, 4)
+        c = a + b
+        return c
+    return add_kernel(a, b)
+
+
+def compile_stage(comp_stage):
+    a = torch.ones((4, 4), dtype=torch.float32) * 2
+    b = torch.ones((4, 4), dtype=torch.float32) * 3
+    output_c = compile_add(a, b, comp_stage)
+
+
+def test_all_compile_stages():
+    compile_stage(pypto.CompStage.TENSOR_GRAPH)
+    compile_stage(pypto.CompStage.TILE_GRAPH)
+    compile_stage(pypto.CompStage.EXECUTE_GRAPH)
+    compile_stage(pypto.CompStage.CODEGEN_INSTRUCTION)
+    compile_stage(pypto.CompStage.CODEGEN_BINARY)
+    assert True
+
+
+if __name__ == "__main__":
+    test_all_compile_stages()
