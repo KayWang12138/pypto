93166ebb7bbb366344f4bf7bf0902a904fa3b1f7
guhuangxing <guhuangxing@hisilicon.com>
2026-02-09 19:25:05 +0800

    修改重复代码及未使用变量

---
+++ framework/include/ir/transform/verify_pass_common.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ * -----------------------------------------------------------------------------------------------------------
+ */
+
+#ifndef PYPTO_IR_TRANSFORM_VERIFY_PASS_COMMON_H_
+#define PYPTO_IR_TRANSFORM_VERIFY_PASS_COMMON_H_
+
+#include "core/error.h"
+#include "core/logging.h"
+#include "ir/function.h"
+#include "ir/kind_traits.h"
+#include "ir/stmt.h"
+#include "ir/transform/base/visitor.h"
+#include "ir/transform/passes.h"
+#include "ir/transform/verification_error.h"
+#include "ir/transform/verifier.h"
+
+#endif  // PYPTO_IR_TRANSFORM_VERIFY_PASS_COMMON_H_
