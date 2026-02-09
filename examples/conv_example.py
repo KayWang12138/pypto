e20d2dab5fe473066ff52b787dc19aff067b2781
zhoumin_ <zhoumin107@huawei.com>
2026-02-09 14:43:00 +0800

    cin

--- examples/conv_example.py
+++ examples/conv_example.py
@@ -37,7 +37,7 @@ def conv_kernel(a, b, d):
     pypto.set_conv_tile_shapes(
         pypto_impl.TileL1Info(
             tileHin=4,
-            tileHout=4,
+            tileHout=1,
             tileWin=8,
             tileWout=8,
             tileCinFmap=16,
@@ -107,7 +107,7 @@ def conv2d_a5_test():
 
 def conv3d_a5_test():
     torch.npu.set_device(0)
-    a = torch.ones([1, 96, 1, 8, 8], dtype=torch.float16)
+    a = torch.ones([1, 96, 1, 16, 1], dtype=torch.float16)
     b = torch.ones([32, 96, 1, 1, 1], dtype=torch.float16)
     c = torch.ones([32], dtype=torch.float16)
     d = torch.empty([1, 32, 1, 8, 8], dtype=torch.float16)
