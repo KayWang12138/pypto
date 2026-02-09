0cc4825403416b983fca3aa0535112f0aacf7c64
arthur_morgan_hw <lipeijie@huawei.com>
2026-02-09 17:21:59 +0800

    feat(Machine):Add tile_fwk_utils and pypto log

---
+++ framework/tests/ut/machine/src/test_log_manager.cpp
@@ -0,0 +1,97 @@
+/**
+* Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_log_manager.cpp
+ * \brief
+ */
+
+#include <gtest/gtest.h>
+#define private public
+#include "utils/host_log/log_manager.h"
+#undef private
+
+namespace tile::fwk {
+class TestLogManager : public testing::Test {
+public:
+    void SetUp() override {
+        unsetenv("ASCEND_GLOBAL_LOG_LEVEL");
+        unsetenv("ASCEND_MODULE_LOG_LEVEL");
+        unsetenv("ASCEND_GLOBAL_EVENT_ENABLE");
+        unsetenv("ASCEND_PROCESS_LOG_PATH");
+    }
+    void TearDown() override {
+        unsetenv("ASCEND_GLOBAL_LOG_LEVEL");
+        unsetenv("ASCEND_MODULE_LOG_LEVEL");
+        unsetenv("ASCEND_GLOBAL_EVENT_ENABLE");
+        unsetenv("ASCEND_PROCESS_LOG_PATH");
+    }
+    void RecoreLog(LogManager &log_manager, const LogLevel logLevel, const char *fmt, ...) {
+        va_list list;
+        va_start(list, fmt);
+        log_manager.Record(logLevel, fmt, list);
+        va_end(list);
+    }
+};
+
+TEST_F(TestLogManager, test_log_level_case0) {
+    LogManager log_manager;
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::ERROR), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::WARN), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::INFO), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::DEBUG), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::EVENT), false);
+    RecoreLog(log_manager, LogLevel::INFO, "I'm a space-bound %s and your heart's the moon", "rocketship");
+    RecoreLog(log_manager, LogLevel::INFO, "And I aiming it right at you, right at you %f", 3.14f);
+    RecoreLog(log_manager, LogLevel::INFO, "%d miles on a clear night in %s", 250000, "June");
+    RecoreLog(log_manager, LogLevel::INFO, "And I'm so lost without you, without you %x", 626);
+}
+
+TEST_F(TestLogManager, test_log_level_case1) {
+    setenv("ASCEND_GLOBAL_LOG_LEVEL", "1", 1);
+    LogManager log_manager;
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::ERROR), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::WARN), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::INFO), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::DEBUG), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::EVENT), false);
+}
+
+TEST_F(TestLogManager, test_log_level_case2) {
+    setenv("ASCEND_GLOBAL_LOG_LEVEL", "1", 1);
+    setenv("ASCEND_MODULE_LOG_LEVEL", "PYPTO=2", 1);
+    LogManager log_manager;
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::ERROR), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::WARN), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::INFO), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::DEBUG), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::EVENT), false);
+}
+
+TEST_F(TestLogManager, test_log_level_case3) {
+    setenv("ASCEND_GLOBAL_EVENT_ENABLE", "1", 1);
+    LogManager log_manager;
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::ERROR), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::WARN), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::INFO), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::DEBUG), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::EVENT), true);
+}
+
+TEST_F(TestLogManager, test_log_level_case4) {
+    setenv("ASCEND_GLOBAL_LOG_LEVEL", "abc", 1);
+    LogManager log_manager;
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::ERROR), true);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::WARN), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::INFO), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::DEBUG), false);
+    EXPECT_EQ(log_manager.CheckLevel(LogLevel::EVENT), false);
+}
+}
