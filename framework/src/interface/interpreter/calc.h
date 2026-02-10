ec832b4c27e39fefb4a496629a46402f598e706a
陈展熹 <chenzhanxi1@huawei.com>
2026-02-10 16:50:37 +0800

    feat(Operation): Add PReLU OP

--- framework/src/interface/interpreter/calc.h
+++ framework/src/interface/interpreter/calc.h
@@ -156,6 +156,9 @@ inline void Cmps(LogicalTensorDataPtr out, LogicalTensorDataPtr self, const Elem
 inline void Hypot(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr other) {
     GetCalcOps()->Hypot(Trans(out), Trans(self), Trans(other));
 }
+inline void PReLU(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr weight) {
+    GetCalcOps()->PReLU(out, self, weight);
+}
 inline void LogicalAnd(LogicalTensorDataPtr out, LogicalTensorDataPtr self, LogicalTensorDataPtr other) {
     GetCalcOps()->LogicalAnd(Trans(out), Trans(self), Trans(other));
 }
