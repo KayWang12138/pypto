2f74238dad148570ef1853c2f5c6724512a80220
xinweiliu <liuxinwei12@huawei.com>
2026-02-10 16:48:26 +0800

    conv pypto + operation + expandfunc

--- framework/src/interface/operation/opcode.cpp
+++ framework/src/interface/operation/opcode.cpp
@@ -914,6 +914,10 @@ std::unordered_map<Opcode, std::string> SUPPORT_TILETENSOR_OPS{
     {         Opcode::OP_L1_COPY_IN,          "TLoad"},
     {       Opcode::OP_L0C_COPY_OUT,         "TStore"},
     {       Opcode::OP_GATHER_IN_L1,    "TGatherInL1"},
+    {    Opcode::OP_L1_COPY_IN_CONV,      "TLoadConv"},
+    {        Opcode::OP_LOAD3D_CONV,        "TLoad3D"},
+    {        Opcode::OP_LOAD2D_CONV,        "TLoad2D"},
+    {  Opcode::OP_L0C_COPY_OUT_CONV,     "TStoreConv"},
 };
 
 std::unordered_set<Opcode> SUPPORT_VF_FUSE_OPS{
