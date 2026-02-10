7db22702d9bf6236560f022257a9cd850947cbed
陈展熹 <chenzhanxi1@huawei.com>
2026-02-10 16:50:37 +0800

    feat(Operation): Add PReLU OP

--- framework/src/interface/operation/vector/unary.cpp
+++ framework/src/interface/operation/vector/unary.cpp
@@ -43,6 +43,68 @@ void TiledUnaryOperation(
     }
 }
 
+void TiledPReLUOperation(
+    Function &function, const TileShape &tileShape, size_t cur, Input &input, Input &weight, const LogicalTensorPtr &result) {
+    if (cur == input.tensor.GetShape().size()) {
+        auto tile = input.tensor.GetStorage()->View(function, input.tileInfo.shape, input.tileInfo.offset);
+        auto weightTile = weight.tensor.GetStorage()->View(function, weight.tileInfo.shape, weight.tileInfo.offset);
+        auto resultTile = result->View(function, input.tileInfo.shape, input.tileInfo.offset);
+        int axis = 5 - (cur + 1) + 1;
+        constexpr size_t ALIGN_SIZE = 32;
+        auto inputDataType = input.tensor.GetStorage()->Datatype();
+        int64_t tmpSize = ALIGN_SIZE / BytesOf(inputDataType);
+        if (axis == 4) {
+            tmpSize = (input.tileInfo.shape[cur] + ALIGN_SIZE - 1) / ALIGN_SIZE * ALIGN_SIZE;
+        }
+        std::vector<int64_t> tmpShape({tmpSize});
+        auto tmpTensor = std::make_shared<LogicalTensor>(function, inputDataType, tmpShape);
+        auto &op = function.AddOperation(Opcode::OP_PRELU, {tile, weightTile}, {resultTile, tmpTensor});
+        op.SetAttribute(OP_ATTR_PREFIX + "axis", axis);
+        return;
+    }
+    auto &vecTile = tileShape.GetVecTile();
+    for (int i = 0; i < input.tensor.GetShape()[cur]; i += vecTile[cur]) {
+        input.tileInfo.shape[cur] = std::min(input.tensor.GetShape()[cur] - i, vecTile[cur]);
+        input.tileInfo.offset[cur] = i;
+        if (cur == 1) {
+            weight.tileInfo.shape[0] = std::min(weight.tensor.GetShape()[0] - i, vecTile[cur]);
+            weight.tileInfo.offset[0] = i;
+        }
+        TiledPReLUOperation(function, tileShape, cur + 1, input, weight, result);
+    }
+}
+
+void PReLUOperationOperandCheck(
+    const std::vector<LogicalTensorPtr> &iOperand, const std::vector<LogicalTensorPtr> &oOperand) {
+    ASSERT(iOperand.size() == 2) << "The input operand size should be 2";
+    ASSERT(oOperand.size() == 1) << "The output operand size should be 1";
+}
+
+void TiledPReLUOperation(
+    Function &function, const TileShape &tileShape, const LogicalTensorPtr &input, const LogicalTensorPtr &weight, const LogicalTensorPtr &result) {
+    ASSERT(input->shape.size() == input->offset.size()) << "The shape size of input and offset must be equal";
+    ASSERT(weight->shape.size() == weight->offset.size()) << "The shape size of weight and offset must be equal";
+
+    TileInfo inputTileInfo(input->shape.size(), input->offset.size());
+    TileInfo weightTileInfo(weight->shape.size(), weight->offset.size());
+    auto inputArg = Input{input, inputTileInfo};
+    auto weightArg = Input{weight, weightTileInfo};
+    TiledPReLUOperation(function, tileShape, 0, inputArg, weightArg, result);
+}
+
+Tensor PReLU(const Tensor &input, const Tensor &weight) {
+    DECLARE_TRACER();
+
+    RETURN_CALL(PReLUOperation, *Program::GetInstance().GetCurrentFunction(), input.GetStorage(), weight.GetStorage());
+}
+
+void PReLUOperationTileFunc(Function &function, const TileShape &tileShape,
+    const std::vector<LogicalTensorPtr> &iOperand, const std::vector<LogicalTensorPtr> &oOperand,
+    [[maybe_unused]] const Operation &op) {
+    PReLUOperationOperandCheck(iOperand, oOperand);
+    TiledPReLUOperation(function, tileShape, iOperand[0], iOperand[1], oOperand[0]);
+}
+
 template <UnaryOpType T>
 void TiledUnaryOperation(
     Function &function, const TileShape &tileShape, const LogicalTensorPtr &operand, const LogicalTensorPtr &result) {
@@ -272,5 +334,6 @@ REGISTER_OPERATION_TILED_FUNC(OP_RECIPROCAL, Opcode::OP_RECIPROCAL, ReciprocalOp
 REGISTER_OPERATION_TILED_FUNC(OP_ABS, Opcode::OP_ABS, AbsOperationTileFunc);
 REGISTER_OPERATION_TILED_FUNC(OP_LN, Opcode::OP_LN, LnOperationTileFunc);
 REGISTER_OPERATION_TILED_FUNC(OP_HUB, Opcode::OP_HUB, HubOperationTileFunc);
+REGISTER_OPERATION_TILED_FUNC(OP_PRELU, Opcode::OP_PRELU, PReLUOperationTileFunc);
 
 } // namespace npu::tile_fwk
