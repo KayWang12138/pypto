661411cdb9f895ac3799effcfa1726cc8e750f0e
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-02-10 20:52:45 +0800

    feat(pass): Add empty commit

--- framework/src/passes/block_graph_pass/insert_sync.cpp
+++ framework/src/passes/block_graph_pass/insert_sync.cpp
@@ -1585,7 +1585,6 @@ Status PipeSync::ProcessViewAssembleOrder(std::vector<Operation *> &opLog, std::
         if (opPtr->GetOpcode() == Opcode::OP_VIEW) {
             if (ProcessViewOrder(*opPtr, opLog, changeMap)) {
                 APASS_LOG_ERROR_F(Elements::Operation, "ProcessViewOrder failed.");
-                
                 return FAILED;
             }
             continue;
