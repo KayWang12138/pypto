c58cc4c8b238507d945567a2caecc693889c9ffe
gggxinmeng <guoxinmeng@huawei.com>
2026-02-10 20:02:54 +0800

    fix(Pass): Modify cv boundaryop order

--- framework/src/passes/block_graph_pass/schedule_ooo/schedule_ooo.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/schedule_ooo.cpp
@@ -69,6 +69,7 @@ void OoOSchedule::OoOHealthCheck(OoOScheduler &oooSchedule, Function &function, 
 Status OoOSchedule::NonMixSchedule(std::vector<Operation*> &opList, Function &function,
     std::pair<uint64_t, Function*> &program, int &maxWorkeSpaceSize) {
     // 直接对oplist进行GenSpill和mainLoop
+    APASS_LOG_INFO_F(Elements::Operation, "=============== START NonMixSchedule ===============");
     OoOScheduler oooSchedule(*program.second, combineAxis);
     if (oooSchedule.Schedule(opList) != SUCCESS) {
         APASS_LOG_ERROR_F(Elements::Operation, "Non-mixGraph schedule failed.");
@@ -128,6 +129,7 @@ Status OoOSchedule::ModifyBoundaryOrder(std::vector<Operation*> &opList) {
 
 Status OoOSchedule::MixSchedule(std::vector<Operation*> &opList, Function &function,
     std::pair<uint64_t, Function*> &program, int &maxWorkeSpaceSize) {
+    APASS_LOG_INFO_F(Elements::Operation, "=============== START MixSchedule ===============");
     std::unordered_map<TargetCoreType, std::string>  targetToString{{TargetCoreType::AIC, "AIC"}, {TargetCoreType::AIV0, "AIV0"}, {TargetCoreType::AIV1, "AIV1"}, {TargetCoreType::UNKNOWN, "UNKNOWN"}};
     TaskSpliter spliter;
     spliter.SplitGraph(opList);
