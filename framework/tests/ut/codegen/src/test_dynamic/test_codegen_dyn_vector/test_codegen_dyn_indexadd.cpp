1fd87e8d03ebbd3b7ed56f2ffc1a6bee51efe1d0
Bee.Hujun <Bee.hujun@huawei.com>
2026-02-10 10:51:04 +0800

    refactor(codegen): Use CS_CODEGEN_INSTRUCTION instead of KEY_CODEGEN_NEED_COMPILE

--- framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_vector/test_codegen_dyn_indexadd.cpp
+++ framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_vector/test_codegen_dyn_indexadd.cpp
@@ -33,10 +33,9 @@ public:
     void SetUp() override {
         Program::GetInstance().Reset();
         config::Reset();
-        config::SetBuildStatic(true);
         config::SetHostOption(COMPILE_STAGE, CS_EXECUTE_GRAPH);
         config::SetPlatformConfig(KEY_ENABLE_COST_MODEL, false);
-        config::SetCodeGenConfig(KEY_CODEGEN_NEED_COMPILE, false);
+        config::SetHostOption(COMPILE_STAGE, CS_CODEGEN_INSTRUCTION);
     }
 
     void TearDown() override {}
