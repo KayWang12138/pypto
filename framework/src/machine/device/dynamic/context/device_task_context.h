cd39f750c4dd5858e3b6d967e0497cbd9ab20d67
duanhaotian <duanhaotian@huawei.com>
2026-02-12 11:19:34 +0800

    fix(machine): Fix some isssues of ctrlflow

--- framework/src/machine/device/dynamic/context/device_task_context.h
+++ framework/src/machine/device/dynamic/context/device_task_context.h
@@ -46,6 +46,8 @@ private:
     npu::tile_fwk::DevStartArgsBase *startArgs_{nullptr};
 private:
     int BuildReadyQueue(DynDeviceTask *dyntask, DevAscendProgram *devProg);
+    void BuildReadyQueueForFunc(DynDeviceTask *dyntask, size_t funcIndex, bool isNeedWrap,
+        uint64_t *opWrapArrayBase, WrapInfoQueue *wrapQueue, uint32_t *wrapTasklistAddr, int &wrapTaskNum);
     void ProcessAivBatchTasks(ReadyCoreFunctionQueue *aivQueue, size_t totalZeroPredAIVBatchEnd,
         const predcount_t *dupPredCountList, size_t funcIndex);
     int InitReadyQueues(DynDeviceTask *dyntask, DevAscendProgram *devProg,
@@ -61,7 +63,8 @@ private:
     void ProcessWrapQueue(DynDeviceTask *dyntask, uint32_t wrapId, int funcIndex, size_t opIndex,
         WrapInfoQueue *wrapQueue, uint32_t *wrapTasklistAddr);
     bool IsNeedWrapProcess(DynDeviceTask *dyntask, DevAscendProgram *devProg);
-
+    void AllocOpWrapList (DynDeviceTask *dyntask, bool isNeedWrap);
+    void AllocOpWrapTaskNumList (DynDeviceTask *dyntask, bool isNeedWrap);
     inline void doResolve(DynDeviceTask *dyntask, int coreType, size_t funcIdx, size_t succIdx, predcount_t *predList) {
         predList[succIdx] -= 1;
         if (predList[succIdx] != 0)
@@ -70,8 +73,13 @@ private:
         if (coreType == static_cast<int>(CoreType::HUB)) {
             ResolveEarlyDepends(dyntask, funcIdx, succIdx);
         } else {
-            int32_t* opWrapList = reinterpret_cast<int32_t *>(dyntask->devTask.mixTaskData.opWrapList[funcIdx]);
-            if (dyntask->devTask.mixTaskData.wrapIdNum > 0 && opWrapList[succIdx] != -1) {
+                /**wraplist**/
+                auto opWrapArrayBase =
+                     reinterpret_cast<uint64_t *>(dyntask->devTask.mixTaskData.opWrapListPtr);
+                int32_t* opWrapList =
+                    (opWrapArrayBase == nullptr) ? nullptr
+                                                 : reinterpret_cast<int32_t *>(opWrapArrayBase[funcIdx]);
+            if (dyntask->devTask.mixTaskData.wrapIdNum > 0 && opWrapList != nullptr && opWrapList[succIdx] != -1) {
                 ProcessWrapQueue(dyntask, MakeMixWrapID(funcIdx, static_cast<uint32_t>(opWrapList[succIdx])), funcIdx, succIdx,
                     reinterpret_cast<WrapInfoQueue *>(dyntask->devTask.mixTaskData.readyWrapCoreFunctionQue),
                     reinterpret_cast<uint32_t *>(dyntask->devTask.mixTaskData.wrapTasklist));
