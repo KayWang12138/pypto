80e888ba8181f6b0c11cffb63df7100f208c15a7
luohui46 <luohui46@huawei.com>
2026-02-12 16:48:44 +0800

    feat(operation): Support op sort32 mrgsort

---
+++ python/tests/st/test_sort.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+"""
+"""
+import os
+import pypto
+import pytest
+import torch
+import numpy as np
+from numpy.testing import assert_allclose
+import torch_npu
+
+
+def test_vector_operation_sort():
+    device_id = int(os.environ.get('TILE_FWK_DEVICE_ID', 0))
+    torch.npu.set_device(device_id)
+    dtype = pypto.DT_FP32
+    n, m = 16, 128
+    shape = (n, m)
+    view_shape = (16, m)
+    tile_shape = (8, m)
+    pypto.runtime._device_init()
+    a = pypto.tensor(shape, dtype, "SORT_TENSOR_a")
+    b = pypto.tensor(shape, dtype, "SORT_TENSOR_b")
+
+    with pypto.function("SORT", a, b):
+        for b_idx in pypto.loop(int(np.ceil(n / view_shape[0])), name="LOOP_SORT_L0", idx_name="b_idx"):
+            for s_idx in pypto.loop(int(np.ceil(m / view_shape[1])), name="LOOP_SORT_L1", idx_name="s_idx"):
+                tile_a = pypto.view(a, view_shape,
+                                    [b_idx * view_shape[0], s_idx * view_shape[1]],
+                                    valid_shape=[(pypto.symbolic_scalar(n) -
+                                                  b_idx * view_shape[0]).min(pypto.symbolic_scalar(view_shape[0])),
+                                                 (pypto.symbolic_scalar(m) - s_idx * view_shape[1]).min(
+                                                     pypto.symbolic_scalar(view_shape[1]))])
+                pypto.set_vec_tile_shapes(tile_shape[0], tile_shape[1])
+                tile_a.move(pypto.sort(tile_a))
+                pypto.assemble(tile_a, [b_idx * view_shape[0], s_idx * view_shape[1]], b)
+
+    a_tensor = (torch.rand(n, m, dtype=torch.float32) * 100 - 50) * 0.123
+    b_tensor = torch.zeros(n, m, dtype=torch.float32)
+
+    pto_a_tensor = pypto.from_torch(a_tensor, "a_tensor")
+    pto_b_tensor = pypto.from_torch(b_tensor, "b_tensor")
+    pypto.runtime._device_run_once_data_from_host(pto_a_tensor, pto_b_tensor)
+
+    val, idx = torch.sort(a_tensor, dim=-1, descending=True, stable=True)
+    assert_allclose(b_tensor.flatten(), expected.flatten(), rtol=1e-3, atol=1e-3)
+    pypto.runtime._device_fini()
+
+
+import pypto
+import torch
+import numpy as np
+from st.pypto_test import TestBuilder
+
+
+# pypto op define, need args: params, tensors
+def op_sort(params, a, b):
+    n, m = a.shape
+    view_shape, tile_shape = params
+    for b_idx in pypto.loop(int(np.ceil(n / view_shape[0])), name="LOOP_RELU_L0", idx_name="b_idx"):
+        for s_idx in pypto.loop(int(np.ceil(m / view_shape[1])), name="LOOP_RELU_L1", idx_name="s_idx"):
+            tile_a = pypto.view(a, view_shape, [b_idx * view_shape[0], s_idx * view_shape[1]], valid_shape=[
+                                 pypto.min(pypto.symbolic_scalar(n) - b_idx * view_shape[0], pypto.symbolic_scalar(n)),
+                                 pypto.min(pypto.symbolic_scalar(m) - b_idx * view_shape[1], pypto.symbolic_scalar(m))])
+            pypto.set_vec_tile_shapes(tile_shape[0], tile_shape[1])
+            tile_a.move(pypto.relu(tile_a))
+            pypto.assemble(tile_a, [b_idx * view_shape[0], s_idx * view_shape[1]], b)
+
+
+def op_sort_golden(param, a, b):
+    val, idx = torch.sort(a, dim=-1, descending=True, stable=True)
+    return val
+
+
+class ReluTest(TestBuilder):
+    def __init__(self, params: tuple, kernel, kernel_golden, tiling: int):
+        super().__init__(params, kernel, kernel_golden, tiling)
+
+    def get_input_from_param(self):
+        n, m = 16, 128
+        a_tensor = torch.rand(n, m, dtype=torch.float32)
+        self.setup_inputs(a_tensor)
+        self.set_tol(rtol=3e-3, atol=3e-3)
+        return (a_tensor, )
+
+
+def test():
+    st = ReluTest(((16, 128), (6, 128)), op_sort, op_sort_golden, tiling=32)
+    st()
