ddcd78395697225190b27517ea75de7016e393b9
toniboehnlein <toni.boehnlein@huawei.com>
2026-02-12 09:55:01 +0000

    decomplexify greedyChildren

--- framework/src/passes/algorithms/osp/bsp/scheduler/GreedySchedulers/GreedyChildren.hpp
+++ framework/src/passes/algorithms/osp/bsp/scheduler/GreedySchedulers/GreedyChildren.hpp
@@ -32,104 +32,163 @@ class GreedyChildren : public Scheduler<GraphT> {
     bool ensureEnoughSources_;
 
   public:
-    GreedyChildren(bool ensureEnoughSources = true) : Scheduler<GraphT>(), ensureEnoughSources_(ensureEnoughSources) {};
+    GreedyChildren(bool ensureEnoughSources = true) : Scheduler<GraphT>(), ensureEnoughSources_(ensureEnoughSources) {}
 
     ReturnStatus ComputeSchedule(BspSchedule<GraphT> &sched) override {
         using VertexType = VertexIdxT<GraphT>;
         const auto &instance = sched.GetInstance();
-
-        for (const auto &v : instance.GetComputationalDag().Vertices()) {
-            sched.SetAssignedProcessor(v, std::numeric_limits<unsigned>::max());
-        }
-
         const auto &graph = instance.GetComputationalDag();
 
-        unsigned superstepCounter = 0;
-
-        std::vector<VertexType> predecessorsCount(instance.NumberOfVertices(), 0);
+        // Initialize state
         std::multiset<std::pair<unsigned, VertexType>, std::greater<>> next;
-        for (const VertexType &i : graph.Vertices()) {
-            if (graph.InDegree(i) == 0)
-                next.emplace(graph.OutDegree(i), i);
-        }
+        std::vector<VertexType> predecessorsCount(instance.NumberOfVertices(), 0);
+        InitializeScheduleState(sched, graph, next);
+
+        unsigned superstepCounter = 0;
 
         while (!next.empty()) {
             std::unordered_set<VertexType> nodesAssignedThisSuperstep;
             std::vector<VWorkwT<GraphT>> processorWeights(instance.NumberOfProcessors(), 0);
-
-            bool fewSources = next.size() < instance.NumberOfProcessors() ? true : false;
+            
+            bool fewSources = next.size() < instance.NumberOfProcessors();
             bool nodeAdded = true;
+            
             while (!next.empty() && nodeAdded) {
                 nodeAdded = false;
                 for (auto iter = next.begin(); iter != next.cend(); iter++) {
-                    const auto &node = iter->second;
-                    bool processorSet = false;
-                    bool failedToAllocate = false;
-                    unsigned processorToBeAllocated = 0;
-
-                    for (const auto &par : graph.Parents(node)) {
-                        if (nodesAssignedThisSuperstep.count(par)) {
-                            if (!processorSet) {
-                                const unsigned parProc = sched.AssignedProcessor(par);
-                                if (!instance.IsCompatible(node, parProc)) {
-                                    failedToAllocate = true;
-                                    break;
-                                }
-                                processorSet = true;
-                                processorToBeAllocated = parProc;
-                            } else if (sched.AssignedProcessor(par) != processorToBeAllocated) {
-                                failedToAllocate = true;
-                                break;
-                            }
-                        }
+                    if (TryScheduleNode(sched, instance, graph, iter, nodesAssignedThisSuperstep,
+                                       processorWeights, predecessorsCount, next, superstepCounter)) {
+                        nodeAdded = true;
+                        break;
                     }
-
-                    if (failedToAllocate) {
-                        continue;
-                    }
-
-                    sched.SetAssignedSuperstep(node, superstepCounter);
-                    if (processorSet) {
-                        sched.SetAssignedProcessor(node, processorToBeAllocated);
-                    } else {
-                        VWorkwT<GraphT> minWeight = std::numeric_limits<VWorkwT<GraphT>>::max();
-                        unsigned bestProc = std::numeric_limits<unsigned>::max();
-                        for (unsigned p = 0; p < instance.NumberOfProcessors(); ++p) {
-                            if (instance.IsCompatible(node, p)) {
-                                if (processorWeights[p] < minWeight) {
-                                    minWeight = processorWeights[p];
-                                    bestProc = p;
-                                }
-                            }
-                        }
-                        sched.SetAssignedProcessor(node, bestProc);
-                    }
-
-                    nodesAssignedThisSuperstep.emplace(node);
-                    processorWeights[sched.AssignedProcessor(node)] += graph.VertexWorkWeight(node);
-                    std::vector<VertexType> newNodes;
-                    for (const auto &chld : graph.Children(node)) {
-                        predecessorsCount[chld]++;
-                        if (predecessorsCount[chld] == graph.InDegree(chld)) {
-                            newNodes.emplace_back(chld);
-                        }
-                    }
-                    next.erase(iter);
-                    for (const auto &vrt : newNodes) {
-                        next.emplace(graph.OutDegree(vrt), vrt);
-                    }
-                    nodeAdded = true;
-                    break;
                 }
                 if (ensureEnoughSources_ && fewSources && next.size() >= instance.NumberOfProcessors()) {
                     break;
                 }
             }
-
             superstepCounter++;
         }
 
         return ReturnStatus::OSP_SUCCESS;
+    }
+
+  private:
+    void InitializeScheduleState(
+        BspSchedule<GraphT> &sched, const GraphT &graph,
+        std::multiset<std::pair<unsigned, VertexIdxT<GraphT>>, std::greater<>> &next) {
+        
+        for (const auto &v : graph.Vertices()) {
+            sched.SetAssignedProcessor(v, std::numeric_limits<unsigned>::max());
+        }
+
+        for (const auto &i : graph.Vertices()) {
+            if (graph.InDegree(i) == 0) {
+                next.emplace(graph.OutDegree(i), i);
+            }
+        }
+    }
+
+    struct ParentCompatibilityResult {
+        bool compatible;
+        bool processorSet;
+        unsigned processor;
+    };
+
+    ParentCompatibilityResult CheckParentCompatibility(
+        const GraphT &graph, const BspSchedule<GraphT> &sched,
+        const BspInstance<GraphT> &instance, VertexIdxT<GraphT> node,
+        const std::unordered_set<VertexIdxT<GraphT>> &nodesAssignedThisSuperstep) {
+        
+        bool processorSet = false;
+        unsigned processorToBeAllocated = 0;
+
+        for (const auto &par : graph.Parents(node)) {
+            if (nodesAssignedThisSuperstep.count(par)) {
+                if (!processorSet) {
+                    const unsigned parProc = sched.AssignedProcessor(par);
+                    if (!instance.IsCompatible(node, parProc)) {
+                        return {false, false, 0};
+                    }
+                    processorSet = true;
+                    processorToBeAllocated = parProc;
+                } else if (sched.AssignedProcessor(par) != processorToBeAllocated) {
+                    return {false, false, 0};
+                }
+            }
+        }
+
+        return {true, processorSet, processorToBeAllocated};
+    }
+
+    unsigned FindBestProcessor(
+        const BspInstance<GraphT> &instance, VertexIdxT<GraphT> node,
+        const std::vector<VWorkwT<GraphT>> &processorWeights) {
+        
+        VWorkwT<GraphT> minWeight = std::numeric_limits<VWorkwT<GraphT>>::max();
+        unsigned bestProc = std::numeric_limits<unsigned>::max();
+        
+        for (unsigned p = 0; p < instance.NumberOfProcessors(); ++p) {
+            if (instance.IsCompatible(node, p)) {
+                if (processorWeights[p] < minWeight) {
+                    minWeight = processorWeights[p];
+                    bestProc = p;
+                }
+            }
+        }
+        
+        return bestProc;
+    }
+
+    void UpdateReadyQueue(
+        const GraphT &graph, VertexIdxT<GraphT> node,
+        std::vector<VertexIdxT<GraphT>> &predecessorsCount,
+        std::multiset<std::pair<unsigned, VertexIdxT<GraphT>>, std::greater<>> &next) {
+        
+        std::vector<VertexIdxT<GraphT>> newNodes;
+        for (const auto &chld : graph.Children(node)) {
+            predecessorsCount[chld]++;
+            if (predecessorsCount[chld] == graph.InDegree(chld)) {
+                newNodes.emplace_back(chld);
+            }
+        }
+        for (const auto &vrt : newNodes) {
+            next.emplace(graph.OutDegree(vrt), vrt);
+        }
+    }
+
+    bool TryScheduleNode(
+        BspSchedule<GraphT> &sched, const BspInstance<GraphT> &instance,
+        const GraphT &graph,
+        typename std::multiset<std::pair<unsigned, VertexIdxT<GraphT>>, std::greater<>>::iterator iter,
+        std::unordered_set<VertexIdxT<GraphT>> &nodesAssignedThisSuperstep,
+        std::vector<VWorkwT<GraphT>> &processorWeights,
+        std::vector<VertexIdxT<GraphT>> &predecessorsCount,
+        std::multiset<std::pair<unsigned, VertexIdxT<GraphT>>, std::greater<>> &next,
+        unsigned superstepCounter) {
+        
+        const auto &node = iter->second;
+        
+        auto result = CheckParentCompatibility(graph, sched, instance, node, nodesAssignedThisSuperstep);
+        
+        if (!result.compatible) {
+            return false;
+        }
+
+        sched.SetAssignedSuperstep(node, superstepCounter);
+        
+        if (result.processorSet) {
+            sched.SetAssignedProcessor(node, result.processor);
+        } else {
+            sched.SetAssignedProcessor(node, FindBestProcessor(instance, node, processorWeights));
+        }
+
+        nodesAssignedThisSuperstep.emplace(node);
+        processorWeights[sched.AssignedProcessor(node)] += graph.VertexWorkWeight(node);
+        
+        UpdateReadyQueue(graph, node, predecessorsCount, next);
+        next.erase(iter);
+        
+        return true;
     }
 };
 
