aa406d74a0bb79cab13b512c6bdd03101dd458e8
duanhaotian <duanhaotian@huawei.com>
2026-02-12 11:19:34 +0800

    fix(machine): Fix some isssues of ctrlflow

--- framework/src/machine/device/dynamic/wrap_manager.h
+++ framework/src/machine/device/dynamic/wrap_manager.h
@@ -288,7 +288,11 @@ public:
         auto dyntask = reinterpret_cast<DynDeviceTask *>(curDevTask_);
         auto funcId = FuncID(taskId);
         auto opIndex = TaskID(taskId);
-        auto opWrapList = reinterpret_cast<int32_t*>(dyntask->devTask.mixTaskData.opWrapList[funcId]);
+        /**wraplist**/
+        auto opWrapArrayBase =
+             reinterpret_cast<uint64_t *>(dyntask->devTask.mixTaskData.opWrapListPtr);
+        auto opWrapList =
+            reinterpret_cast<int32_t*>(opWrapArrayBase[funcId]);
         if (opWrapList[opIndex] != -1) {
             return MakeMixWrapID(funcId, opWrapList[opIndex]);
         } else {
