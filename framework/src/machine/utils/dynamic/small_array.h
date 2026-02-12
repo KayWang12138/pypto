627d5cd8352197eb69b7086c5c499d8277059797
songyangyang18 <songyangyang18@huawei.com>
2026-02-12 20:46:56 +0800

    feat(machine): Improve dev_error log

--- framework/src/machine/utils/dynamic/small_array.h
+++ framework/src/machine/utils/dynamic/small_array.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ public:
 
     inline void resize(size_t size) {
         if (size > N) {
-            DEV_ERROR("resize %zu exceeds maximum allowed value %zu\n", size, N);
+            DEV_ERROR("[data_valid] resize failed: size %zu exceeds maximum allowed value %d.", size, N);
         }
         DEV_ASSERT(size <= N);
         size_ = size;
