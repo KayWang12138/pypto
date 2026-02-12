984fc000ceca122f68ed8524071ac188bdf80584
Immutable01 <gaoqiancheng@huawei.com>
2026-02-12 20:26:38 +0800

    update: 更新文件 log_manager.cpp
    Signed-off-by: Immutable01 <gaoqiancheng@huawei.com>

--- framework/src/utils/host_log/log_manager.cpp
+++ framework/src/utils/host_log/log_manager.cpp
@@ -143,8 +143,6 @@ void LogManager::SetLogLevel(const LogLevel logLevel) {
 
 bool LogManager::CheckLevel(const LogLevel logLevel) const {
     if (logLevel == LogLevel::EVENT) {
-        std::cout << "a event log" << std::endl;
-        std::cout << enableEvent_ << std::endl;
         return enableEvent_;
     }
     return logLevel >= level_;
