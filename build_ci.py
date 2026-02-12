4a1843751db2898c2022cf09c0520e88a00dd03b
yyyfff <yefan29@huawei.com>
2026-02-12 16:32:01 +0800

    fix(tests): Change distribution method

--- build_ci.py
+++ build_ci.py
@@ -1505,7 +1505,7 @@ class BuildCtrl(CMakeParam):
         cmd = f"{sys.executable} -m pytest {filter_str} -v --durations=0 -s --capture=no"
         cmd += f" --rootdir={self.src_root} {ext} --forked"
         if self.check_pip_dependencies(deps={"pytest-xdist": ">=3.8.0"}, raise_err=False, log_err=False):
-            cmd += " --dist=loadscope"
+            cmd += " --dist=loadfile"
             cmd += " --no-loadscope-reorder"
         # cmd 执行
         origin_env = os.environ.copy()
