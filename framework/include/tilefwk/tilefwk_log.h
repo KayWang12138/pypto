7b9e1fa560c474de3ddfffa5cb9c0e2fe9f51ba2
zhengronggui <zhengronggui@huawei.com>
2026-02-12 17:31:02 +0800

    fix(simulation): Simulation log fix
    Signed-off-by: zhengronggui <zhengronggui@huawei.com>

--- framework/include/tilefwk/tilefwk_log.h
+++ framework/include/tilefwk/tilefwk_log.h
@@ -25,11 +25,11 @@
 
 #define PYPTO 76
 
-#ifndef __DEVICE__
 #ifndef __FILE_NAME__
 #define __FILE_NAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif
 
+#ifndef __DEVICE__
 namespace npu::tile_fwk {
 class TilefwkLogFuncInfo {
 public:
