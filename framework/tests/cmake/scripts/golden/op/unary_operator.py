27e3d09df2b3239faa153d8d5a201655edef0675
Apricityh <wangxu359@huawei.com>
2026-02-12 19:40:05 +0800

    feat(Operation): Add Log1p operation

--- framework/tests/cmake/scripts/golden/op/unary_operator.py
+++ framework/tests/cmake/scripts/golden/op/unary_operator.py
@@ -218,6 +218,7 @@ def unary_operator_func_sin_cos(case_name: str, output: Path) -> bool:
         "OnBoardTest.test_unary_operation_16_16_64_70_tileop_sqrt",
         "OnBoardTest.test_unary_operation_32_32_tileop_reciprocal",
         "OnBoardTest.test_unary_operation_16_32_32_tileop_reciprocal",
+        "OnBoardTest.test_unary_operation_16_16_64_64_tileop_log1p",
         "OnBoardTest.test_unary_operation_16_16_64_64_tileop_reciprocal",
         "OnBoardTest.test_unary_operation_16_16_64_64_tileop_relu",
     ]
@@ -339,6 +340,17 @@ def unary_operator_gen_data(case_name: str, output: Path) -> bool:
             x = np.random.uniform(-1, 1, shape_16_16_64_64_i).astype(dtype)
             x.tofile(x_path)
             x = np.reciprocal(x)
+            x.tofile(o_path)
+    elif case_name == "OnBoardTest.test_unary_operation_16_16_64_64_tileop_log1p":
+        x_path = Path(output, 'x.bin')
+        o_path = Path(output, 'res.bin')
+        complete = x_path.exists() and o_path.exists()
+        if complete:
+            logging.debug("Case(%s), Golden complete.", case_name)
+        else:
+            x = np.random.uniform(-1, 1, shape_16_16_64_64_i).astype(dtype)
+            x.tofile(x_path)
+            x = np.log1p(x)
             x.tofile(o_path)
     elif case_name == "OnBoardTest.test_unary_operation_16_16_64_64_tileop_relu":
         x_path = Path(output, 'x.bin')
