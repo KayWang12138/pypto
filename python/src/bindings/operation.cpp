3321f1afbed100b5a6764fdac8152a73218140e4
luohui46 <luohui46@huawei.com>
2026-02-12 16:48:44 +0800

    feat(operation): Support op sort32 mrgsort

--- python/src/bindings/operation.cpp
+++ python/src/bindings/operation.cpp
@@ -334,6 +334,18 @@ void bind_operation(py::module &m) {
         },
         py::arg("operand"), py::arg("k"), py::arg("axis"), py::arg("islargest") = true, "Tensor topk.");
     m.def(
+        "Sort32",
+        [](const Tensor &self, int index) {
+            return npu::tile_fwk::Sort32(self, index);
+        },
+        py::arg("operand"), py::arg("index"), "Tensor sort32.");
+    m.def(
+        "MrgSort",
+        [](const Tensor &self, int mergesize) {
+            return npu::tile_fwk::MrgSort(self, mergesize);
+        },
+        py::arg("operand"), py::arg("mergesize"), "Tensor mrgsort.");
+    m.def(
         "ArgSort",
         [](const Tensor &self, int axis, bool descending) {
             return npu::tile_fwk::ArgSort(self, axis, descending);
