7f66676ac4a6a4d33ec2da36547535b693ee3ff4
zhengyao <zhengyao12@huawei.com>
2026-02-13 16:15:21 +0800

    refactor(example): Refactor deepseekv3.2 examples with @pypto.frontend.jit

--- models/deepseek_v32_exp/deepseekv32_lightning_indexer_quant.py
+++ models/deepseek_v32_exp/deepseekv32_lightning_indexer_quant.py
@@ -384,7 +384,6 @@ def lightning_indexer(case_name: str) -> bool:
     return True
 
 
-@pytest.mark.skip(reason="large test case")
 def test_lightning_indexer_topk_quant_4_b_2_s1_64k_s2():
     lightning_indexer("LightningIndexerSTest.lightning_indexer_quant_4_b_2_s1_64k_s2")
 
