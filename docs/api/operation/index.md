8770ac3a167936b8d31607a18a97044f8607e688
raoliang_sac <raoliang4@huawei.com>
2026-02-13 15:14:20 +0800

    feat(Operation): Support new operation LReLU

--- docs/api/operation/index.md
+++ docs/api/operation/index.md
@@ -83,5 +83,6 @@ pypto-triu
 pypto-trunc
 pypto-unsqueeze
 pypto-view
+pypto-LReLU
 pypto-where
 ```
--- framework/src/interface/interpreter/calc.h
+++ framework/src/interface/interpreter/calc.h
@@ -133,6 +133,9 @@ inline void WhereSS(
     LogicalTensorDataPtr out, LogicalTensorDataPtr condition, const Element &input, const Element &other) {
     GetCalcOps()->WhereSS(Trans(out), Trans(condition), input, other);
 }
+inline void LReLU(LogicalTensorDataPtr out, LogicalTensorDataPtr self, const Element &alpha) {
+    GetCalcOps()->LReLU(out, self, alpha);
+}
 inline void Ln(LogicalTensorDataPtr out, LogicalTensorDataPtr self) {
     GetCalcOps()->Ln(Trans(out), Trans(self));
 }
