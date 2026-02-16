43675a9b36ab4a251e4b452e2edd63d8a17584f6
georgebisbas <georgios.bismpas@h-partners.com>
2026-02-16 09:55:55 +0100

    distributed: work around v4

--- framework/tests/st/distributed/ops/include/distributed_op_test_suite.h
+++ framework/tests/st/distributed/ops/include/distributed_op_test_suite.h
@@ -40,6 +40,8 @@ void TestAllReduce_v2(OpTestParam &testParam, std::string& goldenDir);
 template<typename T>
 void TestAllReduce_v3(OpTestParam &testParam, std::string& goldenDir);
 template<typename T>
+void TestAllReduce_v4(OpTestParam &testParam, std::string& goldenDir);
+template<typename T>
 void TestShmemMoeDispatch(OpTestParam &testParam, std::string& goldenDir);
 template<typename T>
 void TestAllReduceAddAllReduce(OpTestParam &testParam, std::string& goldenDir);
