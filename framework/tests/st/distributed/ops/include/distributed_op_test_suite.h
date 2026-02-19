e745241502d06f5858523519bb306678fb0a8f3a
georgebisbas <georgios.bismpas@h-partners.com>
2026-02-19 15:45:54 +0100

    First effort to add version 6 with one dimension less

--- framework/tests/st/distributed/ops/include/distributed_op_test_suite.h
+++ framework/tests/st/distributed/ops/include/distributed_op_test_suite.h
@@ -44,6 +44,8 @@ void TestAllReduce_v4(OpTestParam &testParam, std::string& goldenDir);
 template<typename T>
 void TestAllReduce_v5(OpTestParam &testParam, std::string& goldenDir);
 template<typename T>
+void TestAllReduce_v6(OpTestParam &testParam, std::string& goldenDir);
+template<typename T>
 void TestAllReduceIREquivalence(OpTestParam &testParam, std::string& goldenDir);
 template<typename T>
 void TestAllReduce_TwoShot_v2(OpTestParam &testParam, std::string& goldenDir);
