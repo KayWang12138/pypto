b9d098eb0b8999bd4157d6de02991f604d3e94a4
georgebisbas <georgios.bismpas@h-partners.com>
2026-02-19 16:35:48 +0100

    tests : Add naming for distributed testing and not only numbers

--- framework/tests/st/distributed/src/test_distributed.cpp
+++ framework/tests/st/distributed/src/test_distributed.cpp
@@ -147,7 +147,12 @@ void GegisterOps()
 
 
 INSTANTIATE_TEST_SUITE_P(TestDistributedOps, DistributedTest,
-    ::testing::ValuesIn(GetOpMetaData<OpMetaData>()));
+    ::testing::ValuesIn(GetOpMetaData<OpMetaData>()),
+    [](const testing::TestParamInfo<OpMetaData>& info) -> std::string {
+        std::string name = info.param.testData_["case_name"].get<std::string>();
+        for (char& c : name) if (!isalnum(c) && c != '_') c = '_';
+        return name;
+    });
 TEST_P(DistributedTest, TestOps)
 {
     RunDistributedTestGeneric(GetParam().testData_, GetParam().fileName_);
