f2d7f72135ff94338f2ac15cd5f58f93b2255251
majiayuan <majiayuan1@huawei.com>
2026-02-24 10:20:06 +0800

    feat(operation): Add Expm1 operation

--- framework/src/interface/interpreter/calc_api.h
+++ framework/src/interface/interpreter/calc_api.h
@@ -50,6 +50,7 @@ struct CalcOps {
 
     void (*Cast)(LogicalTensorDataPtr, LogicalTensorDataPtr, CastMode);
     void (*Exp)(LogicalTensorDataPtr, LogicalTensorDataPtr);
+    void (*Expm1)(LogicalTensorDataPtr, LogicalTensorDataPtr);
     void (*Neg)(LogicalTensorDataPtr, LogicalTensorDataPtr);
     void (*Rsqrt)(LogicalTensorDataPtr, LogicalTensorDataPtr);
     void (*Sqrt)(LogicalTensorDataPtr, LogicalTensorDataPtr);
