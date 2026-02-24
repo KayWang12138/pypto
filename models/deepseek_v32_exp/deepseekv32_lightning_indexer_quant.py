0b8236d38d1e7cdf8be9b86e02b0b3362fabe612
zhengyao <zhengyao12@huawei.com>
2026-02-24 17:44:17 +0800

    refactor(example): Refactor deepseekv3.2 examples with mmout patch

--- models/deepseek_v32_exp/deepseekv32_lightning_indexer_quant.py
+++ models/deepseek_v32_exp/deepseekv32_lightning_indexer_quant.py
@@ -19,6 +19,7 @@ import torch
 import torch_npu
 import numpy as np
 import pypto
+from utils.compare import compare
 
 
 @dataclass
@@ -39,7 +40,7 @@ class LightningIndexerConfigs:
     }
     # tile params
     s1_tile = 2
-    topk_tile = 16384
+    topk_tile = 2048
     # set the tileshape size in cube computation
     c1_tile = [64, 64, 128, 128, 128, 128] # (m, M), (k, K), (n, N)
     c2_tile = [128, 128, 64, 64, 128, 128] # (m, M), (k, K), (n, N)
@@ -265,7 +266,7 @@ def lightning_indexer_compute(input_data_map, params):
                 cur_res, cur_idx = torch.topk(topk_in, k=selected_count, dim=-1) # (1, selected_count)
                 topk_res[(b_idx * s1 + s_idx):(b_idx * s1 + s_idx + 1), :, :] = cur_idx.reshape(1, 1, selected_count)
 
-    return topk_res
+    return topk_res, mm_out
 
 
 def topk_idx_compare(t: torch.Tensor, t_ref: torch.Tensor, name, atol, error_count_threshold):
@@ -327,7 +328,7 @@ def lightning_indexer(case_name: str) -> bool:
     # 根据测试用例名称配置参数
     if case_name == "LightningIndexerSTest.lightning_indexer_quant_4_b_2_s1_64k_s2":
         b, s1 = 4, 2  # batch size和query序列长度
-        act_seq = [64 * 1024] * b  # 每个样本的实际序列长度
+        act_seq = [100] * b  # 每个样本的实际序列长度
     else:
         logging.error("Fail to gen golden for Case(%s)", case_name)
         return False
@@ -363,6 +364,9 @@ def lightning_indexer(case_name: str) -> bool:
     idx_weight_npu = input_data_map["weights"].reshape(b * s1, n1).npu()
     act_seq_key_npu = input_data_map["act_seq"].npu()
     block_table_npu = input_data_map["block_table"].npu()
+    mm_out = torch.zeros([b * s1, max_block_num * block_size], dtype=torch.float32)
+    mm_out_npu = mm_out.npu()
+
 
     unroll_list = [128, 64, 32, 16, 8, 4, 1]
 
@@ -370,15 +374,25 @@ def lightning_indexer(case_name: str) -> bool:
 
     topk_res_npu = lightning_indexer_decode(n1, d, block_size, block_num, unroll_list, configs, selected_count
                     )(idx_query_npu, idx_query_scale_npu, idx_key_cache_npu, idx_key_scale_npu, idx_weight_npu, 
-                    act_seq_key_npu, block_table_npu)
+                    act_seq_key_npu, block_table_npu, mm_out_npu)
 
     torch_npu.npu.synchronize()
 
-    topk_res_golden = lightning_indexer_compute(input_data_map, params)
+    topk_res_golden, mm_out_golden = lightning_indexer_compute(input_data_map, params)
 
-    topk_res_golden = topk_res_golden.reshape(b * s1, 1, selected_count)
+    # topk_res_golden = topk_res_golden.reshape(b * s1, 1, selected_count)
+    # mm_out_golden = mm_out_golden.reshape(b * s1, max_block_num * block_size)
+    torch.set_printoptions(threshold=10000, edgeitems=128, linewidth=140)
 
     # 执行结果比较
+    # print("=====================mm_out_npu==========================")
+    # print(mm_out_npu.cpu())
+    # print(mm_out_golden.cpu())
+    print((mm_out_npu.cpu() - mm_out_golden.cpu()) <= 1e-4)
+    compare(mm_out_npu.cpu(), mm_out_golden.cpu(), "tmp_out", atol=0.000025, rtol=0.005)
+    # print("=====================topk_res_npu==========================")
+    # print(topk_res_npu.cpu())
+    # print(topk_res_golden.cpu())
     topk_idx_compare(topk_res_npu.cpu(), topk_res_golden.cpu(), "topk_res", 5e-3, selected_count)
 
     return True
