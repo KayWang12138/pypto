2d9311ce4655dee421f4b5236fda89461f2618ac
yang-zhipeng19 <yangzhipeng19@huawei.com>
2026-02-24 10:39:50 +0800

    feat(machine): Bind the aicpu and aicore to the same die for DAV3510 optimize

--- framework/src/machine/runtime/device_launcher.h
+++ framework/src/machine/runtime/device_launcher.h
@@ -150,6 +150,20 @@ public:
         return;
     }
 
+    static uint32_t GetAiCpuNumForDav3510(uint32_t aiCpuNum, uint32_t scheCpuNum) {
+        if ((scheCpuNum + dynamic::MAX_OTHER_AICPU_NUM) == aiCpuNum) {
+            return scheCpuNum;
+        }
+
+        uint32_t oneDieMinCpuNum = aiCpuNum >> 1;
+        uint32_t oneDieMaxCpuNum = oneDieMinCpuNum + (aiCpuNum - (oneDieMinCpuNum << 1));
+        uint32_t oneDieMinScheCpuNum = scheCpuNum >> 1;
+        uint32_t oneDieMaxScheCpuNum = oneDieMinScheCpuNum + (scheCpuNum - (oneDieMinScheCpuNum << 1));
+        uint32_t lunchMinCpuNum = oneDieMaxCpuNum + oneDieMaxScheCpuNum + 1;    // more ctrl or singnal cpu
+
+        return lunchMinCpuNum < aiCpuNum ? lunchMinCpuNum : aiCpuNum;
+    }
+
     // Prepare device program scheduling and memory budget related args (keeps <= 50 lines)
     static void PrepareDevProgArgs(DevAscendProgram *devProg, DeviceLauncherConfig &config,
                                   [[maybe_unused]]bool isDevice) {
@@ -164,13 +178,12 @@ public:
         int aiCpuNum = static_cast<int>(Platform::Instance().GetSoc().GetAICPUNum()) - 1;
         devProg->devArgs.scheCpuNum = CalcSchAicpuNumByBlockDim(config.blockdim, aiCpuNum, devProg->devArgs.archInfo);
         config.aicpuNum = devProg->devArgs.scheCpuNum + dynamic::MAX_OTHER_AICPU_NUM;
-        devProg->devArgs.nrAicpu = config.aicpuNum;
         if (devProg->devArgs.archInfo == ArchInfo::DAV_3510) {
-            devProg->devArgs.launchScheCpuNum = aiCpuNum - dynamic::MAX_OTHER_AICPU_NUM;
-            devProg->devArgs.launchAicpu = aiCpuNum;
+            devProg->devArgs.nrAicpu = GetAiCpuNumForDav3510(static_cast<uint32_t>(aiCpuNum), devProg->devArgs.scheCpuNum);
+            devProg->devArgs.launchScheCpuNum = devProg->devArgs.nrAicpu - dynamic::MAX_OTHER_AICPU_NUM;
         } else {
-            devProg->devArgs.launchScheCpuNum = devProg->devArgs.scheCpuNum;
-            devProg->devArgs.launchAicpu = devProg->devArgs.nrAicpu;
+            devProg->devArgs.nrAicpu = config.aicpuNum;
+            devProg->devArgs.launchScheCpuNum = devProg->devArgs.scheCpuNum + 1;
         }
 
 #ifdef BUILD_WITH_CANN
