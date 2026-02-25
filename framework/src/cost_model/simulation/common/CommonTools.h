c58141f7809f4ed1964b44915e442233ff1644b3
zhengronggui <zhengronggui@huawei.com>
2026-02-25 15:41:07 +0800

    fix(simulation): Fix costmodel assert message
    Signed-off-by: zhengronggui <zhengronggui@huawei.com>

--- framework/src/cost_model/simulation/common/CommonTools.h
+++ framework/src/cost_model/simulation/common/CommonTools.h
@@ -16,8 +16,6 @@
 #pragma once
 
 #include "cost_model/simulation/common/CommonType.h"
-#include <cstdio>
-#include <algorithm>
 
 
 namespace CostModel {
@@ -38,25 +36,5 @@ inline int GetMachineType(CostModel::Pid pid)
 inline int GetMachineSeq(CostModel::Pid pid)
 {
     return (pid % PROCESS_ID_OFFSET);
-}
-
-inline void DisablePrintf() {
-    fflush(stdout);
-    FILE* silent_file = fopen("/dev/null", "w");
-    std::swap(*stdout, *silent_file);
-}
-
-inline void EnablePrintf() {
-    if (!g_silent_file) {
-        return;
-    }
-    
-    fflush(stdout);
-    std::swap(*stdout, *g_silent_file);
-    
-    (void)fclose(g_silent_file);
-    g_silent_file = nullptr;
-
-    (void)fflush(stdout);
 }
 }
