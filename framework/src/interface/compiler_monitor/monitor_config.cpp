7152536ddc2f4f54c45449727f5e28341f38ce5c
z00521416 <zhangyongtao7@hisilicon.com>
2026-02-26 16:46:00 +0800

    feat(dfx): Add compiler monitor for tracking compilation progress and detecting timeout.

--- framework/src/interface/compiler_monitor/monitor_config.cpp
+++ framework/src/interface/compiler_monitor/monitor_config.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -8,31 +8,10 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-/*!
- * \file pybind11.cpp
- * \brief
- */
+#include "interface/compiler_monitor/monitor_config.h"
 
-#include "pybind_common.h"
-#include "bindings/bindings.h"
+namespace npu::tile_fwk {
 
-using namespace npu::tile_fwk;
+// Config is in header as static Instance(); this file exists for CMake GLOB consistency.
 
-namespace pypto {
-PYBIND11_MODULE(pypto_impl, m) {
-    m.doc() = "PyPTO";
-    bind_enum(m);
-    BindElement(m);
-    BindTensor(m);
-    BindSymbolicScalar(m);
-    bind_controller(m);
-    bind_operation(m);
-    BindRuntime(m);
-    BindCostModelRuntime(m);
-    bind_pass(m);
-    BindFunction(m);
-    BindDistributed(m);
-    BindPlatform(m);
-    BindMonitor(m);
-};
-} // namespace pypto
+}  // namespace npu::tile_fwk
