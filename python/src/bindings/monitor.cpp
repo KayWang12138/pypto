4e6d5b8f5dcfd0785e5e8c768744936d31951d8b
z00521416 <zhangyongtao7@hisilicon.com>
2026-02-26 16:46:00 +0800

    feat(dfx): Add compiler monitor for tracking compilation progress and detecting timeout.

---
+++ python/src/bindings/monitor.cpp
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file monitor.cpp
+ * \brief
+ */
+
+#include "pybind_common.h"
+#include "interface/compiler_monitor/monitor_manager.h"
+
+using namespace npu::tile_fwk;
+
+namespace pypto {
+
+void BindMonitor(py::module &m) {
+    m.def("InitializeMonitor", []() {
+        MonitorManager::Instance().Initialize();
+    }, "Initialize compiler monitor (called on pypto import).");
+
+    m.def("SetCompilerMonitorOptions", [](bool enable, int interval_sec, int timeout_sec, int total_timeout_sec) {
+        MonitorManager::Instance().SetCompilerMonitorOptions(enable, interval_sec, timeout_sec, total_timeout_sec);
+    }, py::arg("enable") = true, py::arg("interval_sec") = 30, py::arg("timeout_sec") = -1,
+        py::arg("total_timeout_sec") = 600,
+        "Set compiler monitor options: enable, progress print interval in seconds.");
+}
+
+}  // namespace pypto
