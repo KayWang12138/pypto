af9d985828a35d71c40860ea04c93899a844b120
chengding888 <chengding1@hisilicon.com>
2026-02-26 11:10:22 +0800

    feat(machine): support for the function of controlling the number of cores

--- framework/tests/ut/interface/src/configs/test_config_manager.cpp
+++ framework/tests/ut/interface/src/configs/test_config_manager.cpp
@@ -150,6 +150,7 @@ TEST_F(TestConfigManager, NormalRuntimeTest) {
         {STITCH_FUNCTION_NUM_STEP, {0, 128}},
         {STITCH_CFGCACHE_SIZE, {0, 100000000}},
         {STITCH_FUNCTION_SIZE, {1, 65535}},
+        {MAX_CUBE_BLOCKDIM, {0, 24}},
         {CFG_RUN_MODE, {0, 1}},
         {CFG_VALID_SHAPE_OPTIMIZE, {0, 1}},
     };
@@ -168,6 +169,7 @@ TEST_F(TestConfigManager, AbnormalRuntimeTest) {
         {STITCH_FUNCTION_NUM_STEP, {-1, 129}},
         {STITCH_CFGCACHE_SIZE, {-1, 100000001}},
         {STITCH_FUNCTION_SIZE, {0, 65536}},
+        {MAX_CUBE_BLOCKDIM, {-1, 100}},
         {CFG_RUN_MODE, {-1, 2}},
         {CFG_VALID_SHAPE_OPTIMIZE, {-1, 2}},
     };
