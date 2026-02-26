e756c7f768b06ad5a64977a9d6018d656fe43eab
chengding888 <chengding1@hisilicon.com>
2026-02-26 11:10:22 +0800

    feat(machine): support for the function of controlling the number of cores

--- framework/src/machine/runtime/device_launcher.cpp
+++ framework/src/machine/runtime/device_launcher.cpp
@@ -26,16 +26,46 @@ extern "C" __attribute__((weak)) int AdxDataDumpServerInit();
 namespace npu::tile_fwk::dynamic {
 namespace {
     constexpr uint32_t kMinDefaultDim = 20;
+    constexpr uint32_t AICAIVRATIO = 2;
 }
 int GetCfgBlockdim() {
 #ifdef BUILD_WITH_CANN
     auto blk = Platform::Instance().GetSoc().GetAICoreNum();
     blk = blk > 0 ? blk : kMinDefaultDim;
+
+    // 通过GetMaxBlockdim接口获取设置的最大核数，如果设置的最大核数大于硬件物理最大核数时，控核不生效
+    // 如果未进行控核，GetMaxBlockdim接口将通过aclrtGetResInCurrentThread函数返回硬件物理最大核数
+    auto maxBlk = GetMaxBlockdim();
+    blk = maxBlk < static_cast<int>(blk) ? maxBlk : blk;
+
     ALOG_DEBUG_F("Get blockdim[%d].", blk);
     return blk;
 #else
     return kMinDefaultDim;
 #endif
+}
+
+int GetMaxBlockdim() {
+    auto runtimeOptionsMaxBlockDim = config::GetRuntimeOption<int>(MAX_CUBE_BLOCKDIM);
+    if (runtimeOptionsMaxBlockDim != 0) {
+        // runtime options控核的优先级要比set_stream_limit优先级高
+        return runtimeOptionsMaxBlockDim;
+    } else {
+        uint32_t cubeBlockDim = 0;
+        uint32_t vectorBlockDim = 0;
+        aclrtGetResInCurrentThread(ACL_RT_DEV_RES_CUBE_CORE, &cubeBlockDim);
+        aclrtGetResInCurrentThread(ACL_RT_DEV_RES_VECTOR_CORE, &vectorBlockDim);
+        if (vectorBlockDim != cubeBlockDim * AICAIVRATIO) {
+            auto rtsMaxBlockDim = std::min(cubeBlockDim, vectorBlockDim / AICAIVRATIO);
+            ALOG_WARN_F(
+                "The cubeBlockDim[%d] and vectorBlockDim[%d] should be set at a ratio of 1: %d. "
+                "Therefore, the cubeBlockDim and vectorBlockDim are set at %d and %d", 
+                cubeBlockDim, vectorBlockDim, AICAIVRATIO, rtsMaxBlockDim, rtsMaxBlockDim * AICAIVRATIO);
+            return rtsMaxBlockDim;
+        } else {
+            return cubeBlockDim;
+        }
+    }
 }
 
 void (*forceLinkLibraryCompiler)() = &npu::tile_fwk::ForceLinkLibraryCompiler;
