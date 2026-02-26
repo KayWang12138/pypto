1416333e57e4d8e07a9b2ce56e24bf5c48cf4b02
s00831018 <sergio.miguel.martin@huawei.com>
2026-02-26 09:24:02 +0000

    More simplifications2

--- framework/src/machine/device/aicore_manager.h
+++ framework/src/machine/device/aicore_manager.h
@@ -82,7 +82,6 @@ struct sdma_l2_cmo_desc {
 };
 
 #define SDMA_FILE "/dev/sdma"
-
 #define IOCTL_SDMA_L2_CMO  _IOW('s', 3, struct sdma_l2_cmo_desc)
 
 struct DeviceTaskCtrl {
@@ -95,20 +94,16 @@ struct DeviceTaskCtrl {
     int retCode{0};
 
     bool IsFree() { return refcnt == -1; }
-
-    void PutTask(int ret) {
-        if (ret != 0)
-            retCode = ret;
+    void PutTask(int ret)
+    {
+        if (ret != 0) retCode = ret;
 
         auto cnt = refcnt--;
-        while (refcnt.load(std::memory_order_relaxed) != 0)
-            ;
-
-        if (cnt == 1) {
-            if (finishFunc) {
-                finishFunc(devTask);
-            }
-            refcnt = -1;
+        while (refcnt.load(std::memory_order_relaxed) != 0) ;
+        if (cnt == 1)
+        {
+          if (finishFunc != nullptr) finishFunc(devTask);
+          refcnt = -1;
         }
     }
 };
@@ -162,8 +157,139 @@ public:
         for (int i = aicStart_; i < aicEnd_; i++) availableCoreQueue_[(int)MachineType::AIC]->push((uint32_t)i);
     }
 
-    int RunTask(DeviceTaskCtrl *taskCtrl);
-    int Run(int threadIdx, DeviceArgs *deviceArgs, DeviceTaskCtrl *taskCtrl = nullptr);
+    inline int RunTask(DeviceTaskCtrl *taskCtrl)
+    {
+        int ret = npu::tile_fwk::dynamic::DEVICE_MACHINE_OK;
+        DEV_INFO("receive new task %lu\n", taskCtrl->taskId);
+        InitTaskData();
+        if (isLeaderScheduler_ == true)  aicpuTaskManager_.Init(curDevTask_);
+
+        const auto t0 = std::chrono::high_resolution_clock::now();
+
+        npu::tile_fwk::dynamic::TimeCheck tm;
+        uint32_t globalTasksIssued = 0;
+        while (globalTasksIssued < curDevTask_->coreFunctionCnt)
+        {
+            uint32_t tasksIssued = 0;
+            while (tryIssuePendingTasks() > 0) tasksIssued++;
+            checkTaskTerminations();
+            while (tryIssuePendingTasks() > 0) tasksIssued++;
+            globalTasksIssued = curTaskCtrl_->issuedTaskCount.fetch_add(tasksIssued, std::memory_order_relaxed);
+
+            if (npu::tile_fwk::dynamic::CheckTimeOut("wait task send finish.", tm) != 0) return -1;
+        }
+
+        WaitAllAicoreFinish(aivStart_, aivEnd_);
+        WaitAllAicoreFinish(aicStart_, aicEnd_);
+        if (isLeaderScheduler_ == true) while (!aicpuTaskManager_.Finished()) (void)aicpuTaskManager_.TaskProcess();
+
+        const auto tf = std::chrono::high_resolution_clock::now();
+        const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(tf - t0).count();
+        DEV_ERROR("[AICPU %d] Running Time: %ldns", aicpuIdx_, ns);
+
+        if (isLeaderScheduler_ == true) delete availableTaskQueue_;
+        delete runningPairQueue_ ;
+        delete availableCoreQueue_[(int)MachineType::AIV];
+        delete availableCoreQueue_[(int)MachineType::AIC];
+
+        return ret;
+    }
+
+    inline int Run(int threadIdx, DeviceArgs *deviceArgs, DeviceTaskCtrl *taskCtrl = nullptr)
+    {
+        aicpuIdx_ = threadIdx;
+        curTaskCtrl_ = taskCtrl;
+        curDevTask_ = static_cast<DeviceTask *>(taskCtrl->devTask);
+
+        // Getting variable controlling who is the scheduler lead. The first to arrive here should take the lead so that this starts as fast as possible
+        leadSchedulerId_ = (std::atomic<uint32_t>*) &curDevTask_->leadSchedulerId;
+        
+        // Putting myself as leader, if nobody has done it yet
+        uint32_t expectedValue = AICPU_LEAD_SCHEDULER_NULL;
+        isLeaderScheduler_ = leadSchedulerId_->compare_exchange_strong(expectedValue, (uint32_t)aicpuIdx_);
+
+        aicNum_ = deviceArgs->nrAic;
+        aivNum_ = deviceArgs->nrAiv;
+        aicpuNum_ = START_STATIC_AICPU_NUM;
+        aicValidNum_ = deviceArgs->nrValidAic;
+        regAddrs_ = reinterpret_cast<int64_t *>(deviceArgs->coreRegAddr);
+        sharedBuffer_ = deviceArgs->sharedBuffer;
+
+        for (uint32_t idx = 0; idx < MAX_AICORE_NUM; idx++)
+        {
+            auto baseAddress = (uint64_t) regAddrs_[idx];
+            readyRegQueues_[idx]  = (volatile uint64_t *)(baseAddress + (uint64_t)regSprDataMainBase_);
+            finishRegQueues_[idx] = (volatile uint64_t *)(baseAddress + (uint64_t)regSprCond_);
+        }
+
+        auto f = [](int total, int idx, int part, int &start, int &end) {
+            int perCpu = total / part;
+            int remain = total % part;
+            start = idx * perCpu + ((idx < remain) ? idx : remain);
+            end = start + perCpu + ((idx < remain) ? 1 : 0);
+        };
+
+        f(aicValidNum_, aicpuIdx_, aicpuNum_, aicStart_, aicEnd_);
+        f(AIV_NUM_PER_AI_CORE * aicValidNum_, aicpuIdx_, aicpuNum_, aivStart_, aivEnd_);
+        aivStart_ += aicValidNum_;
+        aivEnd_ += aicValidNum_;
+
+        // If I am the lead AICPU scheduler, perform initailization steps
+        if (isLeaderScheduler_ == true)
+        {
+            // Handshake with all AI cores
+            for (uint32_t coreIdx = 0; coreIdx < AIV_CORE_COUNT + AIC_CORE_COUNT; coreIdx++)
+            {
+                auto args = reinterpret_cast<KernelArgs *>((static_cast<uint64_t>(sharedBuffer_)) + SHARED_BUFFER_SIZE * coreIdx);
+                args->taskEntry.reserved[0] = dotStatus_;
+                volatile int64_t *handshakeBuffer = args->shakeBuffer;
+                npu::tile_fwk::dynamic::TimeCheck tm;
+                while ((*handshakeBuffer & 0xFFFFFFFF) != AICORE_SAY_HELLO)
+                {
+                    if (npu::tile_fwk::dynamic::CheckTimeOut("hand shake", tm) != 0) {
+                        DEV_ERROR("hand shake %u timeout.\n", coreIdx);
+                        AbnormalStop();
+                    }
+                }
+            }
+
+            // Setting task as initialized, allowing others to continue
+            curDevTask_->isDeviceInitialized = true;
+        }
+        else // If I am not a lead AICPU scheduler, wait until initialization is ready
+        {
+            while(curDevTask_->isDeviceInitialized == false){ /* Busy wait */ };
+        }
+
+        for (uint32_t coreIdx = 0; coreIdx < AIV_CORE_COUNT + AIC_CORE_COUNT; coreIdx++)
+        {
+            auto args = reinterpret_cast<KernelArgs *>((static_cast<uint64_t>(sharedBuffer_)) + SHARED_BUFFER_SIZE * coreIdx);
+            int64_t *handshakeBuffer = args->shakeBuffer;
+            args_[coreIdx] = args;
+            blockIdToPhyCoreId_[coreIdx] = (*handshakeBuffer >> AICORE_COREID_BIT_OFFSET) & AICORE_COREID_MASK;
+            WriteReg32(coreIdx, REG_SPR_FAST_PATH_ENABLE, REG_SPR_FAST_PATH_OPEN);  // Enabling fast path
+        }
+
+        /* write to MAINBASE reg need reg 0x18 open first */
+        __sync_synchronize();
+
+        int ret = npu::tile_fwk::dynamic::DEVICE_MACHINE_OK;
+        if (taskCtrl != nullptr) {
+            ret = RunTask(taskCtrl);
+        } else {
+            while (true) {
+                taskCtrl = taskQueue_.Dequeue();
+                if (taskCtrl == nullptr)
+                    break;
+                ret = RunTask(taskCtrl);
+                taskCtrl->PutTask(ret);
+            }
+        }
+
+        if (isLeaderScheduler_ == true) NormalStop();
+        return ret;
+    }
+
     void PushTask(DeviceTaskCtrl *taskCtrl) { taskQueue_.Enqueue(taskCtrl); }
 
 private:
@@ -172,10 +298,73 @@ private:
     static inline uint64_t decodePairTask(const aicorePair_t pair) { return pair >> 32; }
     static inline uint64_t decodePairCore(const aicorePair_t pair) { return pair & 0x00000000FFFFFFFFUL; }
 
-    int WaitAllAicoreFinish(int coreIdxStart, int coreIdxEnd);
-    uint64_t TryBatchSendTask();
-    bool checkCoreFinished(const int coreIdx);
-    void ResolveDepForAllAiCore();
+    inline int WaitAllAicoreFinish(int coreIdxStart, int coreIdxEnd)
+    {
+        npu::tile_fwk::dynamic::TimeCheck tm;
+        for (int i = coreIdxStart; i < coreIdxEnd; i++) {
+            while (checkCoreFinished(i) == false)
+            {
+                if (npu::tile_fwk::dynamic::CheckTimeOut("wait tail task", tm) != 0) {
+                    DEV_ERROR("wait tail task finish timeout coreindx=%d.\n", i);
+                    return -1;
+                }
+            }
+        }
+        return 0;
+    }
+
+
+    inline uint64_t tryIssuePendingTasks()
+    {
+        auto taskIdx = (uint64_t)availableTaskQueue_->pop();
+        if (taskIdx == aicoreNullTask) return 0;
+
+        // Getting task's type
+        const auto readyState = reinterpret_cast<CoreFunctionReadyState *>(curDevTask_->coreFunctionReadyStateAddr);
+        const auto coreType = readyState[taskIdx].coreType;
+        auto coreIdx = (uint64_t)availableCoreQueue_[coreType]->pop();
+        if (coreIdx == aicoreNullCore)
+        {
+            availableTaskQueue_->push(taskIdx);
+            return 0;
+        }
+
+        // DEV_ERROR("AICPU %d - Running Task: %lu", aicpuIdx_, *taskSetAddress);
+        SetReadyQueue(coreIdx, taskIdx);
+        runningPairQueue_->push(encodePair(taskIdx, coreIdx));
+        return 1;
+    }
+
+    inline bool checkCoreFinished(const int coreIdx)
+    {
+        uint64_t finTaskVal = GetFinishedTask(coreIdx);
+        uint32_t regLFinTaskState = REG_LOW_TASK_STATE(finTaskVal);
+        if (regLFinTaskState == TASK_FIN_STATE) return true;
+        return false;
+    }
+
+    inline void checkTaskTerminations()
+    {
+        const auto runningPair = runningPairQueue_->pop();
+        if (runningPair == aicoreNullPair) return;
+
+        const int coreIdx = (int)decodePairCore(runningPair);
+
+        if (checkCoreFinished(coreIdx)) 
+        {
+            const uint64_t taskId = (int)decodePairTask(runningPair);
+            ResolveDep(taskId);
+            const auto readyState = reinterpret_cast<CoreFunctionReadyState *>(curDevTask_->coreFunctionReadyStateAddr);
+            const auto coreType = readyState[taskId].coreType; 
+            availableCoreQueue_[coreType]->push(coreIdx);
+        }
+        else
+        {
+            runningPairQueue_->push(runningPair);
+        }
+    }
+
+
     void ResolveVirtualPure(uint64_t dep, CoreFunctionReadyState* readyState);
     void ResolveVirtualMix(uint64_t dep, CoreFunctionReadyState* readyState);
     void ResolveByCoreType(int coretype, uint64_t depTaskId, CoreFunctionReadyState *readyState);
