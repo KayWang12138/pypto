7a91af95b28365d4fc25cee3ba1b6bf056aa2179
KaiqiJin <jinkaiqi@huawei.com>
2026-02-27 17:55:22 +0800

    fix(pass): Remove Repeated in/outcast in leaf function

--- framework/src/passes/tile_graph_pass/subgraph_to_function.cpp
+++ framework/src/passes/tile_graph_pass/subgraph_to_function.cpp
@@ -23,6 +23,7 @@
 #include "passes/pass_check/subgraph_to_function_checker.h"
 #include "passes/pass_utils/graph_utils.h"
 #include "passes/pass_log/pass_log.h"
+#include <set>
 
 
 #define MODULE_NAME "SubgraphToFunction"
@@ -410,19 +411,34 @@ void SubgraphToFunction::SymbolizeFunction(Function &rootFunc, std::vector<Funct
 }
 
 void SubgraphToFunction::InsertParameter(size_t i, Function& leafFunc) {
+    std::set<LogicalTensorPtr> addedIncasts;
+    std::set<LogicalTensorPtr> addedOutcasts;
+
     for (auto &in : subFuncInvokeInfos[i].GetIncastTensorParamList()) {
-        leafFunc.AppendIncast(in.tensor, in.opMagic, in.operandIdx);
+        if (addedIncasts.find(in.tensor) == addedIncasts.end()) {
+            addedIncasts.insert(in.tensor);
+            leafFunc.AppendIncast(in.tensor, in.opMagic, in.operandIdx);
+        }
     }
     for (auto &out : subFuncInvokeInfos[i].GetOutcastTensorParamList()) {
-        leafFunc.AppendOutcast(out.tensor, out.opMagic, out.operandIdx);
+        if (addedOutcasts.find(out.tensor) == addedOutcasts.end()) {
+            addedOutcasts.insert(out.tensor);
+            leafFunc.AppendOutcast(out.tensor, out.opMagic, out.operandIdx);
+        }
     }
     for (auto &tensor : subFuncInvokeInfos[i].GetTensorParamList()) {
         leafFunc.AddGlobalTensor(tensor.tensor);
         if (tensor.isOutputToGM) {
-            leafFunc.AppendOutcast(tensor.tensor, tensor.opMagic, tensor.operandIdx);
+            if (addedOutcasts.find(tensor.tensor) == addedOutcasts.end()) {
+                addedOutcasts.insert(tensor.tensor);
+                leafFunc.AppendOutcast(tensor.tensor, tensor.opMagic, tensor.operandIdx);
+            }
             continue;
         }
-        leafFunc.AppendIncast(tensor.tensor, tensor.opMagic, tensor.operandIdx);
+        if (addedIncasts.find(tensor.tensor) == addedIncasts.end()) {
+            addedIncasts.insert(tensor.tensor);
+            leafFunc.AppendIncast(tensor.tensor, tensor.opMagic, tensor.operandIdx);
+        }
     }
 }
 
