ca373484cf03de5885557e21011aa8aead0cd925
lijunze <lijunze3@huawei.com>
2026-02-27 16:28:38 +0800

    fix

--- framework/src/interface/interpreter/calc_api.h
+++ framework/src/interface/interpreter/calc_api.h
@@ -61,7 +61,7 @@ struct CalcOps {
 
     void (*Cast)(const TensorData &, const TensorData &, CastMode);
     void (*Exp)(const TensorData &, const TensorData &);
-    void (*Exp2)(LogicalTensorDataPtr, LogicalTensorDataPtr);
+    void (*Exp2)(const TensorData &, const TensorData &);
     void (*Neg)(const TensorData &, const TensorData &);
     void (*Rsqrt)(const TensorData &, const TensorData &);
     void (*Sign)(const TensorData &, const TensorData &);
