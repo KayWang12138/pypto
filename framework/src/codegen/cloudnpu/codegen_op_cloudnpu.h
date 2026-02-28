e97ef7cc7ace89eb1703287a67d0faeacd5861cf
yang-fulai-3 <yangfulai3@huawei.com>
2026-02-28 14:56:39 +0800

    feat(operation): Add new operation CumProd

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -120,7 +120,7 @@ public:
     std::string GenIndexOutCastOp() const;
     std::string PrintIndexOutCastTileTensor() const;
 
-    std::string GenCumSumOp() const;
+    std::string GenCumOperationOp() const;
     std::string GenTriULOp() const;
     std::string PrintGatherDynamicUnaligned() const;
     std::string PrintGatherLayout() const;
@@ -448,7 +448,7 @@ private:
     std::string PrintTriULTileTensor(const std::string &diagonal, bool isUpper) const;
 
     std::string PrintCumSumDynamicUnaligned(const PrintCumSumParam &param) const;
-    std::string PrintCumSumTileTensor(int axis) const;
+    std::string PrintCumOperationTileTensor(int axis, bool isSum) const;
 
     WhereParam PrepareWhereParam() const;
     void GetWhereVarAndType(std::vector<std::string> &varExpr, std::vector<std::string> &dataTypeExpr) const;
