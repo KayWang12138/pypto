2fa785e9582dac7268d20d27a03a6aeaa3b27aed
weixin_44864090 <taojianlong1@huawei.com>
2026-02-28 15:05:33 +0800

    feat(operation): Add ceildiv operator

--- python/pypto/op/math.py
+++ python/pypto/op/math.py
@@ -1432,3 +1432,36 @@ def gcd(
         return pypto_impl.Gcd(input, other)
     else:
         return pypto_impl.Gcd(input, pypto_impl.Element(input.dtype, other))
+
+
+@op_wrapper
+def ceil_div(
+    input: Tensor,
+    other:  Union[Tensor, int],
+) -> Tensor:
+    """
+    Calculate the ceiling division of two tensors.
+    Parameters
+    ---------
+    input: Tensor
+        The dividend tensor.
+    other: Tensor or int
+        The divisor tensor or scalar.
+    out: Tensor
+        The tensor after calculating the ceiling division of the corresponding elements of input and other.
+    Examples
+    ---------
+    x = pypto.tensor([2, 3], pypto.data_type.DT_INT32) 
+    y = pypto.tensor([2, 3], pypto.data_type.DT_INT32) 
+    out = pypto.ceildiv(x, y)
+    Input  y : [[1 6 6],
+                [4 6 6]]
+           x : [[1 1 2],
+                [3 4 5]]
+    Output out:[[1 6 3],
+                [2 2 2]]
+    """
+    if isinstance(other, pypto_impl.Tensor):
+        return pypto_impl.CeilDiv(input, other)
+    else:
+        return pypto_impl.CeilDiv(input, pypto_impl.Element(input.dtype, other))
