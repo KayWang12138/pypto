db8699cc7a17f1a23ae4994efecc1ffccfca3488
dingsinuo <dingsinuo@huawei.com>
2026-02-28 17:50:40 +0800

    feat(operation): Add Prod operation

--- framework/include/tilefwk/tilefwk_op.h
+++ framework/include/tilefwk/tilefwk_op.h
@@ -174,6 +174,7 @@ Tensor RowMaxExpand(const Tensor &operand);
 Tensor Sum(const Tensor &self, int axis = -1, bool keepDim=false);
 Tensor Amax(const Tensor &self, int axis = -1, bool keepDim=false);
 Tensor Amin(const Tensor &self, int axis = -1, bool keepDim=false);
+Tensor Prod(const Tensor &self, int axis = -1, bool keepDim=false);
 
 Tensor Compact(const Tensor &operand);
 
