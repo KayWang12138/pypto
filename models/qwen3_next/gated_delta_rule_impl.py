be6cdcbe43e2c8c1155cb8fd8264b19bd2cac3c7
ren-wenqian1 <renwenqian4@huawei.com>
2026-02-28 10:47:50 +0800

    fix(models): Modify the new frontend in gated delta rule operator

--- models/qwen3_next/gated_delta_rule_impl.py
+++ models/qwen3_next/gated_delta_rule_impl.py
@@ -537,7 +537,7 @@ def chunk_gated_delta_rule(b, nqk, nv, d, l):
                 nqk_idx = nv_idx // group
                 pypto.set_vec_tile_shapes(16, 16, 128, 128)
                 last_state = states[b_idx, nv_idx]
-                for s_idx in pypto.loop(0, s, l, name="LOOP_S_TND", idx_name="s_idx"):
+                for s_idx in pypto.loop(0, s, l, name="LOOP_S_TND", idx_name="s_idx", unroll_list=[16, 1]):
                     bs_ofs = b_ofs + s_idx
                     actual_l = (s - s_idx).min(l)
                     ## view
