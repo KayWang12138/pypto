928e7e26f82bc1aa85a0322ed3fe855393358768
wkkk0528 <wangkai578@huawei.com>
2026-02-28 17:39:12 +0800

    fix(vector): Fix cast with satmode

--- framework/src/interface/operation/vector/tensor_transformation.h
+++ framework/src/interface/operation/vector/tensor_transformation.h
@@ -49,10 +49,12 @@ Opcode GetCastOpName() {
 
 template <CastOpType T>
 LogicalTensorPtr TensorCastOperation(
-    Function &function, LogicalTensorPtr self, const DataType &dstDataType, const CastMode &mode = CAST_NONE) {
+    Function &function, LogicalTensorPtr self, const DataType &dstDataType, const CastMode &mode = CAST_NONE,
+    const int64_t &satmode = 0) {
     auto result = std::make_shared<LogicalTensor>(function, dstDataType, self->shape, self->dynValidShape_);
     auto &op = function.AddOperation(GetCastOpName<T>(), {self}, {result});
     op.SetAttribute(OP_ATTR_PREFIX + "mode", mode);
+    op.SetAttribute(OP_ATTR_PREFIX + "satmode", satmode);
     return result;
 }
 
--- framework/src/interface/tileop/vector/cast.h
+++ framework/src/interface/tileop/vector/cast.h
@@ -21,7 +21,7 @@
 
 #define OP_TILE_OP_CAST TCast
 template <typename LastUse = LastUse2Dim<0, 0>, unsigned Mode, typename T0, typename T1>
-TILEOP void TCast(T0 dst, T1 src) {
+TILEOP void TCast(T0 dst, T1 src, pto::Saturation satmode = pto::Saturation::OFF) {
     constexpr auto shapeSize = Std::tuple_size<typename T0::Shape>::value;
     constexpr size_t expectSize = 5;
     const auto dstLayout = dst.GetLayout();
@@ -59,7 +59,7 @@ TILEOP void TCast(T0 dst, T1 src) {
                 auto srcOffset = n0Index * srcStride0 + n1Index * srcStride1 + n2Index * srcStride2;
                 pto::TASSIGN(dstTile, (uint64_t)(dst.GetAddr() + dstOffset * dstTypeSize));
                 pto::TASSIGN(srcTile, (uint64_t)(src.GetAddr() + srcOffset * srcTypeSize));
-                PTO_WITH_LAST_USE(pto::TCVT(dstTile, srcTile, static_cast<pto::RoundMode>(Mode)), n1, n2);
+                PTO_WITH_LAST_USE(pto::TCVT(dstTile, srcTile, static_cast<pto::RoundMode>(Mode), satmode), n1, n2);
             }
         }
     }
