8b99d99aed3f220037c334f5df73afa0a74f4f5c
刘奕哲 <liuyizhe6@huawei.com>
2026-03-02 17:24:51 +0800

    feat: indexput support tile shape dim equal values dim

--- framework/src/interface/operation/vector/indexing.cpp
+++ framework/src/interface/operation/vector/indexing.cpp
@@ -1046,26 +1046,42 @@ Tensor ScatterUpdate(
     return result;
 }
 
-void TiledIndexPut(Function &function, const TileShape &tileShape, const LogicalTensorPtr &inputSelf, Input &inputValues,
-    std::vector<Input> &inputIndices, const LogicalTensorPtr result, bool accumulate) {
-    const auto &vecTile = tileShape.GetVecTile()[0];
-    for (int i = 0; i < inputValues.tensor.GetShape()[0]; i += vecTile) {
-        inputValues.tileInfo.shape[0] = std::min(inputValues.tensor.GetShape()[0] - i, vecTile);
-        inputValues.tileInfo.offset[0] = i;
+void TiledIndexPut(Function &function, const TileShape &tileShape, Input &inputSelf, Input &inputValues,
+    std::vector<Input> &inputIndices, const LogicalTensorPtr result, bool accumulate, size_t selfTiledCount, size_t cur) {
+    if (cur == inputValues.tensor.GetShape().size()) {
+        auto inputSelfTile = inputSelf.tensor.GetStorage()->View(function, inputSelf.tileInfo.shape, inputSelf.tileInfo.offset);
         auto inputValuesTile = inputValues.tensor.GetStorage()->View(function, inputValues.tileInfo.shape, inputValues.tileInfo.offset);
         std::vector<LogicalTensorPtr> inputsTile;
-        inputsTile.push_back(inputSelf);
+        inputsTile.push_back(inputSelfTile);
         inputsTile.push_back(inputValuesTile);
         for (size_t j = 0; j < inputIndices.size(); j++) {
-            inputIndices[j].tileInfo.shape[0] = std::min(inputIndices[j].tensor.GetShape()[0] - i, vecTile);
-            inputIndices[j].tileInfo.offset[0] = i;
-            auto inputIndicesTileTemp = inputIndices[j].tensor.GetStorage()->View(function, inputIndices[j].tileInfo.shape, inputIndices[j].tileInfo.offset);
-            inputsTile.push_back(inputIndicesTileTemp);
+            auto inputIndicesTile = inputIndices[j].tensor.GetStorage()->View(function, inputIndices[j].tileInfo.shape, inputIndices[j].tileInfo.offset);
+            inputsTile.push_back(inputIndicesTile);
         }
         auto &newOp = function.AddOperation(Opcode::OP_INDEX_PUT, inputsTile, {result});
         newOp.SetAttribute(OpAttributeKey::inplaceIdx, 0);
         newOp.SetAttribute(OpAttributeKey::accumulate, accumulate);
         newOp.SetAttribute(OpAttributeKey::indicesSize, static_cast<int>(inputIndices.size()));
+        return;
+    }
+    const auto &vecTile = tileShape.GetVecTile()[0];
+    for (int i = 0; i < inputValues.tensor.GetShape()[cur]; i += vecTile[cur]) {
+        if (cur >= inputSelf.tensor.GetShape().size() - selfTiledCount) {
+            inputSelf.tileInfo.shape[cur] = std::min(inputSelf.tensor.GetShape()[cur] - i, vecTile[cur]);
+            inputSelf.tileInfo.offset[cur] = i;
+        } else {
+            inputSelf.tileInfo.shape[cur] = inputSelf.tensor.GetShape()[cur];
+            inputSelf.tileInfo.offset[cur] = 0;
+        }
+        inputValues.tileInfo.shape[cur] = std::min(inputValues.tensor.GetShape()[cur] - i, vecTile[cur]);
+        inputValues.tileInfo.offset[cur] = i;
+        if (cur == 0) {
+            for (size_t j = 0; j < inputIndices.size(); j++) {
+                inputIndices[j].tileInfo.shape[cur] = std::min(inputIndices[j].tensor.GetShape()[cur] - i, vecTile[cur]);
+                inputIndices[j].tileInfo.offset[cur] = i;
+            }
+        }
+        TiledIndexPut(function, tileShape, inputSelf, inputValues, inputIndices, result, accumulate, selfTiledCount, cur + 1);
     }
 }
 
@@ -1077,18 +1093,17 @@ void TiledIndexPut(Function &function, const TileShape &tileShape, const Logical
         ASSERT(indices[i]->GetShape().size() == indices[i]->GetOffset().size());
     }
     TileInfo valuesTileInfo(values->shape.size(), values->offset.size());
-    for (size_t i = 1; i < values->GetShape().size(); i++) {
-        valuesTileInfo.shape[i] = values->GetShape()[i];
-        valuesTileInfo.offset[i] = 0;
-    }
+    TileInfo selfTileInfo(self->shape.size(), self->offset.size());
     auto inputValues = Input{values, valuesTileInfo};
+    auto inputSelf = Input{self, selfTileInfo};
     std::vector<Input> inputIndices;
     for (size_t i = 0; i < indices.size(); i++) {
         TileInfo indicesTileInfoTemp(indices[i]->shape.size(), indices[i]->offset.size());
         auto inputIndicesTemp = Input{indices[i], indicesTileInfoTemp};
         inputIndices.push_back(inputIndicesTemp);
     }
-    TiledIndexPut(function, tileShape, self, inputValues, inputIndices, result, accumulate);
+    size_t selfTiledCount = values->GetShape().size() - indices.size();
+    TiledIndexPut(function, tileShape, inputSelf, inputValues, inputIndices, result, accumulate, selfTiledCount, cur);
 }
 
 void TensorIndexPut(Function &function, const LogicalTensorPtr &self, const LogicalTensors &indices, const LogicalTensorPtr &values,
