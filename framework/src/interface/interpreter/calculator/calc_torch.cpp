4a075585a686b6518cb74950b3cd4bf96de97ed9
yang-fulai-3 <yangfulai3@huawei.com>
2026-03-02 11:39:25 +0800

    add file

--- framework/src/interface/interpreter/calculator/calc_torch.cpp
+++ framework/src/interface/interpreter/calculator/calc_torch.cpp
@@ -1177,6 +1177,13 @@ void CumSum(const TensorData &out, const TensorData &in, int axis) {
     ToOperand(tout.second, tout.first, out.dtype);
 }
 
+void CumProd(const TensorData &out, const TensorData &in, int axis) {
+    auto tout = From(out);
+    auto input = From(in);
+    torch::cumprod_out(tout.second, input.second, axis);
+    ToOperand(tout.second, tout.first, out.dtype);
+}
+
 void IndexPut(const TensorData &out, const TensorData &self, const std::vector<TensorData> &indices, const TensorData &values, bool accumulate) {
     c10::List<c10::optional<at::Tensor>> indicesList;
     for (const auto idx : indices) {
