ce22819456e73e8599d55ed1017157b2a5de6483
xujiayi1999 <xujiayi28@huawei.com>
2026-03-02 20:50:55 +0800

    fix(log): Fix pass log

--- framework/src/passes/pass_log/pass_log.h
+++ framework/src/passes/pass_log/pass_log.h
@@ -17,10 +17,10 @@
 #define PASS_LOG_H
 
 #include <string>
+#include <chrono>
 #include "interface/utils/log.h"
 #include "interface/operation/operation.h"
 #include "interface/function/function.h"
-#include <chrono>
 #include "tilefwk/pypto_fwk_log.h"
 
 #define APASS_LOG_F(lvl, MODULE_NAME, opName, fmt, args...)                        \
@@ -107,11 +107,11 @@ private:
 #define LOG_SCOPE_END(timerVar) \
     timerVar.End()
 
-
-#define APASS_LOG_DEBUG_F(opEnum, ...)   PYPTO_HOST_LOG(DLOG_DEBUG, (std::string("PASS.") + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
-#define APASS_LOG_INFO_F(opEnum, ...)    PYPTO_HOST_LOG(DLOG_INFO, (std::string("PASS.") + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
-#define APASS_LOG_WARN_F(opEnum, ...)    PYPTO_HOST_LOG(DLOG_WARN, (std::string("PASS.") + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
-#define APASS_LOG_ERROR_F(opEnum, ...)   PYPTO_HOST_LOG(DLOG_ERROR, (std::string("PASS.") + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
-#define APASS_LOG_EVENT_F(opEnum, ...)   PYPTO_HOST_LOG_WITHOUT_LEVEL_CHECK(DLOG_INFO, (std::string("PASS.") + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
+static const std::string PASS_LOG_PREFIX = "PASS.";
+#define APASS_LOG_DEBUG_F(opEnum, ...)   PYPTO_HOST_LOG(DLOG_DEBUG, (PASS_LOG_PREFIX + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
+#define APASS_LOG_INFO_F(opEnum, ...)    PYPTO_HOST_LOG(DLOG_INFO, (PASS_LOG_PREFIX + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
+#define APASS_LOG_WARN_F(opEnum, ...)    PYPTO_HOST_LOG(DLOG_WARN, (PASS_LOG_PREFIX + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
+#define APASS_LOG_ERROR_F(opEnum, ...)   PYPTO_HOST_LOG(DLOG_ERROR, (PASS_LOG_PREFIX + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
+#define APASS_LOG_EVENT_F(opEnum, ...)   PYPTO_HOST_LOG_WITHOUT_LEVEL_CHECK(DLOG_INFO, (PASS_LOG_PREFIX + MODULE_NAME "." + toString(opEnum)).c_str(), __VA_ARGS__)
 
 #endif // PASSES_LOG_H
