7b8d2aad1df6c3f94505e54174311fd0b8a887da
l30075949 <liqingyan10@h-partners.com>
2026-03-02 14:41:04 +0800

    feat(Operation): Add test case for mx matmul with bias

--- framework/src/interface/operation/cube_operation_impl.cpp
+++ framework/src/interface/operation/cube_operation_impl.cpp
@@ -599,9 +599,14 @@ void SetTensorGraphNodes(const std::vector<LogicalTensorPtr> &operandVec, const 
         case 4:  // 4含义：有gmTensor
             tensorGraphNodes.gmAccumulationTensorPtr = operandVec[SHAPE_DIM2];
             break;
-        case 8:
+        case 8: // 8含义: mxmatmul场景
             tensorGraphNodes.aScaleTensorPtr = operandVec[SHAPE_DIM2];
             tensorGraphNodes.bScaleTensorPtr = operandVec[SHAPE_DIM3];
+            break;
+        case 10: // 10含义: mxmatmul场景，有bias
+            tensorGraphNodes.aScaleTensorPtr = operandVec[SHAPE_DIM2];
+            tensorGraphNodes.bScaleTensorPtr = operandVec[SHAPE_DIM3];
+            tensorGraphNodes.biasTensorPtr = operandVec[SHAPE_DIM4];
             break;
         default:
             OP_CHECK(true, { ASSERT(false) << "Invalid tensor graph\n";});
