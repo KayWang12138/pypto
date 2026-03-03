8fe3a899ee36a0529b1ca3fad4f5a0d8d21eb7d1
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-03-03 09:54:39 +0800

    feat(pass): Add test cases

--- python/tests/st/operator/pg/pg_lightning_indexer_prolog_quant_hif8.py
+++ python/tests/st/operator/pg/pg_lightning_indexer_prolog_quant_hif8.py
@@ -551,6 +551,7 @@ def do_test_lightning_indexer_prolog_quant(case_name, configs):
     logging.info(f"=== {case_name}: PASS ===")
 
 
+@pytest.mark.soc("950", "910")
 def test_b1_s1_8k_s2_8k():
     configs = IndexerPrologQuantConfigs(
         q_linear=[16, 16, 512, 512, 128, 128],
@@ -604,6 +605,7 @@ def test_b4_s1_8k_s2_8k():
     do_test_lightning_indexer_prolog_quant("QuantLightningIndexerPrologSTest.b4_s1_8k_s2_8k", configs)
 
 
+@pytest.mark.soc("950", "910")
 def test_b1_s1_4_s2_8k():
     configs = IndexerPrologQuantConfigs(
         q_linear=[16, 16, 512, 512, 128, 128],
