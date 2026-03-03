ae7e71eec3203e6811ef0135e5052919b662c07d
chen-yongkang16 <chenyongkang16@huawei.com>
2026-03-03 14:06:06 +0800

    feat(operation): Support cross-input scenarios for FP8 E4M3 and E5M2 data types.

--- framework/src/interface/operation/cube_operation_impl.cpp
+++ framework/src/interface/operation/cube_operation_impl.cpp
@@ -952,19 +952,37 @@ void CheckGmAccumulationParam(DataType outType, const Tensor &aMatrix, const Ten
     });
 }
 
-void CheckMatmulOperands(DataType outType, const Tensor &operand1, const Tensor &operand2,
-    const MatmulAttrParam &attrParam, const MatmulExtendParam &param = {}) {
+void CheckOperandDtype(DataType outType, const Tensor &operand1, const Tensor &operand2){
     OP_CHECK(true, {
         ASSERT(outType == DataType::DT_FP32 || outType == DataType::DT_FP16 || outType == DataType::DT_BF16 ||
                outType == DataType::DT_INT32)
             << "Unsupported output data type. Only DT_FP32, DT_FP16, DT_BF16, DT_INT32 are supported.";
     });
-    OP_CHECK(true, {
+    if (operand1.GetDataType() == DataType::DT_FP8E5M2 || operand1.GetDataType() == DataType::DT_FP8E4M3){
+        OP_CHECK(true, {
+        ASSERT(operand2.GetDataType() == DataType::DT_FP8E5M2 || operand2.GetDataType() == DataType::DT_FP8E4M3)
+            << "When operand1 is of type DT_FP8E4M3 or DT_FP8E5M2, operand2 must be DT_FP8E4M3 or DT_FP8E5M2. "
+            << "operand1 dataType: " << DataType2String(operand1.GetDataType())
+            << ", operand2 dataType: " << DataType2String(operand2.GetDataType()) << std::endl;
+        });
+        ASSERT(operand1.GetDataType() == DataType::DT_FP8E5M2 && operand1.Format() == TileOpFormat::TILEOP_ND)
+            << "When operand1 data type is DT_FP8E5M2, format must be ND.";
+        ASSERT(operand2.GetDataType() == DataType::DT_FP8E5M2 && operand2.Format() == TileOpFormat::TILEOP_ND)
+            << "When operand2 data type is DT_FP8E5M2, format must be ND.";
+    } else {
+        OP_CHECK(true, {
         ASSERT(operand1.GetDataType() == operand2.GetDataType())
             << "input dataType must be consistent. "
             << "operand1 dataType: " << DataType2String(operand1.GetDataType())
             << ", operand2 dataType: " << DataType2String(operand2.GetDataType()) << std::endl;
-    });
+        });
+    }
+}
+
+void CheckMatmulOperands(DataType outType, const Tensor &operand1, const Tensor &operand2,
+    const MatmulAttrParam &attrParam, const MatmulExtendParam &param = {}) {
+    // dtype valid check
+    CheckOperandDtype(outType, operand1, operand2);
     // GM Acc valid check
     CheckGmAccumulationParam(outType, operand1, operand2, attrParam, param);
     // shape valid check
