e9d02e34a8ab0e042f6fb124ec780a0f1b71236a
wang-zhiyue2 <wangzhiyue2@huawei.com>
2026-03-03 15:37:21 +0800

    feat(operation): Add TF32 matmul feature.

--- framework/src/codegen/cloudnpu/codegen_cube.cpp
+++ framework/src/codegen/cloudnpu/codegen_cube.cpp
@@ -23,8 +23,10 @@ std::string CodeGenOpCloudNPU::PrintMatmulTileTensor(
     bool isAcc, std::unordered_map<OperandType, std::string> &tensorWithMemType) const {
     std::ostringstream oss;
     bool hasBias = tensorWithMemType.count(OperandType::BUF_BT);
+    int64_t transMode = 0;
+    GetAttr(OP_ATTR_PREFIX + "trans_mode", transMode);
     std::vector<std::string> paramList = {tensorWithMemType[OperandType::BUF_L0C],
-        tensorWithMemType[OperandType::BUF_L0A], tensorWithMemType[OperandType::BUF_L0B]};
+        tensorWithMemType[OperandType::BUF_L0A], tensorWithMemType[OperandType::BUF_L0B], std::to_string(transMode)};
     oss << tileOpName;
     if (hasBias) {
         paramList.emplace_back(tensorWithMemType[OperandType::BUF_BT]);
