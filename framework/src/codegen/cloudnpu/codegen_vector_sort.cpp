c30553d9f5dd10f2f162ec7e5ca44b811db84144
luohui46 <luohui46@huawei.com>
2026-03-04 17:44:53 +0800

    fix(operation): Fix bug for tileop bitsort

--- framework/src/codegen/cloudnpu/codegen_vector_sort.cpp
+++ framework/src/codegen/cloudnpu/codegen_vector_sort.cpp
@@ -30,8 +30,8 @@ std::string CodeGenOpCloudNPU::PrintSortDynamicUnaligned(const SortParam &param)
     const std::string &dstDtypeStr = param.dstDtypeStr;
     const std::string &tmpDtypeStr = param.tmpDtypeStr;
 
-    auto dynSrcShape = dynamicValidShape[1];
-    FillIntVecWithDummyInHead<SymbolicScalar>(dynSrcShape, SHAPE_DIM4 - dynamicValidShape[1].size(), 1);
+    auto dynSrcShape = dynamicValidShape[2];
+    FillIntVecWithDummyInHead<SymbolicScalar>(dynSrcShape, SHAPE_DIM4 - dynamicValidShape[2].size(), 1);
 
     std::vector<std::string> paramList;
     paramList.emplace_back(srcDtypeStr);
