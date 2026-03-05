a2861d3eff14b851d95c59758e844b257f160edd
W4termy <wangmingyang21@huawei.com>
2026-03-05 11:51:36 +0800

    fix(machine): Correct incorrect format specifiers in the log messages

--- framework/include/tilefwk/pypto_fwk_log.h
+++ framework/include/tilefwk/pypto_fwk_log.h
@@ -36,7 +36,7 @@ class LogFuncInfo {
 public:
     static LogFuncInfo &Instance();
     int32_t(*checkLevel)(int32_t, int32_t);
-    void(*record)(int32_t, int32_t, const char *, ...);
+    void(*record)(int32_t, int32_t, const char *, ...)__attribute__((format(printf, 3, 4)));
     void(*setAttr)(bool);
 private:
     LogFuncInfo();
