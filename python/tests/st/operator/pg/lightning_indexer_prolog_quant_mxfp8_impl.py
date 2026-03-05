bf58998f78ca18df60ad09a6149ce9f2c16bb4fe
grey-pig-six <jiangtaoyu@huawei.com>
2026-03-05 19:44:17 +0800

    hif8 golden 修改

--- python/tests/st/operator/pg/lightning_indexer_prolog_quant_mxfp8_impl.py
+++ python/tests/st/operator/pg/lightning_indexer_prolog_quant_mxfp8_impl.py
@@ -226,7 +226,7 @@ def rotate_half(input_tensor: pypto.Tensor) -> pypto.Tensor:
     offset2[shape_size - 1] = shape[shape_size - 1]
     x1 = pypto.view(input_tensor, shape, offset1)
     x2 = pypto.view(input_tensor, shape, offset2)
-    return pypto.concat([x2 * (-1.0), x1 + 0.0], -1)
+    return pypto.concat([x2 * (-1.0), x1], -1)
 
 
 def rope_3d(x: pypto.Tensor, cos: pypto.Tensor, sin: pypto.Tensor) -> pypto.Tensor:
@@ -256,11 +256,11 @@ def rope_3d(x: pypto.Tensor, cos: pypto.Tensor, sin: pypto.Tensor) -> pypto.Tens
     head_num = x.shape[head_num_axis]
     rope_dim = x.shape[head_dim_axis]
 
-    pypto.set_vec_tile_shapes(512, 64)
+    pypto.set_vec_tile_shapes(t_tile, 64)
     cast_cos = pypto.cast(cos, pypto.DT_FP32)
     cast_sin = pypto.cast(sin, pypto.DT_FP32)
 
-    pypto.set_vec_tile_shapes(16, 32, 64)
+    pypto.set_vec_tile_shapes(16, 24, 64)
     x_view = pypto.cast(x, pypto.DT_FP32)
     cast_cos = pypto.reshape(cast_cos, [t_tile, 1, rope_dim])
     cast_sin = pypto.reshape(cast_sin, [t_tile, 1, rope_dim])
@@ -343,7 +343,7 @@ def lightning_indexer_prolog_quant_compute(x_in, q_norm_in, q_norm_scale_in, w_q
     gamma_2d = pypto.reshape(ln_gamma_k_in, [1, ln_gamma_k_in.shape[0]], inplace=True)
     beta_2d = pypto.reshape(ln_beta_k_in, [1, ln_beta_k_in.shape[0]], inplace=True)
 
-    unroll_list = [64, 32, 16, 8, 4, 2, 1]
+    unroll_list = [128, 64, 32, 16, 8, 4, 2, 1]
     t_tile_max = unroll_list[0]
     for t_idx, unroll_length in pypto.loop_unroll(0, t, 1, name="IndexerPrologQuantQuantLoop", idx_name="t_idx",
                                                 unroll_list=unroll_list):
@@ -353,17 +353,17 @@ def lightning_indexer_prolog_quant_compute(x_in, q_norm_in, q_norm_scale_in, w_q
         q_norm = pypto.view(q_norm_in, [t_tile, q_lora_rank], [t_idx, 0])
         q_norm_scale = pypto.view(q_norm_scale_in, [t_tile, q_lora_rank // 64, 2], [t_idx, 0, 0])
         pypto.set_cube_tile_shapes([t_tile_max, t_tile_max], [512, 512], [128, 128])
-        pypto.set_vec_tile_shapes(8, 32 * 128)
-        q_cast = pypto.scaled_mm(q_norm, w_qb_in, pypto.DT_BF16, q_norm_scale, w_qb_scale_in, scale_b_trans=True)
+        pypto.set_vec_tile_shapes(8, 24 * 128)
+        q_scaled_mm = pypto.scaled_mm(q_norm, w_qb_in, pypto.DT_BF16, q_norm_scale, w_qb_scale_in, scale_b_trans=True)
 
         pypto.set_semantic_label("Query-Rope")
-        q_bf16 = pypto.reshape(q_cast, [t_tile, head_num, head_dim])
+        q_bf16 = pypto.reshape(q_scaled_mm, [t_tile, head_num, head_dim])
         q_rope = pypto.view(q_bf16, [t_tile, head_num, rope_head_dim], [0, 0, 0])
         q_nope = pypto.view(q_bf16, [t_tile, head_num, head_dim - rope_head_dim], [0, 0, rope_head_dim])
         rope_cos = pypto.view(cos_idx_rope_in, [t_tile, rope_head_dim], [t_idx, 0])
         rope_sin = pypto.view(sin_idx_rope_in, [t_tile, rope_head_dim], [t_idx, 0])
         q_roped = rope_3d(q_rope, rope_cos, rope_sin)
-        pypto.set_vec_tile_shapes(8, 32, 128)
+        pypto.set_vec_tile_shapes(8, 24, 128)
         q_cat = pypto.concat([q_roped, q_nope], -1)
         q_cat_2d = pypto.reshape(q_cat, [t_tile * head_num, head_dim])
 
@@ -373,13 +373,13 @@ def lightning_indexer_prolog_quant_compute(x_in, q_norm_in, q_norm_scale_in, w_q
         q_hadamard_3d = pypto.reshape(q_hadamard, [t_tile, head_num, head_dim])
 
         pypto.set_semantic_label("Query-Quant")
-        pypto.set_vec_tile_shapes(8, 32, 128)
+        pypto.set_vec_tile_shapes(8, 24, 128)
         q_res = prolog_quant(q_hadamard_3d)
         pypto.assemble(q_res[0], [t_idx, 0, 0], q_quant_out)
         pypto.assemble(q_res[1], [t_idx, 0, 0], q_scale_out)
 
         pypto.set_semantic_label("Key-Linear")
-        pypto.set_cube_tile_shapes([t_tile_max, t_tile_max], [512, 512], [64, 64])
+        pypto.set_cube_tile_shapes([64, 64], [512, 512], [64, 64])
         x = pypto.view(x_in, [t_tile, h], [t_idx, 0])
         k = pypto.matmul(x, wk_in, pypto.DT_FP32)
 
@@ -420,15 +420,12 @@ def lightning_indexer_prolog_quant_compute(x_in, q_norm_in, q_norm_scale_in, w_q
 
 @pypto.jit(
     pass_options={
-        "vec_nbuffer_mode": 0,
-        "cube_l1_reuse_setting": {1: 4},
-        "mg_copyin_upper_bound": 2 * 1024 * 1024,
+        "cube_l1_reuse_setting": {-1: 4},
         "pg_upper_bound": 8192
     },
     runtime_options={
-        "stitch_function_inner_memory": 512,
-        "stitch_function_outcast_memory": 512,
-        "device_sched_mode": 1
+        "stitch_function_inner_memory": 128 * 128,
+        "stitch_function_outcast_memory": 128 * 128,
     }
 )
 def lightning_indexer_prolog_quant(x_in, q_norm_in, q_norm_scale_in, w_qb_in,
