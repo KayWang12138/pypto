d3d5ebc59dbbcc6980767f3a8dc69a5348786ea1
wang-zhiyue2 <wangzhiyue2@huawei.com>
2026-03-05 11:15:58 +0800

    feat(operation): Add TF32 matmul feature.

--- framework/src/interface/operation/cube_operation_impl.cpp
+++ framework/src/interface/operation/cube_operation_impl.cpp
@@ -921,7 +921,7 @@ void CheckTransModeParam(DataType inDtype, const MatmulExtendParam &param = {}) 
     if(param.transMode != TransMode::CAST_NONE) {
         OP_CHECK(true, {
             ASSERT(inDtype == DataType::DT_FP32)
-                << "TransMode is supported only when input data type are of type DT_FP32."
+                << "TransMode is supported only when input data types are DT_FP32."
                 << std::endl;
         });
     }
