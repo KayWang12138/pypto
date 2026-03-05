9451cea8415072a3c3752d9f5bedc216111aafdb
KirillChizhov <chizhov.kirill1@huawei.com>
2026-03-05 17:08:04 +0300

    SetHeuristicTileShapes pass new version

--- framework/src/passes/tensor_graph_pass/set_heuristic_tile_shapes.h
+++ framework/src/passes/tensor_graph_pass/set_heuristic_tile_shapes.h
@@ -19,6 +19,9 @@
 #define CUBE_TILES   // comment to disable
 #define VECTOR_TILES // comment to disable
 
+// Custom defines to flexible setting 
+#define L1_TILES_SETTING
+
 #include "tilefwk/platform.h"
 #include "passes/pass_interface/pass.h"
 
@@ -33,9 +36,11 @@ constexpr int64_t MAX_MDIM = 2;
 constexpr int64_t MAX_KDIM = 3;
 constexpr int64_t MAX_NDIM = 2;
 
+constexpr const int64_t MAX_KL1 = 2048;
+constexpr const int64_t MIN_KL1_TILE = 128;
+constexpr const int64_t KL1_FACTOR = 4; // kL1 = 4kL0 feature
 constexpr const int64_t MIN_TILE_SIZE = 2048;
-constexpr const int64_t MAX_TILE_SIZE = 192 * 1024 / 4; // 4 - max num of In/Out/Tmp buffers
-constexpr const int64_t DEFAULT_TILE_SIZE = 4096;
+constexpr const int64_t DEFAULT_TILE_SIZE = 16384;
 constexpr const int64_t BYTES_PER_REPEAT = 256;
 constexpr const int64_t DEFAULT_MAX_PARALLELISM = 128;
 constexpr const int64_t DEFAULT_LATENCY = 10;
