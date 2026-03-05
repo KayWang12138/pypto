44256fb0115def731069dc2fc0aadd5bd00f727b
m00896683 <mayuan33@huawei.com>
2026-03-05 14:20:45 +0800

    Remove the invalid conditional check logic in ExpandFunction

--- framework/tests/ut/passes/src/test_expandfunction.cpp
+++ framework/tests/ut/passes/src/test_expandfunction.cpp
@@ -326,63 +326,6 @@ TEST_F(TestExpandFunctionPass, ExpandFunctionUTest4) {
 }
 
 /*
-TESTExpandFunctionAssembleV2
-inCast1{32,128}->reshape->ubTensor{64,64}->assemble->outCast{32,128}
-inCast1{32,128}->reshape->ubTensor{64,64}->assemble(*4)->outCast{32,128}
-*/
-TEST_F(TestExpandFunctionPass, ExpandFunctionUTest5) {
-    auto currFunctionPtr = std::make_shared<Function>(Program::GetInstance(), "TestExpandFunction", "TestExpandFunction", nullptr);
-    EXPECT_TRUE(currFunctionPtr != nullptr);
-
-    // Prepare the graph
-    std::vector<int64_t> shape1 = {kNumExpFive, kNumExpSeven};
-    std::vector<int64_t> shape2 = {kNumExpSix, kNumExpSix};
-    std::vector<int64_t> shape3 = {kNumExpFive, kNumExpSeven};
-    auto inCast = std::make_shared<LogicalTensor>(*currFunctionPtr, DT_FP32, shape1);
-    auto ubTensor = std::make_shared<LogicalTensor>(*currFunctionPtr, DT_FP32, shape2);
-    auto outCast = std::make_shared<LogicalTensor>(*currFunctionPtr, DT_FP32, shape3);
-
-    std::vector<int64_t> toOffset = {kNumZero, kNumZero};
-    std::vector<SymbolicScalar> symbol = {SymbolicScalar("sym")};
-    auto op_attr = std::make_shared<AssembleOpAttribute>(toOffset, symbol);
-    currFunctionPtr->AddOperation(Opcode::OP_RESHAPE, {inCast}, {ubTensor});
-    currFunctionPtr->AddOperation(Opcode::OP_ASSEMBLE, {ubTensor}, {outCast});
-
-    std::shared_ptr<Operation> reshape_op;
-    std::shared_ptr<Operation> assemble_op;
-    for (uint32_t uIndex = 0; uIndex < currFunctionPtr->Operations().size(); ++uIndex){
-        if (currFunctionPtr->Operations().operations_[uIndex]->GetOpcode() == Opcode::OP_RESHAPE) {
-            reshape_op = currFunctionPtr->Operations().operations_[uIndex];
-        }
-        if (currFunctionPtr->Operations().operations_[uIndex]->GetOpcode() == Opcode::OP_ASSEMBLE) {
-            assemble_op = currFunctionPtr->Operations().operations_[uIndex];
-        }
-    }
-
-    assemble_op->SetOpAttribute(op_attr);
-    reshape_op->tileShape_.SetVecTile({kNumExpFive, kNumExpFive});
-    assemble_op->tileShape_.SetVecTile({kNumExpFive, kNumExpFive});
-
-    currFunctionPtr->inCasts_.push_back(inCast);
-    currFunctionPtr->outCasts_.push_back(outCast);
-    currFunctionPtr->SetGraphType(GraphType::TENSOR_GRAPH);
-
-    ExpandFunction expandfunctionpass;
-    auto status = expandfunctionpass.RunOnFunction(*currFunctionPtr);
-    EXPECT_EQ(status, SUCCESS);
-    EXPECT_EQ(currFunctionPtr->GetGraphType(), GraphType::TILE_GRAPH);
-
-    uint32_t assemble_num = kNumZero;
-    for (auto &op : currFunctionPtr->Operations()) {
-        if (op.GetOpcode() == Opcode::OP_ASSEMBLE) {
-            EXPECT_NE(op.GetOpMagic(), assemble_op->GetOpMagic());
-            ++assemble_num;
-        }
-    }
-    EXPECT_EQ(assemble_num, kNumFour);
-}
-
-/*
 {64, 64} -> exp -> {64, 64}
 {64, 64} -> (view) - > exp -> (assemble) - > {64, 64}
 {64, 64} -> (view) -> (view {32, 64}) - > exp -> (assemble {32, 64}) -> (assemble) - > {64, 64}
