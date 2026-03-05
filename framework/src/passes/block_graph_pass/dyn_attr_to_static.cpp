dbf04bb3dbe873df76ff5922424d5a405b8e2e72
m00896683 <mayuan33@huawei.com>
2026-03-05 20:54:21 +0800

    clean code

--- framework/src/passes/block_graph_pass/dyn_attr_to_static.cpp
+++ framework/src/passes/block_graph_pass/dyn_attr_to_static.cpp
@@ -442,16 +442,17 @@ Status DynAttrToStatic::GetTileFunction(Function* function, std::vector<Function
     for (auto callop : function->GetCallopList()) {
         Function *nextFunc = nullptr;
         if (GetCallee(*callop, nextFunc) != SUCCESS) {
-            APASS_LOG_ERROR_F(Elements::Operation, "GetTileFunction at %s, %s[%d] GetCallee failed.%s",
-                function->GetRawName().c_str(), callop->GetOpcodeStr().c_str(), callop->GetOpMagic(), GetFormatBacktrace(callop).c_str());
+            APASS_LOG_ERROR_F(Elements::Operation, "GetTileFunction, currFunc: %s, %s[%d] GetCallee failed.",
+                function->GetRawName().c_str(), callop->GetOpcodeStr().c_str(), callop->GetOpMagic());
             return FAILED;
         }
+        APASS_LOG_DEBUG_F(Elements::Function, "GetTileFunction, %s --%s[%d]--> %s",
+            function->GetRawName().c_str(), callop->GetOpcodeStr().c_str(), callop->GetOpMagic(), nextFunc->GetRawName().c_str());
         if (nextFunc->GetGraphType() == GraphType::TILE_GRAPH) {
             tileFunctionVec.emplace_back(nextFunc);
-            continue;
         } else {
             if(GetTileFunction(nextFunc, tileFunctionVec) != SUCCESS) {
-                APASS_LOG_ERROR_F(Elements::Operation, "GetTileFunction at %s, nextFunc at %s failed",
+                APASS_LOG_ERROR_F(Elements::Operation, "GetTileFunction, currFunc: %s, nextFunc: %s, recursive search failed",
                     function->GetRawName().c_str(), nextFunc->GetRawName().c_str());
                 return FAILED;
             }
@@ -463,30 +464,28 @@ Status DynAttrToStatic::GetTileFunction(Function* function, std::vector<Function
 Status DynAttrToStatic::DumpFunctionJson(Function& function, const std::string &logFolder, bool beforeFunction) {
     std::vector<Function*> tileFunctionVec;
     if (GetTileFunction(&function, tileFunctionVec) != SUCCESS) {
-        APASS_LOG_ERROR_F(Elements::Function, "Get tile function failed.");
         return FAILED;
     }
-    for(auto tileFunc : tileFunctionVec) {
-        if (Pass::DumpFunctionJson(*tileFunc, logFolder, beforeFunction) != SUCCESS) {
-            APASS_LOG_ERROR_F(Elements::Function, "Dump function json failed.");
-            return FAILED;
-        }
+    APASS_LOG_DEBUG_F(Elements::Function, "Obtained a total of %zu tileFunctions", tileFunctionVec.size());
+    for (auto tileFunc : tileFunctionVec) {
+        APASS_LOG_DEBUG_F(Elements::Function, "Dump tileFunction[%s] json", tileFunc->GetRawName().c_str());
+        Pass::DumpFunctionJson(*tileFunc, logFolder, beforeFunction);
     }
+    APASS_LOG_DEBUG_F(Elements::Function, "Dump function[%s] json finished.", function.GetRawName().c_str());
     return SUCCESS;
 }
 
 Status DynAttrToStatic::PrintFunction(Function& function, const std::string &logFolder, bool beforeFunction) {
     std::vector<Function*> tileFunctionVec;
     if (GetTileFunction(&function, tileFunctionVec) != SUCCESS) {
-        APASS_LOG_ERROR_F(Elements::Function, "Get tile function failed.");
         return FAILED;
     }
-    for(auto tileFunc : tileFunctionVec) {
-        if (Pass::PrintFunction(*tileFunc, logFolder, beforeFunction) != SUCCESS) {
-            APASS_LOG_ERROR_F(Elements::Function, "Print function failed.");
-            return FAILED;
-        }
+    APASS_LOG_DEBUG_F(Elements::Function, "Obtained a total of %zu tileFunctions", tileFunctionVec.size());
+    for (auto tileFunc : tileFunctionVec) {
+        APASS_LOG_DEBUG_F(Elements::Function, "Print tileFunction[%s]", tileFunc->GetRawName().c_str());
+        Pass::PrintFunction(*tileFunc, logFolder, beforeFunction);
     }
+    APASS_LOG_DEBUG_F(Elements::Function, "Print function[%s] finished.", function.GetRawName().c_str());
     return SUCCESS;
 }
 } // namespace tile_fwk
