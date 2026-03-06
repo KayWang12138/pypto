12e989aadf18700e4b8c663e9d4699298f7af6ec
gcw_Lf43RugY <mingrui5@huawei.com>
2026-03-06 10:06:19 +0800

    feat(machine): Add esl model for simulation
    Signed-off-by: gcw_Lf43RugY <mingrui5@huawei.com>

--- framework/src/machine/utils/dynamic/dev_encode_program.h
+++ framework/src/machine/utils/dynamic/dev_encode_program.h
@@ -333,7 +333,7 @@ struct DevAscendProgram {
 
     void ResetFromLaunch() {
         DevArgsPreservedParams preservedParams = BackupDevArgsParams(devArgs);
-        memset_s(&devArgs, sizeof(devArgs), 0, sizeof(devArgs));
+        // memset_s(&devArgs, sizeof(devArgs), 0, sizeof(devArgs));
         RestoreDevArgsParams(devArgs, preservedParams);
 
         controlFlowBinaryAddr = nullptr;
