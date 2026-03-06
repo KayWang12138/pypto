65daa95b63e6d8695c929e33434d58bcd276b3f6
eternityk <taoyikun@huawei.com>
2026-03-06 09:59:09 +0800

    gmm mxfp8

--- examples/gmm_mxfp8_pr.py
+++ examples/gmm_mxfp8_pr.py
@@ -17,6 +17,94 @@ import math
 import time
 from numpy.testing import assert_allclose
 
+def compute_golden_result(x, weight, scaled_x_gloden, scaled_weight_gloden, a_trans, b_trans):
+    if a_trans:
+        x = np.swapaxes(x, -1, -2)
+        scaled_x_gloden = np.swapaxes(scaled_x_gloden, -1, -2)
+        if len(scaled_x_gloden.shape) == 3:
+            scaled_x_gloden = scaled_x_gloden.reshape(scaled_x_gloden.shape[0] * scaled_x_gloden.shape[1], scaled_x_gloden.shape[2])
+        scaled_x_gloden = np.swapaxes(scaled_x_gloden, -1, -2)
+    else:
+        if len(scaled_x_gloden.shape) == 3:
+            scaled_x_gloden = scaled_x_gloden.reshape(scaled_x_gloden.shape[0], scaled_x_gloden.shape[1] * scaled_x_gloden.shape[2])
+    if b_trans:
+        weight = np.swapaxes(weight, -1, -2)
+        if len(scaled_weight_gloden.shape) == 3:
+            scaled_weight_gloden = scaled_weight_gloden.reshape(scaled_weight_gloden.shape[0], scaled_weight_gloden.shape[1] * scaled_weight_gloden.shape[2])
+        scaled_weight_gloden = np.swapaxes(scaled_weight_gloden, -1, -2)
+    else:
+        scaled_weight_gloden = np.swapaxes(scaled_weight_gloden, -1, -2)
+        if len(scaled_weight_gloden.shape) == 3:
+            scaled_weight_gloden = scaled_weight_gloden.reshape(scaled_weight_gloden.shape[0] * scaled_weight_gloden.shape[1], scaled_weight_gloden.shape[2])
+    
+    k_dim = x.shape[-1]
+    if math.ceil(k_dim / 32) % 2 != 0:
+        scaled_x_gloden = scaled_x_gloden[:, :-1]
+        scaled_weight_gloden = scaled_weight_gloden[:-1, :]
+
+    scaled_x_gloden_broadcast = torch.repeat_interleave(scaled_x_gloden, repeats=32, dim=-1)
+    scaled_weight_gloden_broadcast = torch.repeat_interleave(scaled_weight_gloden, repeats=32, dim=-2)
+    x1_dims = len(x.shape)
+    x2_dims = len(weight.shape)
+    x1_pad_len = scaled_x_gloden_broadcast.shape[-1] - x.shape[-1]
+    x2_pad_len = scaled_weight_gloden_broadcast.shape[-2] - weight.shape[-2]
+    x1_pad = [0, x1_pad_len]
+    for _ in range(x1_dims - 1):
+        x1_pad += [0,0]
+    x1_gloden = torch.nn.functional.pad(x, x1_pad, mode='constant', value = 0)
+
+    weight_pad = [0,0]
+    weight_pad += [0, x2_pad_len]
+    for _ in range(x2_dims - 2):
+        weight_pad += [0,0]
+    weight_golden = torch.nn.functional.pad(weight, weight_pad, mode='constant', value = 0)
+
+    x_fp32 = x.to(torch.float32)
+    scaled_x_gloden_broadcast_fp32 = scaled_x_gloden_broadcast.to(torch.float32)
+    x1_gloden = x_fp32 * scaled_x_gloden_broadcast_fp32
+
+    weight_fp32 = weight.to(torch.float32)
+    scaled_weight_gloden_broadcast_fp32 = scaled_weight_gloden_broadcast.to(torch.float32)
+    weight_golden = weight_fp32 * scaled_weight_gloden_broadcast_fp32
+
+    golden = torch.matmul(x1_gloden, weight_golden)
+
+    return golden
+
+def gen_golden(a, b, scaled_a, scaled_b, group_list, a_trans, b_trans):
+    round = b.shape[0]
+    result = []
+    begin = 0
+    end = 0
+
+    for i in range(round):
+        if group_list[i] <= 0:
+            continue
+        begin = end
+        end = end + group_list[i]
+        if a_trans:
+            x = a[:,begin:end]
+        else:
+            x = a[begin:end,:]
+        weight = b[i]
+        if a_trans:
+            scaled_x_gloden = scaled_a[:,begin:end,:]
+        else:
+            scaled_x_gloden = scaled_a[begin:end,:,:]
+        scaled_weight_gloden = scaled_b[i]
+
+        golden_temp = compute_golden_result(
+            x=x,
+            weight=weight,
+            scaled_x_gloden = scaled_x_gloden,
+            scaled_weight_gloden = scaled_weight_gloden,
+            a_trans = a_trans,
+            b_trans = b_trans,
+        )
+        result.append(golden_temp)
+    golden_result = torch.cat(result, dim=0)
+    return golden_result
+
 @dataclass
 class ShapeConfig:
     ori_shape: list
@@ -109,9 +197,16 @@ def test_gmm_mxfp8(params):
     else:
         scaled_b = torch.randn((len(group_list), k // 64, n, 2), dtype=torch.float32).uniform_(0, 1).to(torch.float8_e8m0fnu)
     
+    golden = gen_golden(a, b, scaled_a, scaled_b, group_list, a_trans, b_trans)
+    print("golden")
+    print(golden)
+    
     result = gen_mxfp8(a, b, scaled_a, scaled_b, group_list, tile_config)
+    print("result")
     print(result)
-    print("success")
+    
+    assert_allclose(golden.cpu().numpy(), result.cpu().numpy(), rtol=1e-3, atol=1e-3)
+    print("Gmm mxfp8 completed successfully")
     
 def get_params(case_name):
     if case_name == "testcase6":
