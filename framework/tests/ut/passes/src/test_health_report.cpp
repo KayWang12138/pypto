88ee134982af168f8b44f4606b1e776118ae5fb5
xujiayi1999 <xujiayi28@huawei.com>
2026-03-06 18:16:11 +0800

    Fix(Pass): Change ALOG to APASS_LOG

--- framework/tests/ut/passes/src/test_health_report.cpp
+++ framework/tests/ut/passes/src/test_health_report.cpp
@@ -79,5 +79,5 @@ TEST_F(HealthReportTest, TestDD) {
     TestLoopViewAssembleCopy(t0, t1, blockTable, out, s);
 
     auto funcMap = Program::GetInstance().GetFunctionMap();
-    APASS_LOG_INFO_F(Elements::Function, funcMap.size());
+    APASS_LOG_INFO_F(Elements::Function, "%d", funcMap.size());
 }
