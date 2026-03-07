4be06dea6b13c3b069033d8f88611c437fd5d261
wan_jianfen <wanjianfen@hisilicon.com>
2026-03-07 17:05:19 +0800

    fix(machine): Add Dump Thread

--- framework/tests/ut/machine/src/dynamic/test_dynamic_runner.cpp
+++ framework/tests/ut/machine/src/dynamic/test_dynamic_runner.cpp
@@ -14,6 +14,7 @@
  */
 
 #include "gtest/gtest.h"
+#include <cstdlib>
 #include "machine/runtime/device_runner.h"
 #include "machine/runtime/machine_agent.h"
 #include "machine/runtime/device_launcher.h"
@@ -129,6 +130,7 @@ TEST_F(TestDynamicDeviceRunner, test_pypto_kernel_server_null) {
 }
 
 TEST_F(TestDynamicDeviceRunner, test_dump_device_perf) {
+    setenv("DUMP_DEVICE_PERF", "true", 1);
     DeviceArgs devKernelArgs;
     devKernelArgs.nrAic = 1;
     devKernelArgs.nrAiv = 2;
@@ -143,8 +145,8 @@ TEST_F(TestDynamicDeviceRunner, test_dump_device_perf) {
     taskStat.execEnd =1;
     metr->taskCount = 1;
     metr->tasks[0] = taskStat;
-    metr->perfTrace[0][0] = 1;
-    metr->taskCount = 1;
+    metr->perfTrace[0][0][0] = 1;
+    metr->turnNum = 1;
 
     MetricPerf aicpuMetPer;
     aicpuMetPer.perfAicpuTraceDevTask[0][0][0] = 1;
@@ -159,8 +161,11 @@ TEST_F(TestDynamicDeviceRunner, test_dump_device_perf) {
     free(metr);
     std::string jsonPath = npu::tile_fwk::config::LogTopFolder() + "/tilefwk_L1_prof_data.json";
     EXPECT_EQ(IsPathExist(jsonPath), true);
+    setenv("DUMP_DEVICE_PERF", "true", 1);
+    npu::tile_fwk::dynamic::DumpDevTaskPerfData(devKernelArgs, perfData, true);
     jsonPath = npu::tile_fwk::config::LogTopFolder() + "/machine_runtime_operator_trace.json";
-    EXPECT_EQ(IsPathExist(jsonPath), true);
+    unsetenv("DUMP_DEVICE_PERF");
+    EXPECT_EQ(IsPathExist(jsonPath), false);
 }
 
 TEST_F(TestDynamicDeviceRunner, test_launch_init) {
