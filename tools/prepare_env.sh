6d08ed132b0894c226e28900cce35f1d364c99ff
wangzheng97 <wangzheng97@hisilicon.com>
2026-03-07 16:11:00 +0800

    fix(tools): remove --quiet from else branch (driver/firmware)

--- tools/prepare_env.sh
+++ tools/prepare_env.sh
@@ -686,7 +686,7 @@ install_single_package() {
     elif [[ "$filename" =~ "pto-isa" ]]; then
         install_cmd="$filename --quiet --install --force --install-path=$INSTALL_PATH "
     else
-        install_cmd="$filename --quiet --full --install-path=$INSTALL_PATH "
+        install_cmd="$filename --full --install-path=$INSTALL_PATH "
     fi
 
     log_print "info" "Running: $install_cmd"
