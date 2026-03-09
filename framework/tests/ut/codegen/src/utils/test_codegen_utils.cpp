50179f2159c76d9a5be99af78a0ab0a5868a78db
luyakun <luyakun@h-partners.com>
2026-03-09 20:04:12 +0800

    fix conv codegen code review

--- framework/tests/ut/codegen/src/utils/test_codegen_utils.cpp
+++ framework/tests/ut/codegen/src/utils/test_codegen_utils.cpp
@@ -88,4 +88,26 @@ Function *GenMockFuncDyn(const std::string &funcName, const std::vector<int64_t>
     return function;
 }
 
+std::shared_ptr<LogicalTensor> CreateConvTensor(Function &function, const DataType &dtype,
+    const std::vector<int64_t> &shape, const MemoryType &memType) {
+    std::shared_ptr<LogicalTensor> tensorPtr = nullptr;
+    if (memType == MemoryType::MEM_DEVICE_DDR) {
+        tensorPtr = std::make_shared<LogicalTensor>(function, dtype, shape, SymbolicScalar::FromConcrete(shape),
+                                                    TileOpFormat::TILEOP_ND, "gmTensor", NodeType::INCAST);
+        tensorPtr->UpdateDynValidShape(SymbolicScalar::FromConcrete(shape));
+    } else {
+        tensorPtr = std::make_shared<LogicalTensor>(function, dtype, shape, SymbolicScalar::FromConcrete(shape),
+                                                    TileOpFormat::TILEOP_NZ, "L1Tensor", NodeType::LOCAL);
+        tensorPtr->UpdateDynValidShape(SymbolicScalar::FromConcrete(shape));
+        tensorPtr->UpdateSubgraphID(0);
+        tensorPtr->SetAttr(OpAttributeKey::needAlloc, true);
+        tensorPtr->memoryrange.memId = 0;
+        tensorPtr->memoryrange.start = 0;
+        tensorPtr->memoryrange.end = 0;
+    }
+    tensorPtr->SetMemoryTypeOriginal(memType);
+    tensorPtr->SetMemoryTypeToBe(memType);
+    return tensorPtr;
+}
+
 } // namespace npu::tile_fwk
