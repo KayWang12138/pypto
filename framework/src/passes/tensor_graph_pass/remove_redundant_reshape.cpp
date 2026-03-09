064dfc5c47b1360403f83fe8cbd3dda4d42df3f9
2501_92924845 <niulinkai@huawei.com>
2026-03-09 15:10:27 +0800

    fix codegen ut error

--- framework/src/passes/tensor_graph_pass/remove_redundant_reshape.cpp
+++ framework/src/passes/tensor_graph_pass/remove_redundant_reshape.cpp
@@ -52,10 +52,6 @@ Status RemoveRedundantReshape::RunOnFunction(Function &function) {
     return SUCCESS;
 }
 
-bool containsNegativeOne(const std::vector<int64_t>& vec) {
-    return std::find(vec.begin(), vec.end(), static_cast<int64_t>(-1)) != vec.end();
-}
-
 Status RemoveRedundantReshape::RemoveReshape(Function &function) const {
     std::unordered_set<Operation *> redundantResapes;
     LogicalTensorPtr in;
@@ -73,7 +69,7 @@ Status RemoveRedundantReshape::RemoveReshape(Function &function) const {
             if (consumerOp == nullptr) {
                 APASS_LOG_ERROR_F(Elements::Operation, "Consumer of op [%d] is null; Check if consumer is valid. %s", op.GetOpMagic(), GetFormatBacktrace(op).c_str());
                 return FAILED;}
-            if (in->shape != out->shape || consumerOp->GetOpcode() != Opcode::OP_RESHAPE || containsNegativeOne(in->shape) || containsNegativeOne(out->shape)) {
+            if (in->shape != out->shape || consumerOp->GetOpcode() != Opcode::OP_RESHAPE) {
                 allConsumersIsReshape = false;
                 continue;
             }
