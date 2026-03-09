d17540c20b1e6d2f0d5b1a2dca2e38e541bcf75b
2501_92924845 <niulinkai@huawei.com>
2026-03-09 12:01:46 +0800

    feats(pass): Add register copy for view and assemble

--- framework/src/passes/tensor_graph_pass/infer_memory_conflict.cpp
+++ framework/src/passes/tensor_graph_pass/infer_memory_conflict.cpp
@@ -14,6 +14,7 @@
  */
 
 #include "infer_memory_conflict.h"
+#include "passes/pass_utils/dead_operation_eliminate.h"
 #include "passes/pass_log/pass_log.h"
 
 #define MODULE_NAME "InferMemoryConflict"
@@ -87,6 +88,29 @@ Status InferMemoryConflict::RunOnFunction(Function &function) {
             outOp->UpdateTileShape(viewTypeTile);
         }
     }
+    for (auto &op : function.Operations()) {
+        auto opcode = op.GetOpcode();
+        if(opcode != Opcode::OP_VIEW) {
+            //跳过非view的op
+            continue;
+        }
+        auto consumers = op.oOperand.front()->GetConsumers();
+        //获取view级联的assemble消费者
+        for (const auto &consumer : consumers) {
+            if (consumer->GetOpcode() != Opcode::OP_ASSEMBLE) {
+                //跳过不是assemble的消费者
+                continue;
+            }
+            LogicalTensorPtr inputTensor = consumer->GetIOperands().front();
+            std::shared_ptr<RawTensor> newRawTensor = std::make_shared<RawTensor>(inputTensor->Datatype(), inputTensor->GetShape());
+            Offset newOffset(inputTensor->GetShape().size(), 0);
+            LogicalTensorPtr newTensor = std::make_shared<LogicalTensor>(function, newRawTensor, newOffset, inputTensor->GetShape(), inputTensor->GetDynValidShape());
+            inputTensor->RemoveConsumer(consumer);
+            function.AddRawOperation(Opcode::OP_REGISTER_COPY, {inputTensor}, {newTensor});
+            consumer->ReplaceOutput(newTensor, inputTensor);
+        }
+    }
+    DeadOperationEliminator::EliminateDeadOperation(function);
     APASS_LOG_INFO_F(Elements::Operation, "End InferMemoryConflict for function [%s].", function.GetRawName().c_str());
     return SUCCESS;
 }
