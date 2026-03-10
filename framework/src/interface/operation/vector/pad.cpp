806610aa1f5d8a227d7df68ac01b2c5544258985
gguuaanngg <zhangguangjun2@huawei.com>
2026-03-10 19:26:13 +0800

    fix pad 11

--- framework/src/interface/operation/vector/pad.cpp
+++ framework/src/interface/operation/vector/pad.cpp
@@ -102,7 +102,7 @@ void TiledPadOperation(Function &function, const TileShape &tileShape, const Log
 LogicalTensorPtr TensorPadOperation(
     Function &function, const Tensor &self, const std::vector<int64_t> &padding, const std::string &mode, float value) {
     ASSERT(mode == "constant") << "Pad: only 'constant' mode is supported.";
-    ASSERT(std::isinf(value) || (value == 0.0)) << "Pad: pad value must be -inf, inf, or 0.";
+    ASSERT(std::isinf(value) || (std::abs(value) < 1e-6)) << "Pad: pad value must be -inf, inf, or 0.";
 
     auto operand = self.GetStorage();
     std::vector<int64_t> outputShape = operand->shape;
