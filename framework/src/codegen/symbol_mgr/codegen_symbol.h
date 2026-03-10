dd445ca3b846f04940e3cb68423faacdb68f2c1c
Bee.Hujun <Bee.hujun@huawei.com>
2026-03-10 17:34:49 +0800

    feat(codegen): Add error code

--- framework/src/codegen/symbol_mgr/codegen_symbol.h
+++ framework/src/codegen/symbol_mgr/codegen_symbol.h
@@ -228,6 +228,7 @@ public:
         auto res = tensorMap_.insert({magicNum, tensor});
         if (!res.second) {
             ASSERT(tensor == tensorMap_[magicNum])
+                << GenErrorCode(GenOpCodeErrorScene::TENSOR_MAGIC_CONFLICT)
                 << "!!! ERROR !!! tensor magic : " << magicNum
                 << " is conflicted!!!\ninsert tensor key: " << FormatAllocKey(CreateAllocKey(tensor))
                 << "\ntensor dump info -- " << tensor->Dump()
--- framework/src/codegen/utils/codegen_utils.h
+++ framework/src/codegen/utils/codegen_utils.h
@@ -49,8 +49,8 @@ inline std::string ToStringHelper(const std::string &value) {
     return value;
 }
 template <typename... Ts>
-std::string ToStringHelper(const std::variant<Ts...>& value) {
-    return std::visit([](const auto& arg) { return ToStringHelper(arg); }, value);
+std::string ToStringHelper(const std::variant<Ts...> &value) {
+    return std::visit([](const auto &arg) { return ToStringHelper(arg); }, value);
 }
 
 inline std::string ToStringHelper(const SymbolicScalar &value) {
@@ -105,7 +105,8 @@ std::string GetTypeForB16B32(const DataType &dtype);
 
 inline std::string GetPipeId(PipeType queue) {
     auto res = PIPE_ID.find(queue);
-    ASSERT(res != PIPE_ID.end()) << "can not find pipe id: " << ToUnderlying(queue);
+    ASSERT(res != PIPE_ID.end()) << GenErrorCode(GenOpCodeErrorScene::PIPE_ID_NOT_FOUND)
+                                 << "can not find pipe id: " << ToUnderlying(queue);
     return res->second;
 }
 
@@ -175,7 +176,8 @@ struct FloatSpecVal {
                 return iter->second;
             }
         }
-        ASSERT(false) << "FloatSpecVal not found, dtype: " << ToUnderlying(dtype) << ", value: " << value;
+        ASSERT(false) << GenErrorCode(GenOpCodeErrorScene::DATA_TYPE_UNSUPPORTED)
+                      << "FloatSpecVal not found, dtype: " << ToUnderlying(dtype) << ", value: " << value;
         return "";
     }
 };
