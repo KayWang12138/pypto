8c7961874d98c8607f9e9a270f9f3acc81ef0a48
jiachengtang <tangjiacheng7@huawei.com>
2026-03-10 09:27:38 +0800

    refactor(platform): Refactoring platform

--- framework/tests/ut/passes/src/test_inplace_process.cpp
+++ framework/tests/ut/passes/src/test_inplace_process.cpp
@@ -34,9 +34,7 @@ namespace tile_fwk {
 class InplaceProcessTest : public testing::Test {
 public:
     static void SetUpTestCase() {}
-
     static void TearDownTestCase() {}
-
     void SetUp() override {
         Program::GetInstance().Reset();
         config::Reset();
@@ -44,7 +42,6 @@ public:
         config::SetPlatformConfig(KEY_ENABLE_COST_MODEL, false);
     }
     void TearDown() override {}
-
     bool IsInplace(const Operation &op) {
         auto input = op.GetIOperands().front();
         auto output = op.GetOOperands().front();
