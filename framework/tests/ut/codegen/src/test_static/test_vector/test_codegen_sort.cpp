239c4db537b71247355dcbf9b8e29e82afd2bf76
2501_92924845 <niulinkai@huawei.com>
2026-03-11 16:37:15 +0800

    fix codegen ut error

--- framework/tests/ut/codegen/src/test_static/test_vector/test_codegen_sort.cpp
+++ framework/tests/ut/codegen/src/test_static/test_vector/test_codegen_sort.cpp
@@ -85,7 +85,7 @@ void TopKOnBoardFunc(TopKParams &params) {
     std::string res = GetResultFromCpp(*function);
     std::string expect = R"!!!(#include "TileOpImpl.h"
 
-// funcHash: 15443740490942774689
+// funcHash: 14959618075485442411
 
 extern "C" [aicore] void TENSOR_TOPK_T_TILETENSOR_2_0_4503599627370496(__gm__ GMTensorInfo* param, int64_t GMStackBase, __gm__ int64_t *hcclContext, __gm__ GMTensorInfo* oriAddrParam) {
 float __ubuf__ *UB_S0_E16384 = (float __ubuf__ *)get_imm(0x0); // size: 0x4000
