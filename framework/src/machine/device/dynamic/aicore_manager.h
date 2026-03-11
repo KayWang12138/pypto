45284159fed170cf7841d368c82dac02a51672c1
s00831018 <sergio.miguel.martin@huawei.com>
2026-03-11 01:53:35 +0000

    More simplifications

--- framework/src/machine/device/dynamic/aicore_manager.h
+++ framework/src/machine/device/dynamic/aicore_manager.h
@@ -569,38 +569,32 @@ private:
         if (ready == 0 ) {
             return 0;
         }
-        uint32_t readyId[MAX_MANAGER_AIV_NUM];
+
         ReadyQueueLock(readyQue);
         uint32_t head = __atomic_load_n(&readyQue->head, __ATOMIC_RELAXED);
         uint32_t tail = __atomic_load_n(&readyQue->tail, __ATOMIC_RELAXED);
-        uint32_t taskCount = std::min(ready, tail - head);
-        if (taskCount == 0) {
+        uint32_t pendingTaskCount = tail - head;
+
+        if (pendingTaskCount == 0) {
             ReadyQueueUnLock(readyQue);
             return 0;
         }
-        bool isRealLifo = (enableL2CacheSch_ && !firstLock[static_cast<int>(type)]);
-        if (isRealLifo) {
-            memcpy_s(readyId, taskCount * sizeof(uint64_t),
-                reinterpret_cast<uint8_t *>(&readyQue->elem[tail - taskCount]), taskCount * sizeof(uint32_t));
-            __atomic_fetch_sub(&readyQue->tail, taskCount, std::memory_order_release);
-        } else {
-            __atomic_fetch_add(&readyQue->head, taskCount, std::memory_order_release);
-        }
+
+        uint32_t taskCount = 1;
+        __atomic_fetch_add(&readyQue->head, taskCount, std::memory_order_release);
+
         ReadyQueueUnLock((readyQue));
-        BatchSendTask(type, isRealLifo ? &readyId[taskCount - 1] : &readyQue->elem[head],
-            taskCount, coreIdxStart, coreIdxEnd, isRealLifo);
-        firstLock[static_cast<int>(type)] = false;
+        BatchSendTask(type, &readyQue->elem[head], taskCount, coreIdxStart, coreIdxEnd);
         return taskCount;
     }
 
-    inline uint32_t BatchSendTask(CoreType type, uint32_t *newTask, uint32_t taskCount, int coreIdxStart, int coreIdxEnd, bool isLifo)
+    inline uint32_t BatchSendTask(CoreType type, uint32_t *newTask, uint32_t taskCount, int coreIdxStart, int coreIdxEnd)
     {
         uint32_t sendCnt = 0;
         uint32_t coreRunReadyCnt = context_->coreRunReadyCnt_[static_cast<int>(type)];
         while (sendCnt < static_cast<uint64_t>(coreRunReadyCnt) && sendCnt < taskCount) {
             SendTaskToAiCore(type,
-                context_->runReadyCoreIdx_[static_cast<int>(type)][--context_->coreRunReadyCnt_[static_cast<int>(type)]],
-                isLifo ? *newTask-- : *newTask++);
+                context_->runReadyCoreIdx_[static_cast<int>(type)][--context_->coreRunReadyCnt_[static_cast<int>(type)]], *newTask++);
             sendCnt++;
         }
         context_->corePendReadyCnt_[static_cast<int>(type)] -= sendCnt;
@@ -610,7 +604,7 @@ private:
         uint32_t lastProcCore = idx;
         while (context_->corePendReadyCnt_[static_cast<int>(type)] > 0 && sendCnt < taskCount) {
             if (pendingIds_[idx] == AICORE_TASK_INIT) {
-                SendTaskToAiCore(type, idx, isLifo ? *newTask-- : *newTask++);
+                SendTaskToAiCore(type, idx, *newTask++);
                 sendCnt++;
                 context_->corePendReadyCnt_[static_cast<int>(type)]--;
                 lastProcCore = idx;
@@ -697,11 +691,6 @@ private:
         uint32_t aicIndex = static_cast<uint32_t>(CoreType::AIC);
         uint32_t aivIndex = static_cast<uint32_t>(CoreType::AIV);
         if (context_->readyCount[aicIndex] > 0) {
-            uint32_t needSendCnt = std::min(GetReadyCoreNum(CoreType::AIC), context_->readyCount[aicIndex]);
-            if (needSendCnt > 0) {
-                context_->readyCount[aicIndex] -= BatchSendTask(CoreType::AIC, &context_->readyIds[aicIndex][context_->readyCount[aicIndex] - 1],
-                    needSendCnt, aicStart_, aicEnd_, true);
-            }
             if (context_->readyCount[aicIndex] > 0) {
                 // DEV_ERROR("Push Cube Tasks");
                 ret = PushReadyQue(readyAicCoreFunctionQue_, context_->readyIds[aicIndex], context_->readyCount[aicIndex]);
@@ -713,11 +702,6 @@ private:
         }
 
         if (context_->readyCount[aivIndex] > 0) {
-            uint32_t needSendCnt = std::min(GetReadyCoreNum(CoreType::AIV), context_->readyCount[aivIndex]);
-            if (needSendCnt > 0) {
-                context_->readyCount[aivIndex] -= BatchSendTask(CoreType::AIV, &context_->readyIds[aivIndex][context_->readyCount[aivIndex] - 1],
-                    needSendCnt, aivStart_, aivEnd_, true);
-            }
             if (context_->readyCount[aivIndex] > 0) {
                 // DEV_ERROR("Push Vector Tasks");
                 ret = PushReadyQue(readyAivCoreFunctionQue_, context_->readyIds[aivIndex], context_->readyCount[aivIndex]);
@@ -1100,8 +1084,6 @@ private:
         if constexpr (IsDeviceMode()) {
             aicoreHal_.MapRegistersForAllCores(aicNum_);
         } 
-        firstLock[static_cast<int>(CoreType::AIC)] = true;
-        firstLock[static_cast<int>(CoreType::AIV)] = true;
         preFetchSuccess_ = false;
         preFetchNextDevTaskCtrl_ = nullptr;
     }
@@ -1327,7 +1309,6 @@ private:
     uint64_t seq;
     AicoreHAL aicoreHal_;
     bool isFirstTaskSend_{true};
-    bool firstLock[AICORE_TYPE_NUM]{true,true};
     int aicNum_{0};
     int aivNum_{0};
     int aicValidNum_{0}; // 有效的aic，根据pgmask计算host传过来
