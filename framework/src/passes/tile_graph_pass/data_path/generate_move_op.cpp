3ea14cadb2f3d377a2a635e7cbc2352a234b7870
gggxinmeng <guoxinmeng@huawei.com>
2026-03-11 11:09:27 +0800

    fix(Pass): Modify the attribute isCube for the boundary in generate_move_op

--- framework/src/passes/tile_graph_pass/data_path/generate_move_op.cpp
+++ framework/src/passes/tile_graph_pass/data_path/generate_move_op.cpp
@@ -160,6 +160,9 @@ Status GenerateMoveOp::ProcessDefault(Function &function, Operation &op, ViewOpA
         SetL0C2L1CopyAttr(op, op.GetOOperands()[0]->GetShape(), OpImmediate::Specified(viewOpAttribute->GetFromTensorOffset()), OpImmediate::Specified(ZERO_OFFSET));
     } else {
         SetCopyAttr(op,viewOpAttribute);
+        if (op.GetOpcode() == Opcode::OP_L0C_COPY_UB) {
+            op.SetAttribute(OpAttributeKey::isCube, true);
+        }
     }
     return SUCCESS;
 }
