aeb7cde43feb4bce8993153201cd7cfb8b644f63
jiachengtang <tangjiacheng7@huawei.com>
2026-03-11 16:50:30 +0800

    refactor(platform): Update log & remove core version info

--- framework/src/utils/host_log/log_module_manager.cpp
+++ framework/src/utils/host_log/log_module_manager.cpp
@@ -66,8 +66,8 @@ int ParseStrToInt(const std::string &str) {
     return -1;
 }
 
-s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) { return !std::isspace(c); }));
 void Trim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) { return !std::isspace(c); }));
     s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); }).base(), s.end());
 }
 
