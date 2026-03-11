c0efdd1c0861b6d08e50e70867d9a772948e1920
2501_92924845 <niulinkai@huawei.com>
2026-03-11 16:20:51 +0800

    fix compile errorv2

--- framework/src/passes/tensor_graph_pass/infer_memory_conflict.h
+++ framework/src/passes/tensor_graph_pass/infer_memory_conflict.h
@@ -45,6 +45,7 @@ private:
     Status ObtainReshapeTile(Operation &op, Shape &inTileShape, Shape &outTileShape);
     Status InferTileShape(Operation &op, const LogicalTensorPtr &tensor, TileShape parentTile, Shape &reshapeTile);
     Status SetDefaultShape(const LogicalTensorPtr &tensor, std::vector<int64_t> &defaultTile);
+    Status ProcessViewType(Function &function);
     Status InsertCopysForViewAssemble(Function &function);
     TileShape ObtainTileShape(const std::unordered_set<Operation *> &origOp);
 
