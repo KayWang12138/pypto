5038810257b21aada9cc4e5d7d4402f884267c49
陈展熹 <chenzhanxi1@huawei.com>
2026-03-12 09:48:15 +0800

    fix(operation): Fix TPRELU interface

--- framework/src/interface/operation/vector/binary.cpp
+++ framework/src/interface/operation/vector/binary.cpp
@@ -172,7 +172,7 @@ void TiledBinaryOperation(Function &function, const TileShape &tileShape, Logica
 }
 
 void TiledPReLUOperation(
-    Function &function, const TileShape &tileShape, size_t cur, Input &input, Input &weight, const LogicalTensorPtr &result) {
+    Function &function, const TileShape &tileShape, size_t cur, Input &input, Input weight, const LogicalTensorPtr &result) {
     if (cur == input.tensor.GetShape().size()) {
         auto tile = input.tensor.GetStorage()->View(function, input.tileInfo.shape, input.tileInfo.offset);
         auto weightTile = weight.tensor.GetStorage()->View(function, weight.tileInfo.shape, weight.tileInfo.offset);
@@ -180,10 +180,10 @@ void TiledPReLUOperation(
         int axis = 5 - cur + 1;
         constexpr size_t ALIGN_SIZE = 32;
         constexpr size_t SIZEOFBYTE = 8;
-        int64_t tmpSize = ALIGN_SIZE / SIZEOFBYTE;
+        int64_t tmpSize = ALIGN_SIZE;
         if (axis == 4) {
             tmpSize = (input.tileInfo.shape[cur - 1] + SIZEOFBYTE - 1) / SIZEOFBYTE;
-            tmpSize = (tmpSize + ALIGN_SIZE - 1) / ALIGN_SIZE * ALIGN_SIZE;
+            tmpSize = (tmpSize + ALIGN_SIZE - 1) / ALIGN_SIZE * ALIGN_SIZE + ALIGN_SIZE;
         }
         std::vector<int64_t> tmpShape({tmpSize});
         auto tmpTensor = std::make_shared<LogicalTensor>(function, DT_UINT8, tmpShape);
