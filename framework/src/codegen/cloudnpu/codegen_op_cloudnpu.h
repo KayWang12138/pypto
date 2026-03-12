be6a3e1ecbf90cdb5517ef5791d4d83fd58b29f4
Bee.Hujun <Bee.hujun@huawei.com>
2026-03-12 14:23:30 +0800

    feat(codegen): Add querying interface for type of TileTensor

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -168,7 +168,9 @@ public:
     std::string GenOpCode() const override;
 
 private:
+    const TileTensor &QueryTileTensorByIdx(int paramIdx) const;
     std::string QueryTileTensorNameByIdx(int paramIdx) const;
+    std::string QueryTileTensorTypeByIdx(int paramIdx) const;
 
     std::string GenTemplateParamsForPutAndGet() const;
     std::string GenTemplateParamsForSignal() const;
