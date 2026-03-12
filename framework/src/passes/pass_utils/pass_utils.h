540686d66ca7c10ee421e7919f3fc3364375d91f
2501_92924845 <niulinkai@huawei.com>
2026-03-12 17:52:01 +0800

    fix(pass): Modifier redundant reshape condition for shape

--- framework/src/passes/pass_utils/pass_utils.h
+++ framework/src/passes/pass_utils/pass_utils.h
@@ -511,6 +511,11 @@ public:
         return oss.str();
     }
 
+    // 判断 Tensor 的 shape 是否存在-1
+    static bool ContainsNegativeOne(const Shape &shape) {
+        return std::any_of(shape.begin(), shape.end(), [](int64_t val) { return val == -1; });
+    }
+
     // Number of Elements, 用来计算给定（tensor的）shape的总元素数量
     static int64_t Numel(const Shape &shape) {
         if (shape.empty())
@@ -520,6 +525,11 @@ public:
             numel *= num;
         }
         return numel;
+    }
+
+    // 判断 Tensor 的 shape 是否存在-1
+    static bool ContainsNegativeOne(const Shape &shape) {
+        return std::any_of(shape.begin(), shape.end(), [](int64_t val) { return val == -1; });
     }
 
     static std::unordered_map<MemoryType, int64_t> GetLocalMemorySize();
