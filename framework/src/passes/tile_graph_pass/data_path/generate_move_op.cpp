2efb0eae6cf9af78d47de406c1d95d3378dbb183
xujiayi1999 <xujiayi28@huawei.com>
2026-03-12 23:04:41 +0800

    Revert "fix(Pass):Change old ALOG to APASS_LOG"
    This reverts commit e2bbaf5d903b94c0544ec3b0966b398e83f86b59.

--- framework/src/passes/tile_graph_pass/data_path/generate_move_op.cpp
+++ framework/src/passes/tile_graph_pass/data_path/generate_move_op.cpp
@@ -243,14 +243,11 @@ void GenerateMoveOp::CreateMoveOpForAssemble(Operation &op) const {
     auto assembleOpAttribute = dynamic_cast<AssembleOpAttribute *>(op.GetOpAttribute().get());
     auto ASSEMBLE_in = op.iOperand.front();
     auto parentOp = *ASSEMBLE_in->GetProducers().begin();
-    auto inputMemtype = ASSEMBLE_in->GetMemoryTypeOriginal();
+    auto inputMemtype = op.iOperand.front()->GetMemoryTypeOriginal();
     auto outputMemtype = op.oOperand.front()->GetMemoryTypeOriginal();
     if (inputMemtype == MemoryType::MEM_L0C && outputMemtype == MemoryType::MEM_L1) {
         SetOpcodeByMemPath(op, inputMemtype, outputMemtype);
-        SetL0C2L1CopyAttr(op,
-                          op.GetIOperands()[0]->GetShape(),
-                          OpImmediate::Specified(ZERO_OFFSET),
-                          OpImmediate::Specified(assembleOpAttribute->GetToTensorOffset()));
+        SetL0C2L1CopyAttr(op, op.GetIOperands()[0]->GetShape(), OpImmediate::Specified(ZERO_OFFSET), OpImmediate::Specified(assembleOpAttribute->GetToTensorOffset()));
         return;
     }
     if (inputMemtype == MemoryType::MEM_DEVICE_DDR || outputMemtype != MemoryType::MEM_DEVICE_DDR ||
@@ -271,22 +268,18 @@ void GenerateMoveOp::CreateMoveOpForAssemble(Operation &op) const {
 Status GenerateMoveOp::CreateMoveOpForConvert(Function &function, Operation &op) const {
     auto convertOpAttribute = dynamic_cast<ConvertOpAttribute *>(op.GetOpAttribute().get());
     auto [from, to] = convertOpAttribute->GetConvertPath();
-    Status status = SetOpcodeByMemPath(op, from, to);
-    if (op.GetOpcode() == Opcode::OP_UB_COPY_L1) {
+    Status status = SetOpcodeByMemPath(op,from,to);
+    if(op.GetOpcode() == Opcode::OP_UB_COPY_L1) {
         ProcessUB2L1(function, op);
     }
     if (op.GetOpcode() == Opcode::OP_L0C_TO_L1) {
-        SetL0C2L1CopyAttr(op, 
-                          op.GetOOperands()[0]->GetShape(), 
-                          OpImmediate::Specified(ZERO_OFFSET), 
-                          OpImmediate::Specified(ZERO_OFFSET));
+        SetL0C2L1CopyAttr(op, op.GetOOperands()[0]->GetShape(), OpImmediate::Specified(ZERO_OFFSET), OpImmediate::Specified(ZERO_OFFSET));
     }
-    if (status != SUCCESS) { return status; }
+    if(status != SUCCESS) {return status;}
     auto childOp = *op.oOperand.front()->GetConsumers().begin();
     op.UpdateSubgraphID(childOp->GetSubgraphID());
     return SUCCESS;
 }
-
 void GenerateMoveOp::ProcessUB2L1(Function &function, Operation &op) const {
     //插入UB2L1节点（NZ2NZ)，并设置UBcopyL1的NZ属性
     op.SetAttribute(OP_ATTR_PREFIX + "is_nz", 1);
