50c9ebffa3a5c4c37c94485b97d250fe5d0e375a
王润法 <wangrunfa1@huawei.com>
2026-03-12 17:23:15 +0800

    Permute

--- framework/src/codegen/cloudnpu/codegen_mte_gather.cpp
+++ framework/src/codegen/cloudnpu/codegen_mte_gather.cpp
@@ -297,8 +297,11 @@ std::string CodeGenOpCloudNPU::GenGatherOp() const
     return "";
 }
 
-std::string CodeGenOpCloudNPU::PrintGatherInUBLayout() const
-{
+std::string CodeGenOpCloudNPU::GenPermuteOp() const {
+    return PrintPermuteLayout();
+}
+
+std::string CodeGenOpCloudNPU::PrintGatherInUBLayout() const {
     constexpr int paramIndex = 1;
     constexpr int indicesIndex = 2;
     constexpr int blockTableIndex = 3;
