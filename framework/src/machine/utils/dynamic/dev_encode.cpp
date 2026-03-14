8b6c5447c90a5f73f76b6c2785d3624d64dc9a10
chengding888 <chengding1@hisilicon.com>
2026-03-14 18:02:33 +0800

    fix(machine): Fix bugs of enable_vf = true

--- framework/src/machine/utils/dynamic/dev_encode.cpp
+++ framework/src/machine/utils/dynamic/dev_encode.cpp
@@ -1973,9 +1973,16 @@ struct EncodeDevAscendProgramInfo {
         }
     }
 
+    bool GetEnableVF() {
+        bool enableVF = Platform::Instance().GetSoc().GetNPUArch() == NPUArch::DAV_3510;
+        enableVF = enableVF && config::GetPassGlobalConfig(KEY_ENABLE_VF, false);
+        return (config::GetRuntimeOption<int64_t>(CFG_VALID_SHAPE_OPTIMIZE) == 1 || enableVF);
+    };
+
     void Init(DevAscendProgram *devProg, bool fillContent) {
         uintdevptr_t initOffset = reinterpret_cast<uintdevptr_t>(devProg->data);
         devProg->devArgs.archInfo = static_cast<ArchInfo>(Platform::Instance().GetSoc().GetNPUArch());
+        devProg->devArgs.enableVf = GetEnableVF();
         devProg->slotSize = dyndevAttr->inoutLink.totalSlot;
         devProg->runtimeOutcastPoolSize = dyndevAttr->inoutLink.totalSlot * (ExpectedMaxCachedNum() + 1);
         devProg->assembleSlotSize = dyndevAttr->inoutLink.assembleSlotIndexList.size();
