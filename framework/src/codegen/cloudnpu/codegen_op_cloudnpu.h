77e68a60bd5d35068b902de22d9d3358910a701d
Apricityh <wangxu359@huawei.com>
2026-03-16 19:46:32 +0800

    feat(Operation): Add MScatter operation

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -112,6 +112,8 @@ public:
 
     std::string GenScatterElementSOp() const;
     std::string GenScatterOp() const;
+    std::string PrintScatterLayout() const;
+    std::string PrintScatterDynamicUnaligned() const;
 
     std::string GenIndexAddOp() const;
 
@@ -435,8 +437,8 @@ private:
     std::string PrintScatterElementSOpStatic(const PrintScatterElemParam &param) const;
     std::string PrintScatterElementSOpDynamicUnaligned(const PrintScatterElemParam &param) const;
     std::string PrintScatterElementSTileTensor(const PrintScatterElemParam &param) const;
-    std::string PrintScatterOpDynamicUnaligned(const PrintScatterParam &param) const;
-    std::string PrintScatterTileTensor(const PrintScatterParam &param) const;
+    // std::string PrintScatterOpDynamicUnaligned(const PrintScatterParam &param) const;
+    // std::string PrintScatterTileTensor(const PrintScatterParam &param) const;
 
     std::string PrintIndexAddDynamicUnaligned(const PrintIndexAddParam &param) const;
     std::string PrintIndexAddTileTensor(const PrintIndexAddParam &param) const;
