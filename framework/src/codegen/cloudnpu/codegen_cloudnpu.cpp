7010a92bfa691e06869691188a7bc96ae77b5c32
luozhonglin222 <luozhonglin1@huawei.com>
2026-03-16 17:20:21 +0800

    fix(distributed): Fixed code review

--- framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
@@ -576,17 +576,20 @@ void EncodeWaitUntilInfo(const Operation &op, std::vector<int32_t> &code) {
     for (auto dimShape : op.GetInputOperand(1)->GetShape()) {
         code.push_back(dimShape);
     }
-    // 编码waitUntil的attr属性
+    // 编码waitUntil的attr属性，顺序固定
     std::map<std::string, Any> map = op.GetAllAttribute();
     auto it = map.find(OpAttributeKey::distOpAttr);
     if (it != map.end()) {
         Distributed::ShmemWaitUntilAttr distAttr = AnyCast<Distributed::ShmemWaitUntilAttr>(it->second);
-        code.push_back(static_cast<int32_t>(distAttr.GetFieldCount())); // 属性顺序固定
-        code.push_back(static_cast<int32_t>(distAttr.expectedSum));
-        code.push_back(static_cast<int32_t>(distAttr.signalStride));
-        code.push_back(static_cast<int32_t>(distAttr.resetSignal));
-        code.push_back(static_cast<int32_t>(distAttr.tileRowShape));
-        code.push_back(static_cast<int32_t>(distAttr.tileColShape));
+        std::vector<int32_t> attrs = {
+            static_cast<int32_t>(distAttr.expectedSum),
+            static_cast<int32_t>(distAttr.signalStride),
+            static_cast<int32_t>(distAttr.resetSignal),
+            static_cast<int32_t>(distAttr.tileRowShape),
+            static_cast<int32_t>(distAttr.tileColShape)
+        };
+        code.push_back(static_cast<int32_t>(attrs.size()));
+        code.insert(code.end(), attrs.begin(), attrs.end());
     }
 }
 
