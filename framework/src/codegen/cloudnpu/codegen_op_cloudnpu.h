343e89b9065c0b9c3b48c477f4676ffec8b09644
Apricityh <wangxu359@huawei.com>
2026-03-16 19:46:32 +0800

    feat(Operation): Add MScatter operation

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -115,6 +115,8 @@ public:
 
     std::string GenScatterElementSOp() const;
     std::string GenScatterOp() const;
+    std::string PrintScatterLayout() const;
+    std::string PrintScatterDynamicUnaligned() const;
 
     std::string GenIndexAddOp() const;
 
@@ -444,11 +446,9 @@ private:
     std::string PrintTmove() const;
     std::string PrintL0CToL1TileTensor() const;
 
-    std::string PrintScatterElementSOpStatic(const PrintScatterElemParam& param) const;
-    std::string PrintScatterElementSOpDynamicUnaligned(const PrintScatterElemParam& param) const;
-    std::string PrintScatterElementSTileTensor(const PrintScatterElemParam& param) const;
-    std::string PrintScatterOpDynamicUnaligned(const PrintScatterParam& param) const;
-    std::string PrintScatterTileTensor(const PrintScatterParam& param) const;
+    std::string PrintScatterElementSOpStatic(const PrintScatterElemParam &param) const;
+    std::string PrintScatterElementSOpDynamicUnaligned(const PrintScatterElemParam &param) const;
+    std::string PrintScatterElementSTileTensor(const PrintScatterElemParam &param) const;
 
     std::string PrintIndexAddDynamicUnaligned(const PrintIndexAddParam& param) const;
     std::string PrintIndexAddTileTensor(const PrintIndexAddParam& param) const;
