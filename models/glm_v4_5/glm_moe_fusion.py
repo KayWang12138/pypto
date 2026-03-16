079c133886961713db618f25c43da4b891427af6
z00521416 <zhangyongtao7@hisilicon.com>
2026-03-16 16:30:51 +0800

    feat(dfx): Fix the global settings set_host_options not taking effect.

--- models/glm_v4_5/glm_moe_fusion.py
+++ models/glm_v4_5/glm_moe_fusion.py
@@ -107,10 +107,10 @@ def gen_quan_per_channel_weight_nz(x):
 
 
 @pypto.frontend.jit(
-    host_options={
-        "compile_monitor_enable": True,
-        "compile_timeout": 10,
-        "compile_timeout_stage": 5,
+    host_options={ 
+        "compile_monitor_enable": True, 
+        "compile_timeout": 10, 
+        "compile_timeout_stage": 5, 
         "compile_monitor_print_interval": 2},
     runtime_options={"device_sched_mode": 1,
                     "stitch_function_max_num": 128,
