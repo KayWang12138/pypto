17ec9f28c747d79abb27b9ce27f909a6da12accb
gggxinmeng <guoxinmeng@huawei.com>
2026-03-17 11:31:24 +0800

    feat(Pass): Test yellow ci

--- framework/src/passes/block_graph_pass/schedule_ooo/spill_buffer.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/spill_buffer.cpp
@@ -175,7 +175,7 @@ void OoOScheduler::ReplaceTensorMemId(IssueEntryPtr& issue, int oldMemId, int ne
 Status OoOScheduler::UpdateRemainOpBufId(int oldMemId, int newMemId)
 {
     if (bufRefCount_.find(oldMemId) == bufRefCount_.end()) {
-        APASS_LOG_ERROR_F(Elements::Tensor, "bufRefCount cannot find Tensor[%d].", oldMemId);
+        APASS_LOG_ERROR_F(Elements::Tensor, "bufRefCount cannot find Tensor[%d]. ", oldMemId);
         return FAILED;
     }
     bufRefCount_[newMemId] = bufRefCount_[oldMemId] + TWO_ISSUE;
