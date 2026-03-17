f864063340b65a752e2e9f5ee53d33e8ec1f8baf
guhuangxing <guhuangxing@hisilicon.com>
2026-03-17 10:11:18 +0800

    fix(ir): 在INTERNAL_CHECK中添加span信息以便定位错误来源
    检视意见修复：program.cpp和expr.cpp中的INTERNAL_CHECK
    添加span_.ToString()输出，方便出错时定位到源代码位置。

--- framework/src/interface/ir/program.cpp
+++ framework/src/interface/ir/program.cpp
@@ -32,11 +32,12 @@ Program::Program(const std::vector<FunctionPtr> &functions, std::string name, Sp
     // The map automatically sorts by GlobalVar name via the GlobalVarPtrLess comparator
     std::set<std::string> functionNames;
     for (const auto &func : functions) {
-        INTERNAL_CHECK(func) << "Program constructor encountered null function";
+        INTERNAL_CHECK(func) << "Program constructor encountered null function at " << span_.ToString();
         auto funcName = func->name_;
-        INTERNAL_CHECK(!funcName.empty()) << "Program constructor encountered empty function name";
+        INTERNAL_CHECK(!funcName.empty()) << "Program constructor encountered empty function name at "
+                                          << span_.ToString();
         INTERNAL_CHECK(functionNames.find(funcName) == functionNames.end())
-            << "Duplicate function name \"" << funcName << "\"";
+            << "Duplicate function name \"" << funcName << "\" at " << span_.ToString();
         functionNames.insert(funcName);
         auto globalVar = std::make_shared<const GlobalVar>(funcName);
         functions_.emplace(globalVar, func);
