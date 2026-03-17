29554f9d64aa2143d4245e850c4419a5c4ca9293
gaoxingwang <camuel.gao@huawei.com>
2026-03-17 14:41:20 +0800

    feat(machine): Support BN loop parallel

--- framework/src/machine/utils/dynamic/allocator/allocators.h
+++ framework/src/machine/utils/dynamic/allocator/allocators.h
@@ -21,6 +21,7 @@
 #include "seq_ws_allocator.h"
 #include "ws_allocator_counter.h"
 #include "ws_metadata_allocator.h"
+#include "tilefwk/aikernel_data.h"
 
 namespace npu::tile_fwk::dynamic {
 struct MetadataAllocator {
@@ -30,11 +31,72 @@ struct MetadataAllocator {
 };
 
 struct TensorAllocator {
-    SeqWsAllocator rootInner;
-    SeqWsAllocator devTaskInnerExclusiveOutcasts;
-    WsSlotAllocator devTaskBoundaryOutcasts;
+    uint32_t curParallelWsId{0}; // different parallel for loop use different workspace range
+    uint32_t parallelism{1};
+    SeqWsAllocator rootInner[SCH_DEVTASK_MAX_PARALLELISM];
+    SeqWsAllocator devTaskInnerExclusiveOutcasts[SCH_DEVTASK_MAX_PARALLELISM];
+    WsSlotAllocator devTaskBoundaryOutcasts[SCH_DEVTASK_MAX_PARALLELISM];
+
+    bool RootInnerCanAllocate(uint64_t rootInnerMemReq) {
+        return rootInner[curParallelWsId].CanAllocate(rootInnerMemReq);
+    }
+
+    WsAllocation RootInnerMalloc(uint64_t memReq, WsMemCategory category = WsMemCategory::UNCLASSIFIED) {
+        return rootInner[curParallelWsId].Malloc(memReq, category);
+    }
+
+    void RootInnerResetPool() {
+        return rootInner[curParallelWsId].ResetPool();
+    }
+
+    uint32_t RootInnerResetTimes() const {
+        return rootInner[curParallelWsId].ResetTimes();
+    }
+
+    bool DevTaskInnerExOutCastCanAllocate(uint64_t rootInnerMemReq) {
+        return devTaskInnerExclusiveOutcasts[curParallelWsId].CanAllocate(rootInnerMemReq);
+    }
+
+    WsAllocation DevTaskInnerExOutCastMalloc(uint64_t memReq, WsMemCategory category = WsMemCategory::UNCLASSIFIED) {
+        return devTaskInnerExclusiveOutcasts[curParallelWsId].Malloc(memReq, category);
+    }
+
+    void DevTaskInnerExOutCastResetPool() {
+        devTaskInnerExclusiveOutcasts[curParallelWsId].ResetPool();
+    }
+
+    size_t DevTaskBoundaryOutcastsAvailableSlots() const {
+        return devTaskBoundaryOutcasts[curParallelWsId].AvailableSlots();;
+    }
+
+    bool DevTaskBoundaryOutcastsIsValidSlotMemReq(uint64_t memReq) const {
+        return devTaskBoundaryOutcasts[curParallelWsId].IsValidSlotMemRequirement(memReq);
+    }
+
+    WsAllocation DevTaskBoundaryOutcastsAllocate() {
+        WsAllocation allocation = devTaskBoundaryOutcasts[curParallelWsId].Allocate();
+        allocation.parallelWsId = curParallelWsId;
+        return allocation;
+    }
+
+    uint64_t DevTaskBoundaryOutcastsSlotByteSize() const {
+        return devTaskBoundaryOutcasts[curParallelWsId].SlotByteSize();
+    }
+
+    void DevTaskBoundaryOutcastsDeallocate(WsAllocation allocation) {
+        return devTaskBoundaryOutcasts[allocation.parallelWsId].Deallocate(allocation.ptr);
+    }
+
+    void DumpMemoryUsage(const char* hint) const {
+        for (uint32_t i = 0; i < parallelism; i++) {
+            DEV_MEM_DUMP("Parallel workspace %u.", i);
+            rootInner[i].DumpMemoryUsage(hint, "Tensor (root inner) workspace");
+            devTaskInnerExclusiveOutcasts[i].DumpMemoryUsage(hint, "Tensor (DeviceTask inner outcasts) workspace");
+            devTaskBoundaryOutcasts[i].DumpMemoryUsage(hint);
+        }
+    }
 };
 struct RuntimeReuseInfo {
     uint32_t poolResetTimes;
 };
-} // namespace npu::tile_fwk::dynamic
+}
--- framework/src/machine/utils/dynamic/dev_encode.cpp
+++ framework/src/machine/utils/dynamic/dev_encode.cpp
@@ -2253,7 +2253,8 @@ struct EncodeDevAscendProgramInfo {
         devProg->devArgs.archInfo = static_cast<ArchInfo>(Platform::Instance().GetSoc().GetNPUArch());
         devProg->devArgs.enableVFFusion = GetEnableVFFusion();
         devProg->slotSize = dyndevAttr->inoutLink.totalSlot;
-        devProg->runtimeOutcastPoolSize = dyndevAttr->inoutLink.totalSlot * (ExpectedMaxCachedNum() + 1);
+        devProg->runtimeOutcastPoolSize =
+            dyndevAttr->inoutLink.totalSlot * (ExpectedMaxCachedNum() + 1) * devProg->GetParallelism();
         devProg->assembleSlotSize = dyndevAttr->inoutLink.assembleSlotIndexList.size();
         devProg->InitSymbolTable(initOffset, &dyndevAttr->symbolTable, fillContent);
         devProg->InitExpressionTableBinary(initOffset, dyndevAttr->expressionTableBinaryList, fillContent);
@@ -2577,7 +2578,7 @@ static uint64_t CalcGeneralMetadataSlabWorkspace(DevAscendProgram* devProg)
     uint32_t slabSize = workspace.CalcSlabMemObjmaxSize() * ALLOC_NUM_ONE_SLAB;
     uint32_t slabCapacity[ToUnderlying(WsAicpuSlabMemType::COHERENT_SLAB_MEM_TYPE_BUTT)];
     size_t objUsedNum[ToUnderlying(WsAicpuSlabMemType::COHERENT_SLAB_MEM_TYPE_BUTT)]{
-        ExpectedMaxCachedNum(),         // DevFunctionDupped
+        MAX_STITCH_FUNC_NUM,         // DevFunctionDupped
         1,                              // DynFuncData
         1,                              // VecStitchList
         1,                              // DynDevTask
@@ -2597,20 +2598,20 @@ static uint64_t CalcGeneralMetadataSlabWorkspace(DevAscendProgram* devProg)
         uint32_t requiredSlabNum = (objUsedNum[i] + slabCapacity[i] - 1) / slabCapacity[i];
         // alloc redundant slabpage for DuppedFunction and Readyque to prevent memory border situations
         if(i == ToUnderlying(WsAicpuSlabMemType::DUPPED_FUNC_DATA) ||
-         i == ToUnderlying(WsAicpuSlabMemType::READY_QUE)) requiredSlabNum++;
+         i == ToUnderlying(WsAicpuSlabMemType::READY_QUE)) requiredSlabNum += 3;
         MACHINE_LOGI("[workspaceSize] RequiredSlabNum[%d] is %u.", i, requiredSlabNum);
         generalMetadataSlabSize += static_cast<uint64_t>(requiredSlabNum) * slabSize;
     }
     MACHINE_LOGI("[workspaceSize] General->MetadataSlabSize is %lu.", static_cast<unsigned long>(generalMetadataSlabSize));
     generalMetadataSlabSize = (generalMetadataSlabSize < GENERAL_METADATA_SIZE_MIN) ? GENERAL_METADATA_SIZE_MIN : generalMetadataSlabSize;
-    return generalMetadataSlabSize;
+    return generalMetadataSlabSize * devProg->GetParallelism();
 }
 
 static uint64_t CalcStitchWorkspace(DevAscendProgram& devProg)
 {
     (void)devProg;
     static constexpr uint64_t AICPU_STITCH_SIZE = 2 * MEBI;
-    return AICPU_STITCH_SIZE;
+    return AICPU_STITCH_SIZE * devProg.GetParallelism();
 }
 
 static uint64_t DumpTensorWorkspace()
@@ -2642,12 +2643,13 @@ void EncodeDevAscendProgram(Function* func, uint64_t& offset, DevAscendProgram* 
         encodeInfo.Init(&devfunc, false);
         offset = devfunc.GetSize();
     } else {
+        uint32_t parallism = 1; // need get prallism from  parallel option
+        base->SetParallelism(parallism);
         encodeInfo.Init(base, true);
         offset = base->GetSize();
 
         // Calc workspace size
         TensorWorkspaceResult tensorWsRes = CalcTensorWorkspace(func, *base);
-
         base->slottableOutcastSlotSize = tensorWsRes.totalExclusiveOutcastSlot + tensorWsRes.totalAssembleOutcastSlot;
 
         base->memBudget.tensor.rootInner = tensorWsRes.rootInnerMem;
@@ -2690,7 +2692,9 @@ void DevControlFlowCache::Init(
     uint64_t slottedCount =
         dyndevAttr->inoutLink.totalSlot *
         (std::min((uint32_t)EstimatedStitchingCount(), stitchMaxFunctionNum) + SLOTS_NEED_ALLOC_SIZE);
-    runtimeBackup.workspace.tensorAllocators.slottedOutcastsBlockList.HostInitDataSizeOffset(initOffset, slottedCount);
+    for (uint32_t i = 0; i < SCH_DEVTASK_MAX_PARALLELISM; i++) {
+        runtimeBackup.workspace.tensorAllocators.slottedOutcastsBlockList[i].HostInitDataSizeOffset(initOffset, slottedCount);
+    }
 
     runtimeBackup.slotContext.slotList.HostInitDataSizeOffset(initOffset, dyndevAttr->inoutLink.totalSlot);
     runtimeBackup.workspace.runtimeOutcastTensorPool.HostInitDataSizeOffset(initOffset, runtimeOutcastPoolSize);
@@ -2705,7 +2709,6 @@ void DevControlFlowCache::Init(
     deviceTaskSkippedCount = 0;
     cacheDataOffset = 0;
     workspaceAddr = 0;
-    stitchMaxFunctionNum_ = stitchMaxFunctionNum;
     dataSize = initOffset - reinterpret_cast<uintdevptr_t>(data);
 }
 
