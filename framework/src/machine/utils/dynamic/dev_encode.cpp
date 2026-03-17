34e0207585482a9f75606dac2ea5bbbfa1cca4c5
gaoxingwang <camuel.gao@huawei.com>
2026-03-17 14:41:20 +0800

    feat(machine): Support BN loop parallel

--- framework/src/machine/utils/dynamic/dev_encode.cpp
+++ framework/src/machine/utils/dynamic/dev_encode.cpp
@@ -2577,7 +2577,7 @@ static uint64_t CalcGeneralMetadataSlabWorkspace(DevAscendProgram* devProg)
     uint32_t slabSize = workspace.CalcSlabMemObjmaxSize() * ALLOC_NUM_ONE_SLAB;
     uint32_t slabCapacity[ToUnderlying(WsAicpuSlabMemType::COHERENT_SLAB_MEM_TYPE_BUTT)];
     size_t objUsedNum[ToUnderlying(WsAicpuSlabMemType::COHERENT_SLAB_MEM_TYPE_BUTT)]{
-        ExpectedMaxCachedNum(),         // DevFunctionDupped
+        MAX_STITCH_FUNC_NUM,         // DevFunctionDupped
         1,                              // DynFuncData
         1,                              // VecStitchList
         1,                              // DynDevTask
@@ -2603,14 +2603,14 @@ static uint64_t CalcGeneralMetadataSlabWorkspace(DevAscendProgram* devProg)
     }
     MACHINE_LOGI("[workspaceSize] General->MetadataSlabSize is %lu.", static_cast<unsigned long>(generalMetadataSlabSize));
     generalMetadataSlabSize = (generalMetadataSlabSize < GENERAL_METADATA_SIZE_MIN) ? GENERAL_METADATA_SIZE_MIN : generalMetadataSlabSize;
-    return generalMetadataSlabSize;
+    return generalMetadataSlabSize * devProg->GetParallelism();
 }
 
 static uint64_t CalcStitchWorkspace(DevAscendProgram& devProg)
 {
     (void)devProg;
     static constexpr uint64_t AICPU_STITCH_SIZE = 2 * MEBI;
-    return AICPU_STITCH_SIZE;
+    return AICPU_STITCH_SIZE * devProg.GetParallelism();
 }
 
 static uint64_t DumpTensorWorkspace()
@@ -2647,7 +2647,8 @@ void EncodeDevAscendProgram(Function* func, uint64_t& offset, DevAscendProgram* 
 
         // Calc workspace size
         TensorWorkspaceResult tensorWsRes = CalcTensorWorkspace(func, *base);
-
+        uint32_t parallism = 1; // need get prallism from  parallel option
+        base->SetParallelism(parallism);
         base->slottableOutcastSlotSize = tensorWsRes.totalExclusiveOutcastSlot + tensorWsRes.totalAssembleOutcastSlot;
 
         base->memBudget.tensor.rootInner = tensorWsRes.rootInnerMem;
@@ -2690,7 +2691,9 @@ void DevControlFlowCache::Init(
     uint64_t slottedCount =
         dyndevAttr->inoutLink.totalSlot *
         (std::min((uint32_t)EstimatedStitchingCount(), stitchMaxFunctionNum) + SLOTS_NEED_ALLOC_SIZE);
-    runtimeBackup.workspace.tensorAllocators.slottedOutcastsBlockList.HostInitDataSizeOffset(initOffset, slottedCount);
+    for (uint32_t i = 0; i < SCH_DEVTASK_MAX_PARALLELISM; i++) {
+        runtimeBackup.workspace.tensorAllocators.slottedOutcastsBlockList[i].HostInitDataSizeOffset(initOffset, slottedCount);
+    }
 
     runtimeBackup.slotContext.slotList.HostInitDataSizeOffset(initOffset, dyndevAttr->inoutLink.totalSlot);
     runtimeBackup.workspace.runtimeOutcastTensorPool.HostInitDataSizeOffset(initOffset, runtimeOutcastPoolSize);
