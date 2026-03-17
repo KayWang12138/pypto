a3a7a558baac51ca1389eaf25970560dbf94121d
zhuqingbo <zhuqingbo@huawei.com>
2026-03-17 15:01:58 +0800

    fix(interpreter): Fix the bug of WHERE_SS op

--- python/tests/st/interface/test_verify_frontend_jit.py
+++ python/tests/st/interface/test_verify_frontend_jit.py
@@ -70,3 +70,36 @@ def test_verify_dyn():
     pypto.set_verify_golden_data(goldens=[None, None, golden.cpu()])
     add_dyn_kernel(a, b, output_data)
     assert torch.allclose(output_data, golden)
+
+
+@pypto.frontend.jit(runtime_options={"run_mode": pypto.RunMode.NPU},
+                    verify_options=verify_options
+                    )
+def cmp_where_kenrel(
+        a: pypto.Tensor([pypto.STATIC, pypto.STATIC], pypto.DT_FP16), 
+        out: pypto.Tensor([pypto.STATIC, pypto.STATIC], pypto.DT_FP16)):
+
+    for _ in pypto.loop(1):
+        pypto.set_vec_tile_shapes(16, 16)
+        mask = pypto.ge(a, 0.5)
+        out[:] = pypto.where(mask, 1.0, 0.0)
+
+
+def test_verify_where():
+    device_id = int(os.environ.get('TILE_FWK_DEVICE_ID', 0))
+    torch.npu.set_device(device_id)
+
+    a = torch.rand((64, 64), dtype=torch.float16)
+    c = torch.zeros((64, 64))
+
+    golden = torch.where(a >= 0.5, 1.0, 0.0)
+    pypto.set_verify_golden_data(goldens=[None, golden])
+
+    inputs = [a.to(f"npu:{device_id}")]
+    outputs = [c.to(f"npu:{device_id}")]
+    pto_inputs = [pypto.from_torch(tensor, f"IN_{idx}") for idx, tensor in enumerate(inputs)]
+    pto_outputs = [pypto.from_torch(tensor, f"OUT_{idx}") for idx, tensor in enumerate(outputs)]
+
+    cmp_where_kenrel(*pto_inputs, *pto_outputs)
+
+    assert torch.allclose(outputs[0].cpu(), golden)
