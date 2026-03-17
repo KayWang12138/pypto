1888423e454f494d5cd2de2df887d5988504e083
gaoxingwang <camuel.gao@huawei.com>
2026-03-17 14:41:20 +0800

    feat(machine): Support BN loop parallel

--- framework/src/interface/machine/device/tilefwk/aicpu_common.h
+++ framework/src/interface/machine/device/tilefwk/aicpu_common.h
@@ -19,11 +19,12 @@
 #include <cstddef>
 #include <cstdint>
 #include "aicpu_perf.h"
+#include "aikernel_data.h"
 
 const uint64_t AICORE_TASK_INIT = 0xFFFFFFFF;
-const uint64_t AICORE_TASK_DISTRIBUTED = 0x7FFFFFFE;
-const uint64_t AICORE_TASK_STOP = 0x7FFFFFF0;
-const uint64_t AICORE_FUNC_STOP = 0x7FFFFFE0;
+const uint64_t AICORE_TASK_DISTRIBUTED = 0xF0000003;
+const uint64_t AICORE_TASK_STOP = 0xF0000001;
+const uint64_t AICORE_FUNC_STOP = 0xF0000002;
 const uint64_t AICORE_FIN_MASK = 0x80000000;
 const uint64_t AICORE_TASK_MAX = 0x70000000;
 
@@ -240,6 +241,16 @@ inline const char* AicorePerfTraceName[] = {
     "WAIT_ALL_DEV_TASK_CALLOP_EXEC_FINISH",
     "WAIT_EXIT_NOTIFY"};
 
+
+// use ring buffer to control parallel multi devtask
+struct ParallelDevTask {
+    uint32_t front{0};
+    uint32_t rear{0};
+    uint32_t version;
+    uint32_t reserver;
+    int64_t elements[npu::tile_fwk::SCH_DEVTASK_MAX_PARALLELISM]; // device task ptr
+};
+
 struct TaskEntry {
     int32_t subGraphId;
     int32_t taskId;
@@ -256,6 +267,7 @@ struct KernelArgs {
     int64_t shakeBuffer[8];
     int64_t shakeBufferCpuToCore[8];
     int64_t waveBufferCpuToCore[8];
+    struct ParallelDevTask parallelDevTask;
     TaskEntry taskEntry;
     TaskStat taskStat[2]; // 寄存器高低32位，两个task 和 pending & running task存储： 2 * 2 个
 };
