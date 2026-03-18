b8a3108446e27a645c9076c4d9de07aaa0820ace
luoxinhui666 <luoxinhui2@huawei.com>
2026-03-18 11:17:06 +0800

    feat(distributed): DynValidShape and dynOffset for shmem api

--- framework/src/interface/operation/distributed/shmem_operation_impl.cpp
+++ framework/src/interface/operation/distributed/shmem_operation_impl.cpp
@@ -250,6 +250,21 @@ static Tensor ShmemPutImpl(const Tensor& src, const ShmemTensor& dst, const Symb
     auto& op = isUb2Gm ?
         function.AddOperation(Opcode::OP_SHMEM_PUT_UB2GM, {src.GetStorage(), dst.data.GetStorage(), pred.GetStorage()}, {out}) :
         function.AddOperation(Opcode::OP_SHMEM_PUT, {pred.GetStorage(), src.GetStorage(), dst.data.GetStorage()}, {out});
+    if (src.GetValidShape().size() == 0) {
+        src.GetStorage()->UpdateDynValidShape(SymbolicScalar::FromConcrete(src.GetShape()));
+    }
+    MemoryType fromType = isUb2Gm ? MemoryType::MEM_UB : MemoryType::MEM_DEVICE_DDR;
+    auto copyOpAttribute = std::make_shared<CopyOpAttribute>(
+        fromType,
+        OpImmediate::Specified({0, 0}),
+        OpImmediate::Specified({src.GetShape()}),
+        OpImmediate::Specified({src.GetShape()}),
+        OpImmediate::Specified(src.GetValidShape()));
+    auto viewOp = *dst.data.GetStorage()->GetProducers().begin();
+    std::shared_ptr<ViewOpAttribute> viewOpAttr = std::dynamic_pointer_cast<ViewOpAttribute>(viewOp->GetOpAttribute());
+    copyOpAttribute->SetFromOffset(OpImmediate::Specified(viewOpAttr->GetFromDynOffset()));
+    op.SetOpAttribute(copyOpAttribute);
+    function.UpdateTensorDataUsage(op);
     ShmemPutAttr distOpAttr;
     distOpAttr.atomicType = putOp;
     distOpAttr.ownerRank = dstRank;
@@ -283,6 +298,18 @@ Tensor ShmemGet(const ShmemTensor& src, const SymbolicScalar& srcRank, const Ten
     auto out = std::make_shared<LogicalTensor>(function, targetDataType, shape, src.data.Format());
     auto &op = function.AddOperation(Opcode::OP_SHMEM_GET, {pred.GetStorage(), src.data.GetStorage()},
         {out});
+    if (src.data.GetValidShape().size() == 0) {
+        src.data.GetStorage()->UpdateDynValidShape(SymbolicScalar::FromConcrete(src.data.GetShape()));
+    }
+    op.SetOpAttribute(std::make_shared<CopyOpAttribute>(
+        MemoryType::MEM_DEVICE_DDR,
+        OpImmediate::Specified({0, 0}),
+        OpImmediate::Specified({src.data.GetShape(1), src.data.GetShape(2)}),
+        OpImmediate::Specified({src.data.GetShape(1), src.data.GetShape(2)}),
+        OpImmediate::Specified(std::vector<SymbolicScalar>{
+            src.data.GetValidShape()[1], src.data.GetValidShape()[2]})
+    ));
+    function.UpdateTensorDataUsage(op);
     ShmemGetAttr distOpAttr;
     distOpAttr.ownerRank = srcRank;
     op.SetAttr(OpAttributeKey::distOpAttr, distOpAttr);
@@ -304,13 +331,14 @@ Tensor ShmemLoad(const ShmemTensor& src, const SymbolicScalar& srcRank, const Te
     auto out = std::make_shared<LogicalTensor>(function, nonShmemDataType, shape);
     auto &op = function.AddOperation(Opcode::OP_SHMEM_GET_GM2UB, {pred.GetStorage(), src.data.GetStorage()},
         {out});
-    if (src.data.GetValidShape().size() != 0) {
-        op.SetOpAttribute(std::make_shared<CopyOpAttribute>(OpImmediate::Specified({0, 0}), MEM_UB,
-            OpImmediate::Specified({src.data.GetShape(1), src.data.GetShape(2)}),
-            OpImmediate::Specified({out->shape[0], out->shape[1]}),
-            OpImmediate::Specified(std::vector<SymbolicScalar>{src.data.GetValidShape()[1], src.data.GetValidShape()[2]})));
-        function.UpdateTensorDataUsage(op);
+    if (src.data.GetValidShape().size() == 0) {
+        src.data.GetStorage()->UpdateDynValidShape(SymbolicScalar::FromConcrete(src.data.GetShape()));
     }
+    op.SetOpAttribute(std::make_shared<CopyOpAttribute>(OpImmediate::Specified({0, 0}), MEM_UB,
+        OpImmediate::Specified({src.data.GetShape(1), src.data.GetShape(2)}),
+        OpImmediate::Specified({out->shape[0], out->shape[1]}),
+        OpImmediate::Specified(std::vector<SymbolicScalar>{src.data.GetValidShape()[1], src.data.GetValidShape()[2]})));
+    function.UpdateTensorDataUsage(op);
     ShmemGetAttr distOpAttr;
     distOpAttr.ownerRank = srcRank;
     op.SetAttr(OpAttributeKey::distOpAttr, distOpAttr);
@@ -423,17 +451,19 @@ void AllGather(const Tensor& predToken, const Tensor& in, ShmemTensor &shmemTens
     uint32_t worldSize = shmemTensor.worldSize;
     int32_t row = in.GetShape(0);
     int32_t col = in.GetShape(1);
+    SymbolicScalar validRow = in.GetValidShape()[0];
+    SymbolicScalar validCol = in.GetValidShape()[1];
     ValidateTensor(shmemTensor.data, "shmemTensor.data", {}, {in.GetDataType()}, {in.Format()}, {worldSize, row, col});
     ValidateTensor(out, "out", {}, {in.GetDataType()}, {in.Format()}, {row * worldSize, col});
     SymbolicScalar thisRank = GetHcclRankId(shmemTensor.group);
     for (uint32_t dynRankId = 0; dynRankId < worldSize; ++dynRankId) {
-        auto shmemDataTile = ShmemView(shmemTensor, {1, row, col}, std::vector<SymbolicScalar>{thisRank, 0, 0});
+        auto shmemDataTile = ShmemView(shmemTensor, {1, row, col}, std::vector<SymbolicScalar>{1, validRow, validCol}, std::vector<SymbolicScalar>{thisRank, 0, 0});
         auto shmemPutOut = ShmemPut(in, shmemDataTile, dynRankId, AtomicType::SET, predToken);
         auto shmemSignalOut = ShmemSignal(shmemDataTile, dynRankId, dynRankId, 1, AtomicType::SET, shmemPutOut);
         auto shmemDataLocal = ShmemView(shmemTensor, {1, row, col}, std::vector<SymbolicScalar>{dynRankId, 0, 0});
         auto waitUntilOut= ShmemWaitUntil(shmemDataLocal, thisRank, OpType::EQ, 1, true, shmemSignalOut);
         auto shmemGetOut= ShmemGet(shmemDataLocal, thisRank, waitUntilOut);
-        Assemble(shmemGetOut, {dynRankId * row, 0}, out);
+        Assemble(shmemGetOut, {dynRankId * validRow, 0}, out);
     }
 }
 
--- framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/pre_graph.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/pre_graph.cpp
@@ -65,7 +65,9 @@ Status PreGraphProcess::RunOnFunction(Function &function) {
     // Processing Special Ops
     SetCopyAttr setCopyAttr;
     for (auto &op : opList) {
-        if (IsCopyOut(op.GetOpcode()) && op.GetOpcode() != Opcode::OP_COPY_OUT) {
+        if (IsCopyOut(op.GetOpcode()) && op.GetOpcode() != Opcode::OP_COPY_OUT &&
+            op.GetOpcode() != Opcode::OP_SHMEM_GET && op.GetOpcode() != Opcode::OP_SHMEM_PUT &&
+            op.GetOpcode() != Opcode:: OP_SHMEM_PUT_UB2GM) {
             setCopyAttr.ProcessSpecialMTEOperation(op);
         }
         if (IsCopyIn(op.GetOpcode()) && op.GetOpcode() != Opcode::OP_COPY_IN && op.GetOpcode() != Opcode::OP_SHMEM_GET_GM2UB) {
