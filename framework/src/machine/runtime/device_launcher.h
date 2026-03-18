e85c5584e8342429ad9545142ad951a0b774f783
yang-zhipeng19 <yangzhipeng19@huawei.com>
2026-03-18 15:35:14 +0800

    feat(machine): Support triple stream schedule for c++ examples

--- framework/src/machine/runtime/device_launcher.h
+++ framework/src/machine/runtime/device_launcher.h
@@ -134,6 +134,7 @@ public:
         if (devConfig.aicpuNum == 0 || devConfig.aicpuNum > maxAicpuNum) {
             devConfig.aicpuNum = maxAicpuNum;
         }
+        devConfig.isTripleStream = config::GetRuntimeOption<bool>(CFG_TRIPLE_STREAM_SCHED);
     }
 
     template<typename DeviceMemoryTy>
@@ -154,15 +155,18 @@ public:
         return;
     }
 
-    static uint32_t GetAiCpuNumForDav3510(uint32_t aiCpuNum, uint32_t scheCpuNum) {
+    static uint32_t GetAiCpuNumForDav3510(uint32_t aiCpuNum, uint32_t scheCpuNum, DeviceLauncherConfig &config) {
         if (scheCpuNum == 1) {
-            return scheCpuNum + dynamic::MAX_OTHER_AICPU_NUM;
+            return config.isTripleStream ? scheCpuNum : scheCpuNum + dynamic::MAX_OTHER_AICPU_NUM;
         }
 
         uint32_t oneDieMinCpuNum = aiCpuNum >> 1;
         uint32_t oneDieMaxCpuNum = oneDieMinCpuNum + (aiCpuNum - (oneDieMinCpuNum << 1));
         uint32_t oneDieMinScheCpuNum = scheCpuNum >> 1;
         uint32_t lunchMinCpuNum = oneDieMaxCpuNum + oneDieMinScheCpuNum;
+        if (!config.isTripleStream) {
+            lunchMinCpuNum += dynamic::MAX_OTHER_AICPU_NUM;
+        }
 
         return lunchMinCpuNum < aiCpuNum ? lunchMinCpuNum : aiCpuNum;
     }
@@ -182,7 +186,7 @@ public:
         devProg->devArgs.maxAicpuNum = aiCpuNum;
         config.aicpuNum = devProg->devArgs.scheCpuNum + dynamic::MAX_OTHER_AICPU_NUM;
         if (devProg->devArgs.archInfo == ArchInfo::DAV_3510) {
-            devProg->devArgs.nrAicpu = GetAiCpuNumForDav3510(static_cast<uint32_t>(aiCpuNum), devProg->devArgs.scheCpuNum);
+            devProg->devArgs.nrAicpu = GetAiCpuNumForDav3510(static_cast<uint32_t>(aiCpuNum), devProg->devArgs.scheCpuNum, config);
         } else {
             devProg->devArgs.nrAicpu = config.aicpuNum;
         }
