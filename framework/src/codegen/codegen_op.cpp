b4dad44b9c7a2cb40534631e7f51d03228b2854f
luoxinhui666 <luoxinhui2@huawei.com>
2026-03-18 11:17:06 +0800

    feat(distributed): DynValidShape and dynOffset for shmem api

--- framework/src/codegen/codegen_op.cpp
+++ framework/src/codegen/codegen_op.cpp
@@ -151,7 +151,8 @@ void CodeGenOp::UpdateOffsetForInput(const Operation& oper, const LogicalTensor&
     static const std::set<Opcode> cubeMDLOpCode = {
         Opcode::OP_L1_TO_L0A,   Opcode::OP_L1_TO_L0B,       Opcode::OP_L1_TO_L0_AT,
         Opcode::OP_L1_TO_L0_BT, Opcode::OP_L1_TO_BT,        Opcode::OP_L1_TO_FIX_QUANT_PRE,
-        Opcode::OP_L0C_TO_L1,   Opcode::OP_L1_TO_L0A_SCALE, Opcode::OP_L1_TO_L0B_SCALE};
+        Opcode::OP_L0C_TO_L1,   Opcode::OP_L1_TO_L0A_SCALE, Opcode::OP_L1_TO_L0B_SCALE,
+        Opcode::OP_SHMEM_PUT,   Opcode::OP_SHMEM_PUT_UB2GM};
     bool cubeMDLCondition = cubeMDLOpCode.count(opCode);
     bool useAttrShapeOffsetForInputGM =
         OpcodeManager::Inst().IsCopyIn(opCode) && logicalTensor.GetMemoryTypeOriginal() == MEM_DEVICE_DDR;
