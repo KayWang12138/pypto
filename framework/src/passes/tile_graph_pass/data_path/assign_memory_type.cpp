ae0ba9628da9d016e6c3bbe40f011c6a6643729c
zhaosenqiu <zhaosenqiu@huawei.com>
2026-03-18 16:19:42 +0800

    fix(pass): Assign review comments.

--- framework/src/passes/tile_graph_pass/data_path/assign_memory_type.cpp
+++ framework/src/passes/tile_graph_pass/data_path/assign_memory_type.cpp
@@ -485,10 +485,12 @@ void AssignMemoryType::AssignMoveOpForView(Operation &operation) {
     for (size_t i = 0; i < operation.iOperand.size(); ++i) {
         auto &tensor = operation.iOperand[i];
         MemoryType toType = operation.oOperand.front()->GetMemoryTypeOriginal();
-        // 当view的offset不对齐，需要插DDR时，不能将view前tensor的MemoryTypeOriginal向view后的tensor传递
-        // 避免插DDR后出现original—>DDR->original而DDR->original不存在通路的场景（如original为L0C时）
-        if(!unaligned && toType == MemoryType::MEM_UNKNOWN && tensor->GetMemoryTypeOriginal() != MemoryType::MEM_UNKNOWN
-            && tensor->GetMemoryTypeOriginal() != MemoryType::MEM_L0C) {
+        // 尝试在view的输出original未知而输入original已知时进行内存复用，将输出的original刷为与输入相同
+        // 当出现内存未对齐时，需要插搬运到DDR则不进行复用；当出现输入为L0C时，L0C到L0C无意义，也不进行复用，同时避免出现DDR->L0C
+        auto originalMemType = tensor->GetMemoryTypeOriginal();
+        auto canReuseMemoryType = !unaligned && toType == MemoryType::MEM_UNKNOWN && 
+            originalMemType != MemoryType::MEM_UNKNOWN && originalMemType != MemoryType::MEM_L0C;
+        if(canReuseMemoryType) {
             //view输出的消费者是assemble或者reshape
             operation.oOperand.front()->SetMemoryTypeOriginal(tensor->GetMemoryTypeOriginal());
             viewOpAttribute->SetToType(tensor->GetMemoryTypeOriginal());
