591e610184c04c0250d2c37148e75d3d228143fb
luoxinhui666 <luoxinhui2@huawei.com>
2026-03-18 14:21:25 +0800

    fix(distributed) Fix tips for wait until tiles

--- framework/src/interface/operation/distributed/shmem_expand_function.cpp
+++ framework/src/interface/operation/distributed/shmem_expand_function.cpp
@@ -294,7 +294,7 @@ void TiledShmemWaitUntil(Function& function, const TileShape& tileShape,
 
         auto& tileOp = function.AddOperation(Opcode::OP_SHMEM_WAIT_UNTIL, {predTokenTile, shmemSignalTile}, {outTile});
         tileNumOfWaitUntil++;
-        ASSERT(tileNumOfWaitUntil <= MAX_TILE_NUM) <<
+        ASSERT(tileNumOfWaitUntil < MAX_TILE_NUM || tileNumOfWaitUntil == MAX_TILE_NUM) <<
             "WaitUntil tile count exceeds the maximum allowed value: " << MAX_TILE_NUM;
         ShmemWaitUntilAttr distOpAttr;
         op.GetAttr(OpAttributeKey::distOpAttr, distOpAttr);
