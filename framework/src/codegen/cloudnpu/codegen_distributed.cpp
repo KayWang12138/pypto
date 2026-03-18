76f7c8998a917bf8e62bd535ef55de4ade99207f
luoxinhui666 <luoxinhui2@huawei.com>
2026-03-18 11:17:06 +0800

    feat(distributed): DynValidShape and dynOffset for shmem api

--- framework/src/codegen/cloudnpu/codegen_distributed.cpp
+++ framework/src/codegen/cloudnpu/codegen_distributed.cpp
@@ -226,23 +226,69 @@ std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapes(int32_t operandIndex, int3
     return GenOffsets(operandIndex, dim) + ", " + GenRawShapes(operandIndex, dim);
 }
 
-std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapesForShmemPut() const {
+RawSymbolicScalarPtr FormatSymbol(const RawSymbolicScalarPtr &rawScalar)
+{
+    if (rawScalar->IsSymbol()) {
+        if (std::islower(static_cast<unsigned char>(rawScalar->Dump().front()))) {
+            SymbolicScalar updateIndexScalar("RUNTIME_GetSymbol(INDEX_" + rawScalar->Dump() + ")");
+            auto formatedScalar = updateIndexScalar.Raw();
+            return formatedScalar;
+        }
+        return rawScalar;
+    } else if (rawScalar->IsExpression()) {
+        auto rawExpresssion = std::dynamic_pointer_cast<RawSymbolicExpression>(rawScalar);
+        std::vector<RawSymbolicScalarPtr> newOperands;
+        newOperands.reserve(rawExpresssion->OperandList().size());
+        for (const auto &operand : rawExpresssion->OperandList()) {
+            newOperands.push_back(FormatSymbol(operand));
+        }
+        auto formatedExpression = std::make_shared<RawSymbolicExpression>(rawExpresssion->GetExpressionOpcode(), newOperands);
+        return formatedExpression;
+    }
+    return rawScalar;
+}
+
+std::string CodeGenOpCloudNPU::GenDynOffset(int32_t operandIndex, int32_t dim) const
+{
+    std::ostringstream oss;
+    for (int32_t index = 0; index < dim; ++index) {
+        auto offsetScalar = offsetFromAttr[operandIndex][index];
+        if (offsetScalar.IsValid()) {
+            RawSymbolicScalarPtr rawScalar = offsetScalar.Raw();
+            rawScalar = FormatSymbol(rawScalar);
+            oss << rawScalar->Dump();
+        } else {
+            oss << offsetScalar.Dump();
+        }
+        if (index != dim - 1) {
+            oss << ", ";
+        }
+    }
+    return oss.str();
+}
+
+std::string CodeGenOpCloudNPU::GenDynValidShape(int32_t operandIndex, int32_t dim) const
+{
+    std::ostringstream oss;
+    for (int32_t index = 0; index < dim; ++index) {
+        oss << dynamicValidShape[operandIndex][index];
+        if (index != dim - 1) {
+            oss << ", ";
+        }
+    }
+    return oss.str();
+}
+
+std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapesForShmemPut() const
+{
     std::ostringstream oss;
     int32_t nonShmemDataIndex = 3;
     int32_t shmemDataIndex = 4;
     int32_t nonShmemDataDim = originShape[nonShmemDataIndex].size();
     int32_t shmemDataDim = 4;
-    std::string viewOffsetStr = dynamicValidShape[shmemDataIndex][2].Dump();
-    size_t firstComma = viewOffsetStr.find(",");
-    size_t lastComma = viewOffsetStr.rfind(",");
-    std::string viewOffset = viewOffsetStr.substr(firstComma + 1, lastComma - firstComma - 1);
-    if (viewOffset.find("RUNTIME_GetTensorDataInt32Dim2") != std::string::npos) {
-        oss << ", " << GenOffsetsAndRawShapes(nonShmemDataIndex, nonShmemDataDim) << ", "
-            << GenOffsetsAndRawShapes(shmemDataIndex, shmemDataDim) << ", " << viewOffset;
-    } else {
-        oss << ", " << GenOffsetsAndRawShapes(nonShmemDataIndex, nonShmemDataDim) << ", "
-            << GenOffsetsAndRawShapes(shmemDataIndex, shmemDataDim) << ", " << -1;
-    }
+    int32_t validShapeIndex = 0;
+    oss << ", " << GenOffsetsAndRawShapes(nonShmemDataIndex, nonShmemDataDim) << ", " << GenDynValidShape(validShapeIndex, nonShmemDataDim) <<
+        ", " << GenDynOffset(shmemDataIndex, shmemDataDim) << ", " << GenRawShapes(shmemDataIndex, shmemDataDim);
     return oss.str();
 }
 
@@ -252,8 +298,8 @@ std::string CodeGenOpCloudNPU::GenOffsetsAndRawShapesForShmemGet() const {
     int32_t shmemDataIndex = 3;
     int32_t nonShmemDataDim = originShape[nonShmemDataIndex].size();
     int32_t shmemDataDim = 4;
-    oss << ", " << GenOffsetsAndRawShapes(nonShmemDataIndex, nonShmemDataDim) << ", "
-        << GenOffsetsAndRawShapes(shmemDataIndex, shmemDataDim);
+    oss << ", " << GenDynOffset(nonShmemDataIndex, nonShmemDataDim) << ", " << GenRawShapes(nonShmemDataIndex, nonShmemDataDim) <<
+        ", " << GenOffsetsAndRawShapes(shmemDataIndex, shmemDataDim) << ", " << GenDynValidShape(nonShmemDataIndex, nonShmemDataDim);
     return oss.str();
 }
 
