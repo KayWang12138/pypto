159b95cac7b5e16fa2e35ee06999aea983fc660a
wangqun23 <wangqun23@huawei.com>
2026-03-19 12:05:50 +0800

    fix(pass) : Add CopyOpAttribute for indexOutCastOp When inferring the validshape of indexOutCastOp

--- framework/src/interface/operation/op_infer_shape_impl.cpp
+++ framework/src/interface/operation/op_infer_shape_impl.cpp
@@ -219,6 +219,11 @@ void IndexOutCastInferFunc(Operation* op,
     for (auto output : op->GetOOperands()) {
         outValidShapes.push_back(outValidShape);
     }
+    
+    auto input = op->GetIOperands()[2];
+    auto indexOutCastOpAttribute = std::dynamic_pointer_cast<CopyOpAttribute>(op->GetOpAttribute());
+    indexOutCastOpAttribute->SetFromDynValidShape(OpImmediate::Specified(input->GetDynValidShape()));
+    op->SetOpAttribute(indexOutCastOpAttribute);
 }
 REGISTER_INFER_SHAPE_FUNC(OP_INDEX_OUTCAST, Opcode::OP_INDEX_OUTCAST, IndexOutCastInferFunc);
 
