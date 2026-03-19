a369503ec1a792f6a733794e5d34b1e1d2b99d01
HuangYuqian <huangyuqian2@huawei.com>
2026-03-19 19:24:12 +0800

    convert torch tensor optimize

--- python/src/bindings/torch_tensor_converter.cpp
+++ python/src/bindings/torch_tensor_converter.cpp
@@ -100,7 +100,8 @@ int TorchTensorConverter::Convert(py::sequence &tensors, py::sequence &tensor_de
 
     py::object toDlpack = GetTorchToDlpack();
     py::object device = py::none();
-
+    
+    py::module torch_npu;
     for (size_t i = 0; i < n; i++) {
         py::object torchTensor = tensors[py::int_(i)];
         py::object tensorDef = tensor_defs[py::int_(i)];
@@ -118,7 +119,9 @@ int TorchTensorConverter::Convert(py::sequence &tensors, py::sequence &tensor_de
         } else {
             std::string device_type = py::cast<std::string>(tensorDevice.attr("type"));
             if (device_type == "npu") {
-                py::module torch_npu = py::module::import("torch_npu");
+                if (torch_npu.ptr() == nullptr) {
+                    torch_npu = py::module::import("torch_npu");
+                }
                 int npu_format = py::cast<int>(torch_npu.attr("get_npu_format")(torchTensor));
                 if (npu_format == 29) {
                     format = TileOpFormat::TILEOP_NZ;
