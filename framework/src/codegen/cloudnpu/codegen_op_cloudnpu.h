bc84ca43bc3467cf781372991c99425fc65be6cb
lijunze <lijunze3@huawei.com>
2026-03-20 16:28:24 +0800

    fix codegen

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -378,8 +378,8 @@ private:
     std::string PrintBinaryBrcDynamicUnaligned(const PrintBinaryBrcParam &param) const;
     std::string PrintBinaryBrc(const PrintBinaryBrcParam &param) const;
 
-    std::string PrintPermute(const PrintPermuteParam &param) const;
-    std::string PrintpermuteLayout(const PrintPermuteParam &param) const;
+    std::string PrintPermute(const PrintUnaryTmpBuffParam &param) const;
+    std::string PrintpermuteLayout(const PrintUnaryTmpBuffParam &param) const;
 
     std::string PrintTransposeDataMove(const PrintTransposeDataMoveParam &param) const;
     std::string PrintTransposeDataMoveLayout(const PrintTransposeDataMoveParam &param) const;
