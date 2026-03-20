3201642283b3a7dff648c4c3fd8caed2958e5187
HuangYuqian <huangyuqian2@huawei.com>
2026-03-20 14:36:06 +0800

    parallel loop

--- python/tests/ut/interface/test_pto_loop.py
+++ python/tests/ut/interface/test_pto_loop.py
@@ -25,50 +25,24 @@ def init_tensors():
     return a, b, c
 
 
-def gen_add_golden(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-    out = torch.zeros_like(a, dtype=torch.float32)
-    
-    # 三层循环
-    for i in range(a.shape[0]):    # dim 0: 2
-        for j in range(a.shape[1]):# dim 1: 4
-            for k in range(a.shape[2]): # dim 2:4
-                out[i, j, k] = a[i, j, k] + b[i, j, k]
-    return out
-
-
-def parallel_add_compute_single_parallel(left: pypto.Tensor, right: pypto.Tensor, res: pypto.Tensor):
-    n0 = left.shape[0]
-    n1 = left.shape[1]
-    n2 = left.shape[2]
-    for n0_idx in pypto.loop(0, 2, 1, name="N0_LOOP", idx_name="n0_loop"):
-        for n1_idx in pypto.loop(0, 2, 1, name="N1_LOOP", idx_name="n1_loop", parallel_for=True):
-            for n2_idx in pypto.loop(0, 2, 1, name="N2_LOOP", idx_name="n2_loop"):
-                pypto.set_vec_tile_shapes(1, 1, 2)
-                left_view = pypto.view(left, [1, 2, 2], [n0_idx, n1_idx * 2, n2_idx * 2])
-                right_view = pypto.view(right, [1, 2, 2], [n0_idx, n1_idx * 2, n2_idx * 2])
-                output = left_view + right_view
-                pypto.assemble(output, [n0_idx, n1_idx * 2, n2_idx * 2], res)
-
-
 def test_parallel_add_single_parallel():
-    a = torch.rand((2, 4, 4), dtype=torch.float32) * 2 - 1  # [-1, 1]
-    b = torch.rand((2, 4, 4), dtype=torch.float32) * 2 - 1  # [-1, 1]
-
-    res_golden = gen_add_golden(a, b)
-
-    res = x = torch.empty((2, 4, 4), dtype=torch.float32).uniform_(-1, 1).npu()
-
-    @pypto.frontend.jit
-    def parallel_add(
-        left: pypto.Tensor([pypto.STATIC, pypto.STATIC, pypto.STATIC], pypto.DT_FP32),
-        right: pypto.Tensor([pypto.STATIC, pypto.STATIC, pypto.STATIC], pypto.DT_FP32),
-        res: pypto.Tensor([pypto.STATIC, pypto.STATIC, pypto.STATIC], pypto.DT_FP32),
-    ):
-        parallel_add_compute_single_parallel(left, right, res)
-    a_npu = a.npu()
-    b_npu = b.npu()
-    parallel_add(a_npu, b_npu, res)
-    assert torch.allclose(res.cpu(), res_golden, atol=0.000025, rtol=0.005)
+    dtype = pypto.DT_FP32
+    shape = (2, 4, 4)
+    left = pypto.tensor(shape, dtype, "left")
+    right = pypto.tensor(shape, dtype, "right")
+    res = pypto.tensor(shape, dtype, "res")
+    with pypto.function("MAIN", left, right, res):
+        n0 = left.shape[0]
+        n1 = left.shape[1]
+        n2 = left.shape[2]
+        for n0_idx in pypto.loop(0, 2, 1, name="N0_LOOP", idx_name="n0_loop"):
+            for n1_idx in pypto.loop(0, 2, 1, name="N1_LOOP", idx_name="n1_loop", parallel_for=True):
+                for n2_idx in pypto.loop(0, 2, 1, name="N2_LOOP", idx_name="n2_loop"):
+                    pypto.set_vec_tile_shapes(1, 1, 2)
+                    left_view = pypto.view(left, [1, 2, 2], [n0_idx, n1_idx * 2, n2_idx * 2])
+                    right_view = pypto.view(right, [1, 2, 2], [n0_idx, n1_idx * 2, n2_idx * 2])
+                    output = left_view + right_view
+                    pypto.assemble(output, [n0_idx, n1_idx * 2, n2_idx * 2], res)
 
 
 def test_pto_loop_end_only():
