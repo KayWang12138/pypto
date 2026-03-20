7968955cd425fc4fb8d6a3e34ab64b3ecddddff2
wkkk0528 <wangkai578@huawei.com>
2026-03-20 11:42:41 +0800

    fix(operation): Fix pad with tfillpad

--- framework/src/codegen/utils/codegen_utils.cpp
+++ framework/src/codegen/utils/codegen_utils.cpp
@@ -37,22 +37,23 @@ std::vector<int64_t> NormalizeShape(const std::vector<int64_t> &shapeVec, unsign
 }
 
 std::string FormatFloat(const std::variant<int64_t, uint64_t, double> &v, DataType dtype, int precision) {
-    // 定义处理函数
     auto apply = [&](auto &&val) -> std::string {
         std::ostringstream oss;
         using T = std::decay_t<decltype(val)>;
         if constexpr (std::is_same_v<T, double>) {
-            if ((dtype == DataType::DT_FP32 || dtype == DataType::DT_FP16 || dtype == DataType::DT_BF16) &&
-                (std::isinf(val) || std::isnan(val))) {
-                FloatSpecVal fsv = {dtype, val};
-                oss << fsv.GetFsVarName() << ".f";
+            if (dtype == DataType::DT_FP32 || dtype == DataType::DT_FP16 || dtype == DataType::DT_BF16) {
+                if (std::isinf(val) || std::isnan(val)) {
+                    FloatSpecVal fsv = {dtype, val};
+                    oss << fsv.GetFsVarName() << ".f";
+                } else {
+                    oss << std::setprecision(precision) << val << ".f";
+                }
                 return oss.str();
             }
         }
         oss << std::setprecision(precision) << val;
         return oss.str();
     };
-
     return std::visit(apply, v);
 }
 
