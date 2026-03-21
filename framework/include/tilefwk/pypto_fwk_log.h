fe4e7e7a57564cceb4f23ea8c2596cad161b7a72
zhoumin_ <zhoumin107@huawei.com>
2026-03-21 16:14:54 +0800

    warn

--- framework/include/tilefwk/pypto_fwk_log.h
+++ framework/include/tilefwk/pypto_fwk_log.h
@@ -42,8 +42,9 @@ enum class LogModule {
     VERIFY,
     COMPILER_MONITOR,
     PLATFORM,
-    BOTTOM,
-    MATMUL
+    CONV,
+    MATMUL,
+    BOTTOM
 };
 
 class LogFuncInfo {
@@ -209,4 +210,6 @@ private:
 #define MATMUL_LOGW(...) PYPTO_HOST_LOG(DLOG_WARN,  MATMUL, __VA_ARGS__)
 #define MATMUL_LOGE(...) PYPTO_HOST_LOG(DLOG_ERROR, MATMUL, __VA_ARGS__)
 #define MATMUL_LOGE_E(errCode, fmt, ...) PYPTO_HOST_LOGE(MATMUL, errCode, fmt, ##__VA_ARGS__)
+
+#define CONV_LOGW(...) PYPTO_HOST_LOG(DLOG_WARN,  CONV, __VA_ARGS__)
 #endif
