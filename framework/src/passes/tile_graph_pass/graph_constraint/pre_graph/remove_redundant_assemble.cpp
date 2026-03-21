90a41636c9deef64b71df2fd1ab7fe9078804fa5
2501_92924845 <niulinkai@huawei.com>
2026-03-21 14:59:58 +0800

    fix code space

--- framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
@@ -50,8 +50,8 @@ void UpdateCopyOutAttr(Operation &op, Operation &opNext) {
 bool CalculateNewRawShapeExpand(
     const std::vector<int64_t> &newShape, const std::vector<int64_t> &oriRawShape, std::vector<int64_t> &newRawShape) {
     newRawShape.resize(newShape.size());
-    if (oriRawShape.size() < newShape.size()) { 
-         return false; 
+    if (oriRawShape.size() < newShape.size()) {
+        return false;
     }
     size_t diff = oriRawShape.size() - newShape.size();
     std::copy(oriRawShape.begin() + diff, oriRawShape.end(), newRawShape.begin());
