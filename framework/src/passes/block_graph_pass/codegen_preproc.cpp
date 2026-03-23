a1ddfe38721f13b7eb8d67c64c7d0f3eb476eb5b
xiefangming <xiefangming@huawei.com>
2026-03-23 10:53:41 +0800

    feat(operation): Add multi-axis expand support

--- framework/src/passes/block_graph_pass/codegen_preproc.cpp
+++ framework/src/passes/block_graph_pass/codegen_preproc.cpp
@@ -196,10 +196,20 @@ bool ReduceNeedCombineAxis(const Operation& op)
 
 void CodegenPreproc::FixExpandDimForAxisCombine(Operation& op, int dimSize) const
 {
+    // TODO: 多轴扩展时，合轴的场景还不清晰，
+    // 例：[1, 1, 1] & [m, n, 1] 换轴逻辑
+    // 这个分支判定的逻辑是什么（期望是尾轴都为1且相等）
     if (op.GetOpcode() == Opcode::OP_EXPAND) {
-        int axis = op.GetIntAttribute(OP_ATTR_PREFIX + "EXPANDDIM");
-        if (axis == dimSize - NUM2) {
-            op.SetAttribute(OP_ATTR_PREFIX + "EXPANDDIM", axis + 1);
+        auto axes = op.GetVectorIntAttribute(OP_ATTR_PREFIX + "EXPANDDIMS");
+        bool updated = false;
+        for (auto &axis : axes) {
+            if (axis == dimSize - NUM2) {
+                axis = axis + 1;
+                updated = true;
+            }
+        }
+        if (updated) {
+            op.SetAttribute(OP_ATTR_PREFIX + "EXPANDDIMS", axes);
         }
     }
 }
--- framework/src/passes/tile_graph_pass/graph_constraint/pad_local_buffer.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pad_local_buffer.cpp
@@ -322,10 +322,13 @@ void PadLocalBuffer::PadVector(
 
 bool PadLocalBuffer::IsExpandLastDim(const Operation& op)
 {
-    int axis = op.GetIntAttribute(OP_ATTR_PREFIX + "EXPANDDIM");
-    if (axis == static_cast<int>(op.GetOOperands()[0]->shape.size() - 1)) {
-        return true;
-    }
+    // TODO: 尾轴扩展场景
+    auto axes = op.GetVectorIntAttribute(OP_ATTR_PREFIX + "EXPANDDIMS");
+    int lastDim = static_cast<int>(op.GetOOperands()[0]->shape.size() - 1);
+    for (auto axis : axes) {
+        if (axis == lastDim) {
+            return true;
+        }
     return false;
 }
 
