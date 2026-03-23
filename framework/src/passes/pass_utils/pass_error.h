a768d5ef1c578811b0b67be2839c7b7b1607cdf2
xujiayi1999 <xujiayi28@huawei.com>
2026-03-23 19:53:44 +0800

    fix(Pass): Pass trouble shooting

--- framework/src/passes/pass_utils/pass_error.h
+++ framework/src/passes/pass_utils/pass_error.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -18,65 +18,62 @@
 #include <cstdint>
 
 namespace npu::tile_fwk {
-enum class PassError : uint32_t {
-    PUBLIC_ERROR_TENSOR    = 40000U, // 前端传入公共Tensor错误
-    PUBLIC_ERROR_OPERATION = 41000U, // 前端传入公共Operation错误
-    PUBLIC_ERROR_FUNCTION  = 42000U, // 前端传入公共Function错误
-    PUBLIC_ERROR_GRAPH     = 43000U, // 前端传入公共Graph错误
-    PUBLIC_ERROR_CONFIG    = 44000U, // 前端传入公共Config错误
-    PUBLIC_ERROR_MANAGER   = 45000U, // 前端传入公共Manager错误
-    UNKNOWN                = 49999U  // 未知错误
+
+// 前端传入Tensor错误
+enum class TensorErr : uint32_t {
+    TENSOR_NULL_POINTER         = 40000U,
+    TENSOR_INVALID_MEMORY_TYPE  = 40001U,
+    TENSOR_SUBGRAPH_BOUNDARY    = 40002U,
+    TENSOR_SHAPE_MISMATCH       = 40003U,
+    TENSOR_UNSUPPORTED_DATATYPE = 40004U,
+    TENSOR_MEMORY_ALLOCATION    = 40005U,
+    TENSOR_DYNAMIC_ATTR         = 40006U,
+    TENSOR_MEMORY_CORRUPTION    = 40007U
 };
 
-enum class PublicTensorErr : uint32_t {
-    TENSOR_NULL_POINTER         = 40001U,
-    TENSOR_INVALID_MEMORY_TYPE  = 40002U,
-    TENSOR_SUBGRAPH_BOUNDARY    = 40003U,
-    TENSOR_SHAPE_MISMATCH       = 40004U,
-    TENSOR_UNSUPPORTED_DATATYPE = 40005U,
-    TENSOR_MEMORY_ALLOCATION    = 40006U,
-    TENSOR_DYNAMIC_ATTR         = 40007U,
-    TENSOR_MEMORY_CORRUPTION    = 40008U
+// 前端传入Operation错误
+enum class OperationErr : uint32_t {
+    OP_INVALID_OPERAND_COUNT = 41000U,
+    OP_NULL_POINTER          = 41001U,
+    OP_INVALID_OPCODE        = 41002U,
+    OP_PRODUCER_CONSUMER     = 41003U,
+    OP_SPECIAL_CONSTRAINT    = 41004U,
+    OP_NESTING_DEPTH         = 41005U,
+    OP_SEQUENCE_ERROR        = 41006U
 };
 
-enum class PublicOperationErr : uint32_t {
-    OP_INVALID_OPERAND_COUNT = 41001U,
-    OP_NULL_POINTER          = 41002U,
-    OP_INVALID_OPCODE        = 41003U,
-    OP_PRODUCER_CONSUMER     = 41004U,
-    OP_SPECIAL_CONSTRAINT    = 41005U,
-    OP_NESTING_DEPTH         = 41006U,
-    OP_SEQUENCE_ERROR        = 41007U
+// 前端传入Function错误
+enum class FunctionErr : uint32_t {
+    FUNCTION_GRAPH_STRUCTURE       = 42000U,
+    FUNCTION_BOUNDARY_COMPLETENESS = 42001U,
+    FUNCTION_GRAPH_CONNECTION      = 42002U,
+    FUNCTION_EXPAND_FEATURE        = 42003U,
+    FUNCTION_MEMORY_REACHABILITY   = 42004U,
+    FUNCTION_UNIQUENESS            = 42005U,
+    FUNCTION_SPECIAL_STRUCTURE     = 42006U
 };
 
-enum class PublicFunctionErr : uint32_t {
-    FUNCTION_GRAPH_STRUCTURE       = 42001U,
-    FUNCTION_BOUNDARY_COMPLETENESS = 42002U,
-    FUNCTION_GRAPH_CONNECTION      = 42003U,
-    FUNCTION_EXPAND_FEATURE        = 42004U,
-    FUNCTION_MEMORY_REACHABILITY   = 42005U,
-    FUNCTION_UNIQUENESS            = 42006U,
-    FUNCTION_SPECIAL_STRUCTURE     = 42007U
+// 前端传入Graph错误
+enum class GraphErr : uint32_t {
+    GRAPH_LOOP_DETECTION       = 43000U,
+    GRAPH_TOPOLOGY_STRUCTURE   = 43001U,
+    GRAPH_SUBGRAPH_EMPTY       = 43002U,
+    GRAPH_SUBGRAPH_ID_INVALID  = 43003U,
+    GRAPH_EDGE_CONSISTENCY     = 43004U,
+    GRAPH_COLOR_CONSISTENCY    = 43005U,
+    GRAPH_READY_STATE          = 43006U,
+    GRAPH_AIV_AIC_MIX          = 43007U
 };
 
-enum class PublicGraphErr : uint32_t {
-    GRAPH_LOOP_DETECTION       = 43001U,
-    GRAPH_TOPOLOGY_STRUCTURE   = 43002U,
-    GRAPH_SUBGRAPH_EMPTY       = 43003U,
-    GRAPH_SUBGRAPH_ID_INVALID  = 43004U,
-    GRAPH_EDGE_CONSISTENCY     = 43005U,
-    GRAPH_COLOR_CONSISTENCY    = 43006U,
-    GRAPH_READY_STATE          = 43007U,
-    GRAPH_AIV_AIC_MIX          = 43009U
+// 前端传入Config错误
+enum class ConfigErr : uint32_t {
+    CONFIG_MEMORY_TYPE_REACHABLE  = 44000U,
+    CONFIG_SUBGRAPH_BOUNDARY      = 44001U,
+    CONFIG_TENSOR_MEMORY_TYPE     = 44002U
 };
 
-enum class PublicConfigErr : uint32_t {
-    CONFIG_MEMORY_TYPE_REACHABLE  = 44001U,
-    CONFIG_SUBGRAPH_BOUNDARY      = 44002U,
-    CONFIG_TENSOR_MEMORY_TYPE     = 44003U
-};
-
-enum class PublicManagerErr : uint32_t {
+// 前端传入Manager错误
+enum class ManagerErr : uint32_t {
 };
 
 }  // namespace npu::tile_fwk
