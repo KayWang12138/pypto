4d83352224f08ed023493ff6bb2317a9561350c6
HuangYuqian <huangyuqian2@huawei.com>
2026-03-23 19:19:37 +0800

    parallel loop

--- framework/src/machine/host/backend.cpp
+++ framework/src/machine/host/backend.cpp
@@ -481,6 +481,7 @@ static void BuildControlFlow(FunctionCache &cache, Linker &linker, const std::st
         controlFlowOss << std::setw(indent * TABSIZE) << ' ' << "// hash=" << func->GetFunctionHash() << "\n";
         auto attr = func->GetDynloopAttribute();
         ASSERT(attr != nullptr)<<"attr is nullptr!";
+        (void)GetFunctionParallelMode(func);
         if (attr->submitBeforeLoop) {
             controlFlowOss << std::setw(indent * TABSIZE) << ' ' << "RUNTIME_RootStitch(RUNTIME_FUNCKEY_LOOP_BARRIER); // force submit before LOOP \n";
         }
