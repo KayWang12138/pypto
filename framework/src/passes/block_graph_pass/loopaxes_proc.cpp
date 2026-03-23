dbf2069a7559a017175669fe502b8fcb8947ce45
xiefangming <xiefangming@huawei.com>
2026-03-23 10:53:41 +0800

    feat(operation): Add multi-axis expand support

--- framework/src/passes/block_graph_pass/loopaxes_proc.cpp
+++ framework/src/passes/block_graph_pass/loopaxes_proc.cpp
@@ -74,16 +74,20 @@ bool NeedClearStatus(const Operation& op)
 
     //  Opcode::OP_EXPAND only support last axis or second last axis in for-loop
     if (opCode == Opcode::OP_EXPAND) {
-        std::string axisKey = OP_ATTR_PREFIX + "EXPANDDIM";
-        ASSERT(op.HasAttr(axisKey)) << "attr " << axisKey << "not found";
-        int64_t expandAxis = op.GetIntAttribute(axisKey);
+        std::string axisKey = OP_ATTR_PREFIX + "EXPANDDIMS";
+        ASSERT(op.HasAttr(axisKey)) << "attr " << axisKey << " not found";
+        auto expandAxes = op.GetVectorIntAttribute(axisKey);
         int shapeSize = static_cast<int>(op.GetOOperands().front()->GetDynValidShape().size());
-        expandAxis += SHAPE_DIM4 - shapeSize;
-        if (expandAxis == 0 || expandAxis == 1) {
-            APASS_LOG_DEBUG_F(
-                Elements::Operation, "%d %s expand axis 0/1", op.GetOpMagic(), op.GetOpcodeStr().c_str());
-            return true;
+
+        for (auto expandAxis : expandAxes) {
+            expandAxis += SHAPE_DIM4 - shapeSize;
+            if (expandAxis == 0 || expandAxis == 1) {
+                APASS_LOG_DEBUG_F(
+                    Elements::Operation, "%d %s expand axis 0/1", op.GetOpMagic(), op.GetOpcodeStr().c_str());
+                return true;
+            }
         }
+        return false;
     }
 
     return false;
