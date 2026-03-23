ab85a49ae22d0d1d25a01dcdf5fda13e49d470d7
xiefangming <xiefangming@huawei.com>
2026-03-23 11:47:24 +0800

    codegen_alteration

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -392,8 +392,8 @@ private:
     std::string PrintIndexOutCastDynamic(const PrintIndexOutCastParam &param) const;
     std::string PrintIndexOutCastDynamicUnaligned(const PrintIndexOutCastParam &param) const;
 
-    std::string PrintExpandDynamicUnaligned(const PrintUnaryParam &param, int expandAxis) const;
-    std::string PrintExpandLayout(int expandAxis) const;
+    std::string PrintExpandDynamicUnaligned(const PrintUnaryParam &param, std::vector<int> expandAxes) const;
+    std::string PrintExpandLayout(std::vector<int> expandAxes) const;
     std::string PrintExpand(const std::string &s0Var, const std::string &dVar, const std::string &srcDtypeStr,
         const std::string &dstDtypeStr) const;
     std::string PrintOneHot(const PrintUnaryParam &param) const;
