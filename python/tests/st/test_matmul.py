5be4a5337615ae52e112d5216ba35d00e5a4f6a2
chen-yongkang16 <chenyongkang16@huawei.com>
2026-03-24 16:37:34 +0800

    fix(operation): Fix the online ST for Matmul.

--- python/tests/st/test_matmul.py
+++ python/tests/st/test_matmul.py
@@ -101,7 +101,7 @@ def matmul_kernel_with_mn_split(
     m_view = shape_info.view_shape[0]
     n_view = shape_info.view_shape[1]
     pypto.set_cube_tile_shapes(shape_info.m_tile_shape, shape_info.k_tile_shape, shape_info.n_tile_shape,
-                                enable_multi_data_load=shape_info.mdl_flag, enable_split_k=shape_info.gm_acc)
+                                enable_split_k=shape_info.gm_acc)
     m_loop = (m + m_view - 1) // m_view
     n_loop = (n + n_view - 1) // n_view
     for m_idx in pypto.loop(0, m_loop, 1, name="LOOP_L0_mIdx", idx_name="m_idx"):
@@ -129,14 +129,12 @@ def bmm_kernel_with_no_mn_split(
     shape_info: ShapeConfig,
 ):
     pypto.set_cube_tile_shapes(shape_info.m_tile_shape, shape_info.k_tile_shape, shape_info.n_tile_shape, 
-                               enable_multi_data_load=shape_info.mdl_flag, enable_split_k=shape_info.gm_acc)
+                               enable_split_k=shape_info.gm_acc)
     result = pypto.matmul(a_tensor, b_tensor, a_trans=shape_info.a_trans, b_trans=shape_info.b_trans,
                           out_dtype=shape_info.out_dtype)
     out_tensor.move(result)
 
 
-@pytest.mark.soc("950", "910")
-@pytest.mark.skip(reason="large test case")
 def test_mm_with_mn_split():
     device_id = os.environ.get('TILE_FWK_DEVICE_ID', 0)
     torch_npu.npu.config.allow_internal_format = True
@@ -161,8 +159,6 @@ def test_mm_with_mn_split():
     assert torch.allclose(c1_tensor.cpu().to(torch.float32), golden.cpu().to(torch.float32), atol=1e-3, rtol=1e-3)
 
 
-@pytest.mark.soc("950", "910")
-@pytest.mark.skip(reason="large test case")
 def test_mm_with_mn_split_nz():
     device_id = os.environ.get('TILE_FWK_DEVICE_ID', 0)
     torch_npu.npu.config.allow_internal_format = True
@@ -192,8 +188,6 @@ def test_mm_with_mn_split_nz():
     assert torch.allclose(c1_tensor.cpu().to(torch.float32), golden.cpu().to(torch.float32), atol=1e-3, rtol=1e-3)
 
 
-@pytest.mark.soc("950", "910")
-@pytest.mark.skip(reason="large test case")
 def test_bmm_with_mn_split():
     device_id = os.environ.get('TILE_FWK_DEVICE_ID', 0)
     torch.npu.set_device(int(device_id))
