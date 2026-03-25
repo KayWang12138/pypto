d5b500f3f83143eb10ab2ce63d710e6310ad046f
DCGDDD <yangxu14@huawei.com>
2026-03-25 13:41:01 +0800

    fix(lcov): Add support for lcov 2.3.2

--- cmake/scripts/gen_coverage.py
+++ cmake/scripts/gen_coverage.py
@@ -385,6 +385,7 @@ class GenCoverage:
         if self.lcov_ability.lcov_supported_parallel:
             cmd += f" --rc geninfo_unexecuted_blocks=1"  # 接受未执行块
             cmd += f" --ignore-errors negative"
+            cmd += f" --ignore-errors unused"  # 兼容高版本 LCov, 无独立判断条件, 暂与并行条件放在一起
             cmd += f" -j {self.job_num}"
         ret = subprocess.run(cmd.split(), capture_output=False, check=True, encoding='utf-8')
         ret.check_returncode()
