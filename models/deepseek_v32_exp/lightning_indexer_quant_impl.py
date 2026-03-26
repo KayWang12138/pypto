0ecb30ed8fa96512cf9c253210031543c7ed75c1
e <zhengyao12@huawei.com>
2026-03-26 16:53:55 +0800

    fix(operation): Fix pad with tfillpad

--- models/deepseek_v32_exp/lightning_indexer_quant_impl.py
+++ models/deepseek_v32_exp/lightning_indexer_quant_impl.py
@@ -203,11 +203,12 @@ def lightning_indexer_decode_compute(
                     pypto.assemble(index_pad, [dst_offset, 0, eff_seq], topk_res)
 
                 for _ in pypto.loop(eff_seq >= selected_count, name="TOPK_GE_SC", idx_name="un_used"):
+                    pypto.set_vec_tile_shapes(1, topk_tile)
                     eff_in = pypto.view(max_tensor, [1, MAX_LI_S2], [src_offset, 0], valid_shape=[1, eff_seq])
-                    eff_3d = pypto.reshape(eff_in, [1, 1, MAX_LI_S2], valid_shape=[1, 1, eff_seq])
+                    _, res_index = pypto.topk(eff_in, k=selected_count, dim=-1, largest=True)
                     pypto.set_vec_tile_shapes(1, 1, topk_tile)
-                    _, res_index = pypto.topk(eff_3d, k=selected_count, dim=-1, largest=True)
-                    pypto.assemble(res_index, [dst_offset, 0, 0], topk_res)
+                    eff_3d = pypto.reshape(res_index, [1, 1, selected_count])
+                    pypto.assemble(pypto.clone(eff_3d), [dst_offset, 0, 0], topk_res)
 
 
 @pypto.frontend.jit(
