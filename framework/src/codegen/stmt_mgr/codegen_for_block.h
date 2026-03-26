1e3e484fc2d7cc06f65cb1331b6e5dfdca495b57
Bee.Hujun <Bee.hujun@huawei.com>
2026-03-26 15:41:31 +0800

    feat(codegen): Support binary with broadcast in Loop

--- framework/src/codegen/stmt_mgr/codegen_for_block.h
+++ framework/src/codegen/stmt_mgr/codegen_for_block.h
@@ -15,6 +15,7 @@
 
 #pragma once
 
+#include <map>
 #include <unordered_set>
 #include <vector>
 
@@ -46,28 +47,23 @@ public:
 
     void UpdateAxesList(const std::vector<SymbolicScalar> &axesList);
 
-    void LoopStart() {
-        isInLoop_ = true;
-    }
+    void LoopStart() { isInLoop_ = true; }
 
     void OutLoop() {
         sm_->OutForLoop();
         axesList_.clear();
         forNodes_.clear();
-        tensorNeedSetAddr_.clear();
+        tensorOffset_.clear();
+        allOffsetsInLoop_.clear();
         opList_.clear();
 
         isInLoop_ = false;
+        offsetCnt_ = 0;
     }
 
     bool IsInLoop() { return isInLoop_; }
 
-    void AddTensorInLoopBody(const std::string& tensorFullDim, const TileTensor &tileTensor) {
-        CODEGEN_LOGI("AddTensorInLoopBody : %s", tileTensor.tensorName.c_str());
-        std::string tensorNameInLoop = sm_->AddTileTensor(tileTensor);
-        sm_->InsertTensorNameInLoopToFullDim(tensorNameInLoop, tensorFullDim);
-        tensorNeedSetAddr_.insert(tensorNameInLoop);
-    }
+    void AddTensorInLoopBody(size_t tensorFullDimHash, const TileTensor &tileTensor, Opcode opCode);
 
     void AddOpInLoopBody(std::string &op) {
         CODEGEN_LOGI("AddOpInLoopBody add op : %s", op.c_str());
@@ -82,14 +78,21 @@ private:
     void PrintForEnd(std::ostringstream &os) const;
     void PrintOffsetDef(std::ostringstream &os) const;
     void PrintSetAddrs(std::ostringstream &os) const;
-    void PrintSetAddrSingle(std::ostringstream &os, const std::string &tensor) const;
+    void PrintSetAddrSingle(std::ostringstream &os, const std::string &tensor, const std::string &offset) const;
     void PrintTileOps(std::ostringstream &os) const;
+    void UpdateTensorOffsetInLoop(Opcode opCode, size_t tensorFullDimHash, const std::string &tensorNameInLoop);
 
     std::shared_ptr<SymbolManager> sm_;
     std::vector<SymbolicScalar> axesList_;
     std::vector<ForNode> forNodes_;
-    std::unordered_set<std::string> tensorNeedSetAddr_;
     std::vector<std::string> opList_;
+    // tensorNameInLoop -> offset var
+    std::unordered_map<std::string, std::string> tensorOffset_;
+    // OffsetInLoop -> offset var
+    using OffsetInLoop = std::vector<std::string>;
+    std::map<OffsetInLoop, std::string> allOffsetsInLoop_;
+    OffsetInLoop defaultOffset_;
     bool isInLoop_{false};
+    uint32_t offsetCnt_{0};
 };
 } // namespace npu::tile_fwk
