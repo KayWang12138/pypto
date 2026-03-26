d86293acda1732360cbc0b116317d84347377644
wangqun23 <wangqun23@huawei.com>
2026-03-26 16:24:51 +0800

    fix(pass): Fixed the issue of MTE read address out of bounds in replaceTensor.

--- framework/src/passes/tile_graph_pass/graph_constraint/replace_tensor.h
+++ framework/src/passes/tile_graph_pass/graph_constraint/replace_tensor.h
@@ -160,7 +160,7 @@ private:
 
     Status ForUpdateView(Operation *op);
     Status BackUpdateAssemble(Operation *op);
-    std::vector<OpImmediate>  SumOffsetForCopyIn(const std::vector<OpImmediate> offset1, const std::vector<OpImmediate> offset2);
+    std::vector<OpImmediate> SumOffsetForCopyIn(const std::vector<OpImmediate> offset1, const std::vector<OpImmediate> offset2);
     Status UpdateCopyInAttr(Operation *copyInOp);
 
     Status MarkTensorAsPartialMem(Function &function);
