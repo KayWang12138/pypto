a6c2c19e349684fbf2c1bbd5ee1f92b824490ff4
wan_jianfen <wanjianfen@hisilicon.com>
2026-03-26 10:07:36 +0800

    fix(machine): Free dev mem

--- framework/src/machine/runtime/device_runner.cpp
+++ framework/src/machine/runtime/device_runner.cpp
@@ -811,12 +811,6 @@ void DeviceRunner::StopMachinePerfTraceDumpThread() {
             rtFree(ptr);
         }
     }
-    for (size_t i = 0; i < perfData_.size(); i++) {
-        if (perfData_[i] != nullptr) {
-            rtFree(perfData_[i]);
-        }
-    }
-    perfData_.clear();
 }
 
 void DeviceRunner::MachinePerfTraceDumpThread() {
@@ -832,6 +826,12 @@ void DeviceRunner::MachinePerfTraceDumpThread() {
 DeviceRunner::~DeviceRunner() {
     MACHINE_LOGD("Start to cleanup perfData");
     StopMachinePerfTraceDumpThread();
+    for (size_t i = 0; i < perfData_.size(); i++) {
+        if (perfData_[i] != nullptr) {
+            ret = rtFree(perfData_[i]);
+        }
+    }
+    perfData_.clear();
 }
 
 } // namespace npu::tile_fwk
