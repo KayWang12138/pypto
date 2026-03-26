c2de758e97e57db1b7ec6a4642838cfc39c7fb57
Bee.Hujun <Bee.hujun@huawei.com>
2026-03-26 17:08:33 +0800

    feat(codegen): Get address of GM Tensor from PASS

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.h
@@ -215,10 +215,10 @@ private:
     int GetCacheModeFlag(const std::string &cacheMode) const;
 
     template <typename T>
-    bool GetAttr(const std::string &key, T &value) const {
-        auto it = opAttrs.find(key);
-        if (it == opAttrs.end()) {
-            CODEGEN_LOGI("can not find key: %s in opAttrs", key.c_str());
+    bool GetAttrFromMap(const std::map<std::string, Any> &attrMap, const std::string &key, T &value) const {
+        auto it = attrMap.find(key);
+        if (it == attrMap.end()) {
+            CODEGEN_LOGI("can not find key: %s in attrMap", key.c_str());
             return false;
         }
         if (it->second.Type() == typeid(T)) {
@@ -228,6 +228,20 @@ private:
         CODEGEN_LOGE_E(GenCodeErr::DATA_TYPE_MISMATCHED, "Type of attribute %s from PASS is mismatch: %s != %s",
             key.c_str(), it->second.Type().name(), typeid(T).name());
         return false;
+    }
+
+    template <typename T>
+    bool GetAttr(const std::string &key, T &value) const {
+        return GetAttrFromMap(opAttrs, key, value);
+    }
+
+    template <typename T>
+    bool GetTensorAttr(int idx, const std::string &key, T &value) const {
+        if (idx < 0 || idx >= MAX_OPERANDS) {
+            CODEGEN_LOGE_E(GenCodeErr::INVALID_INDEX, "idx %d out of range [0, %d)", idx, MAX_OPERANDS);
+            return false;
+        }
+        return GetAttrFromMap(tensorAttrs[idx], key, value);
     }
 
     template <typename T = int64_t>
