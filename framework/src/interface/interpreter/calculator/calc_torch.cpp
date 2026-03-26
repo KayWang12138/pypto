931117d1127c4872f18facae3726ad5f567d2202
Crayon <taojianlong1@huawei.com>
2026-03-26 12:06:09 +0800

    feat(tests): Add precision tools for mc2 operations.

--- framework/src/interface/interpreter/calculator/calc_torch.cpp
+++ framework/src/interface/interpreter/calculator/calc_torch.cpp
@@ -20,6 +20,7 @@
 #include "tilefwk/error.h"
 #include "securec.h"
 #include "calc_error.h"
+#include "communication.h"
 
 namespace npu::tile_fwk {
 
@@ -2034,6 +2035,22 @@ static void Scatter(const TensorData &out, const TensorData &self, const TensorD
     }
 }
 
+static void Bind(const TensorData &out, uint64_t groupIndex, uint64_t memType, uint64_t slotSize) {
+    (void) out;
+    (void) groupIndex;
+    auto &comm = CommManager::getInstance();
+    std::string groupName = "";
+    int rank = 0;
+    int worldSize = 0;
+    // TODO: 并非根据 memType 来分配
+    if (memType == 1) {
+        comm.InitControl(groupName, rank, worldSize);
+    }
+    if (memType == 0) {
+        comm.InitData(groupName, (size_t) slotSize);
+    }
+}
+
 static struct CalcOps calcOps = {
     .Random = Random,
     .AllClose = AllClose,
@@ -2155,6 +2172,7 @@ static struct CalcOps calcOps = {
     .BitwiseLeftShiftS = BitwiseLeftShiftS,
     .SBitwiseRightShift = SBitwiseRightShift,
     .SBitwiseLeftShift = SBitwiseLeftShift,
+    .BindTensor = Bind,
 };
 
 extern "C" struct CalcOps *GetCalcOps() {
