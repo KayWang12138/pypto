6563dd7ebf5787b7b52a5bf74008652d64bee363
xujiayi1999 <xujiayi28@huawei.com>
2026-03-27 12:16:50 +0800

    fix

--- framework/src/passes/pass_utils/pass_utils.cpp
+++ framework/src/passes/pass_utils/pass_utils.cpp
@@ -19,8 +19,12 @@
 #include "interface/tensor/logical_tensor.h"
 #include "interface/function/function.h"
 #include "tilefwk/platform.h"
+#include "passes/pass_log/pass_log.h"
+
+#define MODULE_NAME "PassUtils"
 
 namespace npu::tile_fwk {
+
 void FunctionUtils::RelinkOperationInput(Operation *op, const size_t inputIndex, const Operation *targetOp,
                                          const size_t outputIndex) {
     if (op == nullptr || targetOp == nullptr) {
@@ -116,13 +120,13 @@ const int SPACE_NUM_16 = 16;
 } // namespace
 void SubfuncInvokeInfoTy::ConstructActualInvokeParam(int esgId) {
     if (!isFinalized_) {
-        ALOG_ERROR("Error: does not finalized before constructing InvokeParam");
+        APASS_LOG_ERROR_F(Elements::Function, "Error: does not finalized before constructing InvokeParam");
         return;
     }
 
     int paramLoc = 0;
     for (auto &tensorArg : tensorArgs_) {
-        ALOG_DEBUG_F("######## Construct TA for %d ########", esgId);
+        APASS_LOG_DEBUG_F(Elements::Function, "Construct TA for %d", esgId);
         tensorParamList_.emplace_back(paramLoc, tensorArg.realDDRId, tensorArg.offset, tensorArg.shape,
             tensorArg.rawShape, tensorArg.dType, tensorArg.isOutputToGM, tensorArg.tensor, tensorArg.opMagic,
             tensorArg.operandIdx);
@@ -877,6 +881,6 @@ std::unordered_map<MemoryType, int64_t> CommonUtils::GetLocalMemorySize() {
     localMemorySize[MemoryType::MEM_FIX] = die.GetMemoryLimit(MemoryType::MEM_FIX);
     localMemorySize[MemoryType::MEM_FIX_QUANT_PRE] = die.GetMemoryLimit(MemoryType::MEM_FIX_QUANT_PRE);
 
-    return localMemorySize;
+    return localMemorySize; 
 }
 } // namespace npu::tile_fwk
