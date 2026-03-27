aba6d390efc7be8d2bead13c67340bd95cc8450e
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-03-27 17:54:25 +0800

    feat(pass): Insert Sync ignore RESHAPE

--- framework/src/passes/block_graph_pass/insert_sync.h
+++ framework/src/passes/block_graph_pass/insert_sync.h
@@ -254,7 +254,6 @@ private:
     PipeSeq GetPipeSeq(PipeCoreReal pipe);
     PipeCoreReal GetPipeFromSeq(PipeSeq seq);
     Status PipeDispatch(const std::vector<Operation *> opLogPtr, std::vector<IndexOp> &syncedOpLog);
-    Status AdjustReshapeCfg(TileOpCfg &opcfg, Operation &op);
     Status AdjustCopyInCfg(TileOpCfg &opcfg, Operation &op);
     Status AdjustCopyOutCfg(TileOpCfg &opcfg, Operation &op);
     Status AdjustOpCfg(TileOpCfg &opcfg, Operation &op);
