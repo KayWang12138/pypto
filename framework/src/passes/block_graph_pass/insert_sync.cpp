1effb07430821dfff8b0f709ee45fe7b7ecefefa
GaoZiXuan61 <gaozixuan3@huawei.com>
2026-03-27 17:40:55 +0800

    feat(pass): Remove code

--- framework/src/passes/block_graph_pass/insert_sync.cpp
+++ framework/src/passes/block_graph_pass/insert_sync.cpp
@@ -412,9 +412,6 @@ Status PipeSync::AdjustReshapeCfg(TileOpCfg &opcfg, Operation &op) {
         opcfg.pipeIdStart_ = PipeType::PIPE_MTE3;
         opcfg.pipeIdEnd_ = PipeType::PIPE_MTE3;
     }
-    if (op.HasAttribute(OpAttributeKey::isCube) && op.GetBoolAttribute(OpAttributeKey::isCube)) {
-        opcfg.coreType_ = CoreType::AIC;
-    }
     return SUCCESS;
 }
 
