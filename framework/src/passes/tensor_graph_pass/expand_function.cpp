d4222bc5b4dfa5821839e48fc7db55fbc57e9f7b
bytecode <dubingjia1@huawei.com>
2026-03-28 17:38:11 +0800

    refactor(pass): Remove redundant SetScopeId in ProcessForNotExpandOp

--- framework/src/passes/tensor_graph_pass/expand_function.cpp
+++ framework/src/passes/tensor_graph_pass/expand_function.cpp
@@ -69,7 +69,6 @@ Status ExpandFunction::ClearIOOperand(const std::vector<OperationPtr> &tensorOpe
 void ExpandFunction::ProcessForNotExpandOp(Function &function, Operation &op) const {
     auto &newOp = function.AddOperation(op.GetOpcode(), op.GetIOperands(), op.GetOOperands());
     newOp.SetOpAttribute(op.GetOpAttribute());
-    newOp.SetScopeId(op.GetScopeId());
     newOp.CopyAttrFrom(op, OP_EMUOP_PREFIX);
     if (op.HasAttribute(OpAttributeKey::inplaceIdx)) {
         newOp.SetAttribute(OpAttributeKey::inplaceIdx, op.GetIntAttribute(OpAttributeKey::inplaceIdx));
