3268d21577c21edbe9879275c0fb2a947c93dff9
bytecode <dubingjia1@huawei.com>
2026-03-28 17:45:46 +0800

    fix(pass): Add SetScopeId for convert ops to preserve scope inheritance

--- framework/src/passes/tile_graph_pass/data_path/convert_op_inserter.cpp
+++ framework/src/passes/tile_graph_pass/data_path/convert_op_inserter.cpp
@@ -505,6 +505,8 @@ void ConvertInserter::InsertConvertOps(Function &function) {
         auto &convertOp = function.AddRawOperation(Opcode::OP_CONVERT, {c.input}, {c.output});
         convertOp.SetOpAttribute(std::make_shared<ConvertOpAttribute>(c.from, c.to));
         CreateMoveOpForConvert(convertOp);
+        auto producerScopeId = (*(c.input->GetProducers().begin()))->GetScopeId();
+        convertOp.SetScopeId(producerScopeId);
     }
 }
 
