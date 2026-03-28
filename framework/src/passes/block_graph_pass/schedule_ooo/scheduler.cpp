18c7b711921475c9d0ff57b2528d3691bb01f2ca
heyufeng <heyufeng11@hisilicon.com>
2026-03-28 10:14:09 +0800

    feat(pass): Workspace GM tensor alloc in OoO

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.cpp
@@ -1306,10 +1306,10 @@ Status OoOScheduler::Init(
 
 void OoOScheduler::AllocWorkspaceGM(const std::vector<Operation *> &operations) {
     std::set<int> allocedRawmagic;
-    for (auto &inCast : function_->GetIncast()) {
+    for (auto &inCast : function_.GetIncast()) {
         allocedRawmagic.insert(inCast->tensor->GetRawMagic());
     }
-    for (auto &outCast : function_->GetOutcast()) {
+    for (auto &outCast : function_.GetOutcast()) {
         allocedRawmagic.insert(outCast->tensor->GetRawMagic());
     }
     for (auto &op : operations) {
