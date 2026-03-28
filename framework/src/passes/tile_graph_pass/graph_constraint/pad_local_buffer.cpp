5c99378280fdbfb7570b28838d58ed8910a8f863
bytecode <dubingjia1@huawei.com>
2026-03-28 10:36:02 +0800

    update: 更新文件 pad_local_buffer.cpp

--- framework/src/passes/tile_graph_pass/graph_constraint/pad_local_buffer.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pad_local_buffer.cpp
@@ -714,7 +714,7 @@ void PadLocalBuffer::PadVectorForAxisCombine(Operation &op, LogicalTensorPtr &in
     if (calcType == OpCalcType::CAST || calcType == OpCalcType::ELMWISE || calcType == OpCalcType::MOVE_IN ||
         calcType == OpCalcType::MOVE_OUT || op.GetOpcode() == Opcode::OP_VIEW ||
         (producerOp != nullptr && OpcodeManager::Inst().GetOpCalcType(producerOp->GetOpcode()) == OpCalcType::BROADCAST)) {
-        if (!axisCombineMarker.IsTensorEnableAxisCombine(in)) {
+        if (op.GetOpcode() == Opcode::OP_EXPAND || !axisCombineMarker.IsTensorEnableAxisCombine(in)) {
             AlignedRawTensorIfNeed(in, lastIdx, paddingValue);
             return;
         }
