d261b681d1f36a9eda3646617b532e1004e6e024
gcw_Lf43RugY <mingrui5@huawei.com>
2026-03-28 15:10:55 +0800

    update: 更新文件 device_launcher.h
    Signed-off-by: gcw_Lf43RugY <mingrui5@huawei.com>

--- framework/src/machine/runtime/device_launcher.h
+++ framework/src/machine/runtime/device_launcher.h
@@ -186,14 +186,7 @@ public:
         devProg->devArgs.nrValidAic = config.blockdim;
         devProg->devArgs.archInfo = static_cast<ArchInfo>(Platform::Instance().GetSoc().GetNPUArch());
         devProg->devArgs.taskType = DEVICE_TASK_TYPE_DYN;
-#ifdef __ESL_SIMULATION__
-    devProg->devArgs.nrAic = 32;
-    devProg->devArgs.nrAiv = 64;
-    devProg->devArgs.nrValidAic = 32;
-    int aiCpuNum = 7;
-#else
-    int aiCpuNum = static_cast<int>(Platform::Instance().GetSoc().GetAICPUNum());
-#endif
+        int aiCpuNum = static_cast<int>(Platform::Instance().GetSoc().GetAICPUNum());
         devProg->devArgs.scheCpuNum = CalcSchAicpuNumByBlockDim(devProg->devArgs.nrValidAic, aiCpuNum, devProg->devArgs.archInfo);
         devProg->devArgs.maxAicpuNum = aiCpuNum;
         config.aicpuNum = devProg->devArgs.scheCpuNum + dynamic::MAX_OTHER_AICPU_NUM;
