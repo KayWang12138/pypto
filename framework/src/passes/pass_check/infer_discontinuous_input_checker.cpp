ecf2a569371c02cd75b6dc0c58fa1bb6c46e7741
xujiayi1999 <xujiayi28@huawei.com>
2026-03-30 10:34:44 +0800

    fix(Pass): Checker add ErrCode

--- framework/src/passes/pass_check/infer_discontinuous_input_checker.cpp
+++ framework/src/passes/pass_check/infer_discontinuous_input_checker.cpp
@@ -102,7 +102,7 @@ Status checkTensor(const LogicalTensorPtr& tensor)
         std::shared_ptr<AssembleOpAttribute> attr =
             std::dynamic_pointer_cast<AssembleOpAttribute>(producer->GetOpAttribute());
         if (attr == nullptr) {
-            APASS_LOG_ERROR_C(OperationErr::OP_NULL_POINTER, Elements::Operation, "Assemble op %d do not have attribute. %s", producer->GetOpMagic(),
+            APASS_LOG_ERROR_F(Elements::Operation, "Assemble op %d do not have attribute. %s", producer->GetOpMagic(),
                 GetFormatBacktrace(producer).c_str());
             return FAILED;
         }
