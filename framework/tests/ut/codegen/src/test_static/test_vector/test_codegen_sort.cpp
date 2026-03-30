7f5c599861e2c1a02be2264993b968ede691529a
Bee.Hujun <Bee.hujun@huawei.com>
2026-03-30 17:18:21 +0800

    feat(codegen): Use non-thread-safe id generator for SymbolManager

--- framework/tests/ut/codegen/src/test_static/test_vector/test_codegen_sort.cpp
+++ framework/tests/ut/codegen/src/test_static/test_vector/test_codegen_sort.cpp
@@ -46,8 +46,6 @@ public:
         config::SetBuildStatic(true);
         config::SetHostOption(COMPILE_STAGE, CS_EXECUTE_GRAPH);
         config::SetPlatformConfig(KEY_ENABLE_COST_MODEL, false);
-        IdGen<IdType::CG_USING_NAME>::Inst().SetId(DummyFuncMagic);
-        IdGen<IdType::CG_VAR_NAME>::Inst().SetId(DummyFuncMagic);
     }
 
     void TearDown() override {}
