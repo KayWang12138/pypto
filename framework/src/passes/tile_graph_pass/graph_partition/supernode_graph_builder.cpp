4d5e23045ab2b1271c758507cd13be700e6eed41
tonibohnlein <toni.boehnlein@huawei.com>
2026-03-31 15:30:12 +0000

    uninline functions

--- framework/src/passes/tile_graph_pass/graph_partition/supernode_graph_builder.cpp
+++ framework/src/passes/tile_graph_pass/graph_partition/supernode_graph_builder.cpp
@@ -453,7 +453,7 @@ inline bool IsL0cToL1MoveOp(Operation* op)
            op->GetOOperands()[0]->GetMemoryTypeOriginal() == MemoryType::MEM_L1;
 }
 
-inline bool SuperNodeGraphBuilder::L1CopyInCombine(
+bool SuperNodeGraphBuilder::L1CopyInCombine(
     const std::shared_ptr<OperationGraphInfo> operationInfo, std::vector<Operation*>& opList, int32_t i,
     std::vector<std::pair<int32_t, int32_t>>& mergePair)
 {
@@ -490,7 +490,7 @@ inline bool SuperNodeGraphBuilder::L1CopyInCombine(
     return false;
 }
 
-inline bool SuperNodeGraphBuilder::ConvertCombine(
+bool SuperNodeGraphBuilder::ConvertCombine(
     const std::shared_ptr<OperationGraphInfo> operationInfo, std::vector<Operation*>& opList, int32_t i,
     std::vector<std::pair<int32_t, int32_t>>& mergePair)
 {
@@ -527,7 +527,7 @@ inline bool SuperNodeGraphBuilder::ConvertCombine(
     return true;
 }
 
-inline bool SuperNodeGraphBuilder::AssembleCombine(
+bool SuperNodeGraphBuilder::AssembleCombine(
     const std::shared_ptr<OperationGraphInfo> operationInfo, std::vector<Operation*>& opList, int32_t i,
     std::vector<std::pair<int32_t, int32_t>>& mergePair)
 {
@@ -555,7 +555,7 @@ inline bool SuperNodeGraphBuilder::AssembleCombine(
     return false;
 }
 
-inline bool SuperNodeGraphBuilder::CopyOutCombine(
+bool SuperNodeGraphBuilder::CopyOutCombine(
     const std::shared_ptr<OperationGraphInfo> operationInfo, std::vector<Operation*>& opList, int32_t i,
     std::vector<std::pair<int32_t, int32_t>>& mergePair, bool assembleScene)
 {
@@ -576,7 +576,7 @@ inline bool SuperNodeGraphBuilder::CopyOutCombine(
     return false;
 }
 
-inline bool SuperNodeGraphBuilder::CopyInCombine(
+bool SuperNodeGraphBuilder::CopyInCombine(
     const std::shared_ptr<OperationGraphInfo> operationInfo, std::vector<Operation*>& opList, int32_t i,
     std::vector<std::pair<int32_t, int32_t>>& mergePair)
 {
@@ -596,7 +596,7 @@ inline bool SuperNodeGraphBuilder::CopyInCombine(
     return false;
 }
 
-inline bool SuperNodeGraphBuilder::MulAccCombine(
+bool SuperNodeGraphBuilder::MulAccCombine(
     const std::shared_ptr<OperationGraphInfo> operationInfo, std::vector<Operation*>& opList, int32_t i,
     std::vector<std::pair<int32_t, int32_t>>& mergePair)
 {
@@ -645,7 +645,7 @@ bool SuperNodeGraphBuilder::ExpandCombine(const std::shared_ptr<OperationGraphIn
     return false;
 }
 
-inline bool SuperNodeGraphBuilder::AssembleToCopyoutScene(Operation* op)
+bool SuperNodeGraphBuilder::AssembleToCopyoutScene(Operation* op)
 {
     auto assembleIn = op->iOperand.front();
     auto parentOp = *assembleIn->GetProducers().begin();
