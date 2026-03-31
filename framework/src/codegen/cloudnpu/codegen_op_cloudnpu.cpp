843108e3eb4a0620fea1ed55ba843aab763c6c4d
heyufeng <heyufeng11@hisilicon.com>
2026-03-31 11:34:11 +0800

    feat(pass): Set tensor PARAM_ADDR

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
@@ -446,7 +446,12 @@ std::string CodeGenOpCloudNPU::GenGmParamVar(unsigned gmParamIdx) const
         std::ostringstream os;
         os << "GET_PARAM_ADDR(" << GM_TENSOR_PARAM_STR << ", " << GmTensorParamIdxInCallFunc << ", "
            << paramLocation[gmParamIdx] << ")";
-        return os.str();
+        SymbolicScalar addr;
+        bool ret = GetTensorAttr(gmParamIdx, "paramAddr", addr);
+        ASSERT(OperErr::ATTRIBUTE_INVALID, ret)
+            << "paramAddr is not found!! gmParamIdx: " << gmParamIdx << ", op: " << originalOp.Dump();
+        std::string gmParamVar = SymbolicExpressionTable::BuildExpression(addr);
+        return gmParamVar;
     }
 
     auto paramLoc = paramLocation[gmParamIdx];
