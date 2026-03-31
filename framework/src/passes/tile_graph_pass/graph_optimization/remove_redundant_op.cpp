66d839aa089f40bdf5effe3fa89fe331fed91d52
m00896683 <mayuan33@huawei.com>
2026-03-31 19:07:55 +0800

    fix precsion bug

--- framework/src/passes/tile_graph_pass/graph_optimization/remove_redundant_op.cpp
+++ framework/src/passes/tile_graph_pass/graph_optimization/remove_redundant_op.cpp
@@ -82,13 +82,26 @@ bool RemoveRedundantOp::ProcessRedundantOpWithoutDynShape(Operation& op) const
         return false;
     }
     if (op.GetOpcode() == Opcode::OP_ASSEMBLE) {
-        auto assembleOut = op.GetOOperands().front();
-        if (assembleOut->GetProducers().size() > 1) {
-            APASS_LOG_DEBUG_F(
-                Elements::Operation, "assembleOut[%d] has more than one producer, skip removing.",
-                assembleOut->GetMagic());
+        auto assembleOutput = op.GetOOperands().front();
+        if (assembleOutput->GetProducers().size() > 1) {
+            APASS_LOG_DEBUG_F(Elements::Operation, "assembleOutput[%d] has more than one producer, skip removing.",
+                assembleOutput->GetMagic());
             return false;
         }
+        auto assembleInput = op.GetIOperands().front();
+        int parallelAssembleCnt = 0;
+        for (const auto& consumer : assembleInput->GetConsumers()) {
+            if (consumer->GetOpcode() == Opcode::OP_ASSEMBLE) {
+                parallelAssembleCnt++;
+            }
+        }
+        bool hasReshapeConsumer = false;
+        for (const auto& consumer : assembleOutput->GetConsumers()) {
+            if (consumer->GetOpcode() == Opcode::OP_RESHAPE) {
+                hasReshapeConsumer = true;
+            }
+        }
+        if (parallelAssembleCnt > 1 && hasReshapeConsumer) return false;
     }
     return true;
 }
