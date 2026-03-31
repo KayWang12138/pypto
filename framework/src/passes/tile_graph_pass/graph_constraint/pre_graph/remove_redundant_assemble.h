18289b8260e8652bbe1042981ef369016336cb60
wangqun23 <wangqun23@huawei.com>
2026-03-31 19:13:52 +0800

    fix(pass): Add scenarios for PreGraph not to delete Assemble.

--- framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.h
+++ framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.h
@@ -33,6 +33,8 @@ public:
         std::set<Operation*, LogicalTensor::CompareOp>& producersBackup) const;
     void HandleForReshapeToOutcast(Function& function) const;
     void HanldeForMultiAssemble(Function& function, std::unordered_set<Operation*>& concurrentAssembles) const;
+    bool ForwardFindAssembleInsertedCopy(LogicalTensorPtr input) const;
+    bool BackwardFindAssembleInsertedCopy(LogicalTensorPtr input) const;
     bool FindAssembleOut(Operation* con, int assembleOutMagic) const;
     Status HanldeForSingleAssemble(
         Function& function, LogicalTensorPtr input, LogicalTensorPtr output, Operation& op) const;
