a76da2ef90668654203b68ebf09a1125ec2665b9
xujiayi1999 <xujiayi28@huawei.com>
2026-04-01 10:42:19 +0800

    fix(Pass): Checker add ErrCode

--- framework/tests/ut/passes/src/test_removeredundantreshape.cpp
+++ framework/tests/ut/passes/src/test_removeredundantreshape.cpp
@@ -390,6 +390,7 @@ TEST_F(TestRemoveRedundantReshapePass, ReshapeNoConsumer) {
     currFunctionPtr->inCasts_.push_back(inCast);
     currFunctionPtr->outCasts_.push_back(outCast);
     RemoveRedundantReshape pass;
+    
     Status ret = pass.PreCheck(*currFunctionPtr);
     EXPECT_EQ(ret, FAILED);
 }
