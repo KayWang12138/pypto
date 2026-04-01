aa0d938431e5fe68058c64654c36fb551088990e
zhiweiyuan <zhiweiyuan@huawei.com>
2026-04-01 15:59:33 +0800

    fix(simulation): Fix bad incast tensor addr bug
    Signed-off-by: zhiweiyuan <zhiweiyuan@huawei.com>

--- framework/tests/st/operation/src/test_operation.h
+++ framework/tests/st/operation/src/test_operation.h
@@ -26,6 +26,7 @@
 #include "machine/utils/dynamic/dev_encode.h"
 #include "test_dev_func_runner.h"
 #include "interface/tensor/float.h"
+#include "cost_model/simulation/cost_model_launcher.h"
 
 namespace tile_fwk {
 namespace test_operation {
@@ -140,7 +141,8 @@ private:
         if (testCase.onBoard) {
             DevFuncRunner::Run(Program::GetInstance().GetLastFunction());
         } else {
-            CostModelDynFuncRunner::Run(Program::GetInstance().GetLastFunction());
+            config::SetRuntimeOption(CFG_RUN_MODE, CFG_RUN_MODE_SIM);
+            CostModelLauncher::CostModelRunOnce(Program::GetInstance().GetLastFunction());
         }
 
         ASSERT_EQ(testCase.goldenPaths.size(), testCase.outputTensors.size());
