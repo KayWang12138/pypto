0fe72d6172d20e6c11438e3d3370ff791bb07913
wangrunfa <SAmssj5vNgY7ysXXuzejj8g5>
2026-04-01 16:39:24 +0800

    gm41

--- framework/src/interface/tileop/vector/permute.h
+++ framework/src/interface/tileop/vector/permute.h
@@ -4,13 +4,13 @@
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FIT FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
 /*!
  * \file permute.h
- * \brief Permute tile operation implementation
+ * \brief Permute tileOp - GM->UB mode (reference: Tgather)
  */
 
 #ifndef TILEOP_TILE_OPERATOR_PERMUTE__H
@@ -18,137 +18,149 @@
 #include "utils/layout.h"
 #include "utils/tile_tensor.h"
 
+#include <cstdint>
+
 #define OP_TILE_OP_PERMUTE Tpermute
 
-template <typename DstType, typename SrcType, typename DstLayout, typename SrcLayout>
-__aicore__ inline void PermuteCopy4DScalar(
-    __ubuf__ DstType *dstAddr, __ubuf__ SrcType *srcAddr, const DstLayout &dstLayout,
-    const SrcLayout &srcLayout, int axis0, int axis1, int axis2, int axis3) {
-    auto dstShape0 = dstLayout.template GetShapeDim<0, 4>();
-    auto dstShape1 = dstLayout.template GetShapeDim<1, 4>();
-    auto dstShape2 = dstLayout.template GetShapeDim<2, 4>();
-    auto dstShape3 = dstLayout.template GetShapeDim<3, 4>();
-    auto dstStride0 = dstLayout.template GetStrideDim<0, 4>();
-    auto dstStride1 = dstLayout.template GetStrideDim<1, 4>();
-    auto dstStride2 = dstLayout.template GetStrideDim<2, 4>();
-    auto dstStride3 = dstLayout.template GetStrideDim<3, 4>();
-    auto srcStride0 = srcLayout.template GetStrideDim<0, 4>();
-    auto srcStride1 = srcLayout.template GetStrideDim<1, 4>();
-    auto srcStride2 = srcLayout.template GetStrideDim<2, 4>();
-    auto srcStride3 = srcLayout.template GetStrideDim<3, 4>();
-    for (LoopVar i0 = 0; i0 < dstShape0; ++i0) {
-        for (LoopVar i1 = 0; i1 < dstShape1; ++i1) {
-            for (LoopVar i2 = 0; i2 < dstShape2; ++i2) {
-                for (LoopVar i3 = 0; i3 < dstShape3; ++i3) {
-                    int64_t dstCoord[4] = {
-                        static_cast<int64_t>(i0), static_cast<int64_t>(i1), static_cast<int64_t>(i2),
-                        static_cast<int64_t>(i3)};
-                    int64_t srcCoord[4] = {0, 0, 0, 0};
-                    srcCoord[axis0] = dstCoord[0];
-                    srcCoord[axis1] = dstCoord[1];
-                    srcCoord[axis2] = dstCoord[2];
-                    srcCoord[axis3] = dstCoord[3];
-                    auto dstOffset = i0 * dstStride0 + i1 * dstStride1 + i2 * dstStride2 + i3 * dstStride3;
-                    auto srcOffset =
-                        srcCoord[0] * srcStride0 + srcCoord[1] * srcStride1 + srcCoord[2] * srcStride2 +
-                        srcCoord[3] * srcStride3;
-                    dstAddr[dstOffset] = srcAddr[srcOffset];
+template <int axis0, int axis1, int axis2, int axis3, int axis4, int dimCount,
+          typename T0, typename T1, typename C0>
+TILEOP void Tpermute(T0 dst, T1 src, C0 srcCoordinate) {
+    constexpr size_t dstExpectSize = 5;
+    constexpr size_t srcExpectSize = 5;
+    const auto dstLayout = dst.GetLayout();
+    const auto srcLayout = src.GetLayout();
+    auto srcOffset = srcLayout.template GetGmOffset<C0, srcExpectSize>(srcCoordinate);
+    using srcType = typename T1::Type;
+    using dstType = typename T0::Type;
+    __gm__ srcType *srcAddr = (__gm__ srcType *)((uint64_t)(src.GetAddr()));
+    srcAddr += srcOffset;
+    __ubuf__ dstType *dstAddr = (__ubuf__ dstType *)((uint64_t)(dst.GetAddr()));
+
+    constexpr auto shapeSize = Std::tuple_size<typename T0::Shape>::value;
+    constexpr auto tileH = Std::tuple_element<shapeSize - 2, typename T0::TileShape>::type::value;
+    constexpr auto tileW = Std::tuple_element<shapeSize - 1, typename T0::TileShape>::type::value;
+    using ShapeDim5 = pto::Shape<-1, -1, -1, -1, -1>;
+    using StrideDim5 = pto::Stride<-1, -1, -1, -1, -1>;
+    using GlobalData = pto::GlobalTensor<srcType, ShapeDim5, StrideDim5>;
+    using TileDefine = pto::Tile<pto::TileType::Vec, dstType, tileH, tileW, pto::BLayout::RowMajor, -1, -1>;
+    auto srcStride0 = srcLayout.template GetStrideDim<0, srcExpectSize>();
+    auto srcStride1 = srcLayout.template GetStrideDim<1, srcExpectSize>();
+    auto srcStride2 = srcLayout.template GetStrideDim<2, srcExpectSize>();
+    auto srcStride3 = srcLayout.template GetStrideDim<3, srcExpectSize>();
+    auto srcStride4 = srcLayout.template GetStrideDim<4, srcExpectSize>();
+
+    if constexpr (dimCount == 2 && axis0 == 1 && axis1 == 0) {
+        auto d0 = dstLayout.template GetShapeDim<0, 2>();
+        auto d1 = dstLayout.template GetShapeDim<1, 2>();
+        auto ds0 = dstLayout.template GetStrideDim<0, 2>();
+        for (LoopVar i0 = 0; i0 < d0; ++i0) {
+            __ubuf__ dstType *dst0 = dstAddr + i0 * ds0;
+            int64_t sc[5] = {0, 0, 0, 0, 0};
+            sc[axis0] = static_cast<int64_t>(i0);
+            sc[axis1] = 0;
+            auto gmOff = sc[0] * srcStride0 + sc[1] * srcStride1 + sc[2] * srcStride2 + sc[3] * srcStride3 + sc[4] * srcStride4;
+            TileDefine dstTile(1, d1);
+            GlobalData srcGlobal(srcAddr + gmOff,
+                pto::Shape(1, 1, 1, 1, d1), pto::Stride(0, 0, 0, 0, srcStride0));
+            pto::TASSIGN(dstTile, (uint64_t)dst0);
+            pto::TLOAD(dstTile, srcGlobal);
+        }
+        set_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
+        wait_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
+    } else if constexpr (dimCount == 3) {
+        auto d0 = dstLayout.template GetShapeDim<0, 3>();
+        auto d1 = dstLayout.template GetShapeDim<1, 3>();
+        auto d2 = dstLayout.template GetShapeDim<2, 3>();
+        auto ds0 = dstLayout.template GetStrideDim<0, 3>();
+        auto ds1 = dstLayout.template GetStrideDim<1, 3>();
+        auto ds2 = dstLayout.template GetStrideDim<2, 3>();
+        for (LoopVar i0 = 0; i0 < d0; ++i0) {
+            __ubuf__ dstType *dst1 = dstAddr + i0 * ds0;
+            for (LoopVar i1 = 0; i1 < d1; ++i1) {
+                __ubuf__ dstType *dst2 = dst1 + i1 * ds1;
+                int64_t sc[5] = {0, 0, 0, 0, 0};
+                sc[axis0] = static_cast<int64_t>(i0);
+                sc[axis1] = static_cast<int64_t>(i1);
+                sc[axis2] = 0;
+                auto gmOff = sc[0] * srcStride0 + sc[1] * srcStride1 + sc[2] * srcStride2 + sc[3] * srcStride3 + sc[4] * srcStride4;
+                TileDefine dstTile(1, d2);
+                GlobalData srcGlobal(srcAddr + gmOff,
+                    pto::Shape(1, 1, 1, 1, d2), pto::Stride(0, 0, 0, 0, srcStride1));
+                pto::TASSIGN(dstTile, (uint64_t)dst2);
+                pto::TLOAD(dstTile, srcGlobal);
+            }
+        }
+        set_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
+        wait_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
+    } else if constexpr (dimCount == 4) {
+        auto d0 = dstLayout.template GetShapeDim<0, 4>();
+        auto d1 = dstLayout.template GetShapeDim<1, 4>();
+        auto d2 = dstLayout.template GetShapeDim<2, 4>();
+        auto d3 = dstLayout.template GetShapeDim<3, 4>();
+        auto ds0 = dstLayout.template GetStrideDim<0, 4>();
+        auto ds1 = dstLayout.template GetStrideDim<1, 4>();
+        auto ds2 = dstLayout.template GetStrideDim<2, 4>();
+        auto ds3 = dstLayout.template GetStrideDim<3, 4>();
+        for (LoopVar i0 = 0; i0 < d0; ++i0) {
+            __ubuf__ dstType *dst1 = dstAddr + i0 * ds0;
+            for (LoopVar i1 = 0; i1 < d1; ++i1) {
+                __ubuf__ dstType *dst2 = dst1 + i1 * ds1;
+                for (LoopVar i2 = 0; i2 < d2; ++i2) {
+                    __ubuf__ dstType *dst3 = dst2 + i2 * ds2;
+                    int64_t sc[5] = {0, 0, 0, 0, 0};
+                    sc[axis0] = static_cast<int64_t>(i0);
+                    sc[axis1] = static_cast<int64_t>(i1);
+                    sc[axis2] = static_cast<int64_t>(i2);
+                    sc[axis3] = 0;
+                    auto gmOff = sc[0] * srcStride0 + sc[1] * srcStride1 + sc[2] * srcStride2 + sc[3] * srcStride3 + sc[4] * srcStride4;
+                    TileDefine dstTile(1, d3);
+                    GlobalData srcGlobal(srcAddr + gmOff,
+                        pto::Shape(1, 1, 1, 1, d3), pto::Stride(0, 0, 0, 0, srcStride2));
+                    pto::TASSIGN(dstTile, (uint64_t)dst3);
+                    pto::TLOAD(dstTile, srcGlobal);
                 }
             }
         }
-    }
-}
-
-template <typename DstType, typename SrcType, typename DstLayout, typename SrcLayout>
-__aicore__ inline void PermuteCopy5DScalar(
-    __ubuf__ DstType *dstAddr, __ubuf__ SrcType *srcAddr, const DstLayout &dstLayout,
-    const SrcLayout &srcLayout, int axis0, int axis1, int axis2, int axis3, int axis4) {
-    auto dstShape0 = dstLayout.template GetShapeDim<0, 5>();
-    auto dstShape1 = dstLayout.template GetShapeDim<1, 5>();
-    auto dstShape2 = dstLayout.template GetShapeDim<2, 5>();
-    auto dstShape3 = dstLayout.template GetShapeDim<3, 5>();
-    auto dstShape4 = dstLayout.template GetShapeDim<4, 5>();
-    auto dstStride0 = dstLayout.template GetStrideDim<0, 5>();
-    auto dstStride1 = dstLayout.template GetStrideDim<1, 5>();
-    auto dstStride2 = dstLayout.template GetStrideDim<2, 5>();
-    auto dstStride3 = dstLayout.template GetStrideDim<3, 5>();
-    auto dstStride4 = dstLayout.template GetStrideDim<4, 5>();
-    auto srcStride0 = srcLayout.template GetStrideDim<0, 5>();
-    auto srcStride1 = srcLayout.template GetStrideDim<1, 5>();
-    auto srcStride2 = srcLayout.template GetStrideDim<2, 5>();
-    auto srcStride3 = srcLayout.template GetStrideDim<3, 5>();
-    auto srcStride4 = srcLayout.template GetStrideDim<4, 5>();
-    for (LoopVar i0 = 0; i0 < dstShape0; ++i0) {
-        for (LoopVar i1 = 0; i1 < dstShape1; ++i1) {
-            for (LoopVar i2 = 0; i2 < dstShape2; ++i2) {
-                for (LoopVar i3 = 0; i3 < dstShape3; ++i3) {
-                    for (LoopVar i4 = 0; i4 < dstShape4; ++i4) {
-                        int64_t dstCoord[5] = {
-                            static_cast<int64_t>(i0), static_cast<int64_t>(i1), static_cast<int64_t>(i2),
-                            static_cast<int64_t>(i3), static_cast<int64_t>(i4)};
-                        int64_t srcCoord[5] = {0, 0, 0, 0, 0};
-                        srcCoord[axis0] = dstCoord[0];
-                        srcCoord[axis1] = dstCoord[1];
-                        srcCoord[axis2] = dstCoord[2];
-                        srcCoord[axis3] = dstCoord[3];
-                        srcCoord[axis4] = dstCoord[4];
-                        auto dstOffset =
-                            i0 * dstStride0 + i1 * dstStride1 + i2 * dstStride2 + i3 * dstStride3 + i4 * dstStride4;
-                        auto srcOffset = srcCoord[0] * srcStride0 + srcCoord[1] * srcStride1 + srcCoord[2] * srcStride2 +
-                                         srcCoord[3] * srcStride3 + srcCoord[4] * srcStride4;
-                        dstAddr[dstOffset] = srcAddr[srcOffset];
+        set_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
+        wait_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
+    } else if constexpr (dimCount == 5) {
+        auto d0 = dstLayout.template GetShapeDim<0, 5>();
+        auto d1 = dstLayout.template GetShapeDim<1, 5>();
+        auto d2 = dstLayout.template GetShapeDim<2, 5>();
+        auto d3 = dstLayout.template GetShapeDim<3, 5>();
+        auto d4 = dstLayout.template GetShapeDim<4, 5>();
+        auto ds0 = dstLayout.template GetStrideDim<0, 5>();
+        auto ds1 = dstLayout.template GetStrideDim<1, 5>();
+        auto ds2 = dstLayout.template GetStrideDim<2, 5>();
+        auto ds3 = dstLayout.template GetStrideDim<3, 5>();
+        auto ds4 = dstLayout.template GetStrideDim<4, 5>();
+        for (LoopVar i0 = 0; i0 < d0; ++i0) {
+            __ubuf__ dstType *dst1 = dstAddr + i0 * ds0;
+            for (LoopVar i1 = 0; i1 < d1; ++i1) {
+                __ubuf__ dstType *dst2 = dst1 + i1 * ds1;
+                for (LoopVar i2 = 0; i2 < d2; ++i2) {
+                    __ubuf__ dstType *dst3 = dst2 + i2 * ds2;
+                    for (LoopVar i3 = 0; i3 < d3; ++i3) {
+                        __ubuf__ dstType *dst4 = dst3 + i3 * ds3;
+                        int64_t sc[5] = {0, 0, 0, 0, 0};
+                        sc[axis0] = static_cast<int64_t>(i0);
+                        sc[axis1] = static_cast<int64_t>(i1);
+                        sc[axis2] = static_cast<int64_t>(i2);
+                        sc[axis3] = static_cast<int64_t>(i3);
+                        sc[axis4] = 0;
+                        auto gmOff = sc[0] * srcStride0 + sc[1] * srcStride1 + sc[2] * srcStride2 + sc[3] * srcStride3 + sc[4] * srcStride4;
+                        TileDefine dstTile(1, d4);
+                        GlobalData srcGlobal(srcAddr + gmOff,
+                            pto::Shape(1, 1, 1, 1, d4), pto::Stride(0, 0, 0, 0, srcStride3));
+                        pto::TASSIGN(dstTile, (uint64_t)dst4);
+                        pto::TLOAD(dstTile, srcGlobal);
                     }
                 }
             }
         }
+        set_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
+        wait_flag(PIPE_S, PIPE_MTE2, EVENT_ID7);
     }
-}
-
-template <int axis0, int axis1, int axis2, int axis3, int axis4, int dimCount, typename T0, typename T1, typename T2>
-TILEOP void Tpermute(T0 dst, T1 src, T2 tmp) {
-    (void)tmp;
-    const auto dstLayout = dst.GetLayout();
-    const auto srcLayout = src.GetLayout();
-    __ubuf__ typename T0::Type *dstAddr = (__ubuf__ typename T0::Type *)((uint64_t)(dst.GetAddr()));
-    __ubuf__ typename T1::Type *srcAddr = (__ubuf__ typename T1::Type *)((uint64_t)(src.GetAddr()));
-
-    if constexpr (dimCount == 2 && axis0 == 1 && axis1 == 0) {
-        constexpr size_t expectSize = 2;
-        auto dstShape0 = dstLayout.template GetShapeDim<0, expectSize>();
-        auto srcShape0 = srcLayout.template GetShapeDim<0, expectSize>();
-        auto dstStride0 = dstLayout.template GetStrideDim<0, expectSize>();
-        auto srcStride0 = srcLayout.template GetStrideDim<0, expectSize>();
-        constexpr auto dstTileH = TileOp::GetTensorTileShapeDim<T0, 0, expectSize>();
-        constexpr auto dstTileW = TileOp::GetTensorTileShapeDim<T0, 1, expectSize>();
-        constexpr auto srcTileH = TileOp::GetTensorTileShapeDim<T1, 0, expectSize>();
-        constexpr auto srcTileW = TileOp::GetTensorTileShapeDim<T1, 1, expectSize>();
-        using DstTileDefine =
-            pto::Tile<pto::TileType::Vec, typename T0::Type, dstTileH, dstTileW, pto::BLayout::RowMajor, -1, -1>;
-        using SrcTileDefine =
-            pto::Tile<pto::TileType::Vec, typename T1::Type, srcTileH, srcTileW, pto::BLayout::RowMajor, -1, -1>;
-        constexpr unsigned tmpTileW = (sizeof(typename T0::Type) == 1) ? 32 : 16;
-        using TmpTileDefine2D =
-            pto::Tile<pto::TileType::Vec, typename T0::Type, dstTileH, tmpTileW, pto::BLayout::RowMajor, dstTileH, tmpTileW>;
-        DstTileDefine dstTile(dstLayout.template GetShapeDim<0, expectSize>(), dstLayout.template GetShapeDim<1, expectSize>());
-        SrcTileDefine srcTile(srcLayout.template GetShapeDim<0, expectSize>(), srcLayout.template GetShapeDim<1, expectSize>());
-        TmpTileDefine2D tmpTile;
-        pto::TASSIGN(tmpTile, (uint64_t)(tmp.GetAddr()));
-        for (LoopVar i0 = 0; i0 < dstShape0; ++i0) {
-            auto dstOffset = i0 * dstStride0;
-            auto srcOffset = i0 * srcStride0;
-            pto::TASSIGN(dstTile, (uint64_t)(dst.GetAddr() + dstOffset * sizeof(typename T0::Type)));
-            pto::TASSIGN(srcTile, (uint64_t)(src.GetAddr() + srcOffset * sizeof(typename T1::Type)));
-            pto::TTRANS(dstTile, srcTile, tmpTile);
-        }
-    } else if constexpr (dimCount == 4) {
-        PermuteCopy4DScalar<typename T0::Type, typename T1::Type>(dstAddr, srcAddr, dstLayout, srcLayout, axis0,
-            axis1, axis2, axis3);
-    } else if constexpr (dimCount == 5) {
-        PermuteCopy5DScalar<typename T0::Type, typename T1::Type>(dstAddr, srcAddr, dstLayout, srcLayout, axis0,
-            axis1, axis2, axis3, axis4);
-    }
-    set_flag(PIPE_S, PIPE_V, EVENT_ID7);
-    wait_flag(PIPE_S, PIPE_V, EVENT_ID7);
 }
 
 #endif
