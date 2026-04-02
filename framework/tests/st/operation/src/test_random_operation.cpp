8d455936ce4bd0b55e9636e455fd33ec0f80aaa0
raoliang_sac <raoliang4@huawei.com>
2026-04-02 15:13:46 +0800

    feat(Operation): Add new opeartion PhiloxRandom

--- framework/tests/st/operation/src/test_random_operation.cpp
+++ framework/tests/st/operation/src/test_random_operation.cpp
@@ -9,8 +9,8 @@
  */
 
 /*!
- * \file test_random_operation.cpp
- * \brief Test Random operation
+ * \file test_uniform_operation.cpp
+ * \brief Test Uniform operation
  */
 
 #include "test_operation.h"
@@ -19,16 +19,14 @@
 using namespace tile_fwk::test_operation;
 namespace {
 
-static std::vector<uint64_t> SkipCounter(const std::vector<uint64_t> &counter, uint64_t offset) {
-    std::vector<uint64_t> result = counter;
-    
+static void SkipCounter(uint64_t &counter0, uint64_t &counter1, uint64_t offset) {
     uint32_t offsetLo = static_cast<uint32_t>(offset);
     uint32_t offsetHi = static_cast<uint32_t>(offset >> 32);
     
-    uint32_t c0 = static_cast<uint32_t>(result[0] & 0xFFFFFFFF);
-    uint32_t c1 = static_cast<uint32_t>(result[0] >> 32);
-    uint32_t c2 = static_cast<uint32_t>(result[1] & 0xFFFFFFFF);
-    uint32_t c3 = static_cast<uint32_t>(result[1] >> 32);
+    uint32_t c0 = static_cast<uint32_t>(counter0 & 0xFFFFFFFF);
+    uint32_t c1 = static_cast<uint32_t>(counter0 >> 32);
+    uint32_t c2 = static_cast<uint32_t>(counter1 & 0xFFFFFFFF);
+    uint32_t c3 = static_cast<uint32_t>(counter1 >> 32);
     
     c0 += offsetLo;
     if (c0 < offsetLo) {
@@ -41,37 +39,36 @@ static std::vector<uint64_t> SkipCounter(const std::vector<uint64_t> &counter, u
         }
     }
     
-    result[0] = static_cast<uint64_t>(c0) | (static_cast<uint64_t>(c1) << 32);
-    result[1] = static_cast<uint64_t>(c2) | (static_cast<uint64_t>(c3) << 32);
-    
-    return result;
+    counter0 = static_cast<uint64_t>(c0) | (static_cast<uint64_t>(c1) << 32);
+    counter1 = static_cast<uint64_t>(c2) | (static_cast<uint64_t>(c3) << 32);
 }
 
-struct RandomOpFuncArgs : public OpFuncArgs {
-    RandomOpFuncArgs(uint64_t key, const std::vector<uint64_t> &counter, 
-                     const std::vector<int64_t> &viewShape, const std::vector<int64_t> tileShape, uint16_t rounds)
-        : key_(key), counter_(counter), viewShape_(viewShape), tileShape_(tileShape), rounds_(rounds) {}
+struct UniformOpFuncArgs : public OpFuncArgs {
+    UniformOpFuncArgs(uint64_t key, uint64_t counter0, uint64_t counter1, 
+                       const std::vector<int64_t> &viewShape, const std::vector<int64_t> tileShape, uint16_t rounds)
+        : key_(key), counter0_(counter0), counter1_(counter1), viewShape_(viewShape), tileShape_(tileShape), rounds_(rounds) {}
 
     uint64_t key_;
-    std::vector<uint64_t> counter_;
+    uint64_t counter0_;
+    uint64_t counter1_;
     std::vector<int64_t> viewShape_;
     std::vector<int64_t> tileShape_;
     uint16_t rounds_;
 };
 
-struct RandomOpMetaData {
-    explicit RandomOpMetaData(const OpFunc &opFunc, const nlohmann::json &test_data)
+struct UniformOpMetaData {
+    explicit UniformOpMetaData(const OpFunc &opFunc, const nlohmann::json &test_data)
         : opFunc_(opFunc), test_data_(test_data) {}
 
     OpFunc opFunc_;
     nlohmann::json test_data_;
 };
 
-static void RandomOperationExeFunc(
+static void UniformOperationExeFunc(
     [[maybe_unused]] const std::vector<Tensor> &inputs, std::vector<Tensor> &outputs, const OpFuncArgs *opArgs) {
     
     FUNCTION("main", {}, {outputs[0]}) {
-        auto args = static_cast<const RandomOpFuncArgs *>(opArgs);
+        auto args = static_cast<const UniformOpFuncArgs *>(opArgs);
         
         const int64_t outputDim = outputs[0].GetShape()[0];
         const int64_t viewShape = args->viewShape_[0];
@@ -84,20 +81,21 @@ static void RandomOperationExeFunc(
             int64_t validShape = std::min(outputDim - tileIdx * viewShape, viewShape);
             
             uint64_t tileOffset = static_cast<uint64_t>(tileIdx * viewShape);
-            std::vector<uint64_t> tileCounter = SkipCounter(args->counter_, tileOffset);
+            uint64_t tileCounter0 = args->counter0_;
+            uint64_t tileCounter1 = args->counter1_;
+            SkipCounter(tileCounter0, tileCounter1, tileOffset);
             
-            // 打印tileCounter的值
-            std::cout << "tileIdx: " << tileIdx << ", tileOffset: " << tileOffset << ", key: " << args->key_ << ", tileCounter: [";
-            for (size_t i = 0; i < tileCounter.size(); ++i) {
-                std::cout << tileCounter[i];
-                if (i < tileCounter.size() - 1) {
-                    std::cout << ", ";
-                }
-            }
-            std::cout << "]" << std::endl;
+            std::cout << "tileIdx: " << tileIdx << ", tileOffset: " << tileOffset 
+                      << ", key: " << args->key_ 
+                      << ", tileCounter0: " << tileCounter0 
+                      << ", tileCounter1: " << tileCounter1 << std::endl;
             
             TileShape::Current().SetVecTile(args->tileShape_);
-            auto res = Random(args->key_, tileCounter, {validShape}, args->rounds_);
+            auto res = Uniform(Element(DT_UINT64, args->key_), 
+                              Element(DT_UINT64, tileCounter0), 
+                              Element(DT_UINT64, tileCounter1), 
+                              {validShape}, 
+                              Element(DT_UINT16, args->rounds_));
             Assemble(res, {offset}, outputs[0]);
             
             ++tileIdx;
@@ -105,13 +103,13 @@ static void RandomOperationExeFunc(
     }
 }
 
-class RandomOperationTest : public npu::tile_fwk::stest::TestSuite_STest_Ops_Aihac_param<RandomOpMetaData> {};
+class UniformOperationTest : public npu::tile_fwk::stest::TestSuite_STest_Ops_Aihac_param<UniformOpMetaData> {};
 
-INSTANTIATE_TEST_SUITE_P(TestRandom, RandomOperationTest,
-    ::testing::ValuesIn(GetOpMetaData<RandomOpMetaData>({RandomOperationExeFunc}, "Random")));
+INSTANTIATE_TEST_SUITE_P(TestUniform, UniformOperationTest,
+    ::testing::ValuesIn(GetOpMetaData<UniformOpMetaData>({UniformOperationExeFunc}, "Uniform")));
 
-TEST_P(RandomOperationTest, TestRandom) {
-    auto testCase = CreateTestCaseDesc<RandomOpMetaData>(GetParam(), nullptr);
+TEST_P(UniformOperationTest, TestUniform) {
+    auto testCase = CreateTestCaseDesc<UniformOpMetaData>(GetParam(), nullptr);
     nlohmann::json test_data = GetParam().test_data_;
     
     auto viewShape = GetViewShape(test_data);
@@ -122,11 +120,10 @@ TEST_P(RandomOperationTest, TestRandom) {
     
     uint64_t key = GetValueByName<uint64_t>(test_data, "key");
     
-    uint64_t counter_0 = GetValueByName<uint64_t>(test_data, "counter_0");
-    uint64_t counter_1 = GetValueByName<uint64_t>(test_data, "counter_1");
-    std::vector<uint64_t> counter = {counter_0, counter_1};
+    uint64_t counter0 = GetValueByName<uint64_t>(test_data, "counter_0");
+    uint64_t counter1 = GetValueByName<uint64_t>(test_data, "counter_1");
     
-    auto args = RandomOpFuncArgs(key, counter, viewShape, tileShape, rounds);
+    auto args = UniformOpFuncArgs(key, counter0, counter1, viewShape, tileShape, rounds);
     testCase.args = &args;
     TestExecutor::runTest(testCase);
 }
