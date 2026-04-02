267a1e6a0c15c480f85c181a33fb99d6c0ab25b5
m00896683 <mayuan33@huawei.com>
2026-04-02 22:11:10 +0800

    fix precision issues in the var opeartor

--- framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/scheduler.h
@@ -261,7 +261,7 @@ private:
     Status UpdateAssembleBuffer(SpillInfo &spillInfo, LocalBufferPtr allocBuffer, LogicalTensorPtr assembleTensor);
     LogicalTensorPtr CreateAssemblePartTensor(LogicalTensorPtr iOperand, LogicalTensorPtr assembleTensor,
         SpillInfo &spillInfo, std::shared_ptr<AssembleOpAttribute> assembleAttr);
-    int64_t CalcWorkspaceOffset(std::vector<int64_t> shape, std::vector<int64_t> offset);
+    int64_t CalcWorkspaceOffset(std::vector<int64_t> shape, std::vector<int64_t> offset, DataType dataType);
     void GetWorkspaceBaseOffset(LogicalTensorPtr ddrTensor, int64_t& base);
     Status UpdateCopyOutMode(Operation& copyOutOp);
     Status UpdateCopyInMode(Operation& copyInOp);
