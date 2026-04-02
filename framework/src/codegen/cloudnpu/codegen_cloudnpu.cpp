9b41f417ac9faba721f3dc5278a13f663da05f76
lllzzzddd <liuzidi1@huawei.com>
2026-04-02 15:20:48 +0800

    feat(vf): add vf fusion options default open

--- framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_cloudnpu.cpp
@@ -574,13 +574,22 @@ void CodeGenCloudNPU::BuildIncludes(std::ostringstream& oss) const
     }
 }
 
-void CodeGenCloudNPU::AppendVFOptions(NPUArch platform, std::ostringstream& oss)
-{
-    if (platform == NPUArch::DAV_3510 && config::GetPassGlobalConfig(KEY_ENABLE_VF, false)) {
-        oss << "--enable-pto-tile-fusion "
-            << "-mllvm --tile-fusion-skip-shape-inference=true "
-            << "-mllvm --tile-fusion-skip-reduceop-fusion=false "
-            << "-mllvm --tile-fusion-skip-legality-check=false ";
+void CodeGenCloudNPU::AppendVFOptions(NPUArch platform, std::ostringstream &oss) {
+    if (platform != NPUArch::DAV_3510) {
+        return;
+    }
+
+    if (!config::GetPassGlobalConfig(KEY_ENABLE_VF, true)) {
+        oss << "--cce-simd-vf-fusion=false ";
+        return;
+    }
+
+    oss << "--enable-pto-tile-fusion "
+        << "-mllvm --tile-fusion-skip-shape-inference=true "
+        << "-mllvm --tile-fusion-skip-reduceop-fusion=false "
+        << "-mllvm --tile-fusion-skip-legality-check=false ";
+    if (config::GetPassGlobalConfig(KEY_ENABLE_VF_UNROLL, false)) {
+        oss << "-mllvm -enable-unroll-after-fused=true ";
     }
 }
 
