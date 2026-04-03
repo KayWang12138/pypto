5b6974ede81d92106f33e03813f63641637b243a
zhaojunhan <zhaojunhan1@huawei.com>
2026-04-03 14:42:14 +0800

    fix(function): Fix security

--- framework/src/interface/configs/config_manager_ng.cpp
+++ framework/src/interface/configs/config_manager_ng.cpp
@@ -47,7 +47,7 @@ struct TypeInfo {
     void LoadConf(const std::string& path)
     {
         std::ifstream infile(path);
-        FUNCTION_ASSERT(FError::BAD_FD, infile.is_open()) << "Open file " << path << " failed";
+        FUNCTION_ASSERT(FError::BAD_FD, infile.is_open()) << "Open file " << path << " failed.";
         nlohmann::json jData;
         infile >> jData;
 
@@ -135,7 +135,7 @@ const Any& ConfigScope::GetAnyConfig(const std::string& key) const
         if (parent_) {
             return parent_->GetAnyConfig(key);
         }
-        throw std::runtime_error("Config " + key + " not found");
+        throw std::runtime_error("Config " + key + " not found.");
     }
     return values_.at(key);
 }
@@ -148,7 +148,7 @@ bool ConfigScope::HasConfig(const std::string& key) const
 void ConfigScope::Clear()
 {
     values_.clear();
-    FUNCTION_LOGD("Clear config scope successfully");
+    FUNCTION_LOGD("Clear config scope successfully.");
 }
 
 const std::type_info& ConfigScope::Type(const std::string& key) const
@@ -235,10 +235,13 @@ void DumpRange(
 {
     os << "Range: ";
     if (type == typeid(std::map<int64_t, int64_t>)) {
-        os << "{[" << rangeInfos.at(key + "_key").first << ", " << rangeInfos.at(key + "_key").second << "], ["
-           << rangeInfos.at(key + "_val").first << ", " << rangeInfos.at(key + "_val").second << "]}";
+        os << "{[" << rangeInfos.at(key + "_key").first << ", " 
+           << rangeInfos.at(key + "_key").second << "], ["
+           << rangeInfos.at(key + "_val").first << ", " 
+           << rangeInfos.at(key + "_val").second << "]}";
     } else {
-        os << "[" << rangeInfos.at(key).first << ", " << rangeInfos.at(key).second << "]";
+        os << "[" << rangeInfos.at(key).first << ", " 
+           << rangeInfos.at(key).second << "]";
     }
 }
 
@@ -279,7 +282,7 @@ void ConfigScope::UpdateValueWithAny(const std::string& key, Any value)
         std::map<std::string, Any> node;
         node[key] = value;
         DumpValues(os, node, "");
-        os << ", its value doesn't within the value range.";
+        os << ", its value doesn't within the value range. ";
         DumpRange(os, value.Type(), key, ConfigManagerNg::GetInstance().Range());
         os << "\n";
         FUNCTION_ASSERT(FError::INVALID_VAL, false) << os.str();
@@ -312,7 +315,7 @@ struct ConfigManagerImpl {
     void PushScope(ConfigScopePtr scope)
     {
         // Ensure the provided scope is not null
-        FUNCTION_ASSERT(scope != nullptr) << "Cannot push a null scope";
+        FUNCTION_ASSERT(scope != nullptr) << "Cannot push a null scope.";
         scopes.push(scope);
     }
 
@@ -352,7 +355,7 @@ struct ConfigManagerImpl {
     void EndScope(const char* file, int lino)
     {
         /* at least default and global two levels */
-        FUNCTION_ASSERT(scopes.size() >= 0x2) << "No scope to pop";
+        FUNCTION_ASSERT(scopes.size() >= 0x2) << "No scope to pop.";
         auto& scope = scopes.top();
         scope->end_file_ = file;
         scope->end_lino_ = lino;
@@ -383,7 +386,7 @@ struct ConfigManagerImpl {
         for (auto& it : values) {
             try {
                 root->AddValue(it.first, it.second);
-                FUNCTION_LOGD("Set option successfully. Key: %s", it.first.c_str());
+                FUNCTION_LOGD("Set option successfully, Key: %s", it.first.c_str());
             } catch (const std::exception& e) {
                 FUNCTION_LOGE_E(
                     FError::INVALID_VAL, "Failed to set option. Key: %s, Error: %s", it.first.c_str(), e.what());
@@ -464,7 +467,7 @@ private:
             confPath = GetConfDir() + "tile_fwk_config.json";
         }
         std::ifstream ifs(confPath);
-        CHECK(ifs.is_open()) << "Open file " << confPath << " failed";
+        CHECK(ifs.is_open()) << "Open file: " << confPath << " failed";
         nlohmann::json jData;
         ifs >> jData;
         LoadConf(jData, "");
