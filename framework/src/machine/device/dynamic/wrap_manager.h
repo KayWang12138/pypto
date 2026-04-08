c78367e606a70d648e8d50ec0b154c47fd80d1a1
xuyi_37 <xuyi37@huawei.com>
2026-04-08 09:18:37 +0800

    perf(machine): mix optimization

--- framework/src/machine/device/dynamic/wrap_manager.h
+++ framework/src/machine/device/dynamic/wrap_manager.h
@@ -464,30 +464,44 @@ public:
         return true;
     }
 
+    inline WrapInfo* findWrapInfo(uint32_t start, uint32_t end, uint32_t wrapId)
+    {
+        for (uint32_t idx = start; idx < end; idx++) {
+            if (readyWrapCoreFunctionQue_->elem[idx].wrapId == wrapId) {
+                return &readyWrapCoreFunctionQue_->elem[idx];
+            }
+        }
+        return nullptr;
+    }
+
     inline void PushTaskToTasklist(uint32_t wrapId, uint32_t taskId, uint32_t taskIdx)
     {
-        WrapInfo* wrapInfo = nullptr;
-        WrapInfoQueueLock(readyWrapCoreFunctionQue_);
-        for (uint32_t idx = 0; idx < readyWrapCoreFunctionQue_->tail; idx++) {
-            if (readyWrapCoreFunctionQue_->elem[idx].wrapId == wrapId) {
-                wrapInfo = &readyWrapCoreFunctionQue_->elem[idx];
-                break;
-            }
+        uint32_t tail = __atomic_load_n(&readyWrapCoreFunctionQue_->tail, __ATOMIC_RELAXED);
+        WrapInfo* wrapInfo = findWrapInfo(0, tail, wrapId);
+        if (wrapInfo != nullptr) {
+            wrapInfo->tasklist[taskIdx] = taskId;
+            return;
         }
 
-        if (wrapInfo == nullptr) {
-            // add a new wrapinfo
-            wrapInfo = &readyWrapCoreFunctionQue_->elem[readyWrapCoreFunctionQue_->tail];
-            wrapInfo->wrapId = wrapId;
-            wrapInfo->mixResourceType = GetMixResourceType(taskId);
-            for (uint32_t i = 0; i < MAX_WRAP_TASK_NUM; i++) {
-                wrapInfo->tasklist[i] = AICORE_TASK_INIT;
-                wrapInfo->aicoreIdxList[i] = 0;
-            }
-            __atomic_fetch_add(&readyWrapCoreFunctionQue_->tail, 1, std::memory_order_release);
+        WrapInfoQueueLock(readyWrapCoreFunctionQue_);
+        wrapInfo = findWrapInfo(tail, readyWrapCoreFunctionQue_->tail, wrapId);
+        if (unlikely(wrapInfo != nullptr)) {
+            WrapInfoQueueUnLock(readyWrapCoreFunctionQue_);
+            wrapInfo->tasklist[taskIdx] = taskId;
+            return;
         }
+
+        // add a new wrapinfo
+        wrapInfo = &readyWrapCoreFunctionQue_->elem[readyWrapCoreFunctionQue_->tail];
+        __atomic_fetch_add(&readyWrapCoreFunctionQue_->tail, 1, std::memory_order_release);
         WrapInfoQueueUnLock(readyWrapCoreFunctionQue_);
 
+        wrapInfo->wrapId = wrapId;
+        wrapInfo->mixResourceType = GetMixResourceType(taskId);
+        for (uint32_t i = 0; i < MAX_WRAP_TASK_NUM; i++) {
+            wrapInfo->tasklist[i] = AICORE_TASK_INIT;
+            wrapInfo->aicoreIdxList[i] = 0;
+        }
         wrapInfo->tasklist[taskIdx] = taskId;
     }
 
@@ -534,8 +548,9 @@ public:
         WrapInfo* wrapInfo = nullptr;
         uint32_t wrapIdx = 0;
         for (uint32_t idx = wrapQueueForThread_.head; idx < wrapQueueForThread_.tail; idx++) {
-            if (reinterpret_cast<WrapInfo*>(wrapQueueForThread_.elem[idx])->wrapId == wrapId) {
-                wrapInfo = reinterpret_cast<WrapInfo*>(wrapQueueForThread_.elem[idx]);
+            auto tmpInfo = reinterpret_cast<WrapInfo*>(wrapQueueForThread_.elem[idx]);
+            if (tmpInfo->wrapId == wrapId) {
+                wrapInfo = tmpInfo;
                 wrapIdx = idx;
                 break;
             }
@@ -551,8 +566,9 @@ public:
         wrapInfo->tasklist[taskIdx] = AICORE_TASK_STOP;
 
         CoreType coreType = (taskIdx == WRAP_IDX_AIC) ? CoreType::AIC : CoreType::AIV;
-        AddRunReadyCoreIdxForWrap(wrapInfo->aicoreIdxList[taskIdx], coreType); // free wrap core
-        wrapCoreAvail_[wrapInfo->aicoreIdxList[taskIdx]] = true;
+        uint32_t coreIdx = wrapInfo->aicoreIdxList[taskIdx];
+        AddRunReadyCoreIdxForWrap(coreIdx, coreType); // free wrap core
+        wrapCoreAvail_[coreIdx] = true;
 
         if (IsMixTaskFinish(wrapInfo)) { // all tasks for this wrap finish
             DEV_VERBOSE_DEBUG("wrapId %u 's all tasks finish, release wrapcore", wrapId);
