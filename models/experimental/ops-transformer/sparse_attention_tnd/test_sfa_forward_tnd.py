12cca13c1c4fd41047c0a4ba21af733bcc32be2a
sunhaoxuan2024 <sunhaoxuan3@h-partners.com>
2026-04-08 15:31:52 +0800

    feat(operator): Add Sparse Flash Attention Grad

--- models/experimental/ops-transformer/sparse_attention_tnd/test_sfa_forward_tnd.py
+++ models/experimental/ops-transformer/sparse_attention_tnd/test_sfa_forward_tnd.py
@@ -299,6 +299,25 @@ def get_case_config(case_name: str):
             [4096],
             2048
         ),
+        "sfa_tnd_v2_bf16_b1_s1k_seq_32k": (
+            1, 2, 1,
+            [1024],
+            [32768],
+            2048
+        ),
+        "sfa_tnd_v2_bf16_b1_s256_seq_32k": (
+            1, 64, 1,
+            [256],
+            [32768],
+            2048
+        ),
+        "sfa_tnd_v2_bf16_b1_s128_seq_32k": (
+            1, 32, 1,
+            [128],
+            [32768],
+            2048
+        )
+        
     }
     return test_case_config.get(case_name)
 
@@ -357,8 +376,23 @@ def test_sfa_tnd_v2_bf16_b4_s4_seq128k():
     do_test_sfa_tnd_entry("sfa_tnd_v2_bf16_b4_s4_seq128K")
 
 
+@pytest.mark.skip(reason="large test case")
 def test_sfa_tnd_v2_bf16_b1_seq_4k():
     do_test_sfa_tnd_entry("sfa_tnd_v2_bf16_b1_seq_4k")
+    
+
+def test_sfa_tnd_v2_bf16_b1_s1k_seq_32k():
+    do_test_sfa_tnd_entry("sfa_tnd_v2_bf16_b1_s1k_seq_32k")
+    
+
+@pytest.mark.skip(reason="large test case")    
+def test_sfa_tnd_v2_bf16_b1_s256_seq_32k():
+    do_test_sfa_tnd_entry("sfa_tnd_v2_bf16_b1_s256_seq_32k")
+
+
+@pytest.mark.skip(reason="large test case")    
+def test_sfa_tnd_v2_bf16_b1_s128_seq_32k():
+    do_test_sfa_tnd_entry("sfa_tnd_v2_bf16_b1_s128_seq_32k")
 
 
 if __name__ == "__main__":
@@ -366,5 +400,5 @@ if __name__ == "__main__":
         format='%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s: %(message)s',
         level=logging.INFO
     )
-    test_sfa_tnd_v2_bf16_b1_seq_4k()
+    test_sfa_tnd_v2_bf16_b1_s1k_seq_32k()
 
