40b5b0f40eb3977bf9f2a8719c48c83097a75203
e <zhengyao12@huawei.com>
2026-04-09 14:34:33 +0800

    feat(operator): Add a case for performance

--- models/deepseek_v32_exp/deepseekv32_lightning_indexer_quant.py
+++ models/deepseek_v32_exp/deepseekv32_lightning_indexer_quant.py
@@ -395,6 +395,7 @@ def test_lightning_indexer_topk_quant_4_b_2_s1_64k_s2():
 def test_lightning_indexer_topk_quant_8_b_2_s1_64k_s2():
     lightning_indexer("LightningIndexerSTest.lightning_indexer_quant_8_b_2_s1_64k_s2")
 
+
 @pytest.mark.skip(reason="large shape")
 def test_lightning_indexer_topk_quant_4_b_2_s1_64k_s2_perf():
     lightning_indexer("LightningIndexerSTest.lightning_indexer_quant_4_b_2_s1_64k_s2_perf")
