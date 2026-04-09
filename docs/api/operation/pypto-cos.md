3a3dd5abc7c4708591ce0735e07d86746b369e08
dingsinuo <dingsinuo@huawei.com>
2026-04-09 17:17:19 +0800

    feat(operation): Add the Sin/Cos methods

--- docs/api/operation/pypto-cos.md
+++ docs/api/operation/pypto-cos.md
@@ -23,7 +23,7 @@ cos(input: Tensor) -> Tensor
 
 | 参数名 | 输入/输出 | 说明                                                                 |
 |--------|-----------|----------------------------------------------------------------------|
-| input  | 输入      | 源操作数。 <br> 支持的数据类型为：DT_FP32。 <br> 不支持空Tensor；Shape Size不大于2147483647（即INT32_MAX）。 |
+| input  | 输入      | 源操作数。 <br> 支持的数据类型为：DT_FP32, DT_FP16。 <br> 不支持空Tensor；Shape Size不大于2147483647（即INT32_MAX）。 |
 
 ## 返回值说明
 
@@ -31,6 +31,19 @@ cos(input: Tensor) -> Tensor
 
 ## 调用示例
 
+### TileShape设置示例
+
+调用该operation接口前，应通过set_vec_tile_shapes设置TileShape。
+
+TileShape维度应和输出一致。
+
+如输入intput shape为[m, n]，输出为[m, n]，TileShape设置为[m1, n1]，则m1, n1分别用于切分m, n轴。
+
+```python
+pypto.set_vec_tile_shapes(4, 16)
+```
+
+### 接口调用示例
 ```python
 x = pypto.tensor([4], pypto.DT_FP32)
 y = pypto.cos(x)
---
+++ framework/tests/ut/codegen/src/test_dynamic/test_codegen_dyn_vector/test_codegen_dyn_sincos.cpp
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_codegen_dyn_sincos.cpp
+ * \brief Unit test for codegen.
+ */
+
+#include "gtest/gtest.h"
+
+#include "interface/tensor/logical_tensor.h"
+#include "tilefwk/tilefwk.h"
+#include "interface/inner/tilefwk.h"
+#include "interface/configs/config_manager.h"
+#include "interface/operation/operation.h"
+#include "tilefwk/data_type.h"
+#include "codegen/codegen.h"
+#include "codegen/symbol_mgr/codegen_symbol.h"
+#include "codegen/cloudnpu/codegen_cloudnpu.h"
+#include "codegen/cloudnpu/codegen_op_cloudnpu.h"
+#include "test_codegen_utils.h"
+#include "test_codegen_common.h"
+
+namespace npu::tile_fwk {
+
+class TestCodegenDynSinCos : public ::testing::Test {
+public:
+    static void SetUpTestCase() {}
+
+    static void TearDownTestCase() {}
+
+    void SetUp() override
+    {
+        config::Reset();
+        Program::GetInstance().Reset();
+        config::SetHostOption(COMPILE_STAGE, CS_EXECUTE_GRAPH);
+        config::SetPlatformConfig(KEY_ENABLE_COST_MODEL, false);
+        IdGen<IdType::FUNCTION>::Inst().SetId(DummyFuncMagic);
+    }
+
+    void TearDown() override {}
+};
+
+TEST_F(TestCodegenDynSinCos, SinLayout)
+{
+    MockFuncDynUnaryConf config;
+    config.dtype = DT_FP32;
+    auto function =
+        GenMockFuncDynUnary("Sin", config, [](Tensor& input, Tensor& output) { output = Sin(input); });
+
+    npu::tile_fwk::CodeGenCtx ctx;
+    npu::tile_fwk::CodeGenCloudNPU codeGen(ctx);
+    codeGen.GenCode(*function, {});
+
+    std::string res = GetResultFromCpp(*function);
+    std::string expect = R"(TMulS<LastUse2Dim<0, 0>, float>(ubTensor_2, ubTensor_0, 0.318309873);)";
+    CheckStringExist(expect, res);
+}
+
+TEST_F(TestCodegenDynSinCos, CosLayout)
+{
+    MockFuncDynUnaryConf config;
+    config.dtype = DT_FP32;
+    auto function =
+        GenMockFuncDynUnary("Cos", config, [](Tensor& input, Tensor& output) { output = Cos(input); });
+
+    npu::tile_fwk::CodeGenCtx ctx;
+    npu::tile_fwk::CodeGenCloudNPU codeGen(ctx);
+    codeGen.GenCode(*function, {});
+
+    std::string res = GetResultFromCpp(*function);
+    std::string expect = R"(TMulS<LastUse2Dim<0, 0>, float>(ubTensor_2, ubTensor_0, 0.318309873);)";
+    CheckStringExist(expect, res);
+}
+
+} // namespace npu::tile_fwk
