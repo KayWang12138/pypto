c1634f517e10be25f98755df48cb4c108095f6e6
Bee.Hujun <Bee.hujun@huawei.com>
2026-04-10 15:16:50 +0800

    feat(codegen): Support spill with reshape

--- framework/src/codegen/cloudnpu/codegen_mte_gather.cpp
+++ framework/src/codegen/cloudnpu/codegen_mte_gather.cpp
@@ -23,10 +23,10 @@
 namespace npu::tile_fwk {
 std::string CodeGenOpCloudNPU::PrintGatherInL1TileTensor() const
 {
-    std::string srcVar = sm->QueryTileTensorNameByBufVar(GenGmParamVar(ID1));
-    std::string offsetsVar = sm->QueryTileTensorNameByBufVar(GenGmParamVar(ID2));
-    std::string blockTableVar = sm->QueryTileTensorNameByBufVar(GenGmParamVar(ID3));
-    std::string dstVar = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::DST_IDX));
+    std::string srcVar = QueryTileTensorNameByIdx(ID1);
+    std::string offsetsVar = QueryTileTensorNameByIdx(ID2);
+    std::string blockTableVar = QueryTileTensorNameByIdx(ID3);
+    std::string dstVar = QueryTileTensorNameByIdx(ID0);
     int64_t blockSize = AnyCast<int64_t>(opAttrs.at("op_attr_blocksize"));
 
     auto startOffset = opAttrs.at(OpAttributeKey::startOffset);
@@ -317,9 +317,9 @@ std::string CodeGenOpCloudNPU::PrintGatherInUBLayout() const
     std::string coord4Indices = PrintCoord(indicesDim, coordCpindicesOffset);
     std::string coord4BlockTable = PrintCoord(blockTableDim, coordCpblockTableOffset);
     std::string outputTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::DST_IDX));
-    std::string paramTensor = sm->QueryTileTensorNameByBufVar(GenGmParamVar(ToUnderlying(MISOIdx::SRC0_IDX)));
-    std::string indicesTensor = sm->QueryTileTensorNameByBufVar(GenGmParamVar(ToUnderlying(MISOIdx::SRC1_IDX)));
-    std::string pageTableTensor = sm->QueryTileTensorNameByBufVar(GenGmParamVar(ToUnderlying(MISOIdx::SRC2_IDX)));
+    std::string paramTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::SRC0_IDX));
+    std::string indicesTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::SRC1_IDX));
+    std::string pageTableTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::SRC2_IDX));
     std::vector<std::string> paramList;
     ASSERT(OperErr::ATTRIBUTE_INVALID, opAttrs.find(OpAttributeKey::blockSize) != opAttrs.end())
         << "GenGatherOp: There is nop blockSize attribute here";
