f3a52f43312fb7a86574d4d3a2f9e421e90bbf10
Runze-Geng <gengrunze@huawei.com>
2026-04-13 14:08:52 +0800

    Merge Mix Pass PR

--- framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.cpp
+++ framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.cpp
@@ -901,7 +901,7 @@ Status OoOScheduler::Schedule(
         APASS_LOG_ERROR_F(Elements::Operation, "Init failed!");
         return FAILED;
     }
-    AllocWorkspaceGM(opList)
+    AllocWorkspaceGM(opList);
     // 生成spill指令
     if (GenSpillSchedule() != SUCCESS) {
         APASS_LOG_ERROR_F(Elements::Operation, "GenSpillSchedule failed!");
@@ -931,7 +931,7 @@ Status OoOScheduler::Schedule(
     return SUCCESS;
 }
 
-void OoOScheduler::AllocWorkspaceGM(const std::vector<Operation *> &operations) {
+void OoOScheduler::AllocWorkspaceGM(const std::vector<Operation *> &opList) {
     std::set<int> allocedRawmagic;
     for (auto &inCast : function_.GetIncast()) {
         allocedRawmagic.insert(inCast->tensor->GetRawMagic());
@@ -941,7 +941,7 @@ void OoOScheduler::AllocWorkspaceGM(const std::vector<Operation *> &operations) 
     }
     std::map<int, TileRange> rawMagicRange;
     std::map<int, int64_t> rawMagicOffset;
-    for (auto &op : operations) {
+    for (auto &op : opList) {
         for (auto &iOperand : op->GetIOperands()) {
             if (allocedRawmagic.count(iOperand->tensor->GetRawMagic()) && rawMagicRange.count(iOperand->tensor->GetRawMagic())) {
                 iOperand->memoryrange = rawMagicRange[iOperand->tensor->GetRawMagic()];
