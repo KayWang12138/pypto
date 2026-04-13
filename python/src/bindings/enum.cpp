c86b5b2c266f64353aa4dd378efdc1020092f7ec
gguuaanngg <zhangguangjun2@huawei.com>
2026-04-13 09:38:36 +0800

    feat(Operation): Support Sqrt/Exp high precision

--- python/src/bindings/enum.cpp
+++ python/src/bindings/enum.cpp
@@ -106,6 +106,31 @@ void bind_enum(py::module& m)
         .value("INTRINSIC", DivAlgorithm::DEFAULT)
         .value("HIGH_PRECISION", DivAlgorithm::HIGH_PRECISION)
         .export_values();
+    
+    py::enum_<SqrtAlgorithm>(m, "SqrtAlgorithm")
+        .value("INTRINSIC", SqrtAlgorithm::DEFAULT)
+        .value("HIGH_PRECISION", SqrtAlgorithm::HIGH_PRECISION)
+        .export_values();
+
+    py::enum_<RsqrtAlgorithm>(m, "RsqrtAlgorithm")
+        .value("INTRINSIC", RsqrtAlgorithm::DEFAULT)
+        .value("HIGH_PRECISION", RsqrtAlgorithm::HIGH_PRECISION)
+        .export_values();
+
+    py::enum_<ExpAlgorithm>(m, "ExpAlgorithm")
+        .value("INTRINSIC", ExpAlgorithm::DEFAULT)
+        .value("HIGH_PRECISION", ExpAlgorithm::HIGH_PRECISION)
+        .export_values();
+
+    py::enum_<LogAlgorithm>(m, "LogAlgorithm")
+        .value("INTRINSIC", LogAlgorithm::DEFAULT)
+        .value("HIGH_PRECISION", LogAlgorithm::HIGH_PRECISION)
+        .export_values();
+
+    py::enum_<RecipAlgorithm>(m, "RecipAlgorithm")
+        .value("INTRINSIC", RecipAlgorithm::DEFAULT)
+        .value("HIGH_PRECISION", RecipAlgorithm::HIGH_PRECISION)
+        .export_values();
 
     py::enum_<TileType>(m, "TileType")
         .value("VEC", TileType::VEC)
