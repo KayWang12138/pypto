b0c3e6ef556955c32bd273d07107e8ef5334cbd2
gguuaanngg <zhangguangjun2@huawei.com>
2026-04-13 09:38:36 +0800

    feat(Operation): Support Sqrt/Exp high precision

--- python/pypto/tensor.py
+++ python/pypto/tensor.py
@@ -752,8 +752,8 @@ class Tensor:
         return pypto.mrgsort(self, mergesize)
 
     @source_location
-    def exp(self) -> 'Tensor':
-        return pypto.exp(self)
+    def exp(self, precision_type: 'ExpAlgorithm' = pypto.ExpAlgorithm.HIGH_PRECISION) -> 'Tensor':
+        return pypto.exp(self, precision_type)
 
     @source_location
     def sign(self) -> 'Tensor':
@@ -772,20 +772,20 @@ class Tensor:
         return pypto.expm1(self)
 
     @source_location
-    def log(self) -> 'Tensor':
-        return pypto.log(self)
+    def log(self, precision_type: 'LogAlgorithm' = pypto.LogAlgorithm.HIGH_PRECISION) -> 'Tensor':
+        return pypto.log(self, precision_type)
 
     @source_location
     def log1p(self) -> 'Tensor':
         return pypto.log1p(self)
 
     @source_location
-    def log10(self) -> 'Tensor':
-        return pypto.log10(self)
+    def log10(self, precision_type: 'LogAlgorithm' = pypto.LogAlgorithm.HIGH_PRECISION) -> 'Tensor':
+        return pypto.log10(self, precision_type)
 
     @source_location
-    def log2(self) -> 'Tensor':
-        return pypto.log2(self)
+    def log2(self, precision_type: 'LogAlgorithm' = pypto.LogAlgorithm.HIGH_PRECISION) -> 'Tensor':
+        return pypto.log2(self, precision_type)
 
     @source_location
     def logical_not(self) -> 'Tensor':
@@ -816,12 +816,12 @@ class Tensor:
         return pypto.round(self, decimals)
 
     @source_location
-    def rsqrt(self) -> 'Tensor':
-        return pypto.rsqrt(self)
+    def rsqrt(self, precision_type: 'RsqrtAlgorithm' = pypto.RsqrtAlgorithm.HIGH_PRECISION) -> 'Tensor':
+        return pypto.rsqrt(self, precision_type)
 
     @source_location
-    def sqrt(self) -> 'Tensor':
-        return pypto.sqrt(self)
+    def sqrt(self, precision_type: 'SqrtAlgorithm' = pypto.SqrtAlgorithm.HIGH_PRECISION) -> 'Tensor':
+        return pypto.sqrt(self, precision_type)
 
     @source_location
     def ceil(self) -> 'Tensor':
@@ -836,8 +836,8 @@ class Tensor:
         return pypto.trunc(self)
 
     @source_location
-    def reciprocal(self) -> 'Tensor':
-        return pypto.reciprocal(self)
+    def reciprocal(self, precision_type: 'RecipAlgorithm' = pypto.RecipAlgorithm.DEFAULT) -> 'Tensor':
+        return pypto.reciprocal(self, precision_type)
 
     @source_location
     def relu(self) -> 'Tensor':
