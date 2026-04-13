aeba0e941f6c0ca0834b1f4ed523be532ee14ebf
heyufeng <heyufeng11@hisilicon.com>
2026-04-13 11:16:16 +0800

    feat(pass): Workspace GM tensor alloc in OoO

--- framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.h
@@ -251,7 +251,7 @@ private:
     void ReplaceViewOpChainMemId(LogicalTensorPtr startTensor, int oldMemId, int newMemId);
     void UpdateOpInternalSubgraphID(Operation &op, Operation* srcOp);
     void UpdateOpAttr(Operation &op, int opLatency, LogicalTensorPtr spillTensor, std::vector<int64_t> offset,
-        Operation* spillOp, int64_t workspaceBaseOffset);
+        Operation* spillOp);
     Status UpdateTensorAttr(LogicalTensorPtr tensor, MemoryType memType, LogicalTensorPtr spillTensor, int spillMemId);
     int GetBufNextUseOrder(Operation* op, int curMemId);
     int GetBufLastUseOrder(Operation* op, int curMemId);
