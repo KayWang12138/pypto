54587387d22dc44dcc7a0ac8a1c1409f3b8af4fd
gguuaanngg <zhangguangjun2@huawei.com>
2026-04-13 09:38:36 +0800

    feat(Operation): Support Sqrt/Exp high precision

--- framework/src/interface/operation/vector/math.cpp
+++ framework/src/interface/operation/vector/math.cpp
@@ -214,7 +214,7 @@ Tensor Neg(const Tensor& self)
     }
 }
 
-Tensor Log(const Tensor& self, LogBaseType base)
+Tensor Log(const Tensor& self, LogBaseType base, LogAlgorithm precisionType)
 {
     DECLARE_TRACER();
     ASSERT(
@@ -238,8 +238,9 @@ Tensor Log(const Tensor& self, LogBaseType base)
     }
 
     auto resTensor = Tensor(DataType::DT_FP32, self.GetShape());
-    resTensor =
-        CALL(UnaryOperation<UnaryOpType::LN>, *Program::GetInstance().GetCurrentFunction(), operandCast.GetStorage());
+    auto [resTensorStorage, op] = TensorUnaryOperationWithOp<UnaryOpType::LN>(*Program::GetInstance().GetCurrentFunction(), operandCast.GetStorage());
+    op->SetAttribute(OpAttributeKey::precisionType, static_cast<int64_t>(precisionType));
+    resTensor = resTensorStorage;
 
     auto resTensorBeforeCast = Tensor(DataType::DT_FP32, self.GetShape());
     if (base == LogBaseType::LOG_2) {
