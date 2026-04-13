c856ea98e4b04a6b28c6cc17a3a2e05d7a2b4e8b
LordOfMysteries <duhongxiang@huawei.com>
2026-04-13 20:20:09 +0800

    feat(machine): Optimize ctrl flow cache perf

--- framework/src/machine/device/dynamic/device_ctrl.h
+++ framework/src/machine/device/dynamic/device_ctrl.h
@@ -60,11 +60,15 @@ public:
     int AllocNewTaskCtrl()
     {
         uint32_t& taskCtrlIndex = devStartArgs_->devCtrlState.taskCtrlIndex;
+        auto& taskCtrlInitCount = devStartArgs_->devCtrlState.taskCtrlInitCount;
         TIMEOUT_CHECK_START();
         while (true) {
             if (taskCtrlIndex == MAX_DEVICE_TASK_NUM)
                 taskCtrlIndex = 0;
-            if (!GetTaskCtrlInPool(taskCtrlIndex).IsNotFree()) {
+            if (taskCtrlIndex >= taskCtrlInitCount) {
+                taskCtrlInitCount++;
+                return taskCtrlIndex++;
+            } else if (!GetTaskCtrlInPool(taskCtrlIndex).IsNotFree()) {
                 return taskCtrlIndex++;
             }
             taskCtrlIndex++;
@@ -245,6 +249,7 @@ public:
         devStartArgs->InitProgram(devProg, reinterpret_cast<uint64_t>(devStartArgs));
         devStartArgs->devCtrlState.schAicpuNum = devProg->devArgs.scheCpuNum;
         devStartArgs->devCtrlState.taskCtrlIndex = 0;
+        devStartArgs->devCtrlState.taskCtrlInitCount = 0;
         devStartArgs->devScheState.threadIdx = CTRL_THREAD_INDEX;
         devStartArgs->devScheState.finished = 0;
 
