5474145e2b9bad2ae1d1805d7c74138350dfd5a4
wangqun23 <wangqun23@huawei.com>
2026-04-13 11:18:26 +0800

    fix(pass): Add subgraphID for op cloned and Pad tensor on UB

--- framework/src/passes/tile_graph_pass/graph_constraint/remove_unaligned_reshape_op.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/remove_unaligned_reshape_op.cpp
@@ -409,7 +409,9 @@ void RemoveUnalignedReshape::ProcessCopyInOfDDRReshape(
 {
     for (auto* copyInOp : copyInOps) {
         auto copyInInput = copyInOp->GetIOperands().front();
-        auto copyInOutput = copyInOp->GetOOperands().front();
+        auto copyInInputMemType = copyInInput->GetMemoryTypeOriginal(); 
+        auto copyInOutput = copyInOp->GetOOperands().front();	 
+        auto copyInOutputMemType = copyInOutput->GetMemoryTypeOriginal();
 
         bool copyInShapeSame = true;
         if (copyInInput->GetShape().size() != copyInOutput->GetShape().size()) {
@@ -423,10 +425,10 @@ void RemoveUnalignedReshape::ProcessCopyInOfDDRReshape(
             }
         }
 
-        if (copyInInput->GetMemoryTypeOriginal() == MemoryType::MEM_DEVICE_DDR) {
-            if (copyInOutput->GetMemoryTypeOriginal() == MemoryType::MEM_UB && copyInShapeSame) {
+        if (copyInInputMemType == MemoryType::MEM_DEVICE_DDR) {
+            if (copyInOutputMemType == MemoryType::MEM_UB && copyInShapeSame) {
                 copyInOp->SetOpCode(Opcode::OP_RESHAPE_COPY_IN);
-            } else if (copyInOutput->GetMemoryTypeOriginal() != MemoryType::MEM_UB || !copyInShapeSame) {
+            } else if (copyInOutputMemType != MemoryType::MEM_UB || !copyInShapeSame) {
                 // reshape -- copyInInput(DDR) -- COPYIN -- copyInOutout(NOTUB)
                 // reshape -- copyInInput(DDR) -- RESHAPECOPYIN -- newTensor(UB) -- COPYOUT -- newTensor2(DDR) -- COPYIN
                 // --copyInOutout(NOTUB)
