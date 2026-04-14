9dca36de718321b7728e861bd425c7d3ff15590b
root <root@liteserver-hps-2f1a-00004.novalocal>
2026-04-14 09:04:06 +0000

    feat(Interpreter): Dump Input Output Data With Cann Interface

--- python/src/bindings/runtime.cpp
+++ python/src/bindings/runtime.cpp
@@ -937,6 +937,7 @@ private:
 
         kmodule->Launch(kbinary, aicoreStream, tensors, ctrlFlowCache, wsAddr);
         HOST_PERF_TRACE(TracePhase::Launch);
+        DumpIOTensorsWithCann(aicoreStream, tensors, kbinary->GetFunction()->GetRawName());
     }
 };
 
