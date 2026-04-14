ae824e0b928614638091d7f66a3049939024f16b
anyangle <anyangle@huawei.com>
2026-04-14 16:20:21 +0800

    fix(Machine): Fix profiling print bug

--- framework/src/interface/machine/device/tilefwk/aicpu_common.h
+++ framework/src/interface/machine/device/tilefwk/aicpu_common.h
@@ -198,7 +198,7 @@ struct DevDfxArgs {
     int32_t isOpenPerfTrace{0};
 };
 
-constexpr uint32_t PERF_TRACE_INST_MAX_NUM_EVERY_TYPE = 20;
+constexpr uint32_t PERF_TRACE_INST_MAX_NUM_EVERY_TYPE = 256;
 constexpr uint32_t INVALID_DEV_TASK_ID = 0xFFFFFFFF;
 enum AicorePerfTrace {
     PERF_TRACE_CORE_BEGIN = 0,
