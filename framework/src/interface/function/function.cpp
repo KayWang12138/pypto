57076261a83bad246f01b2bfafe9a94af9f99a7a
xujiayi1999 <xujiayi28@huawei.com>
2026-04-14 21:15:48 +0800

    fix(Pass): Delete tensor dynvalidshape string

--- framework/src/interface/function/function.cpp
+++ framework/src/interface/function/function.cpp
@@ -3785,6 +3785,9 @@ std::shared_ptr<LogicalTensor> Function::ConnectWithOverlap(std::shared_ptr<Logi
             auto assembleResult = std::make_shared<LogicalTensor>(
                 *this, matches[0]->Datatype(), maximumShape, iOperand->Format(), "Assemble_" + matches[0]->Symbol(),
                 iOperand->nodetype);
+            if (!iOperand->GetDynValidShape().empty()) {
+ 	                 assembleResult->UpdateDynValidShape(iOperand->GetDynValidShape());
+ 	        }
             FUNCTION_ASSERT(FError::NOT_EXIST, assembleResult->GetProducers().empty())
             "Assemble result should have no producers";
             for (size_t idx = 0; idx < matches.size(); idx++) {
@@ -3796,6 +3799,9 @@ std::shared_ptr<LogicalTensor> Function::ConnectWithOverlap(std::shared_ptr<Logi
             auto viewResult = std::make_shared<LogicalTensor>(
                 *this, assembleResult->Datatype(), iOperand->shape, iOperand->Format(),
                 "View_" + assembleResult->Symbol(), assembleResult->nodetype);
+            if (!iOperand->GetDynValidShape().empty()) {
+ 	                 viewResult->UpdateDynValidShape(iOperand->GetDynValidShape());
+ 	        }
             auto& viewOp = AddRawOperation(Opcode::OP_VIEW, {assembleResult}, {viewResult});
             std::vector<int64_t> newOffset = TensorOffset::Sub(iOperand->GetOffset(), minimumOffset);
             std::vector<SymbolicScalar> newDynOffset = TensorOffset::Sub(iOperand->GetDynOffset(), minimumOffset);
