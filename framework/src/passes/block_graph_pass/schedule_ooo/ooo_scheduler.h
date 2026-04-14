853092cbd9a106a9ca93ea0f90ff6b990e8cc233
Runze-Geng <gengrunze@huawei.com>
2026-04-14 09:38:19 +0800

    feat(pass): Support Mix Spill Tensor

--- framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.h
@@ -251,7 +251,7 @@ private:
     void ReplaceViewOpChainMemId(LogicalTensorPtr startTensor, int oldMemId, int newMemId);
     void UpdateOpInternalSubgraphID(Operation &op, Operation* srcOp);
     void UpdateOpAttr(Operation &op, int opLatency, LogicalTensorPtr spillTensor, std::vector<int64_t> offset,
-        Operation* spillOp, int64_t workspaceBaseOffset);
+        Operation* spillOp);
     Status UpdateTensorAttr(LogicalTensorPtr tensor, MemoryType memType, LogicalTensorPtr spillTensor, int spillMemId);
     int GetBufNextUseOrder(Operation* op, int curMemId);
     int GetBufLastUseOrder(Operation* op, int curMemId);
@@ -266,9 +266,9 @@ private:
     LogicalTensorPtr CreateAssemblePartTensor(LogicalTensorPtr iOperand, LogicalTensorPtr assembleTensor,
         SpillInfo &spillInfo, std::shared_ptr<AssembleOpAttribute> assembleAttr);
     int64_t CalcWorkspaceOffset(std::vector<int64_t> shape, std::vector<int64_t> offset, DataType dataType);
-    void GetWorkspaceBaseOffset(LogicalTensorPtr ddrTensor, int64_t& base);
     Status UpdateCopyOutMode(Operation& copyOutOp);
     Status UpdateCopyInMode(Operation& copyInOp);
+    void AllocWorkspaceGM(const std::vector<Operation *> &opList);
 
     // buffer rearrange
     Status RearrangeBuffer(Operation* allocOp, MemoryType memType, CoreLocationType coreLocation, bool isGenSpill);
