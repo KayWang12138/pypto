c8025ed546dec39e9195c7c166dd563d52cbf3b6
yang-fulai-3 <yangfulai3@huawei.com>
2026-04-14 14:48:17 +0800

    Add new operation TransData

--- framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
+++ framework/src/codegen/cloudnpu/codegen_op_cloudnpu.cpp
@@ -72,6 +72,9 @@ CodeGenOpCloudNPU::CodeGenOpCloudNPU(const CodeGenOpCloudNPUCtx& ctx)
           {Opcode::OP_TRANSPOSE_MOVEOUT, [this]() { return GenTransposeDataMove(); }},
           {Opcode::OP_TRANSPOSE_MOVEIN, [this]() { return GenTransposeDataMove(); }},
 
+          // transData
+          {Opcode::OP_NCHW2NC1HWC0, [this]() { return GenTransData(); }},
+
           // index outcast
           {Opcode::OP_INDEX_OUTCAST, [this]() { return GenIndexOutCastOp(); }},
           // lOC -> UB
