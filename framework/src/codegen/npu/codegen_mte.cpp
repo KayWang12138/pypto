1905c44091205decf2468bfb0bd92208c0ce8369
Bee.Hujun <Bee.hujun@huawei.com>
2026-04-15 20:06:04 +0800

    feat(codegen): Support L0C -> UB with relu

--- framework/src/codegen/npu/codegen_mte.cpp
+++ framework/src/codegen/npu/codegen_mte.cpp
@@ -128,24 +128,7 @@ std::string CodeGenOpNPU::GenMemL1SpillToGM(bool isLocalToGM, unsigned uf) const
     return buffer;
 }
 
-std::string CodeGenOpNPU::GenL0CToUBTileTensor() const
-{
-    std::string dstTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::DST_IDX));
-    std::string src0Tensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::SRC0_IDX));
-    // constructor call parameter ((RUNTIME_COA_GET_PARAM_OFFSET(2, 136, 0)),(RUNTIME_COA_GET_PARAM_OFFSET(2, 136, 1)))
-    std::string coordCp = WrapParamByParentheses(offset[ToUnderlying(MISOIdx::SRC0_IDX)]);
-    // e.g. Coord4Dim((RUNTIME_COA_GET_PARAM_OFFSET(2, 136, 0)),(RUNTIME_COA_GET_PARAM_OFFSET(2, 136, 1)))
-    std::string coord = PrintCoord(rawShape[ToUnderlying(MISOIdx::SRC0_IDX)].size(), coordCp);
-    std::string nzVar = "CopyOutMode::NZ2ND"; // current only support NZ2ND in L0C -> UB
-    std::ostringstream oss;
-    int64_t aivId = 0;
-    GetOpAttr(OpAttributeKey::subBlockIdx, aivId);
-    oss << tileOpName;
-    oss << WrapParamByAngleBrackets({nzVar});
-    oss << WrapParamByParentheses({dstTensor, src0Tensor, coord, std::to_string(aivId)});
-    oss << STMT_END;
-    return oss.str();
-}
+std::string CodeGenOpNPU::GenL0CToUBTileTensor() const { return PrintL0CToL1UBTileTensor(); }
 
 std::string CodeGenOpNPU::PrintMemL1ToL0TileTensor() const
 {
@@ -301,7 +284,7 @@ std::string CodeGenOpNPU::GenReshapeCopyIn() const { return GenMemCopyVar(false)
 
 std::string CodeGenOpNPU::GenReshapeCopyOut() const { return GenMemCopyVar(true); }
 
-std::string CodeGenOpNPU::PrintL0CToL1TileTensor() const
+std::string CodeGenOpNPU::PrintL0CToL1UBTileTensor() const
 {
     std::string dstTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::DST_IDX));
     std::string srcTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::SRC0_IDX));
@@ -313,6 +296,9 @@ std::string CodeGenOpNPU::PrintL0CToL1TileTensor() const
     int64_t reluMode = 0;
     GetOpAttr(OP_ATTR_PREFIX + "relu_type", reluMode);
     std::string nzVar = "CopyOutMode::NZ2NZ";
+    if (opCode == Opcode::OP_L0C_COPY_UB) {
+        nzVar = "CopyOutMode::NZ2ND";
+    }
     std::vector<std::string> storeConfigList = {nzVar, std::to_string(isAcc), std::to_string(reluMode)};
     std::string storeConfig = WrapParamByAngleBrackets(storeConfigList);
     Element scaleValue = Element(DataType::DT_UINT64, 0);
@@ -321,8 +307,14 @@ std::string CodeGenOpNPU::PrintL0CToL1TileTensor() const
     if ((!scaleValue.GetUnsignedData()) && ((operandDtype[ID1] == DT_INT32) && (operandDtype[ID0] == DT_FP16))) {
         src1Tensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::SRC1_IDX));
     }
-    std::vector<std::string> tileOpParamList = {dstTensor, srcTensor, src1Tensor,
-                                                coordDst,  coordSrc,  std::to_string(scaleValue.GetUnsignedData())};
+    std::vector<std::string> tileOpParamList = {dstTensor, srcTensor, src1Tensor, coordDst, coordSrc};
+    int64_t aivId = 0;
+    GetOpAttr(OpAttributeKey::subBlockIdx, aivId);
+    if (opCode == Opcode::OP_L0C_COPY_UB) {
+        tileOpParamList.emplace_back(std::to_string(aivId));
+    }
+    tileOpParamList.emplace_back(std::to_string(scaleValue.GetUnsignedData()));
+
     std::ostringstream oss;
     oss << tileOpName << "<" << TSTORE_CONF << storeConfig << ">";
     oss << WrapParamByParentheses(tileOpParamList);
@@ -333,7 +325,7 @@ std::string CodeGenOpNPU::PrintL0CToL1TileTensor() const
 std::string CodeGenOpNPU::GenMemL0CToL1() const
 {
     if (isSupportLayout) {
-        return PrintL0CToL1TileTensor();
+        return PrintL0CToL1UBTileTensor();
     }
     std::string dstVar = sm->QueryVarNameByTensorMagic(operandWithMagic[ID0]);
     std::string srcVar = sm->QueryVarNameByTensorMagic(operandWithMagic[ID1]);
@@ -394,9 +386,7 @@ std::string CodeGenOpNPU::GenUBToL1TileTensor() const
 
 std::string CodeGenOpNPU::GenUBToUBND2NZTileTensor() const
 {
-    if (!isSupportLayout) {
-        return "";
-    }
+    ASSERT(GenCodeErr::PRINT_MODE_ERROR, isSupportLayout) << "UB to UB ND2NZ only support tile tensor";
 
     std::string dstTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::DST_IDX));
     std::string srcTensor = QueryTileTensorNameByIdx(ToUnderlying(MISOIdx::SRC0_IDX));
