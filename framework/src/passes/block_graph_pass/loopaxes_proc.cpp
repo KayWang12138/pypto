7d3597cdfe19a3b45aeca4a3a2e28ae1e58e377b
xujiayi1999 <xujiayi28@huawei.com>
2026-04-16 21:08:31 +0800

    fix(Pass): Add assert Errcode

--- framework/src/passes/block_graph_pass/loopaxes_proc.cpp
+++ framework/src/passes/block_graph_pass/loopaxes_proc.cpp
@@ -21,6 +21,7 @@
 #include "passes/pass_log/pass_log.h"
 #include "passes/pass_interface/pass.h"
 #include "loopaxes_proc.h"
+#include "passes/pass_utils/pass_error.h"
 
 #define MODULE_NAME "LoopaxesProc"
 
@@ -71,7 +72,7 @@ bool NeedClearStatus(const Operation& op)
     //  Opcode::OP_EXPAND only support last axis or second last axis in for-loop
     if (opCode == Opcode::OP_EXPAND) {
         std::string axisKey = OP_ATTR_PREFIX + "EXPANDDIM";
-        ASSERT(op.HasAttr(axisKey)) << "attr " << axisKey << "not found";
+        ASSERT(OperationErr::OP_SPECIAL_CONSTRAINT, op.HasAttr(axisKey)) << "attr " << axisKey << "not found";
         int64_t expandAxis = op.GetIntAttribute(axisKey);
         int shapeSize = static_cast<int>(op.GetOOperands().front()->GetDynValidShape().size());
         expandAxis += SHAPE_DIM4 - shapeSize;
