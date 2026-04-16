b289cf5bec4d4a4fa3a84df28c8ad36a2a9dba41
LordOfMysteries <duhongxiang@huawei.com>
2026-04-16 10:14:02 +0800

    feat(machine): Simplify runtime config stage 2

--- framework/src/machine/utils/dynamic/dev_encode.cpp
+++ framework/src/machine/utils/dynamic/dev_encode.cpp
@@ -2206,8 +2206,7 @@ void DevAscendProgram::InitControlFlowCache(
     uintdevptr_t& initOffset, const std::shared_ptr<DyndevFunctionAttribute>& dyndevAttr, bool fillContent)
 {
     (void)fillContent;
-
-    ctrlFlowCacheSize = config::GetRuntimeOption<int64_t>(STITCH_CFGCACHE_SIZE);
+    ctrlFlowCacheSize = DEFAULT_STITCH_CFGCACHE_SIZE;
     controlFlowCache.Init(
         dyndevAttr.get(), ctrlFlowCacheSize, runtimeOutcastPoolSize, initOffset, ExpectedMaxCachedNum());
 }
@@ -2664,7 +2663,7 @@ void EncodeDevAscendProgram(Function* func, uint64_t& offset, DevAscendProgram* 
         base->memBudget.aicoreSpilled = tensorWsRes.perCoreSpilledMem * maxCoreNum;
         base->devArgs.machineConfig = func->paramConfigs_.machineConfig_;
         base->stitchMaxFunctionNum = ExpectedMaxCachedNum();
-        base->stitchFunctionsize = config::GetRuntimeOption<uint32_t>(STITCH_FUNCTION_SIZE);
+        base->stitchFunctionsize = MAX_STITCH_LEAFFUNC_NUM;
         base->memBudget.metadata.general = CalcGeneralMetadataSlotWorkspace(base);
         base->memBudget.metadata.general += CalcGeneralMetadataSlabWorkspace(base);
         base->memBudget.metadata.stitchPool = CalcStitchWorkspace(*base);
