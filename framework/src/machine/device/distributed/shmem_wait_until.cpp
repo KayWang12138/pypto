4c359e6d616c8003c614784840125efcb24b7224
mengziyu <mengziyu1@huawei.com>
2026-04-16 09:09:10 +0800

    feat(distributed): Input tensor with 2 to 5 dimensions

--- framework/src/machine/device/distributed/shmem_wait_until.cpp
+++ framework/src/machine/device/distributed/shmem_wait_until.cpp
@@ -74,7 +74,9 @@ TensorInfo ShmemWaitUntilImpl::GetTensorInfo(
     info.rawIndex = GetCoa(index, opAttrs, expressionTable);
     ++index; // 跳过 rawIndex
     info.dim = aicpuCode[paramInfo_.inIndex + AICPU_ATTR_DIM_INDEX];
-    info.offset = GetCoaVector(index, info.dim, opAttrs, expressionTable);
+    for (uint32_t i = 0; i < info.dim && i < 5; ++i) {
+        info.offset[i + (5 - info.dim)] = GetCoa(index + i, opAttrs, expressionTable);
+    }
 
     info.expectedSum = aicpuCode[paramInfo_.attrIndex];
     info.resetSignal = aicpuCode[paramInfo_.attrIndex + AICPU_ATTR_DIM_INDEX];
