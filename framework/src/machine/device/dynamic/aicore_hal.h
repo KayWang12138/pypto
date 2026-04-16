c286491e01f4659dcfad2c1bb29cb76fecdf92ac
huanglu <huanglu22@hisilicon.com>
2026-04-16 08:03:09 +0000

    feat(machine): Swimlane adapt with mixcore task and Pass mix core task info

--- framework/src/machine/device/dynamic/aicore_hal.h
+++ framework/src/machine/device/dynamic/aicore_hal.h
@@ -415,13 +415,6 @@ public:
             SchedErr::ABNOMAL_LAST_WORD, "last_taskId %ld task status [%ld, %ld, %ld, %ld].",
             arg->shakeBuffer[NUM_ONE], arg->shakeBuffer[NUM_TWO], arg->shakeBuffer[NUM_THREE],
             arg->shakeBuffer[NUM_FOUR], arg->shakeBuffer[NUM_FIVE]);
-
-        for (size_t i = 0; i < sizeof(arg->taskStat) / sizeof(TaskStat); i++) {
-            DEV_ERROR(
-                SchedErr::ABNOMAL_LAST_WORD, "task rsp index %lu: taskId %d, subGraphID %d execStart %ld execEnd %ld.",
-                i, arg->taskStat[i].taskId, arg->taskStat[i].subGraphId,
-                arg->taskStat[i].execStart, arg->taskStat[i].execEnd);
-        }
     }
 
     uint64_t GetAicoreStatus(int coreIdx) const
@@ -481,12 +474,6 @@ public:
         }
         ResetParallelDevTask(coreIdx);
         return;
-    }
-
-    volatile TaskStat* GetTaskStat(int coreIdx, int pos)
-    {
-        volatile TaskStat* stat = &args_[coreIdx]->taskStat[pos];
-        return stat;
     }
 
     inline void InitKernelArgs(int coreIdx, int64_t buffer) {
--- framework/src/machine/runtime/dump_device_perf.cpp
+++ framework/src/machine/runtime/dump_device_perf.cpp
@@ -36,6 +36,30 @@ extern "C" void DumpDevTaskPerfData(DeviceArgs& args, const std::vector<void*>& 
     }
 }
 
+json BuildSyncEventsJson(const TaskStat& taskStat)
+{
+    json syncEventsArr = json::array();
+    for (int k = 0; k < taskStat.setEventIdx; ++k) {
+        if (taskStat.setEventCycle[k] != 0) {
+            json setEvent;
+            setEvent["idx"] = k;
+            setEvent["type"] = "CV_SYNC_SET";
+            setEvent["time"] = taskStat.setEventCycle[k];
+            syncEventsArr.push_back(setEvent);
+        }
+    }
+    for (int k = 0; k < taskStat.waitEventIdx; ++k) {
+        if (taskStat.waitEventCycle[k] != 0) {
+            json waitEvent;
+            waitEvent["idx"] = k;
+            waitEvent["type"] = "CV_SYNC_WAIT";
+            waitEvent["time"] = taskStat.waitEventCycle[k];
+            syncEventsArr.push_back(waitEvent);
+        }
+    }
+    return syncEventsArr;
+}
+
 void ConstructTaskInfo(
     const uint32_t& index, json& rootTaskStats, const std::vector<void*>& perfData, const std::string& coreType)
 {
@@ -61,6 +85,10 @@ void ConstructTaskInfo(
             taskObj["taskId"] = taskStats[j].taskId;
             taskObj["execStart"] = taskStats[j].execStart;
             taskObj["execEnd"] = taskStats[j].execEnd;
+            json syncEventsArr = BuildSyncEventsJson(taskStats[j]);
+            if (!syncEventsArr.empty()) {
+                taskObj["syncEvents"] = syncEventsArr;
+            }
             tasksArr.push_back(taskObj);
         }
     }
@@ -93,6 +121,7 @@ void DumpAicoreTaskExectInfo(DeviceArgs& args, const std::vector<void*>& perfDat
     MACHINE_LOGD("tilefwk_L1_prof_data have saved in: %s", jsonFilePath.c_str());
     std::string topo_txt_path = npu::tile_fwk::config::LogTopFolder() + "/dyn_topo.txt";
     std::string program_json_path = npu::tile_fwk::config::LogTopFolder() + "/program.json";
+    std::string mix_event_path = npu::tile_fwk::config::GetAbsoluteTopFolder() + "/mix_event_info.json";
     std::string draw_swim_lane_py_path = GetCurrentSharedLibPath() + "/scripts/draw_swim_lane.py";
     npu::tile_fwk::config::SetRunDataOption(
         KEY_SWIM_GRAPH_PATH, npu::tile_fwk::config::GetAbsoluteTopFolder() + "/merged_swimlane.json");
