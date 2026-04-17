46ba171a3e64cceba0c2a1577a87a90a87f0ad68
junjieHuang <huangjunjie38@h-partners.com>
2026-04-17 01:46:06 +0000

    refactor(function): The interface FUNCTION_LOGE_E() is unified as  FUNCTION_LOGE().

--- framework/src/interface/operation/attr_holder.h
+++ framework/src/interface/operation/attr_holder.h
@@ -280,7 +280,7 @@ public:
                 return;
             }
         } catch (...) {
-            FUNCTION_LOGE_E(FError::INVALID_FILE, "json parse error");
+            FUNCTION_LOGE(FError::INVALID_FILE, "json parse error");
         }
     }
 };
