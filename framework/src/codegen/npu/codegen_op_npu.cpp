c161d7b00749e0d97fa93c1abb892a1f7f9d20e5
heyufeng <heyufeng11@hisilicon.com>
2026-04-17 08:27:19 +0800

    feat(pass): Set tensor PARAM_ADDR

--- framework/src/codegen/npu/codegen_op_npu.cpp
+++ framework/src/codegen/npu/codegen_op_npu.cpp
@@ -441,10 +441,17 @@ SymbolicScalar CodeGenOpNPU::GetOperandStartOffset(int operandIdx) const
 std::string CodeGenOpNPU::GenGmParamVar(unsigned gmParamIdx) const
 {
     if (isUnderDynamicFunction) {
-        std::ostringstream os;
-        os << "GET_PARAM_ADDR(" << GM_TENSOR_PARAM_STR << ", " << GmTensorParamIdxInCallFunc << ", "
-           << paramLocation[gmParamIdx] << ")";
-        return os.str();
+        std::map<int, SymbolicScalar> addrs;
+ 	    bool ret = GetTensorAttr(gmParamIdx, "paramAddr", addrs);
+ 	    ASSERT(OperErr::ATTRIBUTE_INVALID, ret)
+ 	        << "paramAddr is not found!! gmParamIdx: " << gmParamIdx << ", op: " << originalOp.Dump();
+ 	    auto iter = addrs.find(originalOp.GetOpMagic());
+ 	    ASSERT(OperErr::ATTRIBUTE_INVALID, iter != addrs.end())
+ 	        << "add is not found by opMagic: " << originalOp.GetOpMagic() << ", gmParamIdx: " << gmParamIdx
+ 	        << ", op: " << originalOp.Dump();
+ 	    auto addr = iter->second;
+        std::string gmParamVar = SymbolicExpressionTable::BuildExpression(addr);
+        return gmParamVar;
     }
 
     auto paramLoc = paramLocation[gmParamIdx];
