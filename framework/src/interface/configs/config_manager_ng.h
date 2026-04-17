008ae9c819d636b8f7d06af377f432a03ce76223
gaoxingwang <camuel.gao@huawei.com>
2026-04-17 17:46:52 +0800

    feat(machine): Optimize the BN-parallel feature

--- framework/src/interface/configs/config_manager_ng.h
+++ framework/src/interface/configs/config_manager_ng.h
@@ -53,6 +53,7 @@ constexpr const char* CFG_VALID_SHAPE_OPTIMIZE = "valid_shape_optimize";
 constexpr int64_t CFG_RUN_MODE_NPU = 0;
 constexpr int64_t CFG_RUN_MODE_SIM = 1;
 constexpr const char* READY_ON_HOST_TENSORS = "ready_on_host_tensors";
+constexpr const char* DEVICE_SCHED_PARALLISM = "device_sched_parallism";
 
 // host
 constexpr const char* COMPILE_STAGE = "compile_stage";
