9abda67cd5a2331cab71d1aba4438a768c626555
GBowen666 <libowen58@huawei.com>
2026-04-17 15:37:07 +0800

    fix hardcode lite npu

--- framework/include/tilefwk/platform.h
+++ framework/include/tilefwk/platform.h
@@ -135,8 +135,7 @@ public:
     void SetCCECVersion(const std::string& ver) { ccec_version = ver; }
 
     std::string GetVersion() const { 
-        return NPUArch::DAV_3113;
-        // return version; 
+        return version; 
     }
     std::string GetCCECVersion() const { return ccec_version; }
     size_t GetNum() const { return num_; }
@@ -356,7 +355,10 @@ public:
     void SetCCECVersion(const std::unordered_map<std::string, std::string>& ver);
 
     Die& GetDies() { return die_; }
-    NPUArch GetNPUArch() const { return version_; }
+    NPUArch GetNPUArch() const { 
+        return NPUArch::DAV_3113;
+        // return version_; 
+    }
     size_t GetDiesNum() const { return dies_cnt_; }
     std::string GetShortSocVersion() const { return short_soc_ver_; }
     std::string GetCCECVersion(std::string CoreType);
