302d771006403e7d61cf1ae2cfed8cfebe4227b7
xuyi_37 <xuyi37@huawei.com>
2026-04-17 17:17:30 +0800

    perf(machine): mix optimization - wrapIdToWrapInfo map - slabAlloc wrapQueueForThread - Simplify WrapInfo and StaticReadyCoreFunctionQueue

--- framework/src/machine/device/dynamic/context/device_task_context.h
+++ framework/src/machine/device/dynamic/context/device_task_context.h
@@ -68,6 +68,7 @@ private:
 
     // mix subgraph schedule
     WrapInfoQueue* AllocWrapQueue(DynDeviceTask* dyntask);
+    void InitWrapQueueForThread(DynDeviceTask* dyntask);
     void ProcessWrapQueue(
         DynDeviceTask* dyntask, uint32_t wrapId, int funcIndex, size_t opIndex, WrapInfoQueue* wrapQueue);
     bool IsMixArch(DevAscendProgram* devProg);
