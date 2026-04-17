9eb981fba2aceb7a72c9e96fab44675bed1cd6e0
junjieHuang <huangjunjie38@h-partners.com>
2026-04-17 07:05:41 +0000

    refactor(function): Adding errCode inside ASSERT().

--- framework/src/interface/ir/stmt.cpp
+++ framework/src/interface/ir/stmt.cpp
@@ -14,7 +14,7 @@
 #include <utility>
 
 #include "core/error.h"
-#include "core/logging.h"
+#include "interface/utils/function_error.h"
 
 namespace pypto {
 namespace ir {
@@ -24,9 +24,9 @@ OpStmts::OpStmts(std::vector<StmtPtr> stmts, Span span) : Stmt(std::move(span)),
     // Validate that all statements are AssignStmt or EvalStmt
     for (size_t i = 0; i < stmts_.size(); ++i) {
         const auto& stmt = stmts_[i];
-        INTERNAL_CHECK(stmt) << "OpStmts has null statement at index " << i << " at " << span_.ToString();
+        ASSERT(npu::tile_fwk::FError::EINTERNAL, stmt) << "OpStmts has null statement at index " << i << " at " << span_.ToString();
         auto kind = stmt->GetKind();
-        INTERNAL_CHECK(kind == ObjectKind::AssignStmt || kind == ObjectKind::EvalStmt)
+        ASSERT(npu::tile_fwk::FError::EINTERNAL, kind == ObjectKind::AssignStmt || kind == ObjectKind::EvalStmt)
             << "OpStmts only accepts AssignStmt or EvalStmt, but got " << stmt->TypeName() << " at index " << i
             << " at " << span_.ToString();
     }
