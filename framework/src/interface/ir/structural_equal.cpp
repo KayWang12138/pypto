a9058d15bcbfa08e69015ae7042c89f24d62721d
tsungl4 <lidongsheng4@huawei.com>
2026-04-17 16:00:27 +0800

    feat(ir): Fix code checks

--- framework/src/interface/ir/structural_equal.cpp
+++ framework/src/interface/ir/structural_equal.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) PyPTO Contributors.
  * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
@@ -8,7 +9,6 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  * -----------------------------------------------------------------------------------------------------------
  */
-
 #include <cstddef>
 #include <cstdint>
 #include <map>
