a4b3ddda102301a3625ceb5c5e4ca4c4e78c4863
Bee.Hujun <Bee.hujun@huawei.com>
2026-04-20 14:24:51 +0800

    feat(codegen): Support L0C->UB Dual Dst

--- framework/src/interface/operation/operation.h
+++ framework/src/interface/operation/operation.h
@@ -107,6 +107,7 @@ public:
     static const std::string precisionType;
     static const std::string perm;
     static const std::string gmTensorParamIdxInCall;
+    static const std::string splitMN;
 };
 
 class ConvOpAttributeKey {
