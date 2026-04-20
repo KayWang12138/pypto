f3ad6e986100148fba1202b6d9bce6b75628f4b3
hwwangmingjun <wangmingjun13@huawei.com>
2026-04-20 20:53:15 +0800

    feat(pass): Auto calculate the value of pg_upper_bound threshold.

--- models/deepseek_v32_exp/sparse_flash_attention_quant_impl.py
+++ models/deepseek_v32_exp/sparse_flash_attention_quant_impl.py
@@ -422,7 +422,6 @@ def sparse_flash_attention_quant_compute_flash(query_nope, query_rope, key_nope_
 
 @pypto.frontend.jit(
     pass_options={
-        "pg_upper_bound": 5000000,
         "vec_nbuffer_setting": {-1: 2, 0: 8},
         "cube_l1_reuse_setting": {-1: 2},
     },
@@ -484,7 +483,6 @@ def sparse_flash_attention_quant_d(
 
 @pypto.frontend.jit(
     pass_options={
-        "pg_upper_bound": 5000000,
         "vec_nbuffer_setting": {-1: 4, 0: 16},
         "cube_l1_reuse_setting": {-1: 4},
     },
