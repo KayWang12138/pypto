029a179b49f02ea78b613832cb72abd922a1cdf2
junjieHuang <huangjunjie38@h-partners.com>
2026-04-20 07:50:12 +0000

    fix(frontend): Fix print error msg.

--- python/pypto/error.py
+++ python/pypto/error.py
@@ -53,7 +53,7 @@ class PyptoError(Exception):
             err_code = _ERROR_CODE_UNKNOWN
         if isinstance(msg, Exception):
             msg = f"ErrCode: {err_code:X}, {type(msg).__name__}: {msg}"
-        else:
+        elif "ErrCode:" not in msg:
             msg = f"ErrCode: {err_code:X}, {msg}"
         super().__init__(msg)
         self.node: Optional[ast.AST] = None
