88095780fa14d922c4fc02b098d9d0835e69bfdb
wkkk0528 <wangkai578@huawei.com>
2026-04-20 21:10:40 +0800

    fix(operation): Add Axpy operation

--- python/pypto/tensor.py
+++ python/pypto/tensor.py
@@ -617,6 +617,10 @@ class Tensor:
         return pypto.add(self, other)
 
     @source_location
+    def axpy(self, x: 'Tensor', alpha: 'int | float' = 1.0) -> 'Tensor':
+        return pypto.axpy(self, x, alpha)
+
+    @source_location
     def sub(self, other: 'Tensor | int | float') -> 'Tensor':
         return pypto.sub(self, other)
 
