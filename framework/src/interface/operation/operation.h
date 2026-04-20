16fd44e11816eeeda3f09c1fe7580a664257c641
Bee.Hujun <Bee.hujun@huawei.com>
2026-04-20 14:24:51 +0800

    feat(codegen): Support L0C->UB Dual Dst

--- framework/src/interface/operation/operation.h
+++ framework/src/interface/operation/operation.h
@@ -105,6 +105,7 @@ public:
     static const std::string maxTileNum;
     static const std::string precisionType;
     static const std::string perm;
+    static const std::string splitMN;
 };
 
 class ConvOpAttributeKey {
