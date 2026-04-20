b291a497aea5f45cf9a267a2489cfc716e6af030
bytecode <dubingjia1@huawei.com>
2026-04-20 20:02:56 +0800

    fix(pass): Fix scopeid loss in auto_cast

--- framework/src/passes/tensor_graph_pass/auto_cast.h
+++ framework/src/passes/tensor_graph_pass/auto_cast.h
@@ -37,7 +37,8 @@ public:
     Status RemoveRedundantCastChain(Function& function);
     Status DefaultEnabledPreCheck(Function& function) override;
     Status PostCheck(Function& function) override;
-    void InsertCastOp(Function& function, LogicalTensorPtr src, LogicalTensorPtr tgt, const TileShape& tileShape);
+    void InsertCastOp(Function& function, LogicalTensorPtr src, LogicalTensorPtr tgt, const TileShape& tileShape,
+                      int scopeId = -1);
     Status GetInOutConnectedTensor(Function& function);
     std::set<std::pair<DataType, DataType>> legalCastPair{
         {DataType::DT_FP32, DataType::DT_FP16},  {DataType::DT_FP16, DataType::DT_FP32},
