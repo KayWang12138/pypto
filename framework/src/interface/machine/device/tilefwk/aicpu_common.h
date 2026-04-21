a56e6c1bdb7a29471220015195663f4596b8bdbe
wan_jianfen <wanjianfen@hisilicon.com>
2026-04-21 10:04:51 +0800

    feat(machine): Add aicpu trace

--- framework/src/interface/machine/device/tilefwk/aicpu_common.h
+++ framework/src/interface/machine/device/tilefwk/aicpu_common.h
@@ -196,6 +196,8 @@ struct TaskStat {
 struct DevDfxArgs {
     int32_t logLevel{-1};
     int32_t isOpenPerfTrace{0};
+    uint32_t deviceId{0};
+    uint64_t hostPid{0};
 };
 
 constexpr uint32_t PERF_TRACE_INST_MAX_NUM_EVERY_TYPE = 20;
