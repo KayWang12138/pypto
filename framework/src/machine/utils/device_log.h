d72e281237d3b81cb02b7b670fee88881b748be7
arthur_morgan_hw <lipeijie@huawei.com>
2026-04-21 19:56:53 +0800

    fix(machine): Replace assert with abort in DEV_ASSERT marco

--- framework/src/machine/utils/device_log.h
+++ framework/src/machine/utils/device_log.h
@@ -179,7 +179,7 @@ inline void DeviceLogSplitDebug(const char* func, const char* format, Args... ar
     do {                                                                      \
         if (!(expr)) {                                                        \
             DEV_ERROR(errCode, "Assertion failed (%s): " fmt, #expr, ##args); \
-            assert(0);                                                        \
+            abort();                                                          \
         }                                                                     \
     } while (0)
 
@@ -187,7 +187,7 @@ inline void DeviceLogSplitDebug(const char* func, const char* format, Args... ar
     do {                                                        \
         if (!(expr)) {                                          \
             DEV_ERROR(errCode, "Assertion failed (%s)", #expr); \
-            assert(0);                                          \
+            abort();                                            \
         }                                                       \
     } while (0)
 
@@ -214,7 +214,7 @@ inline void DeviceLogSplitDebug(const char* func, const char* format, Args... ar
     do {                                                      \
         if (!(expr)) {                                        \
             MACHINE_LOGE(errCode, "%s :" fmt, #expr, ##args); \
-            assert(0);                                        \
+            abort();                                          \
         }                                                     \
     } while (0)
 
@@ -222,7 +222,7 @@ inline void DeviceLogSplitDebug(const char* func, const char* format, Args... ar
     do {                                        \
         if (!(expr)) {                          \
             MACHINE_LOGE(errCode, "%s", #expr); \
-            assert(0);                          \
+            abort();                            \
         }                                       \
     } while (0)
 
