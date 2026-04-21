a95a8331febcef4d78bc82fb2354eec1446594fe
jiachengtang <tangjiacheng7@huawei.com>
2026-04-21 20:39:32 +0800

    debug

--- framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/ooo_scheduler.h
@@ -225,7 +225,7 @@ private:
     Operation* CreateReshapeOp(LogicalTensorPtr iOperand, LogicalTensorPtr oOperand);
     Operation* CreateAssembleOp(LogicalTensorPtr iOperand, LogicalTensorPtr oOperand, std::shared_ptr<AssembleOpAttribute> assembleAttr);
 
-    Operation* GetSpillOp(int memId);
+    Operation* GetSpillOp(int memId) override;
     LogicalTensorPtr GetSpillTensor(Operation* spillOp, int spillMemId);
     Status UpdateSpillOpDepend(Operation* spillOp, LogicalTensorPtr newTensor, int spillMemId);
     bool HasEnoughBuffer(Operation* allocOp, MemoryType memType);
