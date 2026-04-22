2a23edf58216baaba87ff847db5ad2353b57c9ab
junjieHuang <huangjunjie38@h-partners.com>
2026-04-22 12:32:10 +0000

    fix(frontend): Adding errCode to the frontend of python.

--- python/pypto/frontend/parser/evaluator.py
+++ python/pypto/frontend/parser/evaluator.py
@@ -37,7 +37,7 @@ from typing import Any
 
 import pypto
 
-from pypto.error import ParserError
+from pypto.error import ParserError, FeError
 from .diagnostics import Diagnostics
 
 
@@ -155,4 +155,4 @@ class ExprEvaluator:
         else:
             # Other unsupported expression types, raise python native error,
             # which will be caught by the parser and reported as a bug.
-            raise NotImplementedError("Unsupported expression type.")
+            raise FeError(NotImplementedError("Unsupported expression type."))
