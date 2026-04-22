daf7305a63c25997c39778613fa73c31bcee825c
Bee.Hujun <Bee.hujun@huawei.com>
2026-04-22 15:26:57 +0800

    feat(codegen): add addr in attr of tensor

--- framework/src/codegen/npu/codegen_op_npu.cpp
+++ framework/src/codegen/npu/codegen_op_npu.cpp
@@ -444,7 +444,7 @@ std::string CodeGenOpNPU::GenGmParamVar(unsigned gmParamIdx) const
 {
     if (isUnderDynamicFunction) {
         std::map<int, SymbolicScalar> addrs;
-        bool ret = GetTensorAttr(gmParamIdx, "paramAddr", addrs);
+        bool ret = GetTensorAttr(gmParamIdx, TensorAttributeKey::tensorAddr, addrs);
         ASSERT(OperErr::ATTRIBUTE_INVALID, ret)
             << "paramAddr is not found!! gmParamIdx: " << gmParamIdx << ", op: " << originalOp.Dump();
         auto iter = addrs.find(originalOp.GetOpMagic());
@@ -453,6 +453,7 @@ std::string CodeGenOpNPU::GenGmParamVar(unsigned gmParamIdx) const
             << ", op: " << originalOp.Dump();
         auto addr = iter->second;
         std::string gmParamVar = SymbolicExpressionTable::BuildExpression(addr);
+        CODEGEN_LOGI("gmParamVar: %s", gmParamVar.c_str());
         return gmParamVar;
     }
 
