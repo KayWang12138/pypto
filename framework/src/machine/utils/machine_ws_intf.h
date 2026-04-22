e1a2137961b3a24f4bdb7161b95f881a296fefa8
Pavel Stishenko <Stishenko.Pavel@huawei.com>
2026-04-22 19:26:40 +0800

    Merged master and protected head/tail/elem

--- framework/src/machine/utils/machine_ws_intf.h
+++ framework/src/machine/utils/machine_ws_intf.h
@@ -75,8 +75,7 @@ struct QueueGeneric {
     const T* end() const { return elem + tail; }
 
     typedef T value_type;
-//protected:
-public:
+protected:
     uint32_t head;
     uint32_t tail;
     value_type* elem;
