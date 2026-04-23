f0b46defc9f12c56d33736c423718103b84cbb0d
yang-zhipeng19 <yangzhipeng19@huawei.com>
2026-04-23 15:16:28 +0800

    feat(machine): Control aicore for control flow cache

--- framework/src/machine/device/dynamic/device_sche.h
+++ framework/src/machine/device/dynamic/device_sche.h
@@ -452,6 +452,11 @@ struct DynMachineManager {
         auto beginTime = GetCycles();
         DevStartArgs* runtimeDataCurrent =
             reinterpret_cast<DevStartArgs*>(devProg->GetRuntimeDataList()->GetRuntimeDataCurrent());
+        if (kargs->parameter.ctrlBlockNum != 0 &&
+            kargs->parameter.ctrlBlockNum < static_cast<int>(devProg->devArgs.nrValidAic)) {
+            devProg->devArgs.nrValidAic = kargs->parameter.ctrlBlockNum;
+            DEV_INFO("control aicore before launch, nrValidAic changed to %d", kargs->parameter.ctrlBlockNum);
+        }
         auto devArgs = devProg->devArgs;
         int threadIdx = -1;
         RunSchInit(&devArgs);
