583504771effd5de4f87d984a0c3f50a97e1bd85
huanglu <huanglu22@hisilicon.com>
2026-04-23 07:21:29 +0000

    fix pmu situation

--- framework/tests/ut/machine/src/test_prof.cpp
+++ framework/tests/ut/machine/src/test_prof.cpp
@@ -172,7 +172,6 @@ protected:
             prof->ProfStart();
 
             int32_t aicoreId = 0;
-            int32_t subgraphId = 0;
             int32_t taskId = 0;
             TaskStat* taskStat = new TaskStat();
             taskStat->taskId = 0;
@@ -180,7 +179,7 @@ protected:
             taskStat->execStart = 0;
             taskStat->subGraphId = 0;
             for (int i = 0; i < iterations; i++) {
-                prof->ProfGet(aicoreId, subgraphId, taskId, taskStat);
+                prof->ProfGetLog(aicoreId, taskStat);
             }
             int64_t flag = 0;
             prof->ProfGetSwitch(flag);
