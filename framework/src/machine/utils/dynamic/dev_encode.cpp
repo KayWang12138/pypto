9748813d714a5af367845d2fca35afee2a7b9d79
huanglu <huanglu22@hisilicon.com>
2026-04-23 11:06:55 +0000

    test

--- framework/src/machine/utils/dynamic/dev_encode.cpp
+++ framework/src/machine/utils/dynamic/dev_encode.cpp
@@ -1189,6 +1189,8 @@ struct EncodeDevAscendFunctionInfo {
             EncodeRawTensorAttr& attr = rawAttrs.emplace_back();
             attr.storage = tensor->storage_;
             attr.storageOffset = tensor->storageOffset_;
+            // rawMagicToRawTensor[tensor->GetRawTensor()->rawmagic] = tensor->GetRawTensor();
+            MACHINE_LOGD("ttttttttttttttest");
             rawMagicToRawTensor[tensor->GetRawTensor()->rawmagic] = tensor->GetRawTensor();
         }
     }
