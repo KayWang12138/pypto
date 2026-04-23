3782602c841da2919840278c6f24445878c94455
luohui46 <luohui46@huawei.com>
2026-04-23 16:26:19 +0800

    feat(operation): Support TopK and sum 1d testcase

--- framework/tests/st/operation/src/test_topk_operation.cpp
+++ framework/tests/st/operation/src/test_topk_operation.cpp
@@ -41,6 +41,29 @@ struct TopKOpMetadata {
     nlohmann::json test_data_;
 };
 
+void TopKOpExeFunc1D(const std::vector<Tensor>& inputs, std::vector<Tensor>& outputs, const OpFuncArgs* opArgs)
+{
+    auto args = static_cast<const TopKOpFuncArgs*>(opArgs);
+    SymbolicScalar firstDim = inputs[0].GetShape()[0];
+    const int firstViewShape = args->viewShape_[0];
+    int loop[] = {CeilDiv(firstDim, firstViewShape)};
+    FUNCTION("main", {inputs[0]}, {outputs[0], outputs[1]})
+    {
+        LOOP("LOOP_L0_bIdx", FunctionType::DYNAMIC_LOOP, bIdx, LoopRange(loop[IDX_DIM0]))
+        {
+            std::vector<SymbolicScalar> offset = {bIdx * args->viewShape_[0]};
+            auto viewTensor = View(
+                inputs[0], args->viewShape_,
+                {std::min(firstDim - bIdx * firstViewShape, firstViewShape)},
+                offset);
+            TileShape::Current().SetVecTile(args->tileShape_);
+            auto res = TopK(viewTensor, args->count_[0], args->dims_[0], args->largest_[0]);
+            Assemble(std::get<0>(res), offset, outputs[0]);
+            Assemble(std::get<1>(res), offset, outputs[1]);
+        }
+    }
+}
+
 void TopKOpExeFunc(const std::vector<Tensor>& inputs, std::vector<Tensor>& outputs, const OpFuncArgs* opArgs)
 {
     auto args = static_cast<const TopKOpFuncArgs*>(opArgs);
