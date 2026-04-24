f715aaab275282218bb50b826f74a093799512bc
Pavel Stishenko <Stishenko.Pavel@huawei.com>
2026-04-24 20:30:46 +0800

    Encapsulation of the ReadyCoreFunctionQueue interface, code deduplication

--- framework/src/machine/utils/machine_ws_intf.h
+++ framework/src/machine/utils/machine_ws_intf.h
@@ -19,6 +19,9 @@
 #include "tilefwk/aicpu_common.h"
 #include "interface/utils/common.h"
 #include "tilefwk/core_func_data.h"
+#include <string>
+#include <sstream>
+#include <mutex>
 
 namespace npu::tile_fwk {
 enum class MachineStatus { START = 0, FINISH = 1, STOP = 2 };
@@ -28,16 +31,170 @@ enum class MachineStatus { START = 0, FINISH = 1, STOP = 2 };
 #define WRAP_IDX_AIV0 1
 #define WRAP_IDX_AIV1 2
 
-// aic aiv 已经ready的core function id队列
-struct ReadyCoreFunctionQueue {
+namespace dynamic {
+class DevControlFlowCache;
+}
+
+template <class T>
+struct QueueGeneric {
+    QueueGeneric(uint32_t capacity, T* _elem) : head(0), tail(0), elem(_elem), _capacity(capacity) {}
+
+    QueueGeneric& operator=(const QueueGeneric& rhs)
+    {
+        head = 0;
+        tail = rhs.size();
+        if (capacity() == 0) {
+            return *this;
+        }
+        std::copy(rhs.elem + rhs.head, rhs.elem + rhs.tail, elem);
+        return *this;
+    }
+
+    __attribute__((always_inline)) uint32_t capacity() const { return _capacity; }
+
+    __attribute__((always_inline)) uint32_t size() const { return tail - head; }
+
+    std::string str() const
+    {
+        std::stringstream ss;
+        ss << "Queue at " << this << " head=" << head << " tail=" << tail << " capacity=" << capacity();
+        return ss.str();
+    }
+
+    std::string dump() const
+    {
+        std::stringstream ss;
+        for (value_type* it = elem + head; it != elem + tail; ++it) {
+            ss << *it << " ";
+        }
+        return ss.str();
+    }
+
+    const T* begin() const { return elem + head; }
+
+    const T* end() const { return elem + tail; }
+
+    typedef T value_type;
+
+protected:
     uint32_t head;
     uint32_t tail;
-    uint32_t capacity;
-    uint32_t* elem;
-    size_t lock;
+    value_type* elem;
 
-    uint64_t Size() { return tail - head; }
+    friend class dynamic::DevControlFlowCache;
+    // Only for relocation! Do not use it for elements reading/writing!
+    value_type*& relocable_elem() { return elem; }
+
+private:
+    uint32_t _capacity;
 };
+
+template <class T>
+struct LockableQueueGeneric : public QueueGeneric<T> {
+    using datarange = std::pair<const T*, const T*>;
+    using QueueGeneric<T>::operator=;
+
+    LockableQueueGeneric(uint32_t capacity = 0, T* _elem = nullptr) : QueueGeneric<T>(capacity, _elem), lockFlag(0) {}
+
+    __attribute__((always_inline)) inline void lock()
+    {
+        while (!__sync_bool_compare_and_swap(&lockFlag, 0, 1)) {
+        }
+    }
+
+    __attribute__((always_inline)) inline void unlock()
+    {
+        while (!__sync_bool_compare_and_swap(&lockFlag, 1, 0)) {
+        }
+    }
+
+    __attribute__((always_inline)) inline uint32_t unsafe_size() const
+    {
+        return __atomic_load_n(&this->tail, __ATOMIC_RELAXED) - __atomic_load_n(&this->head, __ATOMIC_RELAXED);
+    }
+
+    __attribute__((always_inline)) inline void unsafe_enqueue(T x)
+    {
+        this->elem[__atomic_fetch_add(&this->tail, 1, std::memory_order_release)] = x;
+    }
+
+    __attribute__((always_inline)) inline void unsafe_enqueue(T* x, uint32_t count)
+    {
+        const uint32_t t = __atomic_fetch_add(&this->tail, count, std::memory_order_release);
+        // Faster analog of std::copy(x, x + count, this->elem + t);
+        memcpy_s(this->elem + t, sizeof(T) * (this->capacity() - t), x, sizeof(T) * count);
+    }
+
+    __attribute__((always_inline)) inline bool try_enqueue(T x)
+    {
+        std::scoped_lock slock(*this);
+        uint32_t t = __atomic_fetch_add(&this->tail, 1, std::memory_order_release);
+        if (unlikely(t >= this->capacity())) {
+            __atomic_store_n(&this->tail, t, __ATOMIC_RELAXED);
+            return false;
+        }
+        this->elem[t] = x;
+        return true;
+    }
+
+    __attribute__((always_inline)) inline bool try_enqueue(const T* x, uint32_t count)
+    {
+        std::scoped_lock slock(*this);
+        uint32_t t = __atomic_fetch_add(&this->tail, count, std::memory_order_release);
+        if (unlikely(t + count > this->capacity())) {
+            __atomic_store_n(&this->tail, t, __ATOMIC_RELAXED);
+            return false;
+        }
+        // Faster analog of std::copy(x, x + count, this->elem + t);
+        memcpy_s(this->elem + t, sizeof(T) * (this->capacity() - t), x, sizeof(T) * count);
+        return true;
+    }
+
+    __attribute__((always_inline)) inline std::pair<const T*, const T*> dequeue_all()
+    {
+        std::scoped_lock slock(*this);
+        uint32_t t = __atomic_load_n(&this->tail, __ATOMIC_RELAXED);
+        uint32_t h = __atomic_exchange_n(&this->head, t, __ATOMIC_RELAXED);
+        return std::make_pair(this->elem + h, this->elem + t);
+    }
+
+    __attribute__((always_inline)) inline datarange dequeue(uint32_t max_count)
+    {
+        std::scoped_lock slock(*this);
+        uint32_t t = __atomic_load_n(&this->tail, __ATOMIC_RELAXED);
+        uint32_t h = __atomic_load_n(&this->head, __ATOMIC_RELAXED);
+        uint32_t cnt = std::min(t - h, max_count);
+        if (cnt == 0) {
+            return datarange(nullptr, nullptr);
+        }
+        __atomic_store_n(&this->head, h + cnt, __ATOMIC_RELAXED);
+        return datarange(this->elem + h, this->elem + h + cnt);
+    }
+
+    __attribute__((always_inline)) inline datarange dequeue_tail(uint32_t max_count, T* out)
+    {
+        std::scoped_lock slock(*this);
+        uint32_t t = __atomic_load_n(&this->tail, __ATOMIC_RELAXED);
+        uint32_t h = __atomic_load_n(&this->head, __ATOMIC_RELAXED);
+        uint32_t cnt = std::min(t - h, max_count);
+        if (cnt == 0) {
+            return datarange(nullptr, nullptr);
+        }
+        __atomic_store_n(&this->tail, t - cnt, __ATOMIC_RELAXED);
+        // Faster analog og std::copy(this->elem + t - cnt, this->elem + t, out);
+        memcpy_s(out, sizeof(T) * max_count, this->elem + t - cnt, sizeof(T) * cnt);
+        return datarange(out, out + cnt);
+    }
+
+private:
+    size_t lockFlag;
+
+    using QueueGeneric<T>::size;
+};
+
+// aic aiv 已经ready的core function id队列
+typedef LockableQueueGeneric<uint32_t> ReadyCoreFunctionQueue;
+typedef QueueGeneric<uint32_t> ReadyCoreFunctionQueueUnsafe;
 
 struct StaticReadyCoreFunctionQueue {
     uint64_t head;
@@ -62,17 +219,9 @@ struct WrapInfoQueue {
     uint64_t Size() { return tail - head; }
 };
 
-inline void ReadyQueueLock(ReadyCoreFunctionQueue* rq)
-{
-    while (!__sync_bool_compare_and_swap(&rq->lock, 0, 1)) {
-    }
-}
+inline void ReadyQueueLock(ReadyCoreFunctionQueue* rq) { rq->lock(); }
 
-inline void ReadyQueueUnLock(ReadyCoreFunctionQueue* rq)
-{
-    while (!__sync_bool_compare_and_swap(&rq->lock, 1, 0)) {
-    }
-}
+inline void ReadyQueueUnLock(ReadyCoreFunctionQueue* rq) { rq->unlock(); }
 
 enum class BinDataType {
     READY_STATUS,        // CoreFunction ready_status(no need update)
