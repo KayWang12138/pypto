e5847ba7d9f1bec46f6083765f46e1bdf8faf804
wangqun23 <wangqun23@huawei.com>
2026-04-24 10:57:37 +0800

    fix(pass): Solving redundant batchmatmul processing in Pass

--- framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/pre_graph/remove_redundant_assemble.cpp
@@ -422,6 +422,9 @@ Status RemoveRedundantAssemble::RemoveViewMultiReshape(
                 GetFormatBacktrace(viewOp).c_str());
             return FAILED;
         }
+        if (viewOp->GetIOperands()[0] != viewOp->GetOOperands()[0]) {
+            continue;
+        }
         auto consumers = viewOp->GetOutputOperand(0)->GetConsumers();
         if (consumers.empty()) {
             APASS_LOG_ERROR_F(
