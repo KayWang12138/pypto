1bb433a3b197376b82879c3a29e456a226b1f2d6
Pavel Stishenko <Stishenko.Pavel@huawei.com>
2026-04-24 22:25:42 +0800

    Encapsulation of the ReadyCoreFunctionQueue interface, code deduplication

--- framework/src/machine/device/dynamic/aicpu_task_manager.h
+++ framework/src/machine/device/dynamic/aicpu_task_manager.h
@@ -39,16 +39,14 @@ public:
         TASK_TYPE_NUM,
     };
 
-    AicpuTaskManager(){};
-    ~AicpuTaskManager(){};
+    AicpuTaskManager() {};
+    ~AicpuTaskManager() {};
 
     // 每个AICPU都会调用
     inline void TaskEnqueue(uint64_t taskId)
     {
-        ReadyQueueLock();
-        readyQueue_->elem[readyQueue_->tail] = taskId;
-        readyQueue_->tail += 1;
-        ReadyQueueUnLock();
+        bool res = readyQueue_->try_enqueue(taskId);
+        DEV_ASSERT(SchedErr::READY_QUEUE_OVERFLOW, res); // fail on queue overflow
     }
 
     // 仅AICPU_0会调用
@@ -76,17 +74,13 @@ public:
     // 仅AICPU_0会调用
     inline int32_t TaskProcess(uint64_t& taskCount)
     {
-        if (__atomic_load_n(&readyQueue_->tail, __ATOMIC_RELAXED) ==
-            __atomic_load_n(&readyQueue_->head, __ATOMIC_RELAXED)) {
+        if (readyQueue_->unsafe_size() == 0) {
             return DEVICE_MACHINE_OK;
         }
-        ReadyQueueLock();
-        uint64_t taskIdx = readyQueue_->head;
-        taskCount = readyQueue_->tail - readyQueue_->head;
-        readyQueue_->head += taskCount;
-        ReadyQueueUnLock();
-        for (uint32_t i = 0; i < taskCount; ++i) {
-            auto ret = TaskDispatch(readyQueue_->elem[taskIdx + i]);
+        auto tasksRange = readyQueue_->dequeue_all();
+        taskCount = tasksRange.second - tasksRange.first;
+        for (auto it = tasksRange.first; it != tasksRange.second; ++it) {
+            auto ret = TaskDispatch(*it);
             if (ret != DEVICE_MACHINE_OK) {
                 return ret;
             }
@@ -117,18 +111,6 @@ public:
     }
 
 private:
-    inline void ReadyQueueLock()
-    {
-        while (!__sync_bool_compare_and_swap(&readyQueue_->lock, 0, 1))
-            ;
-    }
-
-    inline void ReadyQueueUnLock()
-    {
-        while (!__sync_bool_compare_and_swap(&readyQueue_->lock, 1, 0))
-            ;
-    }
-
     inline TaskType GetTaskType(uint64_t taskId)
     {
         auto funcId = FuncID(taskId);
