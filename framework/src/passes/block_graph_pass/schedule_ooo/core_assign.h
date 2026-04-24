c1b8308854671f39f997edfdd345ee513a5f223f
gggxinmeng <guoxinmeng@huawei.com>
2026-04-24 16:40:45 +0800

    feat(Pass): Using reverse topoSeq for 2nd schedule

--- framework/src/passes/block_graph_pass/schedule_ooo/core_assign.h
+++ framework/src/passes/block_graph_pass/schedule_ooo/core_assign.h
@@ -86,7 +86,6 @@ public:
     void GapMinSchedule(TaskGraph& taskGraph, std::vector<int>& topoSeq);
     void GapMinForwardPass(TaskGraph& taskGraph, std::vector<int>& topoSeq);
     void GapMinBackwardShift(TaskGraph& taskGraph, std::vector<int>& topoSeq);
-    void GapMinBackwardShiftByExecOrder(TaskGraph& taskGraph);
     int64_t SumCrossCoreGap(const TaskGraph& g) const;
     void ScheduleOneTask(
         TaskGraph& taskGraph, int taskId,
