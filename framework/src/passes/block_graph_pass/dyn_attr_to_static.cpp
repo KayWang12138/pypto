a9233746424180626a013863721c04a53e090af6
ym-HW <yumeng21@hisilicon.com>
2026-04-24 19:43:17 +0800

    feat(Pass): Optimize scalar branch in CCEC

--- framework/src/passes/block_graph_pass/dyn_attr_to_static.cpp
+++ framework/src/passes/block_graph_pass/dyn_attr_to_static.cpp
@@ -450,12 +450,63 @@ Status DynAttrToStatic::TryRemoveDynAttr(Function* leafFunc, std::vector<Operati
                 return FAILED;
             }
         }
+        BuildParamAddr(op);
     }
 
     // 3. 为dynParam的赋值刷新coa宏
     ReplaceCommonSymbol(leafFunc, callopArglistOneDim);
     ReBuildConcreteParam(leafFunc, callopArglistOneDim);
     return SUCCESS;
+}
+
+std::pair<int, int> ParseRuntimeGetParamAddr(const std::string& input) {
+    std::pair<int, int> paramArgs;
+    // 正则表达式匹配：RUNTIME_GET_PARAM_ADDR(参数1, 参数2, 参数3)
+    std::regex pattern(R"(RUNTIME_GET_PARAM_ADDR\s*\(\s*([^,]+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\))");
+    std::smatch matches;
+    if (std::regex_match(input, matches, pattern)) {
+        paramArgs.first = std::stoi(matches[2].str());  // 第二个参数（1）
+        paramArgs.second = std::stoi(matches[3].str());  // 第三个参数（18）
+    }
+    return paramArgs;
+}
+
+// Helper function to set paramAddr attribute for a tensor
+void UpdateTensorParamAddr(std::shared_ptr<LogicalTensor> &tensor)
+{
+    std::map<int, SymbolicScalar> paramAddrMap;
+    tensor->GetAttr<std::map<int, SymbolicScalar>>(TensorAttributeKey::tensorAddr, paramAddrMap);
+    for (auto &[opMagic, paramAddr] : paramAddrMap) {
+        auto paramArgs = ParseRuntimeGetParamAddr(paramAddr.Dump());
+        int aiCpuFlag{3};
+        if (paramAddr.IsExpression()) {
+            paramAddr = GET_PARAM_ADDR_MAYBE_CONST(
+                SymbolicScalar(static_cast<int64_t>(aiCpuFlag)),
+                SymbolicScalar(static_cast<int64_t>(0)),
+                SymbolicScalar(static_cast<int64_t>(paramArgs.first)),
+                SymbolicScalar(static_cast<int64_t>(paramArgs.second)));
+        }
+    }
+    tensor->SetAttr<std::map<int, SymbolicScalar>>(TensorAttributeKey::tensorAddr, paramAddrMap);
+}
+
+void DynAttrToStatic::BuildParamAddr(Operation &op) {
+    for (auto &iOperand : op.GetIOperands()) {
+        if (visitedTensors_.count(iOperand)) {
+            continue;
+        }
+        if (iOperand->HasAttr(TensorAttributeKey::tensorAddr)) {
+            UpdateTensorParamAddr(iOperand);
+        }
+    }
+    for (auto &oOperand : op.GetOOperands()) {
+        if (visitedTensors_.count(oOperand)) {
+            continue;
+        }
+        if (oOperand->HasAttr(TensorAttributeKey::tensorAddr)) {
+            UpdateTensorParamAddr(oOperand);
+        }
+    }
 }
 
 Status DynAttrToStatic::RunOnFunction(Function& function)
