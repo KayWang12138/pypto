587215b44514cca115fdc3a044be75df0f092077
Immutable01 <gaoqiancheng@huawei.com>
2026-04-24 06:34:04 +0000

    refactor(interpreter): Adjust interpreter log system

--- framework/src/interface/interpreter/function.h
+++ framework/src/interface/interpreter/function.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#include "tilefwk/pypto_fwk_log.h"
+#include "interface/interpreter/interpreter_log.h"
 #include "interface/tensor/tensor_slot.h"
 #include "interface/interpreter/operation.h"
 #include "interface/tensor/symbolic_scalar_evaluate.h"
@@ -568,6 +568,7 @@ struct FunctionInterpreter {
         }
         dumpPath = dumpPath + "/" + "verify_" + timestamp.str() + "/";
         CreateMultiLevelDir(dumpPath);
+        interpreter_log::SetLogFilePath(dumpPath + "interpreter.log");
 
         std::string dumpOpFilePath = dumpPath + "verify_graph_data_metainfo.csv";
         std::string dumpProgrameFilePath = dumpPath + "verify_graph_result_brief.csv";
@@ -929,7 +930,7 @@ struct FunctionInterpreter {
             if (iOpDataList[index] == nullptr) {
                 auto iop = op->GetIOperands()[index];
                 if (frame.callop != nullptr) {
-                    VERIFY_LOGI("ExecuteOperation: iop %zu is null, try to find in mixGlobalTensorDict.", index);
+                    INTERPRETER_LOGI("ExecuteOperation: iop %zu is null, try to find in mixGlobalTensorDict.", index);
                     auto callopAttr = std::static_pointer_cast<CallOpAttribute>(frame.callop->GetOpAttribute());
                     iOpDataList[index] = mixGlobalTensorDict[{iop, callopAttr->wrapId}];
                     if (iOpDataList[index] != nullptr) {
@@ -1191,7 +1192,7 @@ struct FunctionInterpreter {
         ScalarImmediateType end = EvaluateSymbolicScalar(loop->End());
         ScalarImmediateType step = EvaluateSymbolicScalar(loop->Step());
         if (begin == end) {
-            VERIFY_EVENT("Function %s skip execute due to idx range = 0", func->GetMagicName().c_str());
+            INTERPRETER_EVENT("Function %s skip execute due to idx range = 0", func->GetMagicName().c_str());
         }
         for (ScalarImmediateType idx = begin; idx < end; idx += step) {
             UpdateSymbolDict(loop->IterSymbolName(), idx);
