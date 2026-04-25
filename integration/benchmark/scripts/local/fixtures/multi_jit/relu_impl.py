37f242150bb2688bca8b6a057b6e965055f7c30c
zouwenxiang <zouwenxiang1@huawei.com>
2026-04-25 14:18:19 +0800

    feat: add run_kernelbench

---
+++ integration/benchmark/scripts/local/fixtures/multi_jit/relu_impl.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+
+"""multi_jit fixture: 2 个 @pypto.jit, cheat_detector 应判 cheat (jit_def_count)."""
+import torch
+import pypto
+
+
+@pypto.jit
+def relu_a(x):
+    return pypto.dsl.relu(x)
+
+
+@pypto.jit
+def relu_b(x):
+    return pypto.dsl.add(x, 0.0)
+
+
+def relu_wrapper(x):
+    return relu_b(relu_a(x))
