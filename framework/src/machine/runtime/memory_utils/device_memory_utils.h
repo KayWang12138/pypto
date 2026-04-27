ece89cb493a4191cc583d43920decb674f864bb7
arthur_morgan_hw <lipeijie@huawei.com>
2026-04-27 19:03:48 +0800

    refector(machine): Refactoring memory utils

--- framework/src/machine/runtime/memory_utils/device_memory_utils.h
+++ framework/src/machine/runtime/memory_utils/device_memory_utils.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -10,13 +10,13 @@
 
 /*!
  * \file device_memory_utils.h
- * \brief
+ * \brief device memory utils
  */
 
 #pragma once
 
-#include "machine/runtime/runtime.h"
-#include "machine/runtime/device_runner.h"
+#include "adapter/api/acl_api.h"
+#include "adapter/api/runtime_api.h"
 #include "interface/interpreter/raw_tensor_data.h"
 
 namespace npu::tile_fwk::dynamic {
