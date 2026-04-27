adc6d5593e7dec5a81ddde92f14732487461a7fe
bytecode <dubingjia1@huawei.com>
2026-04-27 11:19:34 +0800

    remove skip_pratition

--- python/pypto/config.py
+++ python/pypto/config.py
@@ -54,7 +54,6 @@ def set_print_options(*,
 
 
 def set_pass_options(*,
-                     pg_skip_partition: Optional[bool] = None,
                      vec_nbuffer_setting: Optional[Dict[int, int]] = None,
                      cube_l1_reuse_setting: Optional[Dict[int, int]] = None,
                      cube_nbuffer_setting: Optional[Dict[int, int]] = None,
@@ -65,12 +64,6 @@ def set_pass_options(*,
 
     Parameters
     ---------
-    pg_skip_partition : bool
-        .. deprecated::
-            This parameter is deprecated and will be removed in a future version.
-            Please remove this parameter from your configuration.
-        Whether to skip the subgraph partitioning process.
-
     vec_nbuffer_setting : Dict[int, int]
         Merged graph parameter, used to configure
         the merging quantity of AIV subgraphs with the same structure.
