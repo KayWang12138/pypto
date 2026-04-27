e4cc6a107123cc1dfc6c9e749cb48fbf628f40b3
2501_92924845 <niulinkai@huawei.com>
2026-04-27 16:25:49 +0800

    feats(pass): Add vaildshape for Mva DuplicateOp and RemoveRedundantOp

--- framework/src/passes/pass_utils/infer_shape_utils.cpp
+++ framework/src/passes/pass_utils/infer_shape_utils.cpp
@@ -24,12 +24,15 @@ Status InferShapeUtils::InferShape(Function& function, const std::vector<Operati
     std::vector<Operation*> opList;
     std::unordered_set<Operation*> targetOpSet;
 
-    if (targetOps.empty()) {
+    std::vector<Operation*> mutableTargetOps = targetOps;
+    mutableTargetOps.erase(std::remove_if(mutableTargetOps.begin(), mutableTargetOps.end(),
+        [](Operation* op) {return op == nullptr || op->IsDeleted();}), mutableTargetOps.end());
+    if (mutableTargetOps.empty()) {
         opList = function.Operations().DuplicatedOpList();
     } else {
         // 去重并保留顺序，同时构建 targetOpSet
-        opList.reserve(targetOps.size());
-        for (const auto op : targetOps) {
+        opList.reserve(mutableTargetOps.size());
+        for (const auto op : mutableTargetOps) {
             if (targetOpSet.insert(op).second) {
                 opList.push_back(op);
             }
