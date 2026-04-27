78c26124561f2852995fff7c61dcdae808453331
2501_92924845 <niulinkai@huawei.com>
2026-04-27 14:03:52 +0800

    fix others conflict

--- framework/src/passes/tile_graph_pass/graph_constraint/remove_unaligned_reshape_op.cpp
+++ framework/src/passes/tile_graph_pass/graph_constraint/remove_unaligned_reshape_op.cpp
@@ -39,9 +39,9 @@ Status RemoveUnalignedReshape::RunOnFunction(Function& function)
         auto& newCopyOut = function.AddRawOperation(Opcode::OP_COPY_OUT, {a.input}, {a.output});
         newCopyOut.SetOpAttribute(
             std::make_shared<CopyOpAttribute>(
-            a.from, OpImmediate::Specified(a.toOffset), 
+                a.from, OpImmediate::Specified(a.toOffset), 
                 OpImmediate::Specified(newCopyOut.iOperand.front()->oriShape),
-            OpImmediate::Specified(newCopyOut.oOperand.front()->tensor->GetDynRawShape())));
+                OpImmediate::Specified(newCopyOut.oOperand.front()->tensor->GetDynRawShape())));
         auto producerOp = *(a.input->GetProducers().begin());
         newCopyOut.UpdateSubgraphID(producerOp->GetSubgraphID());
         APASS_LOG_INFO_F(
@@ -70,8 +70,7 @@ Status RemoveUnalignedReshape::RunOnFunction(Function& function)
 LogicalTensorPtr RemoveUnalignedReshape::InsertIOTensor(
     Function& function, Operation& op, std::unordered_map<OverlaprawMagic, std::shared_ptr<RawTensor>>& rawIO,
     LogicalTensorPtr& ioTensor)
-{   
-    (void)op;
+{
     (void)op;
     if (rawIO.count(ioTensor->tensor->rawmagic) == 0) {
         auto reshapeRawTensor =
