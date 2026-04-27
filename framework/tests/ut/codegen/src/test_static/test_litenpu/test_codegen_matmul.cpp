852cc97a847d3c4fc84e2b72f10e3e2ac781a96d
GBowen666 <libowen58@huawei.com>
2026-04-27 09:21:51 +0800

    feat(pass): support LiteNPU pass changes and add ut for LiteNPU

---
+++ framework/tests/ut/codegen/src/test_static/test_litenpu/test_codegen_matmul.cpp
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_codegen_matmul.cpp
+ * \brief
+ */
+
+#include "gtest/gtest.h"
+#include "interface/interpreter/calc.h"
+#include "interface/tensor/logical_tensor.h"
+#include "interface/tensor/raw_tensor.h"
+#include "interface/configs/config_manager.h"
+#include "tilefwk/tilefwk.h"
+#include "interface/inner/tilefwk.h"
+#include "interface/interpreter/calc.h"
+#include "codegen/codegen.h"
+#include "codegen/npu/litenpu/codegen_litenpu.h"
+
+using namespace npu::tile_fwk;
+
+class LiteNPUCodeGenMatmul : public testing::Test {
+public:
+    static void TearDownTestCase() {}
+
+    static void SetUpTestCase() {}
+
+    void SetUp() override
+    {
+        config::Reset();
+        config::SetHostOption(COMPILE_STAGE, CS_EXECUTE_GRAPH);
+        config::SetCodeGenOption<std::string>(PLATFORM_SOC_VERSION, "Kirin9030");
+    }
+
+    void TearDown() override {}
+};
