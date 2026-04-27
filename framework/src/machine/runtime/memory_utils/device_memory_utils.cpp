288594d8e0ffc4f279233e99b98771425655002b
arthur_morgan_hw <lipeijie@huawei.com>
2026-04-27 19:03:48 +0800

    refector(machine): Refactoring memory utils

---
+++ framework/src/machine/runtime/memory_utils/device_memory_utils.cpp
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file device_memory_utils.cpp
+ * \brief device memory utils
+ */
+
+#include "machine/runtime/memory_utils/device_memory_utils.h"
+
+namespace npu::tile_fwk::dynamic {
+
+}
