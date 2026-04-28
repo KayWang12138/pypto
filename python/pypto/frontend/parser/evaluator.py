13b4293b75172c33e31a88f0a861185f2cb1914d
sunhaoxuan2024 <sunhaoxuan3@h-partners.com>
2026-04-28 15:59:29 +0800

    feat(DFX): Log simpilfication

--- python/pypto/frontend/parser/evaluator.py
+++ python/pypto/frontend/parser/evaluator.py
@@ -162,8 +162,8 @@ class ExprEvaluator:
             dict_locals.update(_EVAL_BUILTIN_OVERRIDES)
             try:
                 return eval(exe, {}, dict_locals)  # pylint: disable=eval-used
-            except Exception as e:
-                raise ParserError(node, f"{type(e).__name__}: {e}") from e
+            except Exception:
+                raise 
         elif isinstance(node, ast.Expr):
             # Case 2: a expression in a statement
             mod = ast.fix_missing_locations(ast.Module(body=[node], type_ignores=[]))
