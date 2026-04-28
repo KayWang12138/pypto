ea6c2c66adf14af89d8d191c39d1764b8323c7d7
dingsinuo <dingsinuo@huawei.com>
2026-04-28 19:23:25 +0800

    fix(operation): Modify tmp size setting for sin/cos/exp2/expm1/round operations

--- framework/src/interface/tileop/vector/pto_tile.h
+++ framework/src/interface/tileop/vector/pto_tile.h
@@ -90,6 +90,25 @@ __aicore__ inline size_t GenTileOffset(const T& tensor, const TileOffset4Dim& of
     return offset;
 }
 
+template <typename LA>
+__aicore__ inline size_t GenTileOffset(const LA& layout, const TileOffset& offsets)
+{
+    size_t offset = Std::get<DIM_1ST>(offsets) * layout.template GetStrideDim<DIM_1ST, MAX_DIMS>();
+    offset += Std::get<DIM_2ND>(offsets) * layout.template GetStrideDim<DIM_2ND, MAX_DIMS>();
+    offset += Std::get<DIM_3RD>(offsets) * layout.template GetStrideDim<DIM_3RD, MAX_DIMS>();
+    return offset;
+}
+
+template <typename LA>
+__aicore__ inline size_t GenTileOffset(const LA& layout, const TileOffset4Dim& offsets)
+{
+    size_t offset = Std::get<DIM_1ST>(offsets) * layout.template GetStrideDim<DIM_1ST, MAX_DIMS>();
+    offset += Std::get<DIM_2ND>(offsets) * layout.template GetStrideDim<DIM_2ND, MAX_DIMS>();
+    offset += Std::get<DIM_3RD>(offsets) * layout.template GetStrideDim<DIM_3RD, MAX_DIMS>();
+    offset += Std::get<DIM_4TH>(offsets) * layout.template GetStrideDim<DIM_4TH, MAX_DIMS>();
+    return offset;
+}
+
 template <typename T, bool Mergeable = false>
 __aicore__ inline constexpr size_t GetMergedAxisIfNeed()
 {
@@ -178,7 +197,14 @@ public:
 
     __aicore__ inline void Assign(T& tensor, const TileOffset& offsets)
     {
-        pto::TASSIGN(data_, (uint64_t)(tensor.GetAddr() + GenTileOffset(tensor, offsets) * sizeof(typename T::Type)));
+        const auto layout = tensor.GetLayout();
+        pto::TASSIGN(data_, (uint64_t)(tensor.GetAddr() + GenTileOffset(layout, offsets) * sizeof(typename T::Type)));
+    }
+
+    template <typename LA>
+    __aicore__ inline void Assign(LA& layout, uint64_t addr, const TileOffset& offsets)
+    {
+        pto::TASSIGN(data_, (uint64_t)(addr + GenTileOffset(layout, offsets) * sizeof(typename T::Type)));
     }
 
 private:
