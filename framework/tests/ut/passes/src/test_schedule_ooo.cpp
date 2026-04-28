47e0d0e904e151ff7a682c260a4547071fb26fcc
gggxinmeng <guoxinmeng@huawei.com>
2026-04-28 11:45:06 +0800

    feat(Pass): Fix codecheck bug

--- framework/tests/ut/passes/src/test_schedule_ooo.cpp
+++ framework/tests/ut/passes/src/test_schedule_ooo.cpp
@@ -2063,7 +2063,8 @@ TEST_F(ScheduleOoOTest, TestL1SpillBufferFailed)
     InitSpillInfo(spillInfo, 0, ubCopyL1);
     int num = 0;
     bool isFinish = false;
-    res = oooSchedule.CreateSpecialL1Copyout(spillInfo, spillCopyout, num, isFinish, true, 7);
+    size_t pcIdx = 7;
+    res = oooSchedule.CreateSpecialL1Copyout(spillInfo, spillCopyout, num, isFinish, true, pcIdx);
     EXPECT_EQ(res, FAILED);
 }
 
