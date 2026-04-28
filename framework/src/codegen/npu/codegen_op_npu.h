ca4af6ef4e22316828b170d770ac0fabfdaa7b54
luyakun <luyakun@h-partners.com>
2026-04-28 16:50:10 +0800

    Conv api support NZ2NZ mode

--- framework/src/codegen/npu/codegen_op_npu.h
+++ framework/src/codegen/npu/codegen_op_npu.h
@@ -59,9 +59,11 @@ public:
     std::string GenMemL1ToBt() const;
     std::string GenMemL1CopyIn() const;
     std::string GenMemL1CopyOut() const;
-    std::string GetConvCopyInMode() const;
+    int64_t GetConvCopyInMode() const;
     std::string GetConvCopyOutMode() const;
     std::string GenMemL1CopyInConv() const;
+    std::string GenMemL1CopyInConvNZ2NZ(
+        const std::string& srcTensor, const std::string& dstTensor, const std::string& copyInModeStr) const;
     std::string GenMemL1CopyOutConv() const;
     std::string GenMemL1ToFB() const;
     std::string GenMemL0CCopyOut() const;
